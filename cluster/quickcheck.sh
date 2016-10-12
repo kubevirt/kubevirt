315c2c5632076745c6994333dda95b0be656c27c
Stu Gott <sgott@redhat.com>
2016-10-12 09:52:31 -0400

    strip newlines from node-name if necessary
    Change-Id: I74fc950ab6938f1f1304507fa8857f4d2636f0d4
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- cluster/quickcheck.sh
+++ cluster/quickcheck.sh
@@ -5,9 +5,7 @@ vagrant ssh master -c 'kubectl delete pods -l domain=testvm'
 sleep 2
 vagrant ssh master -c 'curl -X POST -H "Content-Type: application/xml" http://192.168.200.2:8182/api/v1/domain/raw -d @/vagrant/cluster/testdomain.xml'
 sleep 10
-# TODO this gives me the output "$'master'\r" instead of plain "master"
-#NODE=$(vagrant ssh master -c "kubectl get pods -o json -l domain=testvm | jq '.items[].spec.nodeName' -r")
-NODE=master
+NODE=$(vagrant ssh master -c "kubectl get pods -o json -l domain=testvm | jq '.items[].spec.nodeName' -r" | sed -e 's/[[:space:]]*$//')
 
 # VM can also spawn on node
 vagrant ssh $NODE -c "sudo /vagrant/cluster/verify-qemu-kube testvm"
