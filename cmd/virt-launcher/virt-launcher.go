53fb246538d6d4da86dfc25bff2dc97fce4ba363
Francesco Romani <fromani@redhat.com>
2016-10-20 10:54:32 +0200

    virt-launcher: reorganize the flow
    Make the code easier to compose by splitting it into functions,
    in order ro and to make it trivial to implement --receive-only
    in a followup patch
    
    Change-Id: I24cd7268b32100b41c2b7176ee6ec12ce841bdac
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/virt-launcher/virt-launcher.go
+++ cmd/virt-launcher/virt-launcher.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,6 +15,14 @@ import (
 	"strings"
 )
 
+type virtlauncher struct {
+	domainXML  string
+	domainUUID string
+	connURI    string
+	user       string
+	pass       string
+}
+
 func main() {
 	xmlPath := flag.String("domain-path", "/var/run/virt-launcher/dom.xml", "Where to look for the domain xml.")
 	downwardAPIPath := flag.String("downward-api-path", "", "Load domain from this downward API file")
@@ -22,28 +31,75 @@ func main() {
 	pass := flag.String("pass", "shibboleth", "Libvirt password")
 	flag.Parse()
 
-	xml := readDomainXML(*xmlPath, *downwardAPIPath)
-	if xml == "" {
-		log.Fatal("Could not load domain XML. The resulting XML is empty")
+	launcher := virtlauncher{
+		connURI: *conUri,
+		user:    *user,
+		pass:    *pass,
 	}
-	log.Print("Domain description loaded.")
 
-	conn := buildLocalConnection(*conUri, *user, *pass)
-	log.Print("Libvirt connection established.")
+	launcher.ReadDomainXML(*xmlPath, *downwardAPIPath)
+	launcher.CreateDomain()
+	waitUntilSignal()
+	launcher.DestroyDomain()
+}
 
+func (vl *virtlauncher) CreateDomain() {
+	conn, err := libvirt.NewVirConnectionWithAuth(vl.connURI, vl.user, vl.pass)
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to libvirt using %s: %s", vl.connURI, err))
+	}
 	defer func() {
-		if res, _ := conn.CloseConnection(); res != 0 {
-			log.Fatalf("CloseConnection() == %d, expected 0", res)
+		if _, closeErr := conn.CloseConnection(); closeErr != nil {
+			log.Fatalf("CloseConnection() failed: %s", closeErr)
 		}
+		log.Print("Connection closed")
 	}()
 
-	// Launch VM
-	dom, createErr := conn.DomainCreateXML(xml, 0)
-	if createErr != nil {
-		log.Fatal(createErr)
-	}
-	log.Print("Domain started.")
+	log.Print("Libvirt connection established")
 
+	// Launch VM
+	dom, err := conn.DomainCreateXML(vl.domainXML, 0)
+	if err != nil {
+		panic(fmt.Sprintf("Could not create the libvirt domain: %s", err))
+	}
+
+	vl.domainUUID, err = dom.GetUUIDString()
+	if err != nil {
+		panic(fmt.Sprintf("Could not get the domain UUID (as string): %s", err))
+	}
+
+	log.Print("Domain started")
+}
+
+func (vl *virtlauncher) DestroyDomain() {
+	conn, err := libvirt.NewVirConnectionWithAuth(vl.connURI, vl.user, vl.pass)
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to libvirt using %s: %s", vl.connURI, err))
+	}
+	defer func() {
+		if closeRes, _ := conn.CloseConnection(); closeRes != 0 {
+			log.Fatalf("CloseConnection() == %d, expected 0", closeRes)
+		}
+		log.Print("Connection closed")
+	}()
+
+	log.Print("Libvirt connection established")
+
+	dom, err := conn.LookupByUUIDString(vl.domainUUID)
+	if err != nil {
+		panic(fmt.Sprintf("Could not find domain %s: %s", vl.domainUUID, err))
+	}
+
+	err = dom.Destroy()
+	if err != nil {
+		panic(fmt.Sprintf("Domain destroy failed: %s", err))
+	}
+
+	log.Print("Domain destroyed")
+
+}
+
+func waitUntilSignal() {
 	// Wait for termination
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt,
@@ -53,29 +109,21 @@ func main() {
 		syscall.SIGQUIT,
 	)
 
-	log.Print("Waiting forever ...")
+	log.Printf("Waiting forever...")
 	s := <-c
-	log.Print("Got signal:", s)
-
-	destroyErr := dom.Destroy()
-	if destroyErr != nil {
-		log.Fatal(destroyErr)
-	}
-	log.Print("Domain destroyed.")
+	log.Print("Got signal: ", s)
 }
 
-func readDomainXML(xmlPath string, downwardAPIPath string) string {
-	var xml string
-
+func (vl *virtlauncher) ReadDomainXML(xmlPath string, downwardAPIPath string) {
 	if downwardAPIPath == "" {
-		log.Print("Loading Domain from XML file.")
+		log.Print("Loading Domain from XML file")
 		rawXML, err := ioutil.ReadFile(xmlPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-		xml = string(rawXML)
+		vl.domainXML = string(rawXML)
 	} else {
-		log.Print("Loading Domain from downward API file.")
+		log.Print("Loading Domain from downward API file")
 		f, err := os.Open(downwardAPIPath)
 		if err != nil {
 			log.Fatal(err)
@@ -85,21 +133,16 @@ func readDomainXML(xmlPath string, downwardAPIPath string) string {
 		for scanner.Scan() {
 			line := scanner.Text()
 			if strings.HasPrefix(line, `domainXML="`) {
-				xml = DecodeDomainXML(strings.Trim(strings.TrimPrefix(line, "domainXML="), `"`))
+				vl.domainXML = DecodeDomainXML(strings.Trim(strings.TrimPrefix(line, "domainXML="), `"`))
 			}
 		}
 
 	}
 
-	return xml
-}
-
-func buildLocalConnection(uri string, user string, pass string) libvirt.VirConnection {
-	conn, err := libvirt.NewVirConnectionWithAuth(uri, user, pass)
-	if err != nil {
-		panic(err)
+	if vl.domainXML == "" {
+		panic("Could not load domain XML. The resulting XML is empty")
 	}
-	return conn
+	log.Print("Domain description loaded.")
 }
 
 func DecodeDomainXML(domainXML string) string {
