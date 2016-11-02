012288bb1ceb6788f6ebd4c48b8017dfeecec479
Fabian Deutsch <fabiand@redhat.com>
2016-11-02 11:28:21 +0100

    libvirtd: Save augeas changes
    Change-Id: Iad239a6bb549ab3b2a7c7efb207ef4431d50d04d
    Signed-off-by: Fabian Deutsch <fabiand@redhat.com>

--- cluster/images/libvirtd/libvirtd.sh
+++ cluster/images/libvirtd/libvirtd.sh
@@ -4,14 +4,15 @@ set -xe
 
 # HACK
 # Use hosts's /dev to see new devices and allow macvtap
-mkdir /odev
-mount --rbind /dev /odev
-mount --rbind /host/dev /dev
-# Keep some devices from the original /dev
-mount --rbind /odev/shm /dev/shm
-mount --rbind /odev/mqueue /dev/mqueue
-# Keep ptmx/pts for pty creation
-mount --rbind /odev/pts /dev/pts
-mount --rbind /dev/pts/ptmx /dev/ptmx
+mkdir /odev && {
+  mount --rbind /dev /odev
+  mount --rbind /host/dev /dev
+  # Keep some devices from the original /dev
+  mount --rbind /odev/shm /dev/shm
+  mount --rbind /odev/mqueue /dev/mqueue
+  # Keep ptmx/pts for pty creation
+  mount --rbind /odev/pts /dev/pts
+  mount --rbind /dev/pts/ptmx /dev/ptmx
+}
 
 /usr/sbin/libvirtd
