396cad9da297214c3d9b25015f9d2858555e0289
Roman Mohr <rmohr@redhat.com>
2016-11-15 19:17:42 +0100

    Extend libvirt schema definition
    Change-Id: I1683aae32a280534f0805d9549fdebe4f49c9624
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- pkg/libvirt/schema.go
+++ pkg/libvirt/schema.go
@@ -3,45 +3,246 @@ package libvirt
 import (
 	"encoding/xml"
 	"kubevirt/core/pkg/precond"
+	"net"
 )
 
 type Domain struct {
-	XMLName xml.Name `xml:"domain"`
-	Name    string   `xml:"name"`
-	Memory  Memory   `xml:"memory"`
-	Type    string   `xml:"type,attr"`
-	OS      string   `xml:"os>type"`
-	Devices Devices  `xml:"devices"`
+	XMLName xml.Name `xml:"domain" json:"-"`
+	Name    string   `xml:"name" json:"name"`
+	UUID    string   `xml:"uuid,omitempty" json:"uuid,omitempty"`
+	Memory  Memory   `xml:"memory" json:"memory"`
+	Type    string   `xml:"type,attr" json:"type"`
+	OS      OS       `xml:"os" json:"os"`
+	SysInfo *SysInfo `xml:"sysinfo,omitempty" json:"sysInfo,omitempty"`
+	Devices Devices  `xml:"devices" json:"devices"`
+	Clock   *Clock   `xml:"clock,omitempty" json:"clock,omitempty"`
 }
 
 type Memory struct {
-	Value uint   `xml:",chardata"`
-	Unit  string `xml:"unit,attr"`
+	Value uint   `xml:",chardata" json:"value"`
+	Unit  string `xml:"unit,attr" json:"unit"`
 }
 
 type Devices struct {
-	Emulator   Emulator    `xml:"emulator"`
-	Interfaces []Interface `xml:"interface"`
+	Emulator   string      `xml:"emulator" json:"emulator"`
+	Interfaces []Interface `xml:"interface" json:"interfaces,omitempty"`
+	Channels   []Channel   `xml:"channel" json:"channels,omitempty"`
+	Video      []Video     `xml:"video" json:"video,omitempty"`
+	Graphics   []Graphics  `xml:"graphics" json:"graphics,omitempty"`
+	Ballooning *Ballooning `xml:"memballoon,omitempty" json:"memballoon,omitempty"`
 }
 
-type Emulator struct {
-	Value string `xml:",chardata"`
+// BEGIN Disk -----------------------------
+
+type Disk struct {
+	Device     string      `xml:"device,attr" json:"device"`
+	Snapshot   string      `xml:"snapshot,attr" json:"shapshot"`
+	Type       string      `xml:"type,attr" json:"type"`
+	DiskSource DiskSource  `xml:"source" json:"diskSource"`
+	DiskTarget DiskTarget  `xml:"target" json:"diskTarget"`
+	Serial     string      `xml:"serial,omitempty" json:"serial,omitempty"`
+	Driver     *DiskDriver `xml:"driver,omitempty" json:"driver,omitempty"`
+	ReadOnly   *ReadOnly   `xml:"readonly,omitempty" json:"readOnly,omitempty"`
 }
 
+type ReadOnly struct{}
+
+type DiskSource struct {
+	File          string `xml:"file,attr" json:"file"`
+	StartupPolicy string `xml:"startupPolicy,attr,omitempty" json:"startupPolicy,omitempty"`
+}
+
+type DiskTarget struct {
+	Bus    string `xml:"bus,attr" json:"bus"`
+	Device string `xml:"dev,attr" json:"dev"`
+}
+
+type DiskDriver struct {
+	Cache       string `xml:"cache,attr" json:"cache"`
+	ErrorPolicy string `xml:"error_policy,attr" json:"errorPolicy"`
+	IO          string `xml:"io,attr" json:"io"`
+	Name        string `xml:"name,attr" json:"name"`
+	Type        string `xml:"type,attr" json:"type"`
+}
+
+// END Disk -----------------------------
+
+// BEGIN Inteface -----------------------------
 type Interface struct {
-	Type   string `xml:"type,attr"`
-	Source Source `xml:"source"`
+	Type      string     `xml:"type,attr" json:"type"`
+	Source    Source     `xml:"source" json:"source"`
+	Model     *Model     `xml:"model,omitempty" json:"model,omitempty"`
+	Mac       *Mac       `xml:"mac,omitempty" json:"mac,omitempty"`
+	BandWidth *BandWidth `xml:"bandwidth,omitempty" json:"bandwidth,omitempty"`
+	BootOrder *BootOrder `xml:"boot,omitempty" json:"boot,omitempty"`
+	LinkState *LinkState `xml:"link,omitempty" json:"link,omitempty"`
+	FilterRef *FilterRef `xml:"filterref,omitempty" json:"filterRef,omitempty"`
+}
+
+type LinkState struct {
+	State string `xml:"state,attr" json:"state"`
+}
+
+type BandWidth struct {
+}
+
+type BootOrder struct {
+	Order uint `xml:"order,attr" json:"order"`
+}
+
+type Mac struct {
+	Mac net.HardwareAddr `xml:"address,attr" json:"address"`
+}
+
+type FilterRef struct {
+	Filter string `xml:"filter,attr" json:"filter"`
 }
 
 type Source struct {
-	Network string `xml:"network,attr"`
+	Network string `xml:"network,attr,omitempty" json:"network,omitempty"`
+	Device  string `xml:"dev,attr,omitempty" json:"device,omitempty"`
+	Bridge  string `xml:"bridge,attr,omitempty" json:"bridge,omitempty"`
 }
+
+type Model struct {
+	Type string `xml:"type" json:"type"`
+}
+
+// END Inteface -----------------------------
+//BEGIN OS --------------------
+
+type OS struct {
+	Type      OSType    `xml:"type" json:"type"`
+	SMBios    *SMBios   `xml:"smbios,omitempty" json:"smBIOS,omitempty"`
+	BootOrder []Boot    `xml:"boot" json:"bootOrder"`
+	BootMenu  *BootMenu `xml:"bootmenu,omitempty" json:"bootMenu,omitempty"`
+	BIOS      *BIOS     `xml:"bios,omitempty" json:"bios,omitempty"`
+}
+
+type OSType struct {
+	OS      string `xml:",chardata" json:"os"`
+	Arch    string `xml:"arch,attr,omitempty" json:"arch,omitempty"`
+	Machine string `xml:"machine,attr,omitempty" json:"machine,omitempty"`
+}
+
+type SMBios struct {
+	Mode string `xml:"mode,attr" json:"mode"`
+}
+
+type NVRam struct {
+	NVRam    string `xml:",chardata,omitempty" json:"nvRam,omitempty"`
+	Template string `xml:"template,attr,omitempty" json:"template,omitempty"`
+}
+
+type Boot struct {
+	Dev string `xml:"dev,attr" json:"dev"`
+}
+
+type BootMenu struct {
+	Enabled bool  `xml:"enabled,attr" json:"enabled,omitempty"`
+	Timeout *uint `xml:"timeout,attr,omitempty" json:"timeout,omitempty"`
+}
+
+// TODO <loader readonly='yes' secure='no' type='rom'>/usr/lib/xen/boot/hvmloader</loader>
+type BIOS struct {
+}
+
+// TODO <bios useserial='yes' rebootTimeout='0'/>
+type Loader struct {
+}
+
+// Required for SMBios.Mode == 'sysinfo'
+type SysInfo struct {
+	Type      string  `xml:"type,attr" json:"type"`
+	System    []Entry `xml:"system>entry" json:"system"`
+	BIOS      []Entry `xml:"bios>entry" json:"bios"`
+	BaseBoard []Entry `xml:"baseBoard>entry" json:"baseBoard"`
+}
+
+type Entry struct {
+	Name  string `xml:"name" json:"name"`
+	Value string `xml:",chardata" json:"value"`
+}
+
+//END OS --------------------
+
+//BEGIN Clock --------------------
+
+type Clock struct {
+}
+
+type Timer struct {
+	Name       string `xml:"name,attr" json:"name"`
+	TickPolicy string `xml:"tickpolicy,attr,omitempty" json:"tickPolicy,omitempty"`
+	Present    string `xml:"present,attr,omitempty" json:"present,omitempty"`
+}
+
+//END Clock --------------------
+
+//BEGIN Channel --------------------
+type Channel struct {
+	Type   string         `xml:"type,attr" json:"type"`
+	Source ChannelSource  `xml:"target,omitempty" json:"source,omitempty"`
+	Target *ChannelTarget `xml:"target,omitempty" json:"target,omitempty"`
+}
+
+type ChannelTarget struct {
+	Name    string `xml:"name,attr,omitempty" json:"name,omitempty"`
+	Type    string `xml:"type,attr" json:"type"`
+	Address string `xml:"address,attr,omitempty" json:"address,omitempty"`
+	Port    uint   `xml:"port,attr,omitempty" json:"port,omitempty"`
+}
+
+type ChannelSource struct {
+	Mode string `xml:"mode,attr" json:"mode"`
+	Path string `xml:"path,attr" json:"path"`
+}
+
+//END Channel --------------------
+
+//BEGIN Video -------------------
+/*
+<graphics autoport="yes" defaultMode="secure" listen="0" passwd="*****" passwdValidTo="1970-01-01T00:00:01" port="-1" tlsPort="-1" type="spice" />
+*/
+
+type Video struct {
+	Mode VideoMode `xml:"mode"`
+}
+
+type VideoMode struct {
+	Type   string `xml:"type,attr" json:"type"`
+	Heads  uint   `xml:"heads,attr,omitempty" json:"heads,omitempty"`
+	Ram    uint   `xml:"ram,attr,omitempty" json:"ram,omitempty"`
+	VRam   uint   `xml:"vram,attr,omitempty" json:"vram,omitempty"`
+	VGAMem uint   `xml:"vgamem,attr,omitempty" vgamem:"vram,omitempty"`
+}
+
+type Graphics struct {
+	AutoPort      string `xml:"autoPort,attr,omitempty" json:"autoPort,omitempty"`
+	DefaultMode   string `xml:"defaultMode,omitempty" json:"defaultMode,omitempty"`
+	Listen        string `xml:"listen,omitempty" json:"listen,omitempty"`
+	PasswdValidTo string `xml:"passwdValidTo,omitempty" json:"passwdValidTo,omitempty"`
+	Port          int    `xml:"port,omitempty" json:"port,omitempty"`
+	TLSPort       int    `xml:"tlsPort,omitempty" json:"tlsPort,omitempty"`
+	Type          string `xml:"type" json:"type"`
+}
+
+//END Video -------------------
+
+type Ballooning struct {
+	Model string `xml:"model,attr" json:"model"`
+}
+
+type RandomGenerator struct {
+}
+
+// TODO ballooning, rng, cpu ...
 
 func NewMinimalVM(vmName string) *Domain {
 	precond.MustNotBeEmpty(vmName)
-	domain := Domain{OS: "hvm", Type: "qemu", Name: vmName}
+	domain := Domain{OS: OS{Type: OSType{OS: "hvm"}}, Type: "qemu", Name: vmName}
 	domain.Memory = Memory{Unit: "KiB", Value: 8192}
-	domain.Devices = Devices{Emulator: Emulator{Value: "/usr/local/bin/qemu-x86_64"}}
+	domain.Devices = Devices{Emulator: "/usr/local/bin/qemu-x86_64"}
 	domain.Devices.Interfaces = []Interface{
 		{Type: "network", Source: Source{Network: "kubevirt-net"}},
 		{Type: "network", Source: Source{Network: "default"}},
--- pkg/libvirt/schema_test.go
+++ pkg/libvirt/schema_test.go
@@ -1,12 +1,14 @@
 package libvirt
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var example = `
+var exampleXML = `
 <domain type="qemu">
   <name>testvm</name>
   <memory unit="KiB">8192</memory>
@@ -25,6 +27,40 @@ var example = `
 </domain>
 `
 
+var exampleJSON = `
+{
+   "name":"testvm",
+   "memory":{
+      "value":8192,
+      "unit":"KiB"
+   },
+   "type":"qemu",
+   "os":{
+      "type":{
+         "os":"hvm"
+      },
+      "bootOrder":null
+   },
+   "devices":{
+      "emulator":"/usr/local/bin/qemu-x86_64",
+      "interfaces":[
+         {
+            "type":"network",
+            "source":{
+               "network":"kubevirt-net"
+            }
+         },
+         {
+            "type":"network",
+            "source":{
+               "network":"default"
+            }
+         }
+      ]
+   }
+}
+`
+
 var _ = Describe("Schema", func() {
 	Context("With schema", func() {
 		It("Generate expected libvirt xml", func() {
@@ -37,14 +73,32 @@ var _ = Describe("Schema", func() {
 			domain.XMLName.Local = "domain"
 			Expect(newDomain).To(Equal(*domain))
 		})
+		It("Generate expected libvirt json", func() {
+			domain := NewMinimalVM("testvm")
+			buf, err := json.Marshal(domain)
+			Expect(err).To(BeNil())
+			newDomain := Domain{}
+			err = json.Unmarshal(buf, &newDomain)
+			Expect(err).To(BeNil())
+			Expect(newDomain).To(Equal(*domain))
+		})
 	})
 	Context("With example schema in xml", func() {
 		It("Unmarshal into struct", func() {
 			domain := NewMinimalVM("testvm")
 			newDomain := Domain{}
-			err := xml.Unmarshal([]byte(example), &newDomain)
+			err := xml.Unmarshal([]byte(exampleXML), &newDomain)
 			Expect(err).To(BeNil())
 			domain.XMLName.Local = "domain"
+			Expect(newDomain).To(Equal(*domain))
+		})
+	})
+	Context("With example schema in json", func() {
+		It("Unmarshal into struct", func() {
+			domain := NewMinimalVM("testvm")
+			newDomain := Domain{}
+			err := json.Unmarshal([]byte(exampleJSON), &newDomain)
+			Expect(err).To(BeNil())
 			Expect(newDomain).To(Equal(*domain))
 		})
 	})
