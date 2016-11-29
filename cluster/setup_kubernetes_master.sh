b60fcb7d9ba109b4069720a701fbb5247fcf9227
Roman Mohr <rmohr@redhat.com>
2016-11-29 23:47:15 +0100

    Use eth0 for kubeadm deployment
    Some components are sometimes still falling back to eth0 even if we
    advertise the eth1 interface. Especially weave seems to have problems
    with that. Therefore advertise the eth0 IP during deployment.
    
    Change-Id: I50750d15301a4783f51453f2158e78060260b1d3
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster/setup_kubernetes_master.sh
+++ cluster/setup_kubernetes_master.sh
@@ -11,8 +11,10 @@ bash ./setup_kubernetes_common.sh
 yum install -y cockpit cockpit-kubernetes
 systemctl enable cockpit.socket && systemctl start cockpit.socket
 
+ADVERTISED_MASTER_IP=`dig +short master`
+
 # Create the master
-kubeadm init --api-advertise-addresses=$MASTER_IP --pod-network-cidr=10.244.0.0/16 --token abcdef.1234567890123456 --use-kubernetes-version v1.4.5
+kubeadm init --api-advertise-addresses=$ADVERTISED_MASTER_IP --pod-network-cidr=10.244.0.0/16 --token abcdef.1234567890123456 --use-kubernetes-version v1.4.5
 
 set +e
 
@@ -28,9 +30,13 @@ set -e
 
 # Fix proxy definition
 # https://github.com/kubernetes/kubeadm/issues/66#issuecomment-262394822
-kubectl -n kube-system get ds -l 'component=kube-proxy' -o json   | jq '.items[0].spec.template.spec.containers[0].command |= .+ ["--proxy-mode=userspace"]'   |   kubectl apply -f - && kubectl -n kube-system delete pods -l 'component=kube-proxy'
+#kubectl -n kube-system get ds -l 'component=kube-proxy' -o json   | jq '.items[0].spec.template.spec.containers[0].command |= .+ ["--proxy-mode=userspace"]'   |   kubectl apply -f - && kubectl -n kube-system delete pods -l 'component=kube-proxy'
 
-kubectl create -s 127.0.0.1:8080 -f kube-$NETWORK_PROVIDER.yaml
+if [ "$NETWORK_PROVIDER" == "weave" ]; then 
+  kubectl apply -s 127.0.0.1:8080 -f https://git.io/weave-kube
+else
+  kubectl create -s 127.0.0.1:8080 -f kube-$NETWORK_PROVIDER.yaml
+fi
 
 # Allow scheduling pods on master
 kubectl -s 127.0.0.1:8080 taint nodes --all dedicated-
--- cluster/setup_kubernetes_node.sh
+++ cluster/setup_kubernetes_node.sh
@@ -5,15 +5,17 @@
 # export MASTER_IP=192.168.200.2
 bash ./setup_kubernetes_common.sh
 
+ADVERTISED_MASTER_IP=`dig +short master`
+
 set +e
 
 echo 'Trying to register myself...'
 # Skipping preflight checks because of https://github.com/kubernetes/kubeadm/issues/6
-kubeadm join --token abcdef.1234567890123456 $MASTER_IP --skip-preflight-checks
+kubeadm join --token abcdef.1234567890123456 $ADVERTISED_MASTER_IP --skip-preflight-checks
 while [ $? -ne 0 ]
 do
   sleep 30
   echo 'Trying to register myself...'
   # Skipping preflight checks because of https://github.com/kubernetes/kubeadm/issues/6
-  kubeadm join --token abcdef.1234567890123456 $MASTER_IP --skip-preflight-checks
+  kubeadm join --token abcdef.1234567890123456 $ADVERTISED_MASTER_IP --skip-preflight-checks
 done
