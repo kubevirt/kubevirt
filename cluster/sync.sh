104167f8fa45fcbe54a0b39ef3de1ccdf7fcaeec
Stu Gott <sgott@redhat.com>
2016-12-02 07:50:54 -0500

    Report an error message when master cannot be synched
    Change-Id: Id44fe428ad58c55ce2425211f632f547f81481a3
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+vagrant ssh-config master 2>&1 | grep "not yet ready for SSH" >/dev/null \
+        && { echo "Master node is not up"; exit 1; }
+
 OPTIONS=`vagrant ssh-config master | grep -v '^Host ' | awk -v ORS=' ' 'NF{print "-o " $1 "=" $2}'`
 
 scp $OPTIONS master:/usr/bin/kubectl cluster/.kubectl
