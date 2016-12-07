34d4782555a3093831677e30aeba6a854f866688
Roman Mohr <rmohr@redhat.com>
2016-12-07 18:04:42 +0100

    Add utility for comment extraction from structs
    The added tool allows extraction of documentation strings from golang
    structs and provides them in a builder consumable form. It generates
    maps compatible with the swagger builder in go-restful [1].
    
    [1] https://github.com/emicklei/go-restful/blob/7f33558714877d72a21c27e88fe140f264c9e7c9/swagger/README.md
    
    Change-Id: I051f38d9550fff740ddcfb487f3a6f05e3f00b63
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ desc/description_generator.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sourceFile := flag.String("in", "", "golang file containing strucs for swagger")
+	targetFile := flag.String("out", "", "target file where description should be written")
+	flag.Parse()
+
+	if *sourceFile == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *targetFile == "" {
+		if strings.HasSuffix(*sourceFile, "_test.go") {
+			*targetFile = strings.TrimSuffix(*sourceFile, "_test.go") + "_swagger_generated_test.go"
+		} else {
+			*targetFile = strings.TrimSuffix(*sourceFile, ".go") + "_swagger_generated.go"
+		}
+	}
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, *sourceFile, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+	c := Parse(f)
+
+	var file *os.File
+	if *targetFile == "-" {
+		file = os.Stdout
+	} else {
+		file, err = os.Create(*targetFile)
+		defer file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}
+	for x := range c {
+		fmt.Fprintln(file, x)
+	}
+}
+
+func Parse(f *ast.File) chan string {
+	c := make(chan string)
+	go func() {
+		defer close(c)
+		re := regexp.MustCompile(`^([^,]+).*`)
+		firstStruct := true
+		for _, decl := range f.Decls {
+
+			gendecl, ok := decl.(*ast.GenDecl)
+			if !ok {
+				continue
+			}
+			if len(gendecl.Specs) == 0 {
+				continue
+			}
+			typeSpec, ok := gendecl.Specs[0].(*ast.TypeSpec)
+			if !ok {
+				continue
+			}
+			structDecl, ok := typeSpec.Type.(*ast.StructType)
+			if !ok {
+				continue
+			}
+
+			if firstStruct {
+				firstStruct = false
+				c <- "package " + f.Name.Name
+			}
+			c <- ""
+			c <- fmt.Sprintf("func (%s) SwaggerDoc() map[string]string {", typeSpec.Name)
+			c <- "\treturn map[string]string{"
+
+			structDoc := strings.TrimSpace(gendecl.Doc.Text())
+			if len(structDoc) > 0 {
+				c <- "\t\t" + "\"\": " + strconv.Quote(structDoc) + ","
+			}
+
+			for _, field := range structDecl.Fields.List {
+				fieldName := field.Names[0].Name
+				if field.Tag != nil {
+					tag := reflect.StructTag(field.Tag.Value)
+					jsonTag := tag.Get("`json")
+					if len(jsonTag) > 0 {
+						matches := re.FindStringSubmatch(jsonTag)
+						if len(matches) == 2 && len(matches[1]) > 0 {
+							fieldName = matches[1]
+						}
+					}
+				}
+
+				docText := strings.TrimSpace(field.Doc.Text())
+				if len(docText) > 0 {
+					c <- "\t\t" + "\"" + fieldName + "\": " + strconv.Quote(docText) + ","
+				}
+			}
+			c <- "\t}"
+			c <- "}"
+		}
+	}()
+	return c
+}
---
+++ desc/description_generator_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"go/parser"
+	"go/token"
+	"strings"
+	"testing"
+)
+
+//Address doc
+type Address struct {
+	//Country doc
+	Country string `json:"country,omitempty"`
+	//PostCode doc
+	PostCode int `json:"postcode,omitempty"`
+}
+
+//Person doc
+//with multiline
+type Person struct {
+	//FirstName doc
+	FirstName string `json:"firstName,omitempty"`
+	//LastName doc
+	LastName string `json:",omitempty"`
+	//MiddleName doc
+	MiddleName string `json:"middleName"`
+
+	//Field without tag and
+	// multiline comment and something to escape \ "
+	HeightInPounds int
+}
+
+type UndocumentedStruct struct {
+	UndocumentedField string
+}
+
+var expected = strings.TrimSpace(`
+package main
+
+func (Address) SwaggerDoc() map[string]string {
+	return map[string]string{
+		"": "Address doc",
+		"country": "Country doc",
+		"postcode": "PostCode doc",
+	}
+}
+
+func (Person) SwaggerDoc() map[string]string {
+	return map[string]string{
+		"": "Person doc\nwith multiline",
+		"firstName": "FirstName doc",
+		"LastName": "LastName doc",
+		"middleName": "MiddleName doc",
+		"HeightInPounds": "Field without tag and\nmultiline comment and something to escape \\ \"",
+	}
+}
+
+func (UndocumentedStruct) SwaggerDoc() map[string]string {
+	return map[string]string{
+	}
+}
+`)
+
+func TestSwaggerDocGeneration(t *testing.T) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "description_generator_test.go", nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+	c := Parse(f)
+
+	buf := ""
+	for line := range c {
+		buf = buf + line + "\n"
+	}
+	buf = strings.TrimSpace(buf)
+	if buf != expected {
+		t.Fatalf("Expected: %s, got: %s", expected, buf)
+	}
+}
