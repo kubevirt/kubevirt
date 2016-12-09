64a9d2f2bff61f9926b32bfea44d97f5e43803df
Fabian Deutsch <fabiand@redhat.com>
2016-12-09 13:16:53 +0100

    Finally fix everyhing in config.sh
    Change-Id: Icb527c1c9651aeb28981f6a874875dabb79ea690
    Signed-off-by: Fabian Deutsch <fabiand@redhat.com>

--- hack/config.sh
+++ hack/config.sh
@@ -1,5 +1,5 @@
 binaries="cmd/virt-controller cmd/virt-launcher cmd/virt-handler cmd/virt-api"
-docker_images="$binaries contrib/haproxy"
+docker_images="$binaries cluster/haproxy"
 docker_prefix=kubevirt
 docker_tag=latest
 manifest_templates="`ls cluster/manifest/*.in`"
