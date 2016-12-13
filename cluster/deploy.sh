ed626e62e0c6febee01996dabb90d0dbdeb28740
Fabian Deutsch <fabiand@redhat.com>
2016-12-13 12:42:50 +0100

    cluster: Add deploy.sh
    This script can be used to create all objects in k8s.
    Previously it was pretty much tied to vagrant, this
    patch is separating the two areas (deployment and vagrant sync).
    
    Change-Id: I231d4fc9606e15e33d1c5215981835be50e211dc
    Signed-off-by: Fabian Deutsch <fabiand@redhat.com>

---
+++ cluster/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+set -ex
+
+KUBECTL=${KUBECTL:-kubectl}
+
+echo "Cleaning up ..."
+for i in `ls cluster/manifest/*.yaml`; do
+    $KUBECTL delete -f $i --grace-period 0 2>/dev/null || :
+done
+
+sleep 2
+
+echo "Deploying ..."
+for i in `ls cluster/manifest/*.yaml`; do
+    $KUBECTL create -f $i
+done
+echo "Done"
--- scripts/vagrant-sync.sh
+++ scripts/vagrant-sync.sh
@@ -10,12 +10,6 @@ for VM in `vagrant status | grep running | cut -d " " -f1`; do
   vagrant ssh $VM -c "cd /vagrant && sudo hack/build-docker.sh"
 done
 
-# Deploy all manifests files
-set +x
-for i in `ls cluster/manifest/*.yaml`; do
-    contrib/vagrant/kubectl.sh --core delete -f $i --grace-period 0 2>/dev/null || :
-done
-sleep 2
-for i in `ls cluster/manifest/*.yaml`; do
-    contrib/vagrant/kubectl.sh --core create -f $i
-done
+export KUBECTL="contrib/vagrant/kubectl.sh --core"
+
+cluster/deploy.sh
