8646f29baf6b7010cdac44f3cc4981feb4dac908
Stu Gott <sgott@redhat.com>
2016-12-23 10:08:46 -0500

    Supress spurious vagrant status output
    Vagrant can issue chatty output in response to "vagrant status",
    which in some cases can break the cluster/sync.sh script.
    
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -5,7 +5,7 @@ cluster/vagrant/sync_config.sh
 
 make all
 
-for VM in `vagrant status | grep running | cut -d " " -f1`; do
+for VM in `vagrant status | grep -v "^The Libvirt domain is running." | grep running | cut -d " " -f1`; do
   vagrant rsync $VM # if you do not use NFS
   vagrant ssh $VM -c "cd /vagrant && sudo hack/build-docker.sh"
 done
