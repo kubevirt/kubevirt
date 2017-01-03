d790820a66e194959c825b30b126eb8e19e851a6
Yaniv Bronhaim <ybronhei@redhat.com>
2017-01-03 11:51:18 +0200

    Make kubectl available from everywhere
    Set your kubevirt_path by the following and you'll be able to use
    kubectl from everywhere:
    
    export KUBEVIRT_PATH=".../Projects/kubevirt/kubevirt/"
    alias kubectl="$KUBEVIRT_PATH/cluster/kubectl.sh"
    
    I assume you run all clusters from same kubevirt directory in this
    patch.
    
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- cluster/kubectl.sh
+++ cluster/kubectl.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-source hack/config.sh
+source ${KUBEVIRT_PATH}hack/config.sh
 
-SYNC_CONFIG=cluster/vagrant/sync_config.sh
+SYNC_CONFIG=${KUBEVIRT_PATH}cluster/vagrant/sync_config.sh
 
 if [ "x$1" == "x--init" ]
 then
     exec $SYNC_CONFIG
 fi
 
-if [ -e cluster/vagrant/.kubeconfig ] && [ -e cluster/vagrant/.kubectl ] && [ "x$1" == "x--core" ];then
+if [ -e  ${KUBEVIRT_PATH}cluster/vagrant/.kubeconfig ] &&
+   [ -e ${KUBEVIRT_PATH}cluster/vagrant/.kubectl ] &&
+   [ "x$1" == "x--core" ]; then
     shift
-    cluster/vagrant/.kubectl --kubeconfig=cluster/vagrant/.kubeconfig $@
-elif [ -e cluster/vagrant/.kubectl ];then
-    cluster/vagrant/.kubectl -s http://${master_ip}:8184 $@
+    ${KUBEVIRT_PATH}cluster/vagrant/.kubectl --kubeconfig=${KUBEVIRT_PATH}cluster/vagrant/.kubeconfig $@
+elif [ -e ${KUBEVIRT_PATH}cluster/vagrant/.kubectl ];then
+    ${KUBEVIRT_PATH}cluster/vagrant/.kubectl -s http://${master_ip}:8184 $@
 else
     echo "Did you already run '$SYNC_CONFIG' to deploy kubevirt?"
 fi
--- cluster/vagrant/sync_config.sh
+++ cluster/vagrant/sync_config.sh
@@ -6,7 +6,7 @@ vagrant ssh-config master 2>&1 | grep "not yet ready for SSH" >/dev/null \
 
 OPTIONS=`vagrant ssh-config master | grep -v '^Host ' | awk -v ORS=' ' 'NF{print "-o " $1 "=" $2}'`
 
-scp $OPTIONS master:/usr/bin/kubectl cluster/vagrant/.kubectl
+scp $OPTIONS master:/usr/bin/kubectl ${KUBEVIRT_PATH}cluster/vagrant/.kubectl
 chmod u+x cluster/vagrant/.kubectl
 
-vagrant ssh master -c "sudo cat /etc/kubernetes/kubelet.conf" > cluster/vagrant/.kubeconfig
+vagrant ssh master -c "sudo cat /etc/kubernetes/kubelet.conf" > ${KUBEVIRT_PATH}cluster/vagrant/.kubeconfig
