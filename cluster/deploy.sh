327c1fb73dde47e81e339e9f2ee599b177703182
Yaniv Bronhaim <bronhaim@gmail.com>
2017-01-04 15:19:51 +0200

    Update deploy.sh

--- cluster/deploy.sh
+++ cluster/deploy.sh
@@ -5,14 +5,14 @@ set -ex
 KUBECTL=${KUBECTL:-kubectl}
 
 echo "Cleaning up ..."
-for i in `ls ${KUBEVIRT_PATH}manifests/*.yaml`; do
+for i in `ls manifests/*.yaml`; do
     $KUBECTL delete -f $i --grace-period 0 2>/dev/null || :
 done
 
 sleep 2
 
 echo "Deploying ..."
-for i in `ls ${KUBEVIRT_PATH}manifests/*.yaml`; do
+for i in `ls manifests/*.yaml`; do
     $KUBECTL create -f $i
 done
 echo "Done"
