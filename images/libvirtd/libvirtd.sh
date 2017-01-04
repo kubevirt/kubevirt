90fe35d95df6bc62e5030692f7d307b8cdd4da5a
Fabian Deutsch <fabiand@redhat.com>
2017-01-04 21:59:09 +0100

    images: Drop path exposure from libvirtd image
    Somehow we were still exposing the libvirtd paths to the host,
    but in our use-case we will stick to communicate to the isolated
    libvirtd through libvirtd.
    
    This patch is dropping the path exposure.

--- images/libvirtd/libvirtd.sh
+++ images/libvirtd/libvirtd.sh
@@ -6,16 +6,9 @@ fatal() { echo "FATAL: $@" >&2 ; exit 2 ; }
 [[ -f /host/var/run/libvirtd.pid ]] && fatal "libvirtd seems to be running on the host"
 brctl show | grep virbr >/dev/null && fatal "libvirtd bridges are present on the host"
 
-KVM_GID=$(grep "kvm" /etc/group | awk 'BEGIN {FS=":"}; {print $3};')
-
 # HACK
 # Use hosts's /dev to see new devices and allow macvtap
 mkdir /odev && {
-  # the kvm group inside this container is 36--make sure that libvirtd/qemu can
-  # use the kvm device using that
-  # Note: this is done before moving the original /dev directory
-  chown root:"$KVM_GID" /dev/kvm
-  chmod g+rw /dev/kvm
   mount --rbind /dev /odev
   mount --rbind /host/dev /dev
   # Keep some devices from the original /dev
