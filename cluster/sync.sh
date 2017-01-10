9a061a0796342d54ae0ee7f93fa47ef33921e014
Roman Mohr <rmohr@redhat.com>
2017-01-10 09:13:40 +0100

    Use docker tag "devel" for development
    Avoid collision with released "latest" containers in the docker registry
    by using "devel" tag during development.

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -3,11 +3,11 @@ set -ex
 
 cluster/vagrant/sync_config.sh
 
-make all
+make all DOCKER_TAG=devel
 
 for VM in `vagrant status | grep -v "^The Libvirt domain is running." | grep running | cut -d " " -f1`; do
   vagrant rsync $VM # if you do not use NFS
-  vagrant ssh $VM -c "cd /vagrant && sudo hack/build-docker.sh"
+  vagrant ssh $VM -c "cd /vagrant && export DOCKER_TAG=devel && sudo -E hack/build-docker.sh"
 done
 
 export KUBECTL="cluster/kubectl.sh --core"
