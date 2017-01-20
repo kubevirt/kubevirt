1047bf186b3631d08f3937be7122da21fcd5e578
Roman Mohr <rmohr@redhat.com>
2017-01-20 10:21:25 +0100

    Move virt-controller VM controll loop to workqueue
    Instead of directly using a Delta FIFO, use the workqueue to hand over
    the VM to the scheduler if necessary.

--- cmd/virt-controller/virt-controller.go
+++ cmd/virt-controller/virt-controller.go
@@ -9,10 +9,8 @@ import (
 
 	"github.com/emicklei/go-restful"
 	"github.com/facebookgo/inject"
-	"k8s.io/client-go/tools/cache"
 	"kubevirt.io/kubevirt/pkg/kubecli"
 	"kubevirt.io/kubevirt/pkg/logging"
-	"kubevirt.io/kubevirt/pkg/util"
 	"kubevirt.io/kubevirt/pkg/virt-controller/rest"
 	"kubevirt.io/kubevirt/pkg/virt-controller/services"
 	"kubevirt.io/kubevirt/pkg/virt-controller/watch"
@@ -35,15 +33,7 @@ func main() {
 	if err != nil {
 		golog.Fatal(err)
 	}
-	vmHandler, err := watch.NewVMResourceEventHandler()
-	if err != nil {
-		golog.Fatal(err)
-	}
 	podHandler, err := watch.NewPodResourceEventHandler()
-	if err != nil {
-		golog.Fatal(err)
-	}
-	vmCache, err := util.NewVMCache()
 	if err != nil {
 		golog.Fatal(err)
 	}
@@ -54,11 +44,21 @@ func main() {
 		golog.Fatal(err)
 	}
 
+	// Bootstrapping. From here on the initialization order is important
+	stop := make(chan struct{})
+	defer close(stop)
+
+	// Start wachting vms
+	restClient, err := kubecli.GetRESTClient()
+	if err != nil {
+		golog.Fatal(err)
+	}
+	vmCache, vmController := watch.NewVMController(vmService, nil, restClient)
+
 	g.Provide(
 		&inject.Object{Value: clientSet},
 		&inject.Object{Value: templateService},
 		&inject.Object{Value: vmService},
-		&inject.Object{Value: vmHandler},
 		&inject.Object{Value: podHandler},
 		&inject.Object{Value: vmCache},
 	)
@@ -69,20 +69,10 @@ func main() {
 	}
 	restful.Add(rest.WebService)
 
-	// Bootstrapping. From here on the initialization order is important
-	stop := make(chan struct{})
-	defer close(stop)
+	go vmController.Run(1, stop)
 
-	// Warm up the vmCache before the pod watcher is started
-	go vmCache.Run(stop)
-	cache.WaitForCacheSync(stop, vmCache.HasSynced)
-
-	// Start wachting vms
-	vmController, err := watch.NewVMInformer(vmHandler)
-	if err != nil {
-		golog.Fatal(err)
-	}
-	go vmController.Run(stop)
+	// Wait until VM cache has warmed up before we start watching pods
+	vmController.WaitForSync(stop)
 
 	// Start watching pods
 	podController, err := watch.NewPodInformer(podHandler)
--- pkg/util/cache.go
+++ pkg/util/cache.go
@@ -1,8 +1,10 @@
 package util
 
 import (
+	"fmt"
 	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/fields"
+	"k8s.io/client-go/pkg/runtime/schema"
 	"k8s.io/client-go/tools/cache"
 	"kubevirt.io/kubevirt/pkg/api/v1"
 	"kubevirt.io/kubevirt/pkg/kubecli"
@@ -16,4 +18,17 @@ func NewVMCache() (cache.SharedInformer, error) {
 	vmCacheSource := cache.NewListWatchFromClient(restClient, "vms", api.NamespaceDefault, fields.Everything())
 	informer := cache.NewSharedInformer(vmCacheSource, &v1.VM{}, 0)
 	return informer, nil
+}
+
+// TODO Namespace could be different, also store it somewhere in the domain, so that we can report deletes on handler startup properly
+func NewVMReferenceFromName(name string) *v1.VM {
+	vm := &v1.VM{
+		ObjectMeta: api.ObjectMeta{
+			Name:      name,
+			Namespace: api.NamespaceDefault,
+			SelfLink:  fmt.Sprintf("/apis/%s/namespaces/%s/%s", v1.GroupVersion.String(), api.NamespaceDefault, name),
+		},
+	}
+	vm.SetGroupVersionKind(schema.GroupVersionKind{Group: v1.GroupVersion.Group, Kind: "VM", Version: v1.GroupVersion.Version})
+	return vm
 }
