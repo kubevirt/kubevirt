312a8a608be1321add569b3cb1d564c21f2b6a72
Adam Young <adam@younglogic.com>
2017-05-01 12:55:25 -0400

    Move migrationPod controller to migrate.go

--- pkg/virt-controller/watch/pod.go
+++ pkg/virt-controller/watch/pod.go
@@ -6,7 +6,7 @@ import (
 	"github.com/jeevatkm/go-model"
 	"k8s.io/client-go/kubernetes"
 	kubeapi "k8s.io/client-go/pkg/api"
-	"k8s.io/client-go/pkg/api/v1"
+	k8sv1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/fields"
 	"k8s.io/client-go/pkg/labels"
 	"k8s.io/client-go/pkg/types"
@@ -15,7 +15,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 
-	corev1 "kubevirt.io/kubevirt/pkg/api/v1"
+	kubev1 "kubevirt.io/kubevirt/pkg/api/v1"
 	"kubevirt.io/kubevirt/pkg/kubecli"
 	"kubevirt.io/kubevirt/pkg/logging"
 	"kubevirt.io/kubevirt/pkg/virt-controller/services"
@@ -24,20 +24,8 @@ import (
 func scheduledVMPodSelector() kubeapi.ListOptions {
 	fieldSelectionQuery := fmt.Sprintf("status.phase=%s", string(kubeapi.PodRunning))
 	fieldSelector := fields.ParseSelectorOrDie(fieldSelectionQuery)
-	labelSelectorQuery := fmt.Sprintf("!%s, %s in (virt-launcher)", string(corev1.MigrationLabel), corev1.AppLabel)
+	labelSelectorQuery := fmt.Sprintf("!%s, %s in (virt-launcher)", string(kubev1.MigrationLabel), kubev1.AppLabel)
 	labelSelector, err := labels.Parse(labelSelectorQuery)
-	if err != nil {
-		panic(err)
-	}
-	return kubeapi.ListOptions{FieldSelector: fieldSelector, LabelSelector: labelSelector}
-}
-
-func migrationVMPodSelector() kubeapi.ListOptions {
-	fieldSelectionQuery := fmt.Sprintf("status.phase=%s", string(kubeapi.PodRunning))
-	fieldSelector := fields.ParseSelectorOrDie(fieldSelectionQuery)
-	labelSelectorQuery := fmt.Sprintf("%s, %s in (virt-launcher)", string(corev1.MigrationLabel), corev1.AppLabel)
-	labelSelector, err := labels.Parse(labelSelectorQuery)
-
 	if err != nil {
 		panic(err)
 	}
@@ -49,15 +37,7 @@ func NewPodController(vmCache cache.Store, recorder record.EventRecorder, client
 	selector := scheduledVMPodSelector()
 	lw := kubecli.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", kubeapi.NamespaceDefault, selector.FieldSelector, selector.LabelSelector)
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
-	return kubecli.NewController(lw, queue, &v1.Pod{}, NewPodControllerDispatch(vmCache, restClient, vmService, clientset))
-}
-
-func NewMigrationPodController(vmCache cache.Store, recorder record.EventRecorder, clientset *kubernetes.Clientset, restClient *rest.RESTClient, vmService services.VMService, migrationQueue workqueue.RateLimitingInterface) (cache.Store, *kubecli.Controller) {
-
-	selector := migrationVMPodSelector()
-	lw := kubecli.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", kubeapi.NamespaceDefault, selector.FieldSelector, selector.LabelSelector)
-	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
-	return kubecli.NewController(lw, queue, &v1.Pod{}, NewMigrationPodControllerDispatch(vmCache, restClient, vmService, clientset, migrationQueue))
+	return kubecli.NewController(lw, queue, &k8sv1.Pod{}, NewPodControllerDispatch(vmCache, restClient, vmService, clientset))
 }
 
 func NewPodControllerDispatch(vmCache cache.Store, restClient *rest.RESTClient, vmService services.VMService, clientset *kubernetes.Clientset) kubecli.ControllerDispatch {
@@ -70,30 +50,11 @@ func NewPodControllerDispatch(vmCache cache.Store, restClient *rest.RESTClient, 
 	return &dispatch
 }
 
-func NewMigrationPodControllerDispatch(vmCache cache.Store, restClient *rest.RESTClient, vmService services.VMService, clientset *kubernetes.Clientset, migrationQueue workqueue.RateLimitingInterface) kubecli.ControllerDispatch {
-	dispatch := migrationPodDispatch{
-		vmCache:        vmCache,
-		restClient:     restClient,
-		vmService:      vmService,
-		clientset:      clientset,
-		migrationQueue: migrationQueue,
-	}
-	return &dispatch
-}
-
 type podDispatch struct {
 	vmCache    cache.Store
 	restClient *rest.RESTClient
 	vmService  services.VMService
 	clientset  *kubernetes.Clientset
-}
-
-type migrationPodDispatch struct {
-	vmCache        cache.Store
-	restClient     *rest.RESTClient
-	vmService      services.VMService
-	clientset      *kubernetes.Clientset
-	migrationQueue workqueue.RateLimitingInterface
 }
 
 func (pd *podDispatch) Execute(podStore cache.Store, podQueue workqueue.RateLimitingInterface, key interface{}) {
@@ -109,9 +70,9 @@ func (pd *podDispatch) Execute(podStore cache.Store, podQueue workqueue.RateLimi
 		// Do nothing
 		return
 	}
-	pod := obj.(*v1.Pod)
+	pod := obj.(*k8sv1.Pod)
 
-	vmObj, exists, err := pd.vmCache.GetByKey(kubeapi.NamespaceDefault + "/" + pod.GetLabels()[corev1.DomainLabel])
+	vmObj, exists, err := pd.vmCache.GetByKey(kubeapi.NamespaceDefault + "/" + pod.GetLabels()[kubev1.DomainLabel])
 	if err != nil {
 		podQueue.AddRateLimited(key)
 		return
@@ -120,29 +81,29 @@ func (pd *podDispatch) Execute(podStore cache.Store, podQueue workqueue.RateLimi
 		// Do nothing, the pod will timeout.
 		return
 	}
-	vm := vmObj.(*corev1.VM)
-	if vm.GetObjectMeta().GetUID() != types.UID(pod.GetLabels()[corev1.VMUIDLabel]) {
+	vm := vmObj.(*kubev1.VM)
+	if vm.GetObjectMeta().GetUID() != types.UID(pod.GetLabels()[kubev1.VMUIDLabel]) {
 		// Obviously the pod of an outdated VM object, do nothing
 		return
 	}
-	if vm.Status.Phase == corev1.Scheduling {
+	if vm.Status.Phase == kubev1.Scheduling {
 		// This is basically a hack, so that virt-handler can completely focus on the VM object and does not have to care about pods
 		pd.handleScheduling(podQueue, key, vm, pod)
 	}
 	return
 }
 
-func (pd *podDispatch) handleScheduling(podQueue workqueue.RateLimitingInterface, key interface{}, vm *corev1.VM, pod *v1.Pod) {
+func (pd *podDispatch) handleScheduling(podQueue workqueue.RateLimitingInterface, key interface{}, vm *kubev1.VM, pod *k8sv1.Pod) {
 	// deep copy the VM to allow manipulations
-	vmCopy := corev1.VM{}
+	vmCopy := kubev1.VM{}
 	model.Copy(&vmCopy, vm)
 
-	vmCopy.Status.Phase = corev1.Pending
+	vmCopy.Status.Phase = kubev1.Pending
 	// FIXME we store this in the metadata since field selctors are currently not working for TPRs
 	if vmCopy.GetObjectMeta().GetLabels() == nil {
 		vmCopy.ObjectMeta.Labels = map[string]string{}
 	}
-	vmCopy.ObjectMeta.Labels[corev1.NodeNameLabel] = pod.Spec.NodeName
+	vmCopy.ObjectMeta.Labels[kubev1.NodeNameLabel] = pod.Spec.NodeName
 	vmCopy.Status.NodeName = pod.Spec.NodeName
 	// Update the VM
 	logger := logging.DefaultLogger()
@@ -152,37 +113,4 @@ func (pd *podDispatch) handleScheduling(podQueue workqueue.RateLimitingInterface
 		return
 	}
 	logger.Info().Msgf("VM successfully scheduled to %s.", vmCopy.Status.NodeName)
-}
-
-func (pd *migrationPodDispatch) Execute(podStore cache.Store, podQueue workqueue.RateLimitingInterface, key interface{}) {
-	// Fetch the latest Vm state from cache
-	obj, exists, err := podStore.GetByKey(key.(string))
-
-	if err != nil {
-		podQueue.AddRateLimited(key)
-		return
-	}
-
-	if !exists {
-		// Do nothing
-		return
-	}
-	pod := obj.(*v1.Pod)
-
-	vmObj, exists, err := pd.vmCache.GetByKey(kubeapi.NamespaceDefault + "/" + pod.GetLabels()[corev1.DomainLabel])
-	if err != nil {
-		podQueue.AddRateLimited(key)
-		return
-	}
-	if !exists {
-		// Do nothing, the pod will timeout.
-		return
-	}
-	vm := vmObj.(*corev1.VM)
-	if vm.GetObjectMeta().GetUID() != types.UID(pod.GetLabels()[corev1.VMUIDLabel]) {
-		// Obviously the pod of an outdated VM object, do nothing
-		return
-	}
-	pd.migrationQueue.Add(v1.NamespaceDefault + "/" + pod.Labels[corev1.MigrationLabel])
-	return
 }
