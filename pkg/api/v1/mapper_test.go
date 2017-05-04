1eb7c442b5a632f6701b1472fff9b431de422fab
Roman Mohr <rmohr@redhat.com>
2017-05-04 10:00:55 +0200

    Remove unused internal mapping, add it when we need it
    In future, when supporting multiple API versions, like kubernetes does,
    we will need an internal mapping which we store in etcd. For now, we
    don't use it, therefore removing the unused code.

--- pkg/api/v1/mapper_test.go
+++ pkg/api/v1/mapper_test.go
@@ -2,43 +2,14 @@ package v1
 
 import (
 	"encoding/json"
-	"github.com/jeevatkm/go-model"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/satori/go.uuid"
-	kubeapi "k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/types"
-	"kubevirt.io/kubevirt/pkg/api"
 )
 
 var _ = Describe("Mapper", func() {
-	Context("With a VM supplied", func() {
-		It("should map to v1.VM", func() {
-			vm := api.VM{ObjectMeta: kubeapi.ObjectMeta{Name: "testvm", UID: types.UID(uuid.NewV4().String())}}
-			vm.Spec = api.VMSpec{NodeSelector: map[string]string{"key": "val"}}
-			dto := VM{}
-			errs := model.Copy(&dto, &vm)
-			Expect(errs).To(BeNil())
-			Expect(dto.GetObjectMeta().GetName()).To(Equal("testvm"))
-			Expect(dto.GetObjectMeta().GetUID()).To(Equal(vm.GetObjectMeta().GetUID()))
-			Expect(dto.Spec.NodeSelector).To(Equal(vm.Spec.NodeSelector))
-		})
-	})
-	// TODO fix this test when we need api.VM again
-	PContext("With a v1.VM supplied", func() {
-		It("should map to VM", func() {
-			vm := api.VM{}
-			dto := VM{ObjectMeta: v1.ObjectMeta{Name: "testvm", UID: types.UID(uuid.NewV4().String())}}
-			dto.Spec = VMSpec{NodeSelector: map[string]string{"key": "val"}}
-			errs := model.Copy(&vm, &dto)
-			Expect(errs).To(BeNil())
-
-			Expect(vm.GetObjectMeta().GetName()).To(Equal("testvm"))
-			Expect(vm.GetObjectMeta().GetUID()).To(Equal(dto.GetObjectMeta().GetUID()))
-			Expect(vm.Spec.NodeSelector).To(Equal(vm.Spec.NodeSelector))
-		})
-	})
 	Context("With a VM supplied", func() {
 		It("should map to json and back with custom mapper", func() {
 			vm := VM{ObjectMeta: v1.ObjectMeta{Name: "testvm", UID: types.UID(uuid.NewV4().String())}}
