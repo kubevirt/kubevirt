a44ce47cc910fb35c67293f95da0a58fe8a06834
David Vossel <davidvossel@gmail.com>
2017-12-13 11:40:51 -0500

    add kubecli SerialConsole api
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/kubecli/vm.go
+++ pkg/kubecli/vm.go
@@ -20,9 +20,21 @@
 package kubecli
 
 import (
+	goerror "errors"
+	"fmt"
+	"io"
+
+	k8sv1 "k8s.io/api/core/v1"
 	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 
 	"k8s.io/apimachinery/pkg/types"
 
@@ -30,13 +42,97 @@ import (
 )
 
 func (k *kubevirt) VM(namespace string) VMInterface {
-	return &vms{k.restClient, namespace, "virtualmachines"}
+	return &vms{
+		restClient: k.restClient,
+		clientSet:  k.Clientset,
+		namespace:  namespace,
+		resource:   "virtualmachines",
+		master:     k.master,
+		kubeconfig: k.kubeconfig,
+	}
 }
 
 type vms struct {
 	restClient *rest.RESTClient
+	clientSet  *kubernetes.Clientset
 	namespace  string
 	resource   string
+	master     string
+	kubeconfig string
+}
+
+func findPod(clientSet *kubernetes.Clientset, namespace string, name string) (string, error) {
+	fieldSelector := fields.ParseSelectorOrDie("status.phase==" + string(k8sv1.PodRunning))
+	labelSelector, err := labels.Parse(fmt.Sprintf(v1.AppLabel+"=virt-launcher,"+v1.DomainLabel+" in (%s)", name))
+	if err != nil {
+		return "", err
+	}
+	selector := k8smetav1.ListOptions{FieldSelector: fieldSelector.String(), LabelSelector: labelSelector.String()}
+
+	podList, err := clientSet.CoreV1().Pods(namespace).List(selector)
+	if err != nil {
+		return "", err
+	}
+
+	if len(podList.Items) == 0 {
+		return "", goerror.New("console connection failed. No VM pod is running")
+	}
+	return podList.Items[0].ObjectMeta.Name, nil
+}
+
+func (v *vms) SerialConsole(name string, device string, in io.Reader, out io.Writer) error {
+	podName, err := findPod(v.clientSet, v.namespace, name)
+	if err != nil {
+		return err
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags(v.master, v.kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	gv := k8sv1.SchemeGroupVersion
+	config.GroupVersion = &gv
+	config.APIPath = "/api"
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+
+	restClient, err := restclient.RESTClientFor(config)
+	if err != nil {
+		return err
+	}
+	cmd := []string{"/sock-connector", fmt.Sprintf("/var/run/kubevirt-private/%s/%s/virt-%s", v.namespace, name, device)}
+	containerName := "compute"
+	req := restClient.Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(v.namespace).
+		SubResource("exec").
+		Param("container", containerName)
+
+	req = req.VersionedParams(&k8sv1.PodExecOptions{
+		Container: containerName,
+		Command:   cmd,
+		Stdin:     true,
+		Stdout:    true,
+		Stderr:    true,
+		TTY:       true,
+	}, scheme.ParameterCodec)
+
+	// execute request
+	method := "POST"
+	url := req.URL()
+	exec, err := remotecommand.NewSPDYExecutor(config, method, url)
+	if err != nil {
+		return err
+	}
+
+	return exec.Stream(remotecommand.StreamOptions{
+		Stdin:             in,
+		Stdout:            out,
+		Stderr:            out,
+		Tty:               false,
+		TerminalSizeQueue: nil,
+	})
 }
 
 func (v *vms) Get(name string, options k8smetav1.GetOptions) (vm *v1.VirtualMachine, err error) {
