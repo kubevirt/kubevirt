56b986db4c3642c89954da80b4ee8e9f3476a3d0
David Vossel <davidvossel@gmail.com>
2018-01-08 09:41:05 -0500

    increase console test timeout
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- tests/console_test.go
+++ tests/console_test.go
@@ -58,7 +58,7 @@ var _ = Describe("Console", func() {
 			}, 60*time.Second)
 			Expect(err).ToNot(HaveOccurred())
 		})
-		FIt("should be able to reconnect to console multiple times", func() {
+		It("should be able to reconnect to console multiple times", func() {
 			vm := tests.NewRandomVMWithPVC("disk-alpine")
 
 			Expect(virtClient.RestClient().Post().Resource("virtualmachines").Namespace(tests.NamespaceTestDefault).Body(vm).Do().Error()).To(Succeed())
@@ -72,9 +72,9 @@ var _ = Describe("Console", func() {
 				_, err = expecter.ExpectBatch([]expect.Batcher{
 					&expect.BSnd{S: "\n"},
 					&expect.BExp{R: "login"},
-				}, 60*time.Second)
+				}, 100*time.Second)
 				Expect(err).ToNot(HaveOccurred())
 			}
-		})
+		}, 140)
 	})
 })
