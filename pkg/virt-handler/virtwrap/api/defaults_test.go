48b718ec8bc52d0ab9d99184b50834f2f139f7af
Martin Polednik <mpolednik@redhat.com>
2018-01-12 13:31:05 +0100

    virt-handler: add domain type check to machine type check
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- pkg/virt-handler/virtwrap/api/defaults_test.go
+++ pkg/virt-handler/virtwrap/api/defaults_test.go
@@ -7,9 +7,10 @@ import (
 
 var _ = Describe("Defaults", func() {
 
-	It("should set q35 machine type", func() {
+	It("should set q35 machine type and hvm domain type", func() {
 		domain := &Domain{}
 		SetDefaults_OSType(&domain.Spec.OS.Type)
 		Expect(domain.Spec.OS.Type.Machine).To(Equal("q35"))
+		Expect(domain.Spec.OS.Type.OS).To(Equal("hvm"))
 	})
 })
