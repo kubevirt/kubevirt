2f0496a2b69be8f522679be42018048d833b49cf
Martin Polednik <mpolednik@redhat.com>
2018-01-12 13:32:46 +0100

    virt-handler defaults: add test for Domain.Spec defaulting
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- pkg/virt-handler/virtwrap/api/defaults_test.go
+++ pkg/virt-handler/virtwrap/api/defaults_test.go
@@ -13,4 +13,11 @@ var _ = Describe("Defaults", func() {
 		Expect(domain.Spec.OS.Type.Machine).To(Equal("q35"))
 		Expect(domain.Spec.OS.Type.OS).To(Equal("hvm"))
 	})
+
+	It("should set libvirt namespace and use QEMU as emulator", func() {
+		domain := &Domain{}
+		SetDefaults_DomainSpec(&domain.Spec)
+		Expect(domain.Spec.XmlNS).To(Equal("http://libvirt.org/schemas/domain/qemu/1.0"))
+		Expect(domain.Spec.Type).To(Equal("qemu"))
+	})
 })
