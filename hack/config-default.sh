63e4a75f6d1f126fd8d9bcd4409f44a9f63ed69f
David Vossel <davidvossel@gmail.com>
2018-02-06 13:44:37 -0500

    remove fake dnsmasq process used by unit test suite
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -1,4 +1,4 @@
-binaries="cmd/virt-controller cmd/virt-launcher cmd/virt-handler cmd/virtctl cmd/fake-qemu-process cmd/fake-dnsmasq-process"
+binaries="cmd/virt-controller cmd/virt-launcher cmd/virt-handler cmd/virtctl cmd/fake-qemu-process"
 docker_images="cmd/virt-controller cmd/virt-launcher cmd/virt-handler images/iscsi-demo-target-tgtd images/vm-killer cmd/registry-disk-v1alpha images/cirros-registry-disk-demo images/fedora-cloud-registry-disk-demo"
 docker_prefix=kubevirt
 docker_tag=${DOCKER_TAG:-latest}
