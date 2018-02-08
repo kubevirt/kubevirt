d4e049154b1ec39edc562c9b617693fc3c47bf39
David Vossel <davidvossel@gmail.com>
2018-02-08 12:59:13 -0500

    echo qemu pid in entrypoint shutdown logic
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- cmd/virt-launcher/entrypoint.sh
+++ cmd/virt-launcher/entrypoint.sh
@@ -15,15 +15,15 @@ if [ -n "$qemu_pid" ]; then
 	# give the pid 10 seconds to exit. 
 	for x in $(seq 1 10); do
 		if ! [ -d /proc/$qemu_pid ]; then
-			echo "qemu pid exited after after SIGTERM"
+			echo "qemu pid [$qemu_pid] exited after after SIGTERM"
 			exit $rc
 		fi
-		echo "waiting for qemu pid to exit"
+		echo "waiting for qemu pid [$qemu_pid] to exit"
 		sleep 1
 	done
 
 	# if we got here, the pid never exitted gracefully.
-	echo "timed out waiting for qemu pid to exit"
+	echo "timed out waiting for qemu pid [$qemu_pid] to exit"
 fi
 
 exit $rc
