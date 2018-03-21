3959f1eab2f005379ca9a479900bb4f6de8fc0db
Vladik Romanovsky <vromanso@redhat.com>
2018-03-20 20:22:53 -0400

    Enhance DHCP to offer additional static routes via option 121 RFC4332
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

---
+++ pkg/virt-launcher/virtwrap/network/dhcp/dhcp_suite_test.go
@@ -0,0 +1,13 @@
+package dhcp_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestNetwork(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "DHCP test Suite")
+}
