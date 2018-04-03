67c1e291c0d817bd925e0f3d0c76e788685f4546
David Vossel <davidvossel@gmail.com>
2018-04-03 10:02:25 -0400

    sync all k8s dependencies to a single tag
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- vendor/gopkg.in/ini.v1/ini.go
+++ vendor/gopkg.in/ini.v1/ini.go
@@ -32,7 +32,7 @@ const (
 
 	// Maximum allowed depth when recursively substituing variable names.
 	_DEPTH_VALUES = 99
-	_VERSION      = "1.33.0"
+	_VERSION      = "1.34.0"
 )
 
 // Version returns current package version literal.
