023c3c7cc7fe3ed955e0ee346d365b2de97a8b5f
Petr Kotas <petr.kotas@gmail.com>
2018-04-03 10:06:20 +0200

    Expose custom disk in iscsi demo
    Signed-off-by: Petr Kotas <petr.kotas@gmail.com>

--- hack/check.sh
+++ hack/check.sh
@@ -4,5 +4,5 @@ set -e
 source hack/common.sh
 
 shfmt -i 4 -w ${KUBEVIRT_DIR}/cluster/ ${KUBEVIRT_DIR}/hack/ ${KUBEVIRT_DIR}/images/
-goimports -w -local kubevirt.io ${KUBEVIRT_DIR}/cmd/ ${KUBEVIRT_DIR}/pkg/ ${KUBEVIRT_DIR}/tests/
-(cd ${KUBEVIRT_DIR} && go vet ./cmd/... ./pkg/... ./tests/...)
+goimports -w -local kubevirt.io ${KUBEVIRT_DIR}/cmd/ ${KUBEVIRT_DIR}/pkg/ ${KUBEVIRT_DIR}/tests/ ${KUBEVIRT_DIR}/tools
+(cd ${KUBEVIRT_DIR} && go vet ./cmd/... ./pkg/... ./tests/... ./tools/...)
