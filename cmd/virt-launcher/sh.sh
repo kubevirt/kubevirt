5e08d8ea2c5d5030a8820ecd21ad903f2db08fd2
Roman Mohr <rmohr@redhat.com>
2018-04-09 17:10:35 +0200

    Don't confuse libvirt with the oc hack
    If exec is missing libvirt kills the nested sh script instead of the
    domain.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cmd/virt-launcher/sh.sh
+++ cmd/virt-launcher/sh.sh
@@ -4,7 +4,7 @@ args="$@"
 if [ "$args" = "-i -c TERM=xterm /bin/sh" ] ; then
   namespace="$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
   name="$(ls /var/run/kubevirt-private/${namespace}/)"
-  /usr/bin/sh.orig -c "/sock-connector /var/run/kubevirt-private/${namespace}/${name}/virt-serial0"
+  exec /usr/bin/sh.orig -c "/sock-connector /var/run/kubevirt-private/${namespace}/${name}/virt-serial0"
 else
-  /usr/bin/sh.orig "$@"
+  exec /usr/bin/sh.orig "$@"
 fi
