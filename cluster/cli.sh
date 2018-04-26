2faf76076f635e6b17792ade7a027a5468f27b89
Roman Mohr <rmohr@redhat.com>
2018-04-26 10:28:20 +0200

    Update userguide with ssh instructions

--- cluster/cli.sh
+++ cluster/cli.sh
@@ -3,4 +3,4 @@ source $(dirname "$0")/../hack/common.sh
 source ${KUBEVIRT_DIR}/cluster/$PROVIDER/provider.sh
 source hack/config.sh
 
-${_cli} "$@" --prefix $provider_prefix
+${_cli} --prefix $provider_prefix "$@" 
