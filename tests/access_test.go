e35a443eded4cb2677ee424d98cb0a3d14a68458
David Vossel <davidvossel@gmail.com>
2018-05-07 15:29:04 -0400

    Add functional test to verify access to CRD objects using default kubevirt roles
    Signed-off-by: David Vossel <davidvossel@gmail.com>

---
+++ tests/access_test.go
@@ -0,0 +1,128 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2018 Red Hat, Inc.
+ *
+ */
+
+package tests_test
+
+import (
+	"flag"
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"kubevirt.io/kubevirt/tests"
+)
+
+var _ = Describe("User Access", func() {
+
+	flag.Parse()
+
+	BeforeEach(func() {
+		tests.BeforeTestCleanup()
+	})
+
+	Describe("With view, edit, and admin kubevirt service accounts", func() {
+		table.DescribeTable("should verify permissions are correct for each", func(resource string) {
+			tests.SkipIfNoKubectl()
+
+			view := tests.ViewServiceAccountName
+			edit := tests.EditServiceAccountName
+			admin := tests.AdminServiceAccountName
+
+			viewVerbs := make(map[string]string)
+			editVerbs := make(map[string]string)
+			adminVerbs := make(map[string]string)
+
+			// GET
+			viewVerbs["get"] = "yes"
+			editVerbs["get"] = "yes"
+			adminVerbs["get"] = "yes"
+
+			// List
+			viewVerbs["list"] = "yes"
+			editVerbs["list"] = "yes"
+			adminVerbs["list"] = "yes"
+
+			// WATCH
+			viewVerbs["watch"] = "yes"
+			editVerbs["watch"] = "yes"
+			adminVerbs["watch"] = "yes"
+
+			// DELETE
+			viewVerbs["delete"] = "no"
+			editVerbs["delete"] = "yes"
+			adminVerbs["delete"] = "yes"
+
+			// CREATE
+			viewVerbs["create"] = "no"
+			editVerbs["create"] = "yes"
+			adminVerbs["create"] = "yes"
+
+			// UPDATE
+			viewVerbs["update"] = "no"
+			editVerbs["update"] = "yes"
+			adminVerbs["update"] = "yes"
+
+			// PATCH
+			viewVerbs["patch"] = "no"
+			editVerbs["patch"] = "yes"
+			adminVerbs["patch"] = "yes"
+
+			// DELETE COllECTION
+			viewVerbs["deleteCollection"] = "no"
+			editVerbs["deleteCollection"] = "no"
+			adminVerbs["deleteCollection"] = "yes"
+
+			namespace := tests.NamespaceTestDefault
+			verbs := []string{"get", "list", "watch", "delete", "create", "update", "patch", "deletecollection"}
+
+			for _, verb := range verbs {
+				// VIEW
+				By(fmt.Sprintf("verifying VIEW sa for verb %s", verb))
+				expectedRes, _ := viewVerbs[verb]
+				as := fmt.Sprintf("system:serviceaccount:%s:%s", namespace, view)
+				result, err := tests.RunKubectlCommand("auth", "can-i", "--as", as, verb, resource)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring(expectedRes))
+
+				// EDIT
+				By(fmt.Sprintf("verifying EDIT sa for verb %s", verb))
+				expectedRes, _ = editVerbs[verb]
+				as = fmt.Sprintf("system:serviceaccount:%s:%s", namespace, edit)
+				result, err = tests.RunKubectlCommand("auth", "can-i", "--as", as, verb, resource)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring(expectedRes))
+
+				// ADMIN
+				By(fmt.Sprintf("verifying ADMIN sa for verb %s", verb))
+				expectedRes, _ = adminVerbs[verb]
+				as = fmt.Sprintf("system:serviceaccount:%s:%s", namespace, admin)
+				result, err = tests.RunKubectlCommand("auth", "can-i", "--as", as, verb, resource)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring(expectedRes))
+			}
+		},
+			table.Entry("given a vm", "virtualmachines"),
+			table.Entry("given an ovm", "offlinevirtualmachines"),
+			table.Entry("given a vm preset", "virtualmachinepresets"),
+			table.Entry("given a vm replica set", "virtualmachinereplicasets"),
+		)
+	})
+})
