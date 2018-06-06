7b6f4cbd9edd971950d0e99f062023c74ec953c1
Marcin Franczyk <mfranczy@redhat.com>
2018-06-06 14:23:52 +0200

    - add memory validation on API level for memory negative values

--- pkg/virt-api/validating-webhook/validating-webhook.go
+++ pkg/virt-api/validating-webhook/validating-webhook.go
@@ -321,6 +321,17 @@ func validateVirtualMachineSpec(field *k8sfield.Path, spec *v1.VirtualMachineSpe
 		return causes
 	}
 
+	// Validate requested memory size if value is not negative
+	if spec.Domain.Resources.Requests.Memory().Value() < 0 {
+		causes = append(causes, metav1.StatusCause{
+			Type:    metav1.CauseTypeFieldValueInvalid,
+			Message: fmt.Sprintf("%s is specified as negative value", field.Child("domain", "resources", "requests", "memory").String()),
+			Field:   field.Child("domain", "resources", "requests", "memory").String(),
+		})
+		// We won't process anything with requested negative memory size
+		return causes
+	}
+
 	for _, volume := range spec.Volumes {
 		volumeNameMap[volume.Name] = &volume
 	}
