b2c47a4a89c4b640e1d7d80acb3c0ad3edcb1a87
Roman Mohr <rmohr@redhat.com>
2018-06-25 17:52:26 +0200

    Also check for existence of "clean", not just for "dirty"

--- hack/verify-build.sh
+++ hack/verify-build.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-if [ -n "$(cluster/virtctl.sh version | grep dirty)" ]; then
+function report_dirty_build() {
+    set +e
     echo "Build is not clean:"
+    cluster/virtctl.sh version
     git status
     exit 1
+}
+
+# Check that "clean" is reported at least once
+if [ -z "$(cluster/virtctl.sh version | grep clean)" ]; then
+    report_dirty_build
+fi
+
+# Check that "dirty" is never reported
+if [ -n "$(cluster/virtctl.sh version | grep dirty)" ]; then
+    report_dirty_build
 fi
