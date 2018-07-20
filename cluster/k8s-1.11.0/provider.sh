ba2f89bda35774a52968fb5da4a848a597176bb1
Marc Sluiter <msluiter@redhat.com>
2018-07-20 11:08:12 +0200

    Added k8s 1.11.0 provider
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

---
+++ cluster/k8s-1.11.0/provider.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+image="k8s-1.11.0@sha256:1e936a35d84102f96253002e463c6142c3422f4d6012ef4bdcc5e9cd6a63d359"
+
+source cluster/ephemeral-provider-common.sh
+
+function up() {
+    ${_cli} run $(_add_common_params)
+
+    # Copy k8s config and kubectl
+    ${_cli} scp --prefix $provider_prefix /usr/bin/kubectl - >${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl
+    chmod u+x ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl
+    ${_cli} scp --prefix $provider_prefix /etc/kubernetes/admin.conf - >${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubeconfig
+
+    # Set server and disable tls check
+    export KUBECONFIG=${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubeconfig
+    ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --server=https://$(_main_ip):$(_port k8s)
+    ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --insecure-skip-tls-verify=true
+
+    # Make sure that local config is correct
+    prepare_config
+}
