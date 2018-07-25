a43518dd347f47b1aee6650773ccda7763e8e26d
Sebastian Sch <sebassch@gmail.com>
2018-07-25 11:27:05 +0300

    Added new k8s 1.11.0 provider with multus network plugin.

---
+++ cluster/k8s-multus-1.11.0/provider.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+image="k8s-multul-1.11.0"
+
+source cluster/ephemeral-provider-common.sh
+
+function up() {
+    ${_cli} run $(_add_common_params)
+
+    # Copy k8s config and kubectl
+    ${_cli} scp --prefix $provider_prefix /usr/bin/kubectl - >${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl
+    chmod u+x ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl
+    ${_cli} scp --prefix $provider_prefix /etc/kubernetes/admin.conf - >${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubeconfig
+
+    # Set server and disable tls check
+    export KUBECONFIG=${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubeconfig
+    ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --server=https://$(_main_ip):$(_port k8s)
+    ${KUBEVIRT_PATH}cluster/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --insecure-skip-tls-verify=true
+    
+    # Make sure that local config is correct
+    prepare_config
+
+    for i in $(seq 1 ${KUBEVIRT_NUM_NODES}); do
+        cluster/kubectl.sh create clusterrolebinding multus-node-"node$(printf "%02d" ${i})" \
+                    --clusterrole=multus-crd-overpowered \
+                    --user=system:node:"node$(printf "%02d" ${i})"
+    done
+}
