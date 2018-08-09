4ebaaac39efbcf89765bcc1512a0c6adc8f18f2c
Stu Gott <sgott@redhat.com>
2018-08-09 17:19:37 -0400

    Define IOThreads for VirtualMachineInstance objects
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- pkg/api/v1/defaults.go
+++ pkg/api/v1/defaults.go
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2017,2018 Red Hat, Inc.
+ *
+ */
+
 package v1
 
 import (
@@ -130,6 +149,7 @@ func SetDefaults_VirtualMachineInstance(obj *VirtualMachineInstance) {
 	}
 	setDefaults_DiskFromMachineType(obj)
 	SetDefaults_NetworkInterface(obj)
+	setDefaults_IOThreads(obj)
 }
 
 func setDefaults_DiskFromMachineType(obj *VirtualMachineInstance) {
@@ -162,6 +182,16 @@ func SetDefaults_NetworkInterface(obj *VirtualMachineInstance) {
 	if len(obj.Spec.Networks) == 0 {
 		obj.Spec.Domain.Devices.Interfaces = []Interface{*DefaultNetworkInterface()}
 		obj.Spec.Networks = []Network{*DefaultPodNetwork()}
+	}
+}
+
+func setDefaults_IOThreads(obj *VirtualMachineInstance) {
+	ioThreads := obj.Spec.Domain.IOThreads
+	if ioThreads != nil {
+		if ioThreads.ThreadCount == nil {
+			numIOThreads := uint(1)
+			ioThreads.ThreadCount = &numIOThreads
+		}
 	}
 }
 
