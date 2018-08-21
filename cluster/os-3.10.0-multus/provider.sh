c2062acc23d2fb2c8432b4d199286383d6fabc08
Sebastian Sch <sebassch@gmail.com>
2018-08-21 18:45:06 +0300

    Change the provider images digest.

--- cluster/os-3.10.0-multus/provider.sh
+++ cluster/os-3.10.0-multus/provider.sh
@@ -4,4 +4,4 @@ set -e
 
 source cluster/os-3.10.0/provider.sh
 
-image="os-3.10.0-multus@sha256:bb206bfccd46299ea22f28cbf974f0adb7a62eb8af760763f4c8e408ffa975b1"
+image="os-3.10.0-multus@sha256:d9a8df2ba545e579c933d96d33406a1a6b0dca37a81469fbc08bbb1d557d02f3"
