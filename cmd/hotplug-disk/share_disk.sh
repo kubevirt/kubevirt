bf363402a1c2e87c9f2cbdc28c96277c8af1874d
Stu Gott <sgott@redhat.com>
2018-11-02 12:08:55 -0400

    Fix race condition in hotplug-disk container
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- cmd/hotplug-disk/share_disk.sh
+++ cmd/hotplug-disk/share_disk.sh
@@ -10,22 +10,27 @@ fi
 
 NBD_SOURCE=$1
 SOCKET_PATH=$2
+TMP_SOCKET_PATH="$(dirname $SOCKET_PATH)/.$(basename $SOCKET_PATH)"
 
-qemu-nbd $READ_ONLY -t -f raw -k "$SOCKET_PATH" $NBD_SOURCE &
+qemu-nbd $READ_ONLY -f raw -k "$TMP_SOCKET_PATH" $NBD_SOURCE &
 NBD_PID=$!
 
-# FIXME: this is a race condition. virt-launcher might already
-# be trying to attach the disk to the domain
-# unfortunately we can't assume NBD_SOURCE is readable unless root
 if [ "x$READ_ONLY" != "x-r" ] ; then
     RETRY_LIMIT=100
     RETRIES=0
     DONE=0
     while [ "$DONE" -eq "0" ] ; do
-        chown qemu:qemu "$SOCKET_PATH" && DONE=1 || RETRIES=$(($RETRIES + 1))
-        [ $RETRIES -gt $RETRY_LIMIT ] && fail "timed out waiting for socket"
+        chown qemu:qemu "$TMP_SOCKET_PATH" && DONE=1 || RETRIES=$(($RETRIES + 1))
+        [ $RETRIES -gt $RETRY_LIMIT ] && false "timed out waiting for socket"
         sleep 0.1
     done
+else
+    DONE=1
+fi
+
+if [ "$DONE" -eq "1" ] ; then
+    mv "$TMP_SOCKET_PATH" "$SOCKET_PATH"
 fi
 
 tail --pid="$NBD_PID" -f /dev/null
+rm -f "$TMP_SOCKET_PATH" "$SOCKET_PATH"
