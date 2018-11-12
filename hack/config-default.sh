6c09afbc9f9ea418171c1b367568fb73342b1ba3
Stu Gott <sgott@redhat.com>
2018-11-12 17:18:28 -0500

    Use the kubevirt namespace by default
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -6,5 +6,5 @@ docker_prefix=${DOCKER_PREFIX:-kubevirt}
 docker_tag=${DOCKER_TAG:-latest}
 master_ip=192.168.200.2
 network_provider=flannel
-namespace=kube-system
+namespace=kubevirt
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
