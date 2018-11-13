b570bedbc174af25df1ef7279c11c87a3b472068
Quique Llorente <ellorent@redhat.com>
2018-11-13 18:09:18 +0100

    Fix type at goverall call to coverall

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-./hacks/coverage.sh
+./hack/coverage.sh
 goveralls -service=travis-ci -coverprofile=.coverprofile -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
