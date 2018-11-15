151bc1171d692b7de67568ae2911b24eae045b13
Artyom Lukianov <alukiano@redhat.com>
2018-11-15 18:50:04 +0200

    Set travis variable for goveralls script

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 set -e
+
 ./hack/coverage.sh
 goveralls -service=travis-ci -coverprofile=.coverprofile -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
