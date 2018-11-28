2c13925adb13475743afff2f1f93236bd263a66d
Stu Gott <sgott@redhat.com>
2018-11-28 13:32:08 -0500

    Configure namespace for CDI components separately
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -7,4 +7,5 @@ docker_tag=${DOCKER_TAG:-latest}
 master_ip=192.168.200.2
 network_provider=flannel
 namespace=kubevirt
+cdi_namespace=kube-system
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
