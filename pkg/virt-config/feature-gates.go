0f19aea14fd34144dd3e0234a1b7fbb65fc7de91
Arik Hadas <ahadas@redhat.com>
2018-12-06 15:28:17 +0200

    extract parsing of kubevirt-config config map to a separate module
    As preparation for introducing virt-properties that will also
    reside in the kubevirt-config config map.
    
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- pkg/virt-config/feature-gates.go
+++ pkg/virt-config/feature-gates.go
@@ -22,68 +22,14 @@ package virtconfig
 import (
 	"os"
 	"strings"
-	"time"
-
-	k8sv1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
-
-	"kubevirt.io/kubevirt/pkg/kubecli"
-	"kubevirt.io/kubevirt/pkg/log"
-	"kubevirt.io/kubevirt/pkg/util"
 )
-
-const featureGateEnvVar = "FEATURE_GATES"
 
 const (
 	dataVolumesGate   = "DataVolumes"
 	cpuManager        = "CPUManager"
 	liveMigrationGate = "LiveMigration"
 	SRIOVGate         = "SRIOV"
-	configMapName     = "kubevirt-config"
 )
-
-func ParseFeatureGatesFromConfigMap() {
-	virtClient, err := kubecli.GetKubevirtClient()
-	if err != nil {
-		panic(err)
-	}
-
-	var cfgMap *k8sv1.ConfigMap
-	err = wait.PollImmediate(time.Second*1, time.Second*10, func() (bool, error) {
-
-		namespace, curErr := util.GetNamespace()
-		if err != nil {
-			return false, err
-		}
-
-		cfgMap, curErr = virtClient.CoreV1().ConfigMaps(namespace).Get(configMapName, metav1.GetOptions{})
-
-		if curErr != nil {
-			if errors.IsNotFound(curErr) {
-				logger := log.DefaultLogger()
-				logger.Infof("%s ConfigMap does not exist. Using defaults.", configMapName)
-				// ignore if config map does not exist
-				return true, nil
-			}
-			return false, curErr
-		}
-
-		val, ok := cfgMap.Data["feature-gates"]
-		if !ok {
-			// no feature gates set
-			return true, nil
-		}
-
-		os.Setenv(featureGateEnvVar, val)
-		return true, nil
-	})
-
-	if err != nil {
-		panic(err)
-	}
-}
 
 func DataVolumesEnabled() bool {
 	return strings.Contains(os.Getenv(featureGateEnvVar), dataVolumesGate)
