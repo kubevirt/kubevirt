076e467ff0b4379e99e7c2cdc5d90b6e0a29a1dc
Artyom Lukianov <alukiano@redhat.com>
2019-02-19 11:32:33 +0200

    Use bazel under the travis to push docker images

--- hack/bazel-generate.sh
+++ hack/bazel-generate.sh
@@ -10,5 +10,5 @@ bazel run //:gazelle
 bazel run --run_under='cd /root/go/src/kubevirt.io/kubevirt &&' -- @com_github_bazelbuild_buildtools//buildozer 'add cdeps //:libvirt-libs //:libvirt-headers' //vendor/github.com/libvirt/libvirt-go:go_default_library
 bazel run --run_under='cd /root/go/src/kubevirt.io/kubevirt &&' -- @com_github_bazelbuild_buildtools//buildozer 'add copts -Ibazel-out/k8-fastbuild/genfiles' //vendor/github.com/libvirt/libvirt-go:go_default_library
 
-# allign BAZEL files to the single format
+# allign BAZEL files to a single format
 bazel run //:buildifier
