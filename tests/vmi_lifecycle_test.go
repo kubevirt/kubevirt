96100eac388e96235ef86288f2ff45899bc61a30
Tareq Alayan <talayan@redhat.com>
2019-02-27 13:32:05 +0200

    increase timeout waiting for virthandler
    when running this test on different environments virthandler isn't
    ready to work after 60s, hence increase it to 120s

--- tests/vmi_lifecycle_test.go
+++ tests/vmi_lifecycle_test.go
@@ -514,7 +514,7 @@ var _ = Describe("[rfe_id:273][crit:high][vendor:cnv-qe@redhat.com][level:compon
 						ds.Status.DesiredNumberScheduled == virtHandlerAvailablePods &&
 						ds.Status.NumberReady == virtHandlerAvailablePods &&
 						ds.Status.UpdatedNumberScheduled == virtHandlerAvailablePods
-				}, 60*time.Second, 1*time.Second).Should(Equal(true), "Virthandler should be ready to work")
+				}, 120*time.Second, 1*time.Second).Should(Equal(true), "Virthandler should be ready to work")
 			})
 		})
 
