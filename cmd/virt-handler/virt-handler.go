ed48097aa10f4bd033b5ddfbd182f4422eb1f2c7
Francesco Romani <fromani@redhat.com>
2019-03-05 14:19:45 +0100

    virt-handler: remove unused import
    Unfortunately, this sneaked out from the parent patch.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/virt-handler/virt-handler.go
+++ cmd/virt-handler/virt-handler.go
@@ -27,7 +27,6 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	flag "github.com/spf13/pflag"
 	k8sv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/fields"
