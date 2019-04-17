e9bcf6321b98c922ec1454f4689d2f668563e58e
Ihar Hrachyshka <ihar@redhat.com>
2019-04-17 00:20:07 +0000

    Install ginkgo for sriov tests

--- cluster/k8s-1.13.0-sriov/test.sh
+++ cluster/k8s-1.13.0-sriov/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+PATH=~/go/bin:$PATH
+go get -u github.com/onsi/ginkgo/ginkgo
 FUNC_TEST_ARGS='--ginkgo.noColor --junit-output=/go/src/kubevirt.io/kubevirt/tests.junit.xml --ginkgo.focus=SRIOV --kubeconfig /etc/kubernetes/admin.conf' make functest
