1138e3d178ed99e1fab565086aac1214584963e5
Stu Gott <sgott@redhat.com>
2019-04-23 15:46:39 +0000

    Fix unit tests for new virtctl rules
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- pkg/virtctl/vm/vm_test.go
+++ pkg/virtctl/vm/vm_test.go
@@ -1,14 +1,13 @@
 package vm_test
 
 import (
-	"errors"
-
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	v1 "kubevirt.io/kubevirt/pkg/api/v1"
 	"kubevirt.io/kubevirt/pkg/kubecli"
 	"kubevirt.io/kubevirt/tests"
 )
@@ -20,6 +19,12 @@ var _ = Describe("VirtualMachine", func() {
 	var vmInterface *kubecli.MockVirtualMachineInterface
 	var ctrl *gomock.Controller
 
+	running := true
+	notRunning := false
+	runStrategyAlways := v1.RunStrategyAlways
+	runStrategyManual := v1.RunStrategyManual
+	runStrategyHalted := v1.RunStrategyHalted
+
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		kubecli.GetKubevirtClientFromClientConfig = kubecli.GetMockKubevirtClientFromClientConfig
@@ -30,12 +35,10 @@ var _ = Describe("VirtualMachine", func() {
 	Context("should patch VM", func() {
 		It("with spec:running:true", func() {
 			vm := kubecli.NewMinimalVM(vmName)
+			vm.Spec.Running = &notRunning
 
-			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(2)
-			vmInterface.EXPECT().Get(vm.Name, gomock.Any()).Return(vm, nil)
-			runningVM := kubecli.NewMinimalVM("vnName")
-			runningVM.Spec.Running = true
-			vmInterface.EXPECT().Patch(vmName, gomock.Any(), gomock.Any()).Return(runningVM, nil)
+			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+			vmInterface.EXPECT().Start(vm.Name).Return(nil).Times(1)
 
 			cmd := tests.NewVirtctlCommand("start", vmName)
 			Expect(cmd.Execute()).To(BeNil())
@@ -43,13 +46,10 @@ var _ = Describe("VirtualMachine", func() {
 
 		It("with spec:running:false", func() {
 			vm := kubecli.NewMinimalVM(vmName)
-			vm.Spec.Running = true
+			vm.Spec.Running = &running
 
-			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(2)
-			vmInterface.EXPECT().Get(vm.Name, gomock.Any()).Return(vm, nil)
-			stoppedVM := kubecli.NewMinimalVM(vmName)
-			stoppedVM.Spec.Running = false
-			vmInterface.EXPECT().Patch(vmName, gomock.Any(), gomock.Any()).Return(stoppedVM, nil)
+			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+			vmInterface.EXPECT().Stop(vm.Name).Return(nil).Times(1)
 
 			cmd := tests.NewVirtctlCommand("stop", vmName)
 			Expect(cmd.Execute()).To(BeNil())
@@ -57,13 +57,48 @@ var _ = Describe("VirtualMachine", func() {
 
 		It("with spec:running:false when it's false already ", func() {
 			vm := kubecli.NewMinimalVM(vmName)
-			vm.Spec.Running = false
+			vm.Spec.Running = &notRunning
 
-			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface)
-			vmInterface.EXPECT().Get(vm.Name, gomock.Any()).Return(vm, nil)
+			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+			vmInterface.EXPECT().Stop(vm.Name).Return(nil).Times(1)
 
-			cmd := tests.NewRepeatableVirtctlCommand("stop", vmName)
-			Expect(cmd()).NotTo(BeNil())
+			cmd := tests.NewVirtctlCommand("stop", vmName)
+			Expect(cmd.Execute()).To(BeNil())
+		})
+
+		Context("Using RunStrategy", func() {
+			It("with spec:runStrategy:running", func() {
+				vm := kubecli.NewMinimalVM(vmName)
+				vm.Spec.RunStrategy = &runStrategyHalted
+
+				kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+				vmInterface.EXPECT().Start(vm.Name).Return(nil).Times(1)
+
+				cmd := tests.NewVirtctlCommand("start", vmName)
+				Expect(cmd.Execute()).To(BeNil())
+			})
+
+			It("with spec:runStrategy:halted", func() {
+				vm := kubecli.NewMinimalVM(vmName)
+				vm.Spec.RunStrategy = &runStrategyAlways
+
+				kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+				vmInterface.EXPECT().Stop(vm.Name).Return(nil).Times(1)
+
+				cmd := tests.NewVirtctlCommand("stop", vmName)
+				Expect(cmd.Execute()).To(BeNil())
+			})
+
+			It("with spec:runStrategy:halted when it's false already ", func() {
+				vm := kubecli.NewMinimalVM(vmName)
+				vm.Spec.RunStrategy = &runStrategyHalted
+
+				kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+				vmInterface.EXPECT().Stop(vm.Name).Return(nil).Times(1)
+
+				cmd := tests.NewVirtctlCommand("stop", vmName)
+				Expect(cmd.Execute()).To(BeNil())
+			})
 		})
 
 	})
@@ -72,22 +107,24 @@ var _ = Describe("VirtualMachine", func() {
 		It("should restart vm", func() {
 			vm := kubecli.NewMinimalVM(vmName)
 
-			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(2)
-			vmInterface.EXPECT().Get(vm.Name, gomock.Any()).Return(vm, nil)
-			vmInterface.EXPECT().Restart(vmName).Return(nil)
+			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+			vmInterface.EXPECT().Restart(vm.Name).Return(nil).Times(1)
 
 			cmd := tests.NewVirtctlCommand("restart", vmName)
 			Expect(cmd.Execute()).To(BeNil())
 		})
 
-		It("should return an error", func() {
-			vm := kubecli.NewMinimalVM(vmName)
+		Context("using RunStrategy", func() {
+			It("should restart a vm with runStrategy:manual", func() {
+				vm := kubecli.NewMinimalVM(vmName)
+				vm.Spec.RunStrategy = &runStrategyManual
 
-			kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface)
-			vmInterface.EXPECT().Get(vm.Name, gomock.Any()).Return(vm, errors.New(vmName))
+				kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachine(k8smetav1.NamespaceDefault).Return(vmInterface).Times(1)
+				vmInterface.EXPECT().Restart(vm.Name).Return(nil).Times(1)
 
-			cmd := tests.NewVirtctlCommand("restart", vmName)
-			Expect(cmd.Execute()).To(Equal(errors.New("Error fetching VirtualMachine: " + vmName)))
+				cmd := tests.NewVirtctlCommand("restart", vmName)
+				Expect(cmd.Execute()).To(BeNil())
+			})
 		})
 	})
 
