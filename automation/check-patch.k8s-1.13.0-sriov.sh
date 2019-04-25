6ef3550e0732d5bc15daf42450ecb26be13a29c6
Ihar Hrachyshka <ihrachys@redhat.com>
2019-04-25 02:03:27 +0000

    Enable debug logs for kind

--- automation/check-patch.k8s-1.13.0-sriov.sh
+++ automation/check-patch.k8s-1.13.0-sriov.sh
@@ -79,7 +79,7 @@ flock -e  -w "$SRIOV_TIMEOUT_SEC" "$fd" || {
 # ================
 go get -u sigs.k8s.io/kind
 
-kind create cluster --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
+kind --loglevel debug create cluster --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
 
 export KUBECONFIG=$(kind get kubeconfig-path --name=${CLUSTER_NAME})
 kubectl cluster-info
