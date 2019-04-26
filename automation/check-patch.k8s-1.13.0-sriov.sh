369a6e2047368acce639c9d5c367dbefb6fb0b78
Ihar Hrachyshka <ihrachys@redhat.com>
2019-04-26 12:02:08 -0700

    DNM: keep cluster up after failure to debug issues

--- automation/check-patch.k8s-1.13.0-sriov.sh
+++ automation/check-patch.k8s-1.13.0-sriov.sh
@@ -59,6 +59,7 @@ function collect_artifacts {
 }
 
 function finish {
+    sleep $((60*60*4))
     collect_artifacts
     kind delete cluster --name=${CLUSTER_NAME}
 }
@@ -79,7 +80,7 @@ flock -e  -w "$SRIOV_TIMEOUT_SEC" "$fd" || {
 # ================
 go get -u sigs.k8s.io/kind
 
-kind --loglevel debug create cluster --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
+kind --loglevel debug create cluster --retain --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
 
 export KUBECONFIG=$(kind get kubeconfig-path --name=${CLUSTER_NAME})
 kubectl cluster-info
