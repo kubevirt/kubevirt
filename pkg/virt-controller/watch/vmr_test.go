128cf731984a9d84392756de2eefcacafd252061
Petr Kotas <petr.kotas@gmail.com>
2019-04-26 13:39:25 +0200

    Add unit tests for restore

--- pkg/virt-controller/watch/vmr_test.go
+++ pkg/virt-controller/watch/vmr_test.go
@@ -17,68 +17,68 @@ import (
 
 var _ = Describe("VirtualMachineRestore", func() {
 
+	var ctrl *gomock.Controller
+
+	var vmInterface *kubecli.MockVirtualMachineInterface
+	var vmSource *framework.FakeControllerSource
+	var vmInformer cache.SharedIndexInformer
+
+	var vmsInterface *kubecli.MockVirtualMachineSnapshotInterface
+	var vmsSource *framework.FakeControllerSource
+	var vmsInformer cache.SharedIndexInformer
+
+	var vmrInterface *kubecli.MockVirtualMachineRestoreInterface
+	var vmrSource *framework.FakeControllerSource
+	var vmrInformer cache.SharedIndexInformer
+
+	var stop chan struct{}
+	var controller *VMRestoreController
+	var recorder *record.FakeRecorder
+	var mockQueue *testutils.MockWorkQueue
+
+	syncCaches := func(stop chan struct{}) {
+		go vmInformer.Run(stop)
+		go vmsInformer.Run(stop)
+		go vmrInformer.Run(stop)
+		Expect(cache.WaitForCacheSync(stop, vmInformer.HasSynced, vmsInformer.HasSynced, vmrInformer.HasSynced)).To(BeTrue())
+		log.Log.Info("Cache synced")
+	}
+
+	BeforeEach(func() {
+		stop = make(chan struct{})
+		ctrl = gomock.NewController(GinkgoT())
+		virtClient := kubecli.NewMockKubevirtClient(ctrl)
+
+		vmInterface = kubecli.NewMockVirtualMachineInterface(ctrl)
+		vmsInterface = kubecli.NewMockVirtualMachineSnapshotInterface(ctrl)
+		vmrInterface = kubecli.NewMockVirtualMachineRestoreInterface(ctrl)
+
+		vmInformer, vmSource = testutils.NewFakeInformerFor(&v1.VirtualMachine{})
+		vmsInformer, vmsSource = testutils.NewFakeInformerFor(&v1.VirtualMachineSnapshot{})
+		vmrInformer, vmrSource = testutils.NewFakeInformerFor(&v1.VirtualMachineRestore{})
+
+
+		recorder = record.NewFakeRecorder(100)
+
+		controller = NewVMRestoreController(vmInformer, vmsInformer, vmrInformer, recorder, virtClient)
+		// Wrap our workqueue to have a way to detect when we are done processing updates
+		mockQueue = testutils.NewMockWorkQueue(controller.Queue)
+		controller.Queue = mockQueue
+
+		// Set up mock client
+		virtClient.EXPECT().VirtualMachine(metav1.NamespaceDefault).Return(vmInterface).AnyTimes()
+		virtClient.EXPECT().VirtualMachineSnapshot(metav1.NamespaceDefault).Return(vmsInterface).AnyTimes()
+		virtClient.EXPECT().VirtualMachineRestore(metav1.NamespaceDefault).Return(vmrInterface).AnyTimes()
+		syncCaches(stop)
+	})
+
+	addRestore := func(s *v1.VirtualMachineRestore) {
+		mockQueue.ExpectAdds(1)
+		vmrSource.Add(s)
+		mockQueue.Wait()
+	}
+
 	Context("Valid VirtualMachineSnapshot given", func() {
-
-		var ctrl *gomock.Controller
-
-		var vmInterface *kubecli.MockVirtualMachineInterface
-		var vmSource *framework.FakeControllerSource
-		var vmInformer cache.SharedIndexInformer
-
-		var vmsInterface *kubecli.MockVirtualMachineSnapshotInterface
-		var vmsSource *framework.FakeControllerSource
-		var vmsInformer cache.SharedIndexInformer
-
-		var vmrInterface *kubecli.MockVirtualMachineRestoreInterface
-		var vmrSource *framework.FakeControllerSource
-		var vmrInformer cache.SharedIndexInformer
-
-		var stop chan struct{}
-		var controller *VMRestoreController
-		var recorder *record.FakeRecorder
-		var mockQueue *testutils.MockWorkQueue
-
-		syncCaches := func(stop chan struct{}) {
-			go vmInformer.Run(stop)
-			go vmsInformer.Run(stop)
-			go vmrInformer.Run(stop)
-			Expect(cache.WaitForCacheSync(stop, vmInformer.HasSynced, vmsInformer.HasSynced, vmrInformer.HasSynced)).To(BeTrue())
-			log.Log.Info("Cache synced")
-		}
-
-		BeforeEach(func() {
-			stop = make(chan struct{})
-			ctrl = gomock.NewController(GinkgoT())
-			virtClient := kubecli.NewMockKubevirtClient(ctrl)
-
-			vmInterface = kubecli.NewMockVirtualMachineInterface(ctrl)
-			vmsInterface = kubecli.NewMockVirtualMachineSnapshotInterface(ctrl)
-			vmrInterface = kubecli.NewMockVirtualMachineRestoreInterface(ctrl)
-
-			vmInformer, vmSource = testutils.NewFakeInformerFor(&v1.VirtualMachine{})
-			vmsInformer, vmsSource = testutils.NewFakeInformerFor(&v1.VirtualMachineSnapshot{})
-			vmrInformer, vmrSource = testutils.NewFakeInformerFor(&v1.VirtualMachineRestore{})
-
-
-			recorder = record.NewFakeRecorder(100)
-
-			controller = NewVMRestoreController(vmInformer, vmsInformer, vmrInformer, recorder, virtClient)
-			// Wrap our workqueue to have a way to detect when we are done processing updates
-			mockQueue = testutils.NewMockWorkQueue(controller.Queue)
-			controller.Queue = mockQueue
-
-			// Set up mock client
-			virtClient.EXPECT().VirtualMachine(metav1.NamespaceDefault).Return(vmInterface).AnyTimes()
-			virtClient.EXPECT().VirtualMachineSnapshot(metav1.NamespaceDefault).Return(vmsInterface).AnyTimes()
-			virtClient.EXPECT().VirtualMachineRestore(metav1.NamespaceDefault).Return(vmrInterface).AnyTimes()
-			syncCaches(stop)
-		})
-
-		addRestore := func(s *v1.VirtualMachineRestore) {
-			mockQueue.ExpectAdds(1)
-			vmrSource.Add(s)
-			mockQueue.Wait()
-		}
 
 	 	It("should restore the VirtualMachine spec", func() {
 			vms, vm := DefaultVMSnapshot()
@@ -100,7 +100,7 @@ var _ = Describe("VirtualMachineRestore", func() {
 			controller.Execute()
 		})
 
-		FIt("should not restore the VirtualMachine spec once VirtualMachine is running", func() {
+		It("should not restore the VirtualMachine spec when VirtualMachine is running", func() {
 			vms, vm := DefaultVMSnapshot()
 			vmr := DefaultVMRestore(vms)
 
@@ -114,27 +114,130 @@ var _ = Describe("VirtualMachineRestore", func() {
 
 			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
 			vmsInterface.EXPECT().Get(vms, gomock.Any()).Return(vms, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(len(obj.Status.Conditions)).To(Equal(1))
+				Expect(obj.Status.Conditions[0].Reason).To(Equal(VirtualMachineRunningSnapshotReason))
+			})
+
 
 			controller.Execute()
 		})
 
 		It("should restore once VM is not running", func() {
+			vms, vm := DefaultVMSnapshot()
+			vmr := DefaultVMRestore(vms)
+
+			vms.Status.VirtualMachine = vm.DeepCopy()
+
+			vm.Spec.Running = true
+
+			vmSource.Add(vm)
+			vmsSource.Add(vms)
+			addRestore(vmr)
+
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
+			vmsInterface.EXPECT().Get(vms, gomock.Any()).Return(vms, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(len(obj.Status.Conditions)).To(Equal(1))
+				Expect(obj.Status.Conditions[0].Reason).To(Equal(VirtualMachineRunningSnapshotReason))
+			})
+
+			controller.Execute()
+
+			updatedVM := vm.DeepCopy()
+			updatedVM.Spec.Running = false
+			vmSource.Modify(updatedVM)
+
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(updatedVM, nil)
+			vmsInterface.EXPECT().Get(vms, gomock.Any()).Return(vms, nil)
+			vmInterface.EXPECT().Update(updatedVM)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(obj.Status.RestoredOn).ToNot(BeNil())
+			})
 
 			controller.Execute()
 		})
 
 		It("should not restore VM when there is no snapshot", func() {
+			vms, vm := DefaultVMSnapshot()
+			vmr := DefaultVMRestore(vms)
 
+			vmSource.Add(vm)
+			addRestore(vmr)
+
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
+			vmsInterface.EXPECT().Get(gomock.Any(), gomock.Any()).Return(nil, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(len(obj.Status.Conditions)).To(Equal(1))
+				Expect(obj.Status.Conditions[0].Reason).To(Equal(VirtualMachineNoSnapshotReason))
+			})
+
+			controller.Execute()
 		})
 
-		It("shoudl not restore VM when snapshot does not have VM copied", func() {
+		It("should not restore VM when snapshot does not have VM copied", func() {
+			vms, vm := DefaultVMSnapshot()
+			vmr := DefaultVMRestore(vms)
 
+			vmSource.Add(vm)
+			vmsSource.Add(vms)
+			addRestore(vmr)
+
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
+			vmsInterface.EXPECT().Get(gomock.Any(), gomock.Any()).Return(nil, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(len(obj.Status.Conditions)).To(Equal(1))
+				Expect(obj.Status.Conditions[0].Reason).To(Equal(VirtualMachineNoSnapshotReason))
+			})
+
+			controller.Execute()
 		})
 
 	})
 
-	Context("Valid multiple VirtualMachineSnapshots given", func() {
+	FContext("Valid multiple VirtualMachineSnapshots given", func() {
 		It("should restore VM only from one snapshot once", func() {
+			vms, vm := DefaultVMSnapshot()
+			vms_2 := &v1.VirtualMachineSnapshot{
+				ObjectMeta: metav1.ObjectMeta{Name: "test_vms_2", Namespace: vm.ObjectMeta.Namespace, ResourceVersion: "1"},
+				Spec: v1.VirtualMachineSnapshotSpec{
+					VirtualMachine: vm.Name,
+				},
+			}
+
+			vms.Status.VirtualMachine = vm.DeepCopy()
+			vms_2.Status.VirtualMachine = vm.DeepCopy()
+
+			vmr := DefaultVMRestore(vms)
+
+			vmSource.Add(vm)
+			vmsSource.Add(vms)
+			vmsSource.Add(vms_2)
+			addRestore(vmr)
+
+			var oldTime metav1.Time
+
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
+			vmsInterface.EXPECT().Get(vms, gomock.Any()).Return(vms, nil)
+			vmInterface.EXPECT().Update(vm).Return(vm, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(obj.Status.RestoredOn).ToNot(BeNil())
+				oldTime = *obj.Status.RestoredOn
+			})
+
+
+			controller.Execute()
+
+			updatedVMR := vmr.DeepCopy()
+			updatedVMR.Spec.VirtualMachineSnapshot = "test_vms_2"
+
+			vmrSource.Modify(updatedVMR)
+			vmInterface.EXPECT().Get(vm, gomock.Any()).Return(vm, nil)
+			vmsInterface.EXPECT().Get(vms, gomock.Any()).Return(vms, nil)
+			vmInterface.EXPECT().Update(vm).Return(vm, nil)
+			vmrInterface.EXPECT().Update(gomock.Any()).Do(func(obj *v1.VirtualMachineRestore) {
+				Expect(obj.Status.RestoredOn).To(Equal(oldTime))
+			})
 
 		})
 	})
