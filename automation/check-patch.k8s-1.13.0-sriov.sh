b7efda6277bbf9565d346c2bc6120a9415d363b9
Ihar Hrachyshka <ihrachys@redhat.com>
2019-04-26 14:01:51 -0700

    Wait 1h for cluster up

--- automation/check-patch.k8s-1.13.0-sriov.sh
+++ automation/check-patch.k8s-1.13.0-sriov.sh
@@ -80,7 +80,7 @@ flock -e  -w "$SRIOV_TIMEOUT_SEC" "$fd" || {
 # ================
 go get -u sigs.k8s.io/kind
 
-kind --loglevel debug create cluster --retain --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
+kind --loglevel debug create cluster --wait=$((60*60)) --retain --name=${CLUSTER_NAME} --config=${MANIFESTS_DIR}/kind.yaml
 
 export KUBECONFIG=$(kind get kubeconfig-path --name=${CLUSTER_NAME})
 kubectl cluster-info
