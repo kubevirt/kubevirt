20e8e41e5170760fdf435cb93cc92083cc606d5c
Arik Hadas <ahadas@redhat.com>
2019-04-29 13:31:17 +0300

    remove mock_config#MakeFakeClusterConfig in favor of NewFakeClusterConfig
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- pkg/testutils/mock_config.go
+++ pkg/testutils/mock_config.go
@@ -3,9 +3,7 @@ package testutils
 import (
 	v1 "k8s.io/api/core/v1"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/rand"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
 
 	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
@@ -15,25 +13,6 @@ const (
 	configMapName = "kubevirt-config"
 	namespace     = "kubevirt"
 )
-
-func MakeFakeClusterConfig(configMaps []v1.ConfigMap, stopChan chan struct{}) *virtconfig.ClusterConfig {
-	cmListWatch := &cache.ListWatch{
-		ListFunc: func(options v12.ListOptions) (runtime.Object, error) {
-			return &v1.ConfigMapList{Items: configMaps}, nil
-		},
-		WatchFunc: func(options v12.ListOptions) (watch.Interface, error) {
-			fakeWatch := watch.NewFake()
-			for _, cfgMap := range configMaps {
-				fakeWatch.Add(&cfgMap)
-			}
-			return fakeWatch, nil
-		},
-	}
-	cmInformer := cache.NewSharedIndexInformer(cmListWatch, &v1.ConfigMap{}, 0, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
-	go cmInformer.Run(stopChan)
-	cache.WaitForCacheSync(stopChan, cmInformer.HasSynced)
-	return virtconfig.NewClusterConfig(cmInformer, namespace)
-}
 
 func NewFakeClusterConfig(cfgMap *v1.ConfigMap) (*virtconfig.ClusterConfig, cache.SharedIndexInformer) {
 	informer, _ := NewFakeInformerFor(&v1.ConfigMap{})
