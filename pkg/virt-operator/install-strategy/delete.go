e5cef7d0fecaf0865e8b9a1298637e9906aed125
David Vossel <davidvossel@gmail.com>
2019-05-14 15:53:24 -0400

    Use JSON patch type over StrategicMerge for SCC users during install
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/virt-operator/install-strategy/delete.go
+++ pkg/virt-operator/install-strategy/delete.go
@@ -278,10 +278,13 @@ func DeleteAll(kv *v1.KubeVirt,
 		if !ok {
 			return fmt.Errorf("couldn't cast object to SecurityContextConstraints: %+v", privSCCObj)
 		}
+
+		oldUsers := privSCC.Users
 		privSCCCopy := privSCC.DeepCopy()
+		users := privSCCCopy.Users
 
 		modified := false
-		users := privSCCCopy.Users
+
 		for _, acc := range sccPriv.ServiceAccounts {
 			removed := false
 			users, removed = remove(users, acc)
@@ -289,13 +292,19 @@ func DeleteAll(kv *v1.KubeVirt,
 		}
 
 		if modified {
+			oldUserBytes, err := json.Marshal(oldUsers)
+			if err != nil {
+				return err
+			}
 			userBytes, err := json.Marshal(users)
 			if err != nil {
 				return err
 			}
 
-			data := []byte(fmt.Sprintf(`{"users": %s}`, userBytes))
-			_, err = scc.SecurityContextConstraints().Patch(sccPriv.TargetSCC, types.StrategicMergePatchType, data)
+			test := fmt.Sprintf(`{ "op": "test", "path": "/users", "value": %s }`, string(oldUserBytes))
+			patch := fmt.Sprintf(`{ "op": "replace", "path": "/users", "value": %s }`, string(userBytes))
+
+			_, err = scc.SecurityContextConstraints().Patch(sccPriv.TargetSCC, types.JSONPatchType, []byte(fmt.Sprintf("[ %s, %s ]", test, patch)))
 			if err != nil {
 				return fmt.Errorf("unable to patch scc: %v", err)
 			}
