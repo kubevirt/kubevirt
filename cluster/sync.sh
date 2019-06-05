dbd2d1a96a047b43820ae279ebb005ee8656b106
Ido Rosenzwig <irosenzw@redhat.com>
2019-06-05 15:30:09 +0300

    Fix cluster-sync script after moving to quay.io (#112)
    Changing the image repo to quay.io broke the testing env.
    This patch fixes it.
    
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -10,14 +10,14 @@ rm -rf _out/
 cp -r deploy/standard _out/
 
 # Sed from docker.io to local registry
-sed -i "s/image: docker\.io\/rthallisey\/hyperconverged-cluster-operator:latest/image: registry:5000\/rthallisey\/hyperconverged-cluster-operator:latest/g" _out/operator.yaml
+sed -i "s/image: quay\.io\/kubevirt\/hyperconverged-cluster-operator:latest/image: registry:5000\/kubevirt\/hyperconverged-cluster-operator:latest/g" _out/operator.yaml
 
 CMD="./cluster/kubectl.sh" ./hack/clean.sh
 
 IMAGE_REGISTRY=$registry make docker-build-operator docker-push-operator
 
 for i in $(seq 1 ${CLUSTER_NUM_NODES}); do
-    ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" "sudo docker pull registry:5000/rthallisey/hyperconverged-cluster-operator"
+    ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" "sudo docker pull registry:5000/kubevirt/hyperconverged-cluster-operator"
     # Temporary until image is updated with provisioner that sets this field
     # This field is required by buildah tool
     ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" 'sudo sysctl -w user.max_user_namespaces=1024'
