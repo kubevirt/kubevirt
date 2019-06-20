b301d53f478ae1e082e18d0e696294f037c5baf8
Federico Paolinelli <fpaoline@redhat.com>
2019-06-20 09:56:54 +0200

    Run only on sriov tests

--- cluster-up/cluster/k8s-1.13.0-sriov/test.sh
+++ cluster-up/cluster/k8s-1.13.0-sriov/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-FUNC_TEST_ARGS='--ginkgo.noColor --junit-output=/go/src/kubevirt.io/kubevirt/tests.junit.xml --ginkgo.focus=SRIOV --kubeconfig /etc/kubernetes/admin.conf' make functest
+PATH=~/go/bin:$PATH
+go get -u github.com/onsi/ginkgo/ginkgo
+FUNC_TEST_ARGS='--ginkgo.noColor --junit-output=/go/src/kubevirt.io/kubevirt/tests.junit.xml --ginkgo.focus=Multus.*sriov --kubeconfig /etc/kubernetes/admin.conf' make functest
