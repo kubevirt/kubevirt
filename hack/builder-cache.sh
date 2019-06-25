9e20a19f0a4724a1a5f9237a6c4ce30dff5dbd49
Artyom Lukianov <alukiano@redhat.com>
2019-06-25 10:39:58 +0300

    Fix name of build functest image function

--- hack/builder-cache.sh
+++ hack/builder-cache.sh
@@ -7,10 +7,10 @@ source hack/config.sh
 
 target=$1
 
-if [ "${target}" = "push-cache" ]; then
+if [ "${target}" = "push" ]; then
     docker push kubevirt/builder-cache:${KUBEVIRT_UPDATE_CACHE_FROM}
 fi
 
-if [ "${target}" = "pull-cache" ]; then
+if [ "${target}" = "pull" ]; then
     docker pull kubevirt/builder-cache:${KUBEVIRT_CACHE_FROM}
 fi
