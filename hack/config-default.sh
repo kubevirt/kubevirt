0123ef0a30bf16a7174d2050ba664f0bc3d036d5
Marc Sluiter <msluiter@redhat.com>
2019-07-04 12:01:06 +0200

    Pull in latest CI cluster
    - increased pod limit on os clusters to 110
    - updated local volume provisioner
    - new OKD 4.1.2 provider
    - audit logging enabled
    - some fixes and additions to gocli
    
    For a complete list see https://github.com/kubevirt/kubevirtci/commits/master
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -7,7 +7,7 @@ cdi_namespace=cdi
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
 verbosity=${VERBOSITY:-2}
 package_name=${PACKAGE_NAME:-kubevirt-dev}
-kubevirtci_git_hash="45cdf80da619866cdab36295de25a8aeea7eccbc"
+kubevirtci_git_hash="c411ed2b6580b525e2535b440a1cd83f0f837f9c"
 
 # try to derive csv_version from docker tag. But it must start with x.y.z, without leading v
 default_csv_version="${docker_tag/latest/0.0.0}"
