968e1ef07ab2837cb68696c03da5d41adfbc87cb
Federico Paolinelli <fpaoline@redhat.com>
2019-07-26 12:32:51 +0200

    Increase the syncvmi grpc call timeout

--- pkg/virt-handler/cmd-client/client.go
+++ pkg/virt-handler/cmd-client/client.go
@@ -180,7 +180,7 @@ func (c *VirtLauncherClient) genericSendVMICmd(cmdName string,
 		},
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	response, err := cmdFunc(ctx, request)
 
