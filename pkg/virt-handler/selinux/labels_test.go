792c717131ac57acc2f64248fd20aa297496bd1c
Roman Mohr <rmohr@redhat.com>
2019-08-02 12:50:40 +0200

    Discover selinux on the usual pathes and not just on /usr/sbin

---
+++ pkg/virt-handler/selinux/labels_test.go
@@ -0,0 +1,112 @@
+package selinux
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("selinux", func() {
+
+	var tempDir string
+	var selinux *SELinuxImpl
+
+	BeforeEach(func() {
+		var err error
+		tempDir, err = ioutil.TempDir("", "kubevirt")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(os.MkdirAll(filepath.Join(tempDir, "/usr/sbin"), 0777)).ToNot(HaveOccurred())
+		Expect(os.MkdirAll(filepath.Join(tempDir, "/usr/bin"), 0777)).ToNot(HaveOccurred())
+		selinux = &SELinuxImpl{
+			Paths:         []string{"/usr/bin", "/usr/sbin"},
+			procOnePrefix: tempDir,
+		}
+	})
+
+	AfterEach(func() {
+		os.RemoveAll(tempDir)
+	})
+
+	Context("detecting if selinux is present", func() {
+
+		It("should detect that it is disabled if getenforce is not in path", func() {
+			present, err := selinux.IsPresent()
+			Expect(err).To(BeNil())
+			Expect(present).To(BeFalse())
+		})
+		It("should detect that it is disabled if getenforce returns Disabled", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "getenforce"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return []byte("Disabled"), nil
+			}
+			present, err := selinux.IsPresent()
+			Expect(err).To(BeNil())
+			Expect(present).To(BeFalse())
+		})
+		It("should detect that it is enabled if getenforce does not return Disabled", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "getenforce"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return []byte("Enforced"), nil
+			}
+			present, err := selinux.IsPresent()
+			Expect(err).To(BeNil())
+			Expect(present).To(BeTrue())
+		})
+	})
+
+	Context("labeling a directory", func() {
+
+		It("should fail if semanage does not exist", func() {
+			Expect(selinux.Label("whatever", "nothing")).ToNot(Succeed())
+		})
+
+		It("should fail if semanage exists but the command fails", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "semanage"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return nil, fmt.Errorf("I failed")
+			}
+			Expect(selinux.Label("whatever", "nothing")).ToNot(Succeed())
+		})
+
+		It("should succeed if semanage exists and the command runs successful", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "semanage"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return []byte("I succeeded"), nil
+			}
+			Expect(selinux.Label("whatever", "nothing")).To(Succeed())
+		})
+	})
+
+	Context("restoring labels on a directory", func() {
+
+		It("should fail if restorecon does not exist", func() {
+			Expect(selinux.Restore("whatever")).ToNot(Succeed())
+		})
+
+		It("should fail if restorecon exists but the command fails", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "restorecon"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return nil, fmt.Errorf("I failed")
+			}
+			Expect(selinux.Restore("nothing")).ToNot(Succeed())
+		})
+
+		It("should succeed if restorecon exists and the command runs successful", func() {
+			touch(filepath.Join(tempDir, "/usr/bin", "restorecon"))
+			selinux.execFunc = func(binary string, args ...string) (bytes []byte, e error) {
+				return []byte("I succeeded"), nil
+			}
+			Expect(selinux.Restore("nothing")).To(Succeed())
+		})
+	})
+})
+
+func touch(path string) {
+	f, err := os.Create(path)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(f.Close()).To(Succeed())
+}
