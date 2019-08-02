8385c2c15ea810945b00e11a6ac65aa4dd55d454
Petr Kotas <petr.kotas@gmail.com>
2019-08-02 14:35:17 +0200

    Make migration VM smaller and timeout longer
    When doing migration, the copying of memory
    can take time on slower network.
    This fix makes the chance of failure because of
    memoery copy timeouts smaller.

--- tests/migration_test.go
+++ tests/migration_test.go
@@ -1152,7 +1152,7 @@ var _ = Describe("[rfe_id:393][crit:high[vendor:cnv-qe@redhat.com][level:system]
 			tests.CreateSecret(secretName, secret_data)
 
 			vmi := tests.NewRandomVMIWithEphemeralDisk(tests.ContainerDiskFor(tests.ContainerDiskFedora))
-			vmi.Spec.Domain.Resources.Requests[k8sv1.ResourceMemory] = resource.MustParse("2G")
+			vmi.Spec.Domain.Resources.Requests[k8sv1.ResourceMemory] = resource.MustParse("512M")
 			tests.AddUserData(vmi, "cloud-init", "#cloud-config\npassword: fedora\nchpasswd: { expire: False }\n")
 			tests.AddConfigMapDisk(vmi, configMapName)
 			tests.AddSecretDisk(vmi, secretName)
@@ -1163,7 +1163,7 @@ var _ = Describe("[rfe_id:393][crit:high[vendor:cnv-qe@redhat.com][level:system]
 			// execute a migration, wait for finalized state
 			By("Starting the Migration")
 			migration := tests.NewRandomMigration(vmi.Name, vmi.Namespace)
-			migrationUID := runMigrationAndExpectCompletion(migration, 180)
+			migrationUID := runMigrationAndExpectCompletion(migration, 240)
 
 			// check VMI, confirm migration state
 			confirmVMIPostMigration(vmi, migrationUID)
