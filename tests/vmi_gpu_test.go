e374aec2e69b5fa891031e94547fa0d0f3d31582
Vishesh Tanksale <vtanksale@nvidia.com>
2019-09-03 13:38:58 -0700

    Fixing merge errors
    Signed-off-by: Vishesh Tanksale <vtanksale@nvidia.com>

--- tests/vmi_gpu_test.go
+++ tests/vmi_gpu_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 	k8sv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/client-go/log"
