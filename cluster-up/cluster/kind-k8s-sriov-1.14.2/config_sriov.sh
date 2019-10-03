ba4161d727388bedc0008d81043a245cb3239ba9
Federico Paolinelli <fpaoline@redhat.com>
2019-10-03 16:15:32 +0200

    Point to latest kubevirtci commit to fetch changes in the kind lane. Run make generate to fetch the latest changes. Switch to 2 nodes even for sriov tests since the sriov provider now allows that.
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- cluster-up/cluster/kind-k8s-sriov-1.14.2/config_sriov.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.14.2/config_sriov.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -e
 set -x
 
-CONTROL_PLANE_CMD="docker exec -it -d ${CLUSTER_NAME}-control-plane"
+source ${KUBEVIRTCI_PATH}/cluster/kind/common.sh
+
 MANIFESTS_DIR="${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/manifests"
+
+MASTER_NODE="${CLUSTER_NAME}-control-plane"
+FIRST_WORKER_NODE="${CLUSTER_NAME}-worker"
+
 OPERATOR_GIT_HASH=b3ab84a316e16df392fbe9e07dbe0667ad075855
 
 # not using kubectl wait since with the sriov operator the pods get restarted a couple of times and this is
 # more reliable
 function wait_pods_ready {
-    while [ -n "$(kubectl get pods --all-namespaces -o'custom-columns=status:status.containerStatuses[*].ready,metadata:metadata.name' --no-headers | grep false)" ]; do
+    while [ -n "$(_kubectl get pods --all-namespaces -o'custom-columns=status:status.containerStatuses[*].ready,metadata:metadata.name' --no-headers | grep false)" ]; do
         echo "Waiting for all pods to become ready ..."
-        kubectl get pods --all-namespaces -o'custom-columns=status:status.containerStatuses[*].ready,metadata:metadata.name' --no-headers
+        _kubectl get pods --all-namespaces -o'custom-columns=status:status.containerStatuses[*].ready,metadata:metadata.name' --no-headers
         sleep 10
     done
 }
@@ -28,35 +33,45 @@ function deploy_sriov_operator {
     sed -i '/SRIOV_CNI_IMAGE/!b;n;c\              value: nfvpe\/sriov-cni' ./deploy/operator.yaml
 
     # on prow nodes the default shell is dash and some commands are not working
-    make deploy-setup-k8s SHELL=/bin/bash OPERATOR_EXEC=kubectl
+    make deploy-setup-k8s SHELL=/bin/bash OPERATOR_EXEC="${KUBECTL}"
   popd
 }
 
+if [[ -z "$(_kubectl get nodes | grep $FIRST_WORKER_NODE)" ]]; then
+  SRIOV_NODE=$MASTER_NODE
+else
+  SRIOV_NODE=$FIRST_WORKER_NODE
+fi
+
 #move the pf to the node
 mkdir -p /var/run/netns/
-export pid="$(docker inspect -f '{{.State.Pid}}' ${CLUSTER_NAME}-control-plane)"
-ln -sf /proc/$pid/ns/net "/var/run/netns/${CLUSTER_NAME}-control-plane"
+export pid="$(docker inspect -f '{{.State.Pid}}' $SRIOV_NODE)"
+ln -sf /proc/$pid/ns/net "/var/run/netns/$SRIOV_NODE"
 
 sriov_pfs=( /sys/class/net/*/device/sriov_numvfs )
 
-counter=0
+
 for ifs in "${sriov_pfs[@]}"; do
   ifs_name="${ifs%%/device/*}"
   ifs_name="${ifs_name##*/}"
 
-  if  [[ "$counter" -eq 0 ]]; then
-    # These values are used to populate the network definition policy yaml. 
-    # We need the num of vfs because if we don't set this value equals to the total, in case of mellanox 
-    # the sriov operator will trigger a node reboot to update the firmware
-    export FIRST_PF="$ifs_name"
-    export FIRST_PF_NUM_VFS=$(cat /sys/class/net/"$FIRST_PF"/device/sriov_totalvfs)
+  if [ $(echo "${PF_BLACKLIST[@]}" | grep -q "${ifs_name}") ]; then
+    continue
   fi
-  ip link set "$ifs_name" netns "${CLUSTER_NAME}-control-plane"
-  counter=$((counter+1))
+
+  # These values are used to populate the network definition policy yaml.
+  # We just use the first suitable pf
+  # We need the num of vfs because if we don't set this value equals to the total, in case of mellanox
+  # the sriov operator will trigger a node reboot to update the firmware
+  export NODE_PF="$ifs_name"
+  export NODE_PF_NUM_VFS=$(cat /sys/class/net/"$NODE_PF"/device/sriov_totalvfs)
+  break
 done
 
+ip link set "$NODE_PF" netns "$SRIOV_NODE"
+
 # deploy multus
-kubectl create -f $MANIFESTS_DIR/multus.yaml
+_kubectl create -f $MANIFESTS_DIR/multus.yaml
 
 # give them some time to create pods before checking pod status
 sleep 10
@@ -64,15 +79,17 @@ sleep 10
 # make sure all containers are ready
 wait_pods_ready
 
-${CONTROL_PLANE_CMD} mount -o remount,rw /sys     # kind remounts it as readonly when it starts, we need it to be writeable
+SRIOV_NODE_CMD="docker exec -it -d ${SRIOV_NODE}"
+
+${SRIOV_NODE_CMD} mount -o remount,rw /sys     # kind remounts it as readonly when it starts, we need it to be writeable
 
 deploy_sriov_operator
 
-kubectl label node sriov-control-plane node-role.kubernetes.io/worker=
-kubectl label node sriov-control-plane sriov=true 
-envsubst < $MANIFESTS_DIR/network_config_policy.yaml | kubectl create -f -
+_kubectl label node $SRIOV_NODE node-role.kubernetes.io/worker=
+_kubectl label node $SRIOV_NODE sriov=true
+envsubst < $MANIFESTS_DIR/network_config_policy.yaml | _kubectl create -f -
 
 
 wait_pods_ready
 
-${CONTROL_PLANE_CMD} chmod 666 /dev/vfio/vfio
+${SRIOV_NODE_CMD} chmod 666 /dev/vfio/vfio
