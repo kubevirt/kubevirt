850d0c2b6e8decf53bb8ae5ee07d38c09b17074b
Guangming Wang <guangming.wang@daocloud.io>
2019-10-09 23:24:39 +0800

    fix file close panic when error happens in OpenFile
    Signed-off-by: Guangming Wang <guangming.wang@daocloud.io>

--- pkg/virt-launcher/virtwrap/api/converter.go
+++ pkg/virt-launcher/virtwrap/api/converter.go
@@ -130,10 +130,10 @@ func checkDirectIOFlag(path string) bool {
 	// check if fs where disk.img file is located or block device
 	// support direct i/o
 	f, err := os.OpenFile(path, syscall.O_RDONLY|syscall.O_DIRECT, 0)
-	defer f.Close()
 	if err != nil && !os.IsNotExist(err) {
 		return false
 	}
+	defer f.Close()
 	return true
 }
 
