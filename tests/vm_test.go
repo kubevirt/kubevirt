9a11af0c229a5d0fc741dcfd15b1dcb4079e06c5
Francesco Romani <fromani@redhat.com>
2019-10-30 19:10:04 +0100

    DONTMERGE: WIP: TEST: CI pr test #2
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- tests/vm_test.go
+++ tests/vm_test.go
@@ -470,7 +470,7 @@ var _ = Describe("[rfe_id:1177][crit:medium][vendor:cnv-qe@redhat.com][level:com
 			}
 		})
 
-		It("[test_id:1527]should not update the VirtualMachineInstance spec if Running", func() {
+		FIt("[test_id:1527]should not update the VirtualMachineInstance spec if Running", func() {
 			newVM := newVirtualMachine(true)
 
 			Eventually(func() bool {
