ab32e88cf43922e81cf46abda32c6dec094a80a0
Or Shoval <oshoval@redhat.com>
2019-12-25 16:25:08 +0200

    Add bump kubevirtci script
    Script would update hack/config-default.sh,
    and then its possible to run make generate in order to fetch
    newest kubevirtci cluster-up folder
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

---
+++ hack/bump-kubevirtci.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+val=$(git ls-remote https://github.com/kubevirt/kubevirtci | grep HEAD | awk '{print $1}')
+sed -i "/^[[:blank:]]*kubevirtci_git_hash[[:blank:]]*=/s/=.*/=\"${val}\"/" hack/config-default.sh
+git --no-pager diff hack/config-default.sh | grep "kubevirtci_git_hash"
