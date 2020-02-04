f39e4123d2d18f2e2f12898f52f1e3722362f42f
Roman Mohr <rmohr@redhat.com>
2020-02-04 14:56:45 +0100

    Add support for building a multi-arch builder manifest
    Build a ppc64le and amd64 multi-arch image with qemu-user on amd64
    nodes.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/builder/publish.sh
+++ hack/builder/publish.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
+set -ex
 
 SCRIPT_DIR="$(
     cd "$(dirname "$BASH_SOURCE[0]")"
     pwd
 )"
 
+trap 'cleanup' EXIT
+
+cleanup() {
+    rm manifests/ -rf || true
+}
+
 . ${SCRIPT_DIR}/version.sh
 
-docker tag kubevirt/builder:${VERSION} docker.io/kubevirt/builder:${VERSION}
-docker push docker.io/kubevirt/builder:${VERSION}
+cleanup
+
+for ARCH in ${ARCHITECTURES}; do
+    docker push kubevirt/builder:${VERSION}-${ARCH}
+    TMP_IMAGES="${TMP_IMAGES} kubevirt/builder:${VERSION}-${ARCH}"
+done
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker manifest create --amend kubevirt/builder:${VERSION} ${TMP_IMAGES}
+docker manifest push kubevirt/builder:${VERSION}
