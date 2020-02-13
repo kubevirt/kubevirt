d77e9327480f462a3f5a13abe9d695adc1af4d82
Jed Lejosne <jed@redhat.com>
2020-02-13 15:43:59 -0500

    Add test and validation for EFI SecureBoot
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-launcher/virtwrap/api/converter_test.go
+++ pkg/virt-launcher/virtwrap/api/converter_test.go
@@ -2176,8 +2176,26 @@ var _ = Describe("Converter", func() {
 				domainSpec := vmiToDomainXMLToDomainSpec(vmi, c)
 				Expect(domainSpec.OS.BootLoader.ReadOnly).To(Equal("yes"))
 				Expect(domainSpec.OS.BootLoader.Type).To(Equal("pflash"))
+				Expect(domainSpec.OS.BootLoader.Secure).To(Equal("no"))
 				Expect(domainSpec.OS.BootLoader.Path).To(Equal(EFIPath))
 				Expect(domainSpec.OS.NVRam.Template).To(Equal(EFIVarsPath))
+				Expect(domainSpec.OS.NVRam.NVRam).To(Equal("/tmp/mynamespace_testvmi"))
+			})
+
+			It("should configure the EFI bootloader if EFI secure option", func() {
+				vmi.Spec.Domain.Firmware = &v1.Firmware{
+					Bootloader: &v1.Bootloader{
+						EFI: &v1.EFI{
+							SecureBoot: True(),
+						},
+					},
+				}
+				domainSpec := vmiToDomainXMLToDomainSpec(vmi, c)
+				Expect(domainSpec.OS.BootLoader.ReadOnly).To(Equal("yes"))
+				Expect(domainSpec.OS.BootLoader.Type).To(Equal("pflash"))
+				Expect(domainSpec.OS.BootLoader.Secure).To(Equal("yes"))
+				Expect(domainSpec.OS.BootLoader.Path).To(Equal(EFIPathSecureBoot))
+				Expect(domainSpec.OS.NVRam.Template).To(Equal(EFIVarsPathSecureBoot))
 				Expect(domainSpec.OS.NVRam.NVRam).To(Equal("/tmp/mynamespace_testvmi"))
 			})
 		})
