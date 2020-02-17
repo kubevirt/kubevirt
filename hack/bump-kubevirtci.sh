ff1f36d7aff4e659fbf4bbc13ebd39f227d1dbef
Roman Mohr <rmohr@redhat.com>
2020-02-17 11:44:55 +0100

    Add kubevirtci and vendor bump scripts
    Add scripts which can be used to do the following automatic code bumps:
    
     * Bump to latest kubevirtci version
     * Update patch or minor versions of vendor dependencies
    
    Further a KUBEVIRT_RUN_UNNUESTED environment variable is added, which
    allows to execute all Makefile targets without nested docker. That is
    useful when already running inside a builder image in e.g. a prow job.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/bump-kubevirtci.sh
+++ hack/bump-kubevirtci.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+set -ex
+
+source $(dirname "$0")/common.sh
+source $(dirname "$0")/config.sh
+
 val=$(git ls-remote https://github.com/kubevirt/kubevirtci | grep HEAD | awk '{print $1}')
 sed -i "/^[[:blank:]]*kubevirtci_git_hash[[:blank:]]*=/s/=.*/=\"${val}\"/" hack/config-default.sh
-git --no-pager diff hack/config-default.sh | grep "kubevirtci_git_hash"
+
+hack/sync-kubevirtci.sh
