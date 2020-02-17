95f00f985524d2a6ea1ac27133cc4f81808085f7
Vladik Romanovsky <vromanso@redhat.com>
2020-02-17 17:21:37 -0500

    functional test to verify guest time is updated after migration
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- pkg/virt-launcher/virtwrap/manager.go
+++ pkg/virt-launcher/virtwrap/manager.go
@@ -487,7 +487,7 @@ func (l *LibvirtDomainManager) SetGuestTime(vmi *v1.VirtualMachineInstance) erro
 }
 
 func (l *LibvirtDomainManager) setGuestTime(vmi *v1.VirtualMachineInstance, dom cli.VirDomain) {
-	timeout := time.After(5 * time.Second)
+	timeout := time.After(60 * time.Second)
 	tick := time.Tick(1 * time.Second)
 	for {
 		select {
@@ -520,8 +520,9 @@ func (l *LibvirtDomainManager) setGuestTime(vmi *v1.VirtualMachineInstance, dom 
 				default:
 					log.Log.Object(vmi).Reason(err).Error("failed to sync guest time")
 				}
+			} else {
+				return
 			}
-			return
 		}
 	}
 }
