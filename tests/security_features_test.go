461f47700d75f4957222f326979a2877aa473a60
Stu Gott <sgott@redhat.com>
2020-02-20 16:40:39 -0500

    Fix SELinux functional tests
    Some tests assumed that selinuxLauncherType would not be set.
    
    Signed-off-by: Stu Gott <sgott@redhat.com>

--- tests/security_features_test.go
+++ tests/security_features_test.go
@@ -20,58 +20,162 @@
 package tests_test
 
 import (
+	"encoding/json"
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/kubecli"
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 	"kubevirt.io/kubevirt/tests"
 )
 
 var _ = Describe("SecurityFeatures", func() {
 	tests.FlagParse()
 
+	var originalKubeVirtConfig *k8sv1.ConfigMap
 	virtClient, err := kubecli.GetKubevirtClient()
 	tests.PanicOnError(err)
+
+	tests.BeforeAll(func() {
+		originalKubeVirtConfig, err = virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Get("kubevirt-config", metav1.GetOptions{})
+		if err != nil && !errors.IsNotFound(err) {
+			Expect(err).ToNot(HaveOccurred())
+		}
+
+		if errors.IsNotFound(err) {
+			// create an empty kubevirt-config configmap if none exists.
+			cfgMap := &k8sv1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: "kubevirt-config"},
+				Data: map[string]string{
+					"feature-gates": "",
+				},
+			}
+
+			originalKubeVirtConfig, err = virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Create(cfgMap)
+			Expect(err).ToNot(HaveOccurred())
+		}
+	})
+
+	AfterEach(func() {
+		curKubeVirtConfig, err := virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Get("kubevirt-config", metav1.GetOptions{})
+		Expect(err).ToNot(HaveOccurred())
+
+		// if revision changed, revert ConfigMap
+		if curKubeVirtConfig.ResourceVersion != originalKubeVirtConfig.ResourceVersion {
+			// Add Spec Patch
+			newData, err := json.Marshal(originalKubeVirtConfig.Data)
+			Expect(err).ToNot(HaveOccurred())
+			data := fmt.Sprintf(`[{ "op": "replace", "path": "/data", "value": %s }]`, string(newData))
+
+			originalKubeVirtConfig, err = virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Patch("kubevirt-config", types.JSONPatchType, []byte(data))
+			Expect(err).ToNot(HaveOccurred())
+		}
+
+	})
 
 	Context("Check virt-launcher securityContext", func() {
 
 		var container k8sv1.Container
 		var vmi *v1.VirtualMachineInstance
 
-		BeforeEach(func() {
-			vmi = tests.NewRandomVMIWithEphemeralDiskAndUserdata(tests.ContainerDiskFor(tests.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
-		})
+		Context("With selinuxLauncherType undefined", func() {
+			BeforeEach(func() {
+				kubeVirtConfig, err := virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Get("kubevirt-config", metav1.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				// delete selinuxLauncherType if it's set
+				_, ok := kubeVirtConfig.Data[virtconfig.SELinuxLauncherTypeKey]
+				if ok {
+					delete(kubeVirtConfig.Data, virtconfig.SELinuxLauncherTypeKey)
 
-		It("[test_id:2953]Ensure virt-launcher pod securityContext type is virt_launcher.process", func() {
+					newData, err := json.Marshal(kubeVirtConfig.Data)
+					Expect(err).ToNot(HaveOccurred())
+					data := fmt.Sprintf(`[{ "op": "replace", "path": "/data", "value": %s }]`, string(newData))
 
-			By("Starting a VirtualMachineInstance")
-			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
-			Expect(err).ToNot(HaveOccurred())
-			tests.WaitForSuccessfulVMIStart(vmi)
-
-			By("Check virt-launcher pod SecurityContext values")
-			vmiPod := tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault)
-			Expect(vmiPod.Spec.SecurityContext.SELinuxOptions.Type).To(Equal("virt_launcher.process"))
-		})
-
-		It("[test_id:2895]Make sure the virt-launcher pod is not priviledged", func() {
-
-			By("Starting a VirtualMachineInstance")
-			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
-			Expect(err).ToNot(HaveOccurred())
-			tests.WaitForSuccessfulVMIStart(vmi)
-
-			By("Check virt-launcher pod SecurityContext values")
-			vmiPod := tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault)
-			for _, containerSpec := range vmiPod.Spec.Containers {
-				if containerSpec.Name == "compute" {
-					container = containerSpec
-					break
+					kubeVirtConfig, err = virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Patch("kubevirt-config", types.JSONPatchType, []byte(data))
+					Expect(err).ToNot(HaveOccurred())
 				}
-			}
-			Expect(*container.SecurityContext.Privileged).To(BeFalse())
+
+				vmi = tests.NewRandomVMIWithEphemeralDiskAndUserdata(tests.ContainerDiskFor(tests.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
+			})
+
+			It("[test_id:2953]Ensure virt-launcher pod securityContext type is virt_launcher.process", func() {
+
+				By("Starting a VirtualMachineInstance")
+				vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
+				Expect(err).ToNot(HaveOccurred())
+				tests.WaitForSuccessfulVMIStart(vmi)
+
+				By("Check virt-launcher pod SecurityContext values")
+				vmiPod := tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault)
+				Expect(vmiPod.Spec.SecurityContext.SELinuxOptions.Type).To(Equal("virt_launcher.process"))
+			})
+
+			It("[test_id:2895]Make sure the virt-launcher pod is not priviledged", func() {
+
+				By("Starting a VirtualMachineInstance")
+				vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
+				Expect(err).ToNot(HaveOccurred())
+				tests.WaitForSuccessfulVMIStart(vmi)
+
+				By("Check virt-launcher pod SecurityContext values")
+				vmiPod := tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault)
+				for _, containerSpec := range vmiPod.Spec.Containers {
+					if containerSpec.Name == "compute" {
+						container = containerSpec
+						break
+					}
+				}
+				Expect(*container.SecurityContext.Privileged).To(BeFalse())
+			})
+		})
+
+		Context("With selinuxLauncherType defined", func() {
+			var superPrivilegedType string
+
+			BeforeEach(func() {
+				superPrivilegedType = "spc_t"
+				kubeVirtConfig, err := virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Get("kubevirt-config", metav1.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				if kubeVirtConfig.Data[virtconfig.SELinuxLauncherTypeKey] != superPrivilegedType {
+					kubeVirtConfig.Data[virtconfig.SELinuxLauncherTypeKey] = superPrivilegedType
+
+					newData, err := json.Marshal(kubeVirtConfig.Data)
+					Expect(err).ToNot(HaveOccurred())
+					data := fmt.Sprintf(`[{ "op": "replace", "path": "/data", "value": %s }]`, string(newData))
+
+					kubeVirtConfig, err = virtClient.CoreV1().ConfigMaps(tests.KubeVirtInstallNamespace).Patch("kubevirt-config", types.JSONPatchType, []byte(data))
+					Expect(err).ToNot(HaveOccurred())
+				}
+
+				vmi = tests.NewRandomVMIWithEphemeralDisk(tests.ContainerDiskFor(tests.ContainerDiskAlpine))
+			})
+
+			It("Should honor custom SELinux type for virt-launcher", func() {
+				By("Starting a New VMI")
+				vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
+				Expect(err).ToNot(HaveOccurred())
+				tests.WaitForSuccessfulVMIStart(vmi)
+
+				By("Ensuring VMI is running by logging in")
+				tests.WaitUntilVMIReady(vmi, tests.LoggedInAlpineExpecter)
+
+				By("Fetching virt-launcher Pod")
+				pod := tests.GetPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault)
+
+				By("Verifying SELinux context contains custom type")
+				Expect(pod.Spec.SecurityContext.SELinuxOptions.Type).To(Equal(superPrivilegedType))
+
+				By("Deleting the VMI")
+				err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Delete(vmi.Name, &metav1.DeleteOptions{})
+				Expect(err).ToNot(HaveOccurred())
+			})
 		})
 	})
 })
