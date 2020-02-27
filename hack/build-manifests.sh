e8ac719af0b1a5816fa3e72e88fb4864d3ff5b0e
Simone Tiraboschi <stirabos@redhat.com>
2020-02-27 13:49:29 +0100

    bump CNAO to 0.27.2 (#457)
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/build-manifests.sh
+++ hack/build-manifests.sh
@@ -40,7 +40,7 @@ IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-IfNotPresent}"
 
 # Component Images
 KUBEVIRT_IMAGE="${KUBEVIRT_IMAGE:-docker.io/kubevirt/virt-operator:v0.26.0}"
-CNA_IMAGE="${CNA_IMAGE:-quay.io/kubevirt/cluster-network-addons-operator:0.24.0}"
+CNA_IMAGE="${CNA_IMAGE:-quay.io/kubevirt/cluster-network-addons-operator:0.27.2}"
 SSP_IMAGE="${SSP_IMAGE:-quay.io/fromani/kubevirt-ssp-operator-container:v1.0.19}"
 CDI_IMAGE="${CDI_IMAGE:-docker.io/kubevirt/cdi-operator:v1.13.1}"
 NMO_IMAGE="${NMO_IMAGE:-quay.io/kubevirt/node-maintenance-operator:v0.4.0}"
