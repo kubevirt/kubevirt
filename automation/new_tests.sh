ca5de22bee9cb8b20d623be82f58d7eef4c61a8c
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-03-12 12:30:25 +0100

    Address review feedback
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- automation/new_tests.sh
+++ automation/new_tests.sh
@@ -2,11 +2,35 @@
 
 set -euo pipefail
 
-if [[ "$#" -eq 0 ]] || [[ -z "$1" ]]; then
-    TARGET_COMMIT=$(git log -1 --format=%H --merges)
-else
+function usage {
+    cat <<EOF
+usage: $0 [commit_id]
+
+    generate a regex string containing changed tests for using it with
+    [ginkgo.focus](https://onsi.github.io/ginkgo/#focused-specs) parameter.
+    Return a string in the form
+
+        'test1|test2|...|testn'
+
+EOF
+}
+
+if [[ "$#" -gt 0 ]]; then
+    if [[ "$1" =~ '-h' ]]; then
+        usage
+        exit 0
+    fi
     TARGET_COMMIT="$1"
+else
+    # if there's no commit provided default to the latest merge commit
+    TARGET_COMMIT=$(git log -1 --format=%H --merges)
 fi
+
+# 1. fetch the changed file names from within the tests/ directory
+# 2. grep all files ending with '_test.go'
+# 3. remove `tests/` and `_test.go` to only have the test name
+# 4. replace newline with `|`
+# 5. remove last `|`
 git diff --name-only "${TARGET_COMMIT}".. -- tests/ \
     | grep -E '_test\.go$' \
     | sed -E 's/tests\/(.*_test)\.go/\1/' \
--- automation/repeated_test.sh
+++ automation/repeated_test.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+set -euo pipefail
+
+function usage {
+    cat <<EOF
+usage: [NUM_TESTS=x] [NEW_TESTS=test1_test|...|testn_test] $0 [kubevirtci_provider[ kubevirtci_provider ...]]
+
+    run test lanes repeatedly using the set of test files that have been
+    changed or added since last merge commit, set NEW_TESTS to explicitly name the tests to run)
+
+    options:
+        NUM_TESTS - how often each test lane is run, default is 3
+        NEW_TESTS - what set of tests to run, defaults to all test files added or changed since
+                    last merge commit
+
+    example:
+
+        NEW_TESTS='operator_test' ./automation/repeated_test.sh 'k8s-1.16.2'
+
+        runs tests/operator_test.go three times on kubevirtci provider k8s-1.16.2
+
+EOF
+}
+
+if (( $# > 0 )); then
+    if [[ "$1" =~ -h ]]; then
+        usage
+        exit 0
+    fi
+fi
+
 SCRIPT_PATH="$(
     cd "$(dirname "${BASH_SOURCE[0]}")/" || exit
     echo "$(pwd)/"
 )"
 
-NUM_TESTS=${NUM_TESTS:-3}
-if [[ -z ${NEW_TESTS} ]]; then
+NUM_TESTS=${NUM_TESTS-3}
+if [[ -z ${NEW_TESTS-} ]]; then
     NEW_TESTS=$("${SCRIPT_PATH}"/new_tests.sh)
     # skip certain tests for now, as we don't have a strategy currently
     NEW_TESTS=$(echo "$NEW_TESTS" | sed -E 's/\|?[^\|]*(sriov|multus|windows|genie|gpu)[^\|]*//g')
@@ -16,11 +46,24 @@ if [[ -z "${NEW_TESTS}" ]]; then
     exit 0
 fi
 
-TEST_LANES=( 'k8s-1.14.6' 'k8s-1.15.1' 'k8s-1.16.2' )
+if (( $# > 0 )); then
+    declare -a TEST_LANES
+    max="$#"
+    while [ ! $max -lt 1 ]; do
+        max=$((max-1))
+        TEST_LANES[$max]="$1"
+        shift
+    done
+else
+    TEST_LANES=( 'k8s-1.14.6' 'k8s-1.15.1' 'k8s-1.16.2' )
+fi
+echo "Test lanes: ${TEST_LANES[*]}"
 
 trap '{ make cluster-down; }' EXIT SIGINT SIGTERM
 
 for lane in "${TEST_LANES[@]}"; do
+
+    [ -d "cluster-up/cluster/$lane" ] || ( echo "provider $lane does not exist!"; exit 1 )
 
     echo "test lane: $lane, preparing cluster up"
 
@@ -31,7 +74,7 @@ for lane in "${TEST_LANES[@]}"; do
     for i in $(seq 1 "$NUM_TESTS"); do
         echo "test lane: $lane, run: $i"
         make cluster-sync
-        ginko_params="--ginkgo.noColor --ginkgo.focus=${NEW_TESTS} --ginkgo.regexScansFilePath=true"
+        ginko_params="--ginkgo.noColor --ginkgo.succinct -ginkgo.slowSpecThreshold=30 --ginkgo.focus=${NEW_TESTS} --ginkgo.regexScansFilePath=true"
         FUNC_TEST_ARGS="$ginko_params" make functest
         if [[ $? -ne 0 ]]; then
             echo "test lane: $lane, run: $i, tests failed!"
