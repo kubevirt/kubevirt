268f608bb31d4684abdbcba4321d87fb99dd05a0
Sally O'Malley <somalley@redhat.com>
2020-03-15 14:23:16 -0400

    remove nested-virt steps as they are in ci template now
    Signed-off-by: Sally O'Malley <somalley@redhat.com>

--- hack/ci/test.sh
+++ hack/ci/test.sh
@@ -20,34 +20,7 @@ export GOBIN="/usr/bin"
 source /etc/profile.d/gimme.sh
 echo "checking configuration location"
 echo "KUBECONFIG: ${KUBECONFIG}"
-# enable nested-virt
-oc get machineset -n openshift-machine-api -o json >/tmp/machinesets.json
-MACHINE_IMAGE=$(jq -r .items[0].spec.template.spec.providerSpec.value.disks[0].image /tmp/machinesets.json)
-NESTED_VIRT_IMAGE="rhcos43-nested-virt"
-sed -i 's/'"$MACHINE_IMAGE"'/'"$NESTED_VIRT_IMAGE"'/g'
-oc apply -f /tmp/machinesets.json
-oc scale --replicas=0 machineset --all -n openshift-machine-api
-oc get machines -n openshift-machine-api -o json >/tmp/machines.json
-num_machines=$(jq '.items | length' /tmp/machines.json)
-while [ "$num_machines" -ne "3" ]; do
-    oc get machines -n openshift-machine-api -o json >/tmp/machines.json
-    num_machines=$(jq '.items | length' /tmp/machines.json)
-done
-oc scale --replicas=1 machineset --all -n openshift-machine-api
-while [ "$num_machines" -ne "6" ]; do
-    oc get machines -n openshift-machine-api -o json >/tmp/machines.json
-    num_machines=$(jq '.items | length' /tmp/machines.json)
-done
-while [ $(oc get nodes | wc -l) -ne "7" ]; do oc get nodes; done
-nodes_ready=false
-while ! "$nodes_ready"; do sleep 5 && if ! oc get nodes | grep NotReady; then nodes_ready=true; fi; done
 oc project default
-oc apply -f /go/src/kubevirt.io/kubevirt/kvm-ds.yml
-workers=$(oc get nodes | grep worker | awk '{ print $1 }')
-workers_each=($workers)
-for i in {0..2}; do
-    if ! oc debug node/"${workers_each[i]}" -- ls /dev/kvm; then oc debug node/"${workers_each[i]}" -- ls /dev/kvm; fi
-done
 echo "calling cluster-up to prepare config and check whether cluster is reachable"
 bash -x ./cluster-up/up.sh
 echo "checking cluster configuration after config prep"
