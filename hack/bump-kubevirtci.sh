f2cc28c255e9ee0895ce09bf27f07ed6e6856afd
Petr Horacek <phoracek@redhat.com>
2020-03-21 15:37:50 +0100

    DNM: fetch kubevirtci from phoracek repo
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- hack/bump-kubevirtci.sh
+++ hack/bump-kubevirtci.sh
@@ -5,7 +5,7 @@ set -ex
 source $(dirname "$0")/common.sh
 source $(dirname "$0")/config.sh
 
-val=$(git ls-remote https://github.com/kubevirt/kubevirtci | grep HEAD | awk '{print $1}')
+val=$(git ls-remote https://github.com/phoracek/kubevirtci | grep HEAD | awk '{print $1}')
 sed -i "/^[[:blank:]]*kubevirtci_git_hash[[:blank:]]*=/s/=.*/=\"${val}\"/" hack/config-default.sh
 
 hack/sync-kubevirtci.sh
