46801ccb21225e02032bcd6fe46910d5a1396925
Petr Horacek <phoracek@redhat.com>
2020-03-22 22:10:28 +0100

    bump kubevirtci to the latest version
    This includes fixes in the SR-IOV lane. And should allow us to at least run the
    lane per-request (there seem to be a bug that sometimes breaks the deployment of
    the cluster preventing us from enabling it for every push).
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- cluster-up/cluster/kind-k8s-sriov-1.17.0/provider.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.17.0/provider.sh
@@ -8,6 +8,15 @@ export KIND_NODE_IMAGE="kindest/node:v1.17.0"
 source ${KUBEVIRTCI_PATH}/cluster/kind/common.sh
 
 function up() {
+    if [[ "$KUBEVIRT_NUM_NODES" -ne 2 ]]; then
+        echo 'SR-IOV cluster can be only started with 2 nodes'
+        exit 1
+    fi
+
+    # print hardware info for easier debugging based on logs
+    echo 'Available NICs'
+    docker run --rm --cap-add=SYS_RAWIO quay.io/phoracek/lspci@sha256:0f3cacf7098202ef284308c64e3fc0ba441871a846022bb87d65ff130c79adb1 sh -c "lspci | egrep -i 'network|ethernet'"
+
     cp $KIND_MANIFESTS_DIR/kind.yaml ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
     _fetch_kind
     prepare_workers
