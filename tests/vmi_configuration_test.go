1081c244943eb56f69034e1eaf0debfb04ce0cfb
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-03-26 13:37:42 +0100

    kind: adapt PVC vmi_configuration_test
    PVC volumes are mounted into libvirt w/ the `cache=none` parameter.
    When kubevirt is deployed on  kind infrastructure, libvirt doesn't
    accept that cache value, and instead logs a warning, and coerces
    the value to `writethrough`.
    
    This patch adapts the test when the kind infra is run.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- tests/vmi_configuration_test.go
+++ tests/vmi_configuration_test.go
@@ -1627,7 +1627,13 @@ var _ = Describe("Configurations", func() {
 
 			By("checking if default cache 'none' has been set to pvc disk")
 			Expect(disks[4].Alias.Name).To(Equal("hostpath-pvc"))
-			Expect(disks[4].Driver.Cache).To(Equal(cacheNone))
+			// PVC is mounted as tmpfs on kind, which does not support direct I/O.
+			// As such, it behaves as plugging in a hostDisk - check disks[6].
+			if tests.IsRunningOnKindInfra() {
+				Expect(disks[4].Driver.Cache).To(Equal(cacheWritethrough))
+			} else {
+				Expect(disks[4].Driver.Cache).To(Equal(cacheNone))
+			}
 
 			By("checking if default cache 'none' has been set to block pvc")
 			Expect(disks[5].Alias.Name).To(Equal("block-pvc"))
