3346a7c3e7599bb44d86924bbbe7850bd6f1eeb2
August Simonelli <asimonel@redhat.com>
2020-03-30 22:45:49 +1100

    change TARGET to TARGET_NAMESPACE (#514)
    to match deploy_marketplace.sh usage
    
    change TARGET to TARGET_NAMESPACE
    
    to match deploy_marketplace.sh usage
    
    committed cleanup_maketplace to wrong directory
    
    Signed-off-by: August Simonelli <asimonel@redhat.com>

--- deploy/cleanup_marketplace.sh
+++ deploy/cleanup_marketplace.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 MARKETPLACE="${MARKETPLACE:-openshift-marketplace}"
-TARGET="${TARGET:-kubevirt-hyperconverged}"
+TARGET_NAMESPACE="${TARGET_NAMESPACE:-kubevirt-hyperconverged}"
 APP_REGISTRY="${APP_REGISTRY:-kubevirt-hyperconverged}"
 HCO_VERSION="${HCO_VERSION:-1.0.0}"
 
 oc delete csc hco-catalogsource-config -n $MARKETPLACE
 oc delete catalogsource $APP_REGISTRY -n $MARKETPLACE
 oc delete operatorsource $APP_REGISTRY -n $MARKETPLACE
-oc delete hco kubevirt-hyperconverged -n $TARGET
+oc delete hco kubevirt-hyperconverged -n $TARGET_NAMESPACE
 sleep 10
-oc delete sub hco-operatorhub -n $TARGET
-oc delete csv kubevirt-hyperconverged-operator.v${HCO_VERSION} -n $TARGET
-oc delete operatorgroup $TARGET-group -n $TARGET
+oc delete sub hco-operatorhub -n $TARGET_NAMESPACE
+oc delete csv kubevirt-hyperconverged-operator.v${HCO_VERSION} -n $TARGET_NAMESPACE
+oc delete operatorgroup $TARGET_NAMESPACE-group -n $TARGET_NAMESPACE
 oc delete secret $(oc get secret -n $MARKETPLACE | grep quay-registry | awk '{print $1}') -n $MARKETPLACE
