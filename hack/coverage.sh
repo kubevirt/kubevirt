32841a60490b9b08a35a367adde2e07d7e2caa7f
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-03-31 16:58:12 +0200

    Test each package separately in case of exit code
    In rare cases go test -cover seems to swallow test
    failure output, so in case of exit code we explicitly test
    each package again.
    
    See https://github.com/kubevirt/kubevirt/pull/3206 where this happened
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/coverage.sh
+++ hack/coverage.sh
@@ -5,5 +5,23 @@ set -e
 path=${1:-./pkg/...}
 profile=.coverprofile
 
-go test -cover -v -coverprofile=$profile $(go list ${path})
+packages=$(go list ${path})
+
+# in rare cases go test with coverage seems to swallow test
+# failure output, so we keep the exit code
+set +e
+go test -cover -v -coverprofile=$profile ${packages}
+return_value=$?
+
+# in case of exit code we explicitly test
+# each package again to have the output of all
+# failing tests
+if [[ $return_value -ne 0 ]]; then
+    for package in ${packages}; do
+        go test ${package}
+    done
+    exit $return_value
+fi
+
+set -e
 go tool cover -html=$profile -o coverage.html
