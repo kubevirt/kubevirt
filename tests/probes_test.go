b2947857bcccd30d0f6041f2dc8b91a55967b978
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-04-03 19:38:52 +0200

    ipv6: use a fedora image for the http readiness probe test
    A side effect of using a fedora image is that the HTTP server has
    to be created in a different way - i.e. using the embedded python3
    http server.
    
    As such, it was needed to update the StartHTTPServer function to
    be able to work with cirros & fedora VMIs.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- tests/probes_test.go
+++ tests/probes_test.go
@@ -46,12 +46,20 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				},
 			},
 		}
-		table.DescribeTable("should succeed", func(readinessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int)) {
-			if tests.IsRunningOnKindInfra() && readinessProbe == httpProbe {
-				Skip("Skip http probe test till issue https://github.com/kubevirt/kubevirt/issues/3247 is fixed")
-			}
+		table.DescribeTable("should succeed", func(readinessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int, isFedoraVM bool)) {
+			// nc is used to create the HTTP server. The one shipped in CirrOS has a bug where it throws a
+			// 'connection reset by peer' when bound to IPv6 addresses. On those scenarios, we fallback to using a
+			// Fedora VM.
+			useFedoraVM := tests.IsIPv6Cluster(virtClient) && readinessProbe == httpProbe
+
 			By("Specifying a VMI with a readiness probe")
-			vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(tests.ContainerDiskFor(tests.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
+			var vmi *v12.VirtualMachineInstance
+			if useFedoraVM {
+				vmi = tests.NewRandomFedora32VMIWithFedoraUser()
+			} else {
+				vmi = tests.NewRandomVMIWithEphemeralDiskAndUserdata(tests.ContainerDiskFor(tests.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
+			}
+
 			vmi.Spec.ReadinessProbe = readinessProbe
 			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
 			Expect(err).ToNot(HaveOccurred())
@@ -64,7 +72,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			Expect(vmiReady(vmi)).To(Equal(v1.ConditionFalse))
 
 			By("Starting the server inside the VMI")
-			serverStarter(vmi, 1500)
+			serverStarter(vmi, 1500, useFedoraVM)
 
 			By("Checking that the VMI and the pod will be marked as ready to receive traffic")
 			Eventually(func() v1.ConditionStatus {
@@ -74,7 +82,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			}, 60, 1).Should(Equal(v1.ConditionTrue))
 			Expect(tests.PodReady(tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault))).To(Equal(v1.ConditionTrue))
 		},
-			table.Entry("[test_id:1202][posneg:positive]with working TCP probe and tcp server", tcpProbe, tests.StartTCPServer),
+			table.Entry("[test_id:1202][posneg:positive]with working TCP probe and tcp server", tcpProbe, startTCPServer),
 			table.Entry("[test_id:1200][posneg:positive]with working HTTP probe and http server", httpProbe, tests.StartHTTPServer),
 		)
 
@@ -128,7 +136,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				},
 			},
 		}
-		table.DescribeTable("should not fail the VMI", func(livenessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int)) {
+		table.DescribeTable("should not fail the VMI", func(livenessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int, isFedoraVM bool)) {
 			By("Specifying a VMI with a readiness probe")
 			vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(tests.ContainerDiskFor(tests.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
 			vmi.Spec.LivenessProbe = livenessProbe
@@ -138,7 +146,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			tests.WaitForSuccessfulVMIStartIgnoreWarnings(vmi)
 
 			By("Starting the server inside the VMI")
-			serverStarter(vmi, 1500)
+			serverStarter(vmi, 1500, false)
 
 			By("Checking that the VMI is still running after a minute")
 			Consistently(func() bool {
@@ -147,7 +155,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				return vmi.IsFinal()
 			}, 120, 1).Should(Not(BeTrue()))
 		},
-			table.Entry("[test_id:1199][posneg:positive]with working TCP probe and tcp server", tcpProbe, tests.StartTCPServer),
+			table.Entry("[test_id:1199][posneg:positive]with working TCP probe and tcp server", tcpProbe, startTCPServer),
 			table.Entry("[test_id:1201][posneg:positive]with working HTTP probe and http server", httpProbe, tests.StartHTTPServer),
 		)
 
@@ -180,4 +188,8 @@ func vmiReady(vmi *v12.VirtualMachineInstance) v1.ConditionStatus {
 		}
 	}
 	return v1.ConditionFalse
+}
+
+func startTCPServer(vmi *v12.VirtualMachineInstance, port int, useFedoraImg bool) {
+	tests.StartTCPServer(vmi, port)
 }
--- tests/utils.go
+++ tests/utils.go
@@ -2049,6 +2049,14 @@ func AddEphemeralCdrom(vmi *v1.VirtualMachineInstance, name string, bus string, 
 	return vmi
 }
 
+func NewRandomFedora32VMIWithFedoraUser() *v1.VirtualMachineInstance {
+	vmi := NewRandomVMIWithEphemeralDiskAndUserdata(ContainerDiskFor(ContainerDiskFedora), `#!/bin/bash
+	    echo "fedora" |passwd fedora --stdin
+        echo `)
+	vmi.Spec.Domain.Resources.Requests[k8sv1.ResourceMemory] = resource.MustParse("512M")
+	return vmi
+}
+
 func NewRandomFedoraVMIWitGuestAgent() *v1.VirtualMachineInstance {
 	agentVMI := NewRandomVMIWithEphemeralDiskAndUserdata(ContainerDiskFor(ContainerDiskFedora), GetGuestAgentUserData())
 	agentVMI.Spec.Domain.Resources.Requests[k8sv1.ResourceMemory] = resource.MustParse("512M")
@@ -4121,16 +4129,30 @@ func StartTCPServer(vmi *v1.VirtualMachineInstance, port int) {
 	Expect(err).ToNot(HaveOccurred())
 }
 
-func StartHTTPServer(vmi *v1.VirtualMachineInstance, port int) {
-	expecter, err := LoggedInCirrosExpecter(vmi)
-	Expect(err).ToNot(HaveOccurred())
+func StartHTTPServer(vmi *v1.VirtualMachineInstance, port int, isFedoraVM bool) {
+	var err error
+	var expecter expect.Expecter
+	var httpServerMaker string
+	var prompt string
+
+	if isFedoraVM {
+		expecter, err = LoggedInFedoraExpecter(vmi)
+		Expect(err).NotTo(HaveOccurred())
+		httpServerMaker = fmt.Sprintf("python3 -m http.server %d --bind ::0 &\n", port)
+		prompt = "#"
+	} else {
+		expecter, err = LoggedInCirrosExpecter(vmi)
+		Expect(err).NotTo(HaveOccurred())
+		httpServerMaker = fmt.Sprintf("screen -d -m nc -klp %d -e echo -e \"HTTP/1.1 200 OK\\n\\nHello World!\"\n", port)
+		prompt = "\\$"
+	}
 	defer expecter.Close()
 
 	resp, err := expecter.ExpectBatch([]expect.Batcher{
 		&expect.BSnd{S: "\n"},
-		&expect.BExp{R: "\\$ "},
-		&expect.BSnd{S: fmt.Sprintf("screen -d -m nc -klp %d -e echo -e \"HTTP/1.1 200 OK\\n\\nHello World!\"\n", port)},
-		&expect.BExp{R: "\\$ "},
+		&expect.BExp{R: prompt},
+		&expect.BSnd{S: httpServerMaker},
+		&expect.BExp{R: prompt},
 		&expect.BSnd{S: "echo $?\n"},
 		&expect.BExp{R: "0"},
 	}, 60*time.Second)
@@ -4702,4 +4724,9 @@ func ClearKubeVirtConfigMap(key string) error {
 
 func RandTmpDir() string {
 	return tmpPath + "/" + rand.String(10)
+}
+
+func IsIPv6Cluster(virtClient kubecli.KubevirtClient) bool {
+	clusterDnsIP, _ := getClusterDnsServiceIP(virtClient)
+	return netutils.IsIPv6String(clusterDnsIP)
 }
