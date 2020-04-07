f94b46c26293b978c87da61370a7247888ef604a
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-04-07 15:27:07 +0200

    Create tap device on virt-handler
    In order to make virt-handler create the tap device on behalf of
    virt-launcher, a set of changes were required:
      - updated the VIF struct to feature the name of the created tap
        device. This way, pod networking configuration phase #2 can
        instruct libvirt that it should use a pre-existent tap device
        (via 'target' parameter). This will happen in a follow-up
        commit.
      - added a method to the `NetworkHandler` interface that
        creates a tap device. This method receives the tap interface
        name as parameter (to handle multus based scenarios, where one
        VM is plugged multiple taps).
      - create a tap device from the `Bridge` and `Masquerade`
        `BindMechanism`s, using the aforementioned `NetworkHandler`
        method. The name of the created devices will be cached in the
        VIF structure. This tap device will be configured a follow-up
        commit.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- pkg/virt-launcher/virtwrap/network/generated_mock_common.go
+++ pkg/virt-launcher/virtwrap/network/generated_mock_common.go
@@ -303,3 +303,13 @@ func (_m *MockNetworkHandler) GetNFTIPString(proto iptables.Protocol) string {
 func (_mr *_MockNetworkHandlerRecorder) GetNFTIPString(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "GetNFTIPString", arg0)
 }
+
+func (_m *MockNetworkHandler) CreateTapDevice(tapName string) error {
+	ret := _m.ctrl.Call(_m, "CreateTapDevice", tapName)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+func (_mr *_MockNetworkHandlerRecorder) CreateTapDevice(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "CreateTapDevice", arg0)
+}
--- pkg/virt-launcher/virtwrap/network/podinterface_test.go
+++ pkg/virt-launcher/virtwrap/network/podinterface_test.go
@@ -72,6 +72,8 @@ var _ = Describe("Pod Network", func() {
 	var masqueradeIpv6VmAddr *netlink.Addr
 	var masqueradeVmIpv6 string
 	var pid int
+	var tapDeviceName string
+
 	log.Log.SetIOWriter(GinkgoWriter)
 
 	BeforeEach(func() {
@@ -94,6 +96,7 @@ var _ = Describe("Pod Network", func() {
 		routeAddr = netlink.Route{Gw: gw}
 		routeList = []netlink.Route{routeAddr}
 		pid = os.Getpid()
+		tapDeviceName = "tap0"
 
 		// Create a bridge
 		bridgeTest = &netlink.Bridge{
@@ -178,6 +181,7 @@ var _ = Describe("Pod Network", func() {
 		mockNetwork.EXPECT().LinkSetMaster(dummy, bridgeTest).Return(nil)
 		mockNetwork.EXPECT().AddrAdd(bridgeTest, bridgeAddr).Return(nil)
 		mockNetwork.EXPECT().StartDHCP(testNic, bridgeAddr, api.DefaultBridgeName, nil)
+		mockNetwork.EXPECT().CreateTapDevice(tapDeviceName).Return(nil)
 
 		// For masquerade tests
 		mockNetwork.EXPECT().LinkByName(podInterface).Return(dummy, nil)
@@ -196,6 +200,7 @@ var _ = Describe("Pod Network", func() {
 		mockNetwork.EXPECT().StartDHCP(masqueradeTestNic, masqueradeGwAddr, api.DefaultBridgeName, nil)
 		mockNetwork.EXPECT().GetHostAndGwAddressesFromCIDR(api.DefaultVMCIDR).Return(masqueradeGwStr, masqueradeVmStr, nil)
 		mockNetwork.EXPECT().GetHostAndGwAddressesFromCIDR(api.DefaultVMIpv6CIDR).Return(masqueradeIpv6GwStr, masqueradeIpv6VmStr, nil)
+		mockNetwork.EXPECT().CreateTapDevice(tapDeviceName).Return(nil)
 		// Global nat rules using iptables
 		mockNetwork.EXPECT().ConfigureIpv6Forwarding().Return(nil)
 		mockNetwork.EXPECT().GetNFTIPString(iptables.ProtocolIPv4).Return("ip").AnyTimes()
@@ -294,6 +299,7 @@ var _ = Describe("Pod Network", func() {
 			mockNetwork.EXPECT().GetMacDetails(podInterface).Return(fakeMac, nil)
 			mockNetwork.EXPECT().LinkSetMaster(dummy, bridgeTest).Return(nil)
 			mockNetwork.EXPECT().AddrDel(dummy, &fakeAddr).Return(errors.New("device is busy"))
+			mockNetwork.EXPECT().CreateTapDevice(tapDeviceName).Return(nil)
 
 			err := SetupPodNetworkPhase1(vm, pid)
 			Expect(err).To(HaveOccurred(), "SetupPodNetworkPhase1 should return an error")
