d41631282536b5f63d280940b11edb227330d587
Omer Yahud <oyahud@redhat.com>
2020-04-07 16:41:25 +0300

    Removed test focus
    Signed-off-by: Omer Yahud <oyahud@redhat.com>

--- tests/template_test.go
+++ tests/template_test.go
@@ -258,7 +258,7 @@ var _ = Describe("Templates", func() {
 
 		AfterEach(AssertTestCleanupSuccess())
 
-		FContext("with Fedora Template", func() {
+		Context("with Fedora Template", func() {
 			BeforeEach(AssertTemplateSetupSuccess(vmsgen.GetTemplateFedoraWithContainerDisk(tests.ContainerDiskFor(tests.ContainerDiskFedora)), nil))
 
 			AssertTemplateTestSuccess()
