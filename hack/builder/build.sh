80f032b56fcac0e19d1e0b916f891fa20d74f3e1
Howard Zhang <howard.zhang@arm.com>
2020-04-14 10:51:29 +0800

    make bazel-build bazel-image push works on Arm64
    Signed-off-by: Howard Zhang <howard.zhang@arm.com>

--- hack/builder/build.sh
+++ hack/builder/build.sh
@@ -11,6 +11,7 @@ trap 'cleanup' EXIT
 cleanup() {
     docker rm -f dummy-qemu-user-static >/dev/null || true
     rm "${SCRIPT_DIR}/qemu-ppc64le-static" || true
+    rm "${SCRIPT_DIR}/qemu-aarch64-static" || true
 }
 
 # shellcheck source=hack/builder/version.sh
@@ -21,8 +22,23 @@ cleanup
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker create -ti --name dummy-qemu-user-static multiarch/qemu-user-static
 docker cp dummy-qemu-user-static:/usr/bin/qemu-ppc64le-static "${SCRIPT_DIR}/qemu-ppc64le-static"
+docker cp dummy-qemu-user-static:/usr/bin/qemu-aarch64-static "${SCRIPT_DIR}/qemu-aarch64-static"
 
 for ARCH in ${ARCHITECTURES}; do
-    docker build -t "quay.io/kubevirt/builder:${VERSION}-${ARCH}" --build-arg ARCH="${ARCH}" -f "${SCRIPT_DIR}/Dockerfile" "${SCRIPT_DIR}"
+    case ${ARCH} in
+    amd64)
+        sonobuoy_arch="amd64"
+        bazel_arch="x86_64"
+        ;;
+    arm64v8)
+        sonobuoy_arch="arm64"
+        bazel_arch="arm64"
+        ;;
+    *)
+        sonobuoy_arch=${ARCH}
+        bazel_arch=${ARCH}
+        ;;
+    esac
+    docker build -t "quay.io/kubevirt/builder:${VERSION}-${ARCH}" --build-arg ARCH="${ARCH}" --build-arg SONOBUOY_ARCH=${sonobuoy_arch} --build-arg BAZEL_ARCH=${bazel_arch} -f "${SCRIPT_DIR}/Dockerfile" "${SCRIPT_DIR}"
     TMP_IMAGES="${TMP_IMAGES} quay.io/kubevirt/builder:${VERSION}-${ARCH}"
 done
