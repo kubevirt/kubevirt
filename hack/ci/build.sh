eefbbff23fb4bcc950c7074d1663d0d539e096dd
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-04-24 15:37:11 +0200

    Fix file access in docker build
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/ci/build.sh
+++ hack/ci/build.sh
@@ -16,6 +16,11 @@ bash -x ./hack/build-manifests.sh
 # build dump
 CMD_OUT_DIR="$(pwd)/_out/cmd"
 export CMD_OUT_DIR
-mkdir -p _out/cmd/dump/
+mkdir -p "$CMD_OUT_DIR/dump/"
 GOPROXY=off GOFLAGS=-mod=vendor go build -o "$CMD_OUT_DIR/dump/dump" ./cmd/dump
 bash -x ./hack/build-func-tests.sh
+
+rm -rf _ci-configs/
+
+# to avoid any permission problems we reset access rights recursively
+chmod -R 777 .
