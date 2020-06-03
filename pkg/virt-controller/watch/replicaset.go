fa97b92efef6327208ca521de6fc1e74a550c3c6
haemyung_yang <haemyung_yang@tmax.co.kr>
2020-06-03 15:54:25 +0900

    Fix vmirs watcher, Controller can filter abnormal vmis related abnormal pods
    Signed-off-by: haemyung_yang <haemyung_yang@tmax.co.kr>

--- pkg/virt-controller/watch/replicaset.go
+++ pkg/virt-controller/watch/replicaset.go
@@ -351,7 +351,7 @@ func (c *VMIReplicaSet) scale(rs *virtv1.VirtualMachineInstanceReplicaSet, vmis 
 // filterActiveVMIs takes a list of VMIs and returns all VMIs which are not in a final state and not terminating
 func (c *VMIReplicaSet) filterActiveVMIs(vmis []*virtv1.VirtualMachineInstance) []*virtv1.VirtualMachineInstance {
 	return filter(vmis, func(vmi *virtv1.VirtualMachineInstance) bool {
-		return !vmi.IsFinal() && vmi.DeletionTimestamp == nil
+		return !vmi.IsFinal() && vmi.DeletionTimestamp == nil && !controller.NewVirtualMachineInstanceConditionManager().HasConditionWithStatus(vmi, virtv1.VirtualMachineInstanceConditionType(k8score.PodReady), k8score.ConditionFalse)
 	})
 }
 
