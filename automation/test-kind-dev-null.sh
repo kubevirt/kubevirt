4ddbc8d3ad4bf5d8773b5104303d1715b8be0f2b
Quique Llorente <ellorent@redhat.com>
2020-07-01 11:24:42 +0200

    DNM: This runs dummy pod to check kind provider
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

---
+++ automation/test-kind-dev-null.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+export KUBEVIRT_PROVIDER=kind-k8s-1.17.0-ipv6
+make cluster-up
+cat <<'EOF' > pod.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: virt-launcher-dev-null
+spec:
+  containers:
+  - name: virt-launcher-dev-null
+    image: kubevirt/virt-launcher:v0.30.2
+    command: ["/bin/bash"]
+    args: ["-c", "sleep infinity"]
+EOF
+./cluster-up/kubectl.sh apply -f pod.yaml
+./cluster-up/kubectl.sh wait pod virt-launcher-dev-null --for condition=Ready --timeout=200s
+./cluster-up/kubectl.sh exec virt-launcher-dev-null -- bash -xe -c "ls -la /dev/null && echo foo | tee /dev/null"
