caaa3a38a0e43ed11345b861c11ff6e6967150eb
Roman Mohr <rmohr@redhat.com>
2020-07-21 11:22:29 +0200

    Run hack/bump-kubevirtci.sh, updating to b6c51c50...
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-up/cluster/openshift-provider-common.sh
+++
@@ -1,6 +0,0 @@
-ln_kubeconfig () {
-    kube_dir=/root/.kube
-    kubeconfig=/root/install/auth/kubeconfig
-    ${KUBEVIRTCI_PATH}/container.sh mkdir -p $kube_dir
-    ${KUBEVIRTCI_PATH}/container.sh ln -s $kubeconfig $kube_dir
-}
