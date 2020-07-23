daab633f95c64f4a63bbf7f8660d2bb12c167872
Ashley Schuett <ashleyns1992@gmail.com>
2020-07-23 12:02:14 +0200

    update config message to specify which resource type it is using
    Signed-off-by: Ashley Schuett <ashleyns1992@gmail.com>

--- pkg/virt-config/config-map.go
+++ pkg/virt-config/config-map.go
@@ -467,7 +467,7 @@ func (c *ClusterConfig) GetConfig() (config *v1.KubeVirtConfiguration) {
 		return c.lastValidConfig
 	}
 
-	log.DefaultLogger().Infof("Updating cluster config to resource version '%s'", resourceVersion)
+	log.DefaultLogger().Infof("Updating cluster config from %s to resource version '%s'", resourceType, resourceVersion)
 	c.lastValidConfigResourceVersion = resourceVersion
 	c.lastValidConfig = config
 	return c.lastValidConfig
