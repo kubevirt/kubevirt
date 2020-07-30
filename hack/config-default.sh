96376629e3436a81970bade7429750a3fbc66ac0
Or Shoval <oshoval@redhat.com>
2020-07-30 13:33:26 +0300

    Bump kubevirtci
    Mostly Calico bump to 3.15.1 and
    cleaning ocp/okd providers.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -11,7 +11,7 @@ cdi_namespace=cdi
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
 verbosity=${VERBOSITY:-2}
 package_name=${PACKAGE_NAME:-kubevirt-dev}
-kubevirtci_git_hash="b6c51c50c65a403d14bd83c47f1911f3090d0a88"
+kubevirtci_git_hash="25a60ac1375cfbec2ef12bf5e62991737785b894"
 
 # try to derive csv_version from docker tag. But it must start with x.y.z, without leading v
 default_csv_version="${docker_tag/latest/0.0.0}"
