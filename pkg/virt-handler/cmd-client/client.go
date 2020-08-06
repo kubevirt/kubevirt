65049625c3aa607d118d4f456cb4d56ac6fd00b7
rmohr <rmohr@redhat.com>
2020-08-06 17:26:25 +0200

    [virt-handler] let virt-handler probe for virt-launcher readiness
    virt-handler repeatedly probes for the command socket to appear until it
    runs into a timeout. If virt-handler runs in a timeout it does the usual
    cleanup.
    
    virt-handler has to deal with three scenarios:
    
     * watchdog files and the socket on a shared location
     * readiness-file and the socket inside the virt-launcher pod
     * the socket indicating the readiness itself
    
    The first two cases are covered on virt-handler synchronization by
    simply checking if a pod directory exists for the VMI, in case that no
    socket could be found. In this case virt-handler immediately starts the
    cleanup. Readiness checks are in this case not necessary, since the VMI
    must have been ready already. Otherwise virt-handler would in this case
    not see the VMI.
    
    The third case virt-handler repeatedly checks for up to three minuts if
    finally the command socket appears. If during these checks the pod
    directory disappears, it aborts early since the pod is obviously gone
    then already.
    
    Signed-off-by: rmohr <rmohr@redhat.com>

--- pkg/virt-handler/cmd-client/client.go
+++ pkg/virt-handler/cmd-client/client.go
@@ -65,6 +65,7 @@ var (
 )
 
 const StandardLauncherSocketFileName = "launcher-sock"
+const StandardInitLauncherSocketFileName = "launcher-init-sock"
 const StandardLauncherUnresponsiveFileName = "launcher-unresponsive"
 
 type MigrationOptions struct {
@@ -222,6 +223,25 @@ func SocketFilePathOnHost(podUID string) string {
 }
 
 // gets the cmd socket for a VMI
+func FindPodDirOnHost(vmi *v1.VirtualMachineInstance) (string, error) {
+
+	// It is possible for multiple pods to be active on a single VMI
+	// during migrations. This loop will discover the active pod on
+	// this particular local node if it exists. A active pod not
+	// running on this node will not have a kubelet pods directory,
+	// so it will not be found.
+	for podUID, _ := range vmi.Status.ActivePods {
+		socketPodDir := SocketDirectoryOnHost(string(podUID))
+		exists, _ := diskutils.FileExists(socketPodDir)
+		if exists {
+			return socketPodDir, nil
+		}
+	}
+
+	return "", fmt.Errorf("No command socketdir for vmi %s", vmi.UID)
+}
+
+// gets the cmd socket for a VMI
 func FindSocketOnHost(vmi *v1.VirtualMachineInstance) (string, error) {
 	if string(vmi.UID) != "" {
 		legacySockFile := string(vmi.UID) + "_sock"
@@ -246,11 +266,15 @@ func FindSocketOnHost(vmi *v1.VirtualMachineInstance) (string, error) {
 	}
 
 	return "", fmt.Errorf("No command socket found for vmi %s", vmi.UID)
-
 }
 
 func SocketOnGuest() string {
 	sockFile := StandardLauncherSocketFileName
+	return filepath.Join(LegacySocketsDirectory(), sockFile)
+}
+
+func UninitializedSocketOnGuest() string {
+	sockFile := StandardInitLauncherSocketFileName
 	return filepath.Join(LegacySocketsDirectory(), sockFile)
 }
 
