4c6c4166737e6cc77b3dcaf4c9c5a91b27079744
L. Pivarc <lpivarc@redhat.com>
2020-08-11 11:13:14 +0200

    Fix clock timezone
    We didn't set timezone attr. in libvirt xml when clock.timezone was specified.
    This causes the VM to not start.
    
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- pkg/virt-launcher/virtwrap/api/converter_test.go
+++ pkg/virt-launcher/virtwrap/api/converter_test.go
@@ -41,6 +41,26 @@ var _ = Describe("Converter", func() {
 
 	TestSmbios := &cmdv1.SMBios{}
 
+	Context("with timezone", func() {
+		It("Should set timezone attribute", func() {
+			timezone := v1.ClockOffsetTimezone("America/New_York")
+			clock := &v1.Clock{
+				ClockOffset: v1.ClockOffset{
+					Timezone: &timezone,
+				},
+				Timer: &v1.Timer{},
+			}
+
+			var convertClock Clock
+			Convert_v1_Clock_To_api_Clock(clock, &convertClock, &ConverterContext{})
+			data, err := xml.MarshalIndent(convertClock, "", "  ")
+			Expect(err).ToNot(HaveOccurred())
+
+			expectedClock := `<Clock offset="timezone" timezone="America/New_York"></Clock>`
+			Expect(string(data)).To(Equal(expectedClock))
+		})
+	})
+
 	Context("with v1.Disk", func() {
 		It("Should add boot order when provided", func() {
 			order := uint(1)
