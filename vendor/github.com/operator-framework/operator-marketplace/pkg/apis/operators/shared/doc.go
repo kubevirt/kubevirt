da27a3e8eaf3aadae08dc39f8bda9f069d3a7492
Bartosz Rybacki <brybacki@redhat.com>
2020-08-11 16:54:07 +0200

    Run make deps-update
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

---
+++ vendor/github.com/operator-framework/operator-marketplace/pkg/apis/operators/shared/doc.go
@@ -0,0 +1,4 @@
+// Package shared contains structs used by various API groups
+// +k8s:deepcopy-gen=package,register
+// +groupName=operators.coreos.com
+package shared
--- vendor/kubevirt.io/containerized-data-importer/pkg/apis/upload/v1alpha1/types.go
+++ vendor/kubevirt.io/containerized-data-importer/pkg/apis/upload/v1alpha1/types.go
@@ -16,10 +16,6 @@ limitations under the License.
 
 package v1alpha1
 
-//go:generate swagger-doc
-//go:generate deepcopy-gen -i . --go-header-file ../../../../hack/custom-boilerplate.go.txt
-//go:generate openapi-gen -i . --output-package=kubevirt.io/containerized-data-importer/pkg/apis/upload/v1alpha1  --go-header-file ../../../../hack/custom-boilerplate.go.txt
-
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
