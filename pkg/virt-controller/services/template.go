a2c7aa1d106235a44b9e8695c0f11cc22b7e94db
rmohr <rmohr@redhat.com>
2020-08-12 13:21:07 +0200

    Mitigate OOM issues on some kernels
    On some kernel/CRI combinations exec probes lead to higher memory
    consumption in the pods due to the dirty pages cache being counted
    against the container limit. In that case it can happen that before the
    dirty page cache is cleaned, the pod is killed due to OOM.
    
    Some hints indicate that the limit on most systems is roughly 36M for
    the cache. Our limit of 40M is just low enough so that somethimes our
    containerDisk or infra container can get killed.
    
    Bumping it to 80M. Latest kubevirt just dropped the usage of exec probes
    to resolve this, but this old release can't do that to not break update
    guarantees.
    
    Signed-off-by: rmohr <rmohr@redhat.com>

--- pkg/virt-controller/services/template.go
+++ pkg/virt-controller/services/template.go
@@ -940,7 +940,7 @@ func (t *templateService) RenderLaunchManifest(vmi *v1.VirtualMachineInstance) (
 			Resources: k8sv1.ResourceRequirements{
 				Limits: map[k8sv1.ResourceName]resource.Quantity{
 					k8sv1.ResourceCPU:    resource.MustParse("1m"),
-					k8sv1.ResourceMemory: resource.MustParse("40Mi"),
+					k8sv1.ResourceMemory: resource.MustParse("80M"),
 				},
 			},
 			Command:        []string{"/usr/bin/tail", "-f", "/dev/null"},
