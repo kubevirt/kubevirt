72fd0c07b14d511572f54aaae0c640b4765e0f63
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2020-08-17 20:41:58 +0300

    Preparations for index-image in CI (#742)
    * Add script to automatically build index images
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>
    
    * build-manifests
    
    Signed-off-by: Nahshon Unna Tsameret (Rebase PR Action) <nunnatsa@users.noreply.github.com>
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>
    
    Co-authored-by: Nahshon Unna Tsameret (Rebase PR Action) <nunnatsa@users.noreply.github.com>

---
+++ hack/build-index-image.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+set -ex
+
+##################################################################
+# build-index-image build index-image(s) for HCO
+# there are three options:
+# * run with ALL parameter will generate index images for all the
+#   versions
+# * run with LATEST parameter will generate index image for the
+#   latest version
+# * run with a specific version name; e.g. 1.1.0, will generate
+#   index image for this version
+##################################################################
+
+PROJECT_ROOT="$(readlink -e "$(dirname "${BASH_SOURCE[0]}")"/../)"
+DEPLOY_DIR="${PROJECT_ROOT}/deploy/olm-catalog"
+INDEX_IMAGE_PARAM=
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-quay.io}
+REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE:-kubevirt}
+BUNDLE_REGISTRY_IMAGE_NAME=${BUNDLE_REGISTRY_IMAGE_NAME:-hyperconverged-cluster-bundle}
+INDEX_REGISTRY_IMAGE_NAME=${INDEX_REGISTRY_IMAGE_NAME:-hyperconverged-cluster-index}
+
+function create_index_image() {
+  CURRENT_VERSION=$1
+  PREV_VERSION=$2
+  BUNDLE_IMAGE_NAME="${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/${BUNDLE_REGISTRY_IMAGE_NAME}:${CURRENT_VERSION}"
+  INDEX_IMAGE_NAME="${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/${INDEX_REGISTRY_IMAGE_NAME}:${CURRENT_VERSION}"
+
+  INDEX_IMAGE_PARAM=
+  if [[ -n ${2} ]]; then
+    PREV_INDEX_IMAGE="${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/${INDEX_REGISTRY_IMAGE_NAME}:${PREV_VERSION}"
+    INDEX_IMAGE_PARAM=--from-index="${PREV_INDEX_IMAGE}"
+  fi
+
+  docker build -t "${BUNDLE_IMAGE_NAME}" -f bundle.Dockerfile --build-arg "VERSION=${CURRENT_VERSION}" .
+  docker push "${BUNDLE_IMAGE_NAME}"
+  # shellcheck disable=SC2086
+  opm index add --bundles "${BUNDLE_IMAGE_NAME}" ${INDEX_IMAGE_PARAM} --tag "${INDEX_IMAGE_NAME}" -u docker
+  docker push "${INDEX_IMAGE_NAME}"
+}
+
+function create_all_versions() {
+  PREV_VERSION=
+  for version in $(ls -d kubevirt-hyperconverged/*/ | sort -V | cut -d '/' -f 2); do
+    create_index_image "${version}" "${PREV_VERSION}"
+    PREV_VERSION=${version}
+  done
+}
+
+function create_latest_version() {
+  CURRENT_VERSION=
+  PREV_VERSION=
+  for version in $(ls -d kubevirt-hyperconverged/*/ | sort -V | cut -d '/' -f 2); do
+    PREV_VERSION=${CURRENT_VERSION}
+    CURRENT_VERSION=${version}
+  done
+  create_index_image "${CURRENT_VERSION}" "${PREV_VERSION}"
+}
+
+function build_specific_version() {
+  CURRENT_VERSION=
+  PREV_VERSION=
+  for version in $(ls -d kubevirt-hyperconverged/*/ | sort -V | cut -d '/' -f 2); do
+    PREV_VERSION=${CURRENT_VERSION}
+    CURRENT_VERSION=${version}
+    if [[ "$1" == "${CURRENT_VERSION}" ]]; then
+      break
+    fi
+  done
+  create_index_image "${CURRENT_VERSION}" "${PREV_VERSION}"
+}
+
+function help() {
+  echo "usage $0 {ALL|LATEST|<version>}"
+}
+
+if [[ $# -ne 1 ]]; then
+  help
+  exit 1
+fi
+
+cd "${DEPLOY_DIR}"
+
+case "$1" in
+  [Aa][Ll][Ll])
+    create_all_versions
+    ;;
+
+  [Ll][Aa][Tt][Ee][Ss][Tt])
+    create_latest_version
+    ;;
+
+  [Hh][Ee][Ll][Pp])
+    help
+    ;;
+
+  *)
+    build_specific_version $1
+    ;;
+
+esac
--- hack/build-manifests.sh
+++ hack/build-manifests.sh
@@ -222,8 +222,18 @@ csplit --digits=2 --quiet --elide-empty-files \
 
 popd
 
-mkdir -p "${CSV_DIR}"
-rm -f ${CSV_DIR}/*
+rm -fr "${CSV_DIR}"
+mkdir -p "${CSV_DIR}/metadata"
+
+cat << EOF > "${CSV_DIR}/metadata/annotations.yaml"
+annotations:
+  operators.operatorframework.io.bundle.channel.default.v1: ${CSV_VERSION}
+  operators.operatorframework.io.bundle.channels.v1: ${CSV_VERSION}
+  operators.operatorframework.io.bundle.manifests.v1: manifests/
+  operators.operatorframework.io.bundle.mediatype.v1: registry+v1
+  operators.operatorframework.io.bundle.metadata.v1: metadata/
+  operators.operatorframework.io.bundle.package.v1: kubevirt-hyperconverged-operator
+EOF
 
 SMBIOS=$(cat <<- EOM
 Family: KubeVirt
