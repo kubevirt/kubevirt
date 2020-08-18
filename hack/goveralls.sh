89d0b41489c76c4ba89e8a20406b6335303569d2
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-08-18 17:59:29 +0200

    Move coverage from travis to prow
    Also just call `bazel-build-verify` make target only as that does
    everything we should need.
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -3,4 +3,4 @@
 set -e
 
 ./hack/coverage.sh
-goveralls -service=travis-ci -coverprofile=.coverprofile -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
+goveralls -service=prow -coverprofile=.coverprofile -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
