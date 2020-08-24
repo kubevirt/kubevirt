1812ebdacec40f5c22e779bf85e4f8fd31ff4a6a
rmohr <rmohr@redhat.com>
2020-08-24 17:59:13 +0200

    Allow console tests to run in parallel
    Signed-off-by: rmohr <rmohr@redhat.com>

--- tests/console_test.go
+++ tests/console_test.go
@@ -36,7 +36,7 @@ import (
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
 )
 
-var _ = Describe("[Serial][rfe_id:127][posneg:negative][crit:medium][vendor:cnv-qe@redhat.com][level:component]Console", func() {
+var _ = Describe("[rfe_id:127][posneg:negative][crit:medium][vendor:cnv-qe@redhat.com][level:component]Console", func() {
 
 	var virtClient kubecli.KubevirtClient
 
@@ -102,7 +102,7 @@ var _ = Describe("[Serial][rfe_id:127][posneg:negative][crit:medium][vendor:cnv-
 				})
 			})
 
-			Context("with a fedora image", func() {
+			Context("[Serial]with a fedora image", func() {
 				It("[test_id:1589]should return that we are running fedora", func() {
 					vmi := tests.NewRandomVMIWithEphemeralDiskHighMemory(cd.ContainerDiskFor(cd.ContainerDiskFedora))
 					RunVMIAndWaitForStart(vmi)
