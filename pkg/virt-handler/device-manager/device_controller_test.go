e56a4f3a262b74a032373fbc860bdb93ee7dd909
Jed Lejosne <jed@redhat.com>
2020-08-24 14:07:26 -0400

    Fix device controller static tests to match the new API
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-handler/device-manager/device_controller_test.go
+++ pkg/virt-handler/device-manager/device_controller_test.go
@@ -10,6 +10,11 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+
+	"kubevirt.io/kubevirt/pkg/testutils"
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 )
 
 type FakePlugin struct {
@@ -48,8 +53,12 @@ var _ = Describe("Device Controller", func() {
 	var err error
 	var host string
 	var stop chan struct{}
+	var fakeConfigMap *virtconfig.ClusterConfig
+	var fakeInformer cache.SharedIndexInformer
 
 	BeforeEach(func() {
+		fakeConfigMap, _, _, _ = testutils.NewFakeClusterConfig(&k8sv1.ConfigMap{})
+		fakeInformer, _ = testutils.NewFakeInformerFor(&k8sv1.ConfigMap{})
 		workDir, err = ioutil.TempDir("", "kubevirt-test")
 		Expect(err).ToNot(HaveOccurred())
 
@@ -58,13 +67,12 @@ var _ = Describe("Device Controller", func() {
 	})
 
 	AfterEach(func() {
-		close(stop)
 		os.RemoveAll(workDir)
 	})
 
 	Context("Basic Tests", func() {
 		It("Should indicate if node has device", func() {
-			deviceController := NewDeviceController(host, 10)
+			deviceController := NewDeviceController(host, 10, fakeConfigMap, fakeInformer)
 			devicePath := path.Join(workDir, "fake-device")
 			res := deviceController.nodeHasDevice(devicePath)
 			Expect(res).To(BeFalse())
@@ -79,6 +87,8 @@ var _ = Describe("Device Controller", func() {
 	})
 
 	Context("Multiple Plugins", func() {
+		var deviceName1 string
+		var deviceName2 string
 		var devicePath1 string
 		var devicePath2 string
 
@@ -86,20 +96,25 @@ var _ = Describe("Device Controller", func() {
 		var plugin2 *FakePlugin
 
 		BeforeEach(func() {
+			deviceName1 = "fake-device1"
+			deviceName2 = "fake-device2"
+			devicePath1 = path.Join(workDir, deviceName1)
+			devicePath2 = path.Join(workDir, deviceName2)
 			// only create the second device.
-			devicePath1 = path.Join(workDir, "fake-device1")
-			devicePath2 = path.Join(workDir, "fake-device2")
 			os.Create(devicePath2)
 
-			plugin1 = NewFakePlugin("fake-device1", devicePath1)
-			plugin2 = NewFakePlugin("fake-device2", devicePath2)
+			plugin1 = NewFakePlugin(deviceName1, devicePath1)
+			plugin2 = NewFakePlugin(deviceName2, devicePath2)
 		})
 
 		It("should restart the device plugin immeidiately without delays", func() {
 			plugin2 = NewFakePlugin("fake-device2", devicePath2)
-			deviceController := NewDeviceController(host, 10)
-			deviceController.devicePlugins = []GenericDevice{plugin2}
+			deviceController := NewDeviceController(host, 10, fakeConfigMap, fakeInformer)
 			deviceController.backoff = []time.Duration{10 * time.Millisecond, 10 * time.Second}
+			deviceController.devicePlugins[deviceName2] = ControlledDevice{
+				devicePlugin: plugin2,
+				stopChan:     stop,
+			}
 			go deviceController.Run(stop)
 			Eventually(func() int {
 				return int(atomic.LoadInt32(&plugin2.Starts))
@@ -110,9 +125,12 @@ var _ = Describe("Device Controller", func() {
 		It("should restart the device plugin with delays if it returns errors", func() {
 			plugin2 = NewFakePlugin("fake-device2", devicePath2)
 			plugin2.Error = fmt.Errorf("failing")
-			deviceController := NewDeviceController(host, 10)
+			deviceController := NewDeviceController(host, 10, fakeConfigMap, fakeInformer)
 			deviceController.backoff = []time.Duration{10 * time.Millisecond, 300 * time.Millisecond}
-			deviceController.devicePlugins = []GenericDevice{plugin2}
+			deviceController.devicePlugins[deviceName2] = ControlledDevice{
+				devicePlugin: plugin2,
+				stopChan:     stop,
+			}
 			go deviceController.Run(stop)
 			Consistently(func() int {
 				return int(atomic.LoadInt32(&plugin2.Starts))
@@ -121,8 +139,15 @@ var _ = Describe("Device Controller", func() {
 		})
 
 		It("Should not block on other plugins", func() {
-			deviceController := NewDeviceController(host, 10)
-			deviceController.devicePlugins = []GenericDevice{plugin1, plugin2}
+			deviceController := NewDeviceController(host, 10, fakeConfigMap, fakeInformer)
+			deviceController.devicePlugins[deviceName1] = ControlledDevice{
+				devicePlugin: plugin1,
+				stopChan:     stop,
+			}
+			deviceController.devicePlugins[deviceName2] = ControlledDevice{
+				devicePlugin: plugin2,
+				stopChan:     stop,
+			}
 			go deviceController.Run(stop)
 
 			Expect(deviceController.nodeHasDevice(devicePath1)).To(BeFalse())
