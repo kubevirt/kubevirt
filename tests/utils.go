c4c63f3568345376b067b41b25ac09c159def9c3
Quique Llorente <ellorent@redhat.com>
2020-08-26 17:24:01 +0200

    tests, vmi_servers: Add `HTTPServer.Start` and `TCPServer.Start` method to bypass LoggedIn
    In case a VMI has being already logged in calling the current `StartHTTPServer` make it calling
    LoggedIn again and that means setting ipv6 again that takes a time, usually if the tests has already
    functions like `WaitUntilReady` LoggedIn is not needed to be called and we can make the test a little
    faster.
    
    this commit refactor the server as HTTPServer and TCPServer types that
    can be started with `Start` function it runs the server command
    at VMI without calling LoggedIn function, the other StartHTTPServer and
    StartTCPServer are keep and they do the LoggedIn to keep retrocompatibility.
    
    Also a `LoginToCirros` helper has being added in case cirros VMI need to be
    with a logged in console but test does not care for expecter.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- tests/utils.go
+++ tests/utils.go
@@ -2720,22 +2720,6 @@ func WaitUntilVMIReady(vmi *v1.VirtualMachineInstance, expecterFactory VMIExpect
 	return vmi
 }
 
-func WaitUntilVMIReadyWithNamespace(namespace string, vmi *v1.VirtualMachineInstance, expecterFactory VMIExpecterFactory) *v1.VirtualMachineInstance {
-	// Wait for VirtualMachineInstance start
-	WaitForSuccessfulVMIStart(vmi)
-
-	// Fetch the new VirtualMachineInstance with updated status
-	virtClient, err := kubecli.GetKubevirtClient()
-	vmi, err = virtClient.VirtualMachineInstance(namespace).Get(vmi.Name, &metav1.GetOptions{})
-	ExpectWithOffset(1, err).ToNot(HaveOccurred())
-
-	// Lets make sure that the OS is up by waiting until we can login
-	expecter, err := expecterFactory(vmi)
-	ExpectWithOffset(1, err).ToNot(HaveOccurred())
-	expecter.Close()
-	return vmi
-}
-
 func NewInt32(x int32) *int32 {
 	return &x
 }
