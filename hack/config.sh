79951bcfec9c2a9f04d2bd979a95a4c4d2e7e041
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-09-02 10:39:33 +0200

    DNM: test commit
    Fixes for shellcheck though
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/config.sh
+++ hack/config.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 unset binaries docker_images docker_tag docker_tag_alt image_prefix image_prefix_alt manifest_templates \
     namespace image_pull_policy verbosity \
     csv_version package_name
 
+# shellcheck source=hack/config-default.sh
 source ${KUBEVIRT_PATH}hack/config-default.sh
+# shellcheck source=cluster-up/hack/config.sh
 source ${KUBEVIRT_PATH}cluster-up/hack/config.sh
 
 export binaries docker_images docker_tag docker_tag_alt image_prefix image_prefix_alt manifest_templates \
