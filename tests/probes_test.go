def420dfc17046a378f6ff478abc62196165eac7
Quique Llorente <ellorent@redhat.com>
2020-09-03 15:05:28 +0200

    tests, probes: Use a struct to group table test parameters
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- tests/probes_test.go
+++ tests/probes_test.go
@@ -19,6 +19,11 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 	var err error
 	var virtClient kubecli.KubevirtClient
 
+	type ProbeCase struct {
+		probe         *v12.Probe
+		serverStarter func(vmi *v12.VirtualMachineInstance, port int, isFedoraVM bool)
+	}
+
 	BeforeEach(func() {
 		virtClient, err = kubecli.GetKubevirtClient()
 		tests.PanicOnError(err)
@@ -49,11 +54,11 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				},
 			},
 		}
-		table.DescribeTable("should succeed", func(readinessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int, isFedoraVM bool)) {
+		table.DescribeTable("should succeed", func(c ProbeCase) {
 			// nc is used to create the HTTP server. The one shipped in CirrOS has a bug where it throws a
 			// 'connection reset by peer' when bound to IPv6 addresses. On those scenarios, we fallback to using a
 			// Fedora VM.
-			useFedoraVM := tests.IsIPv6Cluster(virtClient) && readinessProbe == httpProbe
+			useFedoraVM := tests.IsIPv6Cluster(virtClient) && c.probe == httpProbe
 
 			By("Specifying a VMI with a readiness probe")
 			var vmi *v12.VirtualMachineInstance
@@ -63,7 +68,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				vmi = tests.NewRandomVMIWithEphemeralDiskAndUserdata(cd.ContainerDiskFor(cd.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
 			}
 
-			vmi.Spec.ReadinessProbe = readinessProbe
+			vmi.Spec.ReadinessProbe = c.probe
 			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
 			Expect(err).ToNot(HaveOccurred())
 			// It may come to modify retries on the VMI because of the kubelet updating the pod, which can trigger controllers more often
@@ -75,7 +80,7 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			Expect(vmiReady(vmi)).To(Equal(v1.ConditionFalse))
 
 			By("Starting the server inside the VMI")
-			serverStarter(vmi, 1500, useFedoraVM)
+			c.serverStarter(vmi, 1500, useFedoraVM)
 
 			By("Checking that the VMI and the pod will be marked as ready to receive traffic")
 			Eventually(func() v1.ConditionStatus {
@@ -85,14 +90,20 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			}, 60, 1).Should(Equal(v1.ConditionTrue))
 			Expect(tests.PodReady(tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault))).To(Equal(v1.ConditionTrue))
 		},
-			table.Entry("[test_id:1202][posneg:positive]with working TCP probe and tcp server", tcpProbe, startTCPServer),
-			table.Entry("[test_id:1200][posneg:positive]with working HTTP probe and http server", httpProbe, tests.StartHTTPServer),
+			table.Entry("[test_id:1202][posneg:positive]with working TCP probe and tcp server", ProbeCase{
+				probe:         tcpProbe,
+				serverStarter: startTCPServer,
+			}),
+			table.Entry("[test_id:1200][posneg:positive]with working HTTP probe and http server", ProbeCase{
+				probe:         httpProbe,
+				serverStarter: tests.StartHTTPServer,
+			}),
 		)
 
-		table.DescribeTable("should fail", func(readinessProbe *v12.Probe) {
+		table.DescribeTable("should fail", func(c ProbeCase) {
 			By("Specifying a VMI with a readiness probe")
 			vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(cd.ContainerDiskFor(cd.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
-			vmi.Spec.ReadinessProbe = readinessProbe
+			vmi.Spec.ReadinessProbe = c.probe
 			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
 			Expect(err).ToNot(HaveOccurred())
 			// It may come to modify retries on the VMI because of the kubelet updating the pod, which can trigger controllers more often
@@ -111,8 +122,12 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			}, 60, 1).Should(Equal(v1.ConditionFalse))
 			Expect(tests.PodReady(tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault))).To(Equal(v1.ConditionFalse))
 		},
-			table.Entry("[test_id:1220][posneg:negative]with working TCP probe and no running server", tcpProbe),
-			table.Entry("[test_id:1219][posneg:negative]with working HTTP probe and no running server", httpProbe),
+			table.Entry("[test_id:1220][posneg:negative]with working TCP probe and no running server", ProbeCase{
+				probe: tcpProbe,
+			}),
+			table.Entry("[test_id:1219][posneg:negative]with working HTTP probe and no running server", ProbeCase{
+				probe: httpProbe,
+			}),
 		)
 	})
 
@@ -139,17 +154,17 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				},
 			},
 		}
-		table.DescribeTable("should not fail the VMI", func(livenessProbe *v12.Probe, serverStarter func(vmi *v12.VirtualMachineInstance, port int, isFedoraVM bool)) {
+		table.DescribeTable("should not fail the VMI", func(c ProbeCase) {
 			By("Specifying a VMI with a readiness probe")
 			vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(cd.ContainerDiskFor(cd.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
-			vmi.Spec.LivenessProbe = livenessProbe
+			vmi.Spec.LivenessProbe = c.probe
 			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
 			Expect(err).ToNot(HaveOccurred())
 			// It may come to modify retries on the VMI because of the kubelet updating the pod, which can trigger controllers more often
 			tests.WaitForSuccessfulVMIStartIgnoreWarnings(vmi)
 
 			By("Starting the server inside the VMI")
-			serverStarter(vmi, 1500, false)
+			c.serverStarter(vmi, 1500, false)
 
 			By("Checking that the VMI is still running after a minute")
 			Consistently(func() bool {
@@ -158,14 +173,20 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				return vmi.IsFinal()
 			}, 120, 1).Should(Not(BeTrue()))
 		},
-			table.Entry("[test_id:1199][posneg:positive]with working TCP probe and tcp server", tcpProbe, startTCPServer),
-			table.Entry("[test_id:1201][posneg:positive]with working HTTP probe and http server", httpProbe, tests.StartHTTPServer),
+			table.Entry("[test_id:1199][posneg:positive]with working TCP probe and tcp server", ProbeCase{
+				probe:         tcpProbe,
+				serverStarter: startTCPServer,
+			}),
+			table.Entry("[test_id:1201][posneg:positive]with working HTTP probe and http server", ProbeCase{
+				probe:         httpProbe,
+				serverStarter: tests.StartHTTPServer,
+			}),
 		)
 
-		table.DescribeTable("should fail the VMI", func(livenessProbe *v12.Probe) {
+		table.DescribeTable("should fail the VMI", func(c ProbeCase) {
 			By("Specifying a VMI with a readiness probe")
 			vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(cd.ContainerDiskFor(cd.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
-			vmi.Spec.LivenessProbe = livenessProbe
+			vmi.Spec.LivenessProbe = c.probe
 			vmi, err = virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Create(vmi)
 			Expect(err).ToNot(HaveOccurred())
 			// It may come to modify retries on the VMI because of the kubelet updating the pod, which can trigger controllers more often
@@ -178,8 +199,12 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 				return vmi.IsFinal()
 			}, 120, 1).Should(BeTrue())
 		},
-			table.Entry("[test_id:1217][posneg:negative]with working TCP probe and no running server", tcpProbe),
-			table.Entry("[test_id:1218][posneg:negative]with working HTTP probe and no running server", httpProbe),
+			table.Entry("[test_id:1217][posneg:negative]with working TCP probe and no running server", ProbeCase{
+				probe: tcpProbe,
+			}),
+			table.Entry("[test_id:1218][posneg:negative]with working HTTP probe and no running server", ProbeCase{
+				probe: httpProbe,
+			}),
 		)
 	})
 })
