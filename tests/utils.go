55611eaaa769c76738c3693b3fefcd5c2e826859
Edward Haas <edwardh@redhat.com>
2020-09-06 22:38:48 +0300

    tests, libnet: Move cloud-init net and dns to libnet
    The DNS and Clound-Init NoCloud Network Data content creation is
    relocated to the new libnet package.
    
    As part of the move, some refactoring has been applied. In particular
    the cloud-init network data has been structured exposing network v2
    format and supporting struct-yaml conversions.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- tests/utils.go
+++ tests/utils.go
@@ -102,6 +102,7 @@ import (
 
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
 	"kubevirt.io/kubevirt/tests/flags"
+	"kubevirt.io/kubevirt/tests/libnet"
 	"kubevirt.io/kubevirt/tests/libvmi"
 
 	"github.com/Masterminds/semver"
@@ -222,11 +223,6 @@ const (
 	swaggerPath     = "api/openapi-spec/swagger.json"
 	artifactsEnv    = "ARTIFACTS"
 	tmpPath         = "/var/provision/kubevirt.io/tests"
-)
-
-const (
-	ipv6MasqueradeAddress = "fd10:0:2::2/120"
-	ipv6MasqueradeGateway = "fd10:0:2::1"
 )
 
 const (
@@ -2135,14 +2131,8 @@ func AddEphemeralCdrom(vmi *v1.VirtualMachineInstance, name string, bus string, 
 }
 
 func NewRandomFedoraVMIWitGuestAgent() *v1.VirtualMachineInstance {
-	virtClient, err := kubecli.GetKubevirtClient()
-	PanicOnError(err)
-
-	dnsServerIP, err := getClusterDnsServiceIP(virtClient)
-	PanicOnError(err)
-
-	searchDomains := getVMISeachDomains()
-	networkData := GetCloudInitNetworkData(ipv6MasqueradeAddress, ipv6MasqueradeGateway, dnsServerIP, searchDomains)
+	networkData, err := libnet.CreateDefaultCloudInitNetworkData()
+	Expect(err).NotTo(HaveOccurred())
 
 	return libvmi.NewFedora(
 		libvmi.WithCloudInitNoCloudUserData(GetGuestAgentUserData(), false),
@@ -2230,27 +2220,6 @@ func GetGuestAgentUserData() string {
                 setenforce 0
                 systemd-run --unit=guestagent /usr/local/bin/qemu-ga
                 `, guestAgentUrl, guestAgentUrl, GetUrl(StressHttpUrl))
-}
-
-// Returns NetworkData for configuring a dynamic IPv4 address, and a static
-//IPv6 address, along with DNS configuration.
-func GetCloudInitNetworkData(ipAddress string, gateway string, dnsServer string, searchDomains []string) string {
-	networkData := fmt.Sprintf(`
-version: 2
-ethernets:
-    eth0:
-        addresses: [ %s ]
-        dhcp4: true
-        gateway6: %s
-        nameservers:
-            addresses: [ %s ]
-            search: [ %s ]
-    `, ipAddress, gateway, dnsServer, strings.Join(searchDomains, " "))
-	return networkData
-}
-
-func getVMISeachDomains() []string {
-	return []string{"default.svc.cluster.local", "svc.cluster.local", "cluster.local"}
 }
 
 func NewRandomVMIWithEphemeralDiskAndUserdata(containerImage string, userData string) *v1.VirtualMachineInstance {
