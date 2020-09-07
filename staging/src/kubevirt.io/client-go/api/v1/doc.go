5cd8b34cf0cfcc60dac5e5d3e32d5690658a953b
L. Pivarc <lpivarc@redhat.com>
2020-09-07 12:41:47 +0200

    adding markers for controller-gen
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- staging/src/kubevirt.io/client-go/api/v1/doc.go
+++ staging/src/kubevirt.io/client-go/api/v1/doc.go
@@ -1,4 +1,8 @@
 // +k8s:deepcopy-gen=package
 // +k8s:defaulter-gen=TypeMeta
 
+// +kubebuilder:validation:Optional
+// +groupName=kubevirt.io
+// +versionName=v1alpha3
+// Package v1 is the v1 version of the API.
 package v1
