641a6ec970a3b7185cd28e53049a6d5ff3968cd2
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-09-08 16:11:28 +0200

    network, tests: check IPv6 probes on dual stack network configs
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- tests/probes_test.go
+++ tests/probes_test.go
@@ -7,11 +7,13 @@ import (
 	v1 "k8s.io/api/core/v1"
 	v13 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	netutils "k8s.io/utils/net"
 
 	v12 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/kubevirt/tests"
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
+	"kubevirt.io/kubevirt/tests/libnet"
 )
 
 var _ = Describe("[ref_id:1182]Probes", func() {
@@ -24,6 +26,30 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 
 		tests.BeforeTestCleanup()
 	})
+
+	buildProbeBackendPodSpec := func(probe *v12.Probe) (*v1.Pod, func() error) {
+		isHTTPProbe := probe.Handler.HTTPGet != nil
+		var probeBackendPod *v1.Pod
+		if isHTTPProbe {
+			port := probe.HTTPGet.Port.IntVal
+			probeBackendPod = tests.StartHTTPServerPod(int(port))
+		} else {
+			port := probe.TCPSocket.Port.IntVal
+			probeBackendPod = tests.StartTCPServerPod(int(port))
+		}
+		return probeBackendPod, func() error {
+			return virtClient.CoreV1().Pods(tests.NamespaceTestDefault).Delete(probeBackendPod.Name, &v13.DeleteOptions{})
+		}
+	}
+
+	getPodIPv6Address := func(pod v1.Pod) *string {
+		for _, ip := range pod.Status.PodIPs {
+			if netutils.IsIPv6String(ip.IP) {
+				return &ip.IP
+			}
+		}
+		return nil
+	}
 
 	Context("for readiness", func() {
 		var vmi *v12.VirtualMachineInstance
@@ -75,6 +101,37 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 			table.Entry("[test_id:1220][posneg:negative]with working TCP probe and no running server", tcpProbe),
 			table.Entry("[test_id:1219][posneg:negative]with working HTTP probe and no running server", httpProbe),
 		)
+
+		table.DescribeTable("should succeed when probing an IPv6 IP on dual stack clusters", func(readinessProbe *v12.Probe) {
+			isClusterDualStack, err := libnet.IsClusterDualStack(virtClient)
+			Expect(err).NotTo(HaveOccurred(), "should have been able to infer if the cluster is dual stack")
+			if !isClusterDualStack {
+				Skip("This test requires a dual stack network config.")
+			}
+
+			By("Create a support pod which will reply to kubelet's probes ...")
+			probeBackendPod, supportPodCleanupFunc := buildProbeBackendPodSpec(readinessProbe)
+			defer func() {
+				Expect(supportPodCleanupFunc()).To(Succeed(), "The support pod responding to the probes should be cleaned-up at test tear-down.")
+			}()
+
+			supportPodIPv6IP := getPodIPv6Address(*probeBackendPod)
+			Expect(supportPodIPv6IP).NotTo(BeNil())
+			readinessProbe = patchProbeWithIPv6Addr(readinessProbe, *supportPodIPv6IP)
+
+			By("Specifying a VMI with a readiness probe")
+			vmi = createReadyCirrosVMIWithReadinessProbe(virtClient, readinessProbe)
+
+			// pod is not ready until our probe contacts the server
+			assertPodNotReady(virtClient, vmi)
+
+			By("Checking that the VMI and the pod will be marked as ready to receive traffic")
+			Eventually(isVMIReady, 60, 1).Should(Equal(true))
+			Expect(tests.PodReady(tests.GetRunningPodByVirtualMachineInstance(vmi, tests.NamespaceTestDefault))).To(Equal(v1.ConditionTrue))
+		},
+			table.Entry("with working TCP probe and tcp server", tcpProbe),
+			table.Entry("with working HTTP probe and http server", httpProbe),
+		)
 	})
 
 	Context("for liveness", func() {
@@ -118,6 +175,36 @@ var _ = Describe("[ref_id:1182]Probes", func() {
 		},
 			table.Entry("[test_id:1217][posneg:negative]with working TCP probe and no running server", tcpProbe),
 			table.Entry("[test_id:1218][posneg:negative]with working HTTP probe and no running server", httpProbe),
+		)
+
+		table.DescribeTable("should not fail the VMI when probing with an IPv6 address", func(livenessProbe *v12.Probe) {
+			isClusterDualStack, err := libnet.IsClusterDualStack(virtClient)
+			Expect(err).NotTo(HaveOccurred(), "should have been able to infer if the cluster is dual stack")
+			if !isClusterDualStack {
+				Skip("This test requires a dual stack network config.")
+			}
+
+			By("Create a support pod which will reply to kubelet's probes ...")
+			probeBackendPod, supportPodCleanupFunc := buildProbeBackendPodSpec(livenessProbe)
+			defer func() {
+				Expect(supportPodCleanupFunc()).To(Succeed(), "The support pod responding to the probes should be cleaned-up at test tear-down.")
+			}()
+
+			supportPodIPv6IP := getPodIPv6Address(*probeBackendPod)
+			Expect(supportPodIPv6IP).NotTo(BeNil())
+			livenessProbe = patchProbeWithIPv6Addr(livenessProbe, *supportPodIPv6IP)
+
+			vmi := createReadyCirrosVMIWithLivenessProbe(virtClient, livenessProbe)
+
+			By("Checking that the VMI is still running after a minute")
+			Consistently(func() bool {
+				vmi, err := virtClient.VirtualMachineInstance(tests.NamespaceTestDefault).Get(vmi.Name, &v13.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				return vmi.IsFinal()
+			}, 120, 1).ShouldNot(BeTrue())
+		},
+			table.Entry("with a TCP probe", tcpProbe),
+			table.Entry("with an HTTP probe", httpProbe),
 		)
 	})
 })
@@ -178,6 +265,15 @@ func createTCPProbe(period int32, initialSeconds int32, port int) *v12.Probe {
 	return createProbeSpecification(period, initialSeconds, httpHandler)
 }
 
+func patchProbeWithIPv6Addr(existingProbe *v12.Probe, ipv6HostIP string) *v12.Probe {
+	if isHTTPProbe(*existingProbe) {
+		existingProbe.HTTPGet.Host = ipv6HostIP
+	} else {
+		existingProbe.TCPSocket.Host = ipv6HostIP
+	}
+	return existingProbe
+}
+
 func createHTTPProbe(period int32, initialSeconds int32, port int) *v12.Probe {
 	httpHandler := v12.Handler{
 		HTTPGet: &v1.HTTPGetAction{
@@ -197,16 +293,4 @@ func createProbeSpecification(period int32, initialSeconds int32, handler v12.Ha
 
 func isHTTPProbe(probe v12.Probe) bool {
 	return probe.Handler.HTTPGet != nil
-}
-
-func buildProbeBackendPodSpec(probe *v12.Probe) *v1.Pod {
-	var probeBackendPod *v1.Pod
-	if isHTTPProbe(*probe) {
-		port := probe.HTTPGet.Port.IntVal
-		probeBackendPod = tests.StartHTTPServerPod(int(port))
-	} else {
-		port := probe.TCPSocket.Port.IntVal
-		probeBackendPod = tests.StartTCPServerPod(int(port))
-	}
-	return probeBackendPod
 }
