4758e28232942091416cf79e87a90885f75e28ba
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-09-10 16:38:35 +0200

    Add creation of bazelrc for running unnested in prow
    Borrow parts of create_bazel_cache_rcs.sh from test-infra bootstrap.
    Add KUBEVIRT_CREATE_BAZELRCS env to enable it on prow builds.
    
    Fix some shellcheck errors BTW.
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/builder/entrypoint.sh
+++ hack/builder/entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 set -o pipefail
 
+if [[ "$KUBEVIRT_CREATE_BAZELRCS" == "true" ]]; then
+    /create_bazel_cache_rcs.sh
+fi
+
 source /etc/profile.d/gimme.sh
 export GOPATH="/root/go"
 eval "$@"
