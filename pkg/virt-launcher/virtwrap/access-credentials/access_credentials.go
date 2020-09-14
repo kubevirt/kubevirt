f613c27f7ed2bb9150c60cfe00a41b454d88f406
David Vossel <davidvossel@gmail.com>
2020-09-14 17:25:46 -0400

    refactor
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/virt-launcher/virtwrap/access-credentials/access_credentials.go
+++ pkg/virt-launcher/virtwrap/access-credentials/access_credentials.go
@@ -36,12 +36,20 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/util"
 )
 
+type openReturn struct {
+	Return int `json:"return"`
+}
+type readReturn struct {
+	Count  int    `json:"count"`
+	BufB64 string `json:"buf-b64"`
+}
+
 type AccessCredentialManager struct {
 	virConn cli.Connection
 
 	// access credentail propagation lock
-	accessCredentialsLock          sync.Mutex
-	accessCredentialsPollerStarted bool
+	lock                 sync.Mutex
+	secretWatcherStarted bool
 }
 
 func NewManager(connection cli.Connection) *AccessCredentialManager {
@@ -51,102 +59,17 @@ func NewManager(connection cli.Connection) *AccessCredentialManager {
 	}
 }
 
-func (l *AccessCredentialManager) agentWriteAuthorizedKeys(domName string, filePath string, authorizedKeys string) error {
+func (l *AccessCredentialManager) writeGuestFile(contents string, domName string, filePath string) error {
 
-	fileExists := false
-	separator := "### AUTO PROPAGATED BY KUBEVIRT BELOW THIS LINE ###\n"
-	curAuthorizedKeys := ""
+	base64Str := base64.StdEncoding.EncodeToString([]byte(contents))
 
-	type openReturn struct {
-		Return int `json:"return"`
-	}
-	type readReturn struct {
-		Count  int    `json:"count"`
-		BufB64 string `json:"buf-b64"`
-	}
-
-	// ######
-	// Step 1. Validate if file exists on guest already or not.
-	// ######
-	cmdOpenFile := fmt.Sprintf(`{"execute": "guest-file-open", "arguments": { "path": "%s", "mode":"r" } }`, filePath)
+	cmdOpenFile := fmt.Sprintf(`{"execute": "guest-file-open", "arguments": { "path": "%s", "mode":"w" } }`, filePath)
 	output, err := l.virConn.QemuAgentCommand(cmdOpenFile, domName)
-	if err == nil {
-		fileExists = true
-	} else if !strings.Contains(err.Error(), "No such file or directory") {
-		log.Log.Infof("VOSSEL -DEBUG- OUPENOUTPUT: %s ERR: %s", output, err.Error())
+	if err != nil {
 		return err
-	}
-
-	if !fileExists {
-		return fmt.Errorf("Unable to update authorized_keys file because file does not exist on the guest at path %s", filePath)
-
 	}
 
 	openRes := &openReturn{}
-	err = json.Unmarshal([]byte(output), openRes)
-	if err != nil {
-		return err
-	}
-
-	// ######
-	// Step 2. read existing file
-	// ######
-	cmdReadFile := fmt.Sprintf(`{"execute": "guest-file-read", "arguments": { "handle": %d } }`, openRes.Return)
-	readOutput, err := l.virConn.QemuAgentCommand(cmdReadFile, domName)
-
-	log.Log.Infof("VOSSEL -DEBUG- READOUTPUT: %s", readOutput)
-	if err != nil {
-		return err
-	}
-
-	readRes := &readReturn{}
-	err = json.Unmarshal([]byte(readOutput), readRes)
-	if err != nil {
-		return err
-	}
-
-	if readRes.Count > 0 {
-		curAuthorizedKeysBytes, err := base64.StdEncoding.DecodeString(readRes.BufB64)
-		if err != nil {
-			return err
-		}
-		curAuthorizedKeys = string(curAuthorizedKeysBytes)
-	}
-
-	log.Log.Infof("VOSSEL -DEBUG- CUR AUTH KEYS: %s", curAuthorizedKeys)
-
-	cmdCloseFile := fmt.Sprintf(`{"execute": "guest-file-close", "arguments": { "handle": %d } }`, openRes.Return)
-	output, err = l.virConn.QemuAgentCommand(cmdCloseFile, domName)
-	if err != nil {
-		return err
-	}
-
-	// ######
-	// Step 3. Merge kubevirt authorized keys to end of file
-	// ######
-
-	// Add a warning line so people know where these entries are coming from
-	// and the risk of altering them
-	split := strings.Split(curAuthorizedKeys, separator)
-	if len(split) > 0 {
-		curAuthorizedKeys = split[0]
-	} else {
-		curAuthorizedKeys = ""
-	}
-	log.Log.Infof("VOSSEL -DEBUG- STRIPPED CUR AUTH KEYS: %s", curAuthorizedKeys)
-	authorizedKeys = fmt.Sprintf("%s\n%s\n%s", curAuthorizedKeys, separator, authorizedKeys)
-	base64Str := base64.StdEncoding.EncodeToString([]byte(authorizedKeys))
-
-	// ######
-	// Step 4. Write merged file
-	// ######
-	cmdOpenFile = fmt.Sprintf(`{"execute": "guest-file-open", "arguments": { "path": "%s", "mode":"w" } }`, filePath)
-	output, err = l.virConn.QemuAgentCommand(cmdOpenFile, domName)
-	if err != nil {
-		return err
-	}
-
-	openRes = &openReturn{}
 	err = json.Unmarshal([]byte(output), openRes)
 	if err != nil {
 		return err
@@ -158,7 +81,7 @@ func (l *AccessCredentialManager) agentWriteAuthorizedKeys(domName string, fileP
 		return err
 	}
 
-	cmdCloseFile = fmt.Sprintf(`{"execute": "guest-file-close", "arguments": { "handle": %d } }`, openRes.Return)
+	cmdCloseFile := fmt.Sprintf(`{"execute": "guest-file-close", "arguments": { "handle": %d } }`, openRes.Return)
 	output, err = l.virConn.QemuAgentCommand(cmdCloseFile, domName)
 	if err != nil {
 		return err
@@ -167,11 +90,190 @@ func (l *AccessCredentialManager) agentWriteAuthorizedKeys(domName string, fileP
 	return nil
 }
 
-func (l *AccessCredentialManager) HandleQemuAgentAccessCredentials(vmi *v1.VirtualMachineInstance) {
-	l.accessCredentialsLock.Lock()
-	defer l.accessCredentialsLock.Unlock()
+func (l *AccessCredentialManager) readGuestFile(domName string, filePath string) (string, error) {
+	contents := ""
 
-	if l.accessCredentialsPollerStarted {
+	cmdOpenFile := fmt.Sprintf(`{"execute": "guest-file-open", "arguments": { "path": "%s", "mode":"r" } }`, filePath)
+	output, err := l.virConn.QemuAgentCommand(cmdOpenFile, domName)
+	if err != nil {
+		return contents, err
+	}
+
+	openRes := &openReturn{}
+	err = json.Unmarshal([]byte(output), openRes)
+	if err != nil {
+		return contents, err
+	}
+
+	cmdReadFile := fmt.Sprintf(`{"execute": "guest-file-read", "arguments": { "handle": %d } }`, openRes.Return)
+	readOutput, err := l.virConn.QemuAgentCommand(cmdReadFile, domName)
+
+	log.Log.Infof("VOSSEL -DEBUG- READOUTPUT: %s", readOutput)
+	if err != nil {
+		return contents, err
+	}
+
+	readRes := &readReturn{}
+	err = json.Unmarshal([]byte(readOutput), readRes)
+	if err != nil {
+		return contents, err
+	}
+
+	if readRes.Count > 0 {
+		readBytes, err := base64.StdEncoding.DecodeString(readRes.BufB64)
+		if err != nil {
+			return contents, err
+		}
+		contents = string(readBytes)
+	}
+
+	cmdCloseFile := fmt.Sprintf(`{"execute": "guest-file-close", "arguments": { "handle": %d } }`, openRes.Return)
+	output, err = l.virConn.QemuAgentCommand(cmdCloseFile, domName)
+	if err != nil {
+		return contents, err
+	}
+
+	return contents, nil
+}
+
+func (l *AccessCredentialManager) agentWriteAuthorizedKeys(domName string, filePath string, authorizedKeys string) error {
+
+	separator := "### AUTO PROPAGATED BY KUBEVIRT BELOW THIS LINE ###\n"
+	curAuthorizedKeys := ""
+
+	// ######
+	// Step 1. Read file on guest
+	// ######
+
+	curAuthorizedKeys, err := l.readGuestFile(domName, filePath)
+	if err != nil {
+		if strings.Contains(err.Error(), "No such file or directory") {
+			err = fmt.Errorf("Unable to update authorized_keys file because file does not exist on the guest at path %s. Error: %v", filePath, err.Error())
+		}
+
+		return err
+	}
+
+	// ######
+	// Step 2. Merge kubevirt authorized keys to end of file
+	// ######
+
+	// Add a warning line so people know where these entries are coming from
+	// and the risk of altering them
+	origAuthorizedKeys := curAuthorizedKeys
+	split := strings.Split(curAuthorizedKeys, separator)
+	if len(split) > 0 {
+		curAuthorizedKeys = split[0]
+	} else {
+		curAuthorizedKeys = ""
+	}
+	authorizedKeys = fmt.Sprintf("%s\n%s\n%s", curAuthorizedKeys, separator, authorizedKeys)
+
+	// ######
+	// Step 3. Write merged file
+	// ######
+	// only update if the updated string is not equal to the current contents on the guest.
+	if origAuthorizedKeys != authorizedKeys {
+		err = l.writeGuestFile(authorizedKeys, domName, filePath)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *AccessCredentialManager) watchSecrets(vmi *v1.VirtualMachineInstance) {
+	logger := log.Log.Object(vmi)
+
+	domName := util.VMINamespaceKeyFunc(vmi)
+	for {
+		// secret name mapped to authorized_keys in that secret
+		secretMap := make(map[string]string)
+		// filepath mapped to secretNames
+		filePathMap := make(map[string][]string)
+
+		// TODO make this inotify based
+		time.Sleep(10 * time.Second)
+
+		// Step 1. Populate Secrets and filepath Map
+		for _, accessCred := range vmi.Spec.AccessCredentials {
+			if accessCred.SSHPublicKey == nil || accessCred.SSHPublicKey.PropagationMethod.QemuGuestAgent == nil {
+				continue
+			}
+
+			secretName := ""
+			if accessCred.SSHPublicKey.Source.Secret != nil {
+				secretName = accessCred.SSHPublicKey.Source.Secret.SecretName
+			}
+
+			if secretName == "" {
+				continue
+			}
+
+			for _, entry := range accessCred.SSHPublicKey.PropagationMethod.QemuGuestAgent.AuthorizedKeysFiles {
+				secrets, ok := filePathMap[entry.FilePath]
+				if !ok {
+					filePathMap[entry.FilePath] = []string{secretName}
+				} else {
+					filePathMap[entry.FilePath] = append(secrets, secretName)
+				}
+			}
+
+			secretDir := filepath.Join(config.SecretSourceDir, secretName+"-access-cred")
+			files, err := ioutil.ReadDir(secretDir)
+			if err != nil {
+				logger.Reason(err).Errorf("Error encountered reading secrets file list from base directory %s", secretDir)
+				continue
+			}
+
+			authorizedKeys := ""
+			for _, file := range files {
+				if file.IsDir() || strings.HasPrefix(file.Name(), "..") {
+					continue
+				}
+
+				pubKeyBytes, err := ioutil.ReadFile(filepath.Join(secretDir, file.Name()))
+				if err != nil {
+					logger.Reason(err).Errorf("Error encountered reading secret file %s", filepath.Join(secretDir, file.Name()))
+					continue
+				}
+
+				pubKey := string(pubKeyBytes)
+				if pubKey == "" {
+					continue
+				}
+				authorizedKeys = fmt.Sprintf("%s\n%s", authorizedKeys, pubKey)
+			}
+
+			secretMap[secretName] = authorizedKeys
+
+		}
+
+		// Step 2. Update Authorized keys file
+		for filePath, secretNames := range filePathMap {
+			authorizedKeys := ""
+
+			for _, secretName := range secretNames {
+				pubKeys, ok := secretMap[secretName]
+				if ok && pubKeys != "" {
+					authorizedKeys = fmt.Sprintf("%s\n%s", authorizedKeys, pubKeys)
+				}
+			}
+
+			err := l.agentWriteAuthorizedKeys(domName, filePath, authorizedKeys)
+			if err != nil {
+				logger.Reason(err).Errorf("Error encountered writing access credentials using guest agent")
+				continue
+			}
+		}
+	}
+}
+
+func (l *AccessCredentialManager) HandleQemuAgentAccessCredentials(vmi *v1.VirtualMachineInstance) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	if l.secretWatcherStarted {
 		// already started
 		return
 	}
@@ -190,91 +292,6 @@ func (l *AccessCredentialManager) HandleQemuAgentAccessCredentials(vmi *v1.Virtu
 		return
 	}
 
-	go func() {
-		logger := log.Log.Object(vmi)
-
-		domName := util.VMINamespaceKeyFunc(vmi)
-		for {
-
-			// secret name mapped to authorized_keys in that secret
-			secretMap := make(map[string]string)
-			// filepath mapped to secretNames
-			filePathMap := make(map[string][]string)
-
-			// TODO make this inotify based
-			time.Sleep(10 * time.Second)
-
-			// Step 1. Populate Secrets and filepath Map
-			for _, accessCred := range vmi.Spec.AccessCredentials {
-				if accessCred.SSHPublicKey == nil || accessCred.SSHPublicKey.PropagationMethod.QemuGuestAgent == nil {
-					continue
-				}
-
-				secretName := ""
-				if accessCred.SSHPublicKey.Source.Secret != nil {
-					secretName = accessCred.SSHPublicKey.Source.Secret.SecretName
-				}
-
-				if secretName == "" {
-					continue
-				}
-
-				for _, entry := range accessCred.SSHPublicKey.PropagationMethod.QemuGuestAgent.AuthorizedKeysFiles {
-					secrets, ok := filePathMap[entry.FilePath]
-					if !ok {
-						filePathMap[entry.FilePath] = []string{secretName}
-					} else {
-						filePathMap[entry.FilePath] = append(secrets, secretName)
-					}
-				}
-
-				secretDir := filepath.Join(config.SecretSourceDir, secretName+"-access-cred")
-				files, err := ioutil.ReadDir(secretDir)
-				if err != nil {
-					logger.Reason(err).Errorf("Error encountered reading secrets file list from base directory %s", secretDir)
-					continue
-				}
-
-				authorizedKeys := ""
-				for _, file := range files {
-					if file.IsDir() || strings.HasPrefix(file.Name(), "..") {
-						continue
-					}
-
-					pubKeyBytes, err := ioutil.ReadFile(filepath.Join(secretDir, file.Name()))
-					if err != nil {
-						logger.Reason(err).Errorf("Error encountered reading secret file %s", filepath.Join(secretDir, file.Name()))
-						continue
-					}
-
-					pubKey := string(pubKeyBytes)
-					if pubKey == "" {
-						continue
-					}
-					authorizedKeys = fmt.Sprintf("%s\n%s", authorizedKeys, pubKey)
-				}
-
-				secretMap[secretName] = authorizedKeys
-
-			}
-
-			// Step 2. Write Authorized keys file
-			for filePath, secretNames := range filePathMap {
-				authorizedKeys := ""
-
-				for _, secretName := range secretNames {
-					pubKeys, ok := secretMap[secretName]
-					if ok && pubKeys != "" {
-						authorizedKeys = fmt.Sprintf("%s\n%s", authorizedKeys, pubKeys)
-					}
-				}
-
-				err := l.agentWriteAuthorizedKeys(domName, filePath, authorizedKeys)
-				if err != nil {
-					logger.Reason(err).Errorf("Error encountered writing access credentials using guest agent")
-					continue
-				}
-			}
-		}
-	}()
+	go l.watchSecrets(vmi)
+	l.secretWatcherStarted = true
 }
