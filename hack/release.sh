003075f8014fc79c971569b919ad56d81a34a282
David Vossel <davidvossel@gmail.com>
2020-09-14 11:41:43 -0400

    fmt updates
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -3,18 +3,18 @@
 set -e
 
 if [ -z ${GPG_PRIVATE_KEY_FILE} ]; then
-	echo "GPG_PRIVATE_KEY_FILE env var must be set"
-	exit 1
+    echo "GPG_PRIVATE_KEY_FILE env var must be set"
+    exit 1
 elif [ -z ${GPG_PASSPHRASE_FILE} ]; then
-	echo "GPG_PASSPHRASE_FILE env var must be set"
-	exit 1
+    echo "GPG_PASSPHRASE_FILE env var must be set"
+    exit 1
 elif [ -z ${GITHUB_API_TOKEN_FILE} ]; then
-	echo "GITHUB_API_TOKEN_FILE env var must be set"
-	exit 1
+    echo "GITHUB_API_TOKEN_FILE env var must be set"
+    exit 1
 fi
 
-GIT_USER=${GIT_USER:-`git config user.name`}
-GIT_EMAIL=${GIT_EMAIL:-`git config user.email`}
+GIT_USER=${GIT_USER:-$(git config user.name)}
+GIT_EMAIL=${GIT_EMAIL:-$(git config user.email)}
 
 echo "git user:  $GIT_USER"
 echo "git email: $GIT_EMAIL"
@@ -33,14 +33,12 @@ kubevirtci/release-tool:latest \
 \"$@\""
 
 docker run -it --rm \
--v ${GPG_PRIVATE_KEY_FILE}:/home/releaser/gpg-private \
--v ${GPG_PASSPHRASE_FILE}:/home/releaser/gpg-passphrase \
--v ${GITHUB_API_TOKEN_FILE}:/home/releaser/github-api-token \
-kubevirtci/release-tool:latest \
---org=kubevirt \
---repo=kubevirt \
---git-email "${GIT_EMAIL}" \
---git-user "${GIT_USER}" \
-"$@"
-
-
+    -v ${GPG_PRIVATE_KEY_FILE}:/home/releaser/gpg-private \
+    -v ${GPG_PASSPHRASE_FILE}:/home/releaser/gpg-passphrase \
+    -v ${GITHUB_API_TOKEN_FILE}:/home/releaser/github-api-token \
+    kubevirtci/release-tool:latest \
+    --org=kubevirt \
+    --repo=kubevirt \
+    --git-email "${GIT_EMAIL}" \
+    --git-user "${GIT_USER}" \
+    "$@"
