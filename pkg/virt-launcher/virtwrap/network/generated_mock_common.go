4d9912871499b50a990e9d145596ec33e5880242
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-09-15 13:14:18 +0200

    create tap device: add multiqueue support to netlink
    The netlink interface requires the user to specify the number of
    queues, instead of blindly accepting a boolean indicating if it is
    multi-queue.
    
    As such, we need to calculate the queue number (re-using the
    funcions available in the `converter` and use that instead.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- pkg/virt-launcher/virtwrap/network/generated_mock_common.go
+++ pkg/virt-launcher/virtwrap/network/generated_mock_common.go
@@ -315,8 +315,8 @@ func (_mr *_MockNetworkHandlerRecorder) GetNFTIPString(arg0 interface{}) *gomock
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "GetNFTIPString", arg0)
 }
 
-func (_m *MockNetworkHandler) CreateTapDevice(tapName string, isMultiqueue bool, launcherPID int) error {
-	ret := _m.ctrl.Call(_m, "CreateTapDevice", tapName, isMultiqueue, launcherPID)
+func (_m *MockNetworkHandler) CreateTapDevice(tapName string, queueNumber uint32, launcherPID int) error {
+	ret := _m.ctrl.Call(_m, "CreateTapDevice", tapName, queueNumber, launcherPID)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
