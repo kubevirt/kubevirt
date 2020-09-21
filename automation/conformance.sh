428efb55495881a9b18cdfc0dcfaf6fb8cf68940
Petr Horacek <phoracek@redhat.com>
2020-09-21 13:51:34 +0200

    Keep conformance artifacts on the top level
    Without this change, artifacts are kept under actifacts/conformance
    directory which prevents junit files from being exposed and introduces
    an unneeded subdirectory.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- automation/conformance.sh
+++ automation/conformance.sh
@@ -6,7 +6,7 @@ export WORKSPACE="${WORKSPACE:-$PWD}"
 readonly ARTIFACTS_PATH="${ARTIFACTS-$WORKSPACE/exported-artifacts}"
 mkdir -p "$ARTIFACTS_PATH"
 
-trap "{ make cluster-down; cp -r _out/artifacts/conformance ${ARTIFACTS_PATH}; }" EXIT SIGINT SIGTERM SIGQUIT
+trap "{ make cluster-down; cp -r _out/artifacts/conformance/* ${ARTIFACTS_PATH}; }" EXIT SIGINT SIGTERM SIGQUIT
 
 make cluster-up
 make cluster-sync
