0684f0ec868b4b8ba4816f88f1c4f94488ce461b
Alona Kaplan <alkaplan@redhat.com>
2020-09-22 19:44:53 +0300

    test, nfs: Change CreateNFSTargetPOD to return a Pod
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- tests/libnet/ipaddress.go
+++ tests/libnet/ipaddress.go
@@ -5,25 +5,26 @@ import (
 	netutils "k8s.io/utils/net"
 )
 
-func GetIp(ips []string, family v1.IPFamily) string {
+func GetPodIpByFamily(pod *v1.Pod, family v1.IPFamily) string {
+	var ips []string
+	for _, ip := range pod.Status.PodIPs {
+		ips = append(ips, ip.IP)
+	}
+	return getIp(ips, family)
+}
+
+func getIp(ips []string, family v1.IPFamily) string {
 	for _, ip := range ips {
-		if netutils.IsIPv6String(ip) {
-			if family == v1.IPv6Protocol {
-				return ip
-			}
-		} else {
-			if family == v1.IPv4Protocol {
-				return ip
-			}
+		if family == getFamily(ip) {
+			return ip
 		}
 	}
 	return ""
 }
 
-func GetPodIpsStrings(podIPs []v1.PodIP) []string {
-	var ips []string
-	for _, ip := range podIPs {
-		ips = append(ips, ip.IP)
+func getFamily(ip string) v1.IPFamily {
+	if netutils.IsIPv6String(ip) {
+		return v1.IPv6Protocol
 	}
-	return ips
+	return v1.IPv4Protocol
 }
