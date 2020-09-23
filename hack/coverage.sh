ac1b7b121e1e92e00ca278da192e93f246a6b0f2
Roman Mohr <rmohr@redhat.com>
2020-09-23 16:28:46 +0200

    Move coverage reports over to bazel
    coverage reports were the last part where we had to compile kubevirt
    with pure go-code. With this change it is possible to remove
    "libvirt-devel" from the builder image. Now no build/test dependencies
    are installedin the build-image which can get out of sync with bazel.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/coverage.sh
+++ hack/coverage.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-
 set -e
 
 source hack/common.sh
 
-path="./pkg/... ./vendor/kubevirt.io/client-go/..."
-coverpkg="../kubevirt/pkg/...,../kubevirt/vendor/kubevirt.io/client-go/..."
-profile=.coverprofile
-
-go test -cover -covermode=atomic -coverpkg="${coverpkg}" -v -coverprofile=${profile}.tmp ${path}
-cat ${profile}.tmp | grep -v generated >${profile}
-go tool cover -html=$profile -o ${OUT_DIR}/coverage.html
-rm -f ${profile}.tmp
+bazel coverage \
+    --config=${ARCHITECTURE} \
+    --stamp \
+    --test_output=errors -- //staging/src/kubevirt.io/client-go/... //pkg/... //cmd/...
--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
-set -e
+coverprofiles=$(cat | sed "s/ /,/g")
 
-./hack/coverage.sh
-goveralls -v -debug -service=${CI_NAME} -coverprofile=.coverprofile -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
+goveralls -v -debug -service=${CI_NAME} -coverprofile=${coverprofiles} -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
