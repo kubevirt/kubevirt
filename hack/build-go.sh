9f723f47db8f400065635cd5ffbcf04da4c24e8d
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-09-23 14:58:58 +0200

    selinux: always build KubeVirt with selinux support
    Upstream, we rely on bazel to set the selinux tag - [0]; on
    downstream, we do not use bazel, which causes KubeVirt to be
    compiled without selinux support, which ultimately causes the
    selinux library used in [1] to use the stub instead of the proper
    linux binding. This will indeed make the selinux do nothing when
    attempting to read labels / set execution labels.
    
    This change is harmless on nodes without selinux (or with it
    disabled) since all runtime calls to this library only happen *if*
    selinux is enabled.
    
    [0] - https://github.com/kubevirt/kubevirt/blob/master/.bazelrc#L18
    [1] - https://github.com/kubevirt/kubevirt/blob/master/pkg/virt-launcher/virtwrap/network/common.go#L36
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- hack/build-go.sh
+++ hack/build-go.sh
@@ -108,7 +108,7 @@ for arg in $args; do
             LINUX_NAME=${ARCH_BASENAME}-linux-amd64
 
             echo "building dynamic binary $BIN_NAME"
-            GOOS=linux GOARCH=amd64 go_build -i -o ${CMD_OUT_DIR}/${BIN_NAME}/${LINUX_NAME} -ldflags "$(kubevirt::version::ldflags)" $(pkg_dir linux amd64)
+            GOOS=linux GOARCH=amd64 go_build -tags selinux -i -o ${CMD_OUT_DIR}/${BIN_NAME}/${LINUX_NAME} -ldflags "$(kubevirt::version::ldflags)" $(pkg_dir linux amd64)
 
             (cd ${CMD_OUT_DIR}/${BIN_NAME} && ln -sf ${LINUX_NAME} ${BIN_NAME})
 
