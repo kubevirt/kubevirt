5815ce215976144615bf77539d8e8c7eef45bc06
L. Pivarc <lpivarc@redhat.com>
2020-09-30 14:16:39 +0200

    update builder image
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- pkg/virtctl/imageupload/imageupload.go
+++ pkg/virtctl/imageupload/imageupload.go
@@ -307,7 +307,7 @@ func getHTTPClient(insecure bool) *http.Client {
 	client := &http.Client{}
 
 	if insecure {
-		// #nosec
+		// #nosec cause: InsecureSkipVerify: true resolution: this method explicitly ask for insecure http client
 		client.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
