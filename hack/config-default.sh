e61f6bcf161428c7e01f3a0523216e57615f5ca0
Roman Mohr <rmohr@redhat.com>
2020-10-09 15:37:26 +0200

    Bump kubevirtci
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -11,7 +11,7 @@ cdi_namespace=cdi
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
 verbosity=${VERBOSITY:-2}
 package_name=${PACKAGE_NAME:-kubevirt-dev}
-kubevirtci_git_hash="f180644d306540c2c934250e8d686468bd7ed9f1"
+kubevirtci_git_hash="2010091151-ea2c81d"
 
 # try to derive csv_version from docker tag. But it must start with x.y.z, without leading v
 default_csv_version="${docker_tag/latest/0.0.0}"
