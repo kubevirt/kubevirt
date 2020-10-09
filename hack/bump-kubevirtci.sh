9a11dae0ba610aa89da0149168b9a3587f8bd416
Roman Mohr <rmohr@redhat.com>
2020-10-09 15:37:43 +0200

    Adjust bump script to use tagged kubevirtci releases
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/bump-kubevirtci.sh
+++ hack/bump-kubevirtci.sh
@@ -5,7 +5,7 @@ set -ex
 source $(dirname "$0")/common.sh
 source $(dirname "$0")/config.sh
 
-val=$(git ls-remote https://github.com/kubevirt/kubevirtci | grep HEAD | awk '{print $1}')
+val=$(curl -L https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest)
 sed -i "/^[[:blank:]]*kubevirtci_git_hash[[:blank:]]*=/s/=.*/=\"${val}\"/" hack/config-default.sh
 
 hack/sync-kubevirtci.sh
