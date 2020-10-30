bd6e8d6777499fdaa9086330997461bf56883b25
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-10-30 12:48:01 +0100

    Consolidate shell script files into functions
    Put several script files into functions that are located in
    entrypoint.sh so that they can just get called from within the
    container.
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/ci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Running tests"
-mkdir -p "$ARTIFACT_DIR"
-# required to be set for test binary
-export ARTIFACTS=${ARTIFACT_DIR}
-
-tests.test -v=5 -kubeconfig=${KUBECONFIG} -container-tag=${DOCKER_TAG} -container-tag-alt= -container-prefix=${DOCKER_PREFIX} -image-prefix-alt=-kv -oc-path=/bin/oc -kubectl-path=/bin/kubectl -gocli-path=$(pwd)/cluster-up/cli.sh -test.timeout 420m -ginkgo.noColor -ginkgo.succinct -ginkgo.slowSpecThreshold=60 '-ginkgo.focus=\[rfe_id:273\]\[crit:high\]' -junit-output=${ARTIFACT_DIR}/junit.functest.xml -installed-namespace=kubevirt -previous-release-tag= -previous-release-registry=index.docker.io/kubevirt -deploy-testing-infra=false
