1cade71cfb4a3a1fe825dc30f4ebb9cd6e33d9bd
Alona Kaplan <alkaplan@redhat.com>
2020-11-17 22:31:06 +0200

    dhcpv6: handle requests from client - adding DUID and IANA options
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- pkg/virt-launcher/virtwrap/network/dhcpv6/dhcpv6.go
+++ pkg/virt-launcher/virtwrap/network/dhcpv6/dhcpv6.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/insomniacslk/dhcp/dhcpv6"
 	"github.com/insomniacslk/dhcp/dhcpv6/server6"
+	"github.com/insomniacslk/dhcp/iana"
 
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/log"
@@ -19,10 +20,9 @@ const (
 )
 
 type DHCPv6Handler struct {
-	serverIP      net.IP
 	clientIP      net.IP
-	clientMAC     net.HardwareAddr
 	leaseDuration time.Duration
+	serverIface   string
 }
 
 func SingleClientDHCPv6Server(
@@ -38,12 +38,11 @@ func SingleClientDHCPv6Server(
 
 	handler := &DHCPv6Handler{
 		clientIP:      clientIP,
-		clientMAC:     clientMAC,
-		serverIP:      serverIP.To4(),
 		leaseDuration: infiniteLease,
+		serverIface:   serverIface,
 	}
 
-	s, err := server6.NewServer("", nil, handler.ServeDHCPv6)
+	s, err := server6.NewServer(serverIface, nil, handler.ServeDHCPv6)
 	if err != nil {
 		return fmt.Errorf("couldn't create dhcpv6 server: %v", err)
 	}
@@ -55,17 +54,38 @@ func SingleClientDHCPv6Server(
 }
 
 func (h *DHCPv6Handler) ServeDHCPv6(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
-	log.Log.V(4).Info("Serving a new request")
+	log.Log.V(4).Info("DHCPv6 serving a new request")
 
 	// TODO how can we make sure the request is from the vm? Is filtering requests arrived to the bridge interface enough?
 
 	msg := m.(*dhcpv6.Message)
-	adv, err := dhcpv6.NewAdvertiseFromSolicit(msg)
+
+	var response *dhcpv6.Message
+
+	optIAAddress := dhcpv6.OptIAAddress{IPv6Addr: h.clientIP, PreferredLifetime: h.leaseDuration, ValidLifetime: h.leaseDuration}
+
+	iface, err := net.InterfaceByName(h.serverIface)
 	if err != nil {
-		log.Log.V(4).Errorf("NewAdvertiseFromSolicit failed: %v", err)
+		log.Log.V(4).Info("DHCPv6 - couldn't get the server interface")
 		return
 	}
-	if _, err := conn.WriteTo(adv.ToBytes(), peer); err != nil {
-		log.Log.V(4).Errorf("Cannot reply to client: %v", err)
+	duid := dhcpv6.Duid{Type: dhcpv6.DUID_LL, HwType: iana.HWTypeEthernet, LinkLayerAddr: iface.HardwareAddr}
+
+	switch msg.Type() {
+	case dhcpv6.MessageTypeSolicit:
+		log.Log.V(4).Info("DHCPv6 - the request has message type Solicit")
+		response, err = dhcpv6.NewAdvertiseFromSolicit(msg, dhcpv6.WithIANA(optIAAddress), dhcpv6.WithServerID(duid))
+	default:
+		log.Log.V(4).Info("DHCPv6 - non Solicit request recieved")
+		response, err = dhcpv6.NewReplyFromMessage(msg, dhcpv6.WithIANA(optIAAddress), dhcpv6.WithServerID(duid))
+	}
+
+	if err != nil {
+		log.Log.V(4).Errorf("DHCPv6 failed sending a response to the client: %v", err)
+		return
+	}
+
+	if _, err := conn.WriteTo(response.ToBytes(), peer); err != nil {
+		log.Log.V(4).Errorf("DHCPv6 cannot reply to client: %v", err)
 	}
 }
