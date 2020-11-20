85daf38d158081a0b21dfe93e7434fd1ec32184c
Miguel Duarte Barroso <mdbarroso@redhat.com>
2020-11-20 12:21:04 +0100

    ipv6, ra: reply w/ RAs to router solicitations
    Advertise the PrefixInformation via RouterAdvertisements as a
    response to router solicitations (sent whenever an interface comes
    up).
    
    Follow up commits will configure filtering in the ICMP listener,
    and also configure route information within the RA msg.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

---
+++ pkg/virt-launcher/virtwrap/network/ndp/ndp.go
@@ -0,0 +1,96 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2020 Red Hat, Inc.
+ *
+ */
+
+package ndp
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/mdlayher/ndp"
+
+	"kubevirt.io/client-go/log"
+)
+
+const (
+	infiniteLease = 999 * 24 * time.Hour
+)
+
+type RouterAdvertisementDaemon struct {
+	raOptions []ndp.Option
+}
+
+func SingleClientRouterAdvertisementDaemon(serverIface string, ipv6CIDR string) error {
+	log.Log.Info("Starting RouterAdvertisement daemon")
+
+	prefix, network, err := net.ParseCIDR(ipv6CIDR)
+	if err != nil {
+		return fmt.Errorf("could not compute prefix / prefix length from %s. Reason: %v", ipv6CIDR, err)
+	}
+	prefixLength, _ := network.Mask.Size()
+
+	handler := &RouterAdvertisementDaemon{
+		raOptions: prepareRAOptions(prefix, uint8(prefixLength)),
+	}
+
+	listener, err := NewICMPv6Listener(serverIface)
+	if err != nil {
+		return fmt.Errorf("could not listen to icmpv6 on interface %s. Reason: %v", serverIface, err)
+	}
+
+	return handler.serve(listener)
+}
+
+func (rad *RouterAdvertisementDaemon) serve(listener *Listener) error {
+	for {
+		msg, _, _, err := listener.ReadFrom()
+		if err != nil {
+			return err
+		}
+
+		switch msg.(type) {
+		case *ndp.RouterSolicitation:
+			log.Log.V(4).Info("Received RouterSolicitation msg. Will reply w/ RA")
+			ra := &ndp.RouterAdvertisement{
+				ManagedConfiguration: true,
+				OtherConfiguration:   true,
+				RouterLifetime:       infiniteLease,
+				ReachableTime:        infiniteLease,
+				Options:              rad.raOptions,
+			}
+
+			if err := listener.WriteTo(ra, nil, net.IPv6linklocalallnodes); err != nil {
+				return fmt.Errorf("failed to send router advertisement: %v", err)
+			}
+		}
+	}
+}
+
+func prepareRAOptions(prefix net.IP, prefixLength uint8) []ndp.Option {
+	prefixInfo := &ndp.PrefixInformation{
+		PrefixLength:                   prefixLength,
+		OnLink:                         true,
+		AutonomousAddressConfiguration: false,
+		ValidLifetime:                  infiniteLease,
+		PreferredLifetime:              infiniteLease,
+		Prefix:                         prefix,
+	}
+	return []ndp.Option{prefixInfo}
+}
