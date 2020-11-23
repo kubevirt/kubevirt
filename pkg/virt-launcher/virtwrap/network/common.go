dfe066ed2afb79e89a45436cb4298d72683a925c
Alona Kaplan <alkaplan@redhat.com>
2020-11-23 13:13:55 +0200

    dhcpv6: run only for masquerade
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- pkg/virt-launcher/virtwrap/network/common.go
+++ pkg/virt-launcher/virtwrap/network/common.go
@@ -368,20 +368,22 @@ func (h *NetworkUtilsHandler) StartDHCP(nic *VIF, serverAddr net.IP, bridgeInter
 		}
 	}()
 
-	go func() {
-		if err = DHCPv6Server(
-			nic.IPv6.IP,
-			bridgeInterfaceName,
-			nameservers,
-			nic.Routes,
-			searchDomains,
-			nic.Mtu,
-			dhcpOptions,
-		); err != nil {
-			log.Log.Errorf("failed to run DHCP: %v", err)
-			panic(err)
-		}
-	}()
+	if nic.IPv6.IPNet != nil {
+		go func() {
+			if err = DHCPv6Server(
+				nic.IPv6.IP,
+				bridgeInterfaceName,
+				nameservers,
+				nic.Routes,
+				searchDomains,
+				nic.Mtu,
+				dhcpOptions,
+			); err != nil {
+				log.Log.Errorf("failed to run DHCPv6: %v", err)
+				panic(err)
+			}
+		}()
+	}
 
 	return nil
 }
