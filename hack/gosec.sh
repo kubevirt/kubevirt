296cd2cc7cf260e5158caa42e8bb2b3949dec6b9
L. Pivarc <lpivarc@redhat.com>
2020-11-23 14:11:23 +0100

    Allow to run subset of rules for gosec
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- hack/gosec.sh
+++ hack/gosec.sh
@@ -7,4 +7,10 @@ mkdir -p $ARTIFACTS
 echo "Run go sec in pkg"
 cd $KUBEVIRT_DIR/pkg
 
-gosec -sort -quiet -out=${ARTIFACTS}/junit-gosec.xml -exclude-dir=testutils -fmt=junit-xml ./...
+if [ -z $GOSEC ]; then
+    gosec -sort -quiet -out=${ARTIFACTS}/junit-gosec.xml -exclude-dir=testutils -fmt=junit-xml ./...
+else
+    echo "Running subset"
+    echo $GOSEC
+    gosec -include=$GOSEC -sort -quiet -out=${ARTIFACTS}/junit-gosec.xml -exclude-dir=testutils -fmt=junit-xml ./...
+fi
