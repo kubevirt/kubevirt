5e31f3abe4bf7c3f1c484ab3a930ba6488b3e2c8
Roman Mohr <rmohr@redhat.com>
2020-12-02 11:31:09 +0100

    Add a ginkgo matcher library especially for kubevirt
    Make common polling and checks easier to write by providing matchers for
    the following cases:
    
    Wait for single objects or sets of objects to disappear:
    ```go
    Eventually(ThisVMI(vmi), 120).Should(BeGone())
    Eventually(ThisPod(pod), 120).Should(Not(Exist()))
    Eventually(ThisDV(datavolume), 120).Should(BeGone())
    Eventually(AllVMIs("mynamespace"), 120).Should(BeGone())
    ```
    
    Ensuring that objects don't disappear:
    ```go
    Consistently(ThisVMI(vmi), 120).Should(Exist())
    Consistently(ThisPod(pod), 120).Should(BePresent())
    Consistently(ThisDV(datavolume), 120).Should(Exist())
    ```
    
    Checking once if objects exist or disappear:
    ```go
    Expect(ThisVMI(vmi), 120).Should(BeGone())
    Expect(ThisPod(pod), 120).Should(Not(Exist()))
    Expect(ThisPod(pod), 120).Should(BePresent())
    ```
    
    Checking if objects have reached certain phases:
    ```
    Eventually(ThisVMI(vmi), 120).Should(BeInPhase("Running"))
    Eventually(ThisPod(pod), 120).Should(Not(HavePhase("Running")))
    ```
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ tests/framework/matcher/getter.go
@@ -0,0 +1,68 @@
+package matcher
+
+import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	virtv1 "kubevirt.io/client-go/api/v1"
+	"kubevirt.io/client-go/kubecli"
+	"kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
+)
+
+// ThisPod fetches the latest state of the pod. If the object does not exist, nil is returned.
+func ThisPod(pod *v1.Pod) func() (*v1.Pod, error) {
+	return func() (p *v1.Pod, err error) {
+		virtClient, err := kubecli.GetKubevirtClient()
+		if err != nil {
+			return nil, err
+		}
+		p, err = virtClient.CoreV1().Pods(pod.Namespace).Get(pod.Name, k8smetav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return
+	}
+}
+
+// ThisVMI fetches the latest state of the pod. If the object does not exist, nil is returned.
+func ThisVMI(vmi *virtv1.VirtualMachineInstance) func() (*virtv1.VirtualMachineInstance, error) {
+	return func() (p *virtv1.VirtualMachineInstance, err error) {
+		virtClient, err := kubecli.GetKubevirtClient()
+		if err != nil {
+			return nil, err
+		}
+		p, err = virtClient.VirtualMachineInstance(vmi.Namespace).Get(vmi.Name, &k8smetav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return
+	}
+}
+
+// AllVMI fetches the latest state of the pod. If the object does not exist, nil is returned.
+func AllVMIs(namespace string) func() ([]virtv1.VirtualMachineInstance, error) {
+	return func() (p []virtv1.VirtualMachineInstance, err error) {
+		virtClient, err := kubecli.GetKubevirtClient()
+		if err != nil {
+			return nil, err
+		}
+		list, err := virtClient.VirtualMachineInstance(namespace).List(&k8smetav1.ListOptions{})
+		return list.Items, err
+	}
+}
+
+// ThisDV fetches the latest state of the pod. If the object does not exist, nil is returned.
+func ThisDV(dv *v1alpha1.DataVolume) func() (*v1alpha1.DataVolume, error) {
+	return func() (p *v1alpha1.DataVolume, err error) {
+		virtClient, err := kubecli.GetKubevirtClient()
+		if err != nil {
+			return nil, err
+		}
+		p, err = virtClient.CdiClient().CdiV1alpha1().DataVolumes(dv.Namespace).Get(dv.Name, k8smetav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return
+	}
+}
