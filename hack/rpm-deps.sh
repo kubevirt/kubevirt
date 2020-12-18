bbb02f2d7994b5df9cfd1a61fd95172ad81e99f3
Roman Mohr <rmohr@redhat.com>
2020-12-18 10:27:59 +0100

    Add a script to resolve RPM dependencies
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -5,6 +5,10 @@ set -ex
 source hack/common.sh
 source hack/config.sh
 
+LIBVIRT_VERSION=0:6.6.0-8
+SEABIOS_VERSION=0:1.14.0-1
+QEMU_VERSION=15:5.1.0-16
+
 basesystem="glibc-langpack-en coreutils-single libcurl-minimal curl-minimal"
 
 # get latest repo data from repo.yaml
@@ -44,7 +48,35 @@ bazel run \
     iputils \
     e2fsprogs
 
+# create a rpmtree for libvirt-devel. libvirt-devel is needed for compilation and unit-testing.
+bazel run \
+    --config=${ARCHITECTURE} \
+    //:bazeldnf -- rpmtree --public --name libvirt-devel_x86_64 $basesystem libvirt-devel-${LIBVIRT_VERSION}
+
+# create a rpmtree for virt-launcher and virt-handler. This is the OS for our node-components.
+bazel run \
+    --config=${ARCHITECTURE} \
+    //:bazeldnf -- rpmtree --public --name launcherbase_x86_64 \
+    $basesystem \
+    libverto-libev \
+    libvirt-daemon-driver-qemu-${LIBVIRT_VERSION} \
+    libvirt-client-${LIBVIRT_VERSION} \
+    libvirt-daemon-driver-storage-core-${LIBVIRT_VERSION} \
+    qemu-kvm-${QEMU_VERSION} \
+    seabios-${SEABIOS_VERSION} \
+    genisoimage \
+    selinux-policy selinux-policy-targeted \
+    nftables \
+    findutils \
+    iptables
+
 # remove all RPMs which are no longer referenced by a rpmtree
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- prune
+
+# update tar2files targets which act as an adapter between rpms
+# and cc_library which we need for virt-launcher and virt-handler
+bazel run \
+    --config=${ARCHITECTURE} \
+    //rpm:ldd
