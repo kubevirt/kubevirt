2bc4f21c04f7b6a06f956029b387c9f2df0bf520
Roman Mohr <rmohr@redhat.com>
2020-12-28 13:59:34 +0100

    Add a RPM verification target
    This target can be used to verify RPMs in the WORKSPACE in CI and on
    local RPM updates.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ hack/verify-rpm-deps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+source hack/common.sh
+source hack/config.sh
+
+# verify that RPMs with given SHASUMs in WORKSPACE files
+# are signed with known GPG keysin repo.yaml
+bazel run \
+    --config=${ARCHITECTURE} \
+    //:bazeldnf -- verify
