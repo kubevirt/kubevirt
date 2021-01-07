7f8eed2b77993274cc251941a6ba71508e4cae5e
Igor Bezukh <ibezukh@redhat.com>
2021-01-07 20:18:00 +0200

    Tablet input device only exists as virtio 1.0
    Tablet input devices always need to set virtio as model.
    
    Signed-off-by: Igor Bezukh <ibezukh@redhat.com>

--- pkg/virt-launcher/virtwrap/converter/converter_test.go
+++ pkg/virt-launcher/virtwrap/converter/converter_test.go
@@ -558,7 +558,7 @@ var _ = Describe("Converter", func() {
       <driver name="qemu" type="raw" iothread="1"></driver>
       <alias name="ua-serviceaccount_test"></alias>
     </disk>
-      <input type="tablet" bus="virtio" model="virtio-non-transitional">
+      <input type="tablet" bus="virtio" model="virtio">
       <alias name="ua-tablet0"></alias>
     </input>
     <serial type="unix">
@@ -758,7 +758,7 @@ var _ = Describe("Converter", func() {
       <driver name="qemu" type="raw" iothread="1"></driver>
       <alias name="ua-serviceaccount_test"></alias>
     </disk>
-    <input type="tablet" bus="virtio-non-transitional">
+    <input type="tablet" bus="virtio" model="virtio">
       <alias name="ua-tablet0"></alias>
     </input>
     <serial type="unix">
@@ -971,7 +971,7 @@ var _ = Describe("Converter", func() {
       <driver name="qemu" type="raw" iothread="1"></driver>
       <alias name="ua-serviceaccount_test"></alias>
     </disk>
-    <input type="tablet" bus="virtio-non-transitional">
+    <input type="tablet" bus="virtio" model="virtio">
       <alias name="ua-tablet0"></alias>
       <address type="pci" domain="0x0000" bus="0x00" slot="0x07" function="0x0"></address>
     </input>
