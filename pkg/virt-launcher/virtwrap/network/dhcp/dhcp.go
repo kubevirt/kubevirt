4d6be5f6669f410a1711293d07fb049c936b9db2
Alona Kaplan <alkaplan@redhat.com>
2021-01-14 15:19:18 +0200

    virt-launcher: [masquerade] Stop filtering dhcp reuqests by vm MAC
    The dhcp server filters out any packets that are not coming via the
    k6t-eth* interface.
    On masquerade binding, only the VM is connected to this interface, so there
    is no reason to add another filtering.
    
    Removing this redundant filtering is enough to fix bug -
    https://bugzilla.redhat.com/1907988
    
    After migration, unless the VM MAC address was specified in the
    spec, the virt-launcher doesn't know what MAC address the VM has.
    Therefore, the target virt-launcher passed the wrong MAC to the dhcp
    server, which caused filtering out of valid dhcp requests.
    
    NOTE: this commit introducing a fix for masquerade binding only.
    Bridge binding doesn't support migration, so the bug is not relevant for
    bridge binding.
    Filtering by mac is kept on bridge binding since, k6t-eth* is connected to
    both the main pod interface and the VM.
    
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- pkg/virt-launcher/virtwrap/network/dhcp/dhcp.go
+++ pkg/virt-launcher/virtwrap/network/dhcp/dhcp.go
@@ -49,6 +49,7 @@ var searchDomainValidationRegex = regexp.MustCompile(`^(?:[_a-z0-9](?:[_a-z0-9-]
 
 func SingleClientDHCPServer(
 	clientMAC net.HardwareAddr,
+	filterByMAC bool,
 	clientIP net.IP,
 	clientMask net.IPMask,
 	serverIface string,
@@ -75,6 +76,7 @@ func SingleClientDHCPServer(
 	handler := &DHCPHandler{
 		clientIP:      clientIP,
 		clientMAC:     clientMAC,
+		filterByMAC:   filterByMAC,
 		serverIP:      serverIP.To4(),
 		leaseDuration: infiniteLease,
 		options:       options,
@@ -177,15 +179,18 @@ type DHCPHandler struct {
 	serverIP      net.IP
 	clientIP      net.IP
 	clientMAC     net.HardwareAddr
+	filterByMAC   bool
 	leaseDuration time.Duration
 	options       dhcp.Options
 }
 
 func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
 	log.Log.V(4).Info("Serving a new request")
-	if mac := p.CHAddr(); !bytes.Equal(mac, h.clientMAC) {
-		log.Log.V(4).Info("The request is not from our client")
-		return nil // Is not our client
+	if h.filterByMAC {
+		if mac := p.CHAddr(); !bytes.Equal(mac, h.clientMAC) {
+			log.Log.V(4).Info("The request is not from our client")
+			return nil // Is not our client
+		}
 	}
 
 	switch msgType {
