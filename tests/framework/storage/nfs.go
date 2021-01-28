8430989a2326e3c255742e197d9e1d91d0160ae2
David Vossel <davidvossel@gmail.com>
2021-01-28 17:11:03 -0500

    Seed the functest NFS server with a datavolumes contents imported from a container image
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- tests/framework/storage/nfs.go
+++ tests/framework/storage/nfs.go
@@ -12,6 +12,53 @@ import (
 	"kubevirt.io/kubevirt/tests/flags"
 )
 
+func RenderNFSServerWithPVC(generateName string, pvcName string) *k8sv1.Pod {
+	image := fmt.Sprintf("%s/nfs-server:%s", flags.KubeVirtRepoPrefix, flags.KubeVirtVersionTag)
+	resources := k8sv1.ResourceRequirements{}
+	resources.Requests = make(k8sv1.ResourceList)
+	resources.Requests[k8sv1.ResourceMemory] = resource.MustParse("256M")
+	resources.Requests[k8sv1.ResourceCPU] = resource.MustParse("500m")
+	pod := &k8sv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: generateName,
+			Labels: map[string]string{
+				v1.AppLabel: generateName,
+			},
+		},
+		Spec: k8sv1.PodSpec{
+			RestartPolicy: k8sv1.RestartPolicyNever,
+			Volumes: []k8sv1.Volume{
+				{
+					Name: "nfsdata",
+					VolumeSource: k8sv1.VolumeSource{
+						PersistentVolumeClaim: &k8sv1.PersistentVolumeClaimVolumeSource{
+							ClaimName: pvcName,
+						},
+					},
+				},
+			},
+			Containers: []k8sv1.Container{
+				{
+					Name:            generateName,
+					Image:           image,
+					ImagePullPolicy: k8sv1.PullAlways,
+					Resources:       resources,
+					SecurityContext: &k8sv1.SecurityContext{
+						Privileged: pointer.BoolPtr(true),
+					},
+					VolumeMounts: []k8sv1.VolumeMount{
+						{
+							Name:      "nfsdata",
+							MountPath: "/data/nfs",
+						},
+					},
+				},
+			},
+		},
+	}
+	return pod
+}
+
 func RenderNFSServer(generateName string, hostPath string) *k8sv1.Pod {
 	image := fmt.Sprintf("%s/nfs-server:%s", flags.KubeVirtRepoPrefix, flags.KubeVirtVersionTag)
 	resources := k8sv1.ResourceRequirements{}
