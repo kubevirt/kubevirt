efb7a68ae82eeb3cc3211e0e638b292643df3b9a
Or Shoval <oshoval@redhat.com>
2021-02-11 16:42:29 +0200

    Revert "Wait for cloud init done"
    This reverts commit 264dc35eafa7bb346e1c146dfbd84442c589f074.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/libvmi/factory.go
+++ tests/libvmi/factory.go
@@ -49,7 +49,6 @@ func NewSriovFedora(opts ...Option) *kvirtv1.VirtualMachineInstance {
 // options.
 func newFedora(containerDisk cd.ContainerDisk, opts ...Option) *kvirtv1.VirtualMachineInstance {
 	configurePassword := `#!/bin/bash
-	rm -f /var/lib/cloud/instance/boot-finished
 	echo "fedora" |passwd fedora --stdin
 	echo `
 
