5a904f901318f9f9fcec94a68bc9b7fca2b16685
Quique Llorente <ellorent@redhat.com>
2021-02-12 15:25:28 +0100

    Call networking phase2 setup at dom recreate
    When libvirt fails starting at a domain the domain is created but state
    is down then kubevirt will call SyncVMI again and the sidecard hooks
    being called but not the network configuration phase2 setup that
    decorate the domxml config with stuff like the tap device and friends.
    This change just add the call to phase2 at the proper place.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- pkg/virt-launcher/virtwrap/manager.go
+++ pkg/virt-launcher/virtwrap/manager.go
@@ -1339,6 +1339,10 @@ func (l *LibvirtDomainManager) SyncVMI(vmi *v1.VirtualMachineInstance, useEmulat
 	// To make sure, that we set the right qemu wrapper arguments,
 	// we update the domain XML whenever a VirtualMachineInstance was already defined but not running
 	if !newDomain && cli.IsDown(domState) {
+		err = network.SetupPodNetworkPhase2(vmi, domain)
+		if err != nil {
+			return nil, fmt.Errorf("preparing the pod network failed: %v", err)
+		}
 		dom, err = l.setDomainSpecWithHooks(vmi, &domain.Spec)
 		if err != nil {
 			return nil, err
