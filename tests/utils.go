1aad4132ff1490c6b57dc4beab6776392d38182b
L. Pivarc <lpivarc@redhat.com>
2021-02-19 22:32:44 +0100

    Force NonRoot to be tested

--- tests/utils.go
+++ tests/utils.go
@@ -766,6 +766,7 @@ func AdjustKubeVirtResource() {
 		virtconfig.HostDiskGate,
 		virtconfig.VirtIOFSGate,
 		virtconfig.HotplugVolumesGate,
+		virtconfig.NonRoot,
 	}
 	kv.Spec.Configuration.SELinuxLauncherType = "virt_launcher.process"
 
