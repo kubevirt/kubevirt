5107164e47205662ed7a3d996febfb9943690b6e
Or Shoval <oshoval@redhat.com>
2021-02-22 16:41:21 +0200

    sriov, Fix race condition between LoginToFedora and cloud init
    VM password is set as part of the Cloud-init UserData.
    End 2 end tests use LoginToFedora, which sometimes tries to login before
    Cloud-init finished setting the password.
    This leads to a race and breaks the sync that the console
    expecter expects (the login prompt is not idempotent when it happens).
    
    In order to fix the race and keep the code as simple as possible,
    this PR does the following:
    1. Use a VM without Guest Agent enabled module.
    2. Enable the Guest Agent as the last step of Cloud-init.
    3. Wait for Guest Agent Connected event - means that the Cloud-init UserData is done,
    and since the Guest Agent enable was the last thing, it means the password is also set,
    because setting the password was part of the Cloud-init UserData.
    4. Login to Fedora and configure VM upon needs.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/libvmi/factory.go
+++ tests/libvmi/factory.go
@@ -34,30 +34,48 @@ const (
 // NewFedora instantiates a new Fedora based VMI configuration,
 // building its extra properties based on the specified With* options.
 func NewFedora(opts ...Option) *kvirtv1.VirtualMachineInstance {
-	return newFedora(cd.ContainerDiskFedora, opts...)
+	userData := `#!/bin/bash
+	echo "fedora" | passwd fedora --stdin
+	echo `
+
+	return newFedora(cd.ContainerDiskFedora, userData, opts...)
 }
 
 // NewSriovFedora instantiates a new Fedora based VMI configuration,
 // building its extra properties based on the specified With* options, the
 // image used include Guest Agent and some moduled needed by SRIOV.
 func NewSriovFedora(opts ...Option) *kvirtv1.VirtualMachineInstance {
-	return newFedora(cd.ContainerDiskFedoraSRIOVLane, opts...)
+	userData := `#!/bin/bash
+	setenforce 0
+	echo "fedora" | passwd fedora --stdin`
+
+	userData = addStartGuestAgentUserDataSuffix(userData)
+	return newFedora(cd.ContainerDiskFedoraSRIOVLane, userData, opts...)
+}
+
+// Start Guest agent must be that last operation of UserData.
+// This will allows to wait for Agent connected event and to know
+// that applying UserData is finished, and that a login can proceed.
+func addStartGuestAgentUserDataSuffix(userData string) string {
+	userData += `
+	cp /home/fedora/qemu-guest-agent.service /lib/systemd/system/
+	systemctl daemon-reload
+	systemctl start qemu-guest-agent
+	systemctl enable qemu-guest-agent`
+
+	return userData
 }
 
 // NewFedora instantiates a new Fedora based VMI configuration with specified
 // containerDisk, building its extra properties based on the specified With*
 // options.
-func newFedora(containerDisk cd.ContainerDisk, opts ...Option) *kvirtv1.VirtualMachineInstance {
-	configurePassword := `#!/bin/bash
-	echo "fedora" |passwd fedora --stdin
-	echo `
-
+func newFedora(containerDisk cd.ContainerDisk, userData string, opts ...Option) *kvirtv1.VirtualMachineInstance {
 	fedoraOptions := []Option{
 		WithTerminationGracePeriod(DefaultTestGracePeriod),
 		WithResourceMemory("512M"),
 		WithRng(),
 		WithContainerImage(cd.ContainerDiskFor(containerDisk)),
-		WithCloudInitNoCloudUserData(configurePassword, false),
+		WithCloudInitNoCloudUserData(userData, false),
 	}
 	opts = append(fedoraOptions, opts...)
 	return New(RandName(DefaultVmiName), opts...)
--- tests/vmi_multus_test.go
+++ tests/vmi_multus_test.go
@@ -660,8 +660,11 @@ var _ = Describe("[Serial]SRIOV", func() {
 			vmi, err := virtClient.VirtualMachineInstance(vmi.Namespace).Get(vmi.Name, &metav1.GetOptions{})
 			Expect(err).ToNot(HaveOccurred())
 
-			tests.WaitUntilVMIReady(vmi, libnet.WithIPv6(console.LoginToFedora))
+			tests.WaitUntilVMIReady(vmi, nil)
 			tests.WaitAgentConnected(virtClient, vmi)
+			By("Login and configurating VM via console")
+			Expect(libnet.WithIPv6(console.LoginToFedora)(vmi)).To(Succeed())
+
 			return vmi
 		}
 
