7d88b5e0e468832544a1bff6c88e586a9c378875
Edward Haas <edwardh@redhat.com>
2021-02-28 11:19:44 +0200

    pkg, cleanup: Remove unused parameters from rbac.GetAllCluster
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- pkg/virt-operator/kubevirt_test.go
+++ pkg/virt-operator/kubevirt_test.go
@@ -835,7 +835,7 @@ var _ = Describe("KubeVirt Operator", func() {
 		all := make([]interface{}, 0)
 
 		// rbac
-		all = append(all, rbac.GetAllCluster(NAMESPACE)...)
+		all = append(all, rbac.GetAllCluster()...)
 		all = append(all, rbac.GetAllApiServer(NAMESPACE)...)
 		all = append(all, rbac.GetAllHandler(NAMESPACE)...)
 		all = append(all, rbac.GetAllController(NAMESPACE)...)
