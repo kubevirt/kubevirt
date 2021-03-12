8aa3267af4ea0e75526798ed3a0172e1f2ae5f5d
Federico Gimenez <fgimenez@redhat.com>
2021-03-12 09:22:06 +0100

    Remove quarantine label from test_id:3145 and add release-blocker label
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- tests/operator_test.go
+++ tests/operator_test.go
@@ -1021,7 +1021,7 @@ spec:
 		// running a VM/VMI using that previous release
 		// Updating KubeVirt to the target tested code
 		// Ensuring VM/VMI is still operational after the update from previous release.
-		It("[QUARANTINE][owner:@sig-compute][test_id:3145]from previous release to target tested release", func() {
+		It("[release-blocker][owner:@sig-compute][test_id:3145]from previous release to target tested release", func() {
 			if !tests.HasCDI() {
 				Skip("Skip Update test when CDI is not present")
 			}
