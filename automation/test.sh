5c329c07e4dc58a4e69aa30da35654fd089ccec5
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2021-03-15 15:21:08 +0200

    Allow pre-setting the KUBEVIRT_E2E_FOCUS env var in tests
    In order to ease lane filtering, the `automation/test.sh` scrip will not compute the `KUBEVIRT_E2E_FOCUS` and the `KUBEVIRT_E2E_SKIP` environment variables according to the TARGET variable, if the `KUBEVIRT_E2E_FOCUS` variable is already set.
    
    That allow setting this variable from the test configuration.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -281,24 +281,30 @@ spec:
     path: /
   storageClassName: windows
 EOF
-  # Run only Windows tests
-  export KUBEVIRT_E2E_FOCUS=Windows
-elif [[ $TARGET =~ (cnao|multus) ]]; then
-  export KUBEVIRT_E2E_FOCUS="Multus|Networking|VMIlifecycle|Expose|Macvtap"
-elif [[ $TARGET =~ sig-network ]]; then
-  export KUBEVIRT_E2E_FOCUS="\\[sig-network\\]"
-elif [[ $TARGET =~ sriov.* ]]; then
-  export KUBEVIRT_E2E_FOCUS=SRIOV
-elif [[ $TARGET =~ gpu.* ]]; then
-  export KUBEVIRT_E2E_FOCUS=GPU
-elif [[ $TARGET =~ (okd|ocp).* ]]; then
-  export KUBEVIRT_E2E_SKIP="SRIOV|GPU"
-else
-  export KUBEVIRT_E2E_SKIP="Multus|SRIOV|GPU|Macvtap"
 fi
 
-if [[ "$KUBEVIRT_STORAGE" == "rook-ceph" ]]; then
-  export KUBEVIRT_E2E_FOCUS=rook-ceph
+# Set KUBEVIRT_E2E_FOCUS and KUBEVIRT_E2E_SKIP only if both of them are not already set
+if [[ -z ${KUBEVIRT_E2E_FOCUS} && -z ${KUBEVIRT_E2E_SKIP} ]]; then
+  if [[ $TARGET =~ windows.* ]]; then
+    # Run only Windows tests
+    export KUBEVIRT_E2E_FOCUS=Windows
+  elif [[ $TARGET =~ (cnao|multus) ]]; then
+    export KUBEVIRT_E2E_FOCUS="Multus|Networking|VMIlifecycle|Expose|Macvtap"
+  elif [[ $TARGET =~ sig-network ]]; then
+    export KUBEVIRT_E2E_FOCUS="\\[sig-network\\]"
+  elif [[ $TARGET =~ sriov.* ]]; then
+    export KUBEVIRT_E2E_FOCUS=SRIOV
+  elif [[ $TARGET =~ gpu.* ]]; then
+    export KUBEVIRT_E2E_FOCUS=GPU
+  elif [[ $TARGET =~ (okd|ocp).* ]]; then
+    export KUBEVIRT_E2E_SKIP="SRIOV|GPU"
+  else
+    export KUBEVIRT_E2E_SKIP="Multus|SRIOV|GPU|Macvtap"
+  fi
+
+  if [[ "$KUBEVIRT_STORAGE" == "rook-ceph" ]]; then
+    export KUBEVIRT_E2E_FOCUS=rook-ceph
+  fi
 fi
 
 # If KUBEVIRT_QUARANTINE is not set, do not run quarantined tests. When it is
