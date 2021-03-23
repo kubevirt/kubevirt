d28f07e6d1dbe4d6e906614116f83d449afa099f
Federico Gimenez <fgimenez@redhat.com>
2021-03-23 14:06:35 +0100

    Quarantine flaky tests
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- tests/infra_test.go
+++ tests/infra_test.go
@@ -177,7 +177,7 @@ var _ = Describe("[Serial]Infrastructure", func() {
 			Expect(console.LoginToAlpine(vmi)).To(Succeed())
 		})
 
-		It("[test_id:4100] should be valid during the whole rotation process", func() {
+		It("[QUARANTINE][owner:@sig-compute][test_id:4100] should be valid during the whole rotation process", func() {
 			oldAPICert := tests.EnsurePodsCertIsSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-api"), flags.KubeVirtInstallNamespace, "8443")
 			oldHandlerCert := tests.EnsurePodsCertIsSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-handler"), flags.KubeVirtInstallNamespace, "8186")
 			Expect(err).ToNot(HaveOccurred())
