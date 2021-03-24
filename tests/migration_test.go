75ff3e231b02679fb905de47e44141c97916eee1
Federico Gimenez <fgimenez@redhat.com>
2021-03-24 12:31:39 +0100

    Add test_id:2226 to the batch
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- tests/migration_test.go
+++ tests/migration_test.go
@@ -1535,7 +1535,7 @@ var _ = Describe("[Serial][rfe_id:393][crit:high][vendor:cnv-qe@redhat.com][leve
 					By("Waiting for VMI to disappear")
 					tests.WaitForVirtualMachineToDisappearWithTimeout(vmi, 240)
 				},
-					table.Entry("[test_id:2226] with ContainerDisk", newVirtualMachineInstanceWithFedoraContainerDisk),
+					table.Entry("[QUARANTINE][owner:@sig-storage][test_id:2226] with ContainerDisk", newVirtualMachineInstanceWithFedoraContainerDisk),
 					table.Entry("[QUARANTINE][owner:@sig-storage][owner:@sig-compute][test_id:2731] with OCS Disk (using ISCSI IPv4 address)", newVirtualMachineInstanceWithFedoraOCSDisk),
 				)
 				It("[QUARANTINE][owner:@sig-compute][test_id:3241]should be able to cancel a migration right after posting it", func() {
