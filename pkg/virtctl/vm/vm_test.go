47b15b4d5beb4e7752747526b682b418d3f99ba3
Alexander Wels <awels@redhat.com>
2021-03-26 15:14:56 -0400

    Added unit tests
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/virtctl/vm/vm_test.go
+++ pkg/virtctl/vm/vm_test.go
@@ -1,15 +1,25 @@
 package vm_test
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
 	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/kubecli"
+	"kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
 	"kubevirt.io/kubevirt/tests"
+
+	"k8s.io/client-go/kubernetes/fake"
+
+	cdifake "kubevirt.io/client-go/generated/containerized-data-importer/clientset/versioned/fake"
 )
 
 var _ = Describe("VirtualMachine", func() {
@@ -270,6 +280,168 @@ var _ = Describe("VirtualMachine", func() {
 
 			cmd := tests.NewVirtctlCommand("userlist", vm.Name)
 			Expect(cmd.Execute()).To(BeNil())
+		})
+	})
+
+	Context("hotplug volume", func() {
+		var (
+			cdiClient  *cdifake.Clientset
+			coreClient *fake.Clientset
+		)
+		createTestDataVolume := func() *v1alpha1.DataVolume {
+			return &v1alpha1.DataVolume{
+				ObjectMeta: k8smetav1.ObjectMeta{
+					Name: "testvolume",
+				},
+			}
+		}
+
+		createTestPVC := func() *corev1.PersistentVolumeClaim {
+			return &corev1.PersistentVolumeClaim{
+				ObjectMeta: k8smetav1.ObjectMeta{
+					Name: "testvolume",
+				},
+			}
+		}
+
+		verifyVolumeSource := func(volumeOptions interface{}, useDv bool) {
+			if useDv {
+				Expect(volumeOptions.(*v1.AddVolumeOptions).VolumeSource.DataVolume).ToNot(BeNil())
+				Expect(volumeOptions.(*v1.AddVolumeOptions).VolumeSource.PersistentVolumeClaim).To(BeNil())
+			} else {
+				Expect(volumeOptions.(*v1.AddVolumeOptions).VolumeSource.DataVolume).To(BeNil())
+				Expect(volumeOptions.(*v1.AddVolumeOptions).VolumeSource.PersistentVolumeClaim).ToNot(BeNil())
+			}
+		}
+
+		expectVMIEndpointAddVolume := func(vmiName, volumeName string, useDv bool) {
+			kubecli.MockKubevirtClientInstance.
+				EXPECT().
+				VirtualMachineInstance(k8smetav1.NamespaceDefault).
+				Return(vmiInterface).
+				Times(1)
+			vmiInterface.EXPECT().AddVolume(vmiName, gomock.Any()).DoAndReturn(func(arg0, arg1 interface{}) interface{} {
+				Expect(arg1.(*v1.AddVolumeOptions).Name).To(Equal(volumeName))
+				verifyVolumeSource(arg1, useDv)
+				return nil
+			})
+		}
+
+		expectVMIEndpointRemoveVolume := func(vmiName, volumeName string, useDv bool) {
+			kubecli.MockKubevirtClientInstance.
+				EXPECT().
+				VirtualMachineInstance(k8smetav1.NamespaceDefault).
+				Return(vmiInterface).
+				Times(1)
+			vmiInterface.EXPECT().RemoveVolume(vmiName, gomock.Any()).DoAndReturn(func(arg0, arg1 interface{}) interface{} {
+				Expect(arg1.(*v1.RemoveVolumeOptions).Name).To(Equal(volumeName))
+				return nil
+			})
+		}
+
+		expectVMIEndpointRemoveVolumeError := func(vmiName, volumeName string) {
+			kubecli.MockKubevirtClientInstance.
+				EXPECT().
+				VirtualMachineInstance(k8smetav1.NamespaceDefault).
+				Return(vmiInterface).
+				Times(1)
+			vmiInterface.EXPECT().RemoveVolume(vmiName, gomock.Any()).DoAndReturn(func(arg0, arg1 interface{}) interface{} {
+				Expect(arg1.(*v1.RemoveVolumeOptions).Name).To(Equal(volumeName))
+				return fmt.Errorf("error removing")
+			})
+		}
+
+		expectVMEndpointAddVolume := func(vmiName, volumeName string, useDv bool) {
+			kubecli.MockKubevirtClientInstance.
+				EXPECT().
+				VirtualMachine(k8smetav1.NamespaceDefault).
+				Return(vmInterface).
+				Times(1)
+			vmInterface.EXPECT().AddVolume(vmiName, gomock.Any()).DoAndReturn(func(arg0, arg1 interface{}) interface{} {
+				Expect(arg1.(*v1.AddVolumeOptions).Name).To(Equal(volumeName))
+				verifyVolumeSource(arg1, useDv)
+				return nil
+			})
+		}
+
+		expectVMEndpointRemoveVolume := func(vmiName, volumeName string, useDv bool) {
+			kubecli.MockKubevirtClientInstance.
+				EXPECT().
+				VirtualMachine(k8smetav1.NamespaceDefault).
+				Return(vmInterface).
+				Times(1)
+			vmInterface.EXPECT().RemoveVolume(vmiName, gomock.Any()).DoAndReturn(func(arg0, arg1 interface{}) interface{} {
+				Expect(arg1.(*v1.RemoveVolumeOptions).Name).To(Equal(volumeName))
+				return nil
+			})
+		}
+
+		BeforeEach(func() {
+			cdiClient = cdifake.NewSimpleClientset()
+			coreClient = fake.NewSimpleClientset()
+		})
+
+		table.DescribeTable("should fail with missing required or invalid parameters", func(commandName, errorString string, args ...string) {
+			commandAndArgs := make([]string, 0)
+			commandAndArgs = append(commandAndArgs, commandName)
+			commandAndArgs = append(commandAndArgs, args...)
+			cmdAdd := tests.NewRepeatableVirtctlCommand(commandAndArgs...)
+			res := cmdAdd()
+			Expect(res).NotTo(BeNil())
+			Expect(res.Error()).To(ContainSubstring(errorString))
+		},
+			table.Entry("addvolume no args", "addvolume", "argument validation failed"),
+			table.Entry("addvolume name, missing required volume-name", "addvolume", "required flag(s)", "testvmi"),
+			table.Entry("addvolume name, invalid extra parameter", "addvolume", "unknown flag", "testvmi", "--volume-name=blah", "--invalid=test"),
+			table.Entry("removevolume no args", "removevolume", "argument validation failed"),
+			table.Entry("removevolume name, missing required volume-name", "removevolume", "required flag(s)", "testvmi"),
+			table.Entry("removevolume name, invalid extra parameter", "removevolume", "unknown flag", "testvmi", "--volume-name=blah", "--invalid=test"),
+		)
+
+		It("addvolume should fail when no source is found", func() {
+			kubecli.MockKubevirtClientInstance.EXPECT().CdiClient().Return(cdiClient)
+			kubecli.MockKubevirtClientInstance.EXPECT().CoreV1().Return(coreClient.CoreV1())
+			cmd := tests.NewVirtctlCommand("addvolume", "testvmi", "--volume-name=testvolume")
+			res := cmd.Execute()
+			Expect(res).ToNot(BeNil())
+			Expect(res.Error()).To(ContainSubstring("Volume testvolume is not a DataVolume or PersistentVolumeClaim"))
+		})
+
+		table.DescribeTable("should call correct endpoint", func(commandName, vmiName, volumeName string, useDv bool, expectFunc func(vmiName, volumeName string, useDv bool), args ...string) {
+			if commandName == "addvolume" {
+				kubecli.MockKubevirtClientInstance.EXPECT().CdiClient().Return(cdiClient)
+				if useDv {
+					cdiClient.CdiV1alpha1().DataVolumes(k8smetav1.NamespaceDefault).Create(context.Background(), createTestDataVolume(), k8smetav1.CreateOptions{})
+				} else {
+					kubecli.MockKubevirtClientInstance.EXPECT().CoreV1().Return(coreClient.CoreV1())
+					coreClient.CoreV1().PersistentVolumeClaims(k8smetav1.NamespaceDefault).Create(context.Background(), createTestPVC(), k8smetav1.CreateOptions{})
+				}
+			}
+			expectFunc(vmiName, volumeName, useDv)
+			commandAndArgs := make([]string, 0)
+			commandAndArgs = append(commandAndArgs, commandName)
+			commandAndArgs = append(commandAndArgs, vmiName)
+			commandAndArgs = append(commandAndArgs, fmt.Sprintf("--volume-name=%s", volumeName))
+			commandAndArgs = append(commandAndArgs, args...)
+			cmd := tests.NewVirtctlCommand(commandAndArgs...)
+			Expect(cmd.Execute()).To(BeNil())
+		},
+			table.Entry("addvolume dv, no persist shoud call VMI endpoint", "addvolume", "testvmi", "testvolume", true, expectVMIEndpointAddVolume),
+			table.Entry("addvolume pvc, no persist shoud call VMI endpoint", "addvolume", "testvmi", "testvolume", false, expectVMIEndpointAddVolume),
+			table.Entry("addvolume dv, with persist shoud call VM endpoint", "addvolume", "testvmi", "testvolume", true, expectVMEndpointAddVolume, "--persist"),
+			table.Entry("addvolume pvc, with persist shoud call VM endpoint", "addvolume", "testvmi", "testvolume", false, expectVMEndpointAddVolume, "--persist"),
+			table.Entry("removevolume dv, no persist shoud call VMI endpoint", "removevolume", "testvmi", "testvolume", true, expectVMIEndpointRemoveVolume),
+			table.Entry("removevolume pvc, no persist shoud call VMI endpoint", "removevolume", "testvmi", "testvolume", false, expectVMIEndpointRemoveVolume),
+			table.Entry("removevolume dv, with persist shoud call VM endpoint", "removevolume", "testvmi", "testvolume", true, expectVMEndpointRemoveVolume, "--persist"),
+			table.Entry("removevolume pvc, with persist shoud call VM endpoint", "removevolume", "testvmi", "testvolume", false, expectVMEndpointRemoveVolume, "--persist"),
+		)
+
+		It("removevolume should report error from call", func() {
+			expectVMIEndpointRemoveVolumeError("testvmi", "testvolume")
+			cmd := tests.NewVirtctlCommand("removevolume", "testvmi", "--volume-name=testvolume")
+			res := cmd.Execute()
+			Expect(res).ToNot(BeNil())
+			Expect(res.Error()).To(ContainSubstring("error removing"))
 		})
 	})
 
