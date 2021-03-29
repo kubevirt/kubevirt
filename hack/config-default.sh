3c306f03ed6204737e3656cdf9e7ae91b88571de
Or Mergi <ormergi@redhat.com>
2021-03-29 11:18:45 +0300

    bump kubevirtci 2103261553-31913e9
    Bump kubevirtci to 2103261553-31913e9 tag,
    includes the following changes:
    
    31913e9 (tag: 2103261553-31913e9, upstream/master) Run bazelisk run
    //robots/cmd/kubevirtci-bumper:kubevirtci-bumper -- -ensure-last-three-minor-of v1 --k8s-provider-dir /home/prow/go/src/github.com/kubevirt/project-infra/../kubevirtci/cluster-provision/k8s
    (kubevirt/kubevirtci#573)
    ca50a75 (tag: 2103252139-ca50a75) SRIOV Provider use bare sriov components instead of sriov-network-operator
    (kubevirt/kubevirtci#563)
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- hack/config-default.sh
+++ hack/config-default.sh
@@ -11,7 +11,7 @@ cdi_namespace=cdi
 image_pull_policy=${IMAGE_PULL_POLICY:-IfNotPresent}
 verbosity=${VERBOSITY:-2}
 package_name=${PACKAGE_NAME:-kubevirt-dev}
-kubevirtci_git_hash="2103250939-432da7e"
+kubevirtci_git_hash="2103261553-31913e9"
 
 # try to derive csv_version from docker tag. But it must start with x.y.z, without leading v
 default_csv_version="${docker_tag/latest/0.0.0}"
