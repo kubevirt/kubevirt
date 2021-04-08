896c68a1963aeb1e729f27b87b542c3fa0b32395
Igor Bezukh <ibezukh@redhat.com>
2021-04-08 10:40:31 +0300

    taking test_id:5268 out of quarantine
    Signed-off-by: Igor Bezukh <ibezukh@redhat.com>

--- tests/vmi_configuration_test.go
+++ tests/vmi_configuration_test.go
@@ -1431,7 +1431,7 @@ var _ = Describe("[owner:@sig-compute]Configurations", func() {
 
 		Context("with Clock and timezone", func() {
 
-			It("[QUARANTINE][owner:@sig-compute][test_id:5268]guest should see timezone", func() {
+			It("[owner:@sig-compute][test_id:5268]guest should see timezone", func() {
 				vmi := tests.NewRandomVMIWithEphemeralDiskAndUserdata(cd.ContainerDiskFor(cd.ContainerDiskCirros), "#!/bin/bash\necho 'hello'\n")
 				timezone := "America/New_York"
 				tz := v1.ClockOffsetTimezone(timezone)
