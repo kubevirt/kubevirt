aa345d185f8c85519980e966a99582ff87981a8c
Karel Šimon <ksimon@redhat.com>
2021-04-12 10:26:44 +0200

    remove test_id:3194 and test_id:3193 from quarantine
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- tests/storage/datavolume.go
+++ tests/storage/datavolume.go
@@ -729,8 +729,8 @@ var _ = SIGDescribe("[Serial]DataVolume Integration", func() {
 				createdVirtualMachine = tests.StartVirtualMachine(createdVirtualMachine)
 				createdVirtualMachine = tests.StopVirtualMachine(createdVirtualMachine)
 			},
-				table.Entry("[QUARANTINE][test_id:3193]with explicit role", explicitCloneRole, false, false),
-				table.Entry("[QUARANTINE][test_id:3194]with implicit role", implicitCloneRole, false, false),
+				table.Entry("[test_id:3193]with explicit role", explicitCloneRole, false, false),
+				table.Entry("[test_id:3194]with implicit role", implicitCloneRole, false, false),
 				table.Entry("[test_id:5253]with explicit role (all namespaces)", explicitCloneRole, true, false),
 				table.Entry("[test_id:5254]with explicit role (one namespace)", explicitCloneRole, false, true),
 			)
