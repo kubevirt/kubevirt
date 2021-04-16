093f857cd240719ef6637833f8a8f1c15a8ec87f
Ashley Schuett <aschuett@redhat.com>
2021-04-16 16:32:37 +0200

    add hash to kubevirt objects during testing
    Signed-off-by: Ashley Schuett <aschuett@redhat.com>

--- pkg/virt-operator/kubevirt_test.go
+++ pkg/virt-operator/kubevirt_test.go
@@ -836,11 +836,7 @@ var _ = Describe("KubeVirt Operator", func() {
 			},
 		})
 		for _, obj := range all {
-			if resource, ok := obj.(runtime.Object); ok {
-				addResource(resource, config, nil)
-			} else {
-				Fail("could not cast to runtime.Object")
-			}
+			addResource(obj, config, nil)
 		}
 		return len(all)
 	}
@@ -965,6 +961,15 @@ var _ = Describe("KubeVirt Operator", func() {
 		}
 
 		for _, obj := range all {
+			m := obj.(metav1.Object)
+			a := m.GetAnnotations()
+			if len(a) == 0 {
+				a = map[string]string{}
+			}
+
+			a[v1.KubeVirtCustomizeComponentAnnotationHash] = c.Hash()
+			m.SetAnnotations(a)
+
 			addResource(obj, config, kv)
 		}
 	}
