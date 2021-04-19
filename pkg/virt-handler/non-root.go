5c3e910c5941435ead6b24a13a7a3fec3dbd91fd
L. Pivarc <lpivarc@redhat.com>
2021-04-19 14:30:34 +0200

    Change ownership of host disk
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- pkg/virt-handler/non-root.go
+++ pkg/virt-handler/non-root.go
@@ -1,11 +1,16 @@
 package virthandler
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
 	"path/filepath"
 
 	v1 "kubevirt.io/client-go/api/v1"
 	diskutils "kubevirt.io/kubevirt/pkg/ephemeral-disk-utils"
+	hostdisk "kubevirt.io/kubevirt/pkg/host-disk"
 	"kubevirt.io/kubevirt/pkg/virt-handler/isolation"
+	"kubevirt.io/kubevirt/pkg/virt-handler/selinux"
 )
 
 func changeOwnershipOfBlockDevices(vmiWithOnlyBlockPVCs *v1.VirtualMachineInstance, res isolation.IsolationResult) error {
@@ -20,11 +25,74 @@ func changeOwnershipOfBlockDevices(vmiWithOnlyBlockPVCs *v1.VirtualMachineInstan
 	return nil
 }
 
-func (d *VirtualMachineController) prepareStorage(vmiWithOnlyBlockPVCS, vmiWithAllPVCs *v1.VirtualMachineInstance) error {
-	res, err := d.podIsolationDetector.Detect(vmiWithOnlyBlockPVCS)
+func changeOwnershipAndRelabel(path string) error {
+	err := diskutils.DefaultOwnershipManager.SetFileOwnership(path)
 	if err != nil {
 		return err
 	}
 
-	return changeOwnershipOfBlockDevices(vmiWithOnlyBlockPVCS, res)
+	_, selinuxEnabled, err := selinux.NewSELinux()
+	if err == nil && selinuxEnabled {
+		unprivilegedContainerSELinuxLabel := "system_u:object_r:container_file_t:s0"
+		err = relabelFiles(unprivilegedContainerSELinuxLabel, filepath.Join(path))
+		if err != nil {
+			return (fmt.Errorf("error relabeling %s: %v", path, err))
+		}
+
+	}
+	return err
+}
+
+func changeOwnershipOfHostDisks(vmiWithAllPVCs *v1.VirtualMachineInstance, res isolation.IsolationResult) error {
+	for i := range vmiWithAllPVCs.Spec.Volumes {
+		if volumeSource := &vmiWithAllPVCs.Spec.Volumes[i].VolumeSource; volumeSource.HostDisk != nil {
+			volumeName := vmiWithAllPVCs.Spec.Volumes[i].Name
+			diskPath := hostdisk.GetMountedHostDiskPath(volumeName, volumeSource.HostDisk.Path)
+
+			_, err := os.Stat(diskPath)
+			if err != nil {
+				if os.IsNotExist(err) {
+					diskDir := hostdisk.GetMountedHostDiskDir(volumeName)
+					if err := changeOwnershipAndRelabel(filepath.Join(res.MountRoot(), diskDir)); err != nil {
+						return fmt.Errorf("Failed to change ownership of HostDisk dir %s, %s", volumeName, err)
+					}
+					continue
+				}
+				return fmt.Errorf("Failed to recognize if hostdisk contains image, %s", err)
+			}
+
+			err = changeOwnershipAndRelabel(filepath.Join(res.MountRoot(), diskPath))
+			if err != nil {
+				return fmt.Errorf("Failed to change ownership of HostDisk image: %s", err)
+			}
+
+		}
+	}
+	return nil
+}
+
+func (d *VirtualMachineController) prepareStorage(vmiWithOnlyBlockPVCS, vmiWithAllPVCs *v1.VirtualMachineInstance) error {
+	res, err := d.podIsolationDetector.Detect(vmiWithAllPVCs)
+	if err != nil {
+		return err
+	}
+	if err := changeOwnershipOfBlockDevices(vmiWithOnlyBlockPVCS, res); err != nil {
+		return err
+	}
+	return changeOwnershipOfHostDisks(vmiWithAllPVCs, res)
+}
+
+func relabelFiles(newLabel string, files ...string) error {
+	relabelArgs := []string{"selinux", "relabel", newLabel}
+	for _, file := range files {
+		cmd := exec.Command("virt-chroot", append(relabelArgs, file)...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("error relabeling file %s with label %s. Reason: %v", file, newLabel, err)
+		}
+	}
+
+	return nil
 }
