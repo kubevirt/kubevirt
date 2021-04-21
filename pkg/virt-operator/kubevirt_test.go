03314d8e60c159019f9ecaa9adf632fa3b3b4b0d
Ashley Schuett <aschuett@redhat.com>
2021-04-21 10:44:35 +0200

    refactor reconcile certificate secret
    Signed-off-by: Ashley Schuett <aschuett@redhat.com>

--- pkg/virt-operator/kubevirt_test.go
+++ pkg/virt-operator/kubevirt_test.go
@@ -965,6 +965,15 @@ var _ = Describe("KubeVirt Operator", func() {
 		}
 
 		for _, obj := range all {
+			m := obj.(metav1.Object)
+			a := m.GetAnnotations()
+			if len(a) == 0 {
+				a = map[string]string{}
+			}
+
+			a[v1.KubeVirtCustomizeComponentAnnotationHash] = c.Hash()
+			m.SetAnnotations(a)
+
 			addResource(obj, config, kv)
 		}
 	}
