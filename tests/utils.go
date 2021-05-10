5b1eb73d4d33adf556a8189fbcdfa99a5b0a4e97
Maya Rashish <mrashish@redhat.com>
2021-05-10 14:40:39 +0300

    Add the option to HTTP import a Fedora image.
    Signed-off-by: Maya Rashish <mrashish@redhat.com>

--- tests/utils.go
+++ tests/utils.go
@@ -127,6 +127,7 @@ const (
 	AlpineHttpUrl = iota
 	DummyFileHttpUrl
 	CirrosHttpUrl
+	FedoraHttpUrl
 	VirtWhatCpuidHelperHttpUrl
 )
 
@@ -4763,6 +4764,8 @@ func GetUrl(urlIndex int) string {
 		str = fmt.Sprintf("http://cdi-http-import-server.%s/dummy.file", flags.KubeVirtInstallNamespace)
 	case CirrosHttpUrl:
 		str = fmt.Sprintf("http://cdi-http-import-server.%s/images/cirros.img", flags.KubeVirtInstallNamespace)
+	case FedoraHttpUrl:
+		str = fmt.Sprintf("http://cdi-http-import-server.%s/images/fedora.img", flags.KubeVirtInstallNamespace)
 	case VirtWhatCpuidHelperHttpUrl:
 		str = fmt.Sprintf("http://cdi-http-import-server.%s/virt-what-cpuid-helper", flags.KubeVirtInstallNamespace)
 	default:
