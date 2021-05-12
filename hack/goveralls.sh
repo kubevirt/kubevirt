b74c931eb9958bce259b7310408b5ba0c6cedbfe
Daniel Hiller <dhiller@redhat.com>
2021-05-12 17:46:30 +0200

    Fix git and build references for coveralls
    Use `PULL_BASE_REF` for referencing the base branch for coverage.
    Use `PULL_PULL_SHA` to override the git sha, as the one fetched from
    the presubmit is different due to branch rebasing. Use `PROW_JOB_ID`
    to reference the prow job this build result originates from.
    
    See prow env var docs: https://github.com/kubernetes/test-infra/blob/master/prow/jobs.md#job-environment-variables
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -2,4 +2,4 @@
 set -eo pipefail
 
 bazel run //vendor/github.com/wadey/gocovmerge:gocovmerge -- $(cat | sed "s# # ${BUILD_WORKING_DIRECTORY}/#g" | sed "s#^#${BUILD_WORKING_DIRECTORY}/#") >coverprofile.dat
-goveralls -service=${CI_NAME} -coverprofile=coverprofile.dat -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
+goveralls -service=${CI_NAME} -jobid=${PROW_JOB_ID} -coverprofile=coverprofile.dat -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
