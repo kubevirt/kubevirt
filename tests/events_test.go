d2799aeb3e1a8b68b30879f8a25adfc571b3ca69
Igor Bezukh <ibezukh@redhat.com>
2021-05-31 15:49:19 +0300

    tests: assign sig-storage label to k8s IO events tests
    Signed-off-by: Igor Bezukh <ibezukh@redhat.com>

--- tests/events_test.go
+++ tests/events_test.go
@@ -26,7 +26,7 @@ const (
 	diskName   = "disk0"
 )
 
-var _ = Describe("[Serial] K8s IO events", func() {
+var _ = Describe("[Serial][sig-storage]K8s IO events", func() {
 	var (
 		ns         string
 		node       string
