87032a0bd4aa68f606a0a93df83df9d6fcd90a04
Quique Llorente <ellorent@redhat.com>
2021-06-07 11:50:06 +0200

    Call make generate
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- pkg/hooks/v1alpha2/api_v1alpha2.pb.go
+++ pkg/hooks/v1alpha2/api_v1alpha2.pb.go
@@ -15,12 +15,15 @@ It has these top-level messages:
 */
 package kubevirt_hooks_v1alpha2
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
 import (
+	fmt "fmt"
+
+	proto "github.com/golang/protobuf/proto"
+
+	math "math"
+
 	context "golang.org/x/net/context"
+
 	grpc "google.golang.org/grpc"
 )
 
