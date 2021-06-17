aeb219c13e013f4dc5c82dd50a4b174ddff5f46c
Roman Mohr <rmohr@redhat.com>
2021-06-17 11:33:59 +0200

    Make numa unit tests more compact
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- pkg/virt-launcher/virtwrap/converter/numa_placement_test.go
+++ pkg/virt-launcher/virtwrap/converter/numa_placement_test.go
@@ -23,18 +23,9 @@ var _ = Describe("NumaPlacement", func() {
 		givenSpec = &api.DomainSpec{
 			CPUTune: &api.CPUTune{
 				VCPUPin: []api.CPUTuneVCPUPin{
-					{
-						VCPU:   0,
-						CPUSet: "10",
-					},
-					{
-						VCPU:   1,
-						CPUSet: "20",
-					},
-					{
-						VCPU:   3,
-						CPUSet: "30",
-					},
+					{VCPU: 0, CPUSet: "10"},
+					{VCPU: 1, CPUSet: "20"},
+					{VCPU: 3, CPUSet: "30"},
 				},
 				IOThreadPin: nil,
 				EmulatorPin: nil,
@@ -47,23 +38,15 @@ var _ = Describe("NumaPlacement", func() {
 				{
 					Id: 0,
 					Cpus: []*cmdv1.CPU{
-						{
-							Id: 10,
-						},
-						{
-							Id: 20,
-						},
+						{Id: 10},
+						{Id: 20},
 					},
 				},
 				{
 					Id: 4,
 					Cpus: []*cmdv1.CPU{
-						{
-							Id: 30,
-						},
-						{
-							Id: 50,
-						},
+						{Id: 30},
+						{Id: 50},
 					},
 				},
 			},
@@ -105,80 +88,72 @@ var _ = Describe("NumaPlacement", func() {
 		Expect(numaMapping(givenVMI, givenSpec, givenTopology)).ToNot(Succeed())
 	})
 
-	It("should detect hugepages and map them equally to nodes", func() {
-		givenVMI.Spec.Domain.Memory.Hugepages = &v1.Hugepages{
-			PageSize: "2Mi",
-		}
+	Context("with hugepages", func() {
+		var expectedMemoryBacking *api.MemoryBacking
+		BeforeEach(func() {
+			givenVMI.Spec.Domain.Memory.Hugepages = &v1.Hugepages{
+				PageSize: "2Mi",
+			}
 
-		givenSpec.MemoryBacking = &api.MemoryBacking{
-			HugePages: &api.HugePages{},
-		}
-		expectedMemoryBacking := &api.MemoryBacking{
-			HugePages: &api.HugePages{HugePage: []api.HugePage{
-				{Size: "2", Unit: "M", NodeSet: "0"},
-				{Size: "2", Unit: "M", NodeSet: "1"},
-			}},
-		}
-
-		Expect(numaMapping(givenVMI, givenSpec, givenTopology)).To(Succeed())
-		Expect(givenSpec.CPUTune).To(Equal(expectedSpec.CPUTune))
-		Expect(givenSpec.NUMATune).To(Equal(expectedSpec.NUMATune))
-		Expect(givenSpec.CPU).To(Equal(expectedSpec.CPU))
-		Expect(givenSpec.MemoryBacking).To(Equal(expectedMemoryBacking))
-	})
-
-	It("should detect not divisable hugepages and shuffle the memory", func() {
-		givenSpec.CPUTune.VCPUPin = append(givenSpec.CPUTune.VCPUPin, api.CPUTuneVCPUPin{
-			VCPU:   4,
-			CPUSet: "40",
+			givenSpec.MemoryBacking = &api.MemoryBacking{
+				HugePages: &api.HugePages{},
+			}
+			expectedMemoryBacking = &api.MemoryBacking{
+				HugePages: &api.HugePages{HugePage: []api.HugePage{
+					{Size: "2", Unit: "M", NodeSet: "0"},
+					{Size: "2", Unit: "M", NodeSet: "1"},
+				}},
+			}
 		})
-		givenTopology.NumaCells = append(givenTopology.NumaCells, &cmdv1.Cell{
-			Id: 5,
-			Cpus: []*cmdv1.CPU{
-				{
-					Id: 40,
+		It("should detect hugepages and map them equally to nodes", func() {
+			Expect(numaMapping(givenVMI, givenSpec, givenTopology)).To(Succeed())
+			Expect(givenSpec.CPUTune).To(Equal(expectedSpec.CPUTune))
+			Expect(givenSpec.NUMATune).To(Equal(expectedSpec.NUMATune))
+			Expect(givenSpec.CPU).To(Equal(expectedSpec.CPU))
+			Expect(givenSpec.MemoryBacking).To(Equal(expectedMemoryBacking))
+		})
+
+		It("should detect not divisable hugepages and shuffle the memory", func() {
+			var err error
+			givenSpec.Memory, err = QuantityToByte(resource.MustParse("66Mi"))
+			givenSpec.CPUTune.VCPUPin = append(givenSpec.CPUTune.VCPUPin, api.CPUTuneVCPUPin{
+				VCPU: 4, CPUSet: "40",
+			})
+			givenTopology.NumaCells = append(givenTopology.NumaCells, &cmdv1.Cell{
+				Id: 5,
+				Cpus: []*cmdv1.CPU{
+					{Id: 40},
 				},
-			},
-		})
-		givenVMI.Spec.Domain.Memory.Hugepages = &v1.Hugepages{
-			PageSize: "2Mi",
-		}
-		givenSpec.MemoryBacking = &api.MemoryBacking{
-			HugePages: &api.HugePages{},
-		}
+			})
 
-		expectedSpec.CPUTune.VCPUPin = append(expectedSpec.CPUTune.VCPUPin, api.CPUTuneVCPUPin{
-			VCPU:   4,
-			CPUSet: "40",
+			expectedSpec.CPUTune.VCPUPin = append(expectedSpec.CPUTune.VCPUPin, api.CPUTuneVCPUPin{
+				VCPU: 4, CPUSet: "40",
+			})
+			expectedSpec.NUMATune.Memory = api.NumaTuneMemory{
+				Mode: "strict", NodeSet: "0,4,5",
+			}
+			expectedSpec.NUMATune.MemNodes = append(expectedSpec.NUMATune.MemNodes, api.MemNode{
+				CellID: 2, Mode: "strict", NodeSet: "5",
+			})
+			expectedMemoryBacking := &api.MemoryBacking{
+				HugePages: &api.HugePages{HugePage: []api.HugePage{
+					{Size: "2", Unit: "M", NodeSet: "0"},
+					{Size: "2", Unit: "M", NodeSet: "1"},
+					{Size: "2", Unit: "M", NodeSet: "2"},
+				}},
+			}
+			expectedSpec.CPU.NUMA.Cells = []api.NUMACell{
+				{ID: "0", CPUs: "0,1", Memory: 22, Unit: "MiB"},
+				{ID: "1", CPUs: "3", Memory: 22, Unit: "MiB"},
+				{ID: "2", CPUs: "4", Memory: 20, Unit: "MiB"},
+			}
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(numaMapping(givenVMI, givenSpec, givenTopology)).To(Succeed())
+			Expect(givenSpec.CPUTune).To(Equal(expectedSpec.CPUTune))
+			Expect(givenSpec.NUMATune).To(Equal(expectedSpec.NUMATune))
+			Expect(givenSpec.CPU).To(Equal(expectedSpec.CPU))
+			Expect(givenSpec.MemoryBacking).To(Equal(expectedMemoryBacking))
 		})
-		expectedSpec.NUMATune.Memory = api.NumaTuneMemory{
-			Mode:    "strict",
-			NodeSet: "0,4,5",
-		}
-		expectedSpec.NUMATune.MemNodes = append(expectedSpec.NUMATune.MemNodes, api.MemNode{
-			CellID:  2,
-			Mode:    "strict",
-			NodeSet: "5",
-		})
-		expectedMemoryBacking := &api.MemoryBacking{
-			HugePages: &api.HugePages{HugePage: []api.HugePage{
-				{Size: "2", Unit: "M", NodeSet: "0"},
-				{Size: "2", Unit: "M", NodeSet: "1"},
-				{Size: "2", Unit: "M", NodeSet: "2"},
-			}},
-		}
-		expectedSpec.CPU.NUMA.Cells = []api.NUMACell{
-			{ID: "0", CPUs: "0,1", Memory: 22, Unit: "MiB"},
-			{ID: "1", CPUs: "3", Memory: 22, Unit: "MiB"},
-			{ID: "2", CPUs: "4", Memory: 20, Unit: "MiB"},
-		}
-		var err error
-		givenSpec.Memory, err = QuantityToByte(resource.MustParse("66Mi"))
-		Expect(err).ToNot(HaveOccurred())
-		Expect(numaMapping(givenVMI, givenSpec, givenTopology)).To(Succeed())
-		Expect(givenSpec.CPUTune).To(Equal(expectedSpec.CPUTune))
-		Expect(givenSpec.NUMATune).To(Equal(expectedSpec.NUMATune))
-		Expect(givenSpec.CPU).To(Equal(expectedSpec.CPU))
-		Expect(givenSpec.MemoryBacking).To(Equal(expectedMemoryBacking))
 	})
 })
