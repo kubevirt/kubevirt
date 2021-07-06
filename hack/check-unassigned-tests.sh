99a003034b05c13eb79a3bb2657b4e21a2bddc5f
Marcelo Amaral <marcelo.amaral1@ibm.com>
2021-07-06 22:08:31 +0900

    hack: add [sig-performance] label as possible assigned test
    Signed-off-by: Marcelo Amaral <marcelo.amaral1@ibm.com>

--- hack/check-unassigned-tests.sh
+++ hack/check-unassigned-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 main() {
-    skip="SRIOV|GPU|\\[sig-operator\\]|\\[sig-network\\]|\\[sig-storage\\]|\\[sig-compute\\]"
+    skip="SRIOV|GPU|\\[sig-operator\\]|\\[sig-network\\]|\\[sig-storage\\]|\\[sig-compute\\]|\\[sig-performance\\]"
     result=$(FUNC_TEST_ARGS="-dryRun -skip=${skip}" make functest)
     total_tests=$(echo "${result}" | grep "Ran[[:space:]].*of" | awk '{print $2}')
     if [ "${total_tests}" != "0" ]; then
-        echo "Found ${total_tests} tests not asigned to any SIG, please check: ${result}"
+        echo "Found ${total_tests} tests not assigned to any SIG, please check: ${result}"
         exit 1
     fi
 }
