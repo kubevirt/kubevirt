83b2a9e754cc977ac463880a878c2e564680d9fa
Andrea Bolognani <abologna@redhat.com>
2021-07-12 17:35:24 +0200

    rpm: Reorganize package lists
    For each image, some packages are the ones we actually care about
    having available, and others are listed simply to avoid dependency
    flipping and thus ensure bazeldnf will produce the same result
    every single time.
    
    Listing these two types of packages separately makes things
    cleaner and will allow us to hopefully reduce the lists over time.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -9,9 +9,24 @@ LIBVIRT_VERSION=0:7.0.0-12
 SEABIOS_VERSION=0:1.14.0-1
 QEMU_VERSION=15:5.2.0-15
 
-# Define some base packages to avoid dependency flipping
-# since some dependencies can be satisfied by multiple packages
-basesystem="glibc-langpack-en coreutils-single libcurl-minimal curl-minimal fedora-logos-httpd vim-minimal"
+# Packages that we want to be included in all container images.
+#
+# Further down we define per-image package lists, which are just like
+# this one are split into two: one for the packages that we actually
+# want to have in the image, and one for (indirect) dependencies that
+# have more than one way of being resolved. Listing the latter
+# explicitly ensures that bazeldnf always reaches the same solution
+# and thus keeps things reproducible
+fedora_base="
+  curl-minimal
+  vim-minimal
+"
+fedora_extra="
+  coreutils-single
+  fedora-logos-httpd
+  glibc-langpack-en
+  libcurl-minimal
+"
 
 # get latest repo data from repo.yaml
 bazel run \
@@ -19,90 +34,105 @@ bazel run \
     //:bazeldnf -- fetch
 
 # create a rpmtree for our test image with misc. tools.
+testimage_base="
+  e2fsprogs
+  iputils
+  nginx
+  nmap-ncat
+  procps-ng
+  qemu-img
+  scsi-target-utils
+  which
+"
+
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --name testimage_x86_64 \
-    $basesystem \
-    qemu-img \
-    which \
-    nginx \
-    scsi-target-utils \
-    procps-ng \
-    nmap-ncat \
-    iputils \
-    e2fsprogs
+    $fedora_base \
+    $fedora_extra \
+    $testimage_base
 
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --arch=aarch64 --name testimage_aarch64 \
-    $basesystem \
-    qemu-img \
-    which \
-    nginx \
-    scsi-target-utils \
-    procps-ng \
-    nmap-ncat \
-    iputils \
-    e2fsprogs
+    $fedora_base \
+    $fedora_extra \
+    $testimage_base
 
 # create a rpmtree for libvirt-devel. libvirt-devel is needed for compilation and unit-testing.
+libvirtdevel_base="
+  libvirt-devel-${LIBVIRT_VERSION}
+"
+libvirtdevel_extra="
+  keyutils-libs
+  krb5-libs
+  libmount
+  lz4-libs
+"
+
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --name libvirt-devel_x86_64 \
-    $basesystem \
-    libvirt-devel-${LIBVIRT_VERSION} \
-    keyutils-libs \
-    krb5-libs \
-    libmount \
-    lz4-libs
+    $fedora_base \
+    $fedora_extra \
+    $libvirtdevel_base \
+    $libvirtdevel_extra
 
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --arch=aarch64 --name libvirt-devel_aarch64 \
-    $basesystem \
-    libvirt-devel-${LIBVIRT_VERSION} \
-    keyutils-libs \
-    krb5-libs \
-    libmount \
-    lz4-libs
+    $fedora_base \
+    $fedora_extra \
+    $libvirtdevel_base \
+    $libvirtdevel_extra
 
 # create a rpmtree for virt-launcher and virt-handler. This is the OS for our node-components.
+launcherbase_base="
+  libvirt-client-${LIBVIRT_VERSION}
+  libvirt-daemon-driver-qemu-${LIBVIRT_VERSION}
+  qemu-kvm-core-${QEMU_VERSION}
+"
+launcherbase_x86_64="
+  seabios-${SEABIOS_VERSION}
+"
+launcherbase_extra="
+  findutils
+  iptables
+  nftables
+  procps-ng
+  selinux-policy
+  selinux-policy-targeted
+  tar
+  xorriso
+"
+
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --name launcherbase_x86_64 \
-    $basesystem \
-    libvirt-daemon-driver-qemu-${LIBVIRT_VERSION} \
-    libvirt-client-${LIBVIRT_VERSION} \
-    qemu-kvm-core-${QEMU_VERSION} \
-    seabios-${SEABIOS_VERSION} \
-    xorriso \
-    selinux-policy selinux-policy-targeted \
-    nftables \
-    findutils \
-    procps-ng \
-    iptables \
-    tar
+    $fedora_base \
+    $fedora_extra \
+    $launcherbase_base \
+    $launcherbase_x86_64 \
+    $launcherbase_extra
 
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- rpmtree --public --arch=aarch64 --name launcherbase_aarch64 \
-    $basesystem \
-    libvirt-daemon-driver-qemu-${LIBVIRT_VERSION} \
-    libvirt-client-${LIBVIRT_VERSION} \
-    qemu-kvm-core-${QEMU_VERSION} \
-    xorriso \
-    selinux-policy selinux-policy-targeted \
-    nftables \
-    findutils \
-    procps-ng \
-    iptables \
-    tar
+    $fedora_base \
+    $fedora_extra \
+    $launcherbase_base \
+    $launcherbase_extra
+
+libguestfstools_base="
+  libguestfs
+  libguestfs-tools
+"
 
 bazel run \
     //:bazeldnf -- rpmtree --public --name libguestfs-tools \
-    $basesystem \
-    libguestfs \
-    libguestfs-tools
+    $fedora_base \
+    $fedora_extra \
+    $libguestfstools_base
 
 # remove all RPMs which are no longer referenced by a rpmtree
 bazel run \
