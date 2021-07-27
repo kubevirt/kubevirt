4b15d3a8a265ac3987ae06cf46798780416e7ee5
Federico Gimenez <fgimenez@redhat.com>
2021-07-27 12:12:27 +0200

    Increase Fossa test timeout
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- hack/fossa.sh
+++ hack/fossa.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
 FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa analyze
-FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa test
+FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa test --timeout 1200
