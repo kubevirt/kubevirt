2636b5016a07093c1a4d4c24cb79ca7af6462526
Roman Mohr <rmohr@redhat.com>
2021-08-06 22:09:59 +0200

    tests: Add python3 to the launcher base
    As a preliminarly measure add python3, since a very small set of e2e
    tests depends on it and add expected binary symlinks.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -136,6 +136,7 @@ launcherbase_base="
   libvirt-client-${LIBVIRT_VERSION}
   libvirt-daemon-driver-qemu-${LIBVIRT_VERSION}
   qemu-kvm-core-${QEMU_VERSION}
+  python36
 "
 launcherbase_x86_64="
   edk2-ovmf-${EDK2_VERSION}
