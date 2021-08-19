fdd6312d25a68c39d4dbe021f520511334f2b5de
Alexander Wels <awels@redhat.com>
2021-08-19 08:57:19 -0400

    Show error if unable to successfully set local directory in test
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- tests/vmi_cloudinit_test.go
+++ tests/vmi_cloudinit_test.go
@@ -73,7 +73,8 @@ var _ = Describe("[rfe_id:151][crit:high][vendor:cnv-qe@redhat.com][level:compon
 		tests.PanicOnError(err)
 
 		// from default virt-launcher flag: do we need to make this configurable in some cases?
-		cloudinit.SetLocalDirectory("/var/run/kubevirt-ephemeral-disks/cloud-init-data")
+		err = cloudinit.SetLocalDirectory("/var/run/kubevirt-ephemeral-disks/cloud-init-data")
+		Expect(err).ToNot(HaveOccurred())
 
 		LaunchVMI = func(vmi *v1.VirtualMachineInstance) *v1.VirtualMachineInstance {
 			By("Starting a VirtualMachineInstance")
