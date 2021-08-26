8356d6de8cd4f28c948128a15147338a4df88b61
Alexander Wels <awels@redhat.com>
2021-08-26 15:03:21 -0400

    Fix hostpath hotplug not working when volume on separate device
    findmnt returns device[path relative in device] when looking up the
    source of a mount by container pid. So on separate device that often
    returned /<path> which is not where it is mounted in the boot device.
    
    This PR modifies the check to look up the device, and then the path
    where the device is mounted, and combines the two paths to get the
    path relative to the boot device so we can properly bind mount the
    hostpath volume into another container.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/virt-handler/hotplug-disk/mount.go
+++ pkg/virt-handler/hotplug-disk/mount.go
@@ -571,6 +571,9 @@ func (m *volumeMounter) mountFileSystemHotplugVolume(vmi *v1.VirtualMachineInsta
 	} else if !isMounted {
 		sourcePath, err := m.getSourcePodFilePath(sourceUID, vmi, volume)
 		if err != nil {
+			log.DefaultLogger().V(3).Infof("Error getting source path: %v", err)
+			// We are eating the error to avoid spamming the log with errors, it might take a while for the volume
+			// to get mounted on the node, and this will error until the volume is mounted.
 			return nil
 		}
 		if err := m.writePathToMountRecord(targetPath, vmi, record); err != nil {
@@ -613,12 +616,21 @@ func (m *volumeMounter) getSourcePodFilePath(sourceUID types.UID, vmi *v1.Virtua
 	}
 	for _, findmnt := range findmounts {
 		if filepath.Base(findmnt.Target) == volume {
-			source := findmnt.GetSource()
+			source := findmnt.GetSourcePath()
 			isBlock, _ := isBlockDevice(filepath.Join(util.HostRootMount, source))
 			if _, err := os.Stat(filepath.Join(util.HostRootMount, source)); os.IsNotExist(err) || isBlock {
 				// file not found, or block device, or directory check if we can find the mount.
 				deviceFindMnt, err := LookupFindmntInfoByDevice(source)
 				if err != nil {
+					// Try the device found from the source
+					deviceFindMnt, err = LookupFindmntInfoByDevice(findmnt.GetSourceDevice())
+					if err != nil {
+						return "", err
+					}
+					// Check if the path was relative to the device.
+					if _, err := os.Stat(filepath.Join(util.HostRootMount, source)); err != nil {
+						return filepath.Join(deviceFindMnt[0].Target, source), nil
+					}
 					return "", err
 				}
 				return deviceFindMnt[0].Target, nil
