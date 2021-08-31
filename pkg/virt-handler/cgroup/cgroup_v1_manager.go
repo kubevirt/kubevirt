15969123505133542a2a1bd59c8cb6de5e6b1eb8
Itamar Holder <iholder@redhat.com>
2021-08-31 13:57:44 +0300

    Keep currently set rules when requesting new device rules
    For example, if the following rules are defined:
    1) {Minor: 111, Major: 111, Allow: true}
    2) {Minor: 222, Major: 222, Allow: true}
    
    And we get a request to enable the following rule: {Minor: 222, Major: 222, Allow: false}
    Than we expect rule (1) to stay unchanged.
    
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-handler/cgroup/cgroup_v1_manager.go
+++ pkg/virt-handler/cgroup/cgroup_v1_manager.go
@@ -1,8 +1,14 @@
 package cgroup
 
 import (
+	"bytes"
 	"fmt"
 	"path/filepath"
+
+	"kubevirt.io/client-go/log"
+
+	cgroup_devices "github.com/opencontainers/runc/libcontainer/cgroups/devices"
+	"github.com/opencontainers/runc/libcontainer/devices"
 
 	runc_cgroups "github.com/opencontainers/runc/libcontainer/cgroups"
 	runc_fs "github.com/opencontainers/runc/libcontainer/cgroups/fs"
@@ -30,8 +36,20 @@ func (v *v1Manager) GetBasePathToHostSubsystem(subsystem string) (string, error)
 }
 
 func (v *v1Manager) Set(r *runc_configs.Resources) error {
-	err := RunWithChroot(HostCgroupBasePath, func() error {
-		err := v.Manager.Set(r)
+	// We want to keep given resources untouched
+	resourcesToSet := *r
+
+	// Adding current rules, see addCurrentRules's documentation for more info
+	requestedAndCurrentRules, err := v.addCurrentRules(r.Devices)
+	if err != nil {
+		return err
+	}
+
+	log.Log.V(loggingVerbosity).Infof("Adding current rules to requested for cgroup %s. Rules added: %d", v1Str, len(requestedAndCurrentRules)-len(r.Devices))
+	resourcesToSet.Devices = requestedAndCurrentRules
+
+	err = RunWithChroot(HostCgroupBasePath, func() error {
+		err := v.Manager.Set(&resourcesToSet)
 		return err
 	})
 
@@ -40,4 +58,54 @@ func (v *v1Manager) Set(r *runc_configs.Resources) error {
 
 func (v *v1Manager) GetCgroupVersion() string {
 	return v1Str
+}
+
+// addCurrentRules gets a slice of rules as a parameter and returns a new slice that contains all given rules
+// and all of the rules that are currently set. This way rules that are already defined won't be deleted by this
+// current request. Every old rule that is part of the new request will be overridden.
+//
+// For example, if the following rules are defined:
+// 1) {Minor: 111, Major: 111, Allow: true}
+// 2) {Minor: 222, Major: 222, Allow: true}
+//
+// And we get a request to enable the following rule: {Minor: 222, Major: 222, Allow: false}
+// Than we expect rule (1) to stay unchanged. Not adding it to our request will automatically delete it.
+func (v *v1Manager) addCurrentRules(deviceRules []*devices.Rule) ([]*devices.Rule, error) {
+	devicesPath, ok := v.GetPaths()["devices"]
+	if !ok {
+		return deviceRules, fmt.Errorf("devices subsystem's path is not defined for this manager")
+	}
+	devicesPath = filepath.Join(HostCgroupBasePath, devicesPath)
+
+	currentRulesStr, err := runc_cgroups.ReadFile(devicesPath, "devices.list")
+	if err != nil {
+		return deviceRules, fmt.Errorf("error reading current rules: %v", err)
+	}
+
+	emulator, err := cgroup_devices.EmulatorFromList(bytes.NewBufferString(currentRulesStr))
+	if err != nil {
+		return deviceRules, fmt.Errorf("error creating emulator out of current rules: %v", err)
+	}
+
+	currentRules, err := emulator.Rules()
+	if err != nil {
+		return deviceRules, fmt.Errorf("error getting rules from emulator: %v", err)
+	}
+
+	isCurrentRulePartOfRequestedRules := func(rule *devices.Rule, rulesSlice []*devices.Rule) bool {
+		for _, ruleInSlice := range rulesSlice {
+			if rule.Type == ruleInSlice.Type && rule.Minor == ruleInSlice.Minor && rule.Major == ruleInSlice.Major {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, currentRule := range currentRules {
+		if !isCurrentRulePartOfRequestedRules(currentRule, deviceRules) {
+			deviceRules = append(deviceRules, currentRule)
+		}
+	}
+
+	return deviceRules, nil
 }
