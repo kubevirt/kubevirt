76570eef795a2bf85d8885404941c7ce2a5e1cdf
Or Shoval <oshoval@redhat.com>
2021-09-05 10:11:34 +0300

    generate, Show git diff in case generate diff exists
    In case generate diff exists, show both the files
    and the actual diff, it makes it easier and faster
    to fix in case its a small change.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/verify-generate.sh
+++ hack/verify-generate.sh
@@ -4,5 +4,6 @@ if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
     echo "ERROR: git tree state is not clean!"
     echo "You probably need to run 'make generate' or 'make' and commit the changes"
     git status
+    git diff
     exit 1
 fi
