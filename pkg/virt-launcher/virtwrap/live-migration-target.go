5b3b7a5700de2623dd0c09d636c5ca2baa640cb8
Jed Lejosne <jed@redhat.com>
2021-10-04 22:52:41 -0400

    migration: empty iso: fix empty iso creation - Fix oversight in for loop - Fill empty isos with 0s instead of creating sparse files - Enter virt-launcher mnt ns to determine files sizes - Add a big functional test that covers all this
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    (cherry picked from commit ac1d217b43af1b1db959c789d6e03f023d84cfe5)
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-launcher/virtwrap/live-migration-target.go
+++ pkg/virt-launcher/virtwrap/live-migration-target.go
@@ -57,10 +57,6 @@ func shouldBlockMigrationTargetPreparation(vmi *v1.VirtualMachineInstance) bool 
 func (l *LibvirtDomainManager) prepareMigrationTarget(vmi *v1.VirtualMachineInstance, useEmulation bool) error {
 	logger := log.Log.Object(vmi)
 
-	if shouldBlockMigrationTargetPreparation(vmi) {
-		return fmt.Errorf("Blocking preparation of migration target in order to satisfy a functional test condition")
-	}
-
 	c, err := l.generateConverterContext(vmi, useEmulation, nil, true)
 	if err != nil {
 		return fmt.Errorf("Failed to generate libvirt domain from VMI spec: %v", err)
@@ -87,6 +83,10 @@ func (l *LibvirtDomainManager) prepareMigrationTarget(vmi *v1.VirtualMachineInst
 	_, err = hooksManager.OnDefineDomain(&dom.Spec, vmi)
 	if err != nil {
 		return fmt.Errorf("executing custom preStart hooks failed: %v", err)
+	}
+
+	if shouldBlockMigrationTargetPreparation(vmi) {
+		return fmt.Errorf("Blocking preparation of migration target in order to satisfy a functional test condition")
 	}
 
 	loopbackAddress := ip.GetLoopbackAddress()
