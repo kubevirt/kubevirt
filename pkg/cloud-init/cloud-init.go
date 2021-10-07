245ca2ef515a3f500d64af1887848655a414da6c
Jed Lejosne <jed@redhat.com>
2021-10-07 10:46:54 -0400

    migration: empty isos: fix flake and nit
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    (cherry picked from commit 3b280cb28606a8e417855c3a7d4b58d69e4f3674)
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/cloud-init/cloud-init.go
+++ pkg/cloud-init/cloud-init.go
@@ -504,7 +504,7 @@ func GenerateEmptyIso(vmiName string, namespace string, data *CloudInitData, siz
 	if err != nil {
 		return err
 	}
-	err = f.Close()
+	util.CloseIOAndCheckErr(f, &err)
 	if err != nil {
 		return err
 	}
