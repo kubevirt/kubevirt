443740daffebfe1f2af26d16925d461b29aabd5a
Itamar Holder <iholder@redhat.com>
2021-10-17 09:49:36 +0300

    Make unit test easier to reason about
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-controller/services/template_test.go
+++ pkg/virt-controller/services/template_test.go
@@ -3058,11 +3058,9 @@ var _ = Describe("Template", func() {
 				containers := pod.Spec.Containers
 				initContainers := pod.Spec.InitContainers
 
-				for _, containerArray := range [][]kubev1.Container{initContainers, containers} {
-					Expect(containerArray).Should(HaveLen(2))
-					Expect(hasContainerWithName(containerArray, "kernel-boot")).To(BeTrue())
-				}
 				Expect(hasContainerWithName(initContainers, "container-disk-binary")).To(BeTrue())
+				Expect(hasContainerWithName(initContainers, "kernel-boot")).To(BeTrue())
+				Expect(hasContainerWithName(containers, "kernel-boot")).To(BeTrue())
 			})
 		})
 
