abac7a288522f5c6f1de97480943ddab494cb108
Howard Zhang <howard.zhang@arm.com>
2021-10-28 11:20:49 +0800

    skip migration test in vm_watch_test on Arm64
    Signed-off-by: Howard Zhang <howard.zhang@arm.com>

--- tests/vm_watch_test.go
+++ tests/vm_watch_test.go
@@ -273,6 +273,11 @@ var _ = Describe("[rfe_id:3423][crit:high][arm64][vendor:cnv-qe@redhat.com][leve
 		Expect(vmStatus).To(ConsistOf(vm.Name, MatchRegexp(vmAgeRegex), string(v12.VirtualMachineStatusRunning), readyConditionTrue),
 			"VM should be in the %s status", v12.VirtualMachineStatusRunning)
 
+		// The previous tests would be done, once succeed the following tests would be skipped on the Arm64 cluster
+		// Otherwise, it would show the failures
+		// TODO: remove this once we have more than one node in the Arm64 cluster
+		tests.SkipIfARM64("arm64 cluster only have one node")
+
 		By("Migrating the VirtualMachine")
 
 		// Verify we have more than one scheduleable node
