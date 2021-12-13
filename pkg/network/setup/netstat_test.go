6c5df8d7b968e19b447ee1a4529c34c4f7c1ecaf
Edward Haas <edwardh@redhat.com>
2021-12-13 21:31:44 +0200

    netstat: Remove test that assumes domain spec without vmi spec
    In the current implementation, the domain spec is directly affected by
    the VMI spec (in the network interfaces scope).
    The scenario were the domain is configured with interfaces that do not
    appear in the VMI spec is no supported (or expected).
    
    Therefore, the test that checks such a scenario (in an implicit manner)
    is removed.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- pkg/network/setup/netstat_test.go
+++ pkg/network/setup/netstat_test.go
@@ -244,37 +244,6 @@ var _ = Describe("netstat", func() {
 		}), "the pod IP/s should be reported in the status")
 	})
 
-	It("should add a new interface based on the domain spec", func() {
-		const (
-			existingNetworkName = "primary"
-			existingMAC         = "C0:01:BE:E7:15:G0:0D"
-
-			newNetworkName = "secondary"
-			newDomainMAC   = "22:22:22:22:22:22"
-		)
-
-		vmi.Status.Interfaces = []v1.VirtualMachineInstanceNetworkInterface{
-			{
-				MAC:  existingMAC,
-				Name: existingNetworkName,
-			},
-		}
-
-		domain := &api.Domain{
-			Spec: api.DomainSpec{Devices: api.Devices{Interfaces: []api.Interface{
-				newDomainSpecIface(existingNetworkName, existingMAC),
-				newDomainSpecIface(newNetworkName, newDomainMAC),
-			}}},
-		}
-
-		netStat.UpdateStatus(vmi, domain)
-
-		Expect(vmi.Status.Interfaces).To(Equal([]v1.VirtualMachineInstanceNetworkInterface{
-			newVMIStatusIface(existingNetworkName, nil, existingMAC, "", netvmispec.InfoSourceDomain),
-			newVMIStatusIface(newNetworkName, nil, newDomainMAC, "", netvmispec.InfoSourceDomain),
-		}), "the new interface should be reported in the status")
-	})
-
 	It("should report SR-IOV interface with MAC and network name, based on VMI spec and guest-agent data", func() {
 		const (
 			networkName    = "sriov-network"
