739349c6ba840d4be8107cfc97061ab49874eaad
Or Shoval <oshoval@redhat.com>
2021-12-15 10:52:19 +0200

    tests, libvmi, Update MultusNetwork to allow specifing network name
    Since two networks can use the same network attachement defintion,
    allow MultusNetwork to specify the network name in addition to
    the nad name.
    This way more than one network can be created using the same nad.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/libvmi/network.go
+++ tests/libvmi/network.go
@@ -78,13 +78,13 @@ func InterfaceWithMac(iface *kvirtv1.Interface, macAddress string) *kvirtv1.Inte
 	return iface
 }
 
-// MultusNetwork returns a Network with the given name
-func MultusNetwork(networkName string) *kvirtv1.Network {
+// MultusNetwork returns a Network with the given name, associated to the given nad
+func MultusNetwork(name, nadName string) *kvirtv1.Network {
 	return &kvirtv1.Network{
-		Name: networkName,
+		Name: name,
 		NetworkSource: kvirtv1.NetworkSource{
 			Multus: &kvirtv1.MultusNetwork{
-				NetworkName: networkName,
+				NetworkName: nadName,
 			},
 		},
 	}
