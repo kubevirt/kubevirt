a7ad8407d699b3a0ae38ae598f95bc59bd12f184
Maya Rashish <mrashish@redhat.com>
2022-01-18 12:16:33 +0200

    Add "IO error" functional tests back to quarantine
    Signed-off-by: Maya Rashish <mrashish@redhat.com>

--- tests/storage/storage.go
+++ tests/storage/storage.go
@@ -123,7 +123,7 @@ var _ = SIGDescribe("Storage", func() {
 				tests.RemoveSCSIDisk(nodeName, address)
 			})
 
-			It(" should pause VMI on IO error", func() {
+			It(" [QUARANTINE]should pause VMI on IO error", func() {
 				By("Creating VMI with faulty disk")
 				vmi := tests.NewRandomVMIWithEphemeralDisk(cd.ContainerDiskFor(cd.ContainerDiskAlpine))
 				vmi = tests.AddPVCDisk(vmi, "pvc-disk", "virtio", pvc.Name)
@@ -205,7 +205,7 @@ var _ = SIGDescribe("Storage", func() {
 				Expect(err).ToNot(HaveOccurred())
 			})
 
-			It(" should pause VMI on IO error", func() {
+			It(" [QUARANTINE]should pause VMI on IO error", func() {
 				By("Creating VMI with faulty disk")
 				vmi := tests.NewRandomVMIWithPVC(pvc.Name)
 				_, err := virtClient.VirtualMachineInstance(util.NamespaceTestDefault).Create(vmi)
