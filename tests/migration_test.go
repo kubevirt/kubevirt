d6dfb88f9050111eaa810b5b653ee6959db0e794
Roman Mohr <rmohr@redhat.com>
2022-02-02 15:00:02 +0100

    Remove deprecated usage of --delete-local-data
    Use on drain commands `--delete-emptydir-data`.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- tests/migration_test.go
+++ tests/migration_test.go
@@ -128,10 +128,10 @@ var _ = Describe("[Serial][rfe_id:393][crit:high][vendor:cnv-qe@redhat.com][leve
 		vmiSelector := v1.AppLabel + "=virt-launcher"
 		k8sClient := tests.GetK8sCmdClient()
 		if k8sClient == "oc" {
-			tests.RunCommandWithNS("", k8sClient, "adm", "drain", node, "--delete-local-data", "--pod-selector", vmiSelector,
+			tests.RunCommandWithNS("", k8sClient, "adm", "drain", node, "--delete-emptydir-data", "--pod-selector", vmiSelector,
 				"--ignore-daemonsets=true", "--force", "--timeout=180s")
 		} else {
-			tests.RunCommandWithNS("", k8sClient, "drain", node, "--delete-local-data", "--pod-selector", vmiSelector,
+			tests.RunCommandWithNS("", k8sClient, "drain", node, "--delete-emptydir-data", "--pod-selector", vmiSelector,
 				"--ignore-daemonsets=true", "--force", "--timeout=180s")
 		}
 	}
