7dd11b558ce638ab9c25806143a5cec7c9983e6c
tyleraharrison <tyleraharrison@gmail.com>
2022-02-20 00:30:28 -0600

    Added ability to specify VNC proxy listening address
    - Adds feature in issue #7254
    
    General motivation
    Our team discovered certain use-cases where external VNC clients were required for certain applications, such as Apache Guacamole.
    The commit that added the --proxy-only feature locks the user choice down to the loopback adapter, so the only way to use the aforementioned tools is tunneling (which is not ideal for many, many ports/VMs).
    This new feature will keep the old functionality and will not affect anyone using this utility unless they explicitly use the new --address flag.
    The --address flag will allow for a user to specify a listening IP address. This feature is important for the following reasons:
    - It allows for the user to open the listening connections to any interface/IP (0.0.0.0)
    - It gives the user the capability to limit the listening to a specific interface
    - It allows for the use of IPv6 specification
    - This enables the ability to use with certain VPS external address configurations or VPN/tunnels that require a program to specify that address
    
    Our team's motivation
    We are using openSUSE Harvester to pioneer a new infrastructure for educational cyber ranges to give students a safe environment to study and practice malware, pentesting, etc.
    We are required to use Apache Guacamole for reasons that are not negotiable. Apache Guacamole is running in the same server environment but not on the same server, so limiting this proxy to the loopback adapter makes this impossible.
    At the time of writing this, our cyber range is working as intended now with the specified interface dedicated to the VNC traffic.
    Specifying IP address vs just using 0.0.0.0 was also required for us, since we needed to make sure that traffic was not accessible from the other interface ports at all.
    One last feature we need to implement is the ability to keep a connection listening after VNC client disconnect so students can connect back to their machines on-demand
    
    Signed-off-by: tyleraharrison <tyleraharrison@gmail.com>

--- pkg/virtctl/vnc/vnc.go
+++ pkg/virtctl/vnc/vnc.go
@@ -39,8 +39,6 @@ import (
 	"kubevirt.io/kubevirt/pkg/virtctl/templates"
 )
 
-const loopbackIPAddressFmt = "127.0.0.1:%d"
-
 const (
 	LISTEN_TIMEOUT = 60 * time.Second
 
@@ -61,6 +59,8 @@ const (
 	TIGER_VNC     = "vncviewer"
 )
 
+var listenAddressFmt string
+var listenAddress = "127.0.0.1"
 var proxyOnly bool
 var customPort = 0
 
@@ -75,7 +75,8 @@ func NewCommand(clientConfig clientcmd.ClientConfig) *cobra.Command {
 			return c.Run(cmd, args)
 		},
 	}
-	cmd.Flags().BoolVar(&proxyOnly, "proxy-only", proxyOnly, "--proxy-only=false: Setting this true will run only the virtctl vnc proxy and show the localhost port where VNC viewers can connect")
+	cmd.Flags().StringVar(&listenAddress, "address", listenAddress, "--address=127.0.0.1: Setting this will change the listening address of the VNC server. Example: --address=0.0.0.0 will make the server listen on all interfaces.")
+	cmd.Flags().BoolVar(&proxyOnly, "proxy-only", proxyOnly, "--proxy-only=false: Setting this true will run only the virtctl vnc proxy and show the port where VNC viewers can connect")
 	cmd.Flags().IntVar(&customPort, "port", customPort,
 		"--port=0: Assigning a port value to this will try to run the proxy on the given port if the port is accessible; If unassigned, the proxy will run on a random port")
 	cmd.SetUsageTemplate(templates.UsageTemplate())
@@ -104,8 +105,9 @@ func (o *VNC) Run(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return fmt.Errorf("Can't access VMI %s: %s", vmi, err.Error())
 	}
-
-	lnAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(loopbackIPAddressFmt, customPort))
+	// Format the listening address to account for the port (ex: 127.0.0.0:5900)
+	listenAddressFmt = listenAddress + ":%d"
+	lnAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(listenAddressFmt, customPort))
 	if err != nil {
 		return fmt.Errorf("Can't resolve the address: %s", err.Error())
 	}
@@ -288,7 +290,7 @@ func checkAndRunVNCViewer(doneChan chan struct{}, viewResChan chan error, port i
 }
 
 func tigerVncArgs(port int) (args []string) {
-	args = append(args, fmt.Sprintf(loopbackIPAddressFmt, port))
+	args = append(args, fmt.Sprintf(listenAddressFmt, port))
 	if glog.V(4) {
 		args = append(args, "Log=*:stderr:100")
 	}
@@ -296,12 +298,12 @@ func tigerVncArgs(port int) (args []string) {
 }
 
 func chickenVncArgs(port int) (args []string) {
-	args = append(args, fmt.Sprintf(loopbackIPAddressFmt, port))
+	args = append(args, fmt.Sprintf(listenAddressFmt, port))
 	return
 }
 
 func realVncArgs(port int) (args []string) {
-	args = append(args, fmt.Sprintf(loopbackIPAddressFmt, port))
+	args = append(args, fmt.Sprintf(listenAddressFmt, port))
 	args = append(args, "-WarnUnencrypted=0")
 	args = append(args, "-Shared=0")
 	args = append(args, "-ShareFiles=0")
@@ -321,5 +323,5 @@ func remoteViewerArgs(port int) (args []string) {
 
 func usage() string {
 	return `  # Connect to 'testvmi' via remote-viewer:\n"
-  {{ProgramName}} vnc testvmi`
+   {{ProgramName}} vnc testvmi`
 }
