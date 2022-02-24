bc18463a477b4807c10d2f3844f568331d25c106
Dan Kenigsberg <danken@redhat.com>
2022-02-25 01:25:50 +0200

    tests/network/vmi_networking: drop unhelpful comment
    "Wait for VirtualMachineInstance start" does not help the reader when
    placed above the properly-named WaitForSuccessfulVMIStart()
    
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

--- tests/network/vmi_networking.go
+++ tests/network/vmi_networking.go
@@ -1089,10 +1089,9 @@ var _ = SIGDescribe("[rfe_id:694][crit:medium][vendor:cnv-qe@redhat.com][level:c
 })
 
 func waitUntilVMIReady(vmi *v1.VirtualMachineInstance, loginTo console.LoginToFunction) *v1.VirtualMachineInstance {
-	// Wait for VirtualMachineInstance start
 	vmi = tests.WaitForSuccessfulVMIStart(vmi)
 
-	// Lets make sure that the OS is up by waiting until we can login
+	// make sure that the OS is up by waiting until we can login
 	Expect(loginTo(vmi)).To(Succeed())
 	return vmi
 }
