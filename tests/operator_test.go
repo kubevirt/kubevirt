ca2c0f5d7dbf650e2b998eff2e081a3eef37e7f9
Igor Bezukh <ibezukh@redhat.com>
2022-03-01 10:12:26 +0200

    e2e: taking test_id:4744 out of quarantine
    Signed-off-by: Igor Bezukh <ibezukh@redhat.com>

--- tests/operator_test.go
+++ tests/operator_test.go
@@ -1365,7 +1365,7 @@ spec:
 
 	Describe("[test_id:4744]should apply component customization", func() {
 
-		It("[QUARANTINE]test applying and removing a patch", func() {
+		It("test applying and removing a patch", func() {
 			annotationPatchValue := "new-annotation-value"
 			annotationPatchKey := "applied-patch"
 
