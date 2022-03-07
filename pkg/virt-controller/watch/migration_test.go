3defe8d3f1768150fc2887d2d949c4eb7de24e01
Itamar Holder <iholder@redhat.com>
2022-03-07 16:52:39 +0200

    Add test to ensure wildcard label value is matched to any value
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-controller/watch/migration_test.go
+++ pkg/virt-controller/watch/migration_test.go
@@ -1424,6 +1424,28 @@ var _ = Describe("Migration watcher", func() {
 				Expect(matchedPolicy).To(BeNil())
 			})
 
+			It("label with an empty value should match any value", func() {
+				const labelKey = "mp-key-0"
+				labelValue := rand.String(5)
+
+				By("Defining a policy that matches a key with empty value")
+				policy := kubecli.NewMinimalMigrationPolicy("testpolicy")
+				policy.Spec.Selectors = &migrationsv1.Selectors{
+					VirtualMachineInstanceSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{labelKey: ""},
+					},
+				}
+
+				By(fmt.Sprintf("Adding a label with same key but random value (%s) to vmi", labelValue))
+				vmi.Labels = map[string]string{labelKey: labelValue}
+
+				By("Expecting the policy to match")
+				policyList := kubecli.NewMinimalMigrationPolicyList(*policy)
+				matchedPolicy := policyList.MatchPolicy(vmi, &namespace)
+				Expect(matchedPolicy).ToNot(BeNil())
+				Expect(matchedPolicy.Name).To(Equal(policy.Name))
+			})
+
 			It("when no policies exist, MatchPolicy() should return nil", func() {
 				policyList := kubecli.NewMinimalMigrationPolicyList()
 				matchedPolicy := policyList.MatchPolicy(vmi, &namespace)
