b783bc5450f15edce1661c160b1e103d00b915fe
Oren Cohen <ocohen@redhat.com>
2022-04-14 11:10:42 +0300

    Bump kubevirtci to Kubernetes 1.23 by default (#1898)
    Signed-off-by: orenc1 <ocohen@redhat.com>

--- cluster/kubevirtci.sh
+++ cluster/kubevirtci.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.22'}
+export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.23'}
 export KUBEVIRTCI_TAG=$(curl -L -Ss https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest)
 KUBEVIRTCI_PATH="${PWD}/_kubevirtci"
 
