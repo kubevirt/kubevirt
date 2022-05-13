0ec2c45fddf70b762e1b2b72c2598de20e5045f1
Jed Lejosne <jed@redhat.com>
2022-05-13 10:02:07 -0400

    operator tests: re-install testing manifests on update
    When testing update by updating virt-operator, the virt-operator manifest gets removed from the cluster.
    However, the virt-operator manifest includes the kubevirt namespace, so all resources in the namespace get removed.
    Testing manifests are part of that, and so they need to be reinstalled or subsequent tests might fail.
    
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -48,6 +48,7 @@ ARCHITECTURE="${BUILD_ARCH:-$(uname -m)}"
 HOST_ARCHITECTURE="$(uname -m)"
 KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 OPERATOR_MANIFEST_PATH=$MANIFESTS_OUT_DIR/release/kubevirt-operator.yaml
+TESTING_MANIFEST_PATH=$MANIFESTS_OUT_DIR/testing
 KUBEVIRT_CRI="$(determine_cri_bin)"
 
 function build_func_tests() {
