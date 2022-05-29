cd233fc8449b09c1e4a861320f924e24d0079e59
Ben Oukhanov <boukhanov@redhat.com>
2022-05-29 19:24:41 +0300

    test: move GenerateVMIJson from utils
    Move GenerateVMIJson from utils to window_test
    to make utils shorter.
    
    Signed-off-by: Ben Oukhanov <boukhanov@redhat.com>

--- tests/utils.go
+++ tests/utils.go
@@ -2238,20 +2238,6 @@ func GenerateVMJson(vm *v1.VirtualMachine, generateDirectory string) (string, er
 	return jsonFile, nil
 }
 
-func GenerateVMIJson(vmi *v1.VirtualMachineInstance, generateDirectory string) (string, error) {
-	data, err := json.Marshal(vmi)
-	if err != nil {
-		return "", fmt.Errorf("failed to generate json for vmi %s", vmi.Name)
-	}
-
-	jsonFile := filepath.Join(generateDirectory, fmt.Sprintf("%s.json", vmi.Name))
-	err = ioutil.WriteFile(jsonFile, data, 0644)
-	if err != nil {
-		return "", fmt.Errorf("failed to write json file %s", jsonFile)
-	}
-	return jsonFile, nil
-}
-
 func GenerateTemplateJson(template *vmsgen.Template, generateDirectory string) (string, error) {
 	data, err := json.Marshal(template)
 	if err != nil {
