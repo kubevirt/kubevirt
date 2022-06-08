96d85dc50ca944facc07b1d87adb6410ff9747e4
bmordeha <bmodeha@redhat.com>
2022-06-08 14:57:13 +0300

    Add functional tests to test the modifiactions of the back port. Modify existing tests to consider the new changes of the back port.
    Signed-off-by: bmordeha <bmodeha@redhat.com>

--- pkg/virt-controller/watch/migration_test.go
+++ pkg/virt-controller/watch/migration_test.go
@@ -1170,6 +1170,7 @@ var _ = Describe("Migration watcher", func() {
 			if toDefineHostModelCPU {
 				node.ObjectMeta.Labels = map[string]string{
 					virtv1.HostModelCPULabel + "fake":              "true",
+					virtv1.SupportedHostModelMigrationCPU + "fake": "true",
 					virtv1.HostModelRequiredFeaturesLabel + "fake": "true",
 				}
 			}
@@ -1181,7 +1182,7 @@ var _ = Describe("Migration watcher", func() {
 			expectPodToHaveProperNodeSelector := func(pod *k8sv1.Pod) {
 				podHasCpuModeLabelSelector := false
 				for key, _ := range pod.Spec.NodeSelector {
-					if strings.Contains(key, virtv1.HostModelCPULabel) {
+					if strings.Contains(key, virtv1.SupportedHostModelMigrationCPU) {
 						podHasCpuModeLabelSelector = true
 						break
 					}
