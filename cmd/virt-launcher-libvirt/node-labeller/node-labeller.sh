8d6dd8413459de8b4717e594faeb3773e3dc1e8b
Sebastien Boeuf <sebastien.boeuf@intel.com>
2022-06-17 17:18:07 +0200

    Move cmd/virt-launcher to cmd/virt-launcher-libvirt
    This is done in order to keep the libvirt implementation along with the
    Cloud Hypervisor one.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

---
+++ cmd/virt-launcher-libvirt/node-labeller/node-labeller.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+KVM_MINOR=$(grep -w 'kvm' /proc/misc | cut -f 1 -d' ')
+VIRTTYPE=qemu
+
+if [ ! -e /dev/kvm ] && [ -n "$KVM_MINOR" ]; then
+  mknod /dev/kvm c 10 $KVM_MINOR
+fi
+
+if [ -e /dev/kvm ]; then
+    chmod o+rw /dev/kvm
+    VIRTTYPE=kvm
+fi
+
+if [ -e /dev/sev ]; then
+  # QEMU requires RW access to query SEV capabilities
+  chmod o+rw /dev/sev
+fi
+
+libvirtd -d
+
+virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/virsh_domcapabilities.xml
+
+cp -r /usr/share/libvirt/cpu_map /var/lib/kubevirt-node-labeller
+
+virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE | virsh hypervisor-cpu-baseline --features /dev/stdin --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/supported_features.xml
+virsh capabilities > /var/lib/kubevirt-node-labeller/capabilities.xml
