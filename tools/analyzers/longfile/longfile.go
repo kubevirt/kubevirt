da107c6338b7b532dba18d264bfd0c511a35fd5c
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2022-07-13 17:11:44 +0300

    Add command line parameters to the longfile linter
    Add the new `--exceptions` commandline parameter to the longfile linter.
    
    Upgrade ruels_go to v0.33.0 in order to support nogo_vet
    `analyzer_flags` configuration field.
    
    Add configuration to the longfile linter.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- tools/analyzers/longfile/longfile.go
+++ tools/analyzers/longfile/longfile.go
@@ -2,42 +2,104 @@ package longfile
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/token"
+	"strconv"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
 )
 
-var max int
-var exceptions map[string]int
+var max = 1500
 
-var Analyzer = &analysis.Analyzer{
-	Name: "longfile",
-	Doc:  "detects if source code files are too long",
-	Run:  checkPath,
-}
+const (
+	exceptionDoc = "comma separated list of files with size greater than the default." +
+		" each item in the list is with this format: <file name>:<number of lines>"
+)
 
-func init() {
-	max = 1000
-	exceptions = map[string]int{
-		"tests/utils.go":               3500,
-		"tests/reporter/kubernetes.go": 2000,
+var Analyzer = newAnalyzer()
+
+func newAnalyzer() *analysis.Analyzer {
+	l := &longFileCfg{
+		// todo: once in rules_go version v0.32.0 or higher, nogo should send the command
+		//       line arguments (analyzer_flags field in nogo_configuration.json);
+		//       then we can replace this initialization by: `exceptions: make(longFileExceptions)`
+		// todo: try to reduce the size of each one of these files
+		exceptions: longFileExceptions{
+			"tests/storage/restore.go":        1621,
+			"tests/infra_test.go":             1686,
+			"tests/migration_test.go":         4382,
+			"tests/operator_test.go":          2990,
+			"tests/utils.go":                  2776,
+			"tests/vm_test.go":                2301,
+			"tests/vmi_configuration_test.go": 3053,
+			"tests/vmi_lifecycle_test.go":     1900,
+		},
 	}
+	a := &analysis.Analyzer{
+		Name:             "longfile",
+		Doc:              "detects if source code files are too long",
+		Run:              l.checkPath,
+		RunDespiteErrors: true,
+	}
+
+	a.Flags.Init("longfiles", flag.ExitOnError)
+	a.Flags.Var(&l.exceptions, "exceptions", exceptionDoc)
+	return a
 }
 
-func checkPath(pass *analysis.Pass) (interface{}, error) {
+type longFileExceptions map[string]int
+
+// implement the flag.Value interface
+func (e longFileExceptions) String() string {
+	b := strings.Builder{}
+	b.WriteRune('[')
+	for k, v := range e {
+		b.WriteString(fmt.Sprintf(`{%q: %d},`, k, v))
+	}
+	b.WriteRune(']')
+
+	return b.String()
+}
+
+func (e longFileExceptions) Set(value string) error {
+	items := strings.Split(value, ",")
+	for _, item := range items {
+		file := strings.Split(item, ":")
+		if len(file) != 2 {
+			return fmt.Errorf("can't parsr the file '%s'; it should be in format of <file name>:<number of lines>", item)
+		}
+		name := strings.TrimSpace(file[0])
+		lines, err := strconv.Atoi(strings.TrimSpace(file[1]))
+		if err != nil {
+			return fmt.Errorf("can't parse number of lines from %s; %w", item, err)
+		}
+		e[name] = lines
+	}
+
+	return nil
+}
+
+type longFileCfg struct {
+	exceptions longFileExceptions
+}
+
+func (l longFileCfg) checkPath(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
 		pos := pass.Fset.Position(file.End())
 		if isGenerated(file, pos) {
 			continue
 		}
 
-		parts := strings.Split(pos.Filename, "execroot/kubevirt/")
-		filename := parts[len(parts)-1]
+		fileName := pos.Filename
+		if strings.Contains(fileName, "execroot/kubevirt/") {
+			parts := strings.Split(pos.Filename, "execroot/kubevirt/")
+			fileName = parts[len(parts)-1]
+		}
 
-		fileMax, exists := exceptions[filename]
+		fileMax, exists := l.exceptions[fileName]
 		if !exists {
 			fileMax = max
 		}
@@ -45,10 +107,9 @@ func checkPath(pass *analysis.Pass) (interface{}, error) {
 		if pos.Line > fileMax {
 			pass.Report(analysis.Diagnostic{
 				Pos:     file.End(),
-				Message: fmt.Sprintf("file has a length of %v which is more than %v lines", pos.Line, fileMax),
+				Message: fmt.Sprintf("file has a length of %v which is more than %v lines; file name: %s", pos.Line, fileMax, pos.Filename),
 			})
 		}
-
 	}
 	return nil, nil
 }
