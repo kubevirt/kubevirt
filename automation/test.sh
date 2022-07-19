b4afe2eb6272726594f0a200681d4052fc6c5a8f
divyaKh <dikhande@redhat.com>
2022-07-18 18:11:31 -0700

    Create label_filter param to use multiple label filters and Pass to functests.sh
    Signed-off-by: divyaKh <dikhande@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -373,17 +373,16 @@ if [[ -z ${KUBEVIRT_E2E_FOCUS} && -z ${KUBEVIRT_E2E_SKIP} ]]; then
     export KUBEVIRT_E2E_FOCUS="\\[sig-storage\\]|\\[storage-req\\]"
     export KUBEVIRT_E2E_SKIP="Migration"
   elif [[ $TARGET =~ vgpu.* ]]; then
-    export KUBEVIRT_E2E_FOCUS="MediatedDevices"
+    label_filter = '--label-filter=needs-mdev-gpu'
   elif [[ $TARGET =~ sig-compute-realtime ]]; then
     export KUBEVIRT_E2E_FOCUS="\\[sig-compute-realtime\\]"
   elif [[ $TARGET =~ sig-compute-migrations ]]; then
     export KUBEVIRT_E2E_FOCUS="Migration"
-    export KUBEVIRT_E2E_SKIP="MediatedDevices"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
   elif [[ $TARGET =~ sig-compute ]]; then
     export KUBEVIRT_E2E_FOCUS="\\[sig-compute\\]"
-    export KUBEVIRT_E2E_SKIP="MediatedDevices|Migration"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    export KUBEVIRT_E2E_SKIP="Migration"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
   elif [[ $TARGET =~ sig-monitoring ]]; then
       export KUBEVIRT_E2E_FOCUS="\\[sig-monitoring\\]"
   elif [[ $TARGET =~ sig-operator ]]; then
@@ -391,13 +390,13 @@ if [[ -z ${KUBEVIRT_E2E_FOCUS} && -z ${KUBEVIRT_E2E_SKIP} ]]; then
   elif [[ $TARGET =~ sriov.* ]]; then
     export KUBEVIRT_E2E_FOCUS="SRIOV"
   elif [[ $TARGET =~ gpu.* ]]; then
-    export ginko_params="$ginko_params --label-filter=needs-gpu"
+    label_filter='--label-filter=needs-gpu'
   elif [[ $TARGET =~ (okd|ocp).* ]]; then
-    export KUBEVIRT_E2E_SKIP="SRIOV|MediatedDevices"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    export KUBEVIRT_E2E_SKIP="SRIOV"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
   else
-    export KUBEVIRT_E2E_SKIP="Multus|SRIOV|Macvtap|MediatedDevices"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    export KUBEVIRT_E2E_SKIP="Multus|SRIOV|Macvtap"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
   fi
 
   if ! [[ $TARGET =~ sig-storage ]]; then
@@ -429,12 +428,12 @@ fi
 # but also currently lack the requirements for SRIOV, GPU, Macvtap and MDEVs.
 if [[ $KUBEVIRT_NUM_NODES = "1" && $KUBEVIRT_INFRA_REPLICAS = "1" ]]; then
   if [ -n "$KUBEVIRT_E2E_SKIP" ]; then
-    export KUBEVIRT_E2E_SKIP="${KUBEVIRT_E2E_SKIP}|SRIOV|Macvtap|MediatedDevices|Migration"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    export KUBEVIRT_E2E_SKIP="${KUBEVIRT_E2E_SKIP}|SRIOV|Macvtap|Migration"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
 
   else
-    export KUBEVIRT_E2E_SKIP="SRIOV|Macvtap|MediatedDevices|Migration"
-    export ginko_params="$ginko_params --label-filter=!needs-gpu"
+    export KUBEVIRT_E2E_SKIP="SRIOV|Macvtap|Migration"
+    label_filter='--label-filter=!needs-gpu && !needs-mdev-gpu'
   fi
 fi
 
@@ -474,7 +473,7 @@ fi
 
 
 # Run functional tests
-FUNC_TEST_ARGS=$ginko_params make functest
+FUNC_TEST_ARGS=$ginko_params FUNC_TEST_LABEL_FILTER=$label_filter make functest
 
 # Run REST API coverage based on k8s audit log and openapi spec
 if [ -n "$RUN_REST_COVERAGE" ]; then
