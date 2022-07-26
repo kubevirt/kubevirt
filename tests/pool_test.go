19c75b2304a79f10b43a853613da1f28a5d288e4
Igor Bezukh <ibezukh@redhat.com>
2022-07-26 10:20:11 +0300

    e2e: improve the vm pool tests
    The VM pool suite contains tests with running VMs. These
    tests only observe the statuses of the VMIs and the VM pool
    resources. They don't interact directly with the guest OS.
    
    Therefore we can use a diskless approach when a VM runs
    the "no bootable device" phase.
    
    It will save the time it takes to setup the container-disk.
    
    Also, the minimum amount of RAM required to run SeaBIOS is 1M,
    I've decided to put 2Mi to be on the safe side.
    
    Signed-off-by: Igor Bezukh <ibezukh@redhat.com>

--- tests/pool_test.go
+++ tests/pool_test.go
@@ -115,7 +115,7 @@ var _ = Describe("[sig-compute]VirtualMachinePool", func() {
 
 	newVirtualMachinePool := func() *poolv1.VirtualMachinePool {
 		By("Create a new VirtualMachinePool")
-		pool := newPoolFromVMI(libvmi.NewCirros())
+		pool := newPoolFromVMI(libvmi.New(libvmi.WithResourceMemory("2Mi")))
 		running := true
 		pool.Spec.VirtualMachineTemplate.Spec.Running = &running
 		return createVirtualMachinePool(pool)
@@ -131,7 +131,6 @@ var _ = Describe("[sig-compute]VirtualMachinePool", func() {
 		doScale(newPool.ObjectMeta.Name, int32(startScale))
 		doScale(newPool.ObjectMeta.Name, int32(stopScale))
 		doScale(newPool.ObjectMeta.Name, int32(0))
-
 	},
 		Entry("[QUARANTINE]to three, to two and then to zero replicas", 3, 2),
 		Entry("[QUARANTINE]to five, to six and then to zero replicas", 5, 6),
