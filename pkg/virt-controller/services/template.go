207317cc22eb93c999d88fa5cff5cb65242796ef
Ram Lavi <ralavi@redhat.com>
2022-08-04 12:26:32 +0300

    virt-controller, Expose network-pci-map with DownwardAPI
    Expose network-pci-map annotation using DownwardAPI feature [0].
    
    [0] https://kubernetes.io/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/virt-controller/services/template.go
+++ pkg/virt-controller/services/template.go
@@ -48,6 +48,7 @@ import (
 	containerdisk "kubevirt.io/kubevirt/pkg/container-disk"
 	"kubevirt.io/kubevirt/pkg/hooks"
 	"kubevirt.io/kubevirt/pkg/network/istio"
+	"kubevirt.io/kubevirt/pkg/network/vmispec"
 	"kubevirt.io/kubevirt/pkg/util"
 	"kubevirt.io/kubevirt/pkg/util/hardware"
 	"kubevirt.io/kubevirt/pkg/util/net/dns"
@@ -1037,6 +1038,10 @@ func (t *templateService) newVolumeRenderer(vmi *v1.VirtualMachineInstance, name
 
 	if !vmi.Spec.Domain.Devices.DisableHotplug {
 		volumeOpts = append(volumeOpts, withHotplugSupport(t.hotplugDiskDir))
+	}
+
+	if vmispec.SRIOVInterfaceExist(vmi.Spec.Domain.Devices.Interfaces) {
+		volumeOpts = append(volumeOpts, withSRIOVPciMapAnnotation())
 	}
 
 	volumeRenderer, err := NewVolumeRenderer(
