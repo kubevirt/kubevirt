9c4e0c3238df4d18125648f3c535a2a04c1d49df
Ram Lavi <ralavi@redhat.com>
2022-08-09 20:42:06 +0300

    virt-launcher, sriov: Use new Network to PCI map with fallback
    Utilize the SRIOV-network PCI-address mapping that was
    digested from the multus network-status annotation and mounted to a
    volume, in order to create the SRIOV devices.
    If the map does not exist, then the mapping will fall back to the former
    approach.
    
    Solves issue [0] reported on flawed mapping of sriov on VMIs
    with multiple SRIOV networks.
    
    [0] https://github.com/kubevirt/kubevirt/issues/7444
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

---
+++ pkg/virt-launcher/virtwrap/device/hostdevice/sriov/pcipool_netstatus.go
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2022 Red Hat, Inc.
+ *
+ */
+
+package sriov
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type PCIAddressWithNetworkStatusPool struct {
+	networkPCIMap map[string]string
+}
+
+// NewPCIAddressPoolWithNetworkStatus creates a PCI address pool based on the networkPciMapPath volume
+func NewPCIAddressPoolWithNetworkStatus(networkPCIMapBytes []byte) (*PCIAddressWithNetworkStatusPool, error) {
+	pool := &PCIAddressWithNetworkStatusPool{}
+
+	var networkPciMap map[string]string
+	err := json.Unmarshal(networkPCIMapBytes, &networkPciMap)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal network-pci map %w", err)
+	}
+
+	pool.networkPCIMap = networkPciMap
+	return pool, nil
+}
+
+// Len returns the length of the pool.
+func (p *PCIAddressWithNetworkStatusPool) Len() int {
+	if p == nil {
+		return 0
+	}
+	return len(p.networkPCIMap)
+}
+
+// Pop gets the next PCI address available to a particular SR-IOV network. The
+// function makes sure that the allocated address is not allocated to other networks.
+func (p *PCIAddressWithNetworkStatusPool) Pop(networkName string) (string, error) {
+	pciAddress, exists := p.networkPCIMap[networkName]
+	if !exists {
+		return "", fmt.Errorf("PCI-Address for SR-IOV network %q not found", networkName)
+	}
+
+	if pciAddress == "" {
+		return "", fmt.Errorf("failed to allocate SR-IOV PCI address for network %q", networkName)
+	}
+	delete(p.networkPCIMap, networkName)
+	return pciAddress, nil
+}
