ccddc69a1fa365ccd153ec8bfb44d4230cdd5527
L. Pivarc <lpivarc@redhat.com>
2022-08-11 09:49:48 +0200

    Set SeccompProfile
    Be compliant with PSA restricted. This can
    be applied to virt-api, virt-operator,
    virt-controller.
    
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- pkg/virt-operator/resource/generate/components/deployments.go
+++ pkg/virt-operator/resource/generate/components/deployments.go
@@ -286,7 +286,8 @@ func NewApiServerDeployment(namespace string, repository string, imagePrefix str
 	pod := &deployment.Spec.Template.Spec
 	pod.ServiceAccountName = rbac.ApiServiceAccountName
 	pod.SecurityContext = &corev1.PodSecurityContext{
-		RunAsNonRoot: boolPtr(true),
+		RunAsNonRoot:   boolPtr(true),
+		SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 	}
 
 	container := &deployment.Spec.Template.Spec.Containers[0]
@@ -336,14 +337,13 @@ func NewApiServerDeployment(namespace string, repository string, imagePrefix str
 		},
 	}
 
-	if container.SecurityContext == nil {
-		container.SecurityContext = &corev1.SecurityContext{}
+	container.SecurityContext = &corev1.SecurityContext{
+		AllowPrivilegeEscalation: pointer.Bool(false),
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{"ALL"},
+		},
+		SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 	}
-	container.SecurityContext.AllowPrivilegeEscalation = pointer.Bool(false)
-	container.SecurityContext.Capabilities = &corev1.Capabilities{
-		Drop: []corev1.Capability{"ALL"},
-	}
-
 	return deployment, nil
 }
 
@@ -360,7 +360,8 @@ func NewControllerDeployment(namespace string, repository string, imagePrefix st
 	pod := &deployment.Spec.Template.Spec
 	pod.ServiceAccountName = rbac.ControllerServiceAccountName
 	pod.SecurityContext = &corev1.PodSecurityContext{
-		RunAsNonRoot: boolPtr(true),
+		RunAsNonRoot:   boolPtr(true),
+		SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 	}
 
 	launcherVersion = AddVersionSeparatorPrefix(launcherVersion)
@@ -424,14 +425,13 @@ func NewControllerDeployment(namespace string, repository string, imagePrefix st
 		},
 	}
 
-	if container.SecurityContext == nil {
-		container.SecurityContext = &corev1.SecurityContext{}
+	container.SecurityContext = &corev1.SecurityContext{
+		AllowPrivilegeEscalation: pointer.Bool(false),
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{"ALL"},
+		},
+		SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 	}
-	container.SecurityContext.AllowPrivilegeEscalation = pointer.Bool(false)
-	container.SecurityContext.Capabilities = &corev1.Capabilities{
-		Drop: []corev1.Capability{"ALL"},
-	}
-
 	return deployment, nil
 }
 
@@ -550,11 +550,13 @@ func NewOperatorDeployment(namespace string, repository string, imagePrefix stri
 								Capabilities: &corev1.Capabilities{
 									Drop: []corev1.Capability{"ALL"},
 								},
+								SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 							},
 						},
 					},
 					SecurityContext: &corev1.PodSecurityContext{
-						RunAsNonRoot: boolPtr(true),
+						RunAsNonRoot:   boolPtr(true),
+						SeccompProfile: &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault},
 					},
 				},
 			},
