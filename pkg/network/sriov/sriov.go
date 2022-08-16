b4841ccd30c3ce245973899872ad51953277ee7a
Ram Lavi <ralavi@redhat.com>
2022-08-16 11:59:46 +0300

    network, sriov: Map sriov devices to pci addresses
    Create a map between SRIOV networks and their appropriate
    PCI-Address, using the multus network-status annotation.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/network/sriov/sriov.go
+++ pkg/network/sriov/sriov.go
@@ -19,4 +19,77 @@
 
 package sriov
 
+import (
+	"encoding/json"
+	"fmt"
+
+	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	v1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
+
+	"kubevirt.io/kubevirt/pkg/network/namescheme"
+	"kubevirt.io/kubevirt/pkg/network/vmispec"
+)
+
 const AliasPrefix = "sriov-"
+
+func CreateNetworkPCIAnnotationValue(networks []v1.Network, interfaces []v1.Interface, networkStatusAnnotationValue string) string {
+	networkPCIMap, err := mapNetworkNameToPCIAddress(networks, interfaces, networkStatusAnnotationValue)
+	if err != nil {
+		log.Log.Warningf("failed to create network-pci-map: %v", err)
+		networkPCIMap = map[string]string{}
+	}
+
+	networkPCIMapBytes, err := json.Marshal(networkPCIMap)
+	if err != nil {
+		log.Log.Warningf("failed to marshal network-pci-map: %v", err)
+		return ""
+	}
+
+	return string(networkPCIMapBytes)
+}
+
+func mapNetworkNameToPCIAddress(networks []v1.Network, interfaces []v1.Interface,
+	networkStatusAnnotationValue string) (map[string]string, error) {
+	multusInterfaceNameToNetworkStatusMap, err := mapMultusInterfaceNameToNetworkStatus(networkStatusAnnotationValue)
+	if err != nil {
+		return nil, err
+	}
+	networkNameScheme := namescheme.CreateNetworkNameScheme(networks)
+
+	networkPCIMap := map[string]string{}
+	for _, sriovIface := range vmispec.FilterSRIOVInterfaces(interfaces) {
+		multusInterfaceName := networkNameScheme[sriovIface.Name]
+		networkStatusEntry, exist := multusInterfaceNameToNetworkStatusMap[multusInterfaceName]
+		if !exist {
+			return nil, fmt.Errorf("failed to find network-status entry with interface %q", multusInterfaceName)
+		}
+		if networkStatusEntry.DeviceInfo == nil || networkStatusEntry.DeviceInfo.Pci == nil {
+			return nil, fmt.Errorf("failed to find device-info/pci-address in network-status annotation for SR-IOV interface %q", sriovIface.Name)
+		}
+
+		pciAddress := networkStatusEntry.DeviceInfo.Pci.PciAddress
+		if pciAddress == "" {
+			return nil, fmt.Errorf("failed to associate pci-address to SR-IOV interface %q", sriovIface.Name)
+		}
+		networkPCIMap[sriovIface.Name] = pciAddress
+	}
+	return networkPCIMap, nil
+}
+
+func mapMultusInterfaceNameToNetworkStatus(networkStatusAnnotationValue string) (map[string]networkv1.NetworkStatus, error) {
+	if networkStatusAnnotationValue == "" {
+		return nil, fmt.Errorf("network-status annotation is not present")
+	}
+	var networkStatusList []networkv1.NetworkStatus
+	if err := json.Unmarshal([]byte(networkStatusAnnotationValue), &networkStatusList); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal network-status annotation: %v", err)
+	}
+
+	multusInterfaceNameToNetworkStatusMap := map[string]networkv1.NetworkStatus{}
+	for _, networkStatus := range networkStatusList {
+		multusInterfaceNameToNetworkStatusMap[networkStatus.Interface] = networkStatus
+	}
+
+	return multusInterfaceNameToNetworkStatusMap, nil
+}
