da50cd9d6dbb9ef437ed2ded5ab57aa60609dc7f
fossedihelm <ffossemo@redhat.com>
2022-08-23 15:36:27 +0200

    e2e test: Cover tlsConfiguration with functest
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- tests/infra_test.go
+++ tests/infra_test.go
@@ -34,6 +34,8 @@ import (
 	"strings"
 	"time"
 
+	kvtls "kubevirt.io/kubevirt/pkg/util/tls"
+
 	"kubevirt.io/kubevirt/tests/framework/checks"
 
 	expect "github.com/google/goexpect"
@@ -369,6 +371,66 @@ var _ = Describe("[Serial][sig-compute]Infrastructure", func() {
 			Entry("[test_id:4105] virt-handlers client side", components.VirtHandlerCertSecretName),
 			Entry("[test_id:4106] virt-handlers server side", components.VirtHandlerServerCertSecretName),
 		)
+	})
+
+	Describe("tls configuration", func() {
+
+		var cipher *tls.CipherSuite
+
+		BeforeEach(func() {
+			cipher = tls.CipherSuites()[0]
+			kvConfig := util.GetCurrentKv(virtClient).Spec.Configuration.DeepCopy()
+			kvConfig.TLSConfiguration = &v1.TLSConfiguration{
+				MinTLSVersion: v1.VersionTLS12,
+				Ciphers:       []string{cipher.Name},
+			}
+			tests.UpdateKubeVirtConfigValueAndWait(*kvConfig)
+			newKv := util.GetCurrentKv(virtClient)
+			Expect(newKv.Spec.Configuration.TLSConfiguration.MinTLSVersion).To(BeEquivalentTo(v1.VersionTLS12))
+			Expect(newKv.Spec.Configuration.TLSConfiguration.Ciphers).To(BeEquivalentTo([]string{cipher.Name}))
+
+		})
+
+		It("should result only connections with the correct client-side tls configurations are accepted by the components", func() {
+			labelSelectorList := []string{"kubevirt.io=virt-api", "kubevirt.io=virt-handler"}
+
+			var podsToTest []k8sv1.Pod
+			for _, labelSelector := range labelSelectorList {
+				podList, err := virtClient.CoreV1().Pods(flags.KubeVirtInstallNamespace).List(context.Background(), metav1.ListOptions{
+					LabelSelector: labelSelector,
+				})
+				Expect(err).ToNot(HaveOccurred())
+				podsToTest = append(podsToTest, podList.Items...)
+			}
+
+			for i, pod := range podsToTest {
+				func(i int, pod k8sv1.Pod) {
+					stopChan := make(chan struct{})
+					defer close(stopChan)
+					Expect(tests.ForwardPorts(&pod, []string{fmt.Sprintf("844%d:%d", i, 8443)}, stopChan, 10*time.Second)).To(Succeed())
+
+					acceptedTLSConfig := &tls.Config{
+						InsecureSkipVerify: true,
+						MaxVersion:         tls.VersionTLS12,
+						CipherSuites:       kvtls.CipherSuiteIds([]string{cipher.Name}),
+					}
+					conn, err := tls.Dial("tcp", fmt.Sprintf("localhost:844%d", i), acceptedTLSConfig)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(conn).ToNot(BeNil())
+					Expect(conn.ConnectionState().Version).To(BeEquivalentTo(tls.VersionTLS12))
+					Expect(conn.ConnectionState().CipherSuite).To(BeEquivalentTo(cipher.ID))
+
+					rejectedTLSConfig := &tls.Config{
+						InsecureSkipVerify: true,
+						MaxVersion:         tls.VersionTLS11,
+					}
+					conn, err = tls.Dial("tcp", fmt.Sprintf("localhost:844%d", i), rejectedTLSConfig)
+					Expect(err).To(HaveOccurred())
+					Expect(conn).To(BeNil())
+					Expect(err.Error()).To(BeEquivalentTo("remote error: tls: protocol version not supported"))
+				}(i, pod)
+			}
+		})
 	})
 
 	// start a VMI, wait for it to run and return the node it runs on
