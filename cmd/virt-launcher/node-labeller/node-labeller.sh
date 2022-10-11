e391ff0793d99101fcf86f73dd65004c59b3df6d
liuminjian <liuminjian@chinatelecom.cn>
2022-10-11 17:29:20 +0800

    更新node-labeller.sh兼容aarch64
    Signed-off-by: liuminjian <liuminjian@chinatelecom.cn>

--- cmd/virt-launcher/node-labeller/node-labeller.sh
+++ cmd/virt-launcher/node-labeller/node-labeller.sh
@@ -21,9 +21,17 @@ fi
 
 libvirtd -d
 
-virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/virsh_domcapabilities.xml
+CPU_MODEL=`virsh nodeinfo|grep 'CPU model'|awk -F: '{print $2}'|xargs`
 
 cp -r /usr/share/libvirt/cpu_map /var/lib/kubevirt-node-labeller
 
-virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE | virsh hypervisor-cpu-baseline --features /dev/stdin --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/supported_features.xml
+if [[ "$CPU_MODEL" == "aarch64" ]]; then
+    virsh domcapabilities --machine virt --arch aarch64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/virsh_domcapabilities.xml
+    > /var/lib/kubevirt-node-labeller/supported_features.xml
+    sed -i "s/<mode name='host-model' supported='no'\/>/<mode name='host-model' supported='yes'>\n<model fallback='forbid'>Kunpeng<\/model>\n<vendor>Kunpeng<\/vendor>\n<\/mode>/g" /var/lib/kubevirt-node-labeller/virsh_domcapabilities.xml
+else
+    virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/virsh_domcapabilities.xml
+    virsh domcapabilities --machine q35 --arch x86_64 --virttype $VIRTTYPE | virsh hypervisor-cpu-baseline --features /dev/stdin --machine q35 --arch x86_64 --virttype $VIRTTYPE > /var/lib/kubevirt-node-labeller/supported_features.xml
+fi
+
 virsh capabilities > /var/lib/kubevirt-node-labeller/capabilities.xml
