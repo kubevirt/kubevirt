79cf2e14f7b1b1f5d65bddf139e511ca2c7a6db7
Marcelo Tosatti <mtosatti@redhat.com>
2022-10-11 11:26:21 -0300

    virt-handler: cgroup: have GetCgroupThreads as a Manager method
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -2613,7 +2613,7 @@ func (d *VirtualMachineController) configureHousekeepingCgroup(vmi *v1.VirtualMa
 		return err
 	}
 
-	tids, err := cgroup.GetCgroupThreads(cgroupManager)
+	tids, err := cgroupManager.GetCgroupThreads()
 	if err != nil {
 		return err
 	}
