0db3037cc911d1cbc7f94f20a637d7f6bf5b06fd
Jed Lejosne <jed@redhat.com>
2022-10-17 08:47:33 -0400

    Bump memory requests according to value seen in testing
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-operator/resource/generate/components/daemonsets.go
+++ pkg/virt-operator/resource/generate/components/daemonsets.go
@@ -266,7 +266,7 @@ func NewHandlerDaemonSet(namespace string, repository string, imagePrefix string
 	container.Resources = corev1.ResourceRequirements{
 		Requests: corev1.ResourceList{
 			corev1.ResourceCPU:    resource.MustParse("10m"),
-			corev1.ResourceMemory: resource.MustParse("230Mi"),
+			corev1.ResourceMemory: resource.MustParse("300Mi"),
 		},
 	}
 
