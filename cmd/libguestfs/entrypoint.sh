c3302dd7a48b2a3d5f098437922d8902c28694e7
Alice Frosi <afrosi@redhat.com>
2022-11-10 11:39:16 +0100

    Use libguestfs appliance with qcow2 root
    Libguestfs package available in centos stream 9 supports using the root
    in qcow2 format. This allows to keep the libguestfs-tools image with a
    reasonable size (~550MB), and at the same time it avoids to extract the
    appliance at runtime.
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- cmd/libguestfs/entrypoint.sh
+++ cmd/libguestfs/entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash -xe
 
-LIBGUESTFS_PATH=${LIBGUESTFS_PATH:=/tmp/guestfs}
-LIBGUEST_APPLIANCE=/usr/local/lib/guestfs/downloaded
-mkdir -p ${LIBGUESTFS_PATH}
-tar -Jxf ${LIBGUEST_APPLIANCE} -C ${LIBGUESTFS_PATH} --strip-components=1
-
-touch ${LIBGUESTFS_PATH}/done
-
 /bin/bash
