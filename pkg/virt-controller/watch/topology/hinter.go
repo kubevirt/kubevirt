ddf8bf1932a53bf7acfc118fb56449cbdc661f59
Itamar Holder <iholder@redhat.com>
2022-11-10 11:55:01 +0200

    Set frequency as 0 if TSC frequency is available on the cluster
    That's how we can distinguish between us waiting for a TSC frequency
    and knowing there is no TSC frequency information, hence we can
    stop waiting for it.
    
    This is necessary, since the vmi controller executes "sync()" and
    "updateStatus()" in every control loop. If TSC frequency is required,
    sync() would wait until topology hints would be set up by
    updateStatus(). Today, updateStatus() doesn't update the topology
    hints at all if no TSC frequency information exists, therefore
    sync will continue waiting forever and the VM wouldn't be able
    to start.
    
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-controller/watch/topology/hinter.go
+++ pkg/virt-controller/watch/topology/hinter.go
@@ -59,9 +59,6 @@ func (t *topologyHinter) LowestTSCFrequencyOnCluster() (int64, error) {
 		HasInvTSCFrequency,
 	)
 	freq := LowestTSCFrequency(nodes)
-	if freq == 0 {
-		return 0, fmt.Errorf("no schedulable node exposes a tsc-frequency")
-	}
 	return freq, nil
 }
 
@@ -69,7 +66,7 @@ func (t *topologyHinter) TSCFrequenciesInUse() []int64 {
 	frequencyMap := map[int64]struct{}{}
 	for _, obj := range t.vmiStore.List() {
 		vmi := obj.(*k6tv1.VirtualMachineInstance)
-		if vmi.Status.TopologyHints != nil && vmi.Status.TopologyHints.TSCFrequency != nil {
+		if AreTSCFrequencyTopologyHintsDefined(vmi) {
 			frequencyMap[*vmi.Status.TopologyHints.TSCFrequency] = struct{}{}
 		}
 	}
