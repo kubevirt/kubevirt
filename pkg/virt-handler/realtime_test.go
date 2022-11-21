e6be3274c78ba3e2ddfc73c594a7e311f40fd33c
Jordi Gil <jgil@redhat.com>
2022-11-21 10:14:49 -0500

    Tweaks to virt-handler/realtime_test.go
    Signed-off-by: Jordi Gil <jgil@redhat.com>

--- pkg/virt-handler/realtime_test.go
+++ pkg/virt-handler/realtime_test.go
@@ -60,44 +60,56 @@ var _ = Describe("Running real time workloads", func() {
 			Entry("Fails to parse a negative vcpu ID", "-1", nil, fmt.Errorf("invalid mask value '-1' in '-1'")),
 			Entry("Fails to parse a negative vcpu ID combined with a valid index", "0,-1", nil, fmt.Errorf("invalid mask value '-1' in '0,-1'")),
 			// With range
-			Entry("Extracts a single range with one core", "0-0", cpuMask{map[string]maskType{"0": enabled}}, nil),
-			Entry("Extracts a single range multiple cores", "0-2", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled}}, nil),
-			Entry("Extracts a multiple non overlapping ranges", "0-2,5-6", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "5": enabled, "6": enabled}}, nil),
-			Entry("Extracts a multiple overlapping ranges", "0-2,2-3", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "3": enabled}}, nil),
-			Entry("Extracts a single range with double digit vcpu IDs", "10-12", cpuMask{map[string]maskType{"10": enabled, "11": enabled, "12": enabled}}, nil),
-			Entry("Extracts a multiple range with double digit vcpu IDs", "10-12,20,21", cpuMask{map[string]maskType{"10": enabled, "11": enabled, "12": enabled, "20": enabled, "21": enabled}}, nil),
-			Entry("Extracts a multiple range with double digit vcpu IDs spaced in between", "10-12 ,  20,21", cpuMask{map[string]maskType{"10": enabled, "11": enabled, "12": enabled, "20": enabled, "21": enabled}}, nil),
-			Entry("Extracts a single range prefixed and postfixed with spaces", "  0-2  ", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled}}, nil),
-			Entry("Extracts a single range with a range of 1 vcpu only", "0-0", cpuMask{map[string]maskType{"0": enabled}}, nil),
+			Entry("Extracts a single range with one core", "0-0", newMask([]string{"0"}, nil), nil),
+			Entry("Extracts a single range multiple cores", "0-2", newMask([]string{"0", "1", "2"}, nil), nil),
+			Entry("Extracts a multiple non overlapping ranges", "0-2,5-6", newMask([]string{"0", "1", "2", "5", "6"}, nil), nil),
+			Entry("Extracts a multiple overlapping ranges", "0-2,2-3", newMask([]string{"0", "1", "2", "3"}, nil), nil),
+			Entry("Extracts a single range with double digit vcpu IDs", "10-12", newMask([]string{"10", "11", "12"}, nil), nil),
+			Entry("Extracts a multiple range with double digit vcpu IDs", "10-12,20,21", newMask([]string{"10", "11", "12", "20", "21"}, nil), nil),
+			Entry("Extracts a multiple range with double digit vcpu IDs spaced in between", "10-12 ,  20,21", newMask([]string{"10", "11", "12", "20", "21"}, nil), nil),
+			Entry("Extracts a single range prefixed and postfixed with spaces", "  0-2  ", newMask([]string{"0", "1", "2"}, nil), nil),
+			Entry("Extracts a single range with a range of 1 vcpu only", "0-0", newMask([]string{"0"}, nil), nil),
 			// Single vcpus
-			Entry("Extracts a single vCPU index id", "0", cpuMask{map[string]maskType{"0": enabled}}, nil),
-			Entry("Extracts a multiple single vCPU index ids", "0,1,2", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled}}, nil),
-			Entry("Extracts a single double digit vcpu index id", "10", cpuMask{map[string]maskType{"10": enabled}}, nil),
+			Entry("Extracts a single vCPU index id", "0", newMask([]string{"0"}, nil), nil),
+			Entry("Extracts a multiple single vCPU index ids", "0,1,2", newMask([]string{"0", "1", "2"}, nil), nil),
+			Entry("Extracts a single double digit vcpu index id", "10", newMask([]string{"10"}, nil), nil),
 			// Negate vcpus
-			Entry("Extracts a single negate vCPU", "^3", cpuMask{map[string]maskType{"3": disabled}}, nil),
-			Entry("Extracts a multiple negate vCPUs", "^0,^1", cpuMask{map[string]maskType{"0": disabled, "1": disabled}}, nil),
-			Entry("Extracts a multiple overlapping negate vCPUs", "^0,^1,^0", cpuMask{map[string]maskType{"0": disabled, "1": disabled}}, nil),
-			Entry("Extracts a double digit negate vCPU", "^13", cpuMask{map[string]maskType{"13": disabled}}, nil),
-			Entry("Extracts a multiple double digit negate vCPU ids", "^13,^15", cpuMask{map[string]maskType{"13": disabled, "15": disabled}}, nil),
+			Entry("Extracts a single negate vCPU", "^3", newMask(nil, []string{"3"}), nil),
+			Entry("Extracts a multiple negate vCPUs", "^0,^1", newMask(nil, []string{"0", "1"}), nil),
+			Entry("Extracts a multiple overlapping negate vCPUs", "^0,^1,^0", newMask(nil, []string{"0", "1"}), nil),
+			Entry("Extracts a double digit negate vCPU", "^13", newMask(nil, []string{"13"}), nil),
+			Entry("Extracts a multiple double digit negate vCPU ids", "^13,^15", newMask(nil, []string{"13", "15"}), nil),
 			// Combination of range and single Ids
-			Entry("Extracts a single range and individual ID without overlaping index but in sequence", "0-2,3", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "3": enabled}}, nil),
-			Entry("Extracts a single range and individual ID with overlaping index", "0-2,1", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled}}, nil),
-			Entry("Extracts a single range and individual ID without overlaping but out of sequence", "5,1-2,6", cpuMask{map[string]maskType{"1": enabled, "2": enabled, "5": enabled, "6": enabled}}, nil),
-			Entry("Extracts a single range and individual ID without overlaping but out of sequence with spaces", " 6 , 1-2 , 5 ", cpuMask{map[string]maskType{"1": enabled, "2": enabled, "5": enabled, "6": enabled}}, nil),
+			Entry("Extracts a single range and individual ID without overlaping index but in sequence", "0-2,3", newMask([]string{"0", "1", "2", "3"}, nil), nil),
+			Entry("Extracts a single range and individual ID with overlaping index", "0-2,1", newMask([]string{"0", "1", "2"}, nil), nil),
+			Entry("Extracts a single range and individual ID without overlaping but out of sequence", "5,1-2,6", newMask([]string{"1", "2", "5", "6"}, nil), nil),
+			Entry("Extracts a single range and individual ID without overlaping but out of sequence with spaces", " 6 , 1-2 , 5 ", newMask([]string{"1", "2", "5", "6"}, nil), nil),
 			// Combination of single Ids and negate index
-			Entry("Extracts a single vCPU index with a negated id", "0,^1", cpuMask{map[string]maskType{"0": enabled, "1": disabled}}, nil),
-			Entry("Extracts a multiple vCPU index and negated ids", "0,1,2,^3,^4", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "3": disabled, "4": disabled}}, nil),
-			Entry("Extracts a multiple vCPU index with an overlapping negated id sandwitched in between", "0,^0,1", cpuMask{map[string]maskType{"0": disabled, "1": enabled}}, nil),
-			Entry("Extracts a single vCPU index with overlapping negated id with negated as first entry", "^0,0", cpuMask{map[string]maskType{"0": disabled}}, nil),
+			Entry("Extracts a single vCPU index with a negated id", "0,^1", newMask([]string{"0"}, []string{"1"}), nil),
+			Entry("Extracts a multiple vCPU index and negated ids", "0,1,2,^3,^4", newMask([]string{"0", "1", "2"}, []string{"3", "4"}), nil),
+			Entry("Extracts a multiple vCPU index with an overlapping negated id sandwitched in between", "0,^0,1", newMask([]string{"1"}, []string{"0"}), nil),
+			Entry("Extracts a single vCPU index with overlapping negated id with negated as first entry", "^0,0", newMask(nil, []string{"0"}), nil),
 			// Combination of range and negate index
-			Entry("Extracts a single range and a negate id outside range", "0-2,^3", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "3": disabled}}, nil),
-			Entry("Extracts a single range and a negate id within range", "0-2,^1", cpuMask{map[string]maskType{"0": enabled, "1": disabled, "2": enabled}}, nil),
-			Entry("Extracts a multiple range and two negate ids within range", "^1,0-2, 3-6,^4 ", cpuMask{map[string]maskType{"0": enabled, "1": disabled, "2": enabled, "3": enabled, "4": disabled, "5": enabled, "6": enabled}}, nil),
+			Entry("Extracts a single range and a negate id outside range", "0-2,^3", newMask([]string{"0", "1", "2"}, []string{"3"}), nil),
+			Entry("Extracts a single range and a negate id within range", "0-2,^1", newMask([]string{"0", "2"}, []string{"1"}), nil),
+			Entry("Extracts a multiple range and two negate ids within range", "^1,0-2, 3-6,^4 ", newMask([]string{"0", "2", "3", "5", "6"}, []string{"1", "4"}), nil),
 			// Combination of all 3 expressions
-			Entry("Extracts a single range, single index and negate ids without overlap", "^10,0-2,3", cpuMask{map[string]maskType{"0": enabled, "1": enabled, "2": enabled, "3": enabled, "10": disabled}}, nil),
-			Entry("Extracts a multiple range, multiple index and multiple negate ids without overlap", "^1,0-2,^5,3,10,6-6", cpuMask{map[string]maskType{"0": enabled, "1": disabled, "2": enabled, "3": enabled, "5": disabled, "6": enabled, "10": enabled}}, nil),
-			Entry("Extracts a multiple range, multiple index and negate ids with overlap", "^1,0-3,^3,4,5,6-8,^7", cpuMask{mask: map[string]maskType{"0": enabled, "1": disabled, "2": enabled, "3": disabled, "4": enabled, "5": enabled, "6": enabled, "7": disabled, "8": enabled}}, nil),
+			Entry("Extracts a single range, single index and negate ids without overlap", "^10,0-2,3", newMask([]string{"0", "1", "2", "3"}, []string{"10"}), nil),
+			Entry("Extracts a multiple range, multiple index and multiple negate ids without overlap", "^1,0-2,^5,3,10,6-6", newMask([]string{"0", "2", "3", "6", "10"}, []string{"1", "5"}), nil),
+			Entry("Extracts a multiple range, multiple index and negate ids with overlap", "^1,0-3,^3,4,5,6-8,^7", newMask([]string{"0", "2", "4", "5", "6", "8"}, []string{"1", "3", "7"}), nil),
 		)
 	})
 
 })
+
+func newMask(cpuEnabled, cpuDisabled []string) cpuMask {
+	m := make(map[string]maskType)
+
+	for _, i := range cpuEnabled {
+		m[i] = enabled
+	}
+	for _, i := range cpuDisabled {
+		m[i] = disabled
+	}
+	return cpuMask{mask: m}
+}
