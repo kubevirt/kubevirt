74442d9510e020bf9935e861126d295303e1a50f
fossedihelm <ffossemo@redhat.com>
2022-11-29 16:50:13 +0100

    Add defined decorators
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

---
+++ tests/decorators/decorators.go
@@ -0,0 +1,25 @@
+package decorators
+
+import . "github.com/onsi/ginkgo/v2"
+
+var (
+	SigCompute           = []interface{}{Label("sig-compute")}
+	SigOperator          = []interface{}{Label("sig-operator")}
+	SigNetwork           = []interface{}{Label("sig-network")}
+	SigStorage           = []interface{}{Label("sig-storage")}
+	SigComputeRealtime   = []interface{}{Label("sig-compute-realtime")}
+	SigComputeMigrations = []interface{}{Label("sig-compute-migrations")}
+	SigMonitoring        = []interface{}{Label("sig-monitoring")}
+	StorageReq           = []interface{}{Label("storage-req")}
+	Sysprep              = []interface{}{Label("Sysprep")}
+	Windows              = []interface{}{Label("Windows")}
+	Multus               = []interface{}{Label("Multus")}
+	Networking           = []interface{}{Label("Networking")}
+	VMIlifecycle         = []interface{}{Label("VMIlifecycle")}
+	Expose               = []interface{}{Label("Expose")}
+	Macvtap              = []interface{}{Label("Macvtap")}
+	GPU                  = []interface{}{Label("GPU")}
+	VGPU                 = []interface{}{Label("VGPU")}
+	SRIOV                = []interface{}{Label("SRIOV")}
+	NonRoot              = []interface{}{Label("verify-non-root")}
+)
