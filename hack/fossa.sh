df9f2c77c59be8cd079ee08d1b8e420274f33c62
Alex Kalenyuk <akalenyu@redhat.com>
2022-12-21 17:27:32 +0200

    Bump FOSSA deprecated CLI
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/fossa.sh
+++ hack/fossa.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
+pwd
+git log --oneline | head
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.sh | bash
 FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa analyze
 FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa test --timeout=1800
