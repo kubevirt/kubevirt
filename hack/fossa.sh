29deaf4661fab41e16d432108bfc56a64737bcdc
Daniel Hiller <dhiller@redhat.com>
2023-01-17 12:58:42 +0100

    Add branch inside ci run
    Since inside prow we are in a detached head state we add the branch
    name.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/fossa.sh
+++ hack/fossa.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.sh | bash
-FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa analyze
+FOSSA_OPTS=""
+if [[ "${CI:-}" == "true" ]]; then
+    FOSSA_OPTS="--branch=$PULL_BASE_REF"
+fi
+FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa analyze $FOSSA_OPTS
 FOSSA_API_KEY="$(cat $FOSSA_TOKEN_FILE)" fossa test --timeout=1800
