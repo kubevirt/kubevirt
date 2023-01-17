39ce841ff06656a7ef259ec9165240b35c60000b
Lee Yarwood <lyarwood@redhat.com>
2023-01-17 11:43:14 +0000

    instancetype: Cleanup instancetype_test.go
    - Remove pointless whitespace where possible
    - Use better names for some test variables
    - Remove duplicate import of k8s.io/apimachinery/pkg/util/validation/field
    - Remove redundant types from slice elements
    - Remove useless conversion of ClockOffset to a pointer
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- pkg/instancetype/instancetype.go
+++ pkg/instancetype/instancetype.go
@@ -263,7 +263,7 @@ func (m *InstancetypeMethods) StoreControllerRevisions(vm *virtv1.VirtualMachine
 		return err
 	}
 	if len(patch) > 0 {
-		if _, err := m.clientset.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, patch, &metav1.PatchOptions{}); err != nil {
+		if _, err := m.Clientset.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, patch, &metav1.PatchOptions{}); err != nil {
 			logger().Reason(err).Error("Failed to update VirtualMachine with instancetype and preference ControllerRevision references.")
 			return err
 		}
