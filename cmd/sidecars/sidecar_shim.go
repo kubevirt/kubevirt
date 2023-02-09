b48b56100a7eda29905f754d3cbdb62141a95bbc
Victor Toso <victortoso@redhat.com>
2023-02-09 18:21:21 +0100

    Create sidecar-shim binary and Container
    The idea is to have a base container that can be used to quickly write
    sidecars without gRPC boilerplate.
    
    The sidecar-shim binary implements the gRPC boilerplate. The user of
    sidecar-shim-image should put his code in $PATH with the name of the
    hook he wants to execute (e.g: /usr/bin/onDefineDomain)
    
    The follow-up commit moves the smbios sidecar to use this image.
    
    https://github.com/kubevirt/kubevirt/pull/6975
    Signed-off-by: Victor Toso <victortoso@redhat.com>

---
+++ cmd/sidecars/sidecar_shim.go
@@ -0,0 +1,194 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ *
+ */
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+	"google.golang.org/grpc"
+	"k8s.io/apimachinery/pkg/util/rand"
+
+	virtv1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
+
+	"kubevirt.io/kubevirt/pkg/hooks"
+	hooksInfo "kubevirt.io/kubevirt/pkg/hooks/info"
+	hooksV1alpha1 "kubevirt.io/kubevirt/pkg/hooks/v1alpha1"
+	hooksV1alpha2 "kubevirt.io/kubevirt/pkg/hooks/v1alpha2"
+)
+
+const (
+	onDefineDomainLoggingMessage = "OnDefineDomain method has been called"
+
+	onDefineDomainBin = "onDefineDomain"
+)
+
+type infoServer struct {
+	Version string
+}
+
+func (s infoServer) Info(ctx context.Context, params *hooksInfo.InfoParams) (*hooksInfo.InfoResult, error) {
+	log.Log.Info("Info method has been called")
+	return &hooksInfo.InfoResult{
+		Name: "shim",
+		Versions: []string{
+			s.Version,
+		},
+		HookPoints: []*hooksInfo.HookPoint{
+			{
+				Name:     hooksInfo.OnDefineDomainHookPointName,
+				Priority: 0,
+			},
+		},
+	}, nil
+}
+
+type v1Alpha1Server struct{}
+type v1Alpha2Server struct{}
+
+func (s v1Alpha2Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha2.OnDefineDomainParams) (*hooksV1alpha2.OnDefineDomainResult, error) {
+	log.Log.Info(onDefineDomainLoggingMessage)
+	newDomainXML, err := runOnDefineDomain(params.GetVmi(), params.GetDomainXML())
+	if err != nil {
+		return nil, err
+	}
+	return &hooksV1alpha2.OnDefineDomainResult{
+		DomainXML: newDomainXML,
+	}, nil
+}
+func (s v1Alpha2Server) PreCloudInitIso(_ context.Context, params *hooksV1alpha2.PreCloudInitIsoParams) (*hooksV1alpha2.PreCloudInitIsoResult, error) {
+	log.Log.Info("PreCloudInitIso method has been called")
+	return &hooksV1alpha2.PreCloudInitIsoResult{
+		CloudInitData: params.GetCloudInitData(),
+	}, nil
+}
+
+func (s v1Alpha1Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha1.OnDefineDomainParams) (*hooksV1alpha1.OnDefineDomainResult, error) {
+	log.Log.Info(onDefineDomainLoggingMessage)
+	newDomainXML, err := runOnDefineDomain(params.GetVmi(), params.GetDomainXML())
+	if err != nil {
+		return nil, err
+	}
+	return &hooksV1alpha1.OnDefineDomainResult{
+		DomainXML: newDomainXML,
+	}, nil
+}
+
+func runOnDefineDomain(vmiJSON []byte, domainXML []byte) ([]byte, error) {
+	if _, err := exec.LookPath(onDefineDomainBin); err != nil {
+		return nil, fmt.Errorf("Failed in finding %s in $PATH: %s", onDefineDomainBin, err.Error())
+	}
+
+	vmiSpec := virtv1.VirtualMachineInstance{}
+	if err := json.Unmarshal(vmiJSON, &vmiSpec); err != nil {
+		log.Log.Reason(err).Errorf("Failed to unmarshal given VMI spec: %s", vmiJSON)
+		panic(err)
+	}
+
+	args := append([]string{},
+		"--vmi", string(vmiJSON),
+		"--domain", string(domainXML))
+
+	log.Log.Infof("Executing %s", onDefineDomainBin)
+	command := exec.Command(onDefineDomainBin, args...)
+	return command.Output()
+}
+
+func parseCommandLineArgs() (string, error) {
+	supportedVersions := []string{"v1alpha1", "v1alpha2"}
+	version := ""
+
+	pflag.StringVar(&version, "version", "", "hook version to use")
+	pflag.Parse()
+	if version == "" {
+		return "", fmt.Errorf("Missing --version parameter. Supported options are %s.", supportedVersions)
+	}
+
+	supported := false
+	for _, v := range supportedVersions {
+		if v == version {
+			supported = true
+			break
+		}
+	}
+	if !supported {
+		return "", fmt.Errorf("Version %s is not supported. Supported options are %s.", version, supportedVersions)
+	}
+
+	return version, nil
+}
+
+func getSocketPath() (string, error) {
+	if _, err := os.Stat(hooks.HookSocketsSharedDirectory); err != nil {
+		return "", fmt.Errorf("Failed dir %s due %s", hooks.HookSocketsSharedDirectory, err.Error())
+	}
+
+	// In case there are multiple shims being used, append random string and try a few times
+	for i := 0; i < 10; i++ {
+		socketName := fmt.Sprintf("shim-%s.sock", rand.String(4))
+		socketPath := filepath.Join(hooks.HookSocketsSharedDirectory, socketName)
+		if _, err := os.Stat(socketPath); !errors.Is(err, os.ErrNotExist) {
+			log.Log.Infof("Failed socket %s due %s", socketName, err.Error())
+			continue
+		}
+		return socketPath, nil
+	}
+
+	return "", fmt.Errorf("Failed generate socket path")
+}
+
+func main() {
+	log.InitializeLogging("shim-sidecar")
+
+	// Shim arguments
+	version, err := parseCommandLineArgs()
+	if err != nil {
+		log.Log.Reason(err).Errorf("Input error")
+		os.Exit(1)
+	}
+
+	socketPath, err := getSocketPath()
+	if err != nil {
+		log.Log.Reason(err).Errorf("Enviroment error")
+		os.Exit(1)
+	}
+
+	socket, err := net.Listen("unix", socketPath)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to initialized socket on path: %s", socket)
+		os.Exit(1)
+	}
+	defer os.Remove(socketPath)
+
+	server := grpc.NewServer([]grpc.ServerOption{}...)
+	hooksInfo.RegisterInfoServer(server, infoServer{Version: version})
+	hooksV1alpha1.RegisterCallbacksServer(server, v1Alpha1Server{})
+	hooksV1alpha2.RegisterCallbacksServer(server, v1Alpha2Server{})
+	log.Log.Infof("shim is now exposing its services on socket %s", socketPath)
+	server.Serve(socket)
+}
