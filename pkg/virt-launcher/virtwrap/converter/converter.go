0de845f92975ebcb8415caa889921578d33fdb2b
Vladik Romanovsky <vromanso@redhat.com>
2023-03-06 22:06:33 -0500

    set the disabled and hotpluggable vcpus based on the MaxSockets
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- pkg/virt-launcher/virtwrap/converter/converter.go
+++ pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1202,10 +1202,32 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 	// in vmi.Spec.Domain.CPU
 	cpuTopology := vcpu.GetCPUTopology(vmi)
 	cpuCount := vcpu.CalculateRequestedVCPUs(cpuTopology)
+	enabledCpuCount := cpuCount
+	// set the maximum number of sockets here to allow hot-plug CPUs
+	vmiCPU := vmi.Spec.Domain.CPU
+	if vmiCPU.MaxSockets != 0 {
+		cpuTopology.Sockets = uint32(vmiCPU.MaxSockets)
+		cpuCount = vcpu.CalculateRequestedVCPUs(cpuTopology)
+	}
 	domain.Spec.CPU.Topology = cpuTopology
 	domain.Spec.VCPU = &api.VCPU{
 		Placement: "static",
 		CPUs:      cpuCount,
+	}
+
+	if vmiCPU.MaxSockets != 0 {
+		VCPUs := &api.VCPUs{}
+		for id := uint32(0); id < cpuCount; id++ {
+			isEnabled := id < enabledCpuCount
+			isHotpluggable := !isEnabled
+			vcpu := api.VCPUsVCPU{
+				ID:           uint32(id),
+				Enabled:      boolToYesNo(&isEnabled, true),
+				Hotpluggable: boolToYesNo(&isHotpluggable, false),
+			}
+			VCPUs.VCPU = append(VCPUs.VCPU, vcpu)
+		}
+		domain.Spec.VCPUs = VCPUs
 	}
 
 	kvmPath := "/dev/kvm"
