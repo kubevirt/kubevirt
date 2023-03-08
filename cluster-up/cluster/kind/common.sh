a28a20e1611ff5dd1083eda08148a0441f730ce2
Or Shoval <oshoval@redhat.com>
2023-03-08 14:00:11 +0200

    Revert "WIP: check if vgpu need cpu manager"
    This reverts commit 4d88db40f2529fa087f214a5bb0cc884724e1d7c.

--- cluster-up/cluster/kind/common.sh
+++ cluster-up/cluster/kind/common.sh
@@ -242,12 +242,27 @@ EOF
   fi
 }
 
+function _add_worker_kubeadm_config_patch() {
+    cat << EOF >> ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
+  kubeadmConfigPatches:
+  - |-
+    kind: JoinConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        "feature-gates": "CPUManager=true"
+        "cpu-manager-policy": "static"
+        "kube-reserved": "cpu=500m"
+        "system-reserved": "cpu=500m"
+EOF
+}
+
 function _add_workers() {
     # appending eventual workers to the yaml
     for ((n=0;n<$(($KUBEVIRT_NUM_NODES-1));n++)); do
         cat << EOF >> ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
 - role: worker
 EOF
+    _add_worker_kubeadm_config_patch
     _add_worker_extra_mounts
     done
 }
