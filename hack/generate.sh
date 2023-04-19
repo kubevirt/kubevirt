937112bfb1c799f0f53d18fd831484b07bd878a6
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2023-04-19 09:58:39 +0300

    fix sanity test (#2321)
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/generate.sh
+++ hack/generate.sh
@@ -11,24 +11,27 @@ k8s.io/code-generator/cmd/openapi-gen@${K8S_VER}
 
 deepcopy-gen \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}/../../.." \
+	--output-base "${PROJECT_ROOT}" \
 	--output-file-base zz_generated.deepcopy \
 	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1
+	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
+        --trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
 
 defaulter-gen \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}/../../.." \
+	--output-base "${PROJECT_ROOT}" \
 	--output-file-base zz_generated.defaults \
 	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1
+	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
+  --trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
 
 openapi-gen \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}/../../.." \
+	--output-base "${PROJECT_ROOT}" \
 	--output-file-base zz_generated.openapi \
 	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1
+	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
+        --trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
 
 go fmt api/v1beta1/zz_generated.deepcopy.go
 go fmt api/v1beta1/zz_generated.defaults.go
