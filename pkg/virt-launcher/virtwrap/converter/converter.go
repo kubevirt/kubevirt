42afbbfdf72ce693364db115161d1468e50e4c42
liuminjian <liuminjian@chinatelecom.cn>
2023-04-20 14:40:03 +0800

    支持qcow2启动虚拟机
    Signed-off-by: liuminjian <liuminjian@chinatelecom.cn>

--- pkg/virt-launcher/virtwrap/converter/converter.go
+++ pkg/virt-launcher/virtwrap/converter/converter.go
@@ -697,6 +697,7 @@ func Convert_v1_Hotplug_Volume_To_api_Disk(source *v1.Volume, disk *api.Disk, c 
 func Convert_v1_Config_To_api_Disk(volumeName string, disk *api.Disk, configType config.Type) error {
 	disk.Type = "file"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_Config_To_api_Disk")
 	disk.Driver.ErrorPolicy = "stop"
 	switch configType {
 	case config.ConfigMap:
@@ -738,6 +739,7 @@ func GetHotplugBlockDeviceVolumePath(volumeName string) string {
 
 func Convert_v1_PersistentVolumeClaim_To_api_Disk(name string, disk *api.Disk, c *ConverterContext) error {
 	if c.IsBlockPVC[name] {
+		log.Log.Info("IsBlockPVC")
 		return Convert_v1_BlockVolumeSource_To_api_Disk(name, disk, c.VolumesDiscardIgnore)
 	}
 	return Convert_v1_FilesystemVolumeSource_To_api_Disk(name, disk, c.VolumesDiscardIgnore)
@@ -770,8 +772,9 @@ func Convert_v1_Hotplug_DataVolume_To_api_Disk(name string, disk *api.Disk, c *C
 func Convert_v1_FilesystemVolumeSource_To_api_Disk(volumeName string, disk *api.Disk, volumesDiscardIgnore []string) error {
 	disk.Type = "file"
 	if disk.Driver.Type == "qcow2" {
-
+		log.Log.Info("disk.Driver.Type == qcow2")
 	} else {
+		log.Log.Info("disk.Driver.Type == raw")
 		disk.Driver.Type = "raw"
 	}
 	// disk.Driver.ErrorPolicy = "stop"
@@ -786,6 +789,7 @@ func Convert_v1_FilesystemVolumeSource_To_api_Disk(volumeName string, disk *api.
 func Convert_v1_Hotplug_FilesystemVolumeSource_To_api_Disk(volumeName string, disk *api.Disk, volumesDiscardIgnore []string) error {
 	disk.Type = "file"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_Hotplug_FilesystemVolumeSource_To_api_Disk")
 	// disk.Driver.ErrorPolicy = "stop"
 	if !contains(volumesDiscardIgnore, volumeName) {
 		disk.Driver.Discard = "unmap"
@@ -797,6 +801,7 @@ func Convert_v1_Hotplug_FilesystemVolumeSource_To_api_Disk(volumeName string, di
 func Convert_v1_BlockVolumeSource_To_api_Disk(volumeName string, disk *api.Disk, volumesDiscardIgnore []string) error {
 	disk.Type = "block"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_BlockVolumeSource_To_api_Disk")
 	// disk.Driver.ErrorPolicy = "stop"
 	if !contains(volumesDiscardIgnore, volumeName) {
 		disk.Driver.Discard = "unmap"
@@ -810,6 +815,7 @@ func Convert_v1_BlockVolumeSource_To_api_Disk(volumeName string, disk *api.Disk,
 func Convert_v1_Hotplug_BlockVolumeSource_To_api_Disk(volumeName string, disk *api.Disk, volumesDiscardIgnore []string) error {
 	disk.Type = "block"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_Hotplug_BlockVolumeSource_To_api_Disk")
 	// disk.Driver.ErrorPolicy = "stop"
 	if !contains(volumesDiscardIgnore, volumeName) {
 		disk.Driver.Discard = "unmap"
@@ -821,6 +827,7 @@ func Convert_v1_Hotplug_BlockVolumeSource_To_api_Disk(volumeName string, disk *a
 func Convert_v1_HostDisk_To_api_Disk(volumeName string, path string, disk *api.Disk) error {
 	disk.Type = "file"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_HostDisk_To_api_Disk")
 	// disk.Driver.ErrorPolicy = "stop"
 	disk.Source.File = hostdisk.GetMountedHostDiskPath(volumeName, path)
 	return nil
@@ -834,6 +841,7 @@ func Convert_v1_SysprepSource_To_api_Disk(volumeName string, disk *api.Disk) err
 	disk.Source.File = config.GetSysprepDiskPath(volumeName)
 	disk.Type = "file"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_SysprepSource_To_api_Disk")
 	return nil
 }
 
@@ -854,6 +862,7 @@ func Convert_v1_CloudInitSource_To_api_Disk(source v1.VolumeSource, disk *api.Di
 	disk.Source.File = cloudinit.GetIsoFilePath(dataSource, c.VirtualMachine.Name, c.VirtualMachine.Namespace)
 	disk.Type = "file"
 	disk.Driver.Type = "raw"
+	log.Log.Info("Convert_v1_CloudInitSource_To_api_Disk")
 	// disk.Driver.ErrorPolicy = "stop"
 	return nil
 }
@@ -1543,13 +1552,13 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 		if volume == nil {
 			return fmt.Errorf("No matching volume with name %s found", disk.Name)
 		}
-		log.Log.Object(vmi).Infof("disk %v", newDisk.Driver.Type)
+		log.Log.Object(vmi).Infof("disk %s %v", disk.Name, newDisk.Driver.Type)
 		if _, ok := c.HotplugVolumes[disk.Name]; !ok {
 			err = Convert_v1_Volume_To_api_Disk(volume, &newDisk, c, volumeIndices[disk.Name])
 		} else {
 			err = Convert_v1_Hotplug_Volume_To_api_Disk(volume, &newDisk, c)
 		}
-		log.Log.Object(vmi).Infof("disk %v", newDisk.Driver.Type)
+		log.Log.Object(vmi).Infof("disk %s %v", disk.Name, newDisk.Driver.Type)
 		if err != nil {
 			return err
 		}
