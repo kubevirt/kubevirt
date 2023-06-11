82044b0d6bc54196281bc0201fb4bea7cc8e4f63
Or Shoval <oshoval@redhat.com>
2023-06-11 12:14:29 +0300

    extend fedora mem
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/libvmi/factory.go
+++ tests/libvmi/factory.go
@@ -32,7 +32,7 @@ import (
 // This image has tooling for the guest agent, stress, SR-IOV and more.
 func NewFedora(opts ...Option) *kvirtv1.VirtualMachineInstance {
 	fedoraOptions := []Option{
-		WithResourceMemory("512M"),
+		WithResourceMemory("1024Mi"),
 		WithRng(),
 		WithContainerImage(cd.ContainerDiskFor(cd.ContainerDiskFedoraTestTooling)),
 	}
