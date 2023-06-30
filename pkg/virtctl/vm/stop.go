50f694ad49f8ace24a0788bec09b351dcf0f5898
prnaraya <prnaraya@redhat.com>
2023-06-30 12:20:09 -0500

    Reduced complexity of nested if/else statements for Stop command
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- pkg/virtctl/vm/stop.go
+++ pkg/virtctl/vm/stop.go
@@ -65,19 +65,17 @@ func (o *Command) stopRun(args []string, cmd *cobra.Command) error {
 		dryRunOption = []string{metav1.DryRunAll}
 		fmt.Printf("Dry Run execution\n")
 	}
+	
+	gracePeriodChanged := cmd.Flags().Changed(gracePeriodArg)
 
-	if cmd.Flags().Changed(gracePeriodArg) && forceRestart == false {
-		return fmt.Errorf("Can not set gracePeriod without --force=true")
+	if gracePeriodChanged != forceRestart {
+		return fmt.Errorf("Must both use --force=true and set --grace-period.")
 	}
 
 	if forceRestart {
-		if cmd.Flags().Changed(gracePeriodArg) {
-			err = virtClient.VirtualMachine(namespace).ForceStop(context.Background(), vmiName, &v1.StopOptions{GracePeriod: &gracePeriod, DryRun: dryRunOption})
-			if err != nil {
-				return fmt.Errorf("Error force stopping VirtualMachine, %v", err)
-			}
-		} else if !cmd.Flags().Changed(gracePeriodArg) {
-			return fmt.Errorf("Can not force stop without gracePeriod")
+		err = virtClient.VirtualMachine(namespace).ForceStop(context.Background(), vmiName, &v1.StopOptions{GracePeriod: &gracePeriod, DryRun: dryRunOption})
+		if err != nil {
+			return fmt.Errorf("Error force stopping VirtualMachine, %v", err)
 		}
 	} else {
 		err = virtClient.VirtualMachine(namespace).Stop(context.Background(), vmiName, &v1.StopOptions{DryRun: dryRunOption})
