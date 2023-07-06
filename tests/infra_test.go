4daea4c9d165ce55cd7e9155ef3ff9379939d360
Daniel Hiller <dhiller@redhat.com>
2023-07-06 12:48:53 +0200

    Extract certificates.go from infra_test.go
    Extract commonly used function to libinfra
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- tests/infra_test.go
+++ tests/infra_test.go
@@ -27,7 +27,6 @@ import (
 	"fmt"
 	"net/http"
 	neturl "net/url"
-	"reflect"
 	"sort"
 	"strconv"
 	"strings"
@@ -56,7 +55,6 @@ import (
 	"kubevirt.io/kubevirt/tests/libnode"
 	"kubevirt.io/kubevirt/tests/util"
 
-	"kubevirt.io/kubevirt/pkg/certificates/triple/cert"
 	"kubevirt.io/kubevirt/tests/libvmi"
 	"kubevirt.io/kubevirt/tests/libwait"
 
@@ -67,8 +65,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
-	"k8s.io/client-go/util/retry"
-
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
 
@@ -76,7 +72,6 @@ import (
 	clusterutil "kubevirt.io/kubevirt/pkg/util/cluster"
 	"kubevirt.io/kubevirt/pkg/virt-controller/leaderelectionconfig"
 	nodelabellerutil "kubevirt.io/kubevirt/pkg/virt-handler/node-labeller/util"
-	"kubevirt.io/kubevirt/pkg/virt-operator/resource/generate/components"
 	"kubevirt.io/kubevirt/tests"
 	"kubevirt.io/kubevirt/tests/console"
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
@@ -105,129 +100,6 @@ var _ = Describe("[Serial][sig-compute]Infrastructure", Serial, decorators.SigCo
 
 			aggregatorClient = aggregatorclient.NewForConfigOrDie(config)
 		}
-	})
-
-	Describe("[rfe_id:4102][crit:medium][vendor:cnv-qe@redhat.com][level:component]certificates", func() {
-
-		It("[test_id:4099] should be rotated when a new CA is created", func() {
-			By("checking that the config-map gets the new CA bundle attached")
-			Eventually(func() int {
-				_, crts := tests.GetBundleFromConfigMap(components.KubeVirtCASecretName)
-				return len(crts)
-			}, 10*time.Second, 1*time.Second).Should(BeNumerically(">", 0))
-
-			By("destroying the certificate")
-			err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-				secret, err := virtClient.CoreV1().Secrets(flags.KubeVirtInstallNamespace).Get(context.Background(), components.KubeVirtCASecretName, metav1.GetOptions{})
-				Expect(err).ToNot(HaveOccurred())
-				secret.Data = map[string][]byte{
-					"tls.crt": []byte(""),
-					"tls.key": []byte(""),
-				}
-
-				_, err = virtClient.CoreV1().Secrets(flags.KubeVirtInstallNamespace).Update(context.Background(), secret, metav1.UpdateOptions{})
-				return err
-			})
-			Expect(err).ToNot(HaveOccurred())
-
-			By("checking that the CA secret gets restored with a new ca bundle")
-			var newCA []byte
-			Eventually(func() []byte {
-				newCA = tests.GetCertFromSecret(components.KubeVirtCASecretName)
-				return newCA
-			}, 10*time.Second, 1*time.Second).Should(Not(BeEmpty()))
-
-			By("checking that one of the CAs in the config-map is the new one")
-			var caBundle []byte
-			Eventually(func() bool {
-				caBundle, _ = tests.GetBundleFromConfigMap(components.KubeVirtCASecretName)
-				return containsCrt(caBundle, newCA)
-			}, 10*time.Second, 1*time.Second).Should(BeTrue(), "the new CA should be added to the config-map")
-
-			By("checking that the ca bundle gets propagated to the validating webhook")
-			Eventually(func() bool {
-				webhook, err := virtClient.AdmissionregistrationV1().ValidatingWebhookConfigurations().Get(context.Background(), components.VirtAPIValidatingWebhookName, metav1.GetOptions{})
-				Expect(err).ToNot(HaveOccurred())
-				if len(webhook.Webhooks) > 0 {
-					return containsCrt(webhook.Webhooks[0].ClientConfig.CABundle, newCA)
-				}
-				return false
-			}, 10*time.Second, 1*time.Second).Should(BeTrue())
-			By("checking that the ca bundle gets propagated to the mutating webhook")
-			Eventually(func() bool {
-				webhook, err := virtClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.Background(), components.VirtAPIMutatingWebhookName, metav1.GetOptions{})
-				Expect(err).ToNot(HaveOccurred())
-				if len(webhook.Webhooks) > 0 {
-					return containsCrt(webhook.Webhooks[0].ClientConfig.CABundle, newCA)
-				}
-				return false
-			}, 10*time.Second, 1*time.Second).Should(BeTrue())
-
-			By("checking that the ca bundle gets propagated to the apiservice")
-			Eventually(func() bool {
-				apiService, err := aggregatorClient.ApiregistrationV1().APIServices().Get(context.Background(), fmt.Sprintf("%s.subresources.kubevirt.io", v1.ApiLatestVersion), metav1.GetOptions{})
-				Expect(err).ToNot(HaveOccurred())
-				return containsCrt(apiService.Spec.CABundle, newCA)
-			}, 10*time.Second, 1*time.Second).Should(BeTrue())
-
-			By("checking that we can still start virtual machines and connect to the VMI")
-			vmi := tests.NewRandomVMIWithEphemeralDisk(cd.ContainerDiskFor(cd.ContainerDiskAlpine))
-			vmi = tests.RunVMIAndExpectLaunch(vmi, 60)
-			Expect(console.LoginToAlpine(vmi)).To(Succeed())
-		})
-
-		It("[sig-compute][test_id:4100] should be valid during the whole rotation process", func() {
-			oldAPICert := tests.EnsurePodsCertIsSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-api"), flags.KubeVirtInstallNamespace, "8443")
-			oldHandlerCert := tests.EnsurePodsCertIsSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-handler"), flags.KubeVirtInstallNamespace, "8186")
-			Expect(err).ToNot(HaveOccurred())
-
-			By("destroying the CA certificate")
-			err = virtClient.CoreV1().Secrets(flags.KubeVirtInstallNamespace).Delete(context.Background(), components.KubeVirtCASecretName, metav1.DeleteOptions{})
-			Expect(err).ToNot(HaveOccurred())
-
-			By("repeatedly starting VMIs until virt-api and virt-handler certificates are updated")
-			Eventually(func() (rotated bool) {
-				vmi := tests.NewRandomVMIWithEphemeralDisk(cd.ContainerDiskFor(cd.ContainerDiskAlpine))
-				vmi = tests.RunVMIAndExpectLaunch(vmi, 60)
-				Expect(console.LoginToAlpine(vmi)).To(Succeed())
-				err = virtClient.VirtualMachineInstance(vmi.Namespace).Delete(context.Background(), vmi.Name, &metav1.DeleteOptions{})
-				Expect(err).ToNot(HaveOccurred())
-				newAPICert, _, err := tests.GetPodsCertIfSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-api"), flags.KubeVirtInstallNamespace, "8443")
-				Expect(err).ToNot(HaveOccurred())
-				newHandlerCert, _, err := tests.GetPodsCertIfSynced(fmt.Sprintf("%s=%s", v1.AppLabel, "virt-handler"), flags.KubeVirtInstallNamespace, "8186")
-				Expect(err).ToNot(HaveOccurred())
-				return !reflect.DeepEqual(oldHandlerCert, newHandlerCert) && !reflect.DeepEqual(oldAPICert, newAPICert)
-			}, 120*time.Second).Should(BeTrue())
-		})
-
-		DescribeTable("should be rotated when deleted for ", func(secretName string) {
-			By("destroying the certificate")
-			err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-				secret, err := virtClient.CoreV1().Secrets(flags.KubeVirtInstallNamespace).Get(context.Background(), secretName, metav1.GetOptions{})
-				if err != nil {
-					return err
-				}
-				secret.Data = map[string][]byte{
-					"tls.crt": []byte(""),
-					"tls.key": []byte(""),
-				}
-				_, err = virtClient.CoreV1().Secrets(flags.KubeVirtInstallNamespace).Update(context.Background(), secret, metav1.UpdateOptions{})
-
-				return err
-			})
-			Expect(err).ToNot(HaveOccurred())
-
-			By("checking that the secret gets restored with a new certificate")
-			Eventually(func() []byte {
-				return tests.GetCertFromSecret(secretName)
-			}, 10*time.Second, 1*time.Second).Should(Not(BeEmpty()))
-		},
-			Entry("[test_id:4101] virt-operator", components.VirtOperatorCertSecretName),
-			Entry("[test_id:4103] virt-api", components.VirtApiCertSecretName),
-			Entry("[test_id:4104] virt-controller", components.VirtControllerCertSecretName),
-			Entry("[test_id:4105] virt-handlers client side", components.VirtHandlerCertSecretName),
-			Entry("[test_id:4106] virt-handlers server side", components.VirtHandlerServerCertSecretName),
-		)
 	})
 
 	Describe("tls configuration", func() {
@@ -1761,18 +1633,4 @@ func getMetricKeyForVmiDisk(keys []string, vmiName string, diskName string) stri
 		}
 	}
 	return ""
-}
-
-func containsCrt(bundle []byte, containedCrt []byte) bool {
-	crts, err := cert.ParseCertsPEM(bundle)
-	Expect(err).ToNot(HaveOccurred())
-	attached := false
-	for _, crt := range crts {
-		crtBytes := cert.EncodeCertPEM(crt)
-		if reflect.DeepEqual(crtBytes, containedCrt) {
-			attached = true
-			break
-		}
-	}
-	return attached
 }
