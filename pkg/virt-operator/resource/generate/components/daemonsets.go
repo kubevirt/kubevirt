ca4c9b5d489d6928083e9e1cf2d9725d59794378
Vasiliy Ulyanov <vulyanov@suse.de>
2023-07-27 10:38:52 +0200

    Do not mount pr-helper-socket-vol to virt-handler
    It turns out that having two host path volumes originating at the same
    root (e.g. /var/run/kubevirt and /var/run/kubevirt/daemons/pr) in a pod
    and bind-mounted with bidirectional propagation to a container leads to
    side effects. That creates additional mount points on the host that are
    not cleaned up afterward:
    
    $ mount | grep daemon
    tmpfs on /run/kubevirt/daemons/pr type tmpfs (rw,nosuid,nodev,seclabel,mode=755)
    tmpfs on /run/kubevirt/daemons/pr type tmpfs (rw,nosuid,nodev,seclabel,mode=755)
    tmpfs on /run/kubevirt/daemons/pr type tmpfs (rw,nosuid,nodev,seclabel,mode=755)
    
    Since the virt-handler container already has the host path volume
    /var/run/kubevirt mounted, it can be used to access the pr-helper
    socket at /var/run/kubevirt/daemons/pr.
    
    Signed-off-by: Vasiliy Ulyanov <vulyanov@suse.de>

--- pkg/virt-operator/resource/generate/components/daemonsets.go
+++ pkg/virt-operator/resource/generate/components/daemonsets.go
@@ -276,9 +276,6 @@ func NewHandlerDaemonSet(namespace, repository, imagePrefix, version, launcherVe
 		{"kubelet-pods", kubeletPodsPath, kubeletPodsPath, &bidi},
 		{"node-labeller", "/var/lib/kubevirt-node-labeller", "/var/lib/kubevirt-node-labeller", nil},
 	}
-	if enablePrHelper {
-		volumes = append(volumes, volume{prVolumeName, reservation.GetPrHelperSocketDir(), reservation.GetPrHelperSocketDir(), &bidi})
-	}
 
 	for _, volume := range volumes {
 		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
@@ -328,6 +325,16 @@ func NewHandlerDaemonSet(namespace, repository, imagePrefix, version, launcherVe
 	}
 
 	if enablePrHelper {
+		directoryOrCreate := corev1.HostPathDirectoryOrCreate
+		pod.Volumes = append(pod.Volumes, corev1.Volume{
+			Name: prVolumeName,
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: reservation.GetPrHelperSocketDir(),
+					Type: &directoryOrCreate,
+				},
+			},
+		})
 		pod.Containers = append(pod.Containers, RenderPrHelperContainer(prHelperImage, pullPolicy))
 	}
 	return daemonset, nil
