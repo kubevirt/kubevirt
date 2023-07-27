ddbf6effcc85e231ac7815063eb02a48c4a0cdc6
Itamar Holder <iholder@redhat.com>
2023-07-27 13:04:06 +0300

    LowestTSCFrequencyOnCluster: Consider only schedulable nodes
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-controller/watch/topology/hinter.go
+++ pkg/virt-controller/watch/topology/hinter.go
@@ -56,6 +56,7 @@ func (t *topologyHinter) LowestTSCFrequencyOnCluster() (int64, error) {
 	}
 	nodes := FilterNodesFromCache(t.nodeStore.List(),
 		HasInvTSCFrequency,
+		IsSchedulable,
 	)
 	freq := LowestTSCFrequency(nodes)
 	return freq, nil
