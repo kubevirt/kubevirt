2a7e478aa20d7fad0f2b09c1a2d3d1bfa1c1f069
Itamar Holder <iholder@redhat.com>
2023-07-30 12:14:25 +0300

    Add Or() and IsNodeRunningVmis() predicates for TSC filtering
    Add two new filter predicates that would be used to filter
    nodes for TSC frequency calculations:
    
    Or(): Can be used to express logical or between multiple
    filters, e.g. Or(filter1, filter2).
    
    IsNodeRunningVmis(vmiStore): returns a filter that includes
    the node if VMIs are running on it.
    
    This way nodes that are considered for TSC frequency calculations
    need to be either schedulable or have VMIs running on it.
    
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-controller/watch/topology/filter.go
+++ pkg/virt-controller/watch/topology/filter.go
@@ -3,6 +3,8 @@ package topology
 import (
 	"math"
 
+	"k8s.io/client-go/tools/cache"
+
 	v1 "k8s.io/api/core/v1"
 
 	virtv1 "kubevirt.io/api/core/v1"
@@ -75,6 +77,17 @@ func Not(f FilterPredicateFunc) FilterPredicateFunc {
 	}
 }
 
+func Or(predicates ...FilterPredicateFunc) FilterPredicateFunc {
+	return func(node *v1.Node) bool {
+		for _, p := range predicates {
+			if p(node) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
 func FilterNodesFromCache(objs []interface{}, predicates ...FilterPredicateFunc) []*v1.Node {
 	match := []*v1.Node{}
 	for _, obj := range objs {
@@ -91,6 +104,22 @@ func FilterNodesFromCache(objs []interface{}, predicates ...FilterPredicateFunc)
 		}
 	}
 	return match
+}
+
+func IsNodeRunningVmis(vmiStore cache.Store) FilterPredicateFunc {
+	return func(node *v1.Node) bool {
+		if node == nil {
+			return false
+		}
+
+		for _, vmi := range vmiStore.List() {
+			vmi := vmi.(*virtv1.VirtualMachineInstance)
+			if vmi.Status.NodeName == node.Name {
+				return true
+			}
+		}
+		return false
+	}
 }
 
 // ToleranceForFrequency returns TSCTolerancePPM parts per million of freq, rounded down to the nearest Hz
