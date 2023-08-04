c8cd1ff258d57c9aad3ea6d2e5fc01d5efc67d5d
Hidehisa Shitomi <hidehisa.shtm@gmail.com>
2023-08-04 02:07:44 +0000

    virtctl: Add adm subcommand under virtctl, and log-verbosity subcommand under adm
    The log-verbosity command is:
    - to show the log verbosity of one or more components
    - to set the log verbosity of one or more components
    - to reset the log verbosity of all components
    
    Signed-off-by: Hidehisa Shitomi <hidehisa.shtm@gmail.com>

--- pkg/virtctl/root.go
+++ pkg/virtctl/root.go
@@ -13,6 +13,7 @@ import (
 	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/client-go/log"
 
+	"kubevirt.io/kubevirt/pkg/virtctl/adm"
 	"kubevirt.io/kubevirt/pkg/virtctl/configuration"
 	"kubevirt.io/kubevirt/pkg/virtctl/console"
 	"kubevirt.io/kubevirt/pkg/virtctl/create"
@@ -109,6 +110,7 @@ func NewVirtctlCommand() (*cobra.Command, clientcmd.ClientConfig) {
 		vmexport.NewVirtualMachineExportCommand(clientConfig),
 		create.NewCommand(clientConfig),
 		credentials.NewCommand(clientConfig),
+		adm.NewCommand(clientConfig),
 		optionsCmd,
 	)
 	return rootCmd, clientConfig
