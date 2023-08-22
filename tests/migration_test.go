37e75a2227652335788e8172bff03f2a0497ed0f
enp0s3 <ibezukh@redhat.com>
2023-08-22 15:49:31 +0300

    e2e: move helper function from product code to migration suite
    Signed-off-by: enp0s3 <ibezukh@redhat.com>

--- tests/migration_test.go
+++ tests/migration_test.go
@@ -98,7 +98,6 @@ import (
 	"kubevirt.io/kubevirt/pkg/certificates/triple"
 	"kubevirt.io/kubevirt/pkg/certificates/triple/cert"
 	"kubevirt.io/kubevirt/pkg/util/cluster"
-	migrations "kubevirt.io/kubevirt/pkg/util/migrations"
 	"kubevirt.io/kubevirt/tests"
 	"kubevirt.io/kubevirt/tests/console"
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
@@ -3813,7 +3812,7 @@ var _ = Describe("[rfe_id:393][crit:high][vendor:cnv-qe@redhat.com][level:system
 					migrationList, err := virtClient.VirtualMachineInstanceMigration(k8sv1.NamespaceAll).List(&metav1.ListOptions{})
 					Expect(err).ToNot(HaveOccurred())
 
-					runningMigrations := migrations.FilterRunningMigrations(migrationList.Items)
+					runningMigrations := filterRunningMigrations(migrationList.Items)
 
 					return len(runningMigrations)
 				}, 2*time.Minute, 1*time.Second).Should(BeNumerically(">", 0))
@@ -3829,7 +3828,7 @@ var _ = Describe("[rfe_id:393][crit:high][vendor:cnv-qe@redhat.com][level:system
 					migrationList, err := virtClient.VirtualMachineInstanceMigration(k8sv1.NamespaceAll).List(&metav1.ListOptions{})
 					Expect(err).ToNot(HaveOccurred())
 
-					runningMigrations := migrations.FilterRunningMigrations(migrationList.Items)
+					runningMigrations := filterRunningMigrations(migrationList.Items)
 					Expect(len(runningMigrations)).To(BeNumerically("<=", 2))
 
 					return nodes
@@ -4986,4 +4985,14 @@ func affinityToMigrateFromSourceToTargetAndBack(sourceNode *k8sv1.Node, targetNo
 			},
 		},
 	}, nil
+}
+
+func filterRunningMigrations(migrations []v1.VirtualMachineInstanceMigration) []v1.VirtualMachineInstanceMigration {
+	runningMigrations := []v1.VirtualMachineInstanceMigration{}
+	for _, migration := range migrations {
+		if migration.IsRunning() {
+			runningMigrations = append(runningMigrations, migration)
+		}
+	}
+	return runningMigrations
 }
