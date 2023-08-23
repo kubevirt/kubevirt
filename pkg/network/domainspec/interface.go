94f1b13026ccbc4c6c4ad6328202091b353f1382
Or Mergi <ormergi@redhat.com>
2023-08-23 13:31:06 +0300

    network setup passt: Execute passt through Libvirt API
    As of today, Kubevirt executed Passt and QRAP emulator manually
    for VMs with Passt interface.
    
    The new Libvirt API for Passt [1] enable creating VMs with Passt
    interfaces, and its no longer needed to execute QRAP and Passt manually.
    
    This change replaces the manual execution of Passt and QRAP emulator with
    generating the Libvirt domain spec Passt interface.
    
    - Passt logs are now stored at /var/run/kubevirt/passt.log inside virt-launcher.
      Previously, Passt logs were appended to virt-laucnher log by listening to Passt
      process stderr.
      Using the new API requires waiting for the Passt log file to created which is
      when the domain starts.
    
    - Configuring passt interface object is extracted to prevent collecting passt
      logs in tests.
    
    [1] https://libvirt.org/formatdomain.html#userspace-slirp-or-passt-connection
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

---
+++ pkg/network/domainspec/interface.go
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2021 Red Hat, Inc.
+ *
+ */
+
+package domainspec
+
+import "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
+
+func LookupIfaceByAliasName(ifaces []api.Interface, name string) *api.Interface {
+	for i, iface := range ifaces {
+		if iface.Alias != nil && iface.Alias.GetName() == name {
+			return &ifaces[i]
+		}
+	}
+
+	return nil
+}
