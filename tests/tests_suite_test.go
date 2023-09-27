edeb6be20890064585c4cb445a38f4c17f46a711
Luboslav Pivarc <lpivarc@redhat.com>
2023-09-27 22:48:30 +0200

    Tests: Introduce sig-compute
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- tests/tests_suite_test.go
+++ tests/tests_suite_test.go
@@ -41,6 +41,7 @@ import (
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	_ "kubevirt.io/kubevirt/tests/compute"
 	_ "kubevirt.io/kubevirt/tests/guestlog"
 	_ "kubevirt.io/kubevirt/tests/hotplug"
 	_ "kubevirt.io/kubevirt/tests/infrastructure"
