4e5b0455730e9f42fa7c6819d6805373258d3b88
Alice Frosi <afrosi@redhat.com>
2023-10-09 11:08:13 +0200

    launcher: generate XML for migrated disks
    Update the libvirt schema with the migrated disks.
    
    For the volume migration classification, the local disks to migrated need
    also to be taken into account.
    
    If the source and destination disks are of different types, like
    one of type filesystem and the other is a block. Their type needs also
    to be update in the migrate XML.
    
    The destination disk could have equal or larger size in comparison to
    the source.
    Currently, libvirt refuses to migrate if the source and destination size don't
    match. To workaround this limiation, we set the slices option in the XML to
    select the portion of the image equal to the source size. See:
      https://issues.redhat.com/browse/RHEL-4607
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/virt-launcher/virtwrap/manager_test.go
+++ pkg/virt-launcher/virtwrap/manager_test.go
@@ -32,7 +32,9 @@ import (
 	"strings"
 	"time"
 
+	storagev1alpha1 "kubevirt.io/api/storage/v1alpha1"
 	"kubevirt.io/kubevirt/pkg/network/vmispec"
+
 	"kubevirt.io/kubevirt/pkg/testutils"
 	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 
@@ -2632,6 +2634,70 @@ var _ = Describe("migratableDomXML", func() {
 
 		By("ensuring the generated XML is accurate")
 		Expect(newXML).To(Equal(expectedXML), "the target XML is not as expected")
+	})
+	It("should add the slices section", func() {
+		retDiskSize := func(disk *api.Disk) (int64, error) {
+			return 2028994560, nil
+		}
+		getDiskVirtualSizeFunc = retDiskSize
+		const (
+			volName       = "datavolumedisk1"
+			sourcePvcName = "src-pvc"
+			destPvcName   = "dst-pvc"
+		)
+		domXML := `<domain type="kvm" id="1">
+  <name>kubevirt</name>
+  <devices>
+    <disk type='file' device='disk' model='virtio-non-transitional'>
+      <driver name='qemu' type='raw' cache='none' error_policy='stop' discard='unmap'/>
+      <source file='/var/run/kubevirt-private/vmi-disks/datavolumedisk1/disk.img' index='1'/>
+      <backingStore/>
+      <target dev='vda' bus='virtio'/>
+      <alias name='ua-datavolumedisk1'/>
+      <address type='pci' domain='0x0000' bus='0x07' slot='0x00' function='0x0'/>
+    </disk>
+  </devices>
+</domain>`
+		// migratableDomXML() removes the migration block but not its ident, which is its own token, hence the blank line below
+		expectedXML := `<domain type="kvm" id="1">
+  <name>kubevirt</name>
+  <devices>
+    <disk type="file" device="disk" model="virtio-non-transitional">
+      <driver name="qemu" type="raw" cache="none" error_policy="stop" discard="unmap"></driver>
+      <source file="/var/run/kubevirt-private/vmi-disks/datavolumedisk1/disk.img">
+        <slices>
+          <slice type="storage" offset="0" size="2028994560"></slice>
+        </slices>
+      </source>
+      <backingStore></backingStore>
+      <target dev="vda" bus="virtio"></target>
+      <alias name="ua-datavolumedisk1"></alias>
+      <address type="pci" domain="0x0000" bus="0x07" slot="0x00" function="0x0"></address>
+    </disk>
+  </devices>
+</domain>`
+		vmi := newVMI("testns", "kubevirt")
+		vmi.Spec.Volumes = append(vmi.Spec.Volumes,
+			v1.Volume{
+				Name: volName,
+				VolumeSource: v1.VolumeSource{
+					DataVolume: &v1.DataVolumeSource{
+						Name: sourcePvcName,
+					},
+				},
+			})
+		vmi.Status.MigratedVolumes = []storagev1alpha1.MigratedVolume{
+			{
+				SourcePvc:      sourcePvcName,
+				DestinationPvc: destPvcName,
+			},
+		}
+		mockDomain.EXPECT().GetXMLDesc(libvirt.DOMAIN_XML_MIGRATABLE).MaxTimes(1).Return(domXML, nil)
+		domSpec := &api.DomainSpec{}
+		Expect(xml.Unmarshal([]byte(domXML), domSpec)).To(Succeed())
+		newXML, err := migratableDomXML(mockDomain, vmi, domSpec)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(newXML).To(Equal(expectedXML))
 	})
 })
 
