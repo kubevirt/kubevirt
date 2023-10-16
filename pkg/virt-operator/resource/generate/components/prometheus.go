52ee6bd2fbdcc4d1fbee451e98abfb646d3bfe50
João Vilaça <jvilaca@redhat.com>
2023-10-16 10:19:11 +0100

    Add default 0 value to kubevirt_nodes_with_kvm
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

--- pkg/virt-operator/resource/generate/components/prometheus.go
+++ pkg/virt-operator/resource/generate/components/prometheus.go
@@ -560,7 +560,7 @@ func GetRecordingRules(namespace string) []KubevirtRecordingRule {
 		{
 			Rule: v1.Rule{
 				Record: "kubevirt_nodes_with_kvm",
-				Expr:   intstr.FromString("count(kube_node_status_allocatable{resource=\"devices_kubevirt_io_kvm\"} != 0)"),
+				Expr:   intstr.FromString("count(kube_node_status_allocatable{resource=\"devices_kubevirt_io_kvm\"} != 0) or vector(0)"),
 			},
 			MType:       prometheusv1.MetricTypeGauge,
 			Description: "The number of nodes in the cluster that have the devices.kubevirt.io/kvm resource available.",
