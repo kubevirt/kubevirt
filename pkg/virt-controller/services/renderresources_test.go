24b0e3cf0722e75a7c7b991d5f4f2a15c1641181
Ram Lavi <ralavi@redhat.com>
2023-10-19 14:29:15 +0300

    virt-controller: Add full-pcpus-only support to IsolateEmulatorThread
    When the VMI is created on a node where the CPUManager policy is
    full-pcpus-only [0], the virt-launcher pod can only have full physical
    cores, otherwise the kubelet will reject it [1].
    This can cause internal conflict when setting "IsolateEmulatorThread:
    true" on the VMI spec. The reason is that IsolateEmulatorThread adds an
    extra emulator CPU to the virt-launcher pod, and can thus cause an
    alignment error.
    
    The conditions needed in order to get this alignment error:
    * The node has hyperthreading (SMT) enabled.
    * Kubelet’s CPUManager is set to static - full-pcpus-only.
    * Kubevirt CR's defaultRuntimeClass is set to a high performance
    runtimeClass.
    * VM is set with dedicatedCPUPlacement and IsolateEmulatorThread
    enabled.
    * VM requests an even number of CPUs.
    
    In order to avoid this conflict - adding an annotation
    "cpuManagerPolicyBetaOptions: full-pcpus-only".
    When annotation set and IsolateEmulatorThread enabled, The
    virt-controller makes sure that the number of CPUs the virt-launcher pod
    requests are even, ensuring that there is no SMT misalignment.
    The meaning of this is that instead of IsolateEmulatorThread adding an
    extra CPU to the virt-launcher pod, it will add two CPUs when needed in
    order to get a total number of even CPUs.
    
    Fixes BZ#2228103
    
    [0]
    https://kubernetes.io/docs/tasks/administer-cluster/cpu-management-policies/#static-policy-options
    [1] https://github.com/kubernetes/kubernetes/pull/101432
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/virt-controller/services/renderresources_test.go
+++ pkg/virt-controller/services/renderresources_test.go
@@ -171,21 +171,22 @@ var _ = Describe("Resource pod spec renderer", func() {
 	})
 
 	Context("WithCPUPinning option", func() {
+		const cpuManagerPolicyBetaOptionsAnnotationDisabled = ""
 		userCPURequest := resource.MustParse("200m")
 		userSpecifiedCPU := kubev1.ResourceList{kubev1.ResourceCPU: userCPURequest}
 
 		It("the user requested CPU configs are *not* overriden", func() {
-			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{Cores: 5}))
+			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{Cores: 5}, cpuManagerPolicyBetaOptionsAnnotationDisabled))
 			Expect(rr.Requests()).To(HaveKeyWithValue(kubev1.ResourceCPU, userCPURequest))
 		})
 
 		It("carries over the CPU limits as requests when no CPUs are requested", func() {
-			rr = NewResourceRenderer(userSpecifiedCPU, nil, WithCPUPinning(&v1.CPU{}))
+			rr = NewResourceRenderer(userSpecifiedCPU, nil, WithCPUPinning(&v1.CPU{}, cpuManagerPolicyBetaOptionsAnnotationDisabled))
 			Expect(rr.Requests()).To(HaveKeyWithValue(kubev1.ResourceCPU, userCPURequest))
 		})
 
 		It("carries over the CPU requests as limits when no CPUs are requested", func() {
-			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{}))
+			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{}, cpuManagerPolicyBetaOptionsAnnotationDisabled))
 			Expect(rr.Requests()).To(HaveKeyWithValue(kubev1.ResourceCPU, userCPURequest))
 		})
 
@@ -195,41 +196,50 @@ var _ = Describe("Resource pod spec renderer", func() {
 				kubev1.ResourceCPU:    userCPURequest,
 				kubev1.ResourceMemory: memoryRequest,
 			}
-			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{Cores: 5}))
+			rr = NewResourceRenderer(nil, userSpecifiedCPU, WithCPUPinning(&v1.CPU{Cores: 5}, cpuManagerPolicyBetaOptionsAnnotationDisabled))
 			Expect(rr.Requests()).To(HaveKeyWithValue(kubev1.ResourceCPU, resource.MustParse("200m")))
 			Expect(rr.Limits()).To(HaveKeyWithValue(kubev1.ResourceMemory, memoryRequest))
 		})
 
 		When("an isolated emulator thread is requested", func() {
-			cpuIsolatedEmulatorThreadOverhead := resource.MustParse("1000m")
 			userSpecifiedCPURequest := kubev1.ResourceList{kubev1.ResourceCPU: userCPURequest}
 
-			DescribeTable("requires an additional 1000m CPU, and an additional CPU is added to the limits", func(defineUserSpecifiedCPULimit bool) {
+			DescribeTable("requires additional EmulatorThread CPUs overhead, and additional CPUs added to the limits",
+				func(fullPCPUEnabled, defineUserSpecifiedCPULimit bool, cores uint32, expectedCPUOverhead string) {
+					cpuIsolatedEmulatorThreadOverhead := resource.MustParse(expectedCPUOverhead)
+					var userSpecifiedCPULimit kubev1.ResourceList
+					var cpuManagerPolicyBetaOption v1.CPUManagerPolicyBetaOptions
 
-				var userSpecifiedCPULimit kubev1.ResourceList
-
-				if defineUserSpecifiedCPULimit {
-					userSpecifiedCPULimit = kubev1.ResourceList{kubev1.ResourceCPU: userCPURequest}
-				}
-				rr = NewResourceRenderer(
-					userSpecifiedCPULimit,
-					userSpecifiedCPURequest,
-					WithCPUPinning(&v1.CPU{
-						Cores:                 5,
-						IsolateEmulatorThread: true,
-					}),
-				)
-				Expect(rr.Limits()).To(HaveKeyWithValue(
-					kubev1.ResourceCPU,
-					*resource.NewQuantity(6, resource.BinarySI),
-				))
-				Expect(rr.Requests()).To(HaveKeyWithValue(
-					kubev1.ResourceCPU,
-					addResources(userCPURequest, cpuIsolatedEmulatorThreadOverhead),
-				))
-			},
-				Entry("only CPU requests set by the user", false),
-				Entry("request and limits set by the user", true),
+					if defineUserSpecifiedCPULimit {
+						userSpecifiedCPULimit = kubev1.ResourceList{kubev1.ResourceCPU: userCPURequest}
+					}
+					if fullPCPUEnabled {
+						cpuManagerPolicyBetaOption = v1.CPUManagerPolicyBetaOptionFullpCPUsOnly
+					}
+					rr = NewResourceRenderer(
+						userSpecifiedCPULimit,
+						userSpecifiedCPURequest,
+						WithCPUPinning(&v1.CPU{
+							Cores:                 cores,
+							IsolateEmulatorThread: true,
+						},
+							cpuManagerPolicyBetaOption),
+					)
+					Expect(rr.Limits()).To(HaveKeyWithValue(
+						kubev1.ResourceCPU,
+						*resource.NewQuantity(cpuIsolatedEmulatorThreadOverhead.Value()+int64(cores), resource.BinarySI),
+					))
+					Expect(rr.Requests()).To(HaveKeyWithValue(
+						kubev1.ResourceCPU,
+						addResources(userCPURequest, cpuIsolatedEmulatorThreadOverhead),
+					))
+				},
+				Entry("full-pcpu-only mode is disabled, only CPU requests set by the user", false, false, uint32(5), "1000m"),
+				Entry("full-pcpu-only mode is disabled, request and limits set by the user", false, true, uint32(5), "1000m"),
+				Entry("full-pcpu-only mode is enabled, only CPU requests set by the user, odd amount of cores is requested", true, false, uint32(5), "1000m"),
+				Entry("full-pcpu-only mode is enabled, only CPU requests set by the user, even amount of cores is requested", true, false, uint32(6), "2000m"),
+				Entry("full-pcpu-only mode is enabled, request and limits set by the user, odd amount of cores is requested", true, true, uint32(5), "1000m"),
+				Entry("full-pcpu-only mode is enabled, request and limits set by the user, even amount of cores is requested", true, true, uint32(6), "2000m"),
 			)
 		})
 	})
