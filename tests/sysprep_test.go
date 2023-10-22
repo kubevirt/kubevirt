2875edb5df222da1eabea984d737fa89bc379725
Dan Kenigsberg <danken@redhat.com>
2023-10-22 12:01:24 +0300

    tests: move AddExplicitPodNetworkInterface next to its single user
    This makes tests/utils.go slightly smaller.
    
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: VamsikrishnaSiddu <vamsikrishna.siddu@ibm.com>

--- tests/sysprep_test.go
+++ tests/sysprep_test.go
@@ -218,6 +218,14 @@ func insertProductKeyToAnswerFileTemplate(answerFileTemplate string) string {
 	return fmt.Sprintf(answerFileTemplate, productKey)
 }
 
+// addExplicitPodNetworkInterface
+//
+// Deprecated: Use libvmi
+func addExplicitPodNetworkInterface(vmi *v1.VirtualMachineInstance) {
+	vmi.Spec.Domain.Devices.Interfaces = []v1.Interface{*v1.DefaultMasqueradeNetworkInterface()}
+	vmi.Spec.Networks = []v1.Network{*v1.DefaultPodNetwork()}
+}
+
 var getWindowsSysprepVMISpec = func() v1.VirtualMachineInstanceSpec {
 	gracePeriod := int64(0)
 	spinlocks := uint32(8191)
@@ -310,7 +318,7 @@ var _ = Describe("[Serial][Sysprep][sig-compute]Syspreped VirtualMachineInstance
 		windowsVMI = tests.NewRandomVMI()
 		windowsVMI.Spec = getWindowsSysprepVMISpec()
 		tests.CreateConfigMap("sysprepautounattend", windowsVMI.Namespace, map[string]string{"Autounattend.xml": answerFileWithKey, "Unattend.xml": answerFileWithKey})
-		tests.AddExplicitPodNetworkInterface(windowsVMI)
+		addExplicitPodNetworkInterface(windowsVMI)
 		windowsVMI.Spec.Domain.Devices.Interfaces[0].Model = "e1000"
 	})
 
