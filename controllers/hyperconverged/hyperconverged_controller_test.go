d423135e0e17a4eba2969d87b59f0f93b8f1c9c4
RamLavi <ralavi@redhat.com>
2023-11-22 14:37:17 +0200

    Enable KubeVirt CPUManager Policy BetaOptions feature gate (#2616)
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- controllers/hyperconverged/hyperconverged_controller_test.go
+++ controllers/hyperconverged/hyperconverged_controller_test.go
@@ -32,6 +32,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	kubevirtcorev1 "kubevirt.io/api/core/v1"
+	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
+	sspv1beta2 "kubevirt.io/ssp-operator/api/v1beta2"
+
 	hcov1beta1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/alerts"
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/common"
@@ -40,9 +44,6 @@ import (
 	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/monitoring/metrics"
 	hcoutil "github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 	"github.com/kubevirt/hyperconverged-cluster-operator/version"
-	kubevirtcorev1 "kubevirt.io/api/core/v1"
-	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
-	sspv1beta2 "kubevirt.io/ssp-operator/api/v1beta2"
 
 	objectreferencesv1 "github.com/openshift/custom-resource-status/objectreferences/v1"
 )
@@ -215,6 +216,7 @@ var _ = Describe("HyperconvergedController", func() {
 					"HotplugNICs",
 					"VMPersistentState",
 					"NetworkBindingPlugins",
+					"CPUManagerPolicyBetaOptions",
 				}
 				// Get the KV
 				kvList := &kubevirtcorev1.KubeVirtList{}
--- controllers/operands/kubevirt.go
+++ controllers/operands/kubevirt.go
@@ -110,6 +110,9 @@ const (
 
 	// Enable using a plugin to bind the pod and the VM network
 	kvHNetworkBindingPluginsGate = "NetworkBindingPlugins"
+
+	// // CPUManagerPolicyBetaOptionsGate allows conforming VM to static CPU-Manager Policies.
+	CPUManagerPolicyBetaOptionsGate = "CPUManagerPolicyBetaOptions"
 )
 
 const (
@@ -136,6 +139,7 @@ var (
 		kvHotplugNicsGate,
 		kvVMPersistentState,
 		kvHNetworkBindingPluginsGate,
+		CPUManagerPolicyBetaOptionsGate,
 	}
 
 	// holds a list of mandatory KubeVirt feature gates. Some of them are the hard coded feature gates and some of
