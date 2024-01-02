f88c1edff9c34fce4e20711c665b0f13cadbbc46
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2024-01-02 06:11:18 -0500

    Fix diff tool (#2710)
    PRs now fails on sanity check, when performing git diff. The auto
    generated files include the text "Copyright <year>". Moving to a new
    year (2024), the git diff fails on sanity.
    
    This PR fixes the git diff so it won't fail for these lines.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/diff-csv.sh
+++ hack/diff-csv.sh
@@ -4,4 +4,4 @@
 #     git difftool -y --extcmd=hacks/diff-csv.sh
 #
 # currently used for make sanity
-diff --unified=1 --ignore-matching-lines='^\s*createdAt:' $@
+diff --unified=1 --ignore-matching-lines='^\s*createdAt:' --ignore-matching-lines='^\s*\* Copyright [0-9]\{4\} Red Hat, Inc\.' $@
