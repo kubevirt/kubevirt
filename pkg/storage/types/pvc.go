3096706fd4d04704fdeb52928542c9e2bfbbe790
Alice Frosi <afrosi@redhat.com>
2024-01-04 10:13:39 +0100

    storagetypes: function to verify hotplugged volumes
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/storage/types/pvc.go
+++ pkg/storage/types/pvc.go
@@ -280,3 +280,21 @@ func GetDiskCapacity(pvcInfo *virtv1.PersistentVolumeClaimInfo) *int64 {
 	preferredSize := Min(storageRequest, storageCapacity)
 	return &preferredSize
 }
+
+func IsHotplugVolume(vol *virtv1.Volume) bool {
+	if vol == nil {
+		return false
+	}
+	volSrc := vol.VolumeSource
+	if volSrc.PersistentVolumeClaim != nil && volSrc.PersistentVolumeClaim.Hotpluggable {
+		return true
+	}
+	if volSrc.DataVolume != nil && volSrc.DataVolume.Hotpluggable {
+		return true
+	}
+	if volSrc.MemoryDump != nil && volSrc.MemoryDump.PersistentVolumeClaimVolumeSource.Hotpluggable {
+		return true
+	}
+
+	return false
+}
