12547e7ecf91e6748e2de1a3facfbb2fa5f2db65
Felix Matouschek <fmatouschek@redhat.com>
2024-01-08 13:49:15 +0100

    fix: Never run tests which exclude native SSH
    We do not want to run tests which exclude native SSH functionality, so
    ensure that they are always skipped by the label filter.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -411,7 +411,7 @@ if [[ -z ${KUBEVIRT_E2E_FOCUS} && -z ${KUBEVIRT_E2E_SKIP} ]]; then
   elif [[ $TARGET =~ sig-compute-migrations ]]; then
     label_filter='(sig-compute-migrations && !(GPU,VGPU))'
   elif [[ $TARGET =~ sig-compute ]]; then
-    label_filter='(sig-compute && !(GPU,VGPU,sig-compute-migrations,exclude-native-ssh))'
+    label_filter='(sig-compute && !(GPU,VGPU,sig-compute-migrations))'
   elif [[ $TARGET =~ sig-monitoring ]]; then
     label_filter='(sig-monitoring)'
   elif [[ $TARGET =~ sig-operator ]]; then
@@ -435,6 +435,9 @@ fi
 
 # No lane currently supports loading a custom policy
 add_to_label_filter '(!CustomSELinux)' '&&'
+
+# We do not want to run tests which exclude native SSH functionality
+add_to_label_filter '(!exclude-native-ssh)' '&&'
 
 # Single-node single-replica test lanes obviously can't run live migrations,
 # but also currently lack the requirements for SRIOV, GPU, Macvtap and MDEVs.
