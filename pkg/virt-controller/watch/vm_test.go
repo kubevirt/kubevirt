10e313fb8b5bb2ac364886a4a130d6b1e82a613a
Luboslav Pivarc <lpivarc@redhat.com>
2024-02-15 17:18:15 +0100

    Fix instance type unit tests
    These tests are relying on a fact that
    a cached object is being modified
    
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/virt-controller/watch/vm_test.go
+++ pkg/virt-controller/watch/vm_test.go
@@ -3881,9 +3881,6 @@ var _ = Describe("VirtualMachine", func() {
 					vmInterface.EXPECT().UpdateStatus(context.Background(), gomock.Any()).Times(1)
 
 					sanityExecute(vm)
-
-					Expect(vm.Spec.Instancetype.RevisionName).To(Equal(instancetypeRevision.Name))
-
 				})
 
 				It("should apply VirtualMachineClusterInstancetype to VirtualMachineInstance", func() {
@@ -3990,9 +3987,6 @@ var _ = Describe("VirtualMachine", func() {
 					vmInterface.EXPECT().UpdateStatus(context.Background(), gomock.Any()).Times(1)
 
 					sanityExecute(vm)
-
-					Expect(vm.Spec.Instancetype.RevisionName).To(Equal(instancetypeRevision.Name))
-
 				})
 
 				It("should reject request if an invalid InstancetypeMatcher Kind is provided", func() {
@@ -4399,9 +4393,6 @@ var _ = Describe("VirtualMachine", func() {
 					vmInterface.EXPECT().UpdateStatus(context.Background(), gomock.Any()).Times(1)
 
 					sanityExecute(vm)
-
-					Expect(vm.Spec.Preference.RevisionName).To(Equal(preferenceRevision.Name))
-
 				})
 
 				It("should apply VirtualMachineClusterPreference to VirtualMachineInstance", func() {
@@ -4509,9 +4500,6 @@ var _ = Describe("VirtualMachine", func() {
 					vmInterface.EXPECT().UpdateStatus(context.Background(), gomock.Any()).Times(1)
 
 					sanityExecute(vm)
-
-					Expect(vm.Spec.Preference.RevisionName).To(Equal(preferenceRevision.Name))
-
 				})
 
 				It("should reject the request if an invalid PreferenceMatcher Kind is provided", func() {
