a6860ba81da66c20c3ab9d00944a1fb088f25060
Shelly Kagan <skagan@redhat.com>
2024-03-10 16:07:51 +0200

    Adjust migration tests to pass with wffc snapshotable storageclass
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- tests/migration/migration.go
+++ tests/migration/migration.go
@@ -2194,7 +2194,7 @@ var _ = SIGMigrationDescribe("VM Live Migration", func() {
 
 				dv, err = virtClient.CdiClient().CdiV1beta1().DataVolumes(testsuite.GetTestNamespace(nil)).Create(context.Background(), dv, metav1.CreateOptions{})
 				Expect(err).ToNot(HaveOccurred())
-				libstorage.EventuallyDV(dv, 600, HaveSucceeded())
+				libstorage.EventuallyDV(dv, 600, Or(HaveSucceeded(), PendingPopulation()))
 				vmi := tests.NewRandomVMIWithDataVolume(dv.Name)
 				tests.AddUserData(vmi, "disk1", "#!/bin/bash\n echo hello\n")
 				return vmi
