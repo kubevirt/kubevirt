kubevirt/kubevirt

commit 637be26ef415dbe77a003e3eef1e094b0ed456b3

virt-launcher: Reserve domain resources for network interfaces
Consume the resource request interface value from the VMI spec when
creating the domain.

The domain configuration is now generated as follows:
- Add to the domain configuration input, interfaces as placeholders.
- Create a (libvirt) object which contains auto-generated domain
  configuration values (beyond the user input one).
- Remove the placeholder interfaces from the (full) domain configuration
- Create a new (libvirt) object which do not contain the placeholder
  interfaces but does contain the auto-generated dependencies from it
  (e.g. the PCIe controllers).

Resources are reserved in this method only if the requested interfaces
count is beyond the existing explicitly defined interfaces in the VMI
spec.

Signed-off-by: Edward Haas <edwardh@redhat.com>

pkg/virt-launcher/virtwrap/manager.go
@@ -882,10 +882,17 @@ func (l *LibvirtDomainManager) SyncVMI(vmi *v1.VirtualMachineInstance, allowEmul
 				logger.Reason(err).Error("pre start setup for VirtualMachineInstance failed.")
 				return nil, err
 			}
-			dom, err = l.setDomainSpecWithHooks(vmi, &domain.Spec)
+
+			dom, err = withNetworkIfacesResources(
+				vmi, &domain.Spec,
+				func(v *v1.VirtualMachineInstance, s *api.DomainSpec) (cli.VirDomain, error) {
+					return l.setDomainSpecWithHooks(v, s)
+				},
+			)
 			if err != nil {
 				return nil, err
 			}
+
 			l.metadataCache.UID.Set(vmi.UID)
 			l.metadataCache.GracePeriod.Set(
 				api.GracePeriodMetadata{DeletionGracePeriodSeconds: converter.GracePeriodSeconds(vmi)},

pkg/virt-launcher/virtwrap/nichotplug.go
@@ -24,12 +24,16 @@ import (
 	"fmt"
 	"strings"
 
+	"libvirt.org/go/libvirt"
+
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
 
+	"kubevirt.io/kubevirt/pkg/apimachinery/resource"
 	netvmispec "kubevirt.io/kubevirt/pkg/network/vmispec"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/cli"
+	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/util"
 )
 
 type vmConfigurator interface {
@@ -119,4 +123,62 @@ func indexedDomainInterfaces(domain *api.Domain) map[string]api.Interface {
 		domainInterfaces[iface.Alias.GetName()] = iface
 	}
 	return domainInterfaces
+}
+
+// withNetworkIfacesResources adds network interfaces as placeholders to the domain spec
+// to trigger the addition of the dependent resources/devices (e.g. PCI controllers).
+// As its last step, it reads the generated configuration and removes the network interfaces
+// so none will be created with the domain creation.
+// The dependent devices are left in the configuration, to allow future hotplug.
+func withNetworkIfacesResources(vmi *v1.VirtualMachineInstance, domainSpec *api.DomainSpec, f func(v *v1.VirtualMachineInstance, s *api.DomainSpec) (cli.VirDomain, error)) (cli.VirDomain, error) {
+	domainSpecWithIfacesResource := appendPlaceholderInterfacesToTheDomain(vmi, domainSpec)
+	dom, err := f(vmi, domainSpecWithIfacesResource)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(domainSpec.Devices.Interfaces) == len(domainSpecWithIfacesResource.Devices.Interfaces) {
+		return dom, nil
+	}
+
+	domainSpecWithoutIfacePlaceholders, err := util.GetDomainSpecWithFlags(dom, libvirt.DOMAIN_XML_INACTIVE)
+	if err != nil {
+		return nil, err
+	}
+	domainSpecWithoutIfacePlaceholders.Devices.Interfaces = domainSpec.Devices.Interfaces
+	domainSpecWithoutIfacePlaceholders.DeepCopyInto(domainSpec)
+
+	return f(vmi, domainSpecWithoutIfacePlaceholders)
+}
+
+func appendPlaceholderInterfacesToTheDomain(vmi *v1.VirtualMachineInstance, domainSpec *api.DomainSpec) *api.DomainSpec {
+	domainSpecWithIfacesResource := domainSpec.DeepCopy()
+	requests := resource.ExtendedResourceList{ResourceList: vmi.Spec.Domain.Resources.Requests}
+	reqInterfaces := int(requests.Interface().Value())
+	interfacePlaceholderCount := reqInterfaces - len(vmi.Spec.Domain.Devices.Interfaces)
+	for i := 0; i < interfacePlaceholderCount; i++ {
+		domainSpecWithIfacesResource.Devices.Interfaces = append(
+			domainSpecWithIfacesResource.Devices.Interfaces,
+			newInterfacePlaceholder(i, interpretModelType(vmi.Spec.Domain.Devices.UseVirtioTransitional)),
+		)
+	}
+	return domainSpecWithIfacesResource
+}
+
+func interpretModelType(useVirtioTransitional *bool) string {
+	if useVirtioTransitional != nil && *useVirtioTransitional {
+		return "virtio-transitional"
+	}
+	return "virtio-non-transitional"
+}
+
+func newInterfacePlaceholder(index int, modelType string) api.Interface {
+	return api.Interface{
+		Type:  "ethernet",
+		Model: &api.Model{Type: modelType},
+		Target: &api.InterfaceTarget{
+			Device:  fmt.Sprintf("placeholder-%d", index),
+			Managed: "no",
+		},
+	}
 }

pkg/virt-launcher/virtwrap/BUILD.bazel
@@ -12,6 +12,7 @@ go_library(
     importpath = "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap",
     visibility = ["//visibility:public"],
     deps = [
+        "//pkg/apimachinery/resource:go_default_library",
         "//pkg/cloud-init:go_default_library",
         "//pkg/config:go_default_library",
         "//pkg/container-disk:go_default_library",
@@ -71,6 +72,7 @@ go_test(
     ],
     embed = [":go_default_library"],
     deps = [
+        "//pkg/apimachinery/resource:go_default_library",
         "//pkg/cloud-init:go_default_library",
         "//pkg/ephemeral-disk-utils:go_default_library",
         "//pkg/ephemeral-disk/fake:go_default_library",

tests/libvmi/BUILD.bazel
@@ -16,6 +16,7 @@ go_library(
     importpath = "kubevirt.io/kubevirt/tests/libvmi",
     visibility = ["//visibility:public"],
     deps = [
+        "//pkg/apimachinery/resource:go_default_library",
         "//pkg/controller:go_default_library",
         "//pkg/pointer:go_default_library",
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",

pkg/virt-launcher/virtwrap/nichotplug_test.go
@@ -20,14 +20,22 @@
 package virtwrap
 
 import (
+	"encoding/xml"
 	"fmt"
+	"strconv"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"libvirt.org/go/libvirt"
+
+	k8sv1 "k8s.io/api/core/v1"
+	k8sresource "k8s.io/apimachinery/pkg/api/resource"
+
 	v1 "kubevirt.io/api/core/v1"
 
+	"kubevirt.io/kubevirt/pkg/apimachinery/resource"
 	"kubevirt.io/kubevirt/pkg/network/vmispec"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/cli"
@@ -138,6 +146,81 @@ var _ = Describe("nic hotplug on virt-launcher", func() {
 			&fakeVMConfigurator{},
 			libvirtClientResult{expectedError: fmt.Errorf("boom")},
 		),
+	)
+})
+
+var _ = Describe("domain network interfaces resources", func() {
+
+	DescribeTable("are ignored when",
+		func(interfaceResourceRequest string, vmiSpecIfaces []v1.Interface) {
+			vmi := &v1.VirtualMachineInstance{}
+			vmi.Spec.Domain.Devices.Interfaces = vmiSpecIfaces
+			if interfaceResourceRequest != "" {
+				vmi.Spec.Domain.Resources.Requests = k8sv1.ResourceList{
+					resource.ResourceInterface: k8sresource.MustParse(interfaceResourceRequest),
+				}
+			}
+			domainSpec := &api.DomainSpec{}
+			countCalls := 0
+			_, _ = withNetworkIfacesResources(vmi, domainSpec, func(v *v1.VirtualMachineInstance, s *api.DomainSpec) (cli.VirDomain, error) {
+				countCalls++
+				return nil, nil
+			})
+			// The counter tracks the tested function behavior.
+			// It is expected that the callback function is called only once when there is no need
+			// to add placeholders interfaces.
+			Expect(countCalls).To(Equal(1))
+		},
+		Entry("no interface resource-request is present", "", []v1.Interface{{}}),
+		Entry("the interface resource-request is zero", "0", []v1.Interface{{}}),
+		Entry("the interface resource-request is less than defined interfaces", "1", []v1.Interface{{}, {}}),
+		Entry("the interface resource-request is equal to the defined interfaces", "2", []v1.Interface{{}, {}}),
+	)
+
+	DescribeTable("are reserved when",
+		func(interfaceResourceRequest string, vmiSpecIfaces []v1.Interface) {
+			vmi := &v1.VirtualMachineInstance{}
+			vmi.Spec.Domain.Devices.Interfaces = vmiSpecIfaces
+			if interfaceResourceRequest != "" {
+				vmi.Spec.Domain.Resources.Requests = k8sv1.ResourceList{
+					resource.ResourceInterface: k8sresource.MustParse(interfaceResourceRequest),
+				}
+			}
+			domainSpec := &api.DomainSpec{}
+			for range vmiSpecIfaces {
+				domainSpec.Devices.Interfaces = append(domainSpec.Devices.Interfaces, api.Interface{})
+			}
+
+			ctrl := gomock.NewController(GinkgoT())
+			mockDomain := cli.NewMockVirDomain(ctrl)
+			domxml, err := xml.MarshalIndent(domainSpec, "", "\t")
+			Expect(err).ToNot(HaveOccurred())
+			mockDomain.EXPECT().GetXMLDesc(libvirt.DomainXMLFlags(2)).Return(string(domxml), nil)
+
+			originalDomainSpec := domainSpec.DeepCopy()
+			countCalls := 0
+			_, err = withNetworkIfacesResources(vmi, domainSpec, func(v *v1.VirtualMachineInstance, s *api.DomainSpec) (cli.VirDomain, error) {
+				// Tracking the behavior of the tested function.
+				// It is expected that the callback function is called twice when placeholders are needed.
+				// The first time it is called with the placeholders in place.
+				// The second time it is called without the placeholders.
+				countCalls++
+				if countCalls == 1 {
+					ifaceRequest, err := strconv.Atoi(interfaceResourceRequest)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(s.Devices.Interfaces).To(HaveLen(ifaceRequest))
+				} else {
+					Expect(s.Devices.Interfaces).To(Equal(originalDomainSpec.Devices.Interfaces))
+				}
+
+				return mockDomain, nil
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(countCalls).To(Equal(2))
+			Expect(domainSpec.Devices.Interfaces).To(Equal(originalDomainSpec.Devices.Interfaces))
+		},
+		Entry("no defined interfaces exist", "1", nil),
+		Entry("the interface resource-request is larger than the defined interfaces", "2", []v1.Interface{{}}),
 	)
 })
 

tests/libvmi/network.go
@@ -20,7 +20,12 @@
 package libvmi
 
 import (
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
 	kvirtv1 "kubevirt.io/api/core/v1"
+
+	k6sresource "kubevirt.io/kubevirt/pkg/apimachinery/resource"
 
 	"kubevirt.io/kubevirt/tests/libnet"
 )
@@ -120,5 +125,15 @@ func MultusNetwork(name, nadName string) *kvirtv1.Network {
 				NetworkName: nadName,
 			},
 		},
+	}
+}
+
+// WithResourceRequestInterface specifies the vmi network interface resource.
+func WithResourceRequestInterface(value string) Option {
+	return func(vmi *kvirtv1.VirtualMachineInstance) {
+		if vmi.Spec.Domain.Resources.Requests == nil {
+			vmi.Spec.Domain.Resources.Requests = k8sv1.ResourceList{}
+		}
+		vmi.Spec.Domain.Resources.Requests[k6sresource.ResourceInterface] = resource.MustParse(value)
 	}
 }

tests/network/hotplug.go
@@ -199,6 +199,58 @@ var _ = SIGDescribe("nic-hotplug", func() {
 		)
 	})
 
+	Context("a running VMI with reserved interfaces resources", func() {
+		var hotPluggedVMI *v1.VirtualMachineInstance
+
+		BeforeEach(func() {
+			By("running a VMI")
+			hotPluggedVMI = tests.RunVMIAndExpectLaunch(
+				libvmi.NewAlpineWithTestTooling(
+					append(libvmi.WithMasqueradeNetworking(), libvmi.WithResourceRequestInterface("3"))...,
+				), 60,
+			)
+			Expect(console.LoginToAlpine(hotPluggedVMI)).To(Succeed())
+
+			By("creating a NAD")
+			Expect(createBridgeNetworkAttachmentDefinition(
+				testsuite.GetTestNamespace(nil), networkName, linuxBridgeName),
+			).To(Succeed())
+
+			By("hotplugging an interface to the VMI")
+			err := libnet.InterfaceExists(hotPluggedVMI, vmIfaceName)
+			Expect(err).To(HaveOccurred())
+
+			Expect(kubevirt.Client().VirtualMachineInstance(hotPluggedVMI.GetNamespace()).AddInterface(
+				context.Background(),
+				hotPluggedVMI.GetName(),
+				addIfaceOptions(networkName, ifaceName),
+			)).To(Succeed())
+		})
+
+		DescribeTable("is able to hotplug multiple network interfaces", func(plugMethod hotplugMethod) {
+			hotPluggedVMI = verifyHotplug(hotPluggedVMI, plugMethod)
+			By("hotplugging the second interface")
+			const secondHotpluggedIfaceName = "iface2"
+			Expect(
+				kubevirt.Client().VirtualMachineInstance(hotPluggedVMI.GetNamespace()).AddInterface(
+					context.Background(),
+					hotPluggedVMI.GetName(),
+					addIfaceOptions(networkName, secondHotpluggedIfaceName),
+				),
+			).To(Succeed())
+
+			if plugMethod == migrationBased {
+				migrate(hotPluggedVMI)
+			}
+
+			hotPluggedVMI = verifyHotplug(hotPluggedVMI, plugMethod)
+			Expect(libnet.InterfaceExists(hotPluggedVMI, "eth2")).To(Succeed())
+		},
+			Entry("In place", decorators.InPlaceHotplugNICs, inPlace),
+			Entry("Migration based", decorators.MigrationBasedHotplugNICs, migrationBased),
+		)
+	})
+
 	Context("a running VM", func() {
 		var hotPluggedVM *v1.VirtualMachine
 		var hotPluggedVMI *v1.VirtualMachineInstance

commit b540bf253d18479d63e6e5f4a252e62f70493054

virt-launcher, hooks: use libvirtxml.Domain
Hook sidecars that implement OnDefineDomain have the opportunity to do
changes that are not supported by virtwrap/api/schema.go hence, we
should be careful if calling the sidecars multiple times, in order to
have included in the XML, the previous changes made by the sidecar as
well.

By using libvirtxml.Domain we will keep stored the full changes for
the period we might call the sidecar twice, in the function
withNetworkIfacesResources.

The patch introduces in virtwrap/libvirtxml two convert functions, to
help convert between schema.go's DomainSpec and libvirtxml.Domain.

Fixes: 637be26ef4
Signed-off-by: Victor Toso <victortoso@redhat.com>

pkg/hooks/BUILD.bazel
@@ -16,11 +16,11 @@ go_library(
         "//pkg/hooks/v1alpha2:go_default_library",
         "//pkg/hooks/v1alpha3:go_default_library",
         "//pkg/util/net/grpc:go_default_library",
-        "//pkg/virt-launcher/virtwrap/api:go_default_library",
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",
         "//staging/src/kubevirt.io/client-go/log:go_default_library",
         "//vendor/github.com/golang/mock/gomock:go_default_library",
         "//vendor/k8s.io/api/core/v1:go_default_library",
+        "//vendor/libvirt.org/go/libvirtxml:go_default_library",
     ],
 )
 

pkg/hooks/generated_mock_manager.go
@@ -8,9 +8,9 @@ import (
 
 	gomock "github.com/golang/mock/gomock"
 	v1 "kubevirt.io/api/core/v1"
+	libvirtxml "libvirt.org/go/libvirtxml"
 
 	cloud_init "kubevirt.io/kubevirt/pkg/cloud-init"
-	api "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 )
 
 // Mock of Manager interface
@@ -44,7 +44,7 @@ func (_mr *_MockManagerRecorder) Collect(arg0, arg1 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "Collect", arg0, arg1)
 }
 
-func (_m *MockManager) OnDefineDomain(_param0 *api.DomainSpec, _param1 *v1.VirtualMachineInstance) (string, error) {
+func (_m *MockManager) OnDefineDomain(_param0 *libvirtxml.Domain, _param1 *v1.VirtualMachineInstance) (string, error) {
 	ret := _m.ctrl.Call(_m, "OnDefineDomain", _param0, _param1)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)

pkg/hooks/manager.go
@@ -33,6 +33,7 @@ import (
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
+	"libvirt.org/go/libvirtxml"
 
 	cloudinit "kubevirt.io/kubevirt/pkg/cloud-init"
 	hooksInfo "kubevirt.io/kubevirt/pkg/hooks/info"
@@ -40,7 +41,6 @@ import (
 	hooksV1alpha2 "kubevirt.io/kubevirt/pkg/hooks/v1alpha2"
 	hooksV1alpha3 "kubevirt.io/kubevirt/pkg/hooks/v1alpha3"
 	grpcutil "kubevirt.io/kubevirt/pkg/util/net/grpc"
-	virtwrapApi "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 )
 
 //go:generate mockgen -source $GOFILE -package=$GOPACKAGE -destination=generated_mock_$GOFILE
@@ -59,7 +59,7 @@ var once sync.Once
 type (
 	Manager interface {
 		Collect(uint, time.Duration) error
-		OnDefineDomain(*virtwrapApi.DomainSpec, *v1.VirtualMachineInstance) (string, error)
+		OnDefineDomain(*libvirtxml.Domain, *v1.VirtualMachineInstance) (string, error)
 		PreCloudInitIso(*v1.VirtualMachineInstance, *cloudinit.CloudInitData) (*cloudinit.CloudInitData, error)
 		Shutdown() error
 	}
@@ -197,7 +197,7 @@ func sortCallbacksPerHookPoint(callbacksPerHookPoint map[string][]*callBackClien
 	}
 }
 
-func (m *hookManager) OnDefineDomain(domainSpec *virtwrapApi.DomainSpec, vmi *v1.VirtualMachineInstance) (string, error) {
+func (m *hookManager) OnDefineDomain(domainSpec *libvirtxml.Domain, vmi *v1.VirtualMachineInstance) (string, error) {
 	domainSpecXML, err := xml.MarshalIndent(domainSpec, "", "\t")
 	if err != nil {
 		return "", fmt.Errorf("Failed to marshal domain spec: %v", domainSpec)

pkg/virt-launcher/virtwrap/libvirtxml/convert.go
@@ -20,6 +20,8 @@
 package libvirtxml
 
 import (
+	"encoding/xml"
+	"fmt"
 	"strconv"
 
 	"libvirt.org/go/libvirtxml"
@@ -302,4 +304,34 @@ func ConvertKubeVirtFeaturesToDomainFeatureList(features *api.Features) *libvirt
 	f.KVM = ConverKubeVirtFeatureKVMToDomainFeatureKVM(features.KVM)
 	return f
 
+}
+
+func ConvertKubeVirtDomainSpecToDomain(apiDomainSpec *api.DomainSpec) (*libvirtxml.Domain, error) {
+	if apiDomainSpec == nil {
+		return nil, fmt.Errorf("can't convert: apiDomainSpec is nil")
+	}
+
+	bytes, err := xml.Marshal(&apiDomainSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	var libvirtDomain libvirtxml.Domain
+	err = xml.Unmarshal(bytes, &libvirtDomain)
+	return &libvirtDomain, err
+}
+
+func ConvertDomainToKubeVirtDomainSpec(libvirtDomain *libvirtxml.Domain) (*api.DomainSpec, error) {
+	if libvirtDomain == nil {
+		return nil, fmt.Errorf("can't convert: libvirtDomain is nil")
+	}
+
+	bytes, err := xml.Marshal(libvirtDomain)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiDomainSpec api.DomainSpec
+	err = xml.Unmarshal(bytes, &apiDomainSpec)
+	return &apiDomainSpec, err
 }

pkg/virt-launcher/virtwrap/manager.go
@@ -60,6 +60,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"libvirt.org/go/libvirt"
+	"libvirt.org/go/libvirtxml"
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
@@ -86,6 +87,7 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/device/hostdevice"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/device/hostdevice/sriov"
 	domainerrors "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/errors"
+	convxml "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/libvirtxml"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/stats"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/util"
 )
@@ -1239,12 +1241,24 @@ func (l *LibvirtDomainManager) lookupOrCreateVirDomain(
 		return nil, err
 	}
 
-	setDomainFn := func(v *v1.VirtualMachineInstance, s *api.DomainSpec) (cli.VirDomain, *api.DomainSpec, error) {
+	libvirtDomain, err := convxml.ConvertKubeVirtDomainSpecToDomain(&domain.Spec)
+	if err != nil {
+		return nil, err
+	}
+
+	setDomainFn := func(v *v1.VirtualMachineInstance, s *libvirtxml.Domain) (cli.VirDomain, *libvirtxml.Domain, error) {
 		return util.SetDomainSpecStrWithHooks(l.virConn, v, s)
 	}
 
-	if dom, _, err = withNetworkIfacesResources(vmi, &domain.Spec, setDomainFn); err != nil {
+	dom, libvirtDomain, err = withNetworkIfacesResources(vmi, libvirtDomain, setDomainFn)
+	if err != nil {
 		return nil, err
+	}
+
+	if newSpec, err := convxml.ConvertDomainToKubeVirtDomainSpec(libvirtDomain); err != nil {
+		return nil, err
+	} else {
+		newSpec.DeepCopyInto(&domain.Spec)
 	}
 
 	l.metadataCache.UID.Set(vmi.UID)

pkg/virt-launcher/virtwrap/nichotplug.go
@@ -26,7 +26,7 @@ import (
 
 	"kubevirt.io/kubevirt/pkg/network/namescheme"
 
-	"libvirt.org/go/libvirt"
+	"libvirt.org/go/libvirtxml"
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
@@ -36,7 +36,6 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/cli"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/converter"
-	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/util"
 )
 
 type vmConfigurator interface {
@@ -172,60 +171,67 @@ func indexedDomainInterfaces(domain *api.Domain) map[string]api.Interface {
 	return domainInterfaces
 }
 
-type SetDomainFunc func(*v1.VirtualMachineInstance, *api.DomainSpec) (cli.VirDomain, *api.DomainSpec, error)
+type SetDomainFunc func(*v1.VirtualMachineInstance, *libvirtxml.Domain) (cli.VirDomain, *libvirtxml.Domain, error)
 
 // withNetworkIfacesResources adds network interfaces as placeholders to the domain spec
 // to trigger the addition of the dependent resources/devices (e.g. PCI controllers).
 // As its last step, it reads the generated configuration and removes the network interfaces
 // so none will be created with the domain creation.
 // The dependent devices are left in the configuration, to allow future hotplug.
-func withNetworkIfacesResources(vmi *v1.VirtualMachineInstance, domainSpec *api.DomainSpec, f SetDomainFunc) (cli.VirDomain, *api.DomainSpec, error) {
-	domainSpecWithIfacesResource := appendPlaceholderInterfacesToTheDomain(vmi, domainSpec)
-	dom, err := f(vmi, domainSpecWithIfacesResource)
+func withNetworkIfacesResources(
+	vmi *v1.VirtualMachineInstance,
+	domainSpec *libvirtxml.Domain,
+	f SetDomainFunc,
+) (cli.VirDomain, *libvirtxml.Domain, error) {
+	domainSpec, originalInterfaces := appendPlaceholderInterfacesToTheDomain(vmi, domainSpec)
+	dom, newSpec, err := f(vmi, domainSpec)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	if len(domainSpec.Devices.Interfaces) == len(domainSpecWithIfacesResource.Devices.Interfaces) {
-		return dom, domainSpec, nil
+	if len(newSpec.Devices.Interfaces) == len(originalInterfaces) {
+		return dom, newSpec, nil
 	}
 
-	domainSpecWithoutIfacePlaceholders, err := util.GetDomainSpecWithFlags(dom, libvirt.DOMAIN_XML_INACTIVE)
-	if err != nil {
-		return nil, nil, err
-	}
-	domainSpecWithoutIfacePlaceholders.Devices.Interfaces = domainSpec.Devices.Interfaces
-	// Only the devices are taken into account because some parameters are not assured to be returned when
-	// getting the domain spec (e.g. the `qemu:commandline` section).
-	domainSpecWithoutIfacePlaceholders.Devices.DeepCopyInto(&domainSpec.Devices)
-
-	return f(vmi, domainSpec)
+	// restore original interfaces
+	newSpec.Devices.Interfaces = originalInterfaces
+	return f(vmi, newSpec)
 }
 
-func appendPlaceholderInterfacesToTheDomain(vmi *v1.VirtualMachineInstance, domainSpec *api.DomainSpec) *api.DomainSpec {
+// return original interface slice in case we want to restore it
+func appendPlaceholderInterfacesToTheDomain(
+	vmi *v1.VirtualMachineInstance,
+	domainSpec *libvirtxml.Domain,
+) (*libvirtxml.Domain, []libvirtxml.DomainInterface) {
 	if len(vmi.Spec.Domain.Devices.Interfaces) == 0 {
-		return domainSpec
+		return domainSpec, domainSpec.Devices.Interfaces
 	}
 	if val := vmi.Annotations[v1.PlacePCIDevicesOnRootComplex]; val == "true" {
-		return domainSpec
+		return domainSpec, domainSpec.Devices.Interfaces
 	}
-	domainSpecWithIfacesResource := domainSpec.DeepCopy()
+
+	original := domainSpec.Devices.Interfaces
+	domainSpec.Devices.Interfaces = make([]libvirtxml.DomainInterface, ReservedInterfaces)
+	copy(domainSpec.Devices.Interfaces, original)
+
 	interfacePlaceholderCount := ReservedInterfaces - len(vmi.Spec.Domain.Devices.Interfaces)
 	for i := 0; i < interfacePlaceholderCount; i++ {
-		domainSpecWithIfacesResource.Devices.Interfaces = append(
-			domainSpecWithIfacesResource.Devices.Interfaces,
+		domainSpec.Devices.Interfaces = append(
+			domainSpec.Devices.Interfaces,
 			newInterfacePlaceholder(i, converter.InterpretTransitionalModelType(vmi.Spec.Domain.Devices.UseVirtioTransitional)),
 		)
 	}
-	return domainSpecWithIfacesResource
+	return domainSpec, original
 }
 
-func newInterfacePlaceholder(index int, modelType string) api.Interface {
-	return api.Interface{
-		Type:  "ethernet",
-		Model: &api.Model{Type: modelType},
-		Target: &api.InterfaceTarget{
-			Device:  fmt.Sprintf("placeholder-%d", index),
+func newInterfacePlaceholder(index int, modelType string) libvirtxml.DomainInterface {
+	return libvirtxml.DomainInterface{
+		Source: &libvirtxml.DomainInterfaceSource{
+			Ethernet: &libvirtxml.DomainInterfaceSourceEthernet{},
+		},
+		Model: &libvirtxml.DomainInterfaceModel{Type: modelType},
+		Target: &libvirtxml.DomainInterfaceTarget{
+			Dev:     fmt.Sprintf("placeholder-%d", index),
 			Managed: "no",
 		},
 	}

pkg/virt-launcher/virtwrap/util/BUILD.bazel
@@ -23,6 +23,7 @@ go_library(
         "//vendor/k8s.io/apimachinery/pkg/apis/meta/v1:go_default_library",
         "//vendor/k8s.io/apimachinery/pkg/types:go_default_library",
         "//vendor/libvirt.org/go/libvirt:go_default_library",
+        "//vendor/libvirt.org/go/libvirtxml:go_default_library",
     ],
 )
 

pkg/virt-launcher/virtwrap/util/libvirt_helper.go
@@ -22,6 +22,7 @@ import (
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"libvirt.org/go/libvirt"
+	"libvirt.org/go/libvirtxml"
 
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/converter"
 
@@ -139,7 +140,11 @@ func SetDomainSpecStr(virConn cli.Connection, vmi *v1.VirtualMachineInstance, wa
 	return dom, nil
 }
 
-func SetDomainSpecStrWithHooks(virConn cli.Connection, vmi *v1.VirtualMachineInstance, wantedSpec *api.DomainSpec) (cli.VirDomain, *api.DomainSpec, error) {
+func SetDomainSpecStrWithHooks(
+	virConn cli.Connection,
+	vmi *v1.VirtualMachineInstance,
+	wantedSpec *libvirtxml.Domain,
+) (cli.VirDomain, *libvirtxml.Domain, error) {
 	hooksManager := getHookManager()
 	domainSpec, err := hooksManager.OnDefineDomain(wantedSpec, vmi)
 	if err != nil {
@@ -147,11 +152,10 @@ func SetDomainSpecStrWithHooks(virConn cli.Connection, vmi *v1.VirtualMachineIns
 	}
 
 	// update wantedSpec to reflect changes made to domain spec by hooks
-	domainSpecObj := &api.DomainSpec{}
+	domainSpecObj := &libvirtxml.Domain{}
 	if err = xml.Unmarshal([]byte(domainSpec), domainSpecObj); err != nil {
 		return nil, nil, err
 	}
-	domainSpecObj.DeepCopyInto(wantedSpec)
 
 	dom, err := SetDomainSpecStr(virConn, vmi, domainSpec)
 	if err != nil {