4ec481942e2fa17df741828510968bb2f683ff5d
Felix Matouschek <fmatouschek@redhat.com>
2024-04-10 14:28:14 +0200

    fix: Disable common-instancetypes feature gate before tests
    Disable the common-instancetypes feature gate explicitly before running
    virt-operator common-instancetypes deployment tests to make sure the
    feature gate was not enabled before (e.g. in downstream deployments).
    After running tests restore the feature gate to its original value.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/operator/operator.go
+++ tests/operator/operator.go
@@ -2973,11 +2973,19 @@ spec:
 
 	Context("[Serial] Deployment of common-instancetypes", Serial, func() {
 		var (
+			fgEnabled     bool
 			appComponent  string
 			labelSelector string
 		)
 
 		BeforeEach(func() {
+			if fgEnabled = checks.HasFeature(virtconfig.PersistentReservation); fgEnabled {
+				// Disable feature gate for cases in which the feature gate was enabled
+				// prior to running any of these tests. (e.g. in downstream)
+				tests.DisableFeatureGate(virtconfig.CommonInstancetypesDeploymentGate)
+				testsuite.EnsureKubevirtReady()
+			}
+
 			appComponent = apply.GetAppComponent(util2.GetCurrentKv(virtClient))
 			labelSelector = labels.Set{
 				v1.AppComponentLabel: appComponent,
@@ -2986,7 +2994,12 @@ spec:
 		})
 
 		AfterEach(func() {
-			tests.DisableFeatureGate(virtconfig.CommonInstancetypesDeploymentGate)
+			// Reset the feature gate to its original value after running any of these tests.
+			if fgEnabled {
+				tests.EnableFeatureGate(virtconfig.CommonInstancetypesDeploymentGate)
+			} else {
+				tests.DisableFeatureGate(virtconfig.CommonInstancetypesDeploymentGate)
+			}
 			testsuite.EnsureKubevirtReady()
 		})
 
