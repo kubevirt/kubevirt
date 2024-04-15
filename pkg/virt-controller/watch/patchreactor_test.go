203f2e5f140b1b146b25e89718ab174ccd81b37a
Luboslav Pivarc <lpivarc@redhat.com>
2024-04-15 11:41:34 +0200

    Add reactors to simulate /status behaviour
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

---
+++ pkg/virt-controller/watch/patchreactor_test.go
@@ -0,0 +1,114 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2024 The KubeVirt Authors
+ *
+ */
+
+package watch
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"k8s.io/client-go/testing"
+)
+
+// PatchReactor should be used to replace default reactor
+// handle - takes subresources and should return if the request should be handled, e.g /status
+// modify - takes new and old object and should return object that should be stored
+func PatchReactor(handle func(string) bool, tracker testing.ObjectTracker,
+	modify func(new, old runtime.Object) runtime.Object,
+) func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+	return func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		if !handle(action.GetSubresource()) {
+			return false, nil, nil
+		}
+		switch action := action.(type) {
+		case testing.PatchActionImpl:
+			obj, err := tracker.Get(action.GetResource(), action.GetNamespace(), action.GetName())
+			if err != nil {
+				return true, nil, err
+			}
+
+			oldObj, err := tracker.Get(action.GetResource(), action.GetNamespace(), action.GetName())
+			if err != nil {
+				return true, nil, err
+			}
+
+			old, err := json.Marshal(obj)
+			if err != nil {
+				return true, nil, err
+			}
+
+			// reset the object in preparation to unmarshal, since unmarshal does not guarantee that fields
+			// in obj that are removed by patch are cleared
+			value := reflect.ValueOf(obj)
+			value.Elem().Set(reflect.New(value.Type().Elem()).Elem())
+
+			switch action.GetPatchType() {
+			case types.JSONPatchType:
+				patch, err := jsonpatch.DecodePatch(action.GetPatch())
+				if err != nil {
+					return true, nil, err
+				}
+				modified, err := patch.Apply(old)
+				if err != nil {
+					return true, nil, err
+				}
+
+				if err = json.Unmarshal(modified, obj); err != nil {
+					return true, nil, err
+				}
+			case types.MergePatchType:
+				modified, err := jsonpatch.MergePatch(old, action.GetPatch())
+				if err != nil {
+					return true, nil, err
+				}
+
+				if err := json.Unmarshal(modified, obj); err != nil {
+					return true, nil, err
+				}
+			case types.StrategicMergePatchType, types.ApplyPatchType:
+				mergedByte, err := strategicpatch.StrategicMergePatch(old, action.GetPatch(), obj)
+				if err != nil {
+					return true, nil, err
+				}
+				if err = json.Unmarshal(mergedByte, obj); err != nil {
+					return true, nil, err
+				}
+			default:
+				return true, nil, fmt.Errorf("PatchType is not supported")
+			}
+
+			obj = modify(obj, oldObj)
+
+			if err = tracker.Update(action.GetResource(), obj, action.GetNamespace()); err != nil {
+				return true, nil, err
+			}
+
+			return true, obj, nil
+
+		default:
+			panic("Unexpected action implementation")
+		}
+	}
+
+}
