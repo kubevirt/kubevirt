d4c37f267c1e1517c845d6c3fdc5816fd54548ab
Jelmer Vernooĳ <jvernooij@evroc.com>
2024-05-09 14:53:49 +0000

    Fix typo: VirutalMachineInstance => VirtualMachineInstance
    Signed-off-by: Jelmer Vernooĳ <jvernooij@evroc.com>

--- pkg/virtctl/pause/pause.go
+++ pkg/virtctl/pause/pause.go
@@ -137,7 +137,7 @@ func (vc *VirtCommand) Run(args []string) error {
 				if errors.IsNotFound(err) {
 					runningStrategy, err := vm.RunStrategy()
 					if err != nil {
-						return fmt.Errorf("Error pausing VirutalMachineInstance %s: %v", vmiName, err)
+						return fmt.Errorf("Error pausing VirtualMachineInstance %s: %v", vmiName, err)
 					}
 					if runningStrategy == kubevirtV1.RunStrategyHalted {
 						return fmt.Errorf("Error pausing VirtualMachineInstance %s. VirtualMachine %s is not set to run", vmiName, vm.Name)
@@ -145,7 +145,7 @@ func (vc *VirtCommand) Run(args []string) error {
 					return fmt.Errorf("Error pausing VirtualMachineInstance %s, it was not found", vmiName)
 
 				}
-				return fmt.Errorf("Error pausing VirutalMachineInstance %s: %v", vmiName, err)
+				return fmt.Errorf("Error pausing VirtualMachineInstance %s: %v", vmiName, err)
 			}
 			printLog(vmiName, vc.command)
 
