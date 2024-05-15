47a2be3784bd709e5ba2b3ac258cd8f0ca914754
Simone Tiraboschi <stirabos@redhat.com>
2024-05-15 15:42:24 +0200

    Align with k8s.io/code-generator@v0.30.0 (#2963)
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/generate.sh
+++ hack/generate.sh
@@ -2,39 +2,36 @@
 set -ex
 
 K8S_VER=$(grep "k8s.io/api => k8s.io/api" go.mod | xargs | cut -d" " -f4)
+KUBEOPENAPI_VER=$(grep "k8s.io/kube-openapi" go.mod | xargs | cut -d" " -f2)
 PROJECT_ROOT="$(readlink -e "$(dirname "${BASH_SOURCE[0]}")"/../)"
 
-# TODO: fix me!
-K8S_VER=v0.29.3
+PACKAGE=github.com/kubevirt/hyperconverged-cluster-operator
+API_FOLDER=api
+API_VERSION=v1beta1
 
 go install \
-k8s.io/code-generator/cmd/deepcopy-gen@${K8S_VER} \
-k8s.io/code-generator/cmd/defaulter-gen@${K8S_VER} \
-k8s.io/code-generator/cmd/openapi-gen@${K8S_VER}
+	k8s.io/code-generator/cmd/deepcopy-gen@${K8S_VER} \
+	k8s.io/code-generator/cmd/defaulter-gen@${K8S_VER}
+
+go install \
+	k8s.io/kube-openapi/cmd/openapi-gen@${KUBEOPENAPI_VER}
 
 deepcopy-gen \
+	--output-file zz_generated.deepcopy.go \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}" \
-	--output-file-base zz_generated.deepcopy \
-	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
+	"${PACKAGE}/${API_FOLDER}/${API_VERSION}"
 
 defaulter-gen \
+	--output-file zz_generated.defaults.go \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}" \
-	--output-file-base zz_generated.defaults \
-	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
+	"${PACKAGE}/${API_FOLDER}/${API_VERSION}"
 
 openapi-gen \
+	--output-file zz_generated.openapi.go \
 	--go-header-file "${PROJECT_ROOT}/hack/boilerplate.go.txt" \
-	--output-base "${PROJECT_ROOT}" \
-	--output-file-base zz_generated.openapi \
-	--input-dirs github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--output-package github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
-	--trim-path-prefix="${PROJECT_ROOT}/github.com/kubevirt/hyperconverged-cluster-operator/"
+	--output-dir api/v1beta1/ \
+	--output-pkg github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1 \
+	"${PACKAGE}/${API_FOLDER}/${API_VERSION}"
 
 go fmt api/v1beta1/zz_generated.deepcopy.go
 go fmt api/v1beta1/zz_generated.defaults.go
