0b67c5d3850549ccddfbbf1ea6c24391281d1704
Oren Cohen <ocohen@redhat.com>
2024-05-22 12:03:42 +0300

    [CNV-39949] enable nosniff for kubevirt-console-plugin nginx config (#2965)
    To increase security measures, it is advise to add the 'nosniff' Anti-MIME-Sniffing header X-Content-Type-Options in the kubevirt-console-plugin
    NGINX configuration. The advantages of setting it are:
        Improved Security: Prevents MIME type sniffing and reduces the risk of executing unintended scripts.
        XSS Mitigation: Decreases the chances of cross-site scripting attacks.
        Effective CSP: Ensures that Content Security Policies are enforced correctly.
        Adherence to Best Practices: Aligns with recommended security practices.
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- controllers/operands/kubevirtConsolePlugin.go
+++ controllers/operands/kubevirtConsolePlugin.go
@@ -264,6 +264,7 @@ http {
 	include            /etc/nginx/mime.types;
 	default_type       application/octet-stream;
 	keepalive_timeout  65;
+	add_header X-Content-Type-Options nosniff;
 		server {
 			listen              %d ssl;
 			ssl_certificate     /var/serving-cert/tls.crt;
