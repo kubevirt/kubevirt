kubevirt/kubevirt

commit 45695b61be5eb9e78d5102eb5a80a59cc208f22c

watch: unit test for replacement volume strategy
Signed-off-by: Alice Frosi <afrosi@redhat.com>

pkg/virt-controller/watch/vm_test.go
@@ -47,6 +47,7 @@ import (
 	virtcontroller "kubevirt.io/kubevirt/pkg/controller"
 	"kubevirt.io/kubevirt/pkg/instancetype"
 	kvpointer "kubevirt.io/kubevirt/pkg/pointer"
+	virtpointer "kubevirt.io/kubevirt/pkg/pointer"
 	"kubevirt.io/kubevirt/pkg/testutils"
 	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 	watchutil "kubevirt.io/kubevirt/pkg/virt-controller/watch/util"
@@ -5466,6 +5467,37 @@ var _ = Describe("VirtualMachine", func() {
 					sanityExecute(vm)
 				})
 
+			})
+
+			Context("Volumes", func() {
+				DescribeTable("should set the restart condition", func(strategy *v1.UpdateVolumesStrategy) {
+					testutils.UpdateFakeKubeVirtClusterConfig(kvInformer, &v1.KubeVirt{
+						Spec: v1.KubeVirtSpec{
+							Configuration: v1.KubeVirtConfiguration{
+								VMRolloutStrategy: &liveUpdate,
+								DeveloperConfiguration: &v1.DeveloperConfiguration{
+									FeatureGates: []string{
+										virtconfig.VMLiveUpdateFeaturesGate,
+										virtconfig.VolumesUpdateStrategy,
+									},
+								},
+							},
+						},
+					})
+					vm, vmi := DefaultVirtualMachine(true)
+					vm.Spec.Template.Spec.Volumes = append(vm.Spec.Template.Spec.Volumes, v1.Volume{
+						Name: "vol1"})
+					vmi.Spec.Volumes = append(vmi.Spec.Volumes, v1.Volume{Name: "vol2"})
+					controller.handleVolumeUpdateRequest(vm, vmi)
+					cond := virtcontroller.NewVirtualMachineConditionManager().GetCondition(vm, v1.VirtualMachineRestartRequired)
+					Expect(cond).ToNot(BeNil())
+					Expect(cond.Status).To(Equal(k8score.ConditionTrue))
+					Expect(cond.Message).To(Equal("the volumes replacement is effective only after restart"))
+				},
+					Entry("without the updateVolumeStrategy field", nil),
+					Entry("with the replacement updateVolumeStrategy",
+						virtpointer.P(v1.UpdateVolumesStrategyReplacement)),
+				)
 			})
 		})
 

commit 20bda0fef0bfe742b44d99e19aebb61ac3e0628d

controller: Add RestartRequired if instance type or preference changed
As VMs referencing instance types are no longer rejected when LiveUpdate
is enabled this change adds some basic logic to ensure the
RestartRequired condition is populated if the matchers are changed.

Conflicts:
    pkg/virt-controller/watch/vm_test.go

NOTE(lyarwood): Conflicts caused by
45695b61be5eb9e78d5102eb5a80a59cc208f22c not being present on
release-1.2. The use of libvmi has also been removed from the tests as it
didn't support the WithInstancetype and WithPreference helpers on the
release-1.2 branch.

Signed-off-by: Lee Yarwood <lyarwood@redhat.com>
(cherry picked from commit 1e1c7f8ea90c2140ffe483eef013d2b3a872a504)

pkg/virt-controller/watch/vm.go
@@ -2989,6 +2989,17 @@ func (c *VMController) addRestartRequiredIfNeeded(lastSeenVMSpec *virtv1.Virtual
 		return vm, true
 	}
 
+	if !equality.Semantic.DeepEqual(lastSeenVMSpec.Instancetype, vm.Spec.Instancetype) || !equality.Semantic.DeepEqual(lastSeenVMSpec.Preference, vm.Spec.Preference) {
+		vmConditionManager := controller.NewVirtualMachineConditionManager()
+		vmConditionManager.UpdateCondition(vm, &virtv1.VirtualMachineCondition{
+			Type:               virtv1.VirtualMachineRestartRequired,
+			LastTransitionTime: metav1.Now(),
+			Status:             k8score.ConditionTrue,
+			Message:            "the instance type or preference matcher of the VM was changed",
+		})
+		return vm, true
+	}
+
 	return vm, false
 }
 

pkg/virt-controller/watch/vm_test.go
@@ -5984,6 +5984,80 @@ var _ = Describe("VirtualMachine", func() {
 				})
 
 			})
+
+			Context("Instance Types and Preferences", func() {
+				BeforeEach(func() {
+					testutils.UpdateFakeKubeVirtClusterConfig(kvInformer, &v1.KubeVirt{
+						Spec: v1.KubeVirtSpec{
+							Configuration: v1.KubeVirtConfiguration{
+								VMRolloutStrategy: &liveUpdate,
+								DeveloperConfiguration: &v1.DeveloperConfiguration{
+									FeatureGates: []string{virtconfig.VMLiveUpdateFeaturesGate},
+								},
+							},
+						},
+					})
+				})
+				DescribeTable("should add RestartRequired to VM when", func(originalVM, updatedVM *v1.VirtualMachine) {
+					_, required := controller.addRestartRequiredIfNeeded(&originalVM.Spec, updatedVM)
+					Expect(required).To(BeTrue())
+					vmConditionController := virtcontroller.NewVirtualMachineConditionManager()
+					Expect(vmConditionController.HasCondition(updatedVM, v1.VirtualMachineRestartRequired)).To(BeTrue())
+				},
+					Entry("instance type changed",
+						&v1.VirtualMachine{
+							Spec: v1.VirtualMachineSpec{
+								Instancetype: &v1.InstancetypeMatcher{
+									Name: "original",
+								},
+								Template: &v1.VirtualMachineInstanceTemplateSpec{
+									Spec: v1.VirtualMachineInstanceSpec{
+										Domain: v1.DomainSpec{},
+									},
+								},
+							},
+						},
+						&v1.VirtualMachine{
+							Spec: v1.VirtualMachineSpec{
+								Instancetype: &v1.InstancetypeMatcher{
+									Name: "updated",
+								},
+								Template: &v1.VirtualMachineInstanceTemplateSpec{
+									Spec: v1.VirtualMachineInstanceSpec{
+										Domain: v1.DomainSpec{},
+									},
+								},
+							},
+						},
+					),
+					Entry("preference changed",
+						&v1.VirtualMachine{
+							Spec: v1.VirtualMachineSpec{
+								Preference: &v1.PreferenceMatcher{
+									Name: "original",
+								},
+								Template: &v1.VirtualMachineInstanceTemplateSpec{
+									Spec: v1.VirtualMachineInstanceSpec{
+										Domain: v1.DomainSpec{},
+									},
+								},
+							},
+						},
+						&v1.VirtualMachine{
+							Spec: v1.VirtualMachineSpec{
+								Preference: &v1.PreferenceMatcher{
+									Name: "updated",
+								},
+								Template: &v1.VirtualMachineInstanceTemplateSpec{
+									Spec: v1.VirtualMachineInstanceSpec{
+										Domain: v1.DomainSpec{},
+									},
+								},
+							},
+						},
+					),
+				)
+			})
 		})
 
 		Context("CPU topology", func() {