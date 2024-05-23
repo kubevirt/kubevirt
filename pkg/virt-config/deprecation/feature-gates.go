4b0064c29782b1d44a28f4a8c726ef2d1af670c0
Edward Haas <edwardh@redhat.com>
2024-05-23 21:05:12 +0300

    virt-config, feature-gates: Relocate the macvtap and passt FG
    Distribute the macvtap and passt feature-gates to their relevant source
    files.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- pkg/virt-config/deprecation/feature-gates.go
+++ pkg/virt-config/deprecation/feature-gates.go
@@ -73,8 +73,6 @@ const (
 	// DynamicPodInterfaceNamingGate enables a mechanism to dynamically determine the primary pod interface for KubeVirt virtual machines.
 	DynamicPodInterfaceNamingGate = "DynamicPodInterfaceNaming" // GA
 
-	PasstGate   = "Passt"   // Deprecated
-	MacvtapGate = "Macvtap" // Deprecated
 	// DockerSELinuxMCSWorkaround sets the SELinux level of all the non-compute virt-launcher containers to "s0".
 	DockerSELinuxMCSWorkaround = "DockerSELinuxMCSWorkaround" // Deprecated
 )
@@ -103,8 +101,6 @@ func init() {
 	RegisterFeatureGate(FeatureGate{Name: NetworkBindingPlugingsGate, State: GA})
 	RegisterFeatureGate(FeatureGate{Name: DynamicPodInterfaceNamingGate, State: GA})
 
-	RegisterFeatureGate(FeatureGate{Name: PasstGate, State: Discontinued, Message: PasstDiscontinueMessage, VmiSpecUsed: passtApiUsed})
-	RegisterFeatureGate(FeatureGate{Name: MacvtapGate, State: Discontinued, Message: MacvtapDiscontinueMessage, VmiSpecUsed: macvtapApiUsed})
 	RegisterFeatureGate(FeatureGate{Name: DockerSELinuxMCSWorkaround, State: Deprecated, Message: fmt.Sprintf("DockerSELinuxMCSWorkaround has been deprecated since v1.4.")})
 }
 
