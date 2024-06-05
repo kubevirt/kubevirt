25f567dafe65e80ffccebb37d4d13ab951dd02e9
Alex Kalenyuk <akalenyu@redhat.com>
2024-06-05 19:39:07 +0300

    Revert "When VM deletion timestamp is set, patch VMI TerminationGracePeriod with VM DeletionGracePeriod"
    This reverts commit b9f51761d72876d8eb735dfa7cfb321dbd2ae05c.
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- pkg/virt-controller/watch/vm.go
+++ pkg/virt-controller/watch/vm.go
@@ -1446,16 +1446,6 @@ func (c *VMController) stopVMI(vm *virtv1.VirtualMachine, vmi *virtv1.VirtualMac
 	}
 
 	// stop it
-	// if for some reason the VM has been requested to be deleted, we want to use the
-	// deletion grace period specified to the VM as the TerminationGracePeriodSeconds
-	// for the VMI.
-	if vm.DeletionTimestamp != nil {
-		err = c.patchVMITerminationGracePeriod(vm.GetDeletionGracePeriodSeconds(), vmi)
-		if err != nil {
-			log.Log.Object(vmi).Errorf("unable to patch vmi termination grace period: %v", err)
-			return err
-		}
-	}
 	c.expectations.ExpectDeletions(vmKey, []string{controller.VirtualMachineInstanceKey(vmi)})
 	err = c.clientset.VirtualMachineInstance(vm.ObjectMeta.Namespace).Delete(context.Background(), vmi.ObjectMeta.Name, &v1.DeleteOptions{})
 
@@ -3085,13 +3075,4 @@ func (c *VMController) setupLiveFeatures(
 
 	c.setupCPUHotplug(vm, vmi, VMIDefaults, maxRatio)
 	c.setupMemoryHotplug(vm, vmi, maxRatio)
-}
-
-func (c *VMController) patchVMITerminationGracePeriod(gracePeriod *int64, vmi *virtv1.VirtualMachineInstance) error {
-	if gracePeriod == nil {
-		return nil
-	}
-	patch := fmt.Sprintf(`{"spec":{"terminationGracePeriodSeconds": %d }}`, *gracePeriod)
-	_, err := c.clientset.VirtualMachineInstance(vmi.Namespace).Patch(context.Background(), vmi.Name, types.MergePatchType, []byte(patch), &v1.PatchOptions{})
-	return err
 }
