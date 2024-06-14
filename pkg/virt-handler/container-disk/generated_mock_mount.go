1709b9a0dd4497889a2663bbad503c6a3678c14a
Alice Frosi <afrosi@redhat.com>
2024-06-14 14:30:12 +0200

    handler: remove unused functions
    The containerdisk artifacts don't need to be mounted by virt-handler
    into the launcher container thanks to the pid namespace sharing.
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/virt-handler/container-disk/generated_mock_mount.go
+++ pkg/virt-handler/container-disk/generated_mock_mount.go
@@ -4,12 +4,8 @@
 package container_disk
 
 import (
-	time "time"
-
 	gomock "github.com/golang/mock/gomock"
 	v1 "kubevirt.io/api/core/v1"
-
-	container_disk "kubevirt.io/kubevirt/pkg/container-disk"
 )
 
 // Mock of Mounter interface
@@ -33,28 +29,6 @@ func (_m *MockMounter) EXPECT() *_MockMounterRecorder {
 	return _m.recorder
 }
 
-func (_m *MockMounter) ContainerDisksReady(vmi *v1.VirtualMachineInstance, notInitializedSince time.Time) (bool, error) {
-	ret := _m.ctrl.Call(_m, "ContainerDisksReady", vmi, notInitializedSince)
-	ret0, _ := ret[0].(bool)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-func (_mr *_MockMounterRecorder) ContainerDisksReady(arg0, arg1 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "ContainerDisksReady", arg0, arg1)
-}
-
-func (_m *MockMounter) MountAndVerify(vmi *v1.VirtualMachineInstance) (map[string]*container_disk.DiskInfo, error) {
-	ret := _m.ctrl.Call(_m, "MountAndVerify", vmi)
-	ret0, _ := ret[0].(map[string]*container_disk.DiskInfo)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-func (_mr *_MockMounterRecorder) MountAndVerify(arg0 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "MountAndVerify", arg0)
-}
-
 func (_m *MockMounter) Unmount(vmi *v1.VirtualMachineInstance) error {
 	ret := _m.ctrl.Call(_m, "Unmount", vmi)
 	ret0, _ := ret[0].(error)
@@ -63,15 +37,4 @@ func (_m *MockMounter) Unmount(vmi *v1.VirtualMachineInstance) error {
 
 func (_mr *_MockMounterRecorder) Unmount(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "Unmount", arg0)
-}
-
-func (_m *MockMounter) ComputeChecksums(vmi *v1.VirtualMachineInstance) (*DiskChecksums, error) {
-	ret := _m.ctrl.Call(_m, "ComputeChecksums", vmi)
-	ret0, _ := ret[0].(*DiskChecksums)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-func (_mr *_MockMounterRecorder) ComputeChecksums(arg0 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "ComputeChecksums", arg0)
 }
--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -80,6 +80,7 @@ import (
 	"kubevirt.io/kubevirt/pkg/util/hardware"
 	"kubevirt.io/kubevirt/pkg/util/migrations"
 
+	container_disk "kubevirt.io/kubevirt/pkg/virt-handler/container-disk"
 	device_manager "kubevirt.io/kubevirt/pkg/virt-handler/device-manager"
 	hotplug_volume "kubevirt.io/kubevirt/pkg/virt-handler/hotplug-disk"
 
@@ -339,6 +340,7 @@ type VirtualMachineController struct {
 	deviceManagerController  *device_manager.DeviceController
 	migrationProxy           migrationproxy.ProxyManager
 	podIsolationDetector     isolation.PodIsolationDetector
+	containerDiskMounter     container_disk.Mounter
 	hotplugVolumeMounter     hotplug_volume.VolumeMounter
 	clusterConfig            *virtconfig.ClusterConfig
 	sriovHotplugExecutorPool *executor.RateLimitedExecutorPool
@@ -2204,6 +2206,11 @@ func (d *VirtualMachineController) processVmCleanup(vmi *v1.VirtualMachineInstan
 	d.migrationProxy.StopSourceListener(vmiId)
 
 	d.downwardMetricsManager.StopServer(vmi)
+
+	// Unmount container disks and clean up remaining files
+	if err := d.containerDiskMounter.Unmount(vmi); err != nil {
+		return err
+	}
 
 	// UnmountAll does the cleanup on the "best effort" basis: it is
 	// safe to pass a nil cgroupManager.
