3d18a1bc0c421d473135ce26615f6f112df23848
Ram Lavi <ralavi@redhat.com>
2024-07-01 10:27:57 +0300

    Move retry on conflict function to libreplicaset
    This function is used in this format only on libreplicaset package.
    There is no real value in setting this function in tests/utils.go.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- tests/libreplicaset/replicaset.go
+++ tests/libreplicaset/replicaset.go
@@ -7,13 +7,15 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	autov1 "k8s.io/api/autoscaling/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
+	"kubevirt.io/client-go/log"
 
-	"kubevirt.io/kubevirt/tests"
 	"kubevirt.io/kubevirt/tests/testsuite"
 )
 
@@ -21,7 +23,7 @@ func DoScaleWithScaleSubresource(virtClient kubecli.KubevirtClient, name string,
 	// Status updates can conflict with our desire to change the spec
 	By(fmt.Sprintf("Scaling to %d", scale))
 	var s *autov1.Scale
-	err := tests.RetryIfModified(func() error {
+	err := retryIfModified(func() error {
 		s, err := virtClient.ReplicaSet(testsuite.GetTestNamespace(nil)).GetScale(context.Background(), name, v12.GetOptions{})
 		ExpectWithOffset(1, err).ToNot(HaveOccurred())
 		s.Spec.Replicas = scale
@@ -51,4 +53,16 @@ func FilterNotDeletedVMIs(vmis *v1.VirtualMachineInstanceList) []v1.VirtualMachi
 		}
 	}
 	return notDeleted
+}
+
+func retryIfModified(do func() error) (err error) {
+	retries := 0
+	for err = do(); errors.IsConflict(err); err = do() {
+		if retries >= 10 {
+			return fmt.Errorf("object seems to be permanently modified, failing after 10 retries: %v", err)
+		}
+		retries++
+		log.DefaultLogger().Reason(err).Infof("Object got modified, will retry.")
+	}
+	return err
 }
