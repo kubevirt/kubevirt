5bbe48aedf31818c65f9193d395ec4bc15d95bae
Orel Misan <omisan@redhat.com>
2024-08-11 09:45:59 +0300

    vmi update admitter: Remove call to IsKubeVirtServiceAccount
    The `IsKubeVirtServiceAccount` internally performs
    a file-system read.
    
    Base the logic on the KubeVirtServiceAccount set
    introduced by PR 11931[1].
    
    [1] https://github.com/kubevirt/kubevirt/pull/11931
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/virt-api/webhooks/validating-webhook/admitters/vmi-update-admitter.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/vmi-update-admitter.go
@@ -95,7 +95,7 @@ func (admitter *VMIUpdateAdmitter) Admit(ar *admissionv1.AdmissionReview) *admis
 	// Reject VMI update if VMI spec changed
 	if !equality.Semantic.DeepEqual(newVMI.Spec, oldVMI.Spec) {
 		// Only allow the KubeVirt SA to modify the VMI spec, since that means it went through the sub resource.
-		if webhooks.IsKubeVirtServiceAccount(ar.Request.UserInfo.Username) {
+		if _, isKubeVirtServiceAccount := admitter.KubeVirtServiceAccounts[ar.Request.UserInfo.Username]; isKubeVirtServiceAccount {
 			hotplugResponse := admitHotplug(oldVMI, newVMI, admitter.ClusterConfig)
 			if hotplugResponse != nil {
 				return hotplugResponse
