0e7d5173e609c4fd47ee8630d3dc1c57ef614378
Michael Henriksen <mhenriks@redhat.com>
2024-08-19 14:33:01 -0400

    add export.kubevirt.io to status updater
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>

--- pkg/util/status/status.go
+++ pkg/util/status/status.go
@@ -15,6 +15,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/api/core/v1"
+	exportv1 "kubevirt.io/api/export/v1beta1"
 	poolv1 "kubevirt.io/api/pool/v1alpha1"
 	"kubevirt.io/client-go/kubecli"
 )
@@ -203,6 +204,13 @@ func (u *updater) updateUnstructured(obj runtime.Object) (oldStatus interface{},
 			return nil, nil, err
 		}
 		return oldObj.Status, newObj.Status, nil
+	case *exportv1.VirtualMachineExport:
+		oldObj := obj.(*exportv1.VirtualMachineExport)
+		newObj, err := u.cli.VirtualMachineExport(a.GetNamespace()).Update(context.Background(), oldObj, metav1.UpdateOptions{})
+		if err != nil {
+			return nil, nil, err
+		}
+		return oldObj.Status, newObj.Status, nil
 	default:
 		panic(unknownObj)
 	}
@@ -232,6 +240,9 @@ func (u *updater) updateStatusUnstructured(obj runtime.Object) (err error) {
 	case *poolv1.VirtualMachinePool:
 		oldObj := obj.(*poolv1.VirtualMachinePool)
 		_, err = u.cli.VirtualMachinePool(oldObj.Namespace).UpdateStatus(context.Background(), oldObj, metav1.UpdateOptions{})
+	case *exportv1.VirtualMachineExport:
+		oldObj := obj.(*exportv1.VirtualMachineExport)
+		_, err = u.cli.VirtualMachineExport(oldObj.Namespace).UpdateStatus(context.Background(), oldObj, metav1.UpdateOptions{})
 	default:
 		panic(unknownObj)
 	}
@@ -363,6 +374,24 @@ func (v *VMPStatusUpdater) PatchStatus(vp *poolv1.VirtualMachinePool, pt types.P
 
 func NewVMPStatusUpdater(cli kubecli.KubevirtClient) *VMPStatusUpdater {
 	return &VMPStatusUpdater{
+		updater: updater{
+			lock:        sync.Mutex{},
+			subresource: true,
+			cli:         cli,
+		},
+	}
+}
+
+type VMExportStatusUpdater struct {
+	updater
+}
+
+func (v *VMExportStatusUpdater) UpdateStatus(vmExport *exportv1.VirtualMachineExport) error {
+	return v.update(vmExport)
+}
+
+func NewVMExportStatusUpdater(cli kubecli.KubevirtClient) *VMExportStatusUpdater {
+	return &VMExportStatusUpdater{
 		updater: updater{
 			lock:        sync.Mutex{},
 			subresource: true,
