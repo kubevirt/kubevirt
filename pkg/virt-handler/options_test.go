2e3929831039f155aaeb5b15e2921bc5c7c83d75
Adi Aloni <adialoni101@gmail.com>
2024-09-08 16:49:42 +0300

    Add options_test.go to test host capabilities parsing
    This commit adds options_test.go to test whether parsing
    nodelabeller/capabilities and converting it to corresponding cmd/v1
    structures succeeds as part of VMI host capabilities inference.
    
    The tests comprise of four tests for the following parsing cases:
    * Empty api.Capabilities
    * Nil api.Capabilities
    * api.Capabilities with a single NUMA node
    * api.Capabilities with multiple NUMA nodes
    
    The data for populating the relevant structs is
    referenced from capabilities.xml[1] and capabilities_with_numa.xml[2]
    respectively.
    
    [1] https://github.com/kubevirt/kubevirt/blob/3766e045c0157db9bc23cbd2698fd8e2809dea4f/pkg/virt-handler/node-labeller/testdata/capabilities.xml
    
    [2] https://github.com/kubevirt/kubevirt/blob/3766e045c0157db9bc23cbd2698fd8e2809dea4f/pkg/virt-handler/node-labeller/testdata/capabilities_with_numa.xml
    
    Signed-off-by: Adi Aloni <adialoni101@gmail.com>

---
+++ pkg/virt-handler/options_test.go
@@ -0,0 +1,342 @@
+/*
+Copyright 2024 The KubeVirt Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package virthandler
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	cmdv1 "kubevirt.io/kubevirt/pkg/handler-launcher-com/cmd/v1"
+	"kubevirt.io/kubevirt/pkg/virt-handler/node-labeller/api"
+)
+
+var _ = Describe("Parsing VMI Options", func() {
+	Context("virt-handler VM processes VMI options during update", func() {
+		const memoryUnit = "KiB"
+		DescribeTable("should convert api.Capabilities to cmdv1.Topology when Capabilities is nil or empty", func(caps *api.Capabilities) {
+			actualTopology := capabilitiesToTopology(caps)
+
+			expectedTopology := &cmdv1.Topology{}
+
+			Expect(actualTopology).To(Equal(expectedTopology))
+		},
+			Entry("api.Capabilities is nil", nil),
+			Entry("api.Capabilities is empty", &api.Capabilities{}),
+		)
+		It("should convert api.Capabilities to cmdv1.Topology with single NUMA node", func() {
+			caps := &api.Capabilities{}
+			caps.Host.Topology.Cells = api.Cells{
+				Cell: []api.Cell{
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 16256896},
+						Pages: []api.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 4064224},
+							{Unit: memoryUnit, Size: 2048, Count: 0},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{{ID: 0, Value: 10}}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0, Siblings: []uint32{0, 4}},
+							{ID: 1, Siblings: []uint32{1, 5}},
+							{ID: 2, Siblings: []uint32{2, 6}},
+							{ID: 3, Siblings: []uint32{3, 7}},
+							{ID: 4, Siblings: []uint32{0, 4}},
+							{ID: 5, Siblings: []uint32{1, 5}},
+							{ID: 6, Siblings: []uint32{2, 6}},
+							{ID: 7, Siblings: []uint32{3, 7}},
+						}},
+					},
+				},
+			}
+
+			actualTopology := capabilitiesToTopology(caps)
+
+			expectedTopology := &cmdv1.Topology{
+				NumaCells: []*cmdv1.Cell{
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 16256896},
+						Pages: []*cmdv1.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 4064224},
+							{Unit: memoryUnit, Size: 2048, Count: 0},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: []*cmdv1.Sibling{{Id: 0, Value: 10}},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0, Siblings: []uint32{0, 4}},
+							{Id: 1, Siblings: []uint32{1, 5}},
+							{Id: 2, Siblings: []uint32{2, 6}},
+							{Id: 3, Siblings: []uint32{3, 7}},
+							{Id: 4, Siblings: []uint32{0, 4}},
+							{Id: 5, Siblings: []uint32{1, 5}},
+							{Id: 6, Siblings: []uint32{2, 6}},
+							{Id: 7, Siblings: []uint32{3, 7}},
+						},
+					},
+				},
+			}
+
+			Expect(actualTopology).To(Equal(expectedTopology))
+		})
+
+		It("should convert api.Capabilities to cmdv1.Topology with multiple NUMA nodes, CPUs and pages", func() {
+			caps := &api.Capabilities{}
+			caps.Host.Topology.Cells = api.Cells{
+				Cell: []api.Cell{
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 1289144},
+						Pages: []api.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 314094},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{
+							{ID: 0, Value: 10},
+							{ID: 1, Value: 10},
+							{ID: 2, Value: 10},
+							{ID: 3, Value: 10},
+						}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0, Siblings: []uint32{0}},
+							{ID: 1, Siblings: []uint32{1}},
+							{ID: 2, Siblings: []uint32{2}},
+							{ID: 3, Siblings: []uint32{3}},
+							{ID: 4, Siblings: []uint32{4}},
+							{ID: 5, Siblings: []uint32{5}},
+						}},
+					},
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 1223960},
+						Pages: []api.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 297798},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{
+							{ID: 0, Value: 10},
+							{ID: 1, Value: 10},
+							{ID: 2, Value: 10},
+							{ID: 3, Value: 10},
+						}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0, Siblings: []uint32{0}},
+							{ID: 1, Siblings: []uint32{1}},
+							{ID: 2, Siblings: []uint32{2}},
+							{ID: 3, Siblings: []uint32{3}},
+							{ID: 4, Siblings: []uint32{4}},
+							{ID: 5, Siblings: []uint32{5}},
+						}},
+					},
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 1251752},
+						Pages: []api.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 304746},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{
+							{ID: 0, Value: 10},
+							{ID: 1, Value: 10},
+							{ID: 2, Value: 10},
+							{ID: 3, Value: 10},
+						}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0, Siblings: []uint32{0}},
+							{ID: 1, Siblings: []uint32{1}},
+							{ID: 2, Siblings: []uint32{2}},
+							{ID: 3, Siblings: []uint32{3}},
+							{ID: 4, Siblings: []uint32{4}},
+							{ID: 5, Siblings: []uint32{5}},
+						}},
+					},
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 1289404},
+						Pages: []api.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 314159},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{
+							{ID: 0, Value: 10},
+							{ID: 1, Value: 10},
+							{ID: 2, Value: 10},
+							{ID: 3, Value: 10},
+						}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0, Siblings: []uint32{0}},
+							{ID: 1, Siblings: []uint32{1}},
+							{ID: 2, Siblings: []uint32{2}},
+							{ID: 3, Siblings: []uint32{3}},
+							{ID: 4, Siblings: []uint32{4}},
+							{ID: 5, Siblings: []uint32{5}},
+						}},
+					},
+				},
+			}
+
+			actualTopology := capabilitiesToTopology(caps)
+
+			expectedTopology := &cmdv1.Topology{
+				NumaCells: []*cmdv1.Cell{
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 1289144},
+						Pages: []*cmdv1.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 314094},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: []*cmdv1.Sibling{
+							{Id: 0, Value: 10},
+							{Id: 1, Value: 10},
+							{Id: 2, Value: 10},
+							{Id: 3, Value: 10},
+						},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0, Siblings: []uint32{0}},
+							{Id: 1, Siblings: []uint32{1}},
+							{Id: 2, Siblings: []uint32{2}},
+							{Id: 3, Siblings: []uint32{3}},
+							{Id: 4, Siblings: []uint32{4}},
+							{Id: 5, Siblings: []uint32{5}},
+						},
+					},
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 1223960},
+						Pages: []*cmdv1.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 297798},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: []*cmdv1.Sibling{
+							{Id: 0, Value: 10},
+							{Id: 1, Value: 10},
+							{Id: 2, Value: 10},
+							{Id: 3, Value: 10},
+						},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0, Siblings: []uint32{0}},
+							{Id: 1, Siblings: []uint32{1}},
+							{Id: 2, Siblings: []uint32{2}},
+							{Id: 3, Siblings: []uint32{3}},
+							{Id: 4, Siblings: []uint32{4}},
+							{Id: 5, Siblings: []uint32{5}},
+						},
+					},
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 1251752},
+						Pages: []*cmdv1.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 304746},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: []*cmdv1.Sibling{
+							{Id: 0, Value: 10},
+							{Id: 1, Value: 10},
+							{Id: 2, Value: 10},
+							{Id: 3, Value: 10},
+						},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0, Siblings: []uint32{0}},
+							{Id: 1, Siblings: []uint32{1}},
+							{Id: 2, Siblings: []uint32{2}},
+							{Id: 3, Siblings: []uint32{3}},
+							{Id: 4, Siblings: []uint32{4}},
+							{Id: 5, Siblings: []uint32{5}},
+						},
+					},
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 1289404},
+						Pages: []*cmdv1.Pages{
+							{Unit: memoryUnit, Size: 4, Count: 314159},
+							{Unit: memoryUnit, Size: 2048, Count: 16},
+							{Unit: memoryUnit, Size: 1048576, Count: 0},
+						},
+						Distances: []*cmdv1.Sibling{
+							{Id: 0, Value: 10},
+							{Id: 1, Value: 10},
+							{Id: 2, Value: 10},
+							{Id: 3, Value: 10},
+						},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0, Siblings: []uint32{0}},
+							{Id: 1, Siblings: []uint32{1}},
+							{Id: 2, Siblings: []uint32{2}},
+							{Id: 3, Siblings: []uint32{3}},
+							{Id: 4, Siblings: []uint32{4}},
+							{Id: 5, Siblings: []uint32{5}},
+						},
+					},
+				},
+			}
+
+			Expect(actualTopology).To(Equal(expectedTopology))
+		})
+		It("should convert api.Capabilities to cmdv1.Topology when certain fields of Capabilities are not initialized", func() {
+			caps := &api.Capabilities{}
+			caps.Host.Topology.Cells = api.Cells{
+				Cell: []api.Cell{
+					{
+						Memory: api.Memory{Unit: memoryUnit, Amount: 16256896},
+						Pages: []api.Pages{
+							{Size: 4, Count: 4064224},
+							{Unit: memoryUnit, Count: 0},
+							{Unit: memoryUnit, Size: 1048576},
+						},
+						Distances: api.Distances{Sibling: []api.Sibling{{ID: 0, Value: 10}}},
+						Cpus: api.CPUs{CPU: []api.CPU{
+							{ID: 0},
+							{Siblings: []uint32{1, 5}},
+							{ID: 2, Siblings: []uint32{2, 6}},
+							{ID: 3, Siblings: []uint32{3, 7}},
+							{ID: 4, Siblings: []uint32{0, 4}},
+							{ID: 5, Siblings: []uint32{1, 5}},
+							{ID: 6, Siblings: []uint32{2, 6}},
+							{ID: 7, Siblings: []uint32{3, 7}},
+						}},
+					},
+				},
+			}
+
+			actualTopology := capabilitiesToTopology(caps)
+
+			expectedTopology := &cmdv1.Topology{
+				NumaCells: []*cmdv1.Cell{
+					{
+						Memory: &cmdv1.Memory{Unit: memoryUnit, Amount: 16256896},
+						Pages: []*cmdv1.Pages{
+							{Size: 4, Count: 4064224},
+							{Unit: memoryUnit, Count: 0},
+							{Unit: memoryUnit, Size: 1048576},
+						},
+						Distances: []*cmdv1.Sibling{{Id: 0, Value: 10}},
+						Cpus: []*cmdv1.CPU{
+							{Id: 0},
+							{Siblings: []uint32{1, 5}},
+							{Id: 2, Siblings: []uint32{2, 6}},
+							{Id: 3, Siblings: []uint32{3, 7}},
+							{Id: 4, Siblings: []uint32{0, 4}},
+							{Id: 5, Siblings: []uint32{1, 5}},
+							{Id: 6, Siblings: []uint32{2, 6}},
+							{Id: 7, Siblings: []uint32{3, 7}},
+						},
+					},
+				},
+			}
+
+			Expect(actualTopology).To(Equal(expectedTopology))
+		})
+	})
+})
