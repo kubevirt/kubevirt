102aff9f820431e656bc57f6d00982a3301ffc89
Jed Lejosne <jed@redhat.com>
2024-09-12 15:53:11 -0400

    migration controller: flush secondary queue when the main queue is empty
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-controller/watch/migration/migration.go
+++ pkg/virt-controller/watch/migration/migration.go
@@ -244,6 +244,9 @@ func (c *Controller) runWorker() {
 }
 
 func (c *Controller) Execute() bool {
+	if c.Queue.Len() == 0 {
+		c.pendingQueue.FlushTo(c.Queue)
+	}
 	key, quit := c.Queue.Get()
 	if quit {
 		return false
@@ -424,9 +427,9 @@ func (c *Controller) failMigration(migration *virtv1.VirtualMachineInstanceMigra
 	return nil
 }
 
-// flushPendingQueue checks if a migration just finished.
+// flushPendingQueueOnMigrationCompletion checks if a migration just finished.
 // If it did, it will enqueue all items from the pendingQueue to the main queue and empty the pendingQueue.
-func (c *Controller) flushPendingQueue(oldVMIM, newVMIM *virtv1.VirtualMachineInstanceMigration) {
+func (c *Controller) flushPendingQueueOnMigrationCompletion(oldVMIM, newVMIM *virtv1.VirtualMachineInstanceMigration) {
 	if oldVMIM.Status.Phase == newVMIM.Status.Phase || !newVMIM.IsFinal() {
 		return
 	}
@@ -580,7 +583,7 @@ func (c *Controller) updateStatus(migration *virtv1.VirtualMachineInstanceMigrat
 		}
 	}
 
-	c.flushPendingQueue(migration, migrationCopy)
+	c.flushPendingQueueOnMigrationCompletion(migration, migrationCopy)
 
 	return nil
 }
