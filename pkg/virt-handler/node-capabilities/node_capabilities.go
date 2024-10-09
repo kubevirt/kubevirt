636dd3483418114a28c4be0f4b5559b5039ce04a
Adi Aloni <adialoni101@gmail.com>
2024-10-09 16:39:58 +0300

    node-capabilities - add domain capabilities parsing
    This commit enhances the node-capabilities package by adding support for
    parsing domain capabilities (and consequentially supported features). The new logic aims to simplify the information
    consumed by the node-labeller by providing it with primitive types
    instead of complex ones, eliminating its need for extracting data from
    intricate structures on its own, ergo the introduction of the new types that
    aggregate this information for it.
    
    Tests and testdata have been adapted from
    node-labeller/cpu_plugin_test.go [1] as part of the goal of gradual
    transition and its eventual deprecation.
    
    Follow-up commits will update node-labeller and virt-handler to utilize
    these newly added functions.
    
    [1] https://github.com/kubevirt/kubevirt/blob/742c2604e88683a427cb6310c01989f1457991e9/pkg/virt-handler/node-labeller/cpu_plugin_test.go
    
    Signed-off-by: Adi Aloni <adialoni101@gmail.com>

--- pkg/virt-handler/node-capabilities/node_capabilities.go
+++ pkg/virt-handler/node-capabilities/node_capabilities.go
@@ -17,17 +17,37 @@ limitations under the License.
 package nodecapabilities
 
 import (
+	"fmt"
+
+	v1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
 	"libvirt.org/go/libvirtxml"
+
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 )
 
 const (
 	CapabilitiesVolumePath    = "/var/lib/kubevirt-node-labeller/"
 	HostCapabilitiesFilename  = "capabilities.xml"
+	DomCapabiliitesFilename   = "virsh_domcapabilities.xml"
+	SupportedFeaturesFilename = "supported_features.xml"
 
 	RequirePolicy = "require"
 	KVMPath       = "/dev/kvm"
 	VmxFeature    = "vmx"
 )
+
+type SupportedCPU struct {
+	Vendor           string
+	Model            string
+	RequiredFeatures []string
+	UsableModels     []string
+}
+
+type SupportedSEV struct {
+	Supported   bool
+	SupportedES bool
+}
 
 func HostCapabilities(hostCapabilities string) (*libvirtxml.Caps, error) {
 	var capabilities libvirtxml.Caps
@@ -35,4 +55,76 @@ func HostCapabilities(hostCapabilities string) (*libvirtxml.Caps, error) {
 		return nil, err
 	}
 	return &capabilities, nil
+}
+
+func DomCapabilities(hostDomCapabilities string) (*libvirtxml.DomainCaps, error) {
+	var capabilities libvirtxml.DomainCaps
+	if err := capabilities.Unmarshal(hostDomCapabilities); err != nil {
+		return nil, err
+	}
+	return &capabilities, nil
+}
+
+func SupportedHostCPUs(cpuModes []libvirtxml.DomainCapsCPUMode, arch string) (*SupportedCPU, error) {
+	var supportedCPU SupportedCPU
+	for _, mode := range cpuModes {
+		if mode.Name == v1.CPUModeHostModel {
+			if virtconfig.IsARM64(arch) {
+				log.Log.Warning("host-model cpu mode is not supported for ARM architecture")
+				continue
+			}
+
+			supportedCPU.Vendor = mode.Vendor
+			// On s390x the xml does not include a CPU Vendor, however there is only one company selling them anyway.
+			if virtconfig.IsS390X(arch) && mode.Vendor == "" {
+				supportedCPU.Vendor = "IBM"
+			}
+
+			if len(mode.Models) < 1 {
+				return nil, fmt.Errorf("host model mode is expected to contain a model")
+			}
+			if len(mode.Models) > 1 {
+				log.Log.Warning("host model mode is expected to contain only one model")
+			}
+
+			supportedCPU.Model = mode.Models[0].Name
+			for _, feature := range mode.Features {
+				if feature.Policy == "require" {
+					supportedCPU.RequiredFeatures = append(supportedCPU.RequiredFeatures, feature.Name)
+				}
+			}
+		}
+
+		for _, model := range mode.Models {
+			if model.Usable == "no" || model.Usable == "" {
+				continue
+			}
+			supportedCPU.UsableModels = append(supportedCPU.UsableModels, model.Name)
+		}
+	}
+	return &supportedCPU, nil
+}
+
+func SupportedHostSEV(sev *libvirtxml.DomainCapsFeatureSEV) *SupportedSEV {
+	supported := sev.Supported == "yes"
+	return &SupportedSEV{
+		Supported:   supported,
+		SupportedES: supported && sev.MaxESGuests > 0,
+	}
+}
+
+func SupportedFeatures(hostCPU string, arch string) ([]string, error) {
+	var cpu libvirtxml.DomainCPU
+	if err := cpu.Unmarshal(hostCPU); err != nil {
+		return nil, err
+	}
+	supportedFeatures := make([]string, 0)
+	for _, feature := range cpu.Features {
+		// On s390x, the policy is not set
+		if feature.Policy == "require" || (virtconfig.IsS390X(arch) && feature.Policy == "") {
+			supportedFeatures = append(supportedFeatures, feature.Name)
+		}
+	}
+	return supportedFeatures, nil
+
 }
