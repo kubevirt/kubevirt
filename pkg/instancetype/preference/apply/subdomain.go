8bbe04e09d04b237b05c031ee0f87d3e084e99c3
Lee Yarwood <lyarwood@redhat.com>
2024-10-10 15:53:09 +0100

    preference: Extract subdomain and termination logic
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- pkg/instancetype/preference/apply/subdomain.go
+++ pkg/instancetype/preference/apply/subdomain.go
@@ -19,30 +19,12 @@
 package apply
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	virtv1 "kubevirt.io/api/core/v1"
 	v1beta1 "kubevirt.io/api/instancetype/v1beta1"
 )
 
-type VMIApplier struct{}
-
-func (a *VMIApplier) Apply(
-	preferenceSpec *v1beta1.VirtualMachinePreferenceSpec,
-	vmiSpec *virtv1.VirtualMachineInstanceSpec,
-	vmiMetadata *metav1.ObjectMeta,
-) {
-	if preferenceSpec == nil {
-		return
+func applySubdomain(preferenceSpec *v1beta1.VirtualMachinePreferenceSpec, vmiSpec *virtv1.VirtualMachineInstanceSpec) {
+	if vmiSpec.Subdomain == "" && preferenceSpec.PreferredSubdomain != nil {
+		vmiSpec.Subdomain = *preferenceSpec.PreferredSubdomain
 	}
-
-	applyCPUPreferences(preferenceSpec, vmiSpec)
-	ApplyDevicePreferences(preferenceSpec, vmiSpec)
-	applyFeaturePreferences(preferenceSpec, vmiSpec)
-	applyFirmwarePreferences(preferenceSpec, vmiSpec)
-	applyMachinePreferences(preferenceSpec, vmiSpec)
-	applyClockPreferences(preferenceSpec, vmiSpec)
-	applySubdomain(preferenceSpec, vmiSpec)
-	applyTerminationGracePeriodSeconds(preferenceSpec, vmiSpec)
-	applyPreferenceAnnotations(preferenceSpec.Annotations, vmiMetadata)
 }
--- pkg/instancetype/preference/apply/termination.go
+++ pkg/instancetype/preference/apply/termination.go
@@ -19,30 +19,14 @@
 package apply
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	virtv1 "kubevirt.io/api/core/v1"
 	v1beta1 "kubevirt.io/api/instancetype/v1beta1"
+
+	"kubevirt.io/kubevirt/pkg/pointer"
 )
 
-type VMIApplier struct{}
-
-func (a *VMIApplier) Apply(
-	preferenceSpec *v1beta1.VirtualMachinePreferenceSpec,
-	vmiSpec *virtv1.VirtualMachineInstanceSpec,
-	vmiMetadata *metav1.ObjectMeta,
-) {
-	if preferenceSpec == nil {
-		return
+func applyTerminationGracePeriodSeconds(preferenceSpec *v1beta1.VirtualMachinePreferenceSpec, vmiSpec *virtv1.VirtualMachineInstanceSpec) {
+	if preferenceSpec.PreferredTerminationGracePeriodSeconds != nil && vmiSpec.TerminationGracePeriodSeconds == nil {
+		vmiSpec.TerminationGracePeriodSeconds = pointer.P(*preferenceSpec.PreferredTerminationGracePeriodSeconds)
 	}
-
-	applyCPUPreferences(preferenceSpec, vmiSpec)
-	ApplyDevicePreferences(preferenceSpec, vmiSpec)
-	applyFeaturePreferences(preferenceSpec, vmiSpec)
-	applyFirmwarePreferences(preferenceSpec, vmiSpec)
-	applyMachinePreferences(preferenceSpec, vmiSpec)
-	applyClockPreferences(preferenceSpec, vmiSpec)
-	applySubdomain(preferenceSpec, vmiSpec)
-	applyTerminationGracePeriodSeconds(preferenceSpec, vmiSpec)
-	applyPreferenceAnnotations(preferenceSpec.Annotations, vmiMetadata)
 }
