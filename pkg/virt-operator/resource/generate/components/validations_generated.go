55fc5895a30b9a165a6250a43dbb47e5b9ac5279
Varun Ramachandra Sekar <vsekar@nvidia.com>
2024-10-22 15:58:57 -0700

    Panic device support: instancetype v1beta1 API
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

--- pkg/virt-operator/resource/generate/components/validations_generated.go
+++ pkg/virt-operator/resource/generate/components/validations_generated.go
@@ -9243,6 +9243,19 @@ var CRDsValidation map[string]string = map[string]string{
               description: PreferredNetworkInterfaceMultiQueue optionally enables
                 the vhost multiqueue feature for virtio interfaces.
               type: boolean
+            preferredPanicDevices:
+              description: PreferredPanicDevices optionally defines any panic devices
+                to be used to signal guest failures.
+              items:
+                properties:
+                  model:
+                    description: |-
+                      Model specifies what type of panic device is provided.
+                      The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                      One of: isa, hyperv, pvpanic.
+                    type: string
+                type: object
+              type: array
             preferredRng:
               description: PreferredRng optionally defines the preferred rng device
                 to be used.
@@ -23727,6 +23740,19 @@ var CRDsValidation map[string]string = map[string]string{
               description: PreferredNetworkInterfaceMultiQueue optionally enables
                 the vhost multiqueue feature for virtio interfaces.
               type: boolean
+            preferredPanicDevices:
+              description: PreferredPanicDevices optionally defines any panic devices
+                to be used to signal guest failures.
+              items:
+                properties:
+                  model:
+                    description: |-
+                      Model specifies what type of panic device is provided.
+                      The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                      One of: isa, hyperv, pvpanic.
+                    type: string
+                type: object
+              type: array
             preferredRng:
               description: PreferredRng optionally defines the preferred rng device
                 to be used.
