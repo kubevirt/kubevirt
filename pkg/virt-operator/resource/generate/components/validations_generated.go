b7153137811f4333fd95a707afdb8851127f3b9d
Varun Ramachandra Sekar <vsekar@nvidia.com>
2024-10-22 13:50:20 -0700

    Panic device support: core v1 API
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

--- pkg/virt-operator/resource/generate/components/validations_generated.go
+++ pkg/virt-operator/resource/generate/components/validations_generated.go
@@ -6478,6 +6478,19 @@ var CRDsValidation map[string]string = map[string]string{
                             depends on additional factors of the VirtualMachineInstance,
                             like the number of guest CPUs.
                           type: boolean
+                        panicDevices:
+                          description: PanicDevices provides additional crash information
+                            when a QEMU guest crashes.
+                          items:
+                            properties:
+                              model:
+                                description: |-
+                                  Model specifies what type of panic device is provided.
+                                  The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                                  One of: isa, hyperv, pvpanic.
+                                type: string
+                            type: object
+                          type: array
                         rng:
                           description: Whether to have random number generator from
                             host
@@ -11685,6 +11698,19 @@ var CRDsValidation map[string]string = map[string]string{
                     factors of the VirtualMachineInstance, like the number of guest
                     CPUs.
                   type: boolean
+                panicDevices:
+                  description: PanicDevices provides additional crash information
+                    when a QEMU guest crashes.
+                  items:
+                    properties:
+                      model:
+                        description: |-
+                          Model specifies what type of panic device is provided.
+                          The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                          One of: isa, hyperv, pvpanic.
+                        type: string
+                    type: object
+                  type: array
                 rng:
                   description: Whether to have random number generator from host
                   type: object
@@ -14859,6 +14885,19 @@ var CRDsValidation map[string]string = map[string]string{
                     factors of the VirtualMachineInstance, like the number of guest
                     CPUs.
                   type: boolean
+                panicDevices:
+                  description: PanicDevices provides additional crash information
+                    when a QEMU guest crashes.
+                  items:
+                    properties:
+                      model:
+                        description: |-
+                          Model specifies what type of panic device is provided.
+                          The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                          One of: isa, hyperv, pvpanic.
+                        type: string
+                    type: object
+                  type: array
                 rng:
                   description: Whether to have random number generator from host
                   type: object
@@ -17265,6 +17304,19 @@ var CRDsValidation map[string]string = map[string]string{
                             depends on additional factors of the VirtualMachineInstance,
                             like the number of guest CPUs.
                           type: boolean
+                        panicDevices:
+                          description: PanicDevices provides additional crash information
+                            when a QEMU guest crashes.
+                          items:
+                            properties:
+                              model:
+                                description: |-
+                                  Model specifies what type of panic device is provided.
+                                  The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                                  One of: isa, hyperv, pvpanic.
+                                type: string
+                            type: object
+                          type: array
                         rng:
                           description: Whether to have random number generator from
                             host
@@ -21776,6 +21828,19 @@ var CRDsValidation map[string]string = map[string]string{
                                     factors of the VirtualMachineInstance, like the
                                     number of guest CPUs.
                                   type: boolean
+                                panicDevices:
+                                  description: PanicDevices provides additional crash
+                                    information when a QEMU guest crashes.
+                                  items:
+                                    properties:
+                                      model:
+                                        description: |-
+                                          Model specifies what type of panic device is provided.
+                                          The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                                          One of: isa, hyperv, pvpanic.
+                                        type: string
+                                    type: object
+                                  type: array
                                 rng:
                                   description: Whether to have random number generator
                                     from host
@@ -26976,6 +27041,19 @@ var CRDsValidation map[string]string = map[string]string{
                                         factors of the VirtualMachineInstance, like
                                         the number of guest CPUs.
                                       type: boolean
+                                    panicDevices:
+                                      description: PanicDevices provides additional
+                                        crash information when a QEMU guest crashes.
+                                      items:
+                                        properties:
+                                          model:
+                                            description: |-
+                                              Model specifies what type of panic device is provided.
+                                              The panic model used when this attribute is missing depends on the hypervisor and guest arch.
+                                              One of: isa, hyperv, pvpanic.
+                                            type: string
+                                        type: object
+                                      type: array
                                     rng:
                                       description: Whether to have random number generator
                                         from host
