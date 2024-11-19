87f55ec5a0eb42563a4f5d6799681bfbe23a42d9
David Vossel <davidvossel@gmail.com>
2024-11-19 16:27:09 -0500

    review feedback enhancements to reset functionality
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/virtctl/reset/reset_test.go
+++ pkg/virtctl/reset/reset_test.go
@@ -6,22 +6,20 @@ import (
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"kubevirt.io/kubevirt/tests/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"kubevirt.io/client-go/kubecli"
 
-	"kubevirt.io/kubevirt/pkg/libvmi"
 	"kubevirt.io/kubevirt/pkg/virtctl/reset"
+	"kubevirt.io/kubevirt/tests/clientcmd"
 )
 
 var _ = Describe("Resetting", func() {
 	var vmiInterface *kubecli.MockVirtualMachineInstanceInterface
-	var ctrl *gomock.Controller
 
 	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
+		ctrl := gomock.NewController(GinkgoT())
 		kubecli.GetKubevirtClientFromClientConfig = kubecli.GetMockKubevirtClientFromClientConfig
 		kubecli.MockKubevirtClientInstance = kubecli.NewMockKubevirtClient(ctrl)
 		vmiInterface = kubecli.NewMockVirtualMachineInstanceInterface(ctrl)
@@ -30,18 +28,17 @@ var _ = Describe("Resetting", func() {
 	Context("With missing input parameters", func() {
 		It("should fail", func() {
 			cmd := clientcmd.NewRepeatableVirtctlCommand(reset.COMMAND_RESET)
-			err := cmd()
-			Expect(err).To(HaveOccurred())
+			Expect(cmd()).To(MatchError(ContainSubstring("received 0")))
 		})
 	})
 
 	It("should reset VMI", func() {
-		vmi := libvmi.New()
+		vmiName := "resetable-vmi"
 
 		kubecli.MockKubevirtClientInstance.EXPECT().VirtualMachineInstance(metav1.NamespaceDefault).Return(vmiInterface).Times(1)
-		vmiInterface.EXPECT().Reset(context.Background(), vmi.Name).Return(nil).Times(1)
+		vmiInterface.EXPECT().Reset(context.Background(), vmiName).Return(nil).Times(1)
 
-		cmd := clientcmd.NewVirtctlCommand(reset.COMMAND_RESET, vmi.Name)
-		Expect(cmd.Execute()).To(Succeed())
+		cmd := clientcmd.NewRepeatableVirtctlCommand(reset.COMMAND_RESET, vmiName)
+		Expect(cmd()).To(Succeed())
 	})
 })
