0462f455b20e97364972846d2531ac268eede9df
Felix Matouschek <fmatouschek@redhat.com>
2024-11-28 15:25:34 +0100

    cleanup(tests,virtctl): Apply cleanups to guestfs tests
    Apply the following cleanups to the virtctl guestfs functests:
    
    - Replace fakeAttacher with lambda capturing a done chan
    - Consolidate BeforeEach/AfterEach for all test cases
    - Use gomega matchers instead of custom code
    - Rename some variables for readability
    - Eliminate unneeded consts/vars
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/virtctl/guestfs.go
+++ tests/virtctl/guestfs.go
@@ -21,117 +21,98 @@ package virtctl
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
 
 	"kubevirt.io/kubevirt/pkg/virtctl/guestfs"
 	"kubevirt.io/kubevirt/tests/clientcmd"
 	"kubevirt.io/kubevirt/tests/decorators"
 	"kubevirt.io/kubevirt/tests/exec"
 	"kubevirt.io/kubevirt/tests/framework/kubevirt"
+	"kubevirt.io/kubevirt/tests/framework/matcher"
 	"kubevirt.io/kubevirt/tests/libstorage"
 	"kubevirt.io/kubevirt/tests/testsuite"
 )
 
-const testGroup = "2000"
-
-var _ = Describe("[sig-storage][virtctl][rfe_id:6364]Guestfs", decorators.SigStorage, Label("guestfs"), func() {
+var _ = Describe("[sig-storage][virtctl]Guestfs", decorators.SigStorage, func() {
 	var (
 		pvcClaim string
-		setGroup bool
+		done     chan struct{}
 	)
 
-	Context("Run libguestfs on PVCs", Label("guestfs"), func() {
-		var (
-			f  *fakeAttacher
-			ns string
-		)
+	// fakeCreateAttacher simulates the attacher to the pod console. It has to block until the test terminates.
+	fakeCreateAttacher := func(_ *guestfs.K8sClient, _ *corev1.Pod, _ string) error {
+		<-done
+		return nil
+	}
 
-		BeforeEach(func() {
-			// TODO: Always setGroup to true until we have the ability to control how virtctl guestfs is run
-			setGroup = true
-			f = createFakeAttacher()
-			ns = testsuite.GetTestNamespace(nil)
+	BeforeEach(func() {
+		guestfs.CreateAttacherFunc = fakeCreateAttacher
+		pvcClaim = "pvc-" + rand.String(5)
+		done = make(chan struct{})
+	})
+
+	AfterEach(func() {
+		guestfs.CreateAttacherFunc = guestfs.CreateAttacher
+		close(done)
+	})
+
+	Context("[rfe_id:6364]Run libguestfs on PVCs without root", func() {
+		// TODO: Always setGroup to true until we have the ability to control how virtctl guestfs is run
+		const setGroup = true
+
+		Context("on a FS PVC", func() {
+			BeforeEach(func() {
+				libstorage.CreateFSPVC(pvcClaim, testsuite.GetTestNamespace(nil), "500Mi", nil)
+			})
+
+			It("Should successfully run libguestfs-test-tool", func() {
+				runGuestfsOnPVC(done, pvcClaim, testsuite.GetTestNamespace(nil), setGroup)
+				stdout, _, err := execCommandLibguestfsPod(
+					getGuestfsPodName(pvcClaim), testsuite.GetTestNamespace(nil), []string{"libguestfs-test-tool"},
+				)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(stdout).To(ContainSubstring("===== TEST FINISHED OK ====="))
+			})
+
+			DescribeTable("[posneg:positive]Should successfully run guestfs command on a filesystem-based PVC", func(extraArgs ...string) {
+				runGuestfsOnPVC(done, pvcClaim, testsuite.GetTestNamespace(nil), setGroup, extraArgs...)
+				verifyCanRunOnFSPVC(getGuestfsPodName(pvcClaim), testsuite.GetTestNamespace(nil))
+			},
+				Entry("[test_id:6480]without extra arguments"),
+				Entry("setting the uid", "--uid", "1002"),
+			)
+
+			It("[posneg:negative][test_id:6480]Should fail to run the guestfs command on a PVC in use", func() {
+				runGuestfsOnPVC(done, pvcClaim, testsuite.GetTestNamespace(nil), setGroup)
+				cmd := guestfsCmd(pvcClaim, testsuite.GetTestNamespace(nil), setGroup)
+				Expect(cmd()).To(MatchError(fmt.Sprintf("PVC %s is used by another pod", pvcClaim)))
+			})
 		})
 
-		AfterEach(func() {
-			f.closeChannel()
-		})
-
-		// libguestfs-test-tool verifies the setup to run libguestfs-tools
-		It("Should successfully run libguestfs-test-tool", Label("guestfs", "FileSystem"), func() {
-			pvcClaim = "pvc-verify"
-			libstorage.CreateFSPVC(pvcClaim, ns, "500Mi", nil)
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup)
-			output, _, err := execCommandLibguestfsPod(getGuestfsPodName(pvcClaim), ns, []string{"libguestfs-test-tool"})
+		It("[posneg:positive][test_id:6479]Should successfully run guestfs command on a block-based PVC", func() {
+			libstorage.CreateBlockPVC(pvcClaim, testsuite.GetTestNamespace(nil), "500Mi")
+			runGuestfsOnPVC(done, pvcClaim, testsuite.GetTestNamespace(nil), setGroup)
+			stdout, stderr, err := execCommandLibguestfsPod(
+				getGuestfsPodName(pvcClaim), testsuite.GetTestNamespace(nil), []string{"guestfish", "-a", "/dev/vda", "run"},
+			)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(output).To(ContainSubstring("===== TEST FINISHED OK ====="))
-		})
-
-		It("[posneg:positive][test_id:6480]Should successfully run guestfs command on a filesystem-based PVC", Label("guestfs", "FileSystem"), func() {
-			pvcClaim = "pvc-fs"
-			libstorage.CreateFSPVC(pvcClaim, ns, "500Mi", nil)
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup)
-			verifyCanRunOnFSPVC(getGuestfsPodName(pvcClaim), ns)
-		})
-
-		It("[posneg:negative][test_id:6480]Should fail to run the guestfs command on a PVC in use", Label("guestfs", "FileSystem"), func() {
-			pvcClaim = "pvc-fail-to-run-twice"
-			libstorage.CreateFSPVC(pvcClaim, ns, "500Mi", nil)
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup)
-			options := []string{"guestfs",
-				pvcClaim,
-				"--namespace", ns}
-			if setGroup {
-				options = append(options, "--fsGroup", testGroup)
-			}
-			guestfsCmd := clientcmd.NewRepeatableVirtctlCommand(options...)
-			Expect(guestfsCmd()).To(HaveOccurred())
-		})
-
-		It("[posneg:positive][test_id:6479]Should successfully run guestfs command on a block-based PVC", Label("guestfs", "Block"), func() {
-			pvcClaim = "pvc-block"
-			libstorage.CreateBlockPVC(pvcClaim, ns, "500Mi")
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup)
-			stdout, stderr, err := execCommandLibguestfsPod(getGuestfsPodName(pvcClaim), ns, []string{"guestfish", "-a", "/dev/vda", "run"})
-			Expect(stderr).To(Equal(""))
-			Expect(stdout).To(Equal(""))
-			Expect(err).ToNot(HaveOccurred())
-
-		})
-		It("Should successfully run guestfs command on a filesystem-based PVC setting the uid", Label("guestfs", "FileSystem"), func() {
-			pvcClaim = "pvc-fs-with-different-uid"
-			libstorage.CreateFSPVC(pvcClaim, ns, "500Mi", nil)
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup, "--uid", "1002")
-			verifyCanRunOnFSPVC(getGuestfsPodName(pvcClaim), ns)
+			Expect(stderr).To(BeEmpty())
+			Expect(stdout).To(BeEmpty())
 		})
 	})
 
-	Context("Run libguestfs on PVCs with root", func() {
-		var f *fakeAttacher
-
-		BeforeEach(func() {
-			setGroup = false
-			f = createFakeAttacher()
-		})
-
-		AfterEach(func() {
-			f.closeChannel()
-		})
-
-		It("Should successfully run guestfs command on a filesystem-based PVC with root", Label("guestfs", "Filesystem"), func() {
-			f := createFakeAttacher()
-			defer f.closeChannel()
-			pvcClaim = "pvc-fs-with-root"
-			ns := testsuite.NamespacePrivileged
-			libstorage.CreateFSPVC(pvcClaim, ns, "500Mi", nil)
-			runGuestfsOnPVC(f, pvcClaim, ns, setGroup, "--root")
-			verifyCanRunOnFSPVC(getGuestfsPodName(pvcClaim), ns)
-		})
+	It("[rfe_id:6364]Should successfully run guestfs command on a filesystem-based PVC with root", func() {
+		libstorage.CreateFSPVC(pvcClaim, testsuite.NamespacePrivileged, "500Mi", nil)
+		runGuestfsOnPVC(done, pvcClaim, testsuite.NamespacePrivileged, false, "--root")
+		verifyCanRunOnFSPVC(getGuestfsPodName(pvcClaim), testsuite.NamespacePrivileged)
 	})
 })
 
@@ -139,86 +120,61 @@ func getGuestfsPodName(pvc string) string {
 	return "libguestfs-tools-" + pvc
 }
 
-func execCommandLibguestfsPod(podName, namespace string, c []string) (string, string, error) {
+func execCommandLibguestfsPod(podName, namespace string, args []string) (string, string, error) {
 	pod, err := kubevirt.Client().CoreV1().Pods(namespace).Get(context.Background(), podName, metav1.GetOptions{})
 	Expect(err).ToNot(HaveOccurred())
-	return exec.ExecuteCommandOnPodWithResults(pod, "libguestfs", c)
+	return exec.ExecuteCommandOnPodWithResults(pod, "libguestfs", args)
 }
 
-func runGuestfsOnPVC(f *fakeAttacher, pvcClaim, namespace string, setGroup bool, options ...string) {
-	podName := getGuestfsPodName(pvcClaim)
-	o := append([]string{"guestfs", pvcClaim, "--namespace", namespace}, options...)
+func guestfsCmd(pvcClaim, namespace string, setGroup bool, extraArgs ...string) func() error {
+	args := append([]string{
+		"guestfs", pvcClaim,
+		"--namespace", namespace,
+	}, extraArgs...)
 	if setGroup {
-		o = append(o, "--fsGroup", testGroup)
+		const testGroup = "2000"
+		args = append(args, "--fsGroup", testGroup)
 	}
-	guestfsCmd := clientcmd.NewRepeatableVirtctlCommand(o...)
-	go guestfsWithSync(f, guestfsCmd)
-	// Waiting until the libguestfs pod is running
-	Eventually(func() bool {
-		pod, _ := kubevirt.Client().CoreV1().Pods(namespace).Get(context.Background(), podName, metav1.GetOptions{})
-		ready := false
-		for _, status := range pod.Status.ContainerStatuses {
-			if status.State.Running != nil {
-				return true
-			}
-		}
-		return ready
-
-	}, 90*time.Second, 2*time.Second).Should(BeTrue())
-	// Verify that the appliance has been extracted before running any tests by checking the done file
-	Eventually(func() bool {
-		_, _, err := execCommandLibguestfsPod(podName, namespace, []string{"ls", "/usr/local/lib/guestfs/appliance/done"})
-		return err == nil
-	}, 30*time.Second, 2*time.Second).Should(BeTrue())
+	return clientcmd.NewRepeatableVirtctlCommand(args...)
 }
 
-func guestfsWithSync(f *fakeAttacher, guestfsCmd func() error) {
+func runGuestfsOnPVC(done chan struct{}, pvcClaim, namespace string, setGroup bool, extraArgs ...string) {
+	go guestfsWithSync(done, guestfsCmd(pvcClaim, namespace, setGroup, extraArgs...))
+
+	podName := getGuestfsPodName(pvcClaim)
+	// Waiting until the libguestfs pod is running
+	Eventually(func(g Gomega) {
+		pod, err := kubevirt.Client().CoreV1().Pods(namespace).Get(context.Background(), podName, metav1.GetOptions{})
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(pod).To(matcher.HaveConditionTrue(corev1.ContainersReady))
+	}, 90*time.Second, 2*time.Second).Should(Succeed())
+	// Verify that the appliance has been extracted before running any tests by checking the done file
+	Eventually(func(g Gomega) {
+		_, _, err := execCommandLibguestfsPod(podName, namespace, []string{"ls", "/usr/local/lib/guestfs/appliance/done"})
+		g.Expect(err).ToNot(HaveOccurred())
+	}, 30*time.Second, 2*time.Second).Should(Succeed())
+}
+
+func guestfsWithSync(done chan struct{}, cmd func() error) {
 	defer GinkgoRecover()
 	errChan := make(chan error)
 	go func() {
-		errChan <- guestfsCmd()
+		errChan <- cmd()
 	}()
 	select {
-	case <-f.doneGuestfs:
+	case <-done:
 	case err := <-errChan:
 		Expect(err).ToNot(HaveOccurred())
 	}
-	// Unblock the fake attacher
-	f.doneAttacher <- true
 }
 
 func verifyCanRunOnFSPVC(podName, namespace string) {
 	stdout, stderr, err := execCommandLibguestfsPod(podName, namespace, []string{"qemu-img", "create", "/disk/disk.img", "500M"})
-	Expect(stderr).To(Equal(""))
+	Expect(stderr).To(BeEmpty())
 	Expect(stdout).To(ContainSubstring("Formatting"))
 	Expect(err).ToNot(HaveOccurred())
 	stdout, stderr, err = execCommandLibguestfsPod(podName, namespace, []string{"guestfish", "-a", "/disk/disk.img", "run"})
 	Expect(stderr).To(BeEmpty())
 	Expect(stdout).To(BeEmpty())
 	Expect(err).ToNot(HaveOccurred())
-}
-
-type fakeAttacher struct {
-	// Channel to unblock the fake attacher for the console
-	doneAttacher chan bool
-	// Channel to unblock the goroutine for the guestfs command
-	doneGuestfs chan bool
-}
-
-// fakeCreateAttacher simulates the attacher to the pod console. It has to block until the test terminates.
-func (f *fakeAttacher) fakeCreateAttacher(client *guestfs.K8sClient, p *corev1.Pod, command string) error {
-	<-f.doneAttacher
-	return nil
-}
-
-func (f *fakeAttacher) closeChannel() {
-	f.doneGuestfs <- true
-}
-
-func createFakeAttacher() *fakeAttacher {
-	f := &fakeAttacher{}
-	f.doneAttacher = make(chan bool, 1)
-	f.doneGuestfs = make(chan bool, 1)
-	guestfs.CreateAttacherFunc = f.fakeCreateAttacher
-	return f
 }
