389e86407ba156f6005a14d994202057e88db76b
Felix Matouschek <fmatouschek@redhat.com>
2024-11-28 12:10:19 +0100

    cleanup(tests,virtctl): Move guestsfs functests
    Move the virtctl guestfs functests to tests/virtctl.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/virtctl/guestfs.go
+++ tests/virtctl/guestfs.go
@@ -1,10 +1,27 @@
-package storage
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors
+ *
+ */
+
+package virtctl
 
 import (
 	"context"
 	"time"
-
-	"kubevirt.io/kubevirt/tests/framework/kubevirt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -17,7 +34,9 @@ import (
 
 	"kubevirt.io/kubevirt/pkg/virtctl/guestfs"
 	"kubevirt.io/kubevirt/tests/clientcmd"
+	"kubevirt.io/kubevirt/tests/decorators"
 	"kubevirt.io/kubevirt/tests/exec"
+	"kubevirt.io/kubevirt/tests/framework/kubevirt"
 	"kubevirt.io/kubevirt/tests/libstorage"
 	"kubevirt.io/kubevirt/tests/testsuite"
 )
@@ -39,7 +58,7 @@ func (f *fakeAttacher) closeChannel() {
 	f.doneGuestfs <- true
 }
 
-var _ = SIGDescribe("[rfe_id:6364]Guestfs", Label("guestfs"), func() {
+var _ = Describe("[sig-storage][virtctl][rfe_id:6364]Guestfs", decorators.SigStorage, Label("guestfs"), func() {
 	var (
 		virtClient kubecli.KubevirtClient
 		pvcClaim   string
