84c6e734b4ce1c1f4bebcc96388885f73ffcc53c
Felix Matouschek <fmatouschek@redhat.com>
2024-12-02 15:05:03 +0100

    chore(tests,virtctl): Add virtctl label and VirtctlDescribe
    Add a label to allow filtering for virtctl tests and add Describes that
    allow to use this label easily. Then make use of the new VirtctlDescribe.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/virtctl/guestfs.go
+++ tests/virtctl/guestfs.go
@@ -40,7 +40,7 @@ import (
 	"kubevirt.io/kubevirt/tests/testsuite"
 )
 
-var _ = Describe("[sig-storage][virtctl]Guestfs", decorators.SigStorage, func() {
+var _ = VirtctlDescribe("[sig-storage]Guestfs", decorators.SigStorage, func() {
 	var (
 		pvcClaim string
 		done     chan struct{}
