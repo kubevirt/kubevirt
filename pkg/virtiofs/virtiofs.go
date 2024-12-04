a9115ee8e8cbf2c60d8a049a208477c74a267207
Alice Frosi <afrosi@redhat.com>
2024-12-04 13:56:38 +0100

    virtiofs: add functions for the placeholder
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/virtiofs/virtiofs.go
+++ pkg/virtiofs/virtiofs.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 	"path/filepath"
 
+	v1 "kubevirt.io/api/core/v1"
+
+	"kubevirt.io/kubevirt/pkg/config"
+	storagetypes "kubevirt.io/kubevirt/pkg/storage/types"
 	"kubevirt.io/kubevirt/pkg/util"
+)
+
+const (
+	PlaceholderSocketDir = "/var/run/sockets"
+	ExtraVolName         = "virtiofs-sockets"
+)
+
+const (
+	dispatcher = "virtiofs-dispatcher"
 )
 
 // This is empty dir
@@ -14,4 +27,48 @@ var VirtioFSContainersMountBaseDir = filepath.Join(util.VirtShareDir, VirtioFSCo
 func VirtioFSSocketPath(volumeName string) string {
 	socketName := fmt.Sprintf("%s.sock", volumeName)
 	return filepath.Join(VirtioFSContainersMountBaseDir, socketName)
+}
+
+func VirtiofsPlaceholderSocketName(volumeName string) string {
+	return fmt.Sprintf("%s.sock", volumeName)
+}
+
+func VirtiofsPlaceholderSocket(volumeName string) string {
+	return filepath.Join(PlaceholderSocketDir, VirtiofsPlaceholderSocketName(volumeName))
+}
+
+func GetFilesystemPersistentVolumes(vmi *v1.VirtualMachineInstance) []v1.Volume {
+	var vols []v1.Volume
+	fss := storagetypes.GetFilesystemsFromVolumes(vmi)
+	for _, volume := range vmi.Spec.Volumes {
+		if _, ok := fss[volume.Name]; !ok {
+			continue
+		}
+		if volume.VolumeSource.PersistentVolumeClaim != nil ||
+			volume.VolumeSource.DataVolume != nil {
+			vols = append(vols, volume)
+		}
+	}
+
+	return vols
+}
+
+func HasFilesystemPersistentVolumes(vmi *v1.VirtualMachineInstance) bool {
+	return len(GetFilesystemPersistentVolumes(vmi)) > 0
+}
+
+func VirtioFSMountPoint(volume *v1.Volume) string {
+	volumeMountPoint := fmt.Sprintf("/%s", volume.Name)
+
+	if volume.ConfigMap != nil {
+		volumeMountPoint = config.GetConfigMapSourcePath(volume.Name)
+	} else if volume.Secret != nil {
+		volumeMountPoint = config.GetSecretSourcePath(volume.Name)
+	} else if volume.ServiceAccount != nil {
+		volumeMountPoint = config.ServiceAccountSourceDir
+	} else if volume.DownwardAPI != nil {
+		volumeMountPoint = config.GetDownwardAPISourcePath(volume.Name)
+	}
+
+	return volumeMountPoint
 }
