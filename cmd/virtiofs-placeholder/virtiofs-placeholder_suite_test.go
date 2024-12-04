12a7f778a92bad74dfc9fc855aace6ad32bf564b
Alice Frosi <afrosi@redhat.com>
2024-12-04 17:26:21 +0100

    virtiofsd-placeholder: add unit tests
    Verify that the placeholder functions correctly:
      - it needs to take multiple connections in case virt-handler tries to
      get multiple times the pid from the socket
      - it needs to terminate if it gets a SICHLD signal
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>
    Signed-off-by: German Maglione <gmaglione@redhat.com>

---
+++ cmd/virtiofs-placeholder/virtiofs-placeholder_suite_test.go
@@ -0,0 +1,18 @@
+package virtiofs_placeholder_test
+
+import (
+	"flag"
+	"testing"
+
+	"kubevirt.io/client-go/testutils"
+)
+
+var placeholderBinary string
+
+func init() {
+	flag.StringVar(&placeholderBinary, "placeholder-binary", "_out/cmd/virtiofs-placeholder", "path to virtiofs placeholder binary")
+}
+
+func TestVirtiofsPlaceholder(t *testing.T) {
+	testutils.KubeVirtTestSuiteSetup(t)
+}
