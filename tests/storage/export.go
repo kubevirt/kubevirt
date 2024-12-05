ac40d9044e8a830b9ceeec646eb9d77b034f036e
Alvaro Romero <alromero@redhat.com>
2024-12-05 09:20:54 +0100

    Move export tests out of quarantine
    Signed-off-by: Alvaro Romero <alromero@redhat.com>

--- tests/storage/export.go
+++ tests/storage/export.go
@@ -1712,7 +1712,7 @@ var _ = SIGDescribe("Export", func() {
 		waitForDisksComplete(resVM)
 	}
 
-	It("[QUARANTINE] should generate updated DataVolumeTemplates on http endpoint when exporting", decorators.Quarantine, func() {
+	It("should generate updated DataVolumeTemplates on http endpoint when exporting", func() {
 		sc, exists := libstorage.GetRWOFileSystemStorageClass()
 		if !exists {
 			Skip("Skip test when Filesystem storage is not present")
@@ -1755,7 +1755,7 @@ var _ = SIGDescribe("Export", func() {
 		checkWithJsonOutput(pod, export, vm)
 	})
 
-	It("[QUARANTINE] should generate updated DataVolumeTemplates on http endpoint when exporting snapshot", decorators.Quarantine, func() {
+	It("should generate updated DataVolumeTemplates on http endpoint when exporting snapshot", func() {
 		virtClient, err := kubecli.GetKubevirtClient()
 		Expect(err).ToNot(HaveOccurred())
 		sc, err := libstorage.GetSnapshotStorageClass(virtClient)
