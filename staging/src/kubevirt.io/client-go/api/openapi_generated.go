42f28a1d5bca06bbd783c91cedca29dfa39bf81f
bmordeha <bmordeha@redhat.com>
2024-12-08 13:17:34 +0200

    Correct VMRolloutStrategy documentation to clarify it applies only to memory and CPU hotplug changes
    Signed-off-by: bmordeha <bmordeha@redhat.com>

--- staging/src/kubevirt.io/client-go/api/openapi_generated.go
+++ staging/src/kubevirt.io/client-go/api/openapi_generated.go
@@ -21003,7 +21003,7 @@ func schema_kubevirtio_api_core_v1_KubeVirtConfiguration(ref common.ReferenceCal
 					},
 					"vmRolloutStrategy": {
 						SchemaProps: spec.SchemaProps{
-							Description: "VMRolloutStrategy defines how changes to a VM object propagate to its VMI",
+							Description: "VMRolloutStrategy defines how memory and CPU hotplug changes to a VM object are propagated to its VMI.",
 							Type:        []string{"string"},
 							Format:      "",
 						},
