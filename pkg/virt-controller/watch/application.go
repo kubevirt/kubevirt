d115d4ff921ef385283fe6adce4723cb1c58c735
Luboslav Pivarc <lpivarc@redhat.com>
2024-12-13 13:00:04 +0100

    Make pdb controller to only remove PDBs
    Now that the Pod eviction webhook is required and the webhook is
    always protecting the VM, the PodDisruptionBudget is duplicate.
    
    Do not expand PDBs on Migration
    
    Now when we dropped the creation of PDBs we
    no longer need to manage them during migration.
    
    Remove PDB e2e tests, these tests are no longer valid.
    
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/virt-controller/watch/application.go
+++ pkg/virt-controller/watch/application.go
@@ -689,7 +689,6 @@ func (vca *VirtControllerApp) initCommon() {
 		vca.persistentVolumeClaimInformer,
 		vca.storageClassInformer,
 		vca.storageProfileInformer,
-		vca.pdbInformer,
 		vca.migrationPolicyInformer,
 		vca.resourceQuotaInformer,
 		vca.vmiRecorder,
@@ -773,7 +772,6 @@ func (vca *VirtControllerApp) initDisruptionBudgetController() {
 		vca.migrationInformer,
 		recorder,
 		vca.clientSet,
-		vca.clusterConfig,
 	)
 	if err != nil {
 		panic(err)
