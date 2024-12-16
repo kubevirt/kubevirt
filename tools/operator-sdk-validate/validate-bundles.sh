b3b88399cbd3f35fd7dd6bac08c2c699c7bb5822
Oren Cohen <ocohen@redhat.com>
2024-12-16 14:28:02 +0200

    validate-bundles: remove sudo usage (#3210)
    the make sanity github action has started to fail for using the sudo directive in this context.
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- tools/operator-sdk-validate/validate-bundles.sh
+++ tools/operator-sdk-validate/validate-bundles.sh
@@ -5,9 +5,9 @@ OPERATOR_SDK_VERSION=1.5.0
 LOWEST_VERSION_TO_VALIDATE=1.3.0
 ALL_VERSIONS=( $(ls -d /manifests/*/ | sort -V | cut -d '/' -f 3) )
 
-curl -L -o /usr/local/bin/operator-sdk \
+curl -L -o /tmp/operator-sdk \
   "https://github.com/operator-framework/operator-sdk/releases/download/v${OPERATOR_SDK_VERSION}/operator-sdk_linux_amd64"
-sudo chmod +x /usr/local/bin/operator-sdk
+chmod +x /tmp/operator-sdk
 
 function ver
 {
@@ -19,7 +19,7 @@ do
    if [ "$(ver ${version})" -ge "$(ver ${LOWEST_VERSION_TO_VALIDATE})" ]
    then
      set -x
-     operator-sdk bundle validate /manifests/${version}
+     ./tmp/operator-sdk bundle validate /manifests/${version}
      set +x
    fi
 done
