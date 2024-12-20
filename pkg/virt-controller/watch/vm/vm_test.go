4de676266dc80bbe6160daec1e4338251b5fd179
Lee Yarwood <lyarwood@redhat.com>
2024-12-20 11:44:03 +0000

    DNM / WIP instancetype: Align ControllerRevision error messages
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- pkg/virt-controller/watch/vm/vm_test.go
+++ pkg/virt-controller/watch/vm/vm_test.go
@@ -4797,7 +4797,7 @@ var _ = Describe("VirtualMachine", func() {
 						"Type":   Equal(v1.VirtualMachineFailure),
 						"Reason": Equal("FailedCreate"),
 						"Message": And(
-							ContainSubstring("Error encountered while storing Instancetype ControllerRevisions"),
+							ContainSubstring("error encountered while storing instancetype.kubevirt.io controllerRevisions"),
 							ContainSubstring(instancetype.VMFieldsConflictsErrorFmt, instancetype.Conflicts{
 								field.NewPath("spec.template.spec.domain.cpu.sockets"),
 								field.NewPath("spec.template.spec.domain.cpu.cores"),
