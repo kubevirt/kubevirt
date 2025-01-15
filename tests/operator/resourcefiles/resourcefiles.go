d888d3da502bc6e93a720d4f1f2dee8fac52277d
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2025-01-15 15:26:56 +0200

    Add struct type for each template
    Add struct type for each template, to populate the template's place
    holders.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- tests/operator/resourcefiles/resourcefiles.go
+++ tests/operator/resourcefiles/resourcefiles.go
@@ -13,3 +13,38 @@ var templates *template.Template
 func init() {
 	templates = template.Must(template.ParseFS(templatesFS, "templates/*"))
 }
+
+type TemplateInfo interface {
+	templateName() string
+}
+
+type VMInfo struct {
+	Version   string
+	Index     int
+	ImageName string
+}
+
+func (VMInfo) templateName() string {
+	return "vm-template.yaml"
+}
+
+type SnapshotInfo struct {
+	Version string
+	Name    string
+	VMName  string
+}
+
+func (SnapshotInfo) templateName() string {
+	return "snapshot-template.yaml"
+}
+
+type RestoreInfo struct {
+	Version      string
+	Name         string
+	VMName       string
+	SnapshotName string
+}
+
+func (RestoreInfo) templateName() string {
+	return "restore-template.yaml"
+}
