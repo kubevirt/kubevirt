c6d492210f2fc3183c74d115587762aa61593349
Antonio Cardace <acardace@redhat.com>
2025-01-16 16:40:34 +0100

    virt-handler: always set the migration EndTimestamp
    Unless an abortion is in progress or failed, as that
    means the migration is still ongoing.
    
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- pkg/virt-launcher/virtwrap/live-migration-source.go
+++ pkg/virt-launcher/virtwrap/live-migration-source.go
@@ -30,6 +30,7 @@ import (
 	"libvirt.org/go/libvirtxml"
 
 	hostdisk "kubevirt.io/kubevirt/pkg/host-disk"
+	"kubevirt.io/kubevirt/pkg/pointer"
 	"kubevirt.io/kubevirt/pkg/util/migrations"
 
 	cmdv1 "kubevirt.io/kubevirt/pkg/handler-launcher-com/cmd/v1"
@@ -389,18 +390,20 @@ func (l *LibvirtDomainManager) setMigrationResultHelper(failed bool, completed b
 	}
 
 	l.metadataCache.Migration.WithSafeBlock(func(migrationMetadata *api.MigrationMetadata, _ bool) {
-		if abortStatus != "" {
-			migrationMetadata.AbortStatus = string(abortStatus)
-		}
-
 		if failed {
 			migrationMetadata.Failed = true
 			migrationMetadata.FailureReason = reason
 		}
 		if completed {
 			migrationMetadata.Completed = true
-			now := metav1.Now()
-			migrationMetadata.EndTimestamp = &now
+		}
+
+		migrationMetadata.AbortStatus = string(abortStatus)
+
+		if abortStatus == "" || abortStatus == v1.MigrationAbortSucceeded {
+			// only mark the migration as complete if there was no abortion or
+			// the abortion succeeded
+			migrationMetadata.EndTimestamp = pointer.P(metav1.Now())
 		}
 	})
 
