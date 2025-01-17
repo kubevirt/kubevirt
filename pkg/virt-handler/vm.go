e8b38bcf50cb1217a966d84b65ffa32591d5c6c7
Antonio Cardace <acardace@redhat.com>
2025-01-17 16:02:13 +0100

    virt-handler: always cleanup a deleted VMI
    regardless of the VMI phase. This fixes a unit test
    that started failing after the changes in this PR.
    
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -1943,9 +1943,7 @@ func (c *VirtualMachineController) defaultExecute(key string,
 			log.Log.Object(vmi).V(3).Info("Deleting domain for VirtualMachineInstance with deletion timestamp.")
 			shouldDelete = true
 		default:
-			if vmi.IsFinal() {
-				shouldCleanUp = true
-			}
+			shouldCleanUp = true
 		}
 	}
 
