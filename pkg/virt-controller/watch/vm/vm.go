ad3e73344b24a389b91e44967c4d26ca3c5accc1
Alice Frosi <afrosi@redhat.com>
2025-01-21 14:56:02 +0100

    vol-mig: invalidate destination DVs with no csi storageclass
    Volume migration with destination datavolumes which their storage class
    isn't a CSI could bring to races and fail. Storageclasses not supporting
    CSI don't use populators. For this reason, CDI uses a legacy import by
    running the importer as an additional pod.
    
    However, this doesn't properly coordinate with the target virt-launcher
    pod created during the migration. In this case, the cdi importer pod
    fails because the PVC results already in use, and the DV never goes in
    succeeded state.
    
    We decided to avoid this situation by refusing to migrate when the
    destination DV is using a no-csi class. CSI and volume populators are
    the modern way to handle storage in Kubernetes, therefore if users want
    still to use legacy storage classes, they need to use PVCs and cannot
    use datavolumes.
    
    A storageclass is recongnized as a CSI storage class if its provisioner
    exists as a csi driver.
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/virt-controller/watch/vm/vm.go
+++ pkg/virt-controller/watch/vm/vm.go
@@ -1033,7 +1033,7 @@ func (c *Controller) handleVolumeUpdateRequest(vm *virtv1.VirtualMachine, vmi *v
 		}
 	case *vm.Spec.UpdateVolumesStrategy == virtv1.UpdateVolumesStrategyMigration:
 		// Validate if the update volumes can be migrated
-		if err := volumemig.ValidateVolumes(vmi, vm); err != nil {
+		if err := volumemig.ValidateVolumes(vmi, vm, c.dataVolumeStore, c.pvcStore); err != nil {
 			log.Log.Object(vm).Errorf("cannot migrate the VM. Volumes are invalid: %v", err)
 			setRestartRequired(vm, err.Error())
 			return nil
