04a1bbbca0ddf9307d6c460b595e432eb6ef38df
Alice Frosi <afrosi@redhat.com>
2025-01-22 14:18:33 +0100

    vm: fix return error for no kvm devices
    claimDeviceOwnership needs to return an error for devices which failed
    with or without software emulation enabled.
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -3517,7 +3517,7 @@ func (c *VirtualMachineController) claimDeviceOwnership(virtLauncherRootMount *s
 	softwareEmulation := c.clusterConfig.AllowEmulation()
 	devicePath, err := safepath.JoinNoFollow(virtLauncherRootMount, filepath.Join("dev", deviceName))
 	if err != nil {
-		if softwareEmulation {
+		if softwareEmulation && deviceName == "kvm" {
 			return nil
 		}
 		return err
