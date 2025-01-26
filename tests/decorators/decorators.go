a82ff5186fc35a440eea0f5840a55d3f8b2ee397
Nir Dothan <ndothan@redhat.com>
2025-01-26 16:51:58 +0200

    e2e, tests-suite: Support OncePerOrderedCleanup label
    Currently most test resources are cleaned up (vm/vmis, nodes, config)
    following each spec, since AfterEach() is global and uncustomizable.
    This does not enable running multiple test cases on a single set of
    VMs without complete cleanup and restart in between.
    Some tests may benefit from fine grained control
    over their cleanup triggers, thus reduce test duration.
    This change introduces a new decorator label 'skipGlobalCleanup',
    for which decorated nodes will not go trough the current
    global AfterEach() cleanup. Instead they will be caught by a new
    AfterEach() that only runs OncePerOrderd
    https://onsi.github.io/ginkgo/#setup-around-ordered-containers-the-onceperordered-decorator
    i.e. For ordered containers it will only run after the last
    test, but for all others (including Serial) it will run after every test.
    
    Summary of algorithm cases:
    1) Regular parallel test: no change, cleanup after each test.
       Second AfterEach() no-op since no label.
    2) Regular test with OncePerOrderedCleanup label (useless): not cleaned
       in first AfterEach() due to label, but caught by the second AfterEach().
    3) Ordered test without label: no change, cleaned after each test.
    4) Ordered test with label: skips first AfterEach(), caught by second
       but only OncePerOrdered i.e. after last test in ordered container.
    5) Serial: just like regular i.e. #1/#2
    
    This should ensure that:
    A) Env is left clean after test.
    B) All existing tests are not affected.
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- tests/decorators/decorators.go
+++ tests/decorators/decorators.go
@@ -114,4 +114,8 @@ var (
 	// LargeStoragePoolRequired indicates that the test may fail in a cluster with a low storage pool capacity.
 	// This decorator can be used to skip the test as the failure might not indicate a functional problem.
 	LargeStoragePoolRequired = Label("large-storage-pool-required")
+
+	// OncePerOrderedCleanup decorates Ordered tests to only cleanup after the last
+	// test in an Ordered container.
+	OncePerOrderedCleanup = Label("OncePerOrderedCleanup")
 )
