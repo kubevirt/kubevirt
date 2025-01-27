8d2705568b50d014a90b705761472f250352c713
Dan Kenigsberg <danken@redhat.com>
2025-01-27 16:51:48 +0200

    tests/vmi_lifecycle_test: drop the last SoftwareEmulation test
    Nothing in our CI is setting this decorator, so this test never run.
    Let us drop the test and the never-used decorator.
    Proper unit testing for Spec.Type=qemu is tracked in
    https://github.com/kubevirt/kubevirt/issues/13837
    
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

--- tests/vmi_lifecycle_test.go
+++ tests/vmi_lifecycle_test.go
@@ -21,10 +21,8 @@ package tests_test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -55,8 +53,6 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-config/featuregate"
 	"kubevirt.io/kubevirt/pkg/virt-controller/services"
 	device_manager "kubevirt.io/kubevirt/pkg/virt-handler/device-manager"
-	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
-	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/converter"
 	"kubevirt.io/kubevirt/tests/console"
 	cd "kubevirt.io/kubevirt/tests/containerdisk"
 	"kubevirt.io/kubevirt/tests/decorators"
@@ -1211,55 +1207,6 @@ var _ = Describe("[rfe_id:273][crit:high][vendor:cnv-qe@redhat.com][level:compon
 				Entry("[test_id:1641]Default test namespace", false),
 				Entry("[test_id:1642]Alternative test namespace", true),
 			)
-		})
-
-		Context("VirtualMachineInstance Emulation Mode", decorators.SoftwareEmulation, func() {
-
-			It("[test_id:1644]should be reflected in domain XML", func() {
-				vmi := libvmifact.NewAlpine()
-				err := kubevirt.Client().RestClient().Post().Resource("virtualmachineinstances").Namespace(testsuite.GetTestNamespace(vmi)).Body(vmi).Do(context.Background()).Error()
-				Expect(err).ToNot(HaveOccurred(), "Should post the VMI")
-
-				listOptions := metav1.ListOptions{}
-
-				Eventually(func() int {
-					podList, err := kubevirt.Client().CoreV1().Pods(testsuite.GetTestNamespace(vmi)).List(context.Background(), listOptions)
-					Expect(err).ToNot(HaveOccurred(), "Should list the pods")
-					return len(podList.Items)
-				}, 75, 0.5).Should(Equal(1), "There should be only one pod")
-
-				Eventually(func() error {
-					podList, err := kubevirt.Client().CoreV1().Pods(testsuite.GetTestNamespace(vmi)).List(context.Background(), listOptions)
-					Expect(err).ToNot(HaveOccurred(), "Should list the pods")
-					for _, item := range podList.Items {
-						if strings.HasPrefix(item.Name, vmi.ObjectMeta.GenerateName) {
-							return nil
-						}
-					}
-					return fmt.Errorf("Associated pod for VirtualMachineInstance '%s' not found", vmi.Name)
-				}, 75, 0.5).Should(Succeed(), "Should find the VMI pod")
-
-				getOptions := metav1.GetOptions{}
-				var newVMI *v1.VirtualMachineInstance
-
-				newVMI, err = kubevirt.Client().VirtualMachineInstance(testsuite.GetTestNamespace(newVMI)).Get(context.Background(), vmi.Name, getOptions)
-				Expect(err).ToNot(HaveOccurred(), "Should get VMI")
-
-				domain := &api.Domain{}
-				context := &converter.ConverterContext{
-					VirtualMachine: newVMI,
-					AllowEmulation: true,
-				}
-				err = converter.Convert_v1_VirtualMachineInstance_To_api_Domain(newVMI, domain, context)
-				Expect(err).ToNot(HaveOccurred())
-
-				expectedType := ""
-				if _, err := os.Stat("/dev/kvm"); errors.Is(err, os.ErrNotExist) {
-					expectedType = "qemu"
-				}
-
-				Expect(domain.Spec.Type).To(Equal(expectedType), "VMI domain type should be of expectedType")
-			})
 		})
 
 		Context("VM Accelerated Mode", decorators.WgS390x, func() {
