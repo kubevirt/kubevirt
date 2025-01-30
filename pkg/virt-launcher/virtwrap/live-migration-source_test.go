5372b9f6619abc102a0a999e79ca3828cb4387ef
Antonio Cardace <acardace@redhat.com>
2025-01-30 11:04:33 +0100

    virt-launcher: add live-migration-source unit tests
    Just a bunch of basic unit tests to cover setting a
    live migration result.
    
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- pkg/virt-launcher/virtwrap/live-migration-source_test.go
+++ pkg/virt-launcher/virtwrap/live-migration-source_test.go
@@ -1,17 +1,117 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ *
+ */
+
 package virtwrap
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
+	"k8s.io/apimachinery/pkg/types"
 
 	v1 "kubevirt.io/api/core/v1"
 
+	"kubevirt.io/kubevirt/pkg/ephemeral-disk/fake"
 	"kubevirt.io/kubevirt/pkg/libvmi"
 	libvmistatus "kubevirt.io/kubevirt/pkg/libvmi/status"
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
+	"kubevirt.io/kubevirt/pkg/virt-launcher/metadata"
+	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/cli"
+	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/errors"
 )
 
-var _ = Describe("Live Migration for the source", func() {
+var _ = Describe("Live migration source", func() {
+	var libvirtDomainManager *LibvirtDomainManager
+
+	Context("Migration result", func() {
+		BeforeEach(func() {
+			vmi := &v1.VirtualMachineInstance{
+				Status: v1.VirtualMachineInstanceStatus{
+					MigrationState: &v1.VirtualMachineInstanceMigrationState{
+						MigrationUID: types.UID(fmt.Sprintf("%v", GinkgoRandomSeed())),
+					},
+				},
+			}
+
+			mockConn := &cli.MockConnection{}
+			testVirtShareDir := fmt.Sprintf("fake-virt-share-%d", GinkgoRandomSeed())
+			testEphemeralDiskDir := fmt.Sprintf("fake-ephemeral-disk-%d", GinkgoRandomSeed())
+			ephemeralDiskCreatorMock := &fake.MockEphemeralDiskImageCreator{}
+			metadataCache := metadata.NewCache()
+
+			manager, _ := NewLibvirtDomainManager(mockConn, testVirtShareDir, testEphemeralDiskDir, nil, "/usr/share/OVMF", ephemeralDiskCreatorMock, metadataCache, nil, virtconfig.DefaultDiskVerificationMemoryLimitBytes, fakeCpuSetGetter)
+			libvirtDomainManager = manager.(*LibvirtDomainManager)
+			libvirtDomainManager.initializeMigrationMetadata(vmi, v1.MigrationPreCopy)
+		})
+
+		It("should only be set once", func() {
+			libvirtDomainManager.setMigrationResult(false, "", "")
+			migrationMetadata, exists := libvirtDomainManager.metadataCache.Migration.Load()
+			Expect(exists).To(BeTrue(), "migrationMetadata not found")
+			Expect(migrationMetadata.EndTimestamp).ToNot(BeNil(), "migration EndTimestamp not set")
+			Expect(migrationMetadata.Failed).To(BeFalse(), "migration has failed")
+
+			endTimestamp := migrationMetadata.EndTimestamp.DeepCopy()
+
+			libvirtDomainManager.setMigrationResult(true, "", "")
+			Expect(exists).To(BeTrue())
+			Expect(migrationMetadata.EndTimestamp).To(Equal(endTimestamp), "migrationMetadata changed")
+			Expect(migrationMetadata.Failed).To(BeFalse(), "migration has failed")
+		})
+
+		It("should return 'Aborting' if abortion is in progress", func() {
+			libvirtDomainManager.setMigrationResult(false, "", v1.MigrationAbortInProgress)
+			migrationMetadata, exists := libvirtDomainManager.metadataCache.Migration.Load()
+			Expect(exists).To(BeTrue(), "migrationMetadata not found")
+			Expect(migrationMetadata.EndTimestamp).To(BeNil(), "migration EndTimestamp is set")
+			Expect(migrationMetadata.Failed).To(BeFalse(), "migration has failed")
+
+			err := libvirtDomainManager.setMigrationResult(false, "", v1.MigrationAbortInProgress)
+			Expect(err).To(Equal(errors.MigrationAbortInProgressError))
+			migrationMetadata, exists = libvirtDomainManager.metadataCache.Migration.Load()
+			Expect(exists).To(BeTrue(), "migrationMetadata not found")
+			Expect(migrationMetadata.EndTimestamp).To(BeNil(), "migration EndTimestamp is set")
+			Expect(migrationMetadata.Failed).To(BeFalse(), "migration has failed")
+		})
+
+		DescribeTable("EndTimestamp", func(isFailed bool, abortStatus v1.MigrationAbortStatus, shouldEndTimestampBeSet bool) {
+			libvirtDomainManager.setMigrationResult(isFailed, "", abortStatus)
+			migrationMetadata, exists := libvirtDomainManager.metadataCache.Migration.Load()
+			Expect(exists).To(BeTrue(), "migrationMetadata not found")
+			Expect(migrationMetadata.Failed).To(Equal(isFailed), "migration result is wrong")
+
+			if shouldEndTimestampBeSet {
+				Expect(migrationMetadata.EndTimestamp).ToNot(BeNil(), "migration EndTimestamp not set")
+			} else {
+				Expect(migrationMetadata.EndTimestamp).To(BeNil(), "migration EndTimestamp is set")
+			}
+		},
+			Entry("should be set when the migration is successful", false, v1.MigrationAbortStatus(""), true),
+			Entry("should be set when the migration has failed", true, v1.MigrationAbortStatus(""), true),
+			Entry("should be set when the migration has been aborted", false, v1.MigrationAbortSucceeded, true),
+			Entry("should not be set when an abortion request does not succeed", false, v1.MigrationAbortFailed, false),
+			Entry("should not be set when an abortion request is still in progress", false, v1.MigrationAbortInProgress, false),
+		)
+	})
+
 	Context("classifyVolumesForMigration", func() {
 		It("should classify shared volumes to migrated when they are part of the migrated volumes set", func() {
 			const vol = "vol"
