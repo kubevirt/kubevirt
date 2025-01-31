d7106b6a7a48006859973f5f31fab9da6faf947b
Jan Schintag <jan.schintag@de.ibm.com>
2025-01-31 15:30:51 +0100

    virt-handler/node-labeller: Move arch specific content into separate files
    Use arch specific files for arch specific configuration.
    
    Signed-off-by: Jan Schintag <jan.schintag@de.ibm.com>

---
+++ pkg/virt-handler/node-labeller/s390x.go
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright the KubeVirt Authors.
+ *
+ */
+
+package nodelabeller
+
+import "kubevirt.io/kubevirt/pkg/virt-handler/node-labeller/util"
+
+// Ensure that there is a compile error should the struct not implement the archLabeller interface anymore.
+var _ = archLabeller(&archLabellerS390X{})
+
+type archLabellerS390X struct{}
+
+func (a archLabellerS390X) ShouldLabelNodes() bool {
+	return true
+}
+
+func (a archLabellerS390X) defaultVendor() string {
+	// On s390x the xml does not include a CPU Vendor, however there is only one company selling them anyway.
+	return "IBM"
+}
+
+func (a archLabellerS390X) requirePolicy(policy string) bool {
+	// On s390x, the policy is not set
+	return policy == util.RequirePolicy || policy == ""
+}
+
+func (a archLabellerS390X) hasHostSupportedFeatures() bool {
+	return true
+}
+
+func (a archLabellerS390X) supportsHostModel() bool {
+	return true
+}
