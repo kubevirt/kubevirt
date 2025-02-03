2523d5c91fc994a8b40bf6bc1bad211cea336bbc
Orel Misan <omisan@redhat.com>
2025-02-03 22:17:52 +0200

    net, ctrls, vm, hotplug: Remove hotplug NIC with bridge binding
    This test is covered by [1].
    
    [1] https://github.com/kubevirt/kubevirt/blob/4360aeb778dfee4752e487b3996958c2a0cf3e3a/pkg/network/controllers/vm_test.go#L129
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/network/controllers/hotplug_test.go
+++ pkg/network/controllers/hotplug_test.go
@@ -47,17 +47,6 @@ var _ = Describe("Network interface hot{un}plug", func() {
 			Expect(updatedVMI.Networks).To(Equal(expectedVMI.Spec.Networks))
 			Expect(updatedVMI.Domain.Devices.Interfaces).To(Equal(expectedVMI.Spec.Domain.Devices.Interfaces))
 		},
-		Entry("when a bridge binding interface has to be hotplugged",
-			libvmi.New(
-				libvmi.WithInterface(bridgeInterface(testNetworkName1)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName1}),
-			),
-			libvmi.New(),
-			libvmi.New(
-				libvmi.WithInterface(bridgeInterface(testNetworkName1)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName1}),
-			),
-			!ordinal),
 		Entry("when an SRIOV  binding interface has to be hotplugged",
 			libvmi.New(
 				libvmi.WithInterface(sriovInterface(testNetworkName1)),
