615b01720b44dad73079f1c30e40dd3d37fbaf9a
Adam Korczynski <adam@adalogics.com>
2025-02-15 23:25:59 +0000

    fix memory creep in VM fuzzer
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- pkg/virt-controller/watch/vm/fuzz_test.go
+++ pkg/virt-controller/watch/vm/fuzz_test.go
@@ -1,6 +1,12 @@
 package vm
 
 import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/rand"
+	stdruntime "runtime"
 	"testing"
 
 	gfh "github.com/AdaLogics/go-fuzz-headers"
@@ -12,12 +18,18 @@ import (
 	k8sfake "k8s.io/client-go/kubernetes/fake"
 	k8sTesting "k8s.io/client-go/testing"
 	"k8s.io/client-go/tools/cache"
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 	"k8s.io/client-go/tools/record"
 	v1 "kubevirt.io/api/core/v1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	KVv1 "kubevirt.io/api/core/v1"
 	cdifake "kubevirt.io/client-go/containerizeddataimporter/fake"
 	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/client-go/kubevirt/fake"
 	cdiv1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
+	"kubevirt.io/client-go/log"
+
+	framework "k8s.io/client-go/tools/cache/testing"
 
 	virtcontroller "kubevirt.io/kubevirt/pkg/controller"
 	instancetypecontroller "kubevirt.io/kubevirt/pkg/instancetype/controller/vm"
@@ -26,7 +38,41 @@ import (
 
 var (
 	maxResources = 3
+	kvObjectNamespace = "kubevirt"
+	kvObjectName      = "kubevirt"
 )
+
+func NewFakeClusterConfigUsingKV(kv *KVv1.KubeVirt) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	return NewFakeClusterConfigUsingKVWithCPUArch(kv, stdruntime.GOARCH)
+}
+
+func NewFakeClusterConfigUsingKVWithCPUArch(kv *KVv1.KubeVirt, CPUArch string) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	kv.ResourceVersion = rand.String(10)
+	kv.Status.Phase = "Deployed"
+	crdInformer, cs1 := testutils.NewFakeInformerFor(&extv1.CustomResourceDefinition{})
+	kubeVirtInformer, cs2 := testutils.NewFakeInformerFor(&KVv1.KubeVirt{})
+
+	kubeVirtInformer.GetStore().Add(kv)
+
+	AddDataVolumeAPI(crdInformer)
+	cfg, _ := virtconfig.NewClusterConfigWithCPUArch(crdInformer, kubeVirtInformer, kvObjectNamespace, CPUArch)
+	return cfg, crdInformer, kubeVirtInformer.GetStore(), cs1, cs2
+}
+
+func AddDataVolumeAPI(crdInformer cache.SharedIndexInformer) {
+	crdInformer.GetStore().Add(&extv1.CustomResourceDefinition{
+		Spec: extv1.CustomResourceDefinitionSpec{
+			Names: extv1.CustomResourceDefinitionNames{
+				Kind: "DataVolume",
+			},
+		},
+	})
+}
+
+func NewFakeClusterConfigUsingKVConfig(kv *KVv1.KubeVirt) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	
+	return NewFakeClusterConfigUsingKV(kv)
+}
 
 // FuzzExecute add up to 3 virtual machines
 // to the context and then runs the controller.
@@ -41,6 +87,16 @@ func FuzzExecute(f *testing.F) {
 			err := fdp.GenerateStruct(vm)
 			if err != nil {
 				return
+			}
+			vm.Namespace = metav1.NamespaceDefault
+
+			setLatestAnnotation, err := fdp.GetBool()
+			if err != nil {
+				return
+			}
+			// This helps the vm overcome some checks early in the callgraph
+			if setLatestAnnotation {
+				virtcontroller.SetLatestApiVersionAnnotation(vm)
 			}
 			vms = append(vms, vm)
 		}
@@ -65,12 +121,23 @@ func FuzzExecute(f *testing.F) {
 		virtFakeClient.PrependReactor("patch", "virtualmachines",
 			PatchReactor(Handle, virtFakeClient.Tracker(), ModifyVM))
 
-		dataVolumeInformer, _ := testutils.NewFakeInformerFor(&cdiv1.DataVolume{})
-		dataSourceInformer, _ := testutils.NewFakeInformerFor(&cdiv1.DataSource{})
-		vmiInformer, _ := testutils.NewFakeInformerWithIndexersFor(&v1.VirtualMachineInstance{}, virtcontroller.GetVMIInformerIndexers())
-		vmInformer, _ := testutils.NewFakeInformerWithIndexersFor(&v1.VirtualMachine{}, virtcontroller.GetVirtualMachineInformerIndexers())
-		pvcInformer, _ := testutils.NewFakeInformerFor(&k8sv1.PersistentVolumeClaim{})
-		namespaceInformer, _ := testutils.NewFakeInformerFor(&k8sv1.Namespace{})
+		dataVolumeInformer, dvCs := testutils.NewFakeInformerFor(&cdiv1.DataVolume{})
+		dataSourceInformer, dsCs := testutils.NewFakeInformerFor(&cdiv1.DataSource{})
+		vmiInformer, viCs := testutils.NewFakeInformerWithIndexersFor(&v1.VirtualMachineInstance{}, virtcontroller.GetVMIInformerIndexers())
+		vmInformer, vCs := testutils.NewFakeInformerWithIndexersFor(&v1.VirtualMachine{}, virtcontroller.GetVirtualMachineInformerIndexers())
+		pvcInformer, piCs := testutils.NewFakeInformerFor(&k8sv1.PersistentVolumeClaim{})
+		namespaceInformer, nsCs := testutils.NewFakeInformerFor(&k8sv1.Namespace{})
+		podInformer, pCs := testutils.NewFakeInformerFor(&k8sv1.Pod{})
+
+		// When running this fuzzer on OSS-Fuzz, we need to shut down
+		// the controller sources to avoid consuming too much memory.
+		defer dvCs.Shutdown()
+		defer dsCs.Shutdown()
+		defer viCs.Shutdown()
+		defer vCs.Shutdown()
+		defer piCs.Shutdown()
+		defer nsCs.Shutdown()
+		defer pCs.Shutdown()
 
 		ns1 := &k8sv1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
@@ -88,8 +155,10 @@ func FuzzExecute(f *testing.F) {
 		if namespaceInformer.GetStore().Add(ns2) != nil {
 			panic("Should not happen")
 		}
+		defer namespaceInformer.GetStore().Delete(ns1)
+		defer namespaceInformer.GetStore().Delete(ns2)
 
-		crInformer, _ := testutils.NewFakeInformerWithIndexersFor(&appsv1.ControllerRevision{}, cache.Indexers{
+		crInformer, crCs := testutils.NewFakeInformerWithIndexersFor(&appsv1.ControllerRevision{}, cache.Indexers{
 			"vm": func(obj interface{}) ([]string, error) {
 				cr := obj.(*appsv1.ControllerRevision)
 				for _, ref := range cr.OwnerReferences {
@@ -100,12 +169,37 @@ func FuzzExecute(f *testing.F) {
 				return nil, nil
 			},
 		})
-		podInformer, _ := testutils.NewFakeInformerFor(&k8sv1.Pod{})
+		defer crCs.Shutdown()
 
 		recorder := record.NewFakeRecorder(100)
 		recorder.IncludeObject = true
 
-		config, _, _ := testutils.NewFakeClusterConfigUsingKVConfig(&v1.KubeVirtConfiguration{})
+		kv := &KVv1.KubeVirt{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      kvObjectName,
+				Namespace: kvObjectNamespace,
+			},
+			Spec: KVv1.KubeVirtSpec{
+				Configuration: v1.KubeVirtConfiguration{},
+			},
+			Status: KVv1.KubeVirtStatus{
+				DefaultArchitecture: stdruntime.GOARCH,
+				Phase:               "Deployed",
+			},
+		}
+
+		config, crdInformer, kubeVirtInformerStore, cs1, cs2 := NewFakeClusterConfigUsingKVConfig(kv)
+		defer cs1.Shutdown()
+		defer cs2.Shutdown()
+		defer kubeVirtInformerStore.Delete(kv)
+		defer func(){
+				for _, obj := range crdInformer.GetStore().List() {
+				err := crdInformer.GetStore().Delete(obj)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
 
 		controller, err := NewController(vmiInformer,
 			vmInformer,
@@ -124,6 +218,8 @@ func FuzzExecute(f *testing.F) {
 		if err != nil {
 			return
 		}
+		// Shutdown default queue to avoid memory creep
+		controller.Queue.ShutDown()
 
 		// Wrap our workqueue to have a way to detect when we are done processing updates
 		mockQueue := testutils.NewMockWorkQueue(controller.Queue)
@@ -148,9 +244,20 @@ func FuzzExecute(f *testing.F) {
 		virtClient.EXPECT().AppsV1().Return(k8sClient.AppsV1()).AnyTimes()
 		virtClient.EXPECT().CoreV1().Return(k8sClient.CoreV1()).AnyTimes()
 		virtClient.EXPECT().AuthorizationV1().Return(k8sClient.AuthorizationV1()).AnyTimes()
-
 		// Add the resources to the context
 		for _, vm := range vms {
+			// We can create the VM by way of the client or put the vm in the queue.
+			create, err := fdp.GetBool() 
+			if err != nil {
+				return
+			}
+			if create {
+				_, err := virtFakeClient.KubevirtV1().VirtualMachines(vm.Namespace).Create(context.Background(), vm, metav1.CreateOptions{})
+				if err != nil {
+					return
+				}
+				continue
+			}
 			key, err := virtcontroller.KeyFunc(vm)
 			if err != nil {
 				return
@@ -164,6 +271,13 @@ func FuzzExecute(f *testing.F) {
 				virtFakeClient.KubevirtV1().VirtualMachines(vm.Namespace),
 			).AnyTimes()
 		}
+		// An empty queue will block the fuzzer and it will timeout
+		if controller.Queue.Len() == 0 {
+			return
+		}
+		// ignore logs
+		var b bytes.Buffer
+		log.Log.SetIOWriter(bufio.NewWriter(&b))
 
 		// Run the controller
 		controller.Execute()
