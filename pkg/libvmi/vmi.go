04a8ee5cc2e5dddf77ca6c0d97d8a69f9c89a081
Shanjiv A <shanjv177@gmail.com>
2025-02-20 00:10:53 +0530

    replaced NewMinimalDomainWithNS with respective libvmi functions
    Signed-off-by: Shanjiv A <shanjv177@gmail.com>

--- pkg/libvmi/vmi.go
+++ pkg/libvmi/vmi.go
@@ -25,6 +25,8 @@ import (
 	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/rand"
 
+	"k8s.io/apimachinery/pkg/types"
+
 	v1 "kubevirt.io/api/core/v1"
 
 	"kubevirt.io/kubevirt/pkg/pointer"
@@ -87,6 +89,19 @@ func WithName(name string) Option {
 func WithNamespace(namespace string) Option {
 	return func(vmi *v1.VirtualMachineInstance) {
 		vmi.Namespace = namespace
+	}
+}
+
+func WithDeletionTimeStamp() Option {
+	return func(vmi *v1.VirtualMachineInstance) {
+		now := k8smetav1.Now()
+		vmi.ObjectMeta.DeletionTimestamp = &now
+	}
+}
+
+func WithUID(uid types.UID) Option {
+	return func(vmi *v1.VirtualMachineInstance) {
+		vmi.ObjectMeta.UID = uid
 	}
 }
 
