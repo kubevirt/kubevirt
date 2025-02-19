6e0892c28a04afaf9ffb07ae0c51126e86ceb3d9
João Vilaça <machadovilaca@gmail.com>
2025-02-19 15:44:14 +0000

    Add kubevirt_vmi_migration_(start|end)_time_seconds metrics
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- tests/monitoring/metrics.go
+++ tests/monitoring/metrics.go
@@ -83,6 +83,8 @@ var _ = Describe("[sig-monitoring]Metrics", decorators.SigMonitoring, func() {
 			"kubevirt_vmi_migration_data_processed_bytes":                        true,
 			"kubevirt_vmi_migration_dirty_memory_rate_bytes":                     true,
 			"kubevirt_vmi_migration_disk_transfer_rate_bytes":                    true,
+			"kubevirt_vmi_migration_start_time_seconds":                          true,
+			"kubevirt_vmi_migration_end_time_seconds":                            true,
 		}
 
 		It("should contain virt components metrics", func() {
