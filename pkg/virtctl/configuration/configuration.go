b723c48dc0a54c7d633ac9aa7c7de7d864576edb
Ashlok2003 <chaudharyashlok@gmail.com>
2025-02-20 18:28:37 +0530

    Refactored virtctl configuration and improved formatting

--- pkg/virtctl/configuration/configuration.go
+++ pkg/virtctl/configuration/configuration.go
@@ -26,9 +26,7 @@ func NewListPermittedDevices() *cobra.Command {
 }
 
 func usage() string {
-	usage := "  # Print the permitted devices for VMIs:\n"
-	usage += "  {{ProgramName}} permitted-devices"
-	return usage
+	return "# Print the permitted devices for VMIs:\n  {{ProgramName}} permitted-devices"
 }
 
 func run(cmd *cobra.Command, _ []string) error {
@@ -43,7 +41,7 @@ func run(cmd *cobra.Command, _ []string) error {
 	}
 
 	if len(kvList.Items) == 0 {
-		return fmt.Errorf("No KubeVirt resource in %q namespace", namespace)
+		return fmt.Errorf("no KubeVirt resource in %q namespace", namespace)
 	}
 
 	kv := kvList.Items[0]
@@ -61,9 +59,9 @@ func run(cmd *cobra.Command, _ []string) error {
 		}
 	}
 
-	fmt.Printf("Permitted Devices: \nHost Devices: \n%s \nGPU Devices: \n%s\n",
-		fmt.Sprint(strings.Join(hostDeviceList, ", ")),
-		fmt.Sprint(strings.Join(gpuDeviceList, ", ")),
+	cmd.Printf("Permitted Devices: \nHost Devices: \n%s \nGPU Devices: \n%s\n",
+		strings.Join(hostDeviceList, ", "),
+		strings.Join(gpuDeviceList, ", "),
 	)
 
 	return nil
