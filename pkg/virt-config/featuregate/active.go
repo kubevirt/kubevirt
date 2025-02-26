6666b39aaeacd8781755db67abbc6bacbd785d5f
Lee Yarwood <lyarwood@redhat.com>
2025-02-26 16:12:32 +0000

    featuregates: Graduate InstancetypeReferencePolicy to Beta
    This graduation from Alpha to Beta was missed by the initial v1.5.0 release but should
    happen and be backported to allow GA and removal of the FG with v1.6.0.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- pkg/virt-config/featuregate/active.go
+++ pkg/virt-config/featuregate/active.go
@@ -60,6 +60,7 @@ const (
 	NodeRestrictionGate = "NodeRestriction"
 	// Owner: @lyarwood
 	// Alpha: v1.4.0
+	// Beta: v1.5.0
 	//
 	// InstancetypeReferencePolicy allows a cluster admin to control how a VirtualMachine references instance types and preferences
 	// through the kv.spec.configuration.instancetype.referencePolicy configurable.
@@ -92,7 +93,7 @@ func init() {
 	RegisterFeatureGate(FeatureGate{Name: MultiArchitecture, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: AlignCPUsGate, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: NodeRestrictionGate, State: Alpha})
-	RegisterFeatureGate(FeatureGate{Name: InstancetypeReferencePolicy, State: Alpha})
+	RegisterFeatureGate(FeatureGate{Name: InstancetypeReferencePolicy, State: Beta})
 	RegisterFeatureGate(FeatureGate{Name: VirtIOFSConfigVolumesGate, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: VirtIOFSStorageVolumeGate, State: Alpha})
 }
