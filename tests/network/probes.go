3fa01524b200e1d91d1a15b7b0bf81ebaec20d9f
Nir Dothan <ndothan@redhat.com>
2025-02-27 18:41:17 +0200

    e2e, net, probes: Flip condition order
    Currently there are 2 conditions that use negative+else.
    i.e. if  !isExecProbe()... else ...
    Improve readability by flipping condition to positive+else
    i.e. if isExecProbe()... else ...
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- tests/network/probes.go
+++ tests/network/probes.go
@@ -60,7 +60,17 @@ var _ = SIGDescribe("[ref_id:1182]Probes", func() {
 		DescribeTable("should succeed", func(readinessProbe *v1.Probe, ipFamily k8sv1.IPFamily) {
 			libnet.SkipWhenClusterNotSupportIPFamily(ipFamily)
 
-			if !isExecProbe(readinessProbe) {
+			if isExecProbe(readinessProbe) {
+				By(specifyingVMReadinessProbe)
+				vmi = libvmifact.NewFedora(libnet.WithMasqueradeNetworking(), withReadinessProbe(readinessProbe))
+				vmi = libvmops.RunVMIAndExpectLaunchIgnoreWarnings(vmi, 180)
+
+				By("Waiting for agent to connect")
+				Eventually(matcher.ThisVMI(vmi)).
+					WithTimeout(12 * time.Minute).
+					WithPolling(2 * time.Second).
+					Should(matcher.HaveConditionTrue(v1.VirtualMachineInstanceAgentConnected))
+			} else {
 				By(specifyingVMReadinessProbe)
 				vmi = createReadyAlpineVMIWithReadinessProbe(readinessProbe)
 
@@ -71,16 +81,6 @@ var _ = SIGDescribe("[ref_id:1182]Probes", func() {
 				vmi, err = kubevirt.Client().VirtualMachineInstance(testsuite.GetTestNamespace(nil)).Get(context.Background(), vmi.Name, metav1.GetOptions{})
 				Expect(err).ToNot(HaveOccurred())
 				serverStarter(vmi, readinessProbe, 1500)
-			} else {
-				By(specifyingVMReadinessProbe)
-				vmi = libvmifact.NewFedora(libnet.WithMasqueradeNetworking(), withReadinessProbe(readinessProbe))
-				vmi = libvmops.RunVMIAndExpectLaunchIgnoreWarnings(vmi, 180)
-
-				By("Waiting for agent to connect")
-				Eventually(matcher.ThisVMI(vmi)).
-					WithTimeout(12 * time.Minute).
-					WithPolling(2 * time.Second).
-					Should(matcher.HaveConditionTrue(v1.VirtualMachineInstanceAgentConnected))
 			}
 
 			Eventually(matcher.ThisVMI(vmi)).
@@ -148,13 +148,7 @@ var _ = SIGDescribe("[ref_id:1182]Probes", func() {
 		DescribeTable("should not fail the VMI", func(livenessProbe *v1.Probe, ipFamily k8sv1.IPFamily) {
 			libnet.SkipWhenClusterNotSupportIPFamily(ipFamily)
 
-			if !isExecProbe(livenessProbe) {
-				By(specifyingVMLivenessProbe)
-				vmi = createReadyAlpineVMIWithLivenessProbe(livenessProbe)
-
-				By("Starting the server inside the VMI")
-				serverStarter(vmi, livenessProbe, 1500)
-			} else {
+			if isExecProbe(livenessProbe) {
 				By(specifyingVMLivenessProbe)
 				vmi = libvmifact.NewFedora(libnet.WithMasqueradeNetworking(), withLivelinessProbe(livenessProbe))
 				vmi = libvmops.RunVMIAndExpectLaunchIgnoreWarnings(vmi, 180)
@@ -164,6 +158,12 @@ var _ = SIGDescribe("[ref_id:1182]Probes", func() {
 					WithTimeout(12 * time.Minute).
 					WithPolling(2 * time.Second).
 					Should(matcher.HaveConditionTrue(v1.VirtualMachineInstanceAgentConnected))
+			} else {
+				By(specifyingVMLivenessProbe)
+				vmi = createReadyAlpineVMIWithLivenessProbe(livenessProbe)
+
+				By("Starting the server inside the VMI")
+				serverStarter(vmi, livenessProbe, 1500)
 			}
 
 			By("Checking that the VMI is still running after a while")
