57c6fd807adbf2d87a3cfbbbc85b6ae40ae27820
Enrique Llorente <ellorent@redhat.com>
2025-03-04 12:33:08 +0100

    rpm: Bump all RPMs
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="ed3477ca6c24b86cffb15aa034bfa3eecc3d4b98"
+sandbox_hash="b8c1d284a1bac0bd905c18bf465b5f9a2ea9f7af"
 
 function kubevirt::bootstrap::regenerate() {
     (
