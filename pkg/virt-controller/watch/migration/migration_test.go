cecc153db34da8a200611147ddbeadf36f60d1c2
Jed Lejosne <jed@redhat.com>
2025-03-06 11:28:06 -0500

    migration controller: switch to a priority queue
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    (cherry picked from commit bdad87fd0d574ad38274296b96f969355f8c86ea)
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-controller/watch/migration/migration_test.go
+++ pkg/virt-controller/watch/migration/migration_test.go
@@ -270,8 +270,9 @@ var _ = Describe("Migration watcher", func() {
 			config,
 		)
 		// Wrap our workqueue to have a way to detect when we are done processing updates
-		mockQueue = testutils.NewMockWorkQueue(controller.Queue)
-		controller.Queue = mockQueue
+		// TODO: mock priority queues
+		//mockQueue = testutils.NewMockWorkQueue(controller.Queue)
+		//controller.Queue = mockQueue
 
 		namespace = k8sv1.Namespace{
 			TypeMeta:   metav1.TypeMeta{Kind: "Namespace"},
