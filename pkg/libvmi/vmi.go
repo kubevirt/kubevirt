959c8a91024e206c9c64ce23f876daab2deae8ee
fossedihelm <ffossemo@redhat.com>
2025-03-24 12:48:42 +0100

    Test 1.33 provider
    increase libvirt verbosity
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- pkg/libvmi/vmi.go
+++ pkg/libvmi/vmi.go
@@ -35,6 +35,7 @@ func New(opts ...Option) *v1.VirtualMachineInstance {
 	vmi := baseVmi(randName())
 
 	WithTerminationGracePeriod(0)(vmi)
+	WithAnnotation("kubevirt.io/libvirt-log-filters", "1:*")(vmi)
 	for _, f := range opts {
 		f(vmi)
 	}
