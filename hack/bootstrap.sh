0fcf2f86169dfc8a47969f43cc91e6f06bfae29d
Alice Frosi <afrosi@redhat.com>
2025-03-27 08:36:01 +0100

    rpms: bump of the rpm dependecies
    The rpm update is necessary in oder to pick the new version of the
    multipath libraries:
      0.8.7-35 -> 0.8.7-37
    
    The new version of the library contains the fix for multipath to use
    regular sockets and it works with containerized application. This enable
    KubeVirt to properly work with SCSI persistent reservation and storage
    using multipath. See for more details:
      https://issues.redhat.com/browse/RHEL-78758
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="0f9400096fa0fb013c9ee8b93747ea66bf1b7417"
+sandbox_hash="843119047c834886bbed864b67b38e9ed6003245"
 
 function kubevirt::bootstrap::regenerate() {
     (
