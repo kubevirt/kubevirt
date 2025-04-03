92132ae31c065bdd729fd8692008d29d67a9a2cf
Orel Misan <omisan@redhat.com>
2025-04-03 19:47:31 +0300

    net, ctrl, vm: Move hotplug + hotunplug test
    Move the test to the caller's level.
    It is done in order to move `ApplyDynamicIfaceRequestOnVMI` in a
    following commit.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/network/controllers/hotplug_test.go
+++ pkg/network/controllers/hotplug_test.go
@@ -23,50 +23,13 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	v1 "kubevirt.io/api/core/v1"
 
-	"kubevirt.io/kubevirt/pkg/libvmi"
 	"kubevirt.io/kubevirt/pkg/network/controllers"
 	"kubevirt.io/kubevirt/pkg/network/vmispec"
 )
 
 var _ = Describe("Network interface hot{un}plug", func() {
-	const (
-		testNetworkName1 = "testnet1"
-		testNetworkName2 = "testnet2"
-
-		ordinal = true
-	)
-
-	DescribeTable("apply dynamic interface request on VMI",
-		func(vmiForVM, currentVMI, expectedVMI *v1.VirtualMachineInstance, hasOrdinalIfaces bool) {
-			vm := virtualMachineFromVMI(currentVMI.Name, vmiForVM)
-			updatedVMI := controllers.ApplyDynamicIfaceRequestOnVMI(vm, currentVMI, hasOrdinalIfaces)
-			Expect(updatedVMI.Networks).To(Equal(expectedVMI.Spec.Networks))
-			Expect(updatedVMI.Domain.Devices.Interfaces).To(Equal(expectedVMI.Spec.Domain.Devices.Interfaces))
-		},
-		Entry("when one interface has to be plugged and other hotunplugged",
-			libvmi.New(
-				libvmi.WithInterface(bridgeAbsentInterface(testNetworkName1)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName1}),
-				libvmi.WithInterface(bridgeInterface(testNetworkName2)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName2}),
-			),
-			libvmi.New(
-				libvmi.WithInterface(bridgeInterface(testNetworkName1)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName1}),
-			),
-			libvmi.New(
-				libvmi.WithInterface(bridgeAbsentInterface(testNetworkName1)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName1}),
-				libvmi.WithInterface(bridgeInterface(testNetworkName2)),
-				libvmi.WithNetwork(&v1.Network{Name: testNetworkName2}),
-			),
-			!ordinal),
-	)
-
 	DescribeTable("spec interfaces",
 		func(specIfaces []v1.Interface, statusIfaces []v1.VirtualMachineInstanceNetworkInterface,
 			expectedInterfaces []v1.Interface, expectedNetworks []v1.Network,
@@ -103,31 +66,3 @@ var _ = Describe("Network interface hot{un}plug", func() {
 		),
 	)
 })
-
-func bridgeInterface(name string) v1.Interface {
-	return v1.Interface{Name: name, InterfaceBindingMethod: v1.InterfaceBindingMethod{Bridge: &v1.InterfaceBridge{}}}
-}
-
-func bridgeAbsentInterface(name string) v1.Interface {
-	iface := bridgeInterface(name)
-	iface.State = v1.InterfaceStateAbsent
-	return iface
-}
-
-func virtualMachineFromVMI(name string, vmi *v1.VirtualMachineInstance) *v1.VirtualMachine {
-	started := true
-	vm := &v1.VirtualMachine{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: vmi.ObjectMeta.Namespace, ResourceVersion: "1", UID: "vm-uid"},
-		Spec: v1.VirtualMachineSpec{
-			Running: &started,
-			Template: &v1.VirtualMachineInstanceTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   vmi.ObjectMeta.Name,
-					Labels: vmi.ObjectMeta.Labels,
-				},
-				Spec: vmi.Spec,
-			},
-		},
-	}
-	return vm
-}
