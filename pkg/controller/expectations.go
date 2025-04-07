5f61685273eb3e5f88b22816cf0842ceed30291c
Felix Matouschek <fmatouschek@redhat.com>
2025-04-07 11:39:45 +0200

    fix(controller,expectations): Prevent data races
    Prevent data races in controller expectations by converting the add and
    del fields in struct ControlleeExpectations from regular int64s to
    atomic.Int64s and by adding a String() method to print the struct in logs.
    The add and del fields have already been mostly accessed atomically,
    but in some cases data races could still occur.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- pkg/controller/expectations.go
+++ pkg/controller/expectations.go
@@ -158,7 +158,9 @@ func panicWithKeyFuncMsg(err error) {
 
 // SetExpectations registers new expectations for the given controller. Forgets existing expectations.
 func (r *ControllerExpectations) SetExpectations(controllerKey string, add, del int) {
-	exp := &ControlleeExpectations{add: int64(add), del: int64(del), key: controllerKey, timestamp: clock.RealClock{}.Now()}
+	exp := &ControlleeExpectations{key: controllerKey, timestamp: clock.RealClock{}.Now()}
+	exp.add.Store(int64(add))
+	exp.del.Store(int64(del))
 	glog.V(4).Infof("Setting expectations %#v", exp)
 	if err := r.Add(exp); err != nil {
 		panicWithKeyFuncMsg(err)
@@ -178,7 +180,7 @@ func (r *ControllerExpectations) LowerExpectations(controllerKey string, add, de
 	if exp, exists, err := r.GetExpectations(controllerKey); err == nil && exists {
 		exp.Add(int64(-add), int64(-del))
 		// The expectations might've been modified since the update on the previous line.
-		glog.V(4).Infof("Lowered expectations %#v", exp)
+		glog.V(4).Infof("Lowered expectations: %s", exp)
 	}
 }
 
@@ -187,7 +189,7 @@ func (r *ControllerExpectations) RaiseExpectations(controllerKey string, add, de
 	if exp, exists, err := r.GetExpectations(controllerKey); err == nil && exists {
 		exp.Add(int64(add), int64(del))
 		// The expectations might've been modified since the update on the previous line.
-		glog.V(4).Infof("Raised expectations %#v", exp)
+		glog.V(4).Infof("Raised expectations: %s", exp)
 	}
 }
 
@@ -200,7 +202,7 @@ func (r *ControllerExpectations) AllPendingCreations() (sum int64) {
 	for _, key := range r.ListKeys() {
 		exp, exists, _ := r.GetExpectations(key)
 		if exists {
-			sum = sum + exp.add
+			sum = sum + exp.add.Load()
 		}
 	}
 	return
@@ -218,29 +220,32 @@ type Expectations interface {
 
 // ControlleeExpectations track controllee creates/deletes.
 type ControlleeExpectations struct {
-	// Important: Since these two int64 fields are using sync/atomic, they have to be at the top of the struct due to a bug on 32-bit platforms
-	// See: https://golang.org/pkg/sync/atomic/ for more information
-	add       int64
-	del       int64
+	add       atomic.Int64
+	del       atomic.Int64
 	key       string
 	timestamp time.Time
 }
 
 // Add increments the add and del counters.
 func (e *ControlleeExpectations) Add(add, del int64) {
-	atomic.AddInt64(&e.add, add)
-	atomic.AddInt64(&e.del, del)
+	e.add.Add(add)
+	e.del.Add(del)
 }
 
 // Fulfilled returns true if this expectation has been fulfilled.
 func (e *ControlleeExpectations) Fulfilled() bool {
 	// TODO: think about why this line being atomic doesn't matter
-	return atomic.LoadInt64(&e.add) <= 0 && atomic.LoadInt64(&e.del) <= 0
+	return e.add.Load() <= 0 && e.del.Load() <= 0
 }
 
 // GetExpectations returns the add and del expectations of the controllee.
 func (e *ControlleeExpectations) GetExpectations() (int64, int64) {
-	return atomic.LoadInt64(&e.add), atomic.LoadInt64(&e.del)
+	return e.add.Load(), e.del.Load()
+}
+
+// String formats the controllee expectations as a string.
+func (e *ControlleeExpectations) String() string {
+	return fmt.Sprintf("key: %s, timestamp: %v, add: %d, del: %d", e.key, e.timestamp, e.add.Load(), e.del.Load())
 }
 
 // NewControllerExpectations returns a store for ControllerExpectations.
