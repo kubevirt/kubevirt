218316b2feee0fd800251a94b824ce9aa09d6f7d
João Vilaça <machadovilaca@gmail.com>
2025-04-15 15:59:39 +0100

    Reduce NodeNetworkInterfaceDown chance for false positives (#3482)
    * Reduce NodeNetworkInterfaceDown chance for false positives
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Refactor pointer conversions in cluster alerts
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    ---------
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- pkg/monitoring/observability/rules/alerts/cluster_alerts.go
+++ pkg/monitoring/observability/rules/alerts/cluster_alerts.go
@@ -23,7 +23,7 @@ func clusterAlerts() []promv1.Rule {
 		{
 			Alert: "HighCPUWorkload",
 			Expr:  intstr.FromString("instance:node_cpu_utilisation:rate1m >= 0.9"),
-			For:   ptr.To(promv1.Duration("5m")),
+			For:   ptr.To[promv1.Duration]("5m"),
 			Annotations: map[string]string{
 				"summary":     "High CPU usage on host {{ $labels.instance }}",
 				"description": "CPU utilization for {{ $labels.instance }} has been above 90% for more than 5 minutes.",
@@ -36,7 +36,7 @@ func clusterAlerts() []promv1.Rule {
 		{
 			Alert: "HAControlPlaneDown",
 			Expr:  intstr.FromString("kube_node_role{role='control-plane'} * on(node) kube_node_status_condition{condition='Ready',status='true'} == 0"),
-			For:   ptr.To(promv1.Duration("5m")),
+			For:   ptr.To[promv1.Duration]("5m"),
 			Annotations: map[string]string{
 				"summary":     "Control plane node {{ $labels.node }} is not ready",
 				"description": "Control plane node {{ $labels.node }} has been not ready for more than 5 minutes.",
@@ -48,8 +48,14 @@ func clusterAlerts() []promv1.Rule {
 		},
 		{
 			Alert: "NodeNetworkInterfaceDown",
-			Expr:  intstr.FromString(fmt.Sprintf("count by (instance) (node_network_up{device!~'%s'} == 0) > 0", strings.Join(ignoredInterfacesForNetworkDown, "|"))),
-			For:   ptr.To(promv1.Duration("5m")),
+			Expr: intstr.FromString(fmt.Sprintf(`count by (instance) (
+					(node_network_flags %% 2) >= 1												# IFF_UP is set
+					and
+					(node_network_flags %% 128) < 64											# IFF_RUNNING is NOT set
+					and
+					on(device) (node_network_flags unless node_network_flags{device=~"%s"})		# Excluding ignored interfaces
+				) > 0`, strings.Join(ignoredInterfacesForNetworkDown, "|"))),
+			For: ptr.To[promv1.Duration]("5m"),
 			Annotations: map[string]string{
 				"summary":     "Network interfaces are down",
 				"description": "{{ $value }} network devices have been down on instance {{ $labels.instance }} for more than 5 minutes.",
