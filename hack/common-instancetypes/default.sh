80b9c66cc4141745732ecf5162310eb1c3ba6cdb
avlitman <alitman@redhat.com>
2025-04-20 18:43:19 +0300

    add license headr to files missing it
    Consistent Copyright should be:
    Copyright by the KubeVirt Authors.
    
    checks only for .sh and .go files.
    
    excluded folders:
    /tools
    /kubevirtci
    /staging
    /vendor
    /pkg/handler-launcher-com
    /pkg/hooks
    /pkg/virt-handler/device-manager/deviceplugin/v1beta1/api.pb.go
    /pkg/vsock/system/v1/system.pb.go
    
    excluded file names:
    generated_mock*.go
    
    Signed-off-by: avlitman <alitman@redhat.com>
    
    fix leftovers
    
    Signed-off-by: avlitman <alitman@redhat.com>

--- hack/common-instancetypes/default.sh
+++ hack/common-instancetypes/default.sh
@@ -1,3 +1,20 @@
+# This file is part of the KubeVirt project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright The KubeVirt Authors.
+#
+
 common_instancetypes_version=${COMMON_INSTANCETYPES_VERSION:-"v1.3.1"}
 cluster_instancetypes_sha256=${CLUSTER_INSTANCETYPES_SHA256:-"5559a22f3e1fabd3c888e440153217f3feb303a267d34fc9e33ad3e28c1b0561"}
 cluster_preferences_sha256=${CLUSTER_PREFERENCES_SHA256:-"5d06b86d92ef82988b615d202a2850e478468b2a8b77c1b68e9c5fb6a167ed01"}
