59cee7a4a1fa7ae6e1607227ca3913927fc86866
Felix Matouschek <fmatouschek@redhat.com>
2025-04-22 17:49:24 +0200

    cleanup(tests): Remove functional kubectl column tests
    These tests are removed because they are already covered by unit tests
    asserting that the expected additional printer columns are present on
    the CRDs generated by virt-operator.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/kubectl_test.go
+++ tests/kubectl_test.go
@@ -1,16 +1,8 @@
 package tests_test
 
 import (
-	"context"
-	"strings"
-	"time"
-
 	"kubevirt.io/kubevirt/tests/clientcmd"
-	"kubevirt.io/kubevirt/tests/console"
 	"kubevirt.io/kubevirt/tests/decorators"
-	"kubevirt.io/kubevirt/tests/framework/kubevirt"
-	. "kubevirt.io/kubevirt/tests/framework/matcher"
-	"kubevirt.io/kubevirt/tests/libmigration"
 	"kubevirt.io/kubevirt/tests/libpod"
 	"kubevirt.io/kubevirt/tests/libvmifact"
 	"kubevirt.io/kubevirt/tests/libvmops"
@@ -18,19 +10,13 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/api/core/v1"
-	"kubevirt.io/client-go/kubecli"
-
-	"kubevirt.io/kubevirt/pkg/libvmi"
 )
 
 var _ = Describe("[sig-compute]oc/kubectl integration", decorators.SigCompute, func() {
-	var (
-		k8sClient, result string
-		err               error
-	)
+	var k8sClient string
+
 	BeforeEach(func() {
 		k8sClient = clientcmd.GetK8sCmdClient()
 		clientcmd.FailIfNoCmd(k8sClient)
@@ -88,137 +74,6 @@ var _ = Describe("[sig-compute]oc/kubectl integration", decorators.SigCompute, f
 			ContainSubstring("metadata	<ObjectMeta>"),
 		))
 		Expect(output).To(ContainSubstring("spec	<Object>"))
-	})
-
-	Describe("[rfe_id:3423][vendor:cnv-qe@redhat.com][level:component]oc/kubectl get vm/vmi tests", func() {
-		var (
-			virtCli kubecli.KubevirtClient
-			vm      *v1.VirtualMachine
-		)
-
-		BeforeEach(func() {
-			virtCli = kubevirt.Client()
-
-			vm = libvmi.NewVirtualMachine(libvmifact.NewCirros(
-				libvmi.WithInterface(libvmi.InterfaceDeviceWithMasqueradeBinding()),
-				libvmi.WithNetwork(v1.DefaultPodNetwork()),
-			), libvmi.WithRunStrategy(v1.RunStrategyAlways))
-			vm, err = virtCli.VirtualMachine(testsuite.GetTestNamespace(vm)).Create(context.Background(), vm, metav1.CreateOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(ThisVM(vm)).WithTimeout(300 * time.Second).WithPolling(time.Second).Should(BeReady())
-		})
-
-		AfterEach(func() {
-			virtCli.VirtualMachine(testsuite.GetTestNamespace(vm)).Delete(context.Background(), vm.Name, metav1.DeleteOptions{})
-		})
-
-		DescribeTable("should verify set of columns for", func(verb, resource string, expectedHeader []string) {
-			result, _, err = clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, verb, resource, vm.Name)
-			// due to issue of kubectl that sometimes doesn't show CRDs on the first try, retry the same command
-			if err != nil {
-				result, _, err = clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, verb, resource, vm.Name)
-			}
-			Expect(err).ToNot(HaveOccurred())
-			Expect(result).ToNot(BeEmpty())
-			resultFields := strings.Fields(result)
-			// Verify that only Header is not present
-			Expect(len(resultFields)).Should(BeNumerically(">", len(expectedHeader)))
-			columnHeaders := resultFields[:len(expectedHeader)]
-			// Verify the generated header is same as expected
-			Expect(columnHeaders).To(Equal(expectedHeader))
-			// Name will be there in all the cases, so verify name
-			Expect(resultFields[len(expectedHeader)]).To(Equal(vm.Name))
-		},
-			Entry("[test_id:3464]virtualmachine", "get", "vm", []string{"NAME", "AGE", "STATUS", "READY"}),
-			Entry("[test_id:3465]virtualmachineinstance", "get", "vmi", []string{"NAME", "AGE", "PHASE", "IP", "NODENAME", "READY"}),
-		)
-
-		DescribeTable("should verify set of wide columns for", func(verb, resource, option string, expectedHeader []string, verifyPos int, expectedData string) {
-
-			result, _, err := clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, verb, resource, vm.Name, "-o", option)
-			// due to issue of kubectl that sometimes doesn't show CRDs on the first try, retry the same command
-			if err != nil {
-				result, _, err = clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, verb, resource, vm.Name, "-o", option)
-			}
-			Expect(err).ToNot(HaveOccurred())
-			Expect(result).ToNot(BeEmpty())
-			resultFields := strings.Fields(result)
-			// Verify that only Header is not present
-			Expect(len(resultFields)).Should(BeNumerically(">", len(expectedHeader)))
-			columnHeaders := resultFields[:len(expectedHeader)]
-			// Verify the generated header is same as expected
-			Expect(columnHeaders).To(Equal(expectedHeader))
-			// Name will be there in all the cases, so verify name
-			Expect(resultFields[len(expectedHeader)]).To(Equal(vm.Name))
-			// Verify one of the wide column output field
-			Expect(resultFields[len(resultFields)-verifyPos]).To(Equal(expectedData))
-
-		},
-			Entry("[test_id:3468]virtualmachine", "get", "vm", "wide", []string{"NAME", "AGE", "STATUS", "READY"}, 1, "True"),
-			Entry("[test_id:3466]virtualmachineinstance", "get", "vmi", "wide", []string{"NAME", "AGE", "PHASE", "IP", "NODENAME", "READY", "LIVE-MIGRATABLE", "PAUSED"}, 1, "True"),
-		)
-
-	})
-
-	Describe("VM instance migration", decorators.RequiresTwoSchedulableNodes, func() {
-		var virtClient kubecli.KubevirtClient
-
-		BeforeEach(func() {
-			virtClient = kubevirt.Client()
-		})
-
-		Context("'kubectl get vmim'", func() {
-			It("print the expected columns and their corresponding values", func() {
-				vmi := libvmifact.NewCirros(
-					libvmi.WithInterface(libvmi.InterfaceDeviceWithMasqueradeBinding()),
-					libvmi.WithNetwork(v1.DefaultPodNetwork()),
-				)
-				By("Starting the VirtualMachineInstance")
-				vmi = libvmops.RunVMIAndExpectLaunch(vmi, libmigration.MigrationWaitTime)
-
-				By("Checking that the VirtualMachineInstance console has expected output")
-				Expect(console.LoginToCirros(vmi)).To(Succeed())
-
-				By("creating the migration")
-				migration := libmigration.New(vmi.Name, vmi.Namespace)
-
-				var migrationCreated *v1.VirtualMachineInstanceMigration
-				By("starting migration")
-				Eventually(func() error {
-					migrationCreated, err = virtClient.VirtualMachineInstanceMigration(migration.Namespace).Create(context.Background(), migration, metav1.CreateOptions{})
-					return err
-				}, libmigration.MigrationWaitTime, 1*time.Second).Should(Succeed(), "migration creation should succeed")
-				migration = migrationCreated
-
-				libmigration.ExpectMigrationToSucceedWithDefaultTimeout(virtClient, migration)
-
-				k8sClient := clientcmd.GetK8sCmdClient()
-				result, _, err := clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, "get", "vmim", migration.Name)
-				// due to issue of kubectl that sometimes doesn't show CRDs on the first try, retry the same command
-				if err != nil {
-					result, _, err = clientcmd.RunCommand(testsuite.GetTestNamespace(nil), k8sClient, "get", "vmim", migration.Name)
-				}
-
-				expectedHeader := []string{"NAME", "PHASE", "VMI"}
-				Expect(err).ToNot(HaveOccurred())
-				Expect(result).ToNot(BeEmpty())
-				resultFields := strings.Fields(result)
-
-				By("Verify that only Header is not present")
-				Expect(len(resultFields)).Should(BeNumerically(">", len(expectedHeader)))
-
-				columnHeaders := resultFields[:len(expectedHeader)]
-				By("Verify the generated header is same as expected")
-				Expect(columnHeaders).To(Equal(expectedHeader))
-
-				By("Verify VMIM name")
-				Expect(resultFields[len(expectedHeader)]).To(Equal(migration.Name), "should match VMIM object name")
-				By("Verify VMIM phase")
-				Expect(resultFields[len(expectedHeader)+1]).To(Equal(string(v1.MigrationSucceeded)), "should have successful state")
-				By("Verify VMI name related to the VMIM")
-				Expect(resultFields[len(expectedHeader)+2]).To(Equal(vmi.Name), "should match the VMI name")
-			})
-		})
 	})
 
 	Describe("oc/kubectl logs", func() {
