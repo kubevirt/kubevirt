e82eb6c05d885c3b4cd7597937113e7cd54d190b
Harshit Gupta <guptaharshit@microsoft.com>
2025-05-08 21:29:32 -0700

    Re-add QUARANTINE label to test
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- tests/vm_test.go
+++ tests/vm_test.go
@@ -519,7 +519,7 @@ var _ = Describe("[rfe_id:1177][crit:medium][vendor:cnv-qe@redhat.com][level:com
 			),
 		)
 
-		It("[test_id:6869]should report an error status when image pull error occurs", decorators.Conformance, decorators.Quarantine, func() {
+		It("[test_id:6869][QUARANTINE]should report an error status when image pull error occurs", decorators.Conformance, decorators.Quarantine, func() {
 			vmi := libvmi.New(
 				libvmi.WithContainerDisk("disk0", "no-such-image"),
 				libvmi.WithResourceMemory("128Mi"),
