0365fe04c865f914e614126b4112e599be532a14
Moshe Yakubov <mosheyaku@gmail.com>
2025-05-13 00:54:43 +0300

    bazel: include //tools/... in test targets
    Update bazel-test.sh to test the entire //tools/... path instead of
    specific subpackages. This ensures coverage of the root tools
    package and tools.go.
    
    Signed-off-by: Moshe Yakubov <mosheyaku@gmail.com>

--- hack/bazel-test.sh
+++ hack/bazel-test.sh
@@ -4,7 +4,7 @@ source hack/common.sh
 source hack/bootstrap.sh
 source hack/config.sh
 
-WHAT=${WHAT:-"//staging/src/kubevirt.io/... //pkg/... //cmd/... //tools/util/... //tools/cache/... //tests/framework/..."}
+WHAT=${WHAT:-"//staging/src/kubevirt.io/... //pkg/... //cmd/... //tools/... //tests/framework/..."}
 rm -rf ${ARTIFACTS}/junit ${ARTIFACTS}/testlogs
 
 if [ "${CI}" == "true" ]; then
