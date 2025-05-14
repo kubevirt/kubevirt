kubevirt/kubevirt

commit 9436b9a38f65631a43a0baa8ed4614ff9a48e1cf

tests, utils: Move DomXML functs to a new package
Move the two last functions of tests.utils to a new home
and delete the file.

DomainXML functions are still widely in use by many tests.
Introduce a new package libdomain to host library functions
that handle common domain aspects for tests.

Signed-off-by: Nir Dothan <ndothan@redhat.com>

tests/hotplug/BUILD.bazel
@@ -21,13 +21,13 @@ go_library(
         "//staging/src/kubevirt.io/api/instancetype/v1beta1:go_default_library",
         "//staging/src/kubevirt.io/api/migrations/v1alpha1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
-        "//tests:go_default_library",
         "//tests/console:go_default_library",
         "//tests/decorators:go_default_library",
         "//tests/flags:go_default_library",
         "//tests/framework/checks:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libkubevirt:go_default_library",
         "//tests/libkubevirt/config:go_default_library",
         "//tests/libmigration:go_default_library",

tests/BUILD.bazel
@@ -1,22 +1,4 @@
-load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
-
-go_library(
-    name = "go_default_library",
-    srcs = ["utils.go"],
-    importpath = "kubevirt.io/kubevirt/tests",
-    visibility = ["//visibility:public"],
-    deps = [
-        "//pkg/util:go_default_library",
-        "//pkg/virt-launcher/virtwrap/api:go_default_library",
-        "//staging/src/kubevirt.io/api/core/v1:go_default_library",
-        "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
-        "//tests/exec:go_default_library",
-        "//tests/framework/kubevirt:go_default_library",
-        "//tests/libpod:go_default_library",
-        "//vendor/k8s.io/apimachinery/pkg/apis/meta/v1:go_default_library",
-    ],
-)
-
+load("@io_bazel_rules_go//go:def.bzl", "go_test")
 load(
     "@io_bazel_rules_docker//container:container.bzl",
     "container_image",
@@ -105,7 +87,6 @@ go_test(
     embedsrcs = ["testdata/sidecar-hook-configmap.sh"],
     visibility = ["//visibility:public"],
     deps = [
-        ":go_default_library",
         "//pkg/apimachinery/patch:go_default_library",
         "//pkg/apimachinery/wait:go_default_library",
         "//pkg/cloud-init:go_default_library",
@@ -164,6 +145,7 @@ go_test(
         "//tests/instancetype:go_default_library",
         "//tests/launchsecurity:go_default_library",
         "//tests/libconfigmap:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libinfra:go_default_library",
         "//tests/libinstancetype:go_default_library",
         "//tests/libkubevirt:go_default_library",

tests/compute/BUILD.bazel
@@ -20,11 +20,11 @@ go_library(
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubevirt/typed/core/v1:go_default_library",
-        "//tests:go_default_library",
         "//tests/console:go_default_library",
         "//tests/decorators:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libkubevirt:go_default_library",
         "//tests/libkubevirt/config:go_default_library",
         "//tests/libnet/cloudinit:go_default_library",

tests/launchsecurity/BUILD.bazel
@@ -10,12 +10,12 @@ go_library(
         "//pkg/virt-config/featuregate:go_default_library",
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
-        "//tests:go_default_library",
         "//tests/console:go_default_library",
         "//tests/decorators:go_default_library",
         "//tests/exec:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libkubevirt/config:go_default_library",
         "//tests/libnode:go_default_library",
         "//tests/libpod:go_default_library",

tests/libdomain/BUILD.bazel
@@ -1,0 +1,18 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+    name = "go_default_library",
+    srcs = ["domain.go"],
+    importpath = "kubevirt.io/kubevirt/tests/libdomain",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/util:go_default_library",
+        "//pkg/virt-launcher/virtwrap/api:go_default_library",
+        "//staging/src/kubevirt.io/api/core/v1:go_default_library",
+        "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
+        "//tests/exec:go_default_library",
+        "//tests/framework/kubevirt:go_default_library",
+        "//tests/libpod:go_default_library",
+        "//vendor/k8s.io/apimachinery/pkg/apis/meta/v1:go_default_library",
+    ],
+)

tests/migration/BUILD.bazel
@@ -38,7 +38,6 @@ go_library(
         "//staging/src/kubevirt.io/api/migrations/v1alpha1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
         "//staging/src/kubevirt.io/client-go/log:go_default_library",
-        "//tests:go_default_library",
         "//tests/console:go_default_library",
         "//tests/containerdisk:go_default_library",
         "//tests/decorators:go_default_library",
@@ -50,6 +49,7 @@ go_library(
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
         "//tests/libconfigmap:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libinfra:go_default_library",
         "//tests/libkubevirt:go_default_library",
         "//tests/libkubevirt/config:go_default_library",

tests/network/BUILD.bazel
@@ -43,7 +43,6 @@ go_library(
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
         "//staging/src/kubevirt.io/client-go/log:go_default_library",
-        "//tests:go_default_library",
         "//tests/clientcmd:go_default_library",
         "//tests/console:go_default_library",
         "//tests/decorators:go_default_library",
@@ -52,6 +51,7 @@ go_library(
         "//tests/framework/checks:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libkubevirt/config:go_default_library",
         "//tests/libmigration:go_default_library",
         "//tests/libnet:go_default_library",

tests/numa/BUILD.bazel
@@ -8,11 +8,11 @@ go_library(
     deps = [
         "//staging/src/kubevirt.io/api/core/v1:go_default_library",
         "//staging/src/kubevirt.io/client-go/kubecli:go_default_library",
-        "//tests:go_default_library",
         "//tests/console:go_default_library",
         "//tests/decorators:go_default_library",
         "//tests/exec:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
+        "//tests/libdomain:go_default_library",
         "//tests/libnode:go_default_library",
         "//tests/libvmifact:go_default_library",
         "//tests/libwait:go_default_library",

commit 7a681efa1fc0a1204aef2563cffb197b1ce9f109

instancetype: Add functional coverage for bug #14595
NOTE(lyarwood): Conflict caused by
9436b9a38f65631a43a0baa8ed4614ff9a48e1cf not being present on
release-1.5

Conflicts:
    tests/hotplug/BUILD.bazel

Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

tests/hotplug/BUILD.bazel
@@ -28,6 +28,7 @@ go_library(
         "//tests/framework/checks:go_default_library",
         "//tests/framework/kubevirt:go_default_library",
         "//tests/framework/matcher:go_default_library",
+        "//tests/libinstancetype/builder:go_default_library",
         "//tests/libkubevirt:go_default_library",
         "//tests/libkubevirt/config:go_default_library",
         "//tests/libmigration:go_default_library",

tests/hotplug/instancetype.go
@@ -6,6 +6,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	gomegatypes "github.com/onsi/gomega/types"
 
 	k8sv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -23,6 +24,7 @@ import (
 	"kubevirt.io/kubevirt/tests/decorators"
 	"kubevirt.io/kubevirt/tests/framework/kubevirt"
 	. "kubevirt.io/kubevirt/tests/framework/matcher"
+	"kubevirt.io/kubevirt/tests/libinstancetype/builder"
 	"kubevirt.io/kubevirt/tests/libkubevirt"
 	"kubevirt.io/kubevirt/tests/libkubevirt/config"
 	"kubevirt.io/kubevirt/tests/libmigration"
@@ -181,5 +183,110 @@ var _ = Describe("[sig-compute]Instance Type and Preference Hotplug", decorators
 	},
 		Entry("with maxGuest and maxSockets defined", true),
 		Entry("without maxGuest and maxSockets defined", false),
+	)
+
+	DescribeTable("should reject live update when preference requirements are no longer met - bug #14595",
+		func(
+			createVM func(*virtv1.VirtualMachineInstance, string) (*virtv1.VirtualMachine, error),
+			updateVM func(*virtv1.VirtualMachine) (*virtv1.VirtualMachine, error),
+			errMatcher gomegatypes.GomegaMatcher,
+		) {
+			vmi := libvmifact.NewAlpine(
+				libvmi.WithResourceMemory("1Gi"),
+				libvmi.WithNamespace(testsuite.GetTestNamespace(nil)),
+			)
+
+			preference := builder.NewPreference()
+			preference.Spec.Requirements = &instancetypev1beta1.PreferenceRequirements{
+				Memory: &instancetypev1beta1.MemoryPreferenceRequirement{
+					Guest: resource.MustParse("1Gi"),
+				},
+			}
+			preference, err := virtClient.VirtualMachinePreference(vmi.Namespace).Create(
+				context.Background(), preference, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+
+			vm, err := createVM(vmi, preference.Name)
+			Expect(err).ToNot(HaveOccurred())
+
+			_, err = updateVM(vm)
+			Expect(err).To(MatchError(errMatcher))
+		},
+		Entry("by VirtualMachine resource requests",
+			func(vmi *virtv1.VirtualMachineInstance, preferenceName string) (*virtv1.VirtualMachine, error) {
+				return virtClient.VirtualMachine(vmi.Namespace).Create(
+					context.Background(),
+					libvmi.NewVirtualMachine(vmi, libvmi.WithPreference(preferenceName)),
+					metav1.CreateOptions{},
+				)
+			},
+			func(vm *virtv1.VirtualMachine) (*virtv1.VirtualMachine, error) {
+				newMemory := vm.Spec.Template.Spec.Domain.Resources.Requests.Memory().DeepCopy()
+				newMemory.Sub(resource.MustParse("512Mi"))
+
+				const memoryPath = "/spec/template/spec/domain/resources/requests/memory"
+				patches := patch.New(
+					patch.WithTest(memoryPath, vm.Spec.Template.Spec.Domain.Resources.Requests.Memory().String()),
+					patch.WithReplace(memoryPath, newMemory.String()),
+				)
+				patchData, err := patches.GeneratePayload()
+				Expect(err).NotTo(HaveOccurred())
+				return virtClient.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, patchData, metav1.PatchOptions{})
+			},
+			ContainSubstring("failure checking preference requirements: insufficient Memory resources of 512Mi provided by VirtualMachine, preference requires 1Gi"),
+		),
+		Entry("by instance type resource requests",
+			func(vmi *virtv1.VirtualMachineInstance, preferenceName string) (*virtv1.VirtualMachine, error) {
+				origInstancetype, err := virtClient.VirtualMachineInstancetype(vmi.Namespace).Create(
+					context.Background(),
+					builder.NewInstancetypeFromVMI(vmi),
+					metav1.CreateOptions{},
+				)
+				Expect(err).ToNot(HaveOccurred())
+
+				vm, err := virtClient.VirtualMachine(vmi.Namespace).Create(
+					context.Background(),
+					libvmi.NewVirtualMachine(
+						vmi,
+						libvmi.WithInstancetype(origInstancetype.Name),
+						libvmi.WithPreference(preferenceName),
+					),
+					metav1.CreateOptions{})
+				Expect(err).ToNot(HaveOccurred())
+
+				// bug #14595 was caused by lookup code using the stashed controller
+				// revision copy of a different object during live update so wait until
+				// these are created before asserting that we fail to live update
+				By("Waiting for VirtualMachineInstancetype and VirtualMachinePreference ControllerRevision to be referenced from the VirtualMachine")
+				Eventually(ThisVM(vm)).WithTimeout(1 * time.Minute).WithPolling(2 * time.Second).Should(HaveControllerRevisionRefs())
+				return vm, nil
+			},
+			func(vm *virtv1.VirtualMachine) (*virtv1.VirtualMachine, error) {
+				origInstancetype, err := virtClient.VirtualMachineInstancetype(vm.Namespace).Get(
+					context.Background(), vm.Spec.Instancetype.Name, metav1.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+
+				newMemory := origInstancetype.Spec.Memory.Guest.DeepCopy()
+				newMemory.Sub(resource.MustParse("512Mi"))
+				newInstancetype, err := virtClient.VirtualMachineInstancetype(vm.Namespace).Create(
+					context.Background(),
+					builder.NewInstancetype(
+						builder.WithCPUs(1),
+						builder.WithMemory(newMemory.String()),
+					),
+					metav1.CreateOptions{},
+				)
+				Expect(err).ToNot(HaveOccurred())
+
+				patches := patch.New(
+					patch.WithTest("/spec/instancetype/name", origInstancetype.Name),
+					patch.WithReplace("/spec/instancetype/name", newInstancetype.Name),
+				)
+				patchData, err := patches.GeneratePayload()
+				Expect(err).NotTo(HaveOccurred())
+				return virtClient.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, patchData, metav1.PatchOptions{})
+			},
+			ContainSubstring("failure checking preference requirements: insufficient Memory resources of 512Mi provided by instance type, preference requires 1Gi"),
+		),
 	)
 })