187d7760423272862037ae105a1daa6e076ce5d3
Maksim Fedotov <maksim.fedotov@flant.com>
2025-05-20 10:30:32 +0300

    Apply patch 025-stream-graceful-shutdown.patch
    Graceful termination of websocket connection for serial console and vnc connections.

--- pkg/virt-handler/rest/console.go
+++ pkg/virt-handler/rest/console.go
@@ -43,6 +43,8 @@ import (
 
 	"kubevirt.io/kubevirt/pkg/util"
 	"kubevirt.io/kubevirt/pkg/virt-handler/isolation"
+
+  "github.com/gorilla/websocket"
 )
 
 //const failedRetrieveVMI = "Failed to retrieve VMI"
@@ -326,7 +328,7 @@ func (t *ConsoleHandler) stream(vmi *v1.VirtualMachineInstance, request *restful
 
 	select {
 	case <-stopCh:
-		break
+		clientSocket.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseGoingAway, "close by another connection"))
 	case err := <-errCh:
 		if err != nil && err != io.EOF {
 			log.Log.Object(vmi).Reason(err).Error("Error in proxing websocket and unix socket")
