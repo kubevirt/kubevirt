a9c740164f040e7330c3a95063a5c16a3c0940df
Maksim Fedotov <maksim.fedotov@flant.com>
2025-05-20 10:30:34 +0300

    Apply patch 044-disable-workload-updater.patch
    This patch disables controller workload-updater in kubevirt.
    We have our implementation in virtualization-controller.

--- pkg/virt-controller/watch/application.go
+++ pkg/virt-controller/watch/application.go
@@ -91,10 +91,10 @@ import (
 )
 
 const (
-	defaultPort = 8182
+	defaultPort        = 8182
 	defaultMetricsPort = 8080
 
-	defaultHost = "0.0.0.0"
+	defaultHost        = "0.0.0.0"
 	defaultMetricsHost = defaultHost
 
 	launcherImage       = "virt-launcher"
@@ -446,7 +446,8 @@ func Execute() {
 	app.initSnapshotController()
 	app.initRestoreController()
 	app.initExportController()
-	app.initWorkloadUpdaterController()
+	// Disable workload-updater
+	// app.initWorkloadUpdaterController()
 	app.initCloneController()
 	go app.Run()
 
@@ -589,7 +590,8 @@ func (vca *VirtControllerApp) onStartedLeading() func(ctx context.Context) {
 				log.Log.Warningf("error running the export controller: %v", err)
 			}
 		}()
-		go vca.workloadUpdateController.Run(stop)
+		// Disable workloadUpdateController
+		// go vca.workloadUpdateController.Run(stop)
 		go vca.nodeTopologyUpdater.Run(vca.nodeTopologyUpdatePeriod, stop)
 		go func() {
 			if err := vca.vmCloneController.Run(vca.cloneControllerThreads, stop); err != nil {
