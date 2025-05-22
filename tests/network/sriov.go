dc61087ec4590a5811ab03ea91dcd076487ecb0a
Or Shoval <oshoval@redhat.com>
2025-05-22 15:05:24 +0300

    e2e,migration,sriov: Increase status update timeout
    Once migration finishes, virt-handler sends sriov reattach request to virt-launcher.
    Sometimes the downwardAPI file (/etc/podinfo/network-info)
    takes time to be created, hence the first reattach request fails.
    
    By simulating errors on the virt-launcher and not letting
    virt-launcher reattach finish, we can see that the next request
    of reattach might arrive even up to 2m after the first one.
    
    Since we wait in the test, after migration finished 2m10s, this is very marginal,
    because beside the above non synchronic event,
    also guest-agent that already started running, has a backoff
    that already accumulated, (10 seconds initial, step 2x, up to 120s)
    and the report of it is crucial (we need the guest interface name).
    
    It can cause 2 problems:
    
    The interface wasn't reattached yet, since it takes time for the 2nd reattach request
    to be triggered.
    Guest agent didn't update yet all the required info.
    Both fail the test.
    
    Follow-up PRs can address this either by
    
    Investigating deeper why it happens, and fixing the synchronization between the
    virt-handler / virt-launcher pods wrt reattach command.
    Move the reattach logic to virt-launcher SyncVMI,
    which is synchronic and deterministic already.
    Option [2] is better, it also makes the flow simpler, and reduces inter pod communication.
    
    This PR extends the timeout, and removes the test from quarantine,
    meanwhile to solve the flake.
    
    Note: tried to run in loops with extended timeout, and the flow eventually successes.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/network/sriov.go
+++ tests/network/sriov.go
@@ -280,7 +280,7 @@ var _ = Describe(SIG("SRIOV", Serial, decorators.SRIOV, func() {
 
 				// It may take some time for the VMI interface status to be updated with the information reported by
 				// the guest-agent.
-				ifaceName, err := findIfaceByMAC(virtClient, vmi, mac, 2*time.Minute+10*time.Second)
+				ifaceName, err := findIfaceByMAC(virtClient, vmi, mac, 5*time.Minute)
 				Expect(err).NotTo(HaveOccurred())
 				updatedVMI, err := virtClient.VirtualMachineInstance(vmi.Namespace).Get(context.Background(), vmi.Name, k8smetav1.GetOptions{})
 				Expect(err).NotTo(HaveOccurred())
