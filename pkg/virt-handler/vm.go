876a798c53f46d0859d4fad050e239b276936429
Luboslav Pivarc <lpivarc@redhat.com>
2025-05-22 14:05:35 +0200

    Isolation detector: Remove unused field
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -290,10 +290,10 @@ type netBindingPluginMemoryCalculator interface {
 }
 
 type VirtualMachineController struct {
-	recorder                 record.EventRecorder
-	clientset                kubecli.KubevirtClient
-	host                     string
-	migrationIpAddress       string
+	recorder           record.EventRecorder
+	clientset          kubecli.KubevirtClient
+	host               string
+	migrationIpAddress string
 	virtShareDir             string
 	virtPrivateDir           string
 	queue                    workqueue.TypedRateLimitingInterface[string]
