c5c31e68f0675296373abf4513d2367541ba9a73
Shelly Kagan <skagan@redhat.com>
2025-05-27 09:57:48 +0300

    Backup subresource: add backup subresource to VMI
    Add subresource and define the path to pass the
    backup request.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- pkg/virt-api/api.go
+++ pkg/virt-api/api.go
@@ -50,6 +50,7 @@ import (
 
 	"kubevirt.io/kubevirt/pkg/util/ratelimiter"
 
+	backupv1 "kubevirt.io/api/backup/v1alpha1"
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
 	"kubevirt.io/client-go/log"
@@ -633,6 +634,17 @@ func (app *virtAPIApp) composeSubresources() {
 			Returns(http.StatusBadRequest, httpStatusBadRequestMessage, "").
 			Returns(http.StatusInternalServerError, httpStatusInternalServerError, ""))
 
+		subws.Route(subws.PUT(definitions.NamespacedResourcePath(subresourcesvmiGVR)+definitions.SubResourcePath("backup")).
+			To(subresourceApp.BackupVMIRequestHandler).
+			Consumes(mime.MIME_ANY).
+			Reads(backupv1.BackupOptions{}).
+			Param(definitions.NamespaceParam(subws)).Param(definitions.NameParam(subws)).
+			Operation(version.Version+"Backup").
+			Doc("Initiate a VirtualMachineInstance backup.").
+			Returns(http.StatusOK, "OK", "").
+			Returns(http.StatusNotFound, httpStatusNotFoundMessage, "").
+			Returns(http.StatusBadRequest, httpStatusBadRequestMessage, ""))
+
 		// Return empty api resource list.
 		// K8s expects to be able to retrieve a resource list for each aggregated
 		// app in order to discover what resources it provides. Without returning
@@ -665,6 +677,10 @@ func (app *virtAPIApp) composeSubresources() {
 					},
 					{
 						Name:       "virtualmachineinstances/portforward",
+						Namespaced: true,
+					},
+					{
+						Name:       "virtualmachineinstances/backup",
 						Namespaced: true,
 					},
 					{
