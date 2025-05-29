f55d9ae977c7156accc51f9a970d4b7ce9526c30
Shelly Kagan <skagan@redhat.com>
2025-05-29 15:41:34 +0300

    tests/storage/cbt add apply of cbt e2e tests
    Add functional test to check cbt being applied,
    removed or keep integrity during actions such as
    restart and migration.
    Modifying the KubeVirt CR to include a default label selector cbt: true.
    This change allows the cbt tests to run without needing
    to patch the KubeVirt CR within the tests themselves,
    which requires the tests to be executed serially.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- tests/testsuite/kubevirtresource.go
+++ tests/testsuite/kubevirtresource.go
@@ -38,6 +38,7 @@ import (
 	v1 "kubevirt.io/api/core/v1"
 
 	"kubevirt.io/kubevirt/pkg/pointer"
+	"kubevirt.io/kubevirt/pkg/storage/cbt"
 	"kubevirt.io/kubevirt/pkg/virt-config/featuregate"
 	"kubevirt.io/kubevirt/tests/flags"
 	"kubevirt.io/kubevirt/tests/framework/checks"
@@ -121,6 +122,14 @@ func AdjustKubeVirtResource() {
 		featuregate.NodeRestrictionGate,
 		featuregate.DecentralizedLiveMigration,
 	)
+	kv.Spec.Configuration.ChangedBlockTrackingLabelSelectors = &v1.ChangedBlockTrackingSelectors{
+		VirtualMachineLabelSelector: &metav1.LabelSelector{
+			MatchLabels: cbt.CBTLabel,
+		},
+		NamespaceLabelSelector: &metav1.LabelSelector{
+			MatchLabels: cbt.CBTLabel,
+		},
+	}
 
 	storageClass, exists := libstorage.GetVMStateStorageClass()
 	if exists {
