7973b76914a31cb66c6c3d48a5903f073e1af82b
Shelly Kagan <skagan@redhat.com>
2025-05-29 16:09:00 +0300

    Handle backend pvc in case of cbt
    * when cbt enabled create vmi with Initializing phase
    * when VMI has initializing phase backend storage is
    marked as needed and so backend storage pvc will be created
    * cbt mounting point will be added to the virt launcher pod
    * in case vm doesnt match the cbt label selector and doesnt
    have persistent features the backend storage pvc will be deleted.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- pkg/virt-controller/services/rendervolumes.go
+++ pkg/virt-controller/services/rendervolumes.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	backendstorage "kubevirt.io/kubevirt/pkg/storage/backend-storage"
+	"kubevirt.io/kubevirt/pkg/storage/backup"
 	"kubevirt.io/kubevirt/pkg/tpm"
 
 	k8sv1 "k8s.io/api/core/v1"
@@ -458,6 +459,15 @@ func withBackendStorage(vmi *v1.VirtualMachineInstance, backendStoragePVCName st
 				ReadOnly:  false,
 				MountPath: PathForNVram(vmi),
 				SubPath:   "nvram",
+			})
+		}
+
+		if backup.HasCBTEnabled(vmi.Status.ChangedBlockTracking) {
+			renderer.podVolumeMounts = append(renderer.podVolumeMounts, k8sv1.VolumeMount{
+				Name:      volumeName,
+				ReadOnly:  false,
+				MountPath: backup.PathForCBT(vmi),
+				SubPath:   "cbt",
 			})
 		}
 
