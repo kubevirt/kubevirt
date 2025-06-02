c13b44599e4468e3379316e480a8315f5be2e576
Luboslav Pivarc <lpivarc@redhat.com>
2025-06-02 14:53:14 +0200

    Remove e2e: [test_id:TODO]Should be set in VirtualMachineInstance
    There is "should set defaults for input devices" unit test
    in pkg/virt-api/webhooks/mutating-webhook/mutators/vmi-mutator_test.go.
    
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- tests/compute/vmidefaults.go
+++ tests/compute/vmidefaults.go
@@ -220,24 +220,6 @@ var _ = Describe(SIG("VMIDefaults", func() {
 
 	Context("Input defaults", func() {
 
-		It("[test_id:TODO]Should be set in VirtualMachineInstance", func() {
-
-			By("Creating a VirtualMachineInstance with an input device without a bus or type set")
-			vmi = libvmifact.NewCirros()
-			vmi.Spec.Domain.Devices.Inputs = append(vmi.Spec.Domain.Devices.Inputs, v1.Input{
-				Name: "foo-1",
-			})
-
-			vmi, err := virtClient.VirtualMachineInstance(testsuite.GetTestNamespace(nil)).Create(context.Background(), vmi, metav1.CreateOptions{})
-			Expect(err).ToNot(HaveOccurred())
-
-			Expect(vmi.Spec.Domain.Devices.Inputs).ToNot(BeEmpty(), "There should be input devices")
-			Expect(vmi.Spec.Domain.Devices.Inputs[0].Name).To(Equal("foo-1"))
-			Expect(vmi.Spec.Domain.Devices.Inputs[0].Type).To(Equal(v1.InputTypeTablet))
-			Expect(vmi.Spec.Domain.Devices.Inputs[0].Bus).To(Equal(v1.InputBusUSB))
-
-		})
-
 		It("[test_id:TODO]Should be applied to a device added by AutoattachInputDevice", func() {
 			By("Creating a VirtualMachine with AutoattachInputDevice enabled")
 			vm := libvmi.NewVirtualMachine(libvmifact.NewCirros(), libvmi.WithRunStrategy(v1.RunStrategyAlways))
