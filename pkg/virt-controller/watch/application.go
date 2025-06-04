65460586b6a0910eb2b70098de627240a3790183
Orel Misan <omisan@redhat.com>
2025-06-04 16:21:17 +0300

    net, migration: Introduce Evaluator
    Add the logic that is responsible for deciding which type of automatic
    migration is required.
    
    There are three possible outputs for the migration evaluator:
    1. Automatic migration is not required.
    2. Automatic migration is required.
    3. Automatic migration is pending.
    
    An automatic migration is required in case at least one SR-IOV secondary
    interface was hotplugged.
    
    In case at lease one secondary interface using bridge binding was
    hot-plugged or hot-unplugged, a pending request is made.
    This is done in order to allow a grace period for the
    dynamic-networks-controller to perform the in-place hotplug / hotunplug.
    
    In case the grace period is over without noticing a change made by the
    dynamic-networks-controller, a migration will be required.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/virt-controller/watch/application.go
+++ pkg/virt-controller/watch/application.go
@@ -96,6 +96,7 @@ import (
 
 	netadmitter "kubevirt.io/kubevirt/pkg/network/admitter"
 	netcontrollers "kubevirt.io/kubevirt/pkg/network/controllers"
+	netmigration "kubevirt.io/kubevirt/pkg/network/migration"
 	"kubevirt.io/kubevirt/pkg/network/netbinding"
 	netannotations "kubevirt.io/kubevirt/pkg/network/pod/annotations"
 	storageannotations "kubevirt.io/kubevirt/pkg/storage/pod/annotations"
@@ -671,7 +672,7 @@ func (vca *VirtControllerApp) initCommon() {
 		func(field *k8sfield.Path, vmiSpec *v1.VirtualMachineInstanceSpec, clusterCfg *virtconfig.ClusterConfig) []metav1.StatusCause {
 			return netadmitter.ValidateCreation(field, vmiSpec, clusterCfg)
 		},
-		stubMigrationEvaluator{},
+		netmigration.NewEvaluator(),
 	)
 	if err != nil {
 		panic(err)
@@ -1062,10 +1063,4 @@ func (vca *VirtControllerApp) setupLeaderElector() (err error) {
 		})
 
 	return
-}
-
-type stubMigrationEvaluator struct{}
-
-func (e stubMigrationEvaluator) Evaluate(_ *v1.VirtualMachineInstance) k8sv1.ConditionStatus {
-	return k8sv1.ConditionUnknown
 }
