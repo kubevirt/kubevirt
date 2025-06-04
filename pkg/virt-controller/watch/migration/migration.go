3390cd6ee26c087b0fa6c1a640d20b707d25e495
Orel Misan <omisan@redhat.com>
2025-06-04 16:34:47 +0300

    migration ctrl: Wait for MigrationRequired to be removed
    Declare the migration as successful only after the `MigrationRequired`
    condition was removed.
    
    This logic is implemented similarly to the existing CPU and memory
    hotplug flows.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/virt-controller/watch/migration/migration.go
+++ pkg/virt-controller/watch/migration/migration.go
@@ -681,7 +681,8 @@ func (c *Controller) processMigrationPhase(
 
 		if vmi.Status.MigrationState.Completed &&
 			!vmiConditionManager.HasCondition(vmi, virtv1.VirtualMachineInstanceVCPUChange) &&
-			!vmiConditionManager.HasConditionWithStatus(vmi, virtv1.VirtualMachineInstanceMemoryChange, k8sv1.ConditionTrue) {
+			!vmiConditionManager.HasConditionWithStatus(vmi, virtv1.VirtualMachineInstanceMemoryChange, k8sv1.ConditionTrue) &&
+			!vmiConditionManager.HasConditionWithStatus(vmi, virtv1.VirtualMachineInstanceMigrationRequired, k8sv1.ConditionTrue) {
 			migrationCopy.Status.Phase = virtv1.MigrationSucceeded
 			c.recorder.Eventf(migration, k8sv1.EventTypeNormal, controller.SuccessfulMigrationReason, "Source node reported migration succeeded")
 			log.Log.Object(migration).Infof("VMI reported migration succeeded.")
