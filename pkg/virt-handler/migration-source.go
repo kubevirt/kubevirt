0868b75e639137198575f34bd18ceac368a2ddec
fossedihelm <ffossemo@redhat.com>
2025-06-25 17:29:22 +0200

    Fix possible nil pointer during upgrades
    // UPGRADE PATH IS
    1. daemonsets - ensures all compute nodes are updated to handle new features
    2. wait for daemonsets to roll over
    3. controllers - ensures control plane is ready for new features
    4. wait for controllers to roll over
    5. apiserver - toggles on new features.
    
    If a migration is triggered from a node where the handler has been updates,
    but the controller has not been updated yet, a nil pointer exception is raised.
    
    This because the older migration controller set the vmi.Status.MigrationState.MigrationConfiguration without specifying the AllowWorkloadDisruption field.
    At that point, the upgraded virt-handler tries to start the migration, but when
    it tries to specify the migration options that should be used, the nil pointer is
    raised since we try to read a nil *bool.
    
    Handle that field properly, checking its existence, and, in case, set the default value.
    
    Added a unit test that could be used to check if the handler will be broken when a
    migration field is added.
    In order to do so, it is enough, not to change the migrationConfiguration and run the test.
    If something is not properly handled, a panic will be raised.
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- pkg/virt-handler/migration-source.go
+++ pkg/virt-handler/migration-source.go
@@ -508,9 +508,19 @@ func (c *MigrationSourceController) migrateVMI(vmi *v1.VirtualMachineInstance, d
 		return fmt.Errorf("failed to handle migration proxy: %v", err)
 	}
 
-	migrationConfiguration := vmi.Status.MigrationState.MigrationConfiguration
-	if migrationConfiguration == nil {
+	var migrationConfiguration *v1.MigrationConfiguration
+	if vmi.Status.MigrationState.MigrationConfiguration == nil {
 		migrationConfiguration = c.clusterConfig.GetMigrationConfiguration()
+	} else {
+		migrationConfiguration = vmi.Status.MigrationState.MigrationConfiguration.DeepCopy()
+	}
+
+	// This check is only for backward compatibility.
+	// During upgrade, AllowWorkloadDisruption could be nil since the migration controller is
+	// updated later the virt-handler.
+	// This check can be removed in future
+	if migrationConfiguration.AllowWorkloadDisruption == nil {
+		migrationConfiguration.AllowWorkloadDisruption = pointer.P(*migrationConfiguration.AllowPostCopy)
 	}
 
 	options := &cmdclient.MigrationOptions{
