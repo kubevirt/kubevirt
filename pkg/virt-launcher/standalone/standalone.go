f384404e1be0ca849647d8a33b3e0e4f13e980b1
Vladik Romanovsky <vromanso@redhat.com>
2025-07-24 16:10:39 -0400

    Add YAML support for STANDALONE_VMI in HandleStandaloneMode
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- pkg/virt-launcher/standalone/standalone.go
+++ pkg/virt-launcher/standalone/standalone.go
@@ -6,6 +6,7 @@ import (
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
+	"sigs.k8s.io/yaml"
 
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap"
 )
@@ -14,9 +15,13 @@ import (
 func HandleStandaloneMode(domainManager virtwrap.DomainManager) {
 	if vmiObjStr, ok := os.LookupEnv("STANDALONE_VMI"); ok {
 		var vmi v1.VirtualMachineInstance
-		if err := json.Unmarshal([]byte(vmiObjStr), &vmi); err != nil {
-			log.Log.Reason(err).Error("Failed to unmarshal VMI from STANDALONE_VMI")
-			panic(err)
+		// Try YAML unmarshal
+		if err := yaml.Unmarshal([]byte(vmiObjStr), &vmi); err != nil {
+			// Fallback to JSON if YAML fails
+			if jsonErr := json.Unmarshal([]byte(vmiObjStr), &vmi); jsonErr != nil {
+				log.Log.Reason(err).Error("Failed to unmarshal VMI from STANDALONE_VMI as YAML/JSON")
+				panic(err)
+			}
 		}
 
 		if _, err := domainManager.SyncVMI(&vmi, true, nil); err != nil {
--- pkg/virt-launcher/standalone/standalone_test.go
+++ pkg/virt-launcher/standalone/standalone_test.go
@@ -1,7 +1,6 @@
 package standalone_test
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 
@@ -9,20 +8,20 @@ import (
 	. "github.com/onsi/gomega"
 	"go.uber.org/mock/gomock"
 
-	v1 "kubevirt.io/api/core/v1"
-
 	"kubevirt.io/kubevirt/pkg/virt-launcher/standalone"
+	virtwrap "kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap"
+	// Import for DomainSpec
 )
 
 var _ = Describe("HandleStandaloneMode", func() {
 	var (
 		mockCtrl *gomock.Controller
-		mockDM   *mockDomainManager
+		mockDM   *virtwrap.MockDomainManager
 	)
 
 	BeforeEach(func() {
 		mockCtrl = gomock.NewController(GinkgoT())
-		mockDM = &mockDomainManager{mockCtrl: mockCtrl}
+		mockDM = virtwrap.NewMockDomainManager(mockCtrl)
 	})
 
 	AfterEach(func() {
@@ -35,8 +34,8 @@ var _ = Describe("HandleStandaloneMode", func() {
 	})
 
 	It("should panic on invalid JSON in STANDALONE_VMI", func() {
-		os.Setenv("VMI_OBJ", "invalid json")
-		defer os.Unsetenv("VMI_OBJ")
+		os.Setenv("STANDALONE_VMI", "invalid json")
+		defer os.Unsetenv("STANDALONE_VMI")
 
 		Expect(func() {
 			standalone.HandleStandaloneMode(mockDM)
@@ -48,11 +47,11 @@ var _ = Describe("HandleStandaloneMode", func() {
 		os.Setenv("STANDALONE_VMI", vmiJSON)
 		defer os.Unsetenv("STANDALONE_VMI")
 
-		mockDM.EXPECT().SyncVMI(gomock.Any(), true, nil).Return(true, fmt.Errorf("sync error"))
+		mockDM.EXPECT().SyncVMI(gomock.Any(), true, nil).Return(nil, fmt.Errorf("sync error"))
 
 		Expect(func() {
 			standalone.HandleStandaloneMode(mockDM)
-		}).To(PanicWith("sync error"))
+		}).To(PanicWith(MatchError(ContainSubstring("sync error"))))
 	})
 
 	It("should succeed with valid JSON and successful SyncVMI", func() {
@@ -60,18 +59,34 @@ var _ = Describe("HandleStandaloneMode", func() {
 		os.Setenv("STANDALONE_VMI", vmiJSON)
 		defer os.Unsetenv("STANDALONE_VMI")
 
-		mockDM.EXPECT().SyncVMI(gomock.Any(), true, nil).Return(true, nil)
+		mockDM.EXPECT().SyncVMI(gomock.Any(), true, nil).Return(nil, nil)
 
 		Expect(func() {
 			standalone.HandleStandaloneMode(mockDM)
 		}).NotTo(Panic())
 	})
+
+	It("should succeed with valid YAML and successful SyncVMI", func() {
+		vmiYAML := `apiVersion: kubevirt.io/v1
+kind: VirtualMachineInstance
+metadata:
+  name: testvmi-yaml`
+		os.Setenv("STANDALONE_VMI", vmiYAML)
+		defer os.Unsetenv("STANDALONE_VMI")
+
+		mockDM.EXPECT().SyncVMI(gomock.Any(), true, nil).Return(nil, nil)
+
+		Expect(func() {
+			standalone.HandleStandaloneMode(mockDM)
+		}).NotTo(Panic())
+	})
+
+	It("should panic on invalid YAML in STANDALONE_VMI", func() {
+		os.Setenv("STANDALONE_VMI", "invalid: yaml: here")
+		defer os.Unsetenv("STANDALONE_VMI")
+
+		Expect(func() {
+			standalone.HandleStandaloneMode(mockDM)
+		}).To(Panic())
+	})
 })
-
-type mockDomainManager struct {
-	mockCtrl *gomock.Controller
-}
-
-func (m *mockDomainManager) SyncVMI(vmi *v1.VirtualMachineInstance, allowEmulation bool, secretUUID *string) (bool, error) {
-	return true, nil
-}
