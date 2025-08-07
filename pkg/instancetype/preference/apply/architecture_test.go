36a7860e05ddb6ab7fd39c56b426eb43e668a1ef
Lee Yarwood <lyarwood@redhat.com>
2025-08-07 14:59:20 +0100

    preference: Wire up PreferredArchitecture
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

---
+++ pkg/instancetype/preference/apply/architecture_test.go
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ */
+
+package apply_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	k8sfield "k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/utils/ptr"
+
+	virtv1 "kubevirt.io/api/core/v1"
+	v1beta1 "kubevirt.io/api/instancetype/v1beta1"
+
+	"kubevirt.io/kubevirt/pkg/instancetype/apply"
+	"kubevirt.io/kubevirt/pkg/libvmi"
+)
+
+var _ = Describe("Preference.Architecture", func() {
+	var (
+		vmi              *virtv1.VirtualMachineInstance
+		instancetypeSpec *v1beta1.VirtualMachineInstancetypeSpec
+		preferenceSpec   *v1beta1.VirtualMachinePreferenceSpec
+
+		field      = k8sfield.NewPath("spec", "template", "spec")
+		vmiApplier = apply.NewVMIApplier()
+	)
+
+	BeforeEach(func() {
+		vmi = libvmi.New()
+	})
+
+	It("should apply preferred architecture to VMI when architecture is not set", func() {
+		preferenceSpec = &v1beta1.VirtualMachinePreferenceSpec{
+			PreferredArchitecture: ptr.To("arm64"),
+		}
+
+		Expect(vmiApplier.ApplyToVMI(field, instancetypeSpec, preferenceSpec, &vmi.Spec, &vmi.ObjectMeta)).To(Succeed())
+
+		Expect(vmi.Spec.Architecture).To(Equal("arm64"))
+	})
+
+	It("should not override existing architecture in VMI", func() {
+		vmi.Spec.Architecture = "amd64"
+		preferenceSpec = &v1beta1.VirtualMachinePreferenceSpec{
+			PreferredArchitecture: ptr.To("arm64"),
+		}
+
+		Expect(vmiApplier.ApplyToVMI(field, instancetypeSpec, preferenceSpec, &vmi.Spec, &vmi.ObjectMeta)).To(Succeed())
+
+		Expect(vmi.Spec.Architecture).To(Equal("amd64"))
+	})
+
+	It("should not apply when PreferredArchitecture is nil", func() {
+		preferenceSpec = &v1beta1.VirtualMachinePreferenceSpec{}
+
+		Expect(vmiApplier.ApplyToVMI(field, instancetypeSpec, preferenceSpec, &vmi.Spec, &vmi.ObjectMeta)).To(Succeed())
+
+		Expect(vmi.Spec.Architecture).To(BeEmpty())
+	})
+
+	It("should not apply when PreferredArchitecture is empty string", func() {
+		preferenceSpec = &v1beta1.VirtualMachinePreferenceSpec{
+			PreferredArchitecture: ptr.To(""),
+		}
+
+		Expect(vmiApplier.ApplyToVMI(field, instancetypeSpec, preferenceSpec, &vmi.Spec, &vmi.ObjectMeta)).To(Succeed())
+
+		Expect(vmi.Spec.Architecture).To(BeEmpty())
+	})
+})
