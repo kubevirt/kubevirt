52047fd0f2421addede76e760a5a91599331c841
Nir Dothan <ndothan@redhat.com>
2025-08-19 15:25:15 +0300

    hack, rpms: Bump passt
    Contains a bug fix of for disconnection during migration.
    https://passt.top/passt/commit/?id=a8782865c342eb2682cca292d5bf92b567344351
    
    Note: Only hack/rpm-deps.sh was edited manually.
    The rest of the files were auto generated by make rpm-deps.
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="77d642ee15e65ed110376cc0900e213b45e89938"
+sandbox_hash="eab71e50c4707073b60efdb6404e3a8838f17e83"
 
 function kubevirt::bootstrap::regenerate() {
     (
