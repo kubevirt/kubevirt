f3f9c85ef821e075fa25abd030d08e02413b9a52
fossedihelm <ffossemo@redhat.com>
2025-09-02 15:41:59 +0200

    device plugin: use base device plugin in generic device
    This will reduce code duplication
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- pkg/virt-handler/device-manager/generic_device.go
+++ pkg/virt-handler/device-manager/generic_device.go
@@ -21,17 +21,13 @@ package device_manager
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net"
 	"os"
-	"path"
-	"path/filepath"
 	"strconv"
 	"sync"
 	"time"
 
-	"github.com/fsnotify/fsnotify"
 	"google.golang.org/grpc"
 
 	"kubevirt.io/client-go/log"
@@ -55,37 +51,27 @@ type Device interface {
 }
 
 type GenericDevicePlugin struct {
-	devs         []*pluginapi.Device
-	server       *grpc.Server
-	socketPath   string
-	stop         <-chan struct{}
-	health       chan deviceHealth
-	devicePath   string
-	deviceName   string
-	resourceName string
-	done         chan struct{}
-	deviceRoot   string
-	preOpen      bool
-	initialized  bool
-	lock         *sync.Mutex
-	permissions  string
-	deregistered chan struct{}
+	*DevicePluginBase
+	preOpen     bool
+	permissions string
 }
 
 func NewGenericDevicePlugin(deviceName string, devicePath string, maxDevices int, permissions string, preOpen bool) *GenericDevicePlugin {
 	serverSock := SocketPath(deviceName)
 	dpi := &GenericDevicePlugin{
-		devs:         []*pluginapi.Device{},
-		socketPath:   serverSock,
-		health:       make(chan deviceHealth),
-		deviceName:   deviceName,
-		devicePath:   devicePath,
-		deviceRoot:   util.HostRootMount,
-		resourceName: fmt.Sprintf("%s/%s", DeviceNamespace, deviceName),
-		preOpen:      preOpen,
-		initialized:  false,
-		lock:         &sync.Mutex{},
-		permissions:  permissions,
+		DevicePluginBase: &DevicePluginBase{
+			devs:         []*pluginapi.Device{},
+			socketPath:   serverSock,
+			health:       make(chan deviceHealth),
+			deviceName:   deviceName,
+			devicePath:   devicePath,
+			deviceRoot:   util.HostRootMount,
+			resourceName: fmt.Sprintf("%s/%s", DeviceNamespace, deviceName),
+			initialized:  false,
+			lock:         &sync.Mutex{},
+		},
+		preOpen:     preOpen,
+		permissions: permissions,
 	}
 
 	for i := 0; i < maxDevices; i++ {
@@ -162,190 +148,8 @@ func (dpi *GenericDevicePlugin) Start(stop <-chan struct{}) (err error) {
 	return err
 }
 
-// Stop stops the gRPC server
-func (dpi *GenericDevicePlugin) stopDevicePlugin() error {
-	defer func() {
-		if !IsChanClosed(dpi.done) {
-			close(dpi.done)
-		}
-	}()
-
-	// Give the device plugin one second to properly deregister
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-	select {
-	case <-dpi.deregistered:
-	case <-ticker.C:
-	}
-	dpi.server.Stop()
-	dpi.setInitialized(false)
-	return dpi.cleanup()
-}
-
-// Register registers the device plugin for the given resourceName with Kubelet.
-func (dpi *GenericDevicePlugin) register() error {
-	conn, err := gRPCConnect(pluginapi.KubeletSocket, connectionTimeout)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := pluginapi.NewRegistrationClient(conn)
-	reqt := &pluginapi.RegisterRequest{
-		Version:      pluginapi.Version,
-		Endpoint:     path.Base(dpi.socketPath),
-		ResourceName: dpi.resourceName,
-	}
-
-	_, err = client.Register(context.Background(), reqt)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (dpi *GenericDevicePlugin) ListAndWatch(e *pluginapi.Empty, s pluginapi.DevicePlugin_ListAndWatchServer) error {
-	s.Send(&pluginapi.ListAndWatchResponse{Devices: dpi.devs})
-
-	done := false
-	for {
-		select {
-		case devHealth := <-dpi.health:
-			// There's only one shared generic device
-			// so update each plugin device to reflect overall device health
-			for _, dev := range dpi.devs {
-				dev.Health = devHealth.Health
-			}
-			s.Send(&pluginapi.ListAndWatchResponse{Devices: dpi.devs})
-		case <-dpi.stop:
-			done = true
-		case <-dpi.done:
-			done = true
-		}
-		if done {
-			break
-		}
-	}
-	// Send empty list to increase the chance that the kubelet acts fast on stopped device plugins
-	// There exists no explicit way to deregister devices
-	emptyList := []*pluginapi.Device{}
-	if err := s.Send(&pluginapi.ListAndWatchResponse{Devices: emptyList}); err != nil {
-		log.DefaultLogger().Reason(err).Infof("%s device plugin failed to deregister", dpi.deviceName)
-	}
-	close(dpi.deregistered)
-	return nil
-}
-
 func (dpi *GenericDevicePlugin) Allocate(ctx context.Context, r *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
-	log.DefaultLogger().Infof("Generic Allocate: resourceName: %s", dpi.deviceName)
-	log.DefaultLogger().Infof("Generic Allocate: request: %v", r.ContainerRequests)
-	response := pluginapi.AllocateResponse{}
-	containerResponse := new(pluginapi.ContainerAllocateResponse)
-
-	dev := new(pluginapi.DeviceSpec)
-	dev.HostPath = dpi.devicePath
-	dev.ContainerPath = dpi.devicePath
-	dev.Permissions = dpi.permissions
-	containerResponse.Devices = []*pluginapi.DeviceSpec{dev}
-
-	response.ContainerResponses = []*pluginapi.ContainerAllocateResponse{containerResponse}
-
-	return &response, nil
-}
-
-func (dpi *GenericDevicePlugin) cleanup() error {
-	if err := os.Remove(dpi.socketPath); err != nil && !errors.Is(err, os.ErrNotExist) {
-		return err
-	}
-
-	return nil
-}
-
-func (dpi *GenericDevicePlugin) GetDevicePluginOptions(_ context.Context, _ *pluginapi.Empty) (*pluginapi.DevicePluginOptions, error) {
-	options := &pluginapi.DevicePluginOptions{
-		PreStartRequired: false,
-	}
-	return options, nil
-}
-
-func (dpi *GenericDevicePlugin) PreStartContainer(_ context.Context, _ *pluginapi.PreStartContainerRequest) (*pluginapi.PreStartContainerResponse, error) {
-	res := &pluginapi.PreStartContainerResponse{}
-	return res, nil
-}
-
-func (dpi *GenericDevicePlugin) healthCheck() error {
-	logger := log.DefaultLogger()
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return fmt.Errorf("failed to creating a fsnotify watcher: %v", err)
-	}
-	defer watcher.Close()
-
-	// This way we don't have to mount /dev from the node
-	devicePath := filepath.Join(dpi.deviceRoot, dpi.devicePath)
-
-	// Start watching the files before we check for their existence to avoid races
-	dirName := filepath.Dir(devicePath)
-	err = watcher.Add(dirName)
-
-	if err != nil {
-		return fmt.Errorf("failed to add the device root path to the watcher: %v", err)
-	}
-
-	_, err = os.Stat(devicePath)
-	if err != nil {
-		if !errors.Is(err, os.ErrNotExist) {
-			return fmt.Errorf("could not stat the device: %v", err)
-		}
-		logger.Warningf("device '%s' is not present, the device plugin can't expose it.", dpi.devicePath)
-		dpi.health <- deviceHealth{Health: pluginapi.Unhealthy}
-	}
-	logger.Infof("device '%s' is present.", dpi.devicePath)
-
-	dirName = filepath.Dir(dpi.socketPath)
-	err = watcher.Add(dirName)
-
-	if err != nil {
-		return fmt.Errorf("failed to add the device-plugin kubelet path to the watcher: %v", err)
-	}
-	_, err = os.Stat(dpi.socketPath)
-	if err != nil {
-		return fmt.Errorf("failed to stat the device-plugin socket: %v", err)
-	}
-
-	for {
-		select {
-		case <-dpi.stop:
-			return nil
-		case err := <-watcher.Errors:
-			logger.Reason(err).Errorf("error watching devices and device plugin directory")
-		case event := <-watcher.Events:
-			logger.V(4).Infof("health Event: %v", event)
-			if event.Name == devicePath {
-				// Health in this case is if the device path actually exists
-				if event.Op == fsnotify.Create {
-					logger.Infof("monitored device %s appeared", dpi.deviceName)
-					dpi.health <- deviceHealth{Health: pluginapi.Healthy}
-				} else if (event.Op == fsnotify.Remove) || (event.Op == fsnotify.Rename) {
-					logger.Infof("monitored device %s disappeared", dpi.deviceName)
-					dpi.health <- deviceHealth{Health: pluginapi.Unhealthy}
-				}
-			} else if event.Name == dpi.socketPath && event.Op == fsnotify.Remove {
-				logger.Infof("device socket file for device %s was removed, kubelet probably restarted.", dpi.deviceName)
-				return nil
-			}
-		}
-	}
-}
-
-func (dpi *GenericDevicePlugin) GetInitialized() bool {
-	dpi.lock.Lock()
-	defer dpi.lock.Unlock()
-	return dpi.initialized
-}
-
-func (dpi *GenericDevicePlugin) setInitialized(initialized bool) {
-	dpi.lock.Lock()
-	defer dpi.lock.Unlock()
-	dpi.initialized = initialized
+	ar, err := dpi.DevicePluginBase.Allocate(ctx, r)
+	ar.ContainerResponses[0].Devices[0].Permissions = dpi.permissions
+	return ar, err
 }
