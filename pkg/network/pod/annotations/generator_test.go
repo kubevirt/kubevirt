9383c410bfddc200ae979a1f35ab88fdf304ceab
Ananya Banerjee <anbanerj@redhat.com>
2025-09-03 15:08:22 +0200

    net, generator: Preserve ext entries in `k8s.v1.cni.cncf.io/networks`
    1. Adds two helper funcs:
    
        1. parseAnnotation func:
        To preserve the external entries to `k8s.v1.cni.cncf.io/networks`
        annotation we need to compare the existing annotation on the pod to
        the annotation kubevirt is about to overwrite. These annotations are
        of type string. The `parseAnnotation` func will help convert them to
        arrays for easier comparison.
    
        2. makeLookupMap func:
        We want to identify which items of `k8s.v1.cni.cncf.io/networks`
        are owned by kubevirt so as not to overwite the ones owned by
        external entities. (name+namespace) serves as a unique identifier
        to spot which are the netselectors corresponding to the interfaces
        of the vmi.
    
    2. Updates generateMultusAnnotation func:
        Splits te func into four parts:
        1. Get the current annotations
        2. Decide which items of this annotation are owned by kubevirt.
        (They are the nw selectors of multus interfaces of the vmi). The
        identifier here is the combination of name and namespace of the
        items.
        3. Copy the items owned by kubevirt to a new var, preserving the
        fields that are not owned by kubevirt. Copy the items added by
        external entities as it is.
        4. Compare this new var against the original annotation and
        return it if there is a diff.
    
    3. Add a unit test to see if the external entry (istio-cni in this case)
    is being preserved.
    
    Signed-off-by: Ananya Banerjee <anbanerj@redhat.com>

--- pkg/network/pod/annotations/generator_test.go
+++ pkg/network/pod/annotations/generator_test.go
@@ -467,6 +467,10 @@ var _ = Describe("Annotations Generator", func() {
 				`{"name":"some-net","interface":"podb1f51a511f1","mac":"8a:37:d9:e7:0f:18","dns":{}}` +
 				`{"name":"other-net","interface":"pod16477688c0e","mac":"25:bb:e2:a3:e8:4d","dns":{}}` +
 				`]`
+
+			multusNetworksAnnotationWithPasstAndIstio = `[` +
+				`{"name":"netbindingpasst","namespace":"default","cni-args":{"logicNetworkName":"default"}},` +
+				`{"name": "istio-cni", "namespace": "default"}]`
 		)
 
 		It("Should not generate network attachment annotation when there are no networks", func() {
@@ -697,6 +701,31 @@ var _ = Describe("Annotations Generator", func() {
 			annotations := generator.GenerateFromActivePod(vmi, newStubVirtLauncherPod(vmi, podAnnotations))
 
 			Expect(annotations).To(HaveKeyWithValue(networkv1.NetworkAttachmentAnnot, ""))
+		})
+
+		It("Should preserve added elements added by external entities", func() {
+			vmi := libvmi.New(
+				libvmi.WithNamespace(testNamespace),
+				libvmi.WithInterface(libvmi.InterfaceWithPasstBindingPlugin()),
+				libvmi.WithNetwork(v1.DefaultPodNetwork()),
+			)
+
+			podAnnotations := map[string]string{
+				networkv1.NetworkAttachmentAnnot: multusNetworksAnnotationWithPasstAndIstio,
+				networkv1.NetworkStatusAnnot:     multusNetworkStatusWithPrimaryNet,
+			}
+
+			generator := annotations.NewGenerator(stubClusterConfig{
+				registeredPlugins: map[string]v1.InterfaceBindingPlugin{
+					"passt": {
+						SidecarImage:                "passt:latest",
+						NetworkAttachmentDefinition: "default/netbindingpasst",
+					},
+				},
+			})
+			annotations := generator.GenerateFromActivePod(vmi, newStubVirtLauncherPod(vmi, podAnnotations))
+
+			Expect(annotations).ToNot(HaveKey(networkv1.NetworkAttachmentAnnot))
 		})
 	})
 })
