0307863efd1588d772b3ac7c30bb8d3b6871e207
fossedihelm <ffossemo@redhat.com>
2025-09-15 12:34:03 +0200

    migration ctrl: use the priority field
    The priority field of the VMIM instruct the controller
    on which migration should be picked first.
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- pkg/util/migrations/migrations.go
+++ pkg/util/migrations/migrations.go
@@ -15,8 +15,12 @@ import (
 const CancelMigrationFailedVmiNotMigratingErr = "failed to cancel migration - vmi is not migrating"
 
 const (
-	PriorityRunning int = 1000
-	PriorityPending int = -100
+	QueuePriorityRunning           int = 1000
+	QueuePrioritySystemCritical    int = 100
+	QueuePriorityUserTriggered     int = 50
+	QueuePrioritySystemMaintenance int = 20
+	QueuePriorityDefault           int = 0
+	QueuePriorityPending           int = -100
 )
 
 func ListUnfinishedMigrations(indexer cache.Indexer) []*v1.VirtualMachineInstanceMigration {
@@ -135,4 +139,20 @@ func ActiveMigrationExistsForVMI(migrationIndexer cache.Indexer, vmi *v1.Virtual
 		}
 	}
 	return false, nil
+}
+
+func PriorityFromMigration(migration *v1.VirtualMachineInstanceMigration) int {
+	if migration.Spec.Priority == nil {
+		return QueuePriorityDefault
+	}
+	switch *migration.Spec.Priority {
+	case v1.PrioritySystemCritical:
+		return QueuePrioritySystemCritical
+	case v1.PriorityUserTriggered:
+		return QueuePriorityUserTriggered
+	case v1.PrioritySystemMaintenance:
+		return QueuePrioritySystemMaintenance
+	default:
+		return QueuePriorityDefault
+	}
 }
