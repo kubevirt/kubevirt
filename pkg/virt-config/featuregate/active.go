48c05c167ce4dd0d30d16806050d10aed10091ba
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-09-16 22:10:50 +0200

    feat(feature-gates): add HyperVLayered gate (#23)
    Signed-off-by: Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>

--- pkg/virt-config/featuregate/active.go
+++ pkg/virt-config/featuregate/active.go
@@ -128,6 +128,12 @@ const (
 	//
 	// PasstIPStackMigration enables seamless migration with passt network binding.
 	PasstIPStackMigration = "PasstIPStackMigration"
+
+	// Owner: @DariuszPorowski
+	// Alpha: v1.?.0
+	// This feature is disabled by default. When enabled, it allows setting specific Hyper-V
+	// fields on the VMI spec which are otherwise restricted.
+	HyperVLayered = "HyperVLayered"
 )
 
 func init() {
@@ -161,4 +167,5 @@ func init() {
 	RegisterFeatureGate(FeatureGate{Name: VideoConfig, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: PanicDevicesGate, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: PasstIPStackMigration, State: Alpha})
+	RegisterFeatureGate(FeatureGate{Name: HyperVLayered, State: Alpha})
 }
