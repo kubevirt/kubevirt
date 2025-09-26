73f405f47629e4bf5a218fdefb845b32a8bc4576
svarnam <svarnam@nvidia.com>
2025-09-26 13:37:25 +0530

    Add API support for auto-healing strategy Signed-off-by: svarnam <svarnam@nvidia.com>

--- pkg/virt-operator/resource/generate/components/validations_generated.go
+++ pkg/virt-operator/resource/generate/components/validations_generated.go
@@ -20271,6 +20271,10 @@ var CRDsValidation map[string]string = map[string]string{
       type: object
     spec:
       properties:
+        autohealing:
+          description: Autohealing specifies when a VMpool should replace a failing
+            VM with a reprovisioned instance
+          type: boolean
         maxUnavailable:
           anyOf:
           - type: integer
