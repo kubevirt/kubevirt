611d4ed08b797cfe937834196e1a0fa01ee2cb7c
Lee Yarwood <lyarwood@redhat.com>
2025-10-02 18:45:49 +0100

    reporter: Add lsfd --summary and ulimit -a output
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- tests/reporter/kubernetes.go
+++ tests/reporter/kubernetes.go
@@ -1512,6 +1512,8 @@ func (r *KubernetesReporter) executeNodeCommands(virtCli kubecli.KubevirtClient,
 		{command: networkPrefix + bridgeJVlanShow, fileNameSuffix: "brvlan"},
 		{command: networkPrefix + bridgeFdb, fileNameSuffix: "brfdb"},
 		{command: networkPrefix + "nft list ruleset", fileNameSuffix: "nftlist"},
+		{command: "lsfd --summary", fileNameSuffix: "lsfd-summary"},
+		{command: "ulimit -a", fileNameSuffix: "ulimit-a"},
 	}
 
 	if checks.IsRunningOnKindInfra() {
