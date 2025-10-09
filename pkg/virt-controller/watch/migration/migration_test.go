1e30e94abdbec33d2a79a95a0f29cfbb9bc84e1c
Luboslav Pivarc <lpivarc@redhat.com>
2025-10-09 15:01:01 +0200

    Migration unit: Remove sprintf
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/virt-controller/watch/migration/migration_test.go
+++ pkg/virt-controller/watch/migration/migration_test.go
@@ -725,7 +725,7 @@ var _ = Describe("Migration watcher", func() {
 
 			sanityExecute()
 
-			expectPodDoesNotExist(vmi.Namespace, fmt.Sprintf("testvmi"), "testmigration")
+			expectPodDoesNotExist(vmi.Namespace, "testvmi", "testmigration")
 		})
 
 		It("should not overload the cluster and detect pending migrations as running if they have a target pod", func() {
@@ -761,7 +761,7 @@ var _ = Describe("Migration watcher", func() {
 
 			sanityExecute()
 
-			expectPodDoesNotExist(vmi.Namespace, fmt.Sprintf("testvmi"), "testmigration")
+			expectPodDoesNotExist(vmi.Namespace, "testvmi", "testmigration")
 		})
 
 		It("should create another target pods if there is only one outbound migration on the node", func() {
