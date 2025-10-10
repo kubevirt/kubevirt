17a46881bafa81775e4563d7f129f75d1785c96f
Felix Matouschek <fmatouschek@redhat.com>
2025-10-10 15:59:18 +0200

    fix(virt-handler): Correctly set up test suites
    Correctly set up the test suites for filewatcher and multipath-monitor,
    so failing tests are listed in the created test reports.
    
    Conflicts:
      pkg/virt-handler/multipath-monitor/BUILD.bazel
    
    Note: The conflicts were due to differences in the imported
    dependencies in the bazel file.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- pkg/virt-handler/multipath-monitor/multipath_monitor_suite_test.go
+++ pkg/virt-handler/multipath-monitor/multipath_monitor_suite_test.go
@@ -3,11 +3,9 @@ package multipath_monitor_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"kubevirt.io/client-go/testutils"
 )
 
 func TestMultipathMonitor(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "MultipathMonitor Suite")
+	testutils.KubeVirtTestSuiteSetup(t)
 }
