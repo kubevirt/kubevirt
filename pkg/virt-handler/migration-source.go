7e5a913a83e9686df83315b0c5119444c0089404
fossedihelm <ffossemo@redhat.com>
2025-10-15 18:09:49 +0200

    fix: do not override startTimestamp
    The startTimestamp addition should be an atomic op.
    Avoid the possibility of overriding it.
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- pkg/virt-handler/migration-source.go
+++ pkg/virt-handler/migration-source.go
@@ -186,7 +186,9 @@ func (c *MigrationSourceController) setMigrationProgressStatus(vmi *v1.VirtualMa
 	if migrationMetadata.UID != vmi.Status.MigrationState.MigrationUID {
 		return
 	}
-	vmi.Status.MigrationState.StartTimestamp = migrationMetadata.StartTimestamp
+	if migrationMetadata.StartTimestamp != nil {
+		vmi.Status.MigrationState.StartTimestamp = migrationMetadata.StartTimestamp
+	}
 
 	vmi.Status.MigrationState.Failed = migrationMetadata.Failed
 
