f529059d14198f602f42c55beaf4c2cf8a0d68a2
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-10-17 09:06:14 +0200

    net, downward api: Extend NetworkInfo with device Mac address
    Users of the network deviceinfo downwardAPI might need mac address
    information as well.
    
    vDPA devices are one of those cases. If these are assigned a mac address
    on creation, those can't be updated, at least from the virt-launcher pod
    nor qemu command line. Another case could be sriov virtual functions in
    legacy eswitch mode that were configured with spoofcheck enabled.
    
    Network binding plugins, or network-info consumers, will need to fill
    the correct mac address in the domainXML to avoid assigning a random mac
    address to those devices. That usually leads to broken traffic.
    
    This commit extends the downwardapi's networkinfo structure, then fills
    it with the mac address obtained from the multus network status
    annotations.
    
    On simple terms, this commit extends the Interface struct on
    pkg/network/downwardapi/types.go, which directly affects the NetworkInfo
    struct passed to network binding plugins through the downwardAPI.
    
    To do so, it extends the the Interface struct in network/downwardapi
    which affects the NetworkInfo struct passed to network binding plugins
    by downwardAPI. Then, it extends CreateNetworkInfoAnnotationValue and
    generateNetworkInfo to accept network<->macaddr maps as function inputs
    and fills the NetworkInfo with the correct data.
    
    Finally, it updates some of the tests, and adds some more to cover this
    new behavior.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- pkg/network/pod/annotations/generator.go
+++ pkg/network/pod/annotations/generator.go
@@ -165,12 +165,16 @@ func (g Generator) generateDeviceInfoAnnotation(vmi *v1.VirtualMachineInstance, 
 		return iface.SRIOV != nil || vmispec.HasBindingPluginDeviceInfo(iface, g.clusterConfigurer.GetNetworkBindings())
 	})
 
-	networkDeviceInfoMap := deviceinfo.MapNetworkNameToDeviceInfo(vmi.Spec.Networks, ifaces, multus.NetworkStatusesFromPod(pod))
-	if len(networkDeviceInfoMap) == 0 {
+	multusNetworkStatuses := multus.NetworkStatusesFromPod(pod)
+
+	networkDeviceInfoMap := deviceinfo.MapNetworkNameToDeviceInfo(vmi.Spec.Networks, ifaces, multusNetworkStatuses)
+
+	networkDeviceMacAddressMap := deviceinfo.MapNetworkNameToDeviceMacAddress(vmi.Spec.Networks, ifaces, multusNetworkStatuses)
+	if len(networkDeviceInfoMap) == 0 && len(networkDeviceMacAddressMap) == 0 {
 		return ""
 	}
 
-	return downwardapi.CreateNetworkInfoAnnotationValue(networkDeviceInfoMap)
+	return downwardapi.CreateNetworkInfoAnnotationValue(networkDeviceInfoMap, networkDeviceMacAddressMap)
 }
 
 func shouldAddIstioKubeVirtAnnotation(vmi *v1.VirtualMachineInstance) bool {
