49b87029452d33578374798628e3b8b49c63a29a
Shelly Kagan <skagan@redhat.com>
2025-10-21 20:07:05 +0300

    Add utility volumes to VMI spec
    This new utility volumes type defines hot-pluggable volumes mounted
    directly into the `virt-launcher` Pod, not exposed to the VM guest,
    ensuring clear separation from guest-facing volumes in `spec.volumes`.
    This volumes will be used to store data/output recieved from the
    virt-launcher pod such as memory dump and future vm backup output.
    Memory dump process should move to using utility volumes rather then the
    memorydumpvolumesource which will be deprecated in the future.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- staging/src/kubevirt.io/api/core/v1/types_swagger_generated.go
+++ staging/src/kubevirt.io/api/core/v1/types_swagger_generated.go
@@ -40,6 +40,7 @@ func (VirtualMachineInstanceSpec) SwaggerDoc() map[string]string {
 		"accessCredentials":             "Specifies a set of public keys to inject into the vm guest\n+listType=atomic\n+optional\n+kubebuilder:validation:MaxItems:=256",
 		"architecture":                  "Specifies the architecture of the vm guest you are attempting to run. Defaults to the compiled architecture of the KubeVirt components",
 		"resourceClaims":                "ResourceClaims define which ResourceClaims must be allocated\nand reserved before the VMI, hence virt-launcher pod is allowed to start. The resources\nwill be made available to the domain which consumes them\nby name.\n\nThis is an alpha field and requires enabling the\nDynamicResourceAllocation feature gate in kubernetes\n https://kubernetes.io/docs/concepts/scheduling-eviction/dynamic-resource-allocation/\nThis field should only be configured if one of the feature-gates GPUsWithDRA or HostDevicesWithDRA is enabled.\nThis feature is in alpha.\n\n+listType=map\n+listMapKey=name\n+optional",
+		"utilityVolumes":                "List of utility volumes that can be mounted to the vmi virt-launcher pod\nwithout having a matching disk in the domain.\nUsed to collect data for various operational workflows.\n+kubebuilder:validation:MaxItems:=256\n+listType=map\n+listMapKey=name\n+optional",
 	}
 }
 
