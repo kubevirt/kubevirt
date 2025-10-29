d1df05b14e3728fc5892d752bf54966f588d4b74
Jed Lejosne <jed@redhat.com>
2025-10-29 13:19:31 -0400

    vmi-admitter: fail if memory limits < hugepages size
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    (cherry picked from commit f73df3fbf16da218670ddf2d7ce0e27b68648a86)
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    (cherry picked from commit 7df59e054a253c411694d225935c33b280f38239)
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
@@ -836,6 +836,31 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			Expect(causes).To(HaveLen(1))
 			Expect(causes[0].Field).To(Equal("fake.domain.resources.requests.memory"))
 		})
+
+		It("should reject greater hugepages.size than guest memory", func() {
+			vmi.Spec.Domain.Memory = &v1.Memory{
+				Guest:     pointer.P(resource.MustParse("64Mi")),
+				Hugepages: &v1.Hugepages{},
+			}
+			vmi.Spec.Domain.Memory.Hugepages.PageSize = "1Gi"
+
+			causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
+			Expect(causes).To(HaveLen(1))
+			Expect(causes[0].Field).To(Equal("fake.domain.resources.requests.memory"))
+		})
+
+		It("should reject greater hugepages.size than requests.limit", func() {
+			vmi.Spec.Domain.Resources.Limits = k8sv1.ResourceList{
+				k8sv1.ResourceMemory: resource.MustParse("64Mi"),
+			}
+			vmi.Spec.Domain.Memory = &v1.Memory{Hugepages: &v1.Hugepages{}}
+			vmi.Spec.Domain.Memory.Hugepages.PageSize = "1Gi"
+
+			causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
+			Expect(causes).To(HaveLen(1))
+			Expect(causes[0].Field).To(Equal("fake.domain.resources.requests.memory"))
+		})
+
 		It("should allow smaller guest memory than requested memory", func() {
 			vmi := api.NewMinimalVMI("testvmi")
 			guestMemory := resource.MustParse("1Mi")
