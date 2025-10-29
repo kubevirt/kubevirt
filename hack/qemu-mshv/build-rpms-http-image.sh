4424b09ccf84d4e5e610da635b1390b9429e4ca1
Harshit Gupta <harshitgupta1337@gmail.com>
2025-10-29 11:08:24 -0400

    Set correct version in QEMU RPM build-info and avoid silent failure of make rpm-deps (#85)
    Co-authored-by: Harshit Gupta <guptaharshit@microsoft.com>
    Signed-off by: Harshit Gupta <guptaharshit@microsoft.com>

--- hack/qemu-mshv/build-rpms-http-image.sh
+++ hack/qemu-mshv/build-rpms-http-image.sh
@@ -2,21 +2,21 @@
 
 # Write an argument parser for REGISTRY, IMAGE_NAME, RPM_IMAGE_TAG using getopts
 while getopts r:i:t:d: flag; do
-	case "${flag}" in
-	r) REGISTRY=${OPTARG} ;;
-	i) IMAGE_NAME=${OPTARG} ;;
-	t) RPM_IMAGE_TAG=${OPTARG} ;;
-	d) RPMS_DIR=${OPTARG} ;;
-	*)
-		echo "Invalid option"
-		exit 1
-		;;
-	esac
+    case "${flag}" in
+    r) REGISTRY=${OPTARG} ;;
+    i) IMAGE_NAME=${OPTARG} ;;
+    t) RPM_IMAGE_TAG=${OPTARG} ;;
+    d) RPMS_DIR=${OPTARG} ;;
+    *)
+        echo "Invalid option"
+        exit 1
+        ;;
+    esac
 done
 
 if [ -z "$REGISTRY" ] || [ -z "$IMAGE_NAME" ] || [ -z "$RPM_IMAGE_TAG" ] || [ -z "$RPMS_DIR" ]; then
-	echo "Usage: $0 -r <REGISTRY> -i <IMAGE_NAME> -t <RPM_IMAGE_TAG> -d <RPMS_DIR>"
-	exit 1
+    echo "Usage: $0 -r <REGISTRY> -i <IMAGE_NAME> -t <RPM_IMAGE_TAG> -d <RPMS_DIR>"
+    exit 1
 fi
 
 # Create Dockerfile for RPM distribution
