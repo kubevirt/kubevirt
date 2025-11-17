cd583788c8d9f41b93f07b906239ca4741c0dd8e
Jed Lejosne <jed@redhat.com>
2025-11-17 13:58:31 -0500

    vsock: graduate to beta
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-config/featuregate/active.go
+++ pkg/virt-config/featuregate/active.go
@@ -170,7 +170,7 @@ func init() {
 	RegisterFeatureGate(FeatureGate{Name: Root, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: WorkloadEncryptionSEV, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: WorkloadEncryptionTDX, State: Alpha})
-	RegisterFeatureGate(FeatureGate{Name: VSOCKGate, State: Alpha})
+	RegisterFeatureGate(FeatureGate{Name: VSOCKGate, State: Beta})
 	RegisterFeatureGate(FeatureGate{Name: KubevirtSeccompProfile, State: Beta})
 	RegisterFeatureGate(FeatureGate{Name: DisableMediatedDevicesHandling, State: Alpha})
 	RegisterFeatureGate(FeatureGate{Name: PersistentReservation, State: Alpha})
