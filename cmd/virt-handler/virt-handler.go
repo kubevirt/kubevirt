f6f35e07d3c058577e8fc27f7d692075e8aabdc0
fossedihelm <ffossemo@redhat.com>
2025-11-19 17:08:09 +0100

    handler: move nodeInformer creation to main
    It can be used by node labeller and device controller
    
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- cmd/virt-handler/virt-handler.go
+++ cmd/virt-handler/virt-handler.go
@@ -33,6 +33,7 @@ import (
 	"github.com/emicklei/go-restful/v3"
 	flag "github.com/spf13/pflag"
 	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"libvirt.org/go/libvirtxml"
 
 	"kubevirt.io/kubevirt/pkg/virt-handler/ksm"
@@ -296,8 +297,17 @@ func (app *virtHandlerApp) Run() {
 	stop := make(chan struct{})
 	defer close(stop)
 
-	ksmHandler := ksm.NewHandler(app.HostOverride, app.virtCli.CoreV1(), app.clusterConfig)
-	go ksmHandler.Run(stop)
+	// Create a ListWatch filtered to only the local node
+	listWatch := cache.NewListWatchFromClient(
+		app.virtCli.CoreV1().RESTClient(),
+		"nodes",
+		metav1.NamespaceAll,
+		fields.OneTermEqualSelector("metadata.name", app.HostOverride),
+	)
+
+	nodeInformer := cache.NewSharedInformer(listWatch, &k8sv1.Node{}, controller.ResyncPeriod(12*time.Hour))
+
+	ksmHandler := ksm.NewHandler(app.HostOverride, app.virtCli.CoreV1(), nodeInformer.GetStore(), app.clusterConfig)
 
 	var capabilities libvirtxml.Caps
 	var hostCpuModel string
@@ -427,6 +437,7 @@ func (app *virtHandlerApp) Run() {
 
 	factory.Start(stop)
 	go domainSharedInformer.Run(stop)
+	go nodeInformer.Run(stop)
 
 	se, exists, err := selinux.NewSELinux()
 	if err == nil && exists {
@@ -457,6 +468,7 @@ func (app *virtHandlerApp) Run() {
 		domainSharedInformer.HasSynced,
 		factory.CRD().HasSynced,
 		factory.KubeVirt().HasSynced,
+		nodeInformer.HasSynced,
 	)
 
 	if err := metrics.SetupMetrics(app.HostOverride, app.MaxRequestsInFlight, vmiSourceInformer, machines); err != nil {
@@ -470,6 +482,7 @@ func (app *virtHandlerApp) Run() {
 	go migrationSourceController.Run(5, stop)
 	go migrationTargetController.Run(5, stop)
 	go vmController.Run(10, stop)
+	go ksmHandler.Run(stop)
 
 	doneCh := make(chan string)
 	defer close(doneCh)
