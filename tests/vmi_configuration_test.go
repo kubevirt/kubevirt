589f2f4c7e304791b9fb61dc3cc67747b164b11e
Or Shoval <oshoval@redhat.com>
2025-11-25 11:03:50 +0200

    e2e: Wait for guest agent connected event before login
    We see that some tests login before waiting for guest agent connected event.
    Since fedora 39 update, it seems VM takes more time to load.
    
    As a preliminary step, use the right order that is done already on most of the tests.
    It gives more time to boot the guest, so it will either success login,
    or allows us to debug deeper with less noise.
    
    There are more occurrences, this PR fix some of them.
    Prow search keywords: "expect: timer expired after 120 seconds"
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- tests/vmi_configuration_test.go
+++ tests/vmi_configuration_test.go
@@ -2168,6 +2168,8 @@ var _ = Describe("[sig-compute]Configurations", decorators.SigCompute, func() {
 				Expect(isNodeHasCPUManagerLabel(node2)).To(BeTrue())
 				Expect(node2).To(Equal(node))
 
+				Eventually(matcher.ThisVMI(vmi), 12*time.Minute, 2*time.Second).Should(matcher.HaveConditionTrue(v1.VirtualMachineInstanceAgentConnected))
+
 				By("Expecting the VirtualMachineInstance console")
 				Expect(console.LoginToFedora(vmi)).To(Succeed())
 			})
@@ -2191,6 +2193,8 @@ var _ = Describe("[sig-compute]Configurations", decorators.SigCompute, func() {
 				node1 := libwait.WaitForSuccessfulVMIStart(cpuvmi).Status.NodeName
 				Expect(isNodeHasCPUManagerLabel(node1)).To(BeTrue())
 				Expect(node1).To(Equal(node))
+
+				Eventually(matcher.ThisVMI(vmi), 12*time.Minute, 2*time.Second).Should(matcher.HaveConditionTrue(v1.VirtualMachineInstanceAgentConnected))
 
 				By("Expecting the VirtualMachineInstance console")
 				Expect(console.LoginToFedora(cpuvmi)).To(Succeed())
