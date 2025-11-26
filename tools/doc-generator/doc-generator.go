4de5625b04c6f8584159adbc46999ef9d0281334
ronilerr <rrabinov@redhat.com>
2025-11-26 16:09:23 +0200

    update the format of the metrics presentation to table
    Signed-off-by: ronilerr <rrabinov@redhat.com>

--- tools/doc-generator/doc-generator.go
+++ tools/doc-generator/doc-generator.go
@@ -11,31 +11,26 @@ import (
 	"github.com/rhobs/operator-observability-toolkit/pkg/docs"
 )
 
-const tpl = `# KubeVirt metrics
-
-{{- range . }}
-
-{{ $deprecatedVersion := "" -}}
-{{- with index .ExtraFields "DeprecatedVersion" -}}
-    {{- $deprecatedVersion = printf " in %s" . -}}
-{{- end -}}
-
-{{- $stabilityLevel := "" -}}
-{{- if and (.ExtraFields.StabilityLevel) (ne .ExtraFields.StabilityLevel "STABLE") -}}
-	{{- $stabilityLevel = printf "[%s%s] " .ExtraFields.StabilityLevel $deprecatedVersion -}}
-{{- end -}}
-
-### {{ .Name }}
-{{ print $stabilityLevel }}{{ .Help }} Type: {{ .Type -}}.
-
-{{- end }}
-
-## Developing new metrics
-
-All metrics documented here are auto-generated and reflect exactly what is being
-exposed. After developing new metrics or changing old ones please regenerate
-this document.
-`
+const tpl = "# KubeVirt metrics\n\n" +
+	"| name | Type | Description |\n" +
+	"|------|------|-------------|\n" +
+	"{{- range . }}\n" +
+	"{{ $deprecatedVersion := \"\" -}}\n" +
+	"{{- with index .ExtraFields \"DeprecatedVersion\" -}}\n" +
+	"    {{- $deprecatedVersion = printf \" in %s\" . -}}\n" +
+	"{{- end -}}\n" +
+	"{{- $stabilityLevel := \"\" -}}\n" +
+	"{{- if and (.ExtraFields.StabilityLevel) (ne .ExtraFields.StabilityLevel \"STABLE\") -}}\n" +
+	"	{{- $stabilityLevel = printf \"[%s%s] \" .ExtraFields.StabilityLevel $deprecatedVersion -}}\n" +
+	"{{- end -}}\n" +
+	"{{- $description := printf \"%s%s\" $stabilityLevel .Help -}}\n" +
+	"{{- $nameWithBackticks := printf \"`%s`\" .Name -}}\n" +
+	"| {{ $nameWithBackticks }} | {{ .Type }} | {{ $description }} |\n" +
+	"{{- end }}\n\n" +
+	"## Developing new metrics\n\n" +
+	"All metrics documented here are auto-generated and reflect exactly what is being\n" +
+	"exposed. After developing new metrics or changing old ones please regenerate\n" +
+	"this document.\n"
 
 func main() {
 	if err := libmonitoring.RegisterAllMetrics(); err != nil {
