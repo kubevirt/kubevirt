4965ba0869ad1707864a16da4622eb935cc29ce8
ronilerr <rrabinov@redhat.com>
2025-11-27 22:48:20 +0200

    use embedded metrics template and clean up doc-generator
    Signed-off-by: ronilerr <rrabinov@redhat.com>

--- tools/doc-generator/doc-generator.go
+++ tools/doc-generator/doc-generator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 
 	"github.com/rhobs/operator-observability-toolkit/pkg/operatormetrics"
@@ -11,26 +12,8 @@ import (
 	"github.com/rhobs/operator-observability-toolkit/pkg/docs"
 )
 
-const tpl = "# KubeVirt metrics\n\n" +
-	"| name | Type | Description |\n" +
-	"|------|------|-------------|\n" +
-	"{{- range . }}\n" +
-	"{{ $deprecatedVersion := \"\" -}}\n" +
-	"{{- with index .ExtraFields \"DeprecatedVersion\" -}}\n" +
-	"    {{- $deprecatedVersion = printf \" in %s\" . -}}\n" +
-	"{{- end -}}\n" +
-	"{{- $stabilityLevel := \"\" -}}\n" +
-	"{{- if and (.ExtraFields.StabilityLevel) (ne .ExtraFields.StabilityLevel \"STABLE\") -}}\n" +
-	"	{{- $stabilityLevel = printf \"[%s%s] \" .ExtraFields.StabilityLevel $deprecatedVersion -}}\n" +
-	"{{- end -}}\n" +
-	"{{- $description := printf \"%s%s\" $stabilityLevel .Help -}}\n" +
-	"{{- $nameWithBackticks := printf \"`%s`\" .Name -}}\n" +
-	"| {{ $nameWithBackticks }} | {{ .Type }} | {{ $description }} |\n" +
-	"{{- end }}\n\n" +
-	"## Developing new metrics\n\n" +
-	"All metrics documented here are auto-generated and reflect exactly what is being\n" +
-	"exposed. After developing new metrics or changing old ones please regenerate\n" +
-	"this document.\n"
+//go:embed metrics.tpl
+var metricTemplate string
 
 func main() {
 	if err := libmonitoring.RegisterAllMetrics(); err != nil {
@@ -40,6 +23,6 @@ func main() {
 	metricsList := operatormetrics.ListMetrics()
 	rulesList := rules.ListRecordingRules()
 
-	docsString := docs.BuildMetricsDocsWithCustomTemplate(metricsList, rulesList, tpl)
+	docsString := docs.BuildMetricsDocsWithCustomTemplate(metricsList, rulesList, metricTemplate)
 	fmt.Print(docsString)
 }
