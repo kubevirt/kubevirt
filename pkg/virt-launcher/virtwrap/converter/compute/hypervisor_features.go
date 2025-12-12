0f26ade600fb49058a55263f4a29b3ac37554d42
Orel Misan <omisan@redhat.com>
2025-12-12 11:45:36 +0200

    launcher, converter: Always set domain.Spec.Features
    Defaulter.setDefaults_Features makes sure domain.Spec.Features is always
    non-nil.
    Move this logic to HypervisorFeaturesDomainConfigurator.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/virt-launcher/virtwrap/converter/compute/hypervisor_features.go
+++ pkg/virt-launcher/virtwrap/converter/compute/hypervisor_features.go
@@ -38,16 +38,19 @@ func NewHypervisorFeaturesDomainConfigurator(hasVMPort, useLaunchSecurityTDX boo
 }
 
 func (h HypervisorFeaturesDomainConfigurator) Configure(vmi *v1.VirtualMachineInstance, domain *api.Domain) error {
-	if vmi.Spec.Domain.Features != nil {
-		domain.Spec.Features = &api.Features{}
-		err := convert_v1_Features_To_api_Features(vmi.Spec.Domain.Features, domain.Spec.Features, h.useLaunchSecurityTDX)
-		if err != nil {
-			return err
-		}
+	domain.Spec.Features = &api.Features{}
 
-		if h.hasVMPort {
-			domain.Spec.Features.VMPort = &api.FeatureState{State: "off"}
-		}
+	if vmi.Spec.Domain.Features == nil {
+		return nil
+	}
+
+	err := convert_v1_Features_To_api_Features(vmi.Spec.Domain.Features, domain.Spec.Features, h.useLaunchSecurityTDX)
+	if err != nil {
+		return err
+	}
+
+	if h.hasVMPort {
+		domain.Spec.Features.VMPort = &api.FeatureState{State: "off"}
 	}
 
 	return nil
