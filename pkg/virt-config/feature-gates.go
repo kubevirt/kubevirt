dcbcac25360a6263ccb33e6372e32d656801692d
Nir Dothan <ndothan@redhat.com>
2025-12-28 14:29:47 +0200

    config, fg: Introduce the PasstBinding feature gate
    Per VEP 21 [1] support the PasstBinding Feature Gate.
    
    [1] https://github.com/kubevirt/enhancements/blob/ce150e985ce1d82d76965f2785c45763934f4bea/veps/sig-network/passt/passt-beta.md
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- pkg/virt-config/feature-gates.go
+++ pkg/virt-config/feature-gates.go
@@ -197,6 +197,10 @@ func (config *ClusterConfig) PasstIPStackMigrationEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.PasstIPStackMigration)
 }
 
+func (config *ClusterConfig) PasstBindingEnabled() bool {
+	return config.isFeatureGateEnabled(featuregate.PasstBinding)
+}
+
 func (config *ClusterConfig) DecentralizedLiveMigrationEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.DecentralizedLiveMigration)
 }
