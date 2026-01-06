e246a459ef61a39d02a88ab5242a81536c73a9e8
Luboslav Pivarc <lpivarc@redhat.com>
2026-01-06 14:20:29 +0100

    Monitoring, queue: Simplify
    There is no need to have a context as we cancel it
    only in the actaul work path.
    
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/monitoring/metrics/virt-handler/migrationdomainstats/queue.go
+++ pkg/monitoring/metrics/virt-handler/migrationdomainstats/queue.go
@@ -20,7 +20,6 @@ package migrationdomainstats
 
 import (
 	"container/ring"
-	"context"
 	"fmt"
 	"sync"
 	"sync/atomic"
@@ -59,8 +58,6 @@ type queue struct {
 	collector domstatsCollector.Collector
 	results   *ring.Ring
 
-	ctxCancel context.CancelFunc
-
 	isActive atomic.Bool
 }
 
@@ -76,42 +73,37 @@ func newQueue(vmiStore cache.Store, vmi *v1.VirtualMachineInstance) *queue {
 
 func (q *queue) startPolling() {
 	q.isActive.Store(true)
-	ctx, cancel := context.WithCancel(context.Background())
-	q.ctxCancel = cancel
 	ticker := time.NewTicker(pollingInterval)
 
 	go func() {
 		defer q.isActive.Store(false)
 		defer ticker.Stop()
 		log.Log.V(4).Infof("collecting domain stats for VMI %s/%s (initial)", q.vmi.Namespace, q.vmi.Name)
-		q.collect()
+		if !q.collect() {
+			log.Log.V(2).Infof("stopping domain stats collection for VMI %s/%s", q.vmi.Namespace, q.vmi.Name)
+			return
+		}
 
 		for {
-			select {
-			case <-ctx.Done():
+			<-ticker.C
+			log.Log.V(4).Infof("collecting domain stats for VMI %s/%s", q.vmi.Namespace, q.vmi.Name)
+			if !q.collect() {
 				log.Log.V(2).Infof("stopping domain stats collection for VMI %s/%s", q.vmi.Namespace, q.vmi.Name)
 				return
-			case <-ticker.C:
-				log.Log.V(4).Infof("collecting domain stats for VMI %s/%s", q.vmi.Namespace, q.vmi.Name)
-				q.collect()
 			}
 		}
 	}()
 }
 
-func (q *queue) collect() {
+func (q *queue) collect() bool {
 	if q.isMigrationFinished() {
-		q.Lock()
-		defer q.Unlock()
-
-		q.ctxCancel()
-		return
+		return false
 	}
 
 	values, err := q.scrapeDomainStats()
 	if err != nil {
 		log.Log.Reason(err).Errorf("failed to scrape domain stats for VMI %s/%s", q.vmi.Namespace, q.vmi.Name)
-		return
+		return true
 	}
 
 	r := result{
@@ -127,6 +119,7 @@ func (q *queue) collect() {
 	defer q.Unlock()
 	q.results.Value = r
 	q.results = q.results.Next()
+	return true
 }
 
 func (q *queue) scrapeDomainStats() (*stats.DomainStats, error) {
