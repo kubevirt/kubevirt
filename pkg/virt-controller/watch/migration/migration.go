3f3bb1fd68022decf202c842e13ebb2d9d95dfba
Alexander Wels <awels@redhat.com>
2026-01-12 14:46:38 -0600

    Added GetCondition to VirtualMachineInstanceMigrationConditionManager Added unit tests to check the condition is set in the migration when the VMI condition is added and removed
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/virt-controller/watch/migration/migration.go
+++ pkg/virt-controller/watch/migration/migration.go
@@ -649,20 +649,7 @@ func (c *Controller) updateStatus(migration *virtv1.VirtualMachineInstanceMigrat
 		}
 	}
 
-	if vmiCondition := controller.NewVirtualMachineInstanceConditionManager().GetCondition(vmi, virtv1.VirtualMachineInstanceDecentralizedLiveMigrationFailure); vmiCondition != nil {
-		condition := virtv1.VirtualMachineInstanceMigrationCondition{
-			Type:          virtv1.VirtualMachineInstanceDecentralizedMigrationBlocked,
-			Reason:        vmiCondition.Reason,
-			Message:       vmiCondition.Message,
-			Status:        k8sv1.ConditionTrue,
-			LastProbeTime: v1.Now(),
-		}
-		if !conditionManager.HasCondition(migration, virtv1.VirtualMachineInstanceDecentralizedMigrationBlocked) {
-			migrationCopy.Status.Conditions = append(migrationCopy.Status.Conditions, condition)
-		} else {
-			conditionManager.UpdateCondition(migration, &condition)
-		}
-	}
+	updateDecentralizedMigrationCondition(vmi, migrationCopy, conditionManager)
 
 	controller.SetVMIMigrationPhaseTransitionTimestamp(migration, migrationCopy)
 	controller.SetSourcePod(migrationCopy, vmi, c.podIndexer)
@@ -682,6 +669,26 @@ func (c *Controller) updateStatus(migration *virtv1.VirtualMachineInstanceMigrat
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func updateDecentralizedMigrationCondition(vmi *virtv1.VirtualMachineInstance, migrationCopy *virtv1.VirtualMachineInstanceMigration, conditionManager *controller.VirtualMachineInstanceMigrationConditionManager) error {
+	if vmiCondition := controller.NewVirtualMachineInstanceConditionManager().GetCondition(vmi, virtv1.VirtualMachineInstanceDecentralizedLiveMigrationFailure); vmiCondition != nil {
+		condition := virtv1.VirtualMachineInstanceMigrationCondition{
+			Type:          virtv1.VirtualMachineInstanceDecentralizedMigrationBlocked,
+			Reason:        vmiCondition.Reason,
+			Message:       vmiCondition.Message,
+			Status:        k8sv1.ConditionTrue,
+			LastProbeTime: v1.Now(),
+		}
+		if !conditionManager.HasCondition(migrationCopy, virtv1.VirtualMachineInstanceDecentralizedMigrationBlocked) {
+			migrationCopy.Status.Conditions = append(migrationCopy.Status.Conditions, condition)
+		} else {
+			conditionManager.UpdateCondition(migrationCopy, &condition)
+		}
+	} else {
+		conditionManager.RemoveCondition(migrationCopy, virtv1.VirtualMachineInstanceDecentralizedMigrationBlocked)
 	}
 	return nil
 }
