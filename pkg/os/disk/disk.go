ad7aa5cf4044d3a6de9669e7b3dd7bf335edd748
Shelly Kagan <skagan@redhat.com>
2026-01-13 11:28:09 +0200

    Validate disk bitmaps during checkpoint redefinition
    When redefining checkpoints after VM restart, validate that each disk
    in the checkpoint still exists in the domain and has the corresponding
    bitmap in its qcow2 file. This prevents checkpoint redefinition failures
    when volumes have been removed (e.g., hotplug volumes) between backups.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- pkg/os/disk/disk.go
+++ pkg/os/disk/disk.go
@@ -8,19 +8,59 @@ import (
 )
 
 type DiskInfo struct {
-	Format      string `json:"format"`
-	BackingFile string `json:"backing-filename"`
-	ActualSize  int64  `json:"actual-size"`
-	VirtualSize int64  `json:"virtual-size"`
+	Format         string          `json:"format"`
+	BackingFile    string          `json:"backing-filename"`
+	ActualSize     int64           `json:"actual-size"`
+	VirtualSize    int64           `json:"virtual-size"`
+	FormatSpecific *FormatSpecific `json:"format-specific,omitempty"`
+}
+
+type FormatSpecific struct {
+	Type string              `json:"type"`
+	Data *FormatSpecificData `json:"data,omitempty"`
+}
+
+type FormatSpecificData struct {
+	Bitmaps []BitmapInfo `json:"bitmaps,omitempty"`
+}
+
+type BitmapInfo struct {
+	Name        string   `json:"name"`
+	Granularity int64    `json:"granularity"`
+	Flags       []string `json:"flags,omitempty"`
 }
 
 const (
 	QEMUIMGPath = "/usr/bin/qemu-img"
 )
 
+func (d *DiskInfo) HasBitmap(name string) bool {
+	if d.FormatSpecific == nil || d.FormatSpecific.Data == nil {
+		return false
+	}
+	for _, bm := range d.FormatSpecific.Data.Bitmaps {
+		if bm.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
 func GetDiskInfo(imagePath string) (*DiskInfo, error) {
+	return getDiskInfo(imagePath, false)
+}
+
+func GetDiskInfoWithForceShare(imagePath string) (*DiskInfo, error) {
+	return getDiskInfo(imagePath, true)
+}
+
+func getDiskInfo(imagePath string, forceShare bool) (*DiskInfo, error) {
 	// #nosec No risk for attacker injection. Only get information about an image
 	args := []string{"info", imagePath, "--output", "json"}
+	if forceShare {
+		// -U (force-share) allows reading disk info even when the disk is in use by QEMU
+		args = []string{"info", "-U", imagePath, "--output", "json"}
+	}
 	cmd := exec.Command(QEMUIMGPath, args...)
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
