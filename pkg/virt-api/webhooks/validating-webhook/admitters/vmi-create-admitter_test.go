af5cd532f460e2c84ca71edc4a379a6b8235873d
Varun Ramachandra Sekar <vsekar@nvidia.com>
2026-01-12 16:19:02 -0800

    Graduate PanicDevices FeatureGate to GA
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

--- pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
@@ -1430,17 +1430,7 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 		})
 
 		Context("with panic devices defined", func() {
-			It("should fail when PanicDevices featuregate is disabled", func() {
-				vmi := api.NewMinimalVMI("testvm")
-				vmi.Spec.Domain.Devices.PanicDevices = []v1.PanicDevice{{Model: pointer.P(v1.Hyperv)}}
-				causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
-				Expect(causes).To(HaveLen(1))
-				Expect(causes[0].Field).To(Equal("fake.domain.devices.panicDevices"))
-				Expect(causes[0].Message).To(Equal("Panic Devices feature gate is not enabled in kubevirt-config"))
-			})
-
 			It("should allow valid panic device model", func() {
-				enableFeatureGates(featuregate.PanicDevicesGate)
 				vmi := api.NewMinimalVMI("testvm")
 				vmi.Spec.Domain.Devices.PanicDevices = []v1.PanicDevice{{Model: pointer.P(v1.Hyperv)}}
 				causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
@@ -1448,7 +1438,6 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			})
 
 			It("should reject invalid panic device model", func() {
-				enableFeatureGates(featuregate.PanicDevicesGate)
 				vmi := api.NewMinimalVMI("testvm")
 				panicDeviceModel := v1.PanicDeviceModel("bad")
 				vmi.Spec.Domain.Devices.PanicDevices = []v1.PanicDevice{{Model: &panicDeviceModel}}
@@ -1459,7 +1448,6 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			})
 
 			It("should reject panic devices on s390x architecture", func() {
-				enableFeatureGates(featuregate.PanicDevicesGate)
 				vmi := api.NewMinimalVMI("testvm")
 				vmi.Spec.Architecture = "s390x"
 				vmi.Spec.Domain.Devices.PanicDevices = []v1.PanicDevice{{Model: pointer.P(v1.Isa)}}
