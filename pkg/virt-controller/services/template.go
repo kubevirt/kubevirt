37a8f37ad807d845f98bf0e0c71cac2f8557f791
Daniil Antoshin <daniil.antoshin@flant.com>
2026-01-14 15:53:21 +0200

    feat(hp): exclude from quota (#59)
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- pkg/virt-controller/services/template.go
+++ pkg/virt-controller/services/template.go
@@ -1022,8 +1022,9 @@ func (t *templateService) RenderHotplugAttachmentPodTemplate(volumes []*v1.Volum
 				}),
 			},
 			Labels: map[string]string{
-				v1.AppLabel:                            hotplugDisk,
-				"security.deckhouse.io/skip-pss-check": "true",
+				v1.AppLabel:                                    hotplugDisk,
+				"security.deckhouse.io/skip-pss-check":         "true",
+				"resource-quota-overrides.deckhouse.io/ignore": "true",
 			},
 		},
 		Spec: k8sv1.PodSpec{
@@ -1199,8 +1200,9 @@ func (t *templateService) RenderHotplugAttachmentTriggerPodTemplate(volume *v1.V
 				}),
 			},
 			Labels: map[string]string{
-				v1.AppLabel:                            hotplugDisk,
-				"security.deckhouse.io/skip-pss-check": "true",
+				v1.AppLabel:                                    hotplugDisk,
+				"security.deckhouse.io/skip-pss-check":         "true",
+				"resource-quota-overrides.deckhouse.io/ignore": "true",
 			},
 			Annotations: annotationsList,
 		},
