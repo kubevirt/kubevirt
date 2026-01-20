768828944a4ec7d465c9f48e6f8cd80bd51aed50
Harshit Gupta <guptaharshit@microsoft.com>
2026-01-20 09:45:45 -0800

    Wait for cache-sync before creating Controllers
    Multiple controllers in virt-handler are instantiated before the
    ClusterConfig informer and other informers have synced. This lets the
    Controllers be instantiated with the actual cluster config from the CR
    instead of being instantiated with default config and being updated
    later via callbacks.
    
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- cmd/virt-handler/virt-handler.go
+++ cmd/virt-handler/virt-handler.go
@@ -373,6 +373,23 @@ func (app *virtHandlerApp) Run() {
 	netStat := netsetup.NewNetStat()
 	passtRepairHandler := passt.NewRepairManager()
 
+	// Bootstrapping. From here on the startup order matters
+
+	factory.Start(stop)
+	go domainSharedInformer.Run(stop)
+	go nodeInformer.Run(stop)
+	cache.WaitForCacheSync(
+		stop,
+		vmiInformer.HasSynced,
+		vmiSourceInformer.HasSynced,
+		vmiTargetInformer.HasSynced,
+		domainSharedInformer.HasSynced,
+		factory.CRD().HasSynced,
+		factory.KubeVirt().HasSynced,
+		nodeInformer.HasSynced,
+		backupTrackerInformer.HasSynced,
+	)
+
 	migrationSourceController, err := virthandler.NewMigrationSourceController(
 		recorder,
 		app.virtCli,
@@ -457,12 +474,6 @@ func (app *virtHandlerApp) Run() {
 	go app.migrationCertManager.Start()
 	go app.vsockClientCertManager.Start()
 
-	// Bootstrapping. From here on the startup order matters
-
-	factory.Start(stop)
-	go domainSharedInformer.Run(stop)
-	go nodeInformer.Run(stop)
-
 	se, exists, err := selinux.NewSELinux()
 	if err == nil && exists {
 		// relabel tun device
@@ -483,18 +494,6 @@ func (app *virtHandlerApp) Run() {
 		//an error occurred
 		panic(fmt.Errorf("failed to detect the presence of selinux: %v", err))
 	}
-
-	cache.WaitForCacheSync(
-		stop,
-		vmiInformer.HasSynced,
-		vmiSourceInformer.HasSynced,
-		vmiTargetInformer.HasSynced,
-		domainSharedInformer.HasSynced,
-		factory.CRD().HasSynced,
-		factory.KubeVirt().HasSynced,
-		nodeInformer.HasSynced,
-		backupTrackerInformer.HasSynced,
-	)
 
 	if err := metrics.SetupMetrics(app.HostOverride, app.MaxRequestsInFlight, vmiSourceInformer, machines); err != nil {
 		panic(err)
