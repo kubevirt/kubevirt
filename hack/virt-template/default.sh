a244997ebb07a6240cef57dce38ae32c3df78b13
Felix Matouschek <fmatouschek@redhat.com>
2026-01-27 15:05:01 +0100

    ci(hack,virt-template): Add bump and sync scripts
    Add bump and sync scripts that automatically find the latest release for
    a given virt-template branch and sync it to this repo.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- hack/virt-template/default.sh
+++ hack/virt-template/default.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 virt_template_version=${VIRT_TEMPLATE_VERSION:-"v0.1.3"}
+virt_template_yaml_sha256=${VIRT_TEMPLATE_YAML_SHA256:-"63d2fcd851043b526f0c03ff814a9d6ad34e62111410851bc72dc19db745dcf2"}
