316010fe2771da54c575d3f9251f51c6e2252042
Alex Kalenyuk <akalenyu@redhat.com>
2026-01-27 19:40:20 +0200

    reservation tests: convert disable scenario e2e->unit test
    really no value in e2e testing this;
    this will make it easier to get rid of all in-test toggling
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- tests/storage/reservation.go
+++ tests/storage/reservation.go
@@ -9,7 +9,6 @@ import (
 	expect "github.com/google/goexpect"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gstruct"
 	corev1 "k8s.io/api/core/v1"
 	k8sv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -340,59 +339,37 @@ var _ = Describe(SIG("SCSI persistent reservation", Serial, func() {
 		})
 	})
 
-	Context("with PersistentReservation feature gate toggled", func() {
-		It("should delete and recreate virt-handler", func() {
-			config.DisableFeatureGate(featuregate.PersistentReservation)
-
-			Eventually(func() []k8sv1.Container {
-				ds, err := virtClient.AppsV1().DaemonSets(flags.KubeVirtInstallNamespace).Get(context.TODO(), "virt-handler", metav1.GetOptions{})
+	Context("With multipath", func() {
+		const mpathSocket = "/proc/1/root/run/multipathd.socket"
+		BeforeEach(func() {
+			// Check if mulitpathd socket exists on the nodes, if not simulate the existance by creating a mock socket
+			nodes := libnode.GetAllSchedulableNodes(virtClient)
+			for _, node := range nodes.Items {
+				_, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"ls", mpathSocket})
 				if err != nil {
-					return nil
+					By(fmt.Sprintf("Create a fake mulitpathd.socket in node %s", node.Name))
+					libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"touch", mpathSocket})
+					DeferCleanup(func() {
+						_, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"rm", "-f", mpathSocket})
+						Expect(err).ToNot(HaveOccurred())
+					})
 				}
-				return ds.Spec.Template.Spec.Containers
-			}, time.Minute*5, time.Second*2).ShouldNot(
-				ContainElement((gstruct.MatchFields(gstruct.IgnoreExtras, gstruct.Fields{
-					"Name": Equal("pr-helper")},
-				))))
-
-			// Switching the PersistentReservation feature gate on/off
-			// causes redeployment of all KubeVirt components.
-			By("Ensuring all KubeVirt components are ready")
-			testsuite.EnsureKubevirtReady()
+			}
 		})
 
-		Context("With multipath", func() {
-			const mpathSocket = "/proc/1/root/run/multipathd.socket"
-			BeforeEach(func() {
-				// Check if mulitpathd socket exists on the nodes, if not simulate the existance by creating a mock socket
-				nodes := libnode.GetAllSchedulableNodes(virtClient)
-				for _, node := range nodes.Items {
-					_, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"ls", mpathSocket})
-					if err != nil {
-						By(fmt.Sprintf("Create a fake mulitpathd.socket in node %s", node.Name))
-						libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"touch", mpathSocket})
-						DeferCleanup(func() {
-							_, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"rm", "-f", mpathSocket})
-							Expect(err).ToNot(HaveOccurred())
-						})
-					}
-				}
-			})
-
-			It("ensure multipath socket is bind mounted and available to the pr-helper daemon", func() {
-				nodes := libnode.GetAllSchedulableNodes(virtClient)
-				for _, node := range nodes.Items {
-					Eventually(func(g Gomega) {
-						output, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"cat", "/proc/mounts"})
-						g.Expect(err).ToNot(HaveOccurred())
-						g.Expect(strings.Count(output, "multipathd.socket")).Should(Equal(1),
-							"the multipathd socket should be mounted only once")
-					}).
-						Within(20 * time.Second).
-						WithPolling(1 * time.Second).
-						Should(Succeed())
-				}
-			})
+		It("ensure multipath socket is bind mounted and available to the pr-helper daemon", func() {
+			nodes := libnode.GetAllSchedulableNodes(virtClient)
+			for _, node := range nodes.Items {
+				Eventually(func(g Gomega) {
+					output, err := libnode.ExecuteCommandInVirtHandlerPod(node.Name, []string{"cat", "/proc/mounts"})
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(strings.Count(output, "multipathd.socket")).Should(Equal(1),
+						"the multipathd socket should be mounted only once")
+				}).
+					Within(20 * time.Second).
+					WithPolling(1 * time.Second).
+					Should(Succeed())
+			}
 		})
 	})
 
