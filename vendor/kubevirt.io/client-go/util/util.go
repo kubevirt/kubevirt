7933a6f34ca23a75745df500c4c24a22032ee6c7
sbiradar10 <sbiradar@redhat.com>
2026-01-29 22:03:26 +0530

    CVE-2025-52881: Bump selinux to 1.12.0 & runc to v1.2.8
    Signed-off-by: sbiradar10 <sbiradar@redhat.com>

---
+++ vendor/kubevirt.io/client-go/util/util.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const ServiceAccountNamespaceFile = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+const namespaceKubevirt = "kubevirt"
+
+func GetNamespace() (string, error) {
+	if data, err := os.ReadFile(ServiceAccountNamespaceFile); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns, nil
+		}
+	} else if !errors.Is(err, os.ErrNotExist) {
+		return "", fmt.Errorf("failed to determine namespace from %s: %v", ServiceAccountNamespaceFile, err)
+	}
+	return namespaceKubevirt, nil
+}
