b58264a42769f4d0fe484a49aa2fdc1920afc91d
Ananya Banerjee <anbanerj@redhat.com>
2026-02-02 17:40:53 +0100

    virt-ctl, lifecycle: Adding the correct migration conditions
    As a part of implementation of VEP 140 [1] this commit updates the
    Evaluate method to trigger an immediate migration when the networkName
    in the VMI spec differs from the NAD ref in the current pod annotation.
    
    This is protected by FG LiveUpdateNADRef.
    
    [1] https://github.com/kubevirt/enhancements/pull/138
    
    Signed-off-by: Ananya Banerjee <anbanerj@redhat.com>

--- pkg/network/migration/evaluator.go
+++ pkg/network/migration/evaluator.go
@@ -20,12 +20,17 @@
 package migration
 
 import (
+	"encoding/json"
+	"fmt"
 	"slices"
+	"strings"
 	"time"
 
 	k8scorev1 "k8s.io/api/core/v1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 
 	v1 "kubevirt.io/api/core/v1"
 
@@ -44,21 +49,33 @@ const (
 
 type timeProviderFunc func() metav1.Time
 
+type clusterConfigurer interface {
+	LiveUpdateNADRefEnabled() bool
+}
+
 type Evaluator struct {
-	timeProvider timeProviderFunc
+	timeProvider      timeProviderFunc
+	clusterConfigurer clusterConfigurer
 }
 
-func NewEvaluator() Evaluator {
-	return NewEvaluatorWithTimeProvider(metav1.Now)
+func NewEvaluator(clusterConfigurer clusterConfigurer) Evaluator {
+	return NewEvaluatorWithTimeProvider(metav1.Now, clusterConfigurer)
 }
 
-func NewEvaluatorWithTimeProvider(timeProvider timeProviderFunc) Evaluator {
-	return Evaluator{timeProvider: timeProvider}
+func NewEvaluatorWithTimeProvider(timeProvider timeProviderFunc, clusterConfigurer clusterConfigurer) Evaluator {
+	return Evaluator{
+		timeProvider:      timeProvider,
+		clusterConfigurer: clusterConfigurer,
+	}
 }
 
-func (e Evaluator) Evaluate(vmi *v1.VirtualMachineInstance) k8scorev1.ConditionStatus {
+func (e Evaluator) Evaluate(vmi *v1.VirtualMachineInstance,
+	pod *k8scorev1.Pod,
+) k8scorev1.ConditionStatus {
 	result := shouldVMIBeMarkedForAutoMigration(
 		vmi,
+		pod,
+		e.clusterConfigurer.LiveUpdateNADRefEnabled(),
 	)
 
 	switch result {
@@ -82,10 +99,13 @@ func (e Evaluator) Evaluate(vmi *v1.VirtualMachineInstance) k8scorev1.ConditionS
 
 func shouldVMIBeMarkedForAutoMigration(
 	vmi *v1.VirtualMachineInstance,
+	pod *k8scorev1.Pod,
+	isLiveUpdateNADRefEnabled bool,
 ) migrationRequirementKind {
 	ifaces := vmi.Spec.Domain.Devices.Interfaces
 	nets := vmi.Spec.Networks
 	ifaceStatuses := vmi.Status.Interfaces
+	namespace := vmi.Namespace
 
 	secondaryIfaces := vmispec.FilterInterfacesByNetworks(
 		ifaces,
@@ -93,6 +113,7 @@ func shouldVMIBeMarkedForAutoMigration(
 	)
 
 	ifaceStatusesByName := vmispec.IndexInterfaceStatusByName(ifaceStatuses, nil)
+	netsByName := vmispec.IndexNetworkSpecByName(nets)
 
 	for _, iface := range secondaryIfaces {
 		ifaceStatus, ifaceStatusExists := ifaceStatusesByName[iface.Name]
@@ -103,6 +124,22 @@ func shouldVMIBeMarkedForAutoMigration(
 
 		if req := checkHotUnplugMigration(iface, ifaceStatus, ifaceStatusExists); req != notRequired {
 			return req
+		}
+		if isLiveUpdateNADRefEnabled {
+			if iface.State == v1.InterfaceStateAbsent || !ifaceStatusExists {
+				continue
+			}
+			net, netExists := netsByName[iface.Name]
+			if !netExists || net.Multus == nil {
+				continue
+			}
+			nadName := nadNameFromPod(pod, ifaceStatus.PodInterfaceName)
+			if nadName == "" {
+				continue
+			}
+			if !isNADNameEqual(net.Multus.NetworkName, nadName, namespace) {
+				return immediateMigration
+			}
 		}
 	}
 	return notRequired
@@ -131,6 +168,36 @@ func checkHotUnplugMigration(
 		return pendingMigration
 	}
 	return notRequired
+}
+
+func nadNameFromPod(pod *k8scorev1.Pod, ifaceName string) string {
+	if pod == nil {
+		return ""
+	}
+	annot, ok := pod.Annotations[networkv1.NetworkStatusAnnot]
+	if !ok {
+		return ""
+	}
+	var elements []networkv1.NetworkStatus
+	if err := json.Unmarshal([]byte(annot), &elements); err != nil {
+		return ""
+	}
+	for _, element := range elements {
+		if element.Interface == ifaceName {
+			return element.Name
+		}
+	}
+	return ""
+}
+
+func isNADNameEqual(nameFromSpec, nameFromPod, vmiNamespace string) bool {
+	if nameFromSpec == nameFromPod {
+		return true
+	}
+	if !strings.Contains(nameFromSpec, "/") {
+		nameFromSpec = fmt.Sprintf("%s/%s", vmiNamespace, nameFromSpec)
+	}
+	return nameFromSpec == nameFromPod
 }
 
 func lookupMigrationRequiredCondition(conditions []v1.VirtualMachineInstanceCondition) *v1.VirtualMachineInstanceCondition {
