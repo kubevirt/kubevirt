54329642603d6cf519166e18e291cb7b4ab7adac
Harshit Gupta <guptaharshit@microsoft.com>
2026-02-05 16:49:46 +0000

    Introduce LauncherHypervisorResources interface
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

---
+++ pkg/hypervisor/launcherhypervisorresources.go
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ *
+ */
+
+package hypervisor
+
+import (
+	"k8s.io/apimachinery/pkg/api/resource"
+	v1 "kubevirt.io/api/core/v1"
+)
+
+// Interface to abstract the hypervisor-specific resources available to the virt-launcher,
+// such as devices and memory overhead.
+type LauncherHypervisorResources interface {
+	GetHypervisorDevice() string
+	GetMemoryOverhead(vmi *v1.VirtualMachineInstance, arch string, additionalOverheadRatio *string) resource.Quantity
+}
+
+func NewLauncherHypervisorResources(hypervisor string) LauncherHypervisorResources {
+	switch hypervisor {
+	// Other hypervisors can be added here
+	default:
+		return nil
+	}
+}
