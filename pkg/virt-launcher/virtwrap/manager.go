918c98df4756eeaba82e55f8fbdc4e2fdc84e29c
Michail Resvanis <mresvani@redhat.com>
2026-02-05 15:42:03 +0100

    Add PCINUMAAwareTopologyEnabled field to cluster config and wire to converter
    This change:
    - Adds PCINUMAAwareTopologyEnabled field to the protobuf ClusterConfig
    - Wires the feature gate to the converter context in the domain manager
    - Adds the tests for the feature gate integration
    
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

--- pkg/virt-launcher/virtwrap/manager.go
+++ pkg/virt-launcher/virtwrap/manager.go
@@ -1086,6 +1086,7 @@ func (l *LibvirtDomainManager) generateConverterContext(vmi *v1.VirtualMachineIn
 			c.FreePageReporting = isFreePageReportingEnabled(options.GetClusterConfig().GetFreePageReportingDisabled(), vmi)
 			c.BochsForEFIGuests = options.GetClusterConfig().GetBochsDisplayForEFIGuests()
 			c.SerialConsoleLog = isSerialConsoleLogEnabled(options.GetClusterConfig().GetSerialConsoleLogDisabled(), vmi)
+			c.PCINUMAAwareTopologyEnabled = options.GetClusterConfig().GetPCINUMAAwareTopologyEnabled()
 		}
 
 		c.DomainAttachmentByInterfaceName = options.GetInterfaceDomainAttachment()
