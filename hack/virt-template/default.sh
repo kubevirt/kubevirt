d977547f4bb84c3b58d0fc63ead7592e81da9a82
Felix Matouschek <fmatouschek@redhat.com>
2026-02-17 17:35:19 +0100

    feat(virt-template): Bump to version v0.1.6
    Bump the bundled virt-template to version v0.1.6.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- hack/virt-template/default.sh
+++ hack/virt-template/default.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-virt_template_version=${VIRT_TEMPLATE_VERSION:-"v0.1.3"}
-virt_template_yaml_sha256=${VIRT_TEMPLATE_YAML_SHA256:-"63d2fcd851043b526f0c03ff814a9d6ad34e62111410851bc72dc19db745dcf2"}
+virt_template_version=${VIRT_TEMPLATE_VERSION:-"v0.1.6"}
+virt_template_yaml_sha256=${VIRT_TEMPLATE_YAML_SHA256:-"ed911cdd483b94bb7de9a818204760915a9048fdd47fb98427cf67725e5d0863"}
