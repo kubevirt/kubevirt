72cd04e402ea8de0cf2959b976c62e712145d035
Alex Kalenyuk <akalenyu@redhat.com>
2026-02-18 12:57:24 +0200

    bazel server: reload on network changes
    the server container starting before network changes
    can trigger all sorts of weird errors like
    ```
    java.io.IOException: Error downloading [https://github.com/google/go-containerregistry/releases/download/v0.18.0/go-containerregistry_Linux_x86_64.tar.gz] to /root/.cache/bazel/_bazel_root/6f347497f91c9a385dcd9294645b76e0/external/oci_crane_linux_amd64/temp750214973604621885/go-containerregistry_Linux_x86_64.tar.gz: Unknown host: github.com
    ```
    this could be a nice quality of life improvement for the dev environment
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/bazel-server.sh
+++ hack/bazel-server.sh
@@ -3,6 +3,21 @@ if [ "$KUBEVIRT_NO_BAZEL" = true ]; then
     sleep infinity
 else
     BAZEL_PID=$(bazel info | grep server_pid | cut -d " " -f 2)
+
+    # Shut down the bazel server on network changes
+    (
+        initial=$(cat /proc/net/route 2>/dev/null)
+        while kill -0 $BAZEL_PID 2>/dev/null; do
+            sleep 5
+            current=$(cat /proc/net/route 2>/dev/null)
+            if [ "$initial" != "$current" ]; then
+                echo "Network change detected, shutting down bazel server..."
+                bazel shutdown
+                break
+            fi
+        done
+    ) &
+
     while kill -0 $BAZEL_PID 2>/dev/null; do sleep 1; done
     # Might not be necessary, just to be sure that exec shutdowns always succeed
     # and are not killed by docker.
