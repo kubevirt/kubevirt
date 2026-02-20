1aedd14abf54035a4721c5354472e15917e6b628
Harshit Gupta <guptaharshit@microsoft.com>
2026-02-20 20:46:54 +0000

    Add dummy impl for KVM Domain Builder Factory
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/hypervisor/domain-builder-factory.go
+++ pkg/hypervisor/domain-builder-factory.go
@@ -1,9 +1,20 @@
 package hypervisor
 
 import (
+	v1 "kubevirt.io/api/core/v1"
+
+	"kubevirt.io/kubevirt/pkg/hypervisor/kvm"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/converter/types"
 )
 
 type DomainBuilderFactory interface {
-	MakeDomainBuilder(c *types.ConverterContext) *types.DomainBuilder
+	MakeDomainBuilder(vmi *v1.VirtualMachineInstance, c *types.ConverterContext) *types.DomainBuilder
+}
+
+func NewDomainBuilderFactory(hypervisor string) DomainBuilderFactory {
+	switch hypervisor {
+	// Other hypervisors can be added here
+	default:
+		return &kvm.KvmDomainBuilderFactory{} // Default to KVM
+	}
 }
