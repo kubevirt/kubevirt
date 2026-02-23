c1fe8bad949adb50e3fc15e73d8929e769d4dfb9
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2026-02-23 13:58:34 +0100

    util: VMI memory reserved overhead predicates
    Adds a pair of predicates to check whether the VMI requires some memory
    overhead or special memlock configuration.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -26,6 +26,8 @@ import (
 	v1 "kubevirt.io/api/core/v1"
 
 	"kubevirt.io/kubevirt/pkg/pointer"
+
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 var _ = Describe("GPU VMI Predicates", func() {
@@ -99,3 +101,135 @@ var _ = Describe("Host Device VMI Predicates", func() {
 		Expect(IsHostDevVMI(vmi)).To(BeTrue())
 	})
 })
+
+var _ = DescribeTable("memory overhead reservation requirements",
+	func(vmi *v1.VirtualMachineInstance, expected bool) {
+		res := RequiresMemoryOverheadReservation(vmi)
+		Expect(res).To(Equal(expected))
+	},
+	Entry(
+		"Domain Memory reference is nil",
+		&v1.VirtualMachineInstance{},
+		false,
+	),
+	Entry(
+		"ReservedOverhead reference is nil",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"ReservedOverhead reference is empty",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{},
+					},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"AddedOverhead has a value",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{
+							AddedOverhead: pointer.P(resource.MustParse("1Gi")),
+						},
+					},
+				},
+			},
+		},
+		true,
+	),
+)
+
+var _ = DescribeTable("memory lock limit requirements",
+	func(vmi *v1.VirtualMachineInstance, expected bool) {
+		res := RequiresLockingMemory(vmi)
+		Expect(res).To(Equal(expected))
+	},
+	Entry(
+		"Domain Memory reference is nil",
+		&v1.VirtualMachineInstance{},
+		false,
+	),
+	Entry(
+		"ReservedOverhead reference is nil",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"ReservedOverhead reference is empty",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{},
+					},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"MemLock is whatever other than Required",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{
+							MemLock: pointer.P(v1.MemLockRequirement("notExpectedValue")),
+						},
+					},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"MemLock is NotRequired",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{
+							MemLock: pointer.P(v1.MemLockRequirement(v1.MemLockNotRequired)),
+						},
+					},
+				},
+			},
+		},
+		false,
+	),
+	Entry(
+		"MemLock is Required",
+		&v1.VirtualMachineInstance{
+			Spec: v1.VirtualMachineInstanceSpec{
+				Domain: v1.DomainSpec{
+					Memory: &v1.Memory{
+						ReservedOverhead: &v1.ReservedOverhead{
+							MemLock: pointer.P(v1.MemLockRequirement(v1.MemLockRequired)),
+						},
+					},
+				},
+			},
+		},
+		true,
+	),
+)
