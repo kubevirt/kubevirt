e5967bc3cc6b1e9abd3b614063ab0501e551dc5d
Harshit Gupta <guptaharshit@microsoft.com>
2026-02-23 19:15:33 +0000

    Migrate AdjustResources function to VirtRuntime
    Unify the order of conditions for calculating vmiBaseMemory to
    Memory.MaxGuest --> Resources.Requests.Memory() --> Memory.Guest.
    Enforce same order for both running and non-running VMIs.
    
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/hypervisor/virt-runtime.go
+++ pkg/hypervisor/virt-runtime.go
@@ -21,7 +21,9 @@ package hypervisor
 
 import (
 	v1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
 
+	"kubevirt.io/kubevirt/pkg/hypervisor/kvm"
 	"kubevirt.io/kubevirt/pkg/virt-handler/cgroup"
 	"kubevirt.io/kubevirt/pkg/virt-handler/isolation"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
@@ -33,5 +35,8 @@ type VirtRuntime interface {
 }
 
 func GetVirtRuntime(podIsolationDetector isolation.PodIsolationDetector, hypervisorName string) VirtRuntime {
-	return nil
+	switch hypervisorName {
+	default:
+		return kvm.NewKvmVirtRuntime(podIsolationDetector, log.Log.With("controller", "vm"))
+	}
 }
