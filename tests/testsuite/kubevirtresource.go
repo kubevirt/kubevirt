6676c36aa4a5a716d26b2ac55755ebee921e12d8
Alvaro Romero <alromero@redhat.com>
2026-03-05 19:27:28 +0100

    VMExport: GA feature gate
    This commit graduates the feature gate to GA.
    
    This feature was included in Kubevirt v0.55 and has been stable and widely used since then.
    
    Signed-off-by: Alvaro Romero <alromero@redhat.com>

--- tests/testsuite/kubevirtresource.go
+++ tests/testsuite/kubevirtresource.go
@@ -114,7 +114,6 @@ func AdjustKubeVirtResource() {
 		featuregate.VirtIOFSStorageVolumeGate,
 		featuregate.DownwardMetricsFeatureGate,
 		featuregate.WorkloadEncryptionSEV,
-		featuregate.VMExportGate,
 		featuregate.KubevirtSeccompProfile,
 		featuregate.ObjectGraph,
 		featuregate.DeclarativeHotplugVolumesGate,
