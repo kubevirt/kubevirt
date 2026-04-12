8c1da0efee9a8f957712b049b6a1acc92009d68a
Brian Carey <brian.carey@protonmail.com>
2026-04-12 11:03:33 +0100

    build: disable cc toolchain resolution for alt virtctl platforms
    cc toolchain resolution caused build failures for darwin and windows platforms
    
    As virtctl is a pure go binary this can be disabled for these binary builds
    
    Signed-off-by: Brian Carey <brian.carey@protonmail.com>

--- hack/bazel-test.sh
+++ hack/bazel-test.sh
@@ -4,7 +4,13 @@ source hack/common.sh
 source hack/bootstrap.sh
 source hack/config.sh
 
-WHAT=${WHAT:-"//staging/src/kubevirt.io/... //pkg/... //cmd/... //tools/... //tests/framework/... //tests/vmlogchecker/..."}
+default_test_query='tests(//staging/src/kubevirt.io/... + //pkg/... + //cmd/... + //tools/... + //tests/framework/... + //tests/vmlogchecker/...)'
+
+if [[ -n "${WHAT}" ]]; then
+    read -r -a bazel_test_targets <<<"${WHAT}"
+else
+    readarray -t bazel_test_targets < <(bazel query "${default_test_query}")
+fi
 rm -rf ${ARTIFACTS}/junit ${ARTIFACTS}/testlogs
 
 if [ "${CI}" == "true" ]; then
@@ -31,4 +37,4 @@ ${KUBEVIRT_DIR}/hack/bazel-race.sh
 
 bazel test \
     --config=${ARCHITECTURE} ${BAZEL_CS_CONFIG} \
-    --test_output=errors -- ${WHAT}
+    --test_output=errors -- "${bazel_test_targets[@]}"
