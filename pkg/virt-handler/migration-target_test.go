4d6640dd2b55eb9dc55d12118689e6f5bed94043
SAY-5 <SAY-5@users.noreply.github.com>
2026-04-16 15:10:13 -0700

    Fix occured typo in migration-target_test.go
    Signed-off-by: SAY-5 <SAY-5@users.noreply.github.com>

--- pkg/virt-handler/migration-target_test.go
+++ pkg/virt-handler/migration-target_test.go
@@ -768,7 +768,7 @@ var _ = Describe("VirtualMachineInstance migration target", func() {
 
 		updatedVMI, err := virtfakeClient.KubevirtV1().VirtualMachineInstances(metav1.NamespaceDefault).Get(context.TODO(), vmi.Name, metav1.GetOptions{})
 		Expect(err).ToNot(HaveOccurred())
-		// Update occured for labels but not spec
+		// Update occurred for labels but not spec
 		Expect(updatedVMI.Spec.Volumes).To(Equal(originalVMI.Spec.Volumes))
 		Expect(updatedVMI.Labels).To(Not(HaveKey(v1.MigrationTargetNodeNameLabel)))
 	})
