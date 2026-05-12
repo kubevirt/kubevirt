bc34e17cd66ed4e7585f15f3d5fee15b0ee8721a
Luboslav Pivarc <lpivarc@redhat.com>
2026-05-12 13:49:00 +0200

    Rest, console: Test ConsoleRequestHandler
    Introduce test case verifying we can read what server sent.
    
    Signed-off-by: Luboslav Pivarc <lpivarc@redhat.com>

--- pkg/virt-api/rest/console_test.go
+++ pkg/virt-api/rest/console_test.go
@@ -22,6 +22,7 @@ package rest
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
 	"crypto/tls"
 	"io"
 	"net/http"
@@ -249,6 +250,122 @@ var _ = Describe("Console Subresource api", func() {
 		close(readerChan)
 		Expect(<-streamDone).ToNot(HaveOccurred())
 
+	})
+
+	It("should proxy websocket data from handler to client", func() {
+		const vmiName = "testvmi"
+		// Create a fake handler server that echoes back messages
+		handlerServer := ghttp.NewTLSServer()
+		defer handlerServer.Close()
+
+		// More than our buffer
+		randomData := make([]byte, 10340)
+		_, err := rand.Read(randomData)
+		Expect(err).ToNot(HaveOccurred())
+
+		handlerServer.AppendHandlers(
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", "/v1/namespaces/default/virtualmachineinstances/"+vmiName+"/console"),
+				func(w http.ResponseWriter, r *http.Request) {
+					upgrader := websocket.Upgrader{CheckOrigin: func(*http.Request) bool { return true }}
+					conn, err := upgrader.Upgrade(w, r, nil)
+					Expect(err).ToNot(HaveOccurred())
+					defer conn.Close()
+
+					conn.WriteMessage(websocket.BinaryMessage, randomData)
+					conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+				},
+			),
+		)
+
+		// Create a running VMI with serial console
+		vmi := libvmi.New(
+			libvmi.WithName(vmiName),
+			libvmi.WithNamespace(metav1.NamespaceDefault),
+		)
+		vmi.Status.Phase = v1.Running
+		vmi.Status.NodeName = "test-node"
+		autoattach := true
+		vmi.Spec.Domain.Devices.AutoattachSerialConsole = &autoattach
+		_, err = virtClient.KubevirtV1().VirtualMachineInstances(metav1.NamespaceDefault).Create(context.TODO(), vmi, metav1.CreateOptions{})
+		Expect(err).ToNot(HaveOccurred())
+
+		// Extract the handler server's address and port
+		handlerAddr := handlerServer.Addr()
+		handlerParts := strings.Split(handlerAddr, ":")
+		handlerPort := handlerParts[len(handlerParts)-1]
+
+		// Set up mock handler pod for node lookup
+		kubeClient := k8sfake.NewClientset(
+			&k8scorev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "virt-handler-test",
+					Namespace: "kubevirt",
+					Labels: map[string]string{
+						"kubevirt.io": "virt-handler",
+					},
+				},
+				Spec: k8scorev1.PodSpec{
+					NodeName: "test-node",
+				},
+				Status: k8scorev1.PodStatus{
+					PodIP: "127.0.0.1",
+				},
+			},
+		)
+
+		// Replace the mock client's CoreV1 to return our fake kubeClient
+		mockVirtClient := kubecli.NewMockKubevirtClient(gomock.NewController(GinkgoT()))
+		mockVirtClient.EXPECT().CoreV1().Return(kubeClient.CoreV1()).AnyTimes()
+		mockVirtClient.EXPECT().VirtualMachineInstance(metav1.NamespaceDefault).Return(virtClient.KubevirtV1().VirtualMachineInstances(metav1.NamespaceDefault)).AnyTimes()
+		mockVirtClient.EXPECT().VirtualMachineInstance("").Return(virtClient.KubevirtV1().VirtualMachineInstances("")).AnyTimes()
+
+		// Create app with updated mock that has CoreV1 setup
+		handlerPortInt, err := strconv.Atoi(handlerPort)
+		Expect(err).ToNot(HaveOccurred())
+		app = NewSubresourceAPIApp(mockVirtClient, handlerPortInt, &tls.Config{InsecureSkipVerify: true}, config)
+
+		// Create API server that wraps ConsoleRequestHandler
+		apiServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			restReq := restful.NewRequest(r)
+			// restReq.
+			restReq.PathParameters()["namespace"] = metav1.NamespaceDefault
+			restReq.PathParameters()["name"] = vmiName
+
+			app.ConsoleRequestHandler(restReq, restful.NewResponse(w))
+		}))
+		defer apiServer.Close()
+
+		// Use AsyncSubresourceHelper (from client-go) to connect as a real client would
+		clientConfig := rest.Config{
+			Host:            apiServer.URL,
+			TLSClientConfig: rest.TLSClientConfig{Insecure: true},
+		}
+
+		stream, err := corev1.AsyncSubresourceHelper(
+			&clientConfig,
+			"virtualmachineinstances",
+			metav1.NamespaceDefault,
+			vmiName,
+			"console",
+			url.Values{},
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		var out bytes.Buffer
+		readerChan := make(chan struct{})
+		defer close(readerChan)
+
+		streamDone := make(chan error, 1)
+		go func() {
+			streamDone <- stream.Stream(corev1.StreamOptions{
+				In:  blockReader(readerChan),
+				Out: &out,
+			})
+		}()
+
+		Eventually(out.Bytes).Should(Equal(randomData))
+		Expect(<-streamDone).ToNot(HaveOccurred())
 	})
 
 })
