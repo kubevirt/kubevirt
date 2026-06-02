714961706a1925ae71ba108ec49131681169d119
Adi Aloni <aaloni@redhat.com>
2026-06-02 15:02:34 +0300

    virtwrap, storage, driver: fix linter issues
    Signed-off-by: Adi Aloni <aaloni@redhat.com>

--- pkg/virt-launcher/virtwrap/storage/diskdriver/diskdriver.go
+++ pkg/virt-launcher/virtwrap/storage/diskdriver/diskdriver.go
@@ -34,6 +34,8 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/disksource"
 )
 
+const filePermissions = 0o600
+
 type ioChecker interface {
 	CheckBlockDevice(path string) (bool, error)
 	CheckFile(path string) (bool, error)
@@ -51,63 +53,68 @@ func NewMock() *Configurator {
 	return &Configurator{&mockIOChecker{}}
 }
 
-func (c *Configurator) SetDriverCacheMode(disk *api.Disk) error {
-	if disk == nil {
+func (c *Configurator) SetDriverCacheMode(d *api.Disk) error {
+	if d == nil {
 		return fmt.Errorf("unable to set a driver cache mode, disk is nil")
 	}
 
-	t := disksource.Resolve(*disk)
+	t := disksource.Resolve(*d)
 
 	if t.BackendPath() == "" {
-		if disk.Device == "cdrom" {
+		if d.Device == "cdrom" {
 			return nil
 		}
 		return fmt.Errorf("unable to set a driver cache mode, disk has no backend path")
 	}
 
-	var err error
-	supportDirectIO := true
-	mode := v1.DriverCache(disk.Driver.Cache)
+	mode := v1.DriverCache(d.Driver.Cache)
 
-	if mode == "" || mode == v1.CacheNone {
-		if t.BackendIsBlock() {
-			supportDirectIO, err = c.ioChecker.CheckBlockDevice(t.BackendPath())
+	switch mode {
+	case v1.CacheNone:
+		if !c.supportDirectIO(t, d.BackingStore) {
+			return fmt.Errorf("unable to use '%s' cache mode, file system where %s is stored does not support direct I/O", mode, t.BackendPath())
+		}
+	case "":
+		if c.supportDirectIO(t, d.BackingStore) {
+			mode = v1.CacheNone
 		} else {
-			supportDirectIO, err = c.ioChecker.CheckFile(t.BackendPath())
+			mode = v1.CacheWriteThrough
 		}
-		if err != nil {
-			log.Log.Reason(err).Errorf("Direct IO check failed for %s", t.BackendPath())
-		} else if !supportDirectIO {
-			log.Log.Infof("%s file system does not support direct I/O", t.BackendPath())
-		}
-		// when the disk is backed-up by another file, we need to also check if that
-		// file sits on a file system that supports direct I/O
-		if backingFile := disk.BackingStore; backingFile != nil {
-			backingFilePath := backingFile.Source.File
-			backFileDirectIOSupport, err := c.ioChecker.CheckFile(backingFilePath)
-			if err != nil {
-				log.Log.Reason(err).Errorf("Direct IO check failed for %s", backingFilePath)
-			} else if !backFileDirectIOSupport {
-				log.Log.Infof("%s backing file system does not support direct I/O", backingFilePath)
-			}
-			supportDirectIO = supportDirectIO && backFileDirectIOSupport
-		}
+	case v1.CacheWriteThrough, v1.CacheWriteBack:
 	}
 
-	if mode == v1.CacheNone && !supportDirectIO {
-		return fmt.Errorf("Unable to use '%s' cache mode, file system where %s is stored does not support direct I/O", mode, t.BackendPath())
-	}
-
-	if mode == "" && supportDirectIO {
-		mode = v1.CacheNone
-	} else if mode == "" && !supportDirectIO {
-		mode = v1.CacheWriteThrough
-	}
-
-	disk.Driver.Cache = string(mode)
+	d.Driver.Cache = string(mode)
 	log.Log.Infof("Driver cache mode for %s set to %s", t.BackendPath(), mode)
 
 	return nil
+}
+
+func (c *Configurator) supportDirectIO(t disksource.ResolvedDiskSource, backingStore *api.BackingStore) bool {
+	supported := c.checkPathDirectIO(t.BackendPath(), t.BackendIsBlock())
+
+	// when the disk is backed-up by another file, we need to also check if that
+	// file sits on a file system that supports direct I/O
+	if backingStore != nil && backingStore.Source != nil {
+		supported = supported && c.checkPathDirectIO(backingStore.Source.File, false)
+	}
+
+	return supported
+}
+
+func (c *Configurator) checkPathDirectIO(path string, isBlock bool) bool {
+	var supported bool
+	var err error
+	if isBlock {
+		supported, err = c.ioChecker.CheckBlockDevice(path)
+	} else {
+		supported, err = c.ioChecker.CheckFile(path)
+	}
+	if err != nil {
+		log.Log.Reason(err).Errorf("Direct IO check failed for %s", path)
+	} else if !supported {
+		log.Log.Infof("%s file system does not support direct I/O", path)
+	}
+	return supported
 }
 
 func SetOptimalIOMode(d *api.Disk) {
@@ -154,28 +161,32 @@ func (c *directIOChecker) CheckBlockDevice(path string) (bool, error) {
 func (c *directIOChecker) CheckFile(path string) (bool, error) {
 	flags := syscall.O_RDONLY
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-		flags = flags | syscall.O_CREAT
+		flags |= syscall.O_CREAT
 		defer os.Remove(path)
 	}
 	return c.check(path, flags)
 }
 
 func (c *directIOChecker) check(path string, flags int) (bool, error) {
-	// #nosec No risk for path injection as we only open the file, not read from it. The function leaks only whether the directory to `path` exists.
-	f, err := os.OpenFile(path, flags|syscall.O_DIRECT, 0600)
+	// #nosec No risk for path injection as we only open the file, not read from it.
+	// The function leaks only whether the directory to `path` exists.
+	f, err := os.OpenFile(path, flags|syscall.O_DIRECT, filePermissions)
+	if err == nil {
+		defer util.CloseIOAndCheckErr(f, nil)
+		return true, nil
+	}
+
+	var pathErr *os.PathError
+	if !errors.As(err, &pathErr) || pathErr.Err != syscall.EINVAL {
+		return false, err
+	}
+
+	f, err = os.OpenFile(path, flags&^syscall.O_DIRECT, filePermissions)
 	if err != nil {
-		if err, ok := err.(*os.PathError); ok && err.Err == syscall.EINVAL {
-			// #nosec No risk for path injection as we only open the file, not read from it. The function leaks only whether the directory to `path` exists.
-			f, err := os.OpenFile(path, flags & ^syscall.O_DIRECT, 0600)
-			if err == nil {
-				defer util.CloseIOAndCheckErr(f, nil)
-				return false, nil
-			}
-		}
 		return false, err
 	}
 	defer util.CloseIOAndCheckErr(f, nil)
-	return true, nil
+	return false, nil
 }
 
 type mockIOChecker struct{}
