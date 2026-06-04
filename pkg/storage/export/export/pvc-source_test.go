fb8693b19aea9334c2c8fe8fe65f949742f78032
Alexander Wels <awels@redhat.com>
2026-06-04 08:03:09 -0500

    Set pod affinity on VMExport pod when CBT is enabled
    When CBT is enabled, we need to set pod affinity
    on the VMExport pod to co-locate with the
    virt-launcher pod. This allows the VMExport pod
    to have a loop back to the virt-launcher pod
    instead of using the network.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/storage/export/export/pvc-source_test.go
+++ pkg/storage/export/export/pvc-source_test.go
@@ -85,6 +85,7 @@ var _ = Describe("PVC source", func() {
 		clusterPreferenceInformer   cache.SharedIndexInformer
 		controllerRevisionInformer  cache.SharedIndexInformer
 		vmBackupInformer            cache.SharedIndexInformer
+		vmBackupTrackerInformer     cache.SharedIndexInformer
 		rqInformer                  cache.SharedIndexInformer
 		nsInformer                  cache.SharedIndexInformer
 		k8sClient                   *k8sfake.Clientset
@@ -122,6 +123,7 @@ var _ = Describe("PVC source", func() {
 		clusterPreferenceInformer, _ = testutils.NewFakeInformerFor(&instancetypev1beta1.VirtualMachineClusterPreference{})
 		controllerRevisionInformer, _ = testutils.NewFakeInformerFor(&appsv1.ControllerRevision{})
 		vmBackupInformer, _ = testutils.NewFakeInformerFor(&backupv1.VirtualMachineBackup{})
+		vmBackupTrackerInformer, _ = testutils.NewFakeInformerFor(&backupv1.VirtualMachineBackupTracker{})
 		rqInformer, _ = testutils.NewFakeInformerFor(&k8sv1.ResourceQuota{})
 		nsInformer, _ = testutils.NewFakeInformerFor(&k8sv1.Namespace{})
 		fakeVolumeSnapshotProvider = &MockVolumeSnapshotProvider{
@@ -169,6 +171,7 @@ var _ = Describe("PVC source", func() {
 			ClusterPreferenceInformer:   clusterPreferenceInformer,
 			ControllerRevisionInformer:  controllerRevisionInformer,
 			VMBackupInformer:            vmBackupInformer,
+			VMBackupTrackerInformer:     vmBackupTrackerInformer,
 		}
 		initCert = func(ctrl *VMExportController) {
 			ctrl.caCertManager.Start()
