bae9ab4e3d3b4aabaf39ce2db70637c9f6a33924
Nir Dothan <ndothan@redhat.com>
2026-06-13 19:32:48 +0300

    virtwrap, api: Add IPs to schema
    In preparation for setting explicit IP addresses for passt, in order
    to fix [1], introduce the IPs slice into the interface virt API.
    In addition, in order to use the existing NetworkHandler interface,
    add a method signature to provide discovered interface IP addresses.
    Add dummy implementation to configurator test in order not to break
    interface implementation.
    
    Run make generate to introduce changes in generated code.
    Autogenerated artifacts include:
    - pkg/network/driver/generated_mock_common.go
    - pkg/virt-launcher/virtwrap/api/deepcopy_generated.go
    
    [1] [1] https://github.com/kubevirt/kubevirt/issues/17841
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>
    
    Assisted by: Claude Sonnet 4.5 <noreply@anthropic.com>

--- pkg/virt-launcher/virtwrap/api/schema.go
+++ pkg/virt-launcher/virtwrap/api/schema.go
@@ -1012,6 +1012,7 @@ type Interface struct {
 	ACPI                *ACPI                  `xml:"acpi,omitempty"`
 	Backend             *InterfaceBackend      `xml:"backend,omitempty"`
 	PortForward         []InterfacePortForward `xml:"portForward,omitempty"`
+	IPs                 []InterfaceIP          `xml:"ip,omitempty"`
 }
 
 type InterfacePortForward struct {
@@ -1031,6 +1032,12 @@ type InterfacePortForwardRange struct {
 type InterfaceBackend struct {
 	Type    string `xml:"type,attr,omitempty"`
 	LogFile string `xml:"logFile,attr,omitempty"`
+}
+
+type InterfaceIP struct {
+	Family  string `xml:"family,attr"`
+	Address string `xml:"address,attr"`
+	Prefix  string `xml:"prefix,attr,omitempty"`
 }
 
 type ACPI struct {
