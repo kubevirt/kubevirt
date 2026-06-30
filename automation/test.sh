a7e351e0b53e58c84829f46078e2623674c78b7c
Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>
2026-06-30 10:09:59 +0530

    decouple the base image build from main kubevirt build.
    Signed-off-by: Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>

--- automation/test.sh
+++ automation/test.sh
@@ -67,6 +67,7 @@ case "$TARGET" in
     echo "picking the default provider for windows tests"
     ;;
   *sig-network*)
+    export KUBEVIRT_NO_BAZEL=true
     export KUBEVIRT_WITH_DYN_NET_CTRL="${KUBEVIRT_WITH_DYN_NET_CTRL:-false}"
     export KUBEVIRT_NUM_NODES=3
     export KUBEVIRT_WITH_CNAO=true
@@ -87,6 +88,7 @@ case "$TARGET" in
     export KUBEVIRT_VERBOSITY=${KUBEVIRT_VERBOSITY:-"virtLauncher:3,virtHandler:3"}
     ;;
   *sig-storage*)
+    export KUBEVIRT_NO_BAZEL=true
     export KUBEVIRT_PROVIDER=${TARGET/-sig-storage/}
     export KUBEVIRT_STORAGE="rook-ceph-default"
     export KUBEVIRT_DEPLOY_NFS_CSI=true
@@ -129,6 +131,7 @@ case "$TARGET" in
     export KUBEVIRT_PROVIDER=${TARGET/-sig-compute-serial/}
     ;;
   *sig-compute-parallel*)
+    export KUBEVIRT_NO_BAZEL=true
     export KUBEVIRT_PROVIDER=${TARGET/-sig-compute-parallel/}
     ;;
   *sig-compute-conformance*)
@@ -316,7 +319,7 @@ build_images() {
     # we repeat the build images action
     local tries=3
     for i in $(seq 1 $tries); do
-        make bazel-build-images && return
+        make container-build-images && return
         rc=$?
     done
 
@@ -702,7 +705,7 @@ if [[ $TARGET =~ sig-storage ]]; then
   kubectl wait -n ${namespace} kv kubevirt --for condition=Available --timeout 5m
 fi
 
-
+unset KUBEVIRT_NO_BAZEL
 # Run functional tests
 FUNC_TEST_ARGS=$ginko_params FUNC_TEST_LABEL_FILTER="--label-filter=(!flake-check)&&(${label_filter})" make functest
 
