5d5bb0b7c8ad1003fbef7eb3b5d555350cec5320
Brian Carey <brian.carey@protonmail.com>
2026-07-07 21:45:09 +0100

    virt-launcher: fix goconst linter issues
    Signed-off-by: Brian Carey <brian.carey@protonmail.com>

--- pkg/virt-launcher/virtwrap/converter/network/passt_test.go
+++ pkg/virt-launcher/virtwrap/converter/network/passt_test.go
@@ -34,9 +34,16 @@ import (
 )
 
 const (
-	ifaceTypeVhostUser = "vhostuser"
+	defaultNetworkName      = "default"
+	defaultPodInterfaceName = "eth0"
+	excludeYes              = "yes"
+	ifaceTypeVhostUser      = "vhostuser"
 	//nolint:gosec //linter is confusing passt for password
 	passtLogFilePath           = "/var/run/kubevirt/passt.log"
+	portForwardProtocolTCP     = "tcp"
+	portForwardProtocolUDP     = "udp"
+	portProtocolTCP            = "TCP"
+	portProtocolUDP            = "UDP"
 	virtioModel                = "virtio-non-transitional"
 	multusSecondaryNetworkName = "multus-network"
 	nadName                    = "multus-nad"
@@ -57,24 +64,27 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "not-default", PodInterfaceName: "eth0"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{
+								Name:             "not-default",
+								PodInterfaceName: defaultPodInterfaceName,
+							}),
 						),
 					),
 				),
 			),
 			Entry("no matching status entry",
 				libvmi.New(
-					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 				),
 			),
 			Entry("empty PodInterfaceName in status",
 				libvmi.New(
-					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "default"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: defaultNetworkName}),
 						),
 					),
 				),
@@ -85,7 +95,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "default"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: defaultNetworkName}),
 						),
 					),
 				),
@@ -99,7 +109,10 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: iface.Name, PodInterfaceName: "eth0"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{
+								Name:             iface.Name,
+								PodInterfaceName: defaultPodInterfaceName,
+							}),
 						),
 					),
 				)
@@ -113,14 +126,14 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			},
 			Entry("passt binding",
 				newPasstInterface(),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 				),
 			),
 			Entry("PCI address",
 				newPasstInterface(withIfacePCI("0000:02:02.0")),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 					withPCIAddress("0000:02:02.0"),
@@ -128,7 +141,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			),
 			Entry("MAC address",
 				newPasstInterface(withIfaceMAC("02:02:02:02:02:02")),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 					withMAC("02:02:02:02:02:02"),
@@ -136,7 +149,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			),
 			Entry("ACPI address",
 				newPasstInterface(withIfaceACPI(2)),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 					withACPI(2),
@@ -144,27 +157,27 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			),
 			Entry("non virtio model",
 				newPasstInterface(withIfaceModel("e1000")),
-				newPasstDomainInterface("default", "e1000",
+				newPasstDomainInterface(defaultNetworkName, "e1000",
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 				),
 			),
 			Entry("tcp ports (should forward tcp ports only)",
 				newPasstInterface(withIfacePorts([]v1.Port{
-					{Protocol: "TCP", Port: 1},
-					{Protocol: "TCP", Port: 4},
+					{Protocol: portProtocolTCP, Port: 1},
+					{Protocol: portProtocolTCP, Port: 4},
 				})),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardTCP([]uint{1, 4}),
 				),
 			),
 			Entry("udp ports (should forward udp ports only)",
 				newPasstInterface(withIfacePorts([]v1.Port{
-					{Protocol: "UDP", Port: 2},
-					{Protocol: "UDP", Port: 3},
+					{Protocol: portProtocolUDP, Port: 2},
+					{Protocol: portProtocolUDP, Port: 3},
 				})),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardUDP([]uint{2, 3}),
 				),
@@ -172,11 +185,11 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			Entry("both tcp and udp ports",
 				newPasstInterface(withIfacePorts([]v1.Port{
 					{Port: 1},
-					{Protocol: "UDP", Port: 2},
-					{Protocol: "UDP", Port: 3},
-					{Protocol: "TCP", Port: 4},
+					{Protocol: portProtocolUDP, Port: 2},
+					{Protocol: portProtocolUDP, Port: 3},
+					{Protocol: portProtocolTCP, Port: 4},
 				})),
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardTCP([]uint{1, 4}),
 					withPasstPortForwardUDP([]uint{2, 3}),
@@ -195,16 +208,19 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			},
 			Entry("virtio transitional enabled",
 				libvmi.New(
-					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "default", PodInterfaceName: "eth0"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{
+								Name:             defaultNetworkName,
+								PodInterfaceName: defaultPodInterfaceName,
+							}),
 						),
 					),
 				),
 				"virtio-transitional",
-				newPasstDomainInterface("default", "virtio-transitional",
+				newPasstDomainInterface(defaultNetworkName, "virtio-transitional",
 					withPasstBackend(),
 					withPasstPortForwardAll(),
 				),
@@ -212,16 +228,19 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			Entry("isitio proxy injection enabled",
 				libvmi.New(
 					libvmi.WithAnnotation("sidecar.istio.io/inject", "true"),
-					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+					libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 					libvmi.WithNetwork(v1.DefaultPodNetwork()),
 					libvmistatus.WithStatus(
 						libvmistatus.New(
-							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "default", PodInterfaceName: "eth0"}),
+							libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{
+								Name:             defaultNetworkName,
+								PodInterfaceName: defaultPodInterfaceName,
+							}),
 						),
 					),
 				),
 				virtioModel,
-				newPasstDomainInterface("default", virtioModel,
+				newPasstDomainInterface(defaultNetworkName, virtioModel,
 					withPasstBackend(),
 					withPasstPortForwardIstio(),
 				),
@@ -230,7 +249,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 
 		It("should not override other interfaces", func() {
 			vmi := libvmi.New(
-				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 				libvmi.WithInterface(v1.Interface{
 					Name: multusSecondaryNetworkName,
 					InterfaceBindingMethod: v1.InterfaceBindingMethod{
@@ -242,7 +261,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 				libvmistatus.WithStatus(
 					libvmistatus.New(
 						libvmistatus.WithInterfaceStatus(
-							v1.VirtualMachineInstanceNetworkInterface{Name: "default", PodInterfaceName: "eth0"}),
+							v1.VirtualMachineInstanceNetworkInterface{Name: defaultNetworkName, PodInterfaceName: defaultPodInterfaceName}),
 						libvmistatus.WithInterfaceStatus(
 							v1.VirtualMachineInstanceNetworkInterface{Name: multusSecondaryNetworkName, PodInterfaceName: "eth1"}),
 					),
@@ -257,7 +276,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			var domain api.Domain
 			Expect(configurator.Configure(vmi, &domain)).To(Succeed())
 
-			expectedPasstIface := newPasstDomainInterface("default", virtioModel,
+			expectedPasstIface := newPasstDomainInterface(defaultNetworkName, virtioModel,
 				withPasstBackend(),
 				withPasstPortForwardAll(),
 			)
@@ -270,11 +289,14 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 
 		It("should set domain interface correctly when executed more than once", func() {
 			vmi := libvmi.New(
-				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 				libvmi.WithNetwork(v1.DefaultPodNetwork()),
 				libvmistatus.WithStatus(
 					libvmistatus.New(
-						libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{Name: "default", PodInterfaceName: "eth0"}),
+						libvmistatus.WithInterfaceStatus(v1.VirtualMachineInstanceNetworkInterface{
+							Name:             defaultNetworkName,
+							PodInterfaceName: defaultPodInterfaceName,
+						}),
 					),
 				),
 			)
@@ -283,7 +305,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 				network.WithVirtioModel(virtioModel),
 			)
 
-			expectedDomainIface := newPasstDomainInterface("default", virtioModel,
+			expectedDomainIface := newPasstDomainInterface(defaultNetworkName, virtioModel,
 				withPasstBackend(),
 				withPasstPortForwardAll(),
 			)
@@ -300,11 +322,11 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 
 		It("should set domain interface source link to the optional one if exists", func() {
 			vmi := libvmi.New(
-				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding("default")),
+				libvmi.WithInterface(libvmi.InterfaceDeviceWithPasstBinding(defaultNetworkName)),
 				libvmi.WithNetwork(v1.DefaultPodNetwork()),
 			)
 			vmi.Status.Interfaces = []v1.VirtualMachineInstanceNetworkInterface{
-				{Name: "default", PodInterfaceName: "ovn-udn1"},
+				{Name: defaultNetworkName, PodInterfaceName: "ovn-udn1"},
 			}
 
 			configurator := network.NewDomainConfigurator(network.WithVirtioModel(virtioModel))
@@ -312,7 +334,7 @@ var _ = Describe("Passt Network Domain Configurator", func() {
 			var domain api.Domain
 			Expect(configurator.Configure(vmi, &domain)).To(Succeed())
 
-			expectedDomainIface := newPasstDomainInterface("default", virtioModel,
+			expectedDomainIface := newPasstDomainInterface(defaultNetworkName, virtioModel,
 				withPasstBackend(),
 				withPasstPortForwardAll(),
 				withSourceDevice("ovn-udn1"),
@@ -331,7 +353,7 @@ func newPasstDomainInterface(networkName, modelType string, options ...passtOpti
 		Model: &api.Model{Type: modelType},
 		Type:  ifaceTypeVhostUser,
 		Source: api.InterfaceSource{
-			Device: "eth0",
+			Device: defaultPodInterfaceName,
 		},
 	}
 
@@ -354,8 +376,8 @@ func withPasstBackend() passtOption {
 func withPasstPortForwardAll() passtOption {
 	return func(iface *api.Interface) {
 		iface.PortForward = []api.InterfacePortForward{
-			{Proto: "tcp"},
-			{Proto: "udp"},
+			{Proto: portForwardProtocolTCP},
+			{Proto: portForwardProtocolUDP},
 		}
 	}
 }
@@ -369,7 +391,7 @@ func withPasstPortForwardTCP(ports []uint) passtOption {
 		if iface.PortForward == nil {
 			iface.PortForward = []api.InterfacePortForward{}
 		}
-		iface.PortForward = append(iface.PortForward, api.InterfacePortForward{Proto: "tcp", Ranges: ranges})
+		iface.PortForward = append(iface.PortForward, api.InterfacePortForward{Proto: portForwardProtocolTCP, Ranges: ranges})
 	}
 }
 
@@ -382,7 +404,7 @@ func withPasstPortForwardUDP(ports []uint) passtOption {
 		if iface.PortForward == nil {
 			iface.PortForward = []api.InterfacePortForward{}
 		}
-		iface.PortForward = append(iface.PortForward, api.InterfacePortForward{Proto: "udp", Ranges: ranges})
+		iface.PortForward = append(iface.PortForward, api.InterfacePortForward{Proto: portForwardProtocolUDP, Ranges: ranges})
 	}
 }
 
@@ -390,18 +412,18 @@ func withPasstPortForwardIstio() passtOption {
 	return func(iface *api.Interface) {
 		iface.PortForward = []api.InterfacePortForward{
 			{
-				Proto: "tcp",
+				Proto: portForwardProtocolTCP,
 				Ranges: []api.InterfacePortForwardRange{
-					{Start: 15000, Exclude: "yes"},
-					{Start: 15001, Exclude: "yes"},
-					{Start: 15004, Exclude: "yes"},
-					{Start: 15006, Exclude: "yes"},
-					{Start: 15008, Exclude: "yes"},
-					{Start: 15009, Exclude: "yes"},
-					{Start: 15020, Exclude: "yes"},
-					{Start: 15021, Exclude: "yes"},
-					{Start: 15053, Exclude: "yes"},
-					{Start: 15090, Exclude: "yes"},
+					{Start: 15000, Exclude: excludeYes},
+					{Start: 15001, Exclude: excludeYes},
+					{Start: 15004, Exclude: excludeYes},
+					{Start: 15006, Exclude: excludeYes},
+					{Start: 15008, Exclude: excludeYes},
+					{Start: 15009, Exclude: excludeYes},
+					{Start: 15020, Exclude: excludeYes},
+					{Start: 15021, Exclude: excludeYes},
+					{Start: 15053, Exclude: excludeYes},
+					{Start: 15090, Exclude: excludeYes},
 				},
 			},
 		}
