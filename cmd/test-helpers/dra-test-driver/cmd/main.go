c5f00f8e4c679b7f20398f48854f7cd365fb97ed
Nir Dothan <ndothan@redhat.com>
2026-07-12 14:37:31 +0300

    cmd, test-helpers, dra: Add basic DRA plugin registration
    Initialize minimal DRA driver using the upstream kubeletplugin library
    for plugin registration, gRPC serving, and kubelet integration.
    Implements stub PrepareResourceClaims/UnprepareResourceClaims handlers.
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>
    
    Assisted by: Claude Sonnet 4.5 <noreply@anthropic.com>

---
+++ cmd/test-helpers/dra-test-driver/cmd/main.go
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ *
+ */
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/dynamic-resource-allocation/kubeletplugin"
+
+	"kubevirt.io/kubevirt/cmd/test-helpers/dra-test-driver/pkg/driver"
+)
+
+const driverName = "hostpath.dra.kubevirt.io"
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nodeName := os.Getenv("NODE_NAME")
+	if nodeName == "" {
+		log.Fatal("NODE_NAME environment variable is required")
+	}
+
+	d := &driver.Driver{}
+	helper, err := kubeletplugin.Start(ctx, d,
+		kubeletplugin.DriverName(driverName),
+		kubeletplugin.KubeClient(clientset),
+		kubeletplugin.NodeName(nodeName),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DRA plugin started")
+	<-ctx.Done()
+	helper.Stop()
+}
