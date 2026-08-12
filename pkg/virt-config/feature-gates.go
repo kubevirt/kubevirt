3da5d20e6bc26991978f04f8391b1cc47eab5ceb
svarnam <svarnam@nvidia.com>
2026-08-12 14:01:36 +0530

    Revert "VEP 19: Graduate IBM Secure Execution feature"
    This reverts commit 1107d4d64456f89c9a4a5c5b348051723bc2b72b.

--- pkg/virt-config/feature-gates.go
+++ pkg/virt-config/feature-gates.go
@@ -151,6 +151,10 @@ func (config *ClusterConfig) DeclarativeHotplugVolumesEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.DeclarativeHotplugVolumesGate)
 }
 
+func (config *ClusterConfig) SecureExecutionEnabled() bool {
+	return config.isFeatureGateEnabled(featuregate.SecureExecution)
+}
+
 func (config *ClusterConfig) PasstBindingEnabled() bool {
 	return config.isFeatureGateEnabled(featuregate.PasstBinding)
 }
