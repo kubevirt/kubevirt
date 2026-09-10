50bc34a3698303013e7f91f4a0e786fdca0c64ed
Michal Skrivanek <michal.skrivanek@redhat.com>
2026-09-10 09:24:14 +0200

    regenerate to pick up the new libvirt versions
    run make rpm-deps
    
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="0927246b156b00f8670e67c25ebb72f934f16948"
+sandbox_hash="15df2de43742a982e25338e9a974b2fb6d1c4068"
 
 function kubevirt::bootstrap::regenerate() {
     (
