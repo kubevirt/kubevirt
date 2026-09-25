9c2246908713f87fab3d0a224002a773711e3a8a
Shresth Juyal <shresthajuyal96@gmail.com>
2026-09-26 00:42:50 +0530

    fix: correct IPv6 masquerade route test
    Signed-off-by: Shresth Juyal <shresthajuyal96@gmail.com>

--- tests/network/vmi_networking.go
+++ tests/network/vmi_networking.go
@@ -582,7 +582,7 @@ var _ = Describe(SIG("[rfe_id:694][crit:medium][vendor:cnv-qe@redhat.com][level:
 				Expect(console.RunCommand(vmi, fmt.Sprintf("ip -6 addr show dev eth0 | grep -q '%s'", expectedIP), 15*time.Second)).To(Succeed())
 
 				By("checking that default IPv6 route is automatically configured via masquerade gateway")
-				Expect(console.RunCommand(vmi, fmt.Sprintf("ip -6 route show default | grep -q 'default via %s'; echo $?\n", gatewayIP), 15*time.Second)).To(Succeed())
+				Expect(console.RunCommand(vmi, fmt.Sprintf("ip -6 route show default | grep -q 'default via %s'", gatewayIP), 15*time.Second)).To(Succeed())
 
 				By("Checking ping (IPv6) from vmi to default IPv6 masquerade gateway")
 				Expect(libnet.PingFromVMConsole(vmi, gatewayIP)).To(Succeed())
